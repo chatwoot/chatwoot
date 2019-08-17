bacc87ad8c351ea6917b3bce3ee5c9d33082d6d1
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2019-08-17 17:49:59 +0530

    Add letter opener to view emails locally

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,9 @@ Rails.application.configure do
   end
 
   config.action_mailer.default_url_options = { :host => 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => 'localhost', :port => 1025 }
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
