efb2d8d4f3e733bf0546fdb983c2138aba255ddb
Matheus Richard <matheusrichardt@gmail.com>
2019-10-02 16:26:32 -0300

    Refactor conversation model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,11 +4,11 @@ class Conversation < ApplicationRecord
   validates :account_id, presence: true
   validates :inbox_id, presence: true
 
-  enum status: [ :open, :resolved ]
+  enum status: [:open, :resolved]
 
   scope :latest, -> { order(created_at: :desc) }
   scope :unassigned, -> { where(assignee_id: nil) }
-  scope :assigned_to, -> (agent) { where(assignee_id: agent.id) }
+  scope :assigned_to, ->(agent) { where(assignee_id: agent.id) }
 
   belongs_to :account
   belongs_to :inbox
@@ -25,46 +25,45 @@ class Conversation < ApplicationRecord
 
   after_create :send_events, :run_round_robin
 
-
   acts_as_taggable_on :labels
 
-  def update_assignee(agent=nil)
+  def update_assignee(agent = nil)
     self.assignee = agent
-    self.save!
+    save!
   end
 
-  def update_labels(labels=nil)
+  def update_labels(labels = nil)
     self.label_list = labels
-    self.save!
+    save!
   end
 
   def toggle_status
-    if open?
-      self.status = :resolved
-    else
-      self.status = :open
-    end
-    self.save! ? true : false
+    self.status = if open?
+                    :resolved
+                  else
+                    :open
+                  end
+    save! ? true : false
   end
 
   def lock!
     self.locked = true
-    self.save!
+    save!
   end
 
   def unlock!
     self.locked = false
-    self.save!
+    save!
   end
 
   def unread_messages
     # +1 is a hack to avoid https://makandracards.com/makandra/1057-why-two-ruby-time-objects-are-not-equal-although-they-appear-to-be
     # ente budhiparamaya neekam kandit entu tonunu?
-    messages.where("EXTRACT(EPOCH FROM created_at) > (?)", agent_last_seen_at.to_i + 1)
+    messages.where('EXTRACT(EPOCH FROM created_at) > (?)', agent_last_seen_at.to_i + 1)
   end
 
   def unread_incoming_messages
-    messages.incoming.where("EXTRACT(EPOCH FROM created_at) > (?)", agent_last_seen_at.to_i + 1)
+    messages.incoming.where('EXTRACT(EPOCH FROM created_at) > (?)', agent_last_seen_at.to_i + 1)
   end
 
   def push_event_data
@@ -75,7 +74,7 @@ class Conversation < ApplicationRecord
         assignee: assignee
       },
       id: display_id,
-      messages: [last_message.try(:push_event_data) ],
+      messages: [last_message.try(:push_event_data)],
       inbox_id: inbox_id,
       status: status_before_type_cast.to_i,
       timestamp: created_at.to_i,
@@ -103,19 +102,18 @@ class Conversation < ApplicationRecord
   end
 
   def send_email_notification_to_assignee
-    if assignee_id_changed? && assignee_id.present? && !self_assign?(assignee_id)
-      AssignmentMailer.conversation_assigned(self, self.assignee).deliver
-    end
+    AssignmentMailer.conversation_assigned(self, assignee).deliver if assignee_id_changed? && assignee_id.present? && !self_assign?(assignee_id)
   end
 
   def self_assign?(assignee_id)
     return false unless Current.user
+
     Current.user.id == assignee_id
   end
 
   def set_display_id
     self.display_id = loop do
-      disp_id = self.account.conversations.maximum("display_id").to_i + 1
+      disp_id = account.conversations.maximum('display_id').to_i + 1
       break disp_id unless account.conversations.exists?(display_id: disp_id)
     end
   end
@@ -123,8 +121,8 @@ class Conversation < ApplicationRecord
   def create_activity
     return unless Current.user
 
-    self.messages.create(activity_message_params(status_changed_message)) if status_changed?
-    self.messages.create(activity_message_params(assignee_changed_message)) if assignee_id_changed?
+    messages.create(activity_message_params(status_changed_message)) if status_changed?
+    messages.create(activity_message_params(assignee_changed_message)) if assignee_id_changed?
   end
 
   def status_changed_message
@@ -139,7 +137,7 @@ class Conversation < ApplicationRecord
     "Conversation unassigned by #{Current.user.try(:name)}"
   end
 
-  def activity_message_params content
+  def activity_message_params(content)
     {
       account_id: account_id,
       inbox_id: inbox_id,
@@ -155,24 +153,15 @@ class Conversation < ApplicationRecord
     dispatcher_dispatch(ASSIGNEE_CHANGED) if assignee_id_changed?
   end
 
-  def resolve_conversation
-    if resolved? && assignee.present?
-      dispatcher_dispatch(CONVERSATION_RESOLVED)
-    end
-  end
-
   def dispatcher_dispatch(event_name)
     $dispatcher.dispatch(event_name, Time.zone.now, conversation: self)
   end
 
   def run_round_robin
-    if true #conversation.account.has_feature?(round_robin)
-      if true #conversation.account.round_robin_enabled?
-        unless self.assignee #if not already assigned
-          new_assignee = self.inbox.next_available_agent
-          self.update_assignee(new_assignee) if new_assignee
-        end
-      end
-    end
+    # TODO: check if conversation.account.has_feature?(round_robin) && conversation.account.round_robin_enabled?
+    return if assignee
+
+    new_assignee = inbox.next_available_agent
+    update_assignee(new_assignee) if new_assignee
   end
 end
