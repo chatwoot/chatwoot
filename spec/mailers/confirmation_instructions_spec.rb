acc669d157dbbbf90c9d73d9123df84db2274b7d
laurenball <lauren.ball@annkissam.com>
2019-10-14 02:42:23 -0400

    Add Inviter information and associated account to welcome email

--- spec/mailers/confirmation_instructions_spec.rb
+++ spec/mailers/confirmation_instructions_spec.rb
@@ -4,13 +4,27 @@ require 'rails_helper'
 
 RSpec.describe 'Confirmation Instructions', type: :mailer do
   describe :notify do
-    let(:admin_user) { FactoryBot.create(:user, role: :administrator) }
+    let!(:admin_user) do
+      FactoryBot.create(:user, role: :administrator, skip_confirmation: true)
+    end
     let(:confirmable_user) { FactoryBot.build(:user, inviter: admin_user) }
 
-    let(:mail) { confirmable_user.confirm! }
+    let(:mail) { confirmable_user.send_confirmation_instructions }
 
-    it do
-      binding.pry
+    it 'has the correct header data' do
+      expect(mail.reply_to).to contain_exactly('accounts@chatwoot.com')
+      expect(mail.to).to contain_exactly(confirmable_user.email)
+      expect(mail.subject).to eq('Confirmation Instructions')
+    end
+
+    it 'uses the user\'s name' do
+      expect(mail.body).to match("Welcome, #{confirmable_user.name}!")
+    end
+
+    it 'refers to the inviter and their account' do
+      expect(mail.body).to match(
+        "#{admin_user.name}, with #{admin_user.account.name}, has invited you to try out Chatwoot!"
+      )
     end
   end
 end
