8a7b1848d1f11c429d6970de0c2078b0af4a12d7
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2019-10-16 12:29:23 +0530

    Remove corejs option in preset-env
    Fixes #148

--- babel.config.js
+++ babel.config.js
@@ -32,7 +32,6 @@ module.exports = api => {
         {
           forceAllTransforms: true,
           useBuiltIns: 'entry',
-          corejs: 3,
           modules: false,
           exclude: ['transform-typeof-symbol'],
         },
