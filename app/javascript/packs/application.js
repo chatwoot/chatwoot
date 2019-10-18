79656cc83803f01c89945a61d87add675488f77e
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2019-10-18 12:35:35 +0530

    Add workbox config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -58,3 +58,16 @@ window.onload = () => {
   }).$mount('#app');
   window.pusher = vuePusher.init();
 };
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(registration => {
+        console.log('SW registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
