252beda0302ff044e882274deaad81f8b5c1fbf7
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2019-10-20 13:32:46 +0530

    Upgrade bourbon to ^6.0.0

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,13 @@
+/* eslint-disable */
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
-        flexbox: 'no-2009'
+        flexbox: 'no-2009',
       },
-      stage: 3
-    })
-  ]
-}
+      stage: 3,
+    }),
+  ],
+};
