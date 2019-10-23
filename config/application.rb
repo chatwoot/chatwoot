61becaedd2d13946046abeda4047e8cf0de019cd
Nursoltan Saipolda <nursoltan.s@gmail.com>
2019-10-23 09:01:24 +0800

    deleted coffee files & coffee-rails gem. disable js generation on application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Chatwoot
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators.javascripts = false
   end
 end
