e114182a0cc531899a5023bbd158a58e865cd467
Moisés Ñañez <moisesnandres@gmail.com>
2019-10-28 10:30:02 -0500

    [Enhancement] Move chart options to a variable (#189)

--- app/javascript/dashboard/components/widgets/chart/BarChart.js
+++ app/javascript/dashboard/components/widgets/chart/BarChart.js
@@ -3,41 +3,43 @@ import { Bar } from 'vue-chartjs';
 const fontFamily =
   '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 
+const chartOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+  legend: {
+    labels: {
+      fontFamily,
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        barPercentage: 1.9,
+        ticks: {
+          fontFamily,
+        },
+        gridLines: {
+          display: false,
+        },
+      },
+    ],
+    yAxes: [
+      {
+        ticks: {
+          fontFamily,
+        },
+        gridLines: {
+          display: false,
+        },
+      },
+    ],
+  },
+};
+
 export default {
   extends: Bar,
   props: ['collection'],
   mounted() {
-    this.renderChart(this.collection, {
-      responsive: true,
-      maintainAspectRatio: false,
-      legend: {
-        labels: {
-          fontFamily,
-        },
-      },
-      scales: {
-        xAxes: [
-          {
-            barPercentage: 1.9,
-            ticks: {
-              fontFamily,
-            },
-            gridLines: {
-              display: false,
-            },
-          },
-        ],
-        yAxes: [
-          {
-            ticks: {
-              fontFamily,
-            },
-            gridLines: {
-              display: false,
-            },
-          },
-        ],
-      },
-    });
+    this.renderChart(this.collection, chartOptions);
   },
 };
