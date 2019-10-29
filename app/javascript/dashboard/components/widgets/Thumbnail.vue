644e0da75c4394ce8c13b960b2ef352cb8965ad0
José Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2019-10-29 13:06:21 -0500

    [Enhancement] Add default avatar when profile image is not available (#188)
    * Add default avatar when agent image is not available
    
    * Remove fonts from avatar
    Separate non-computed style values

--- app/javascript/dashboard/components/widgets/Thumbnail.vue
+++ app/javascript/dashboard/components/widgets/Thumbnail.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="user-thumbnail-box" v-bind:style="{ height: size, width: size }">
-    <img v-bind:src="src" class="user-thumbnail">
-    <img class="source-badge" src="~dashboard/assets/images/fb-badge.png" v-if="badge === 'Facebook'">
+  <div class="user-thumbnail-box" :style="{ height: size, width: size }">
+    <img
+      v-if="!imgError && Boolean(src)"
+      id="image"
+      :src="src"
+      class="user-thumbnail"
+      @error="onImgError()"
+    />
+    <Avatar
+      v-else
+      :username="username"
+      class="user-thumbnail"
+      background-color="#1f93ff"
+      color="white"
+    >
+    </Avatar>
+    <img
+      v-if="badge === 'Facebook'"
+      id="badge"
+      class="source-badge"
+      src="~dashboard/assets/images/fb-badge.png"
+    />
   </div>
 </template>
 <script>
@@ -10,8 +29,14 @@
  * Src - source for round image
  * Size - Size of the thumbnail
  * Badge - Chat source indication { fb / telegram }
+ * Username - User name for avatar
  */
+import Avatar from './Avatar';
+
 export default {
+  components: {
+    Avatar,
+  },
   props: {
     src: {
       type: String,
@@ -23,6 +48,19 @@ export default {
     badge: {
       type: String,
       default: 'fb',
+    },
+    username: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      imgError: false,
+    };
+  },
+  methods: {
+    onImgError() {
+      this.imgError = true;
     },
   },
 };
