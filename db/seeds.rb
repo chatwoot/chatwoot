0abfe3055c6f4b99a33c30301335e58173a577d2
Ronald Walker <rondwalker22@gmail.com>
2019-11-17 19:27:41 -0800

    remove 'inbox' attribute on Contact and use join table(ContactInbox) instead (#214)

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ web_widget = Channel::WebWidget.create!(account: account, website_name: 'Acme', 
 inbox = Inbox.create!(channel: web_widget, account: account, name: 'Acme Support')
 InboxMember.create!(user: user, inbox: inbox)
 
-contact = Contact.create!(name: 'jane', email: 'jane@example.com', phone_number: '0000', inbox: inbox, account: account)
+contact = Contact.create!(name: 'jane', email: 'jane@example.com', phone_number: '0000', account: account)
+ContactInbox.create!(inbox: inbox, contact: contact, source_id: user.id)
 conversation = Conversation.create!(account: account, inbox: inbox, status: :open, assignee: user, contact: contact)
 Message.create!(content: 'Hello', account: account, inbox: inbox, conversation: conversation, message_type: :incoming)
