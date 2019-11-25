9c6a10176820ddec49a690a05bba86b3f484de42
Sojan Jose <sojan@pepalo.com>
2019-11-25 23:55:18 +0530

    [Chore] Add bullet gem  #249  (#281)
    * closes #249 add bullet gem
    
    - upgrade gems
    - fix the warning in the console
    - fix the deprecation message for where not

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,6 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-require 'action_cable/testing/rspec'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
