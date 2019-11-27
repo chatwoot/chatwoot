fb5bebda86d952b08bafdac35b43e5190eab4a3d
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2019-11-27 12:35:59 +0530

    Fix breaking migrations, revert migrations to 6.0

--- db/migrate/20190819010457_create_channels.rb
+++ db/migrate/20190819010457_create_channels.rb
@@ -1,4 +1,4 @@
-class CreateChannels < ActiveRecord::Migration[6.1]
+class CreateChannels < ActiveRecord::Migration[6.0]
   def change
     create_table :channels do |t|
       t.string :name
--- db/migrate/20191124091232_add_widget_color_to_web_widget.rb
+++ db/migrate/20191124091232_add_widget_color_to_web_widget.rb
@@ -1,4 +1,4 @@
-class AddWidgetColorToWebWidget < ActiveRecord::Migration[6.1]
+class AddWidgetColorToWebWidget < ActiveRecord::Migration[6.0]
   def change
     add_column :channel_web_widgets, :widget_color, :string, default: '#1f93ff'
   end
