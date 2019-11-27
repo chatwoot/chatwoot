5e568d78b0651f3b07d789596e75d06efa1dcaea
Pranav Raj S <pranavrajs@gmail.com>
2019-11-27 12:42:00 +0530

    Fix breaking migrations, revert migrations to 6.0 (#314)
    Closes #313

--- db/migrate/20191020173522_rename_sender_id_to_contact_in_conversation.rb
+++ db/migrate/20191020173522_rename_sender_id_to_contact_in_conversation.rb
@@ -1,4 +1,4 @@
-class RenameSenderIdToContactInConversation < ActiveRecord::Migration[6.1]
+class RenameSenderIdToContactInConversation < ActiveRecord::Migration[6.0]
   def change
     rename_column :conversations, :sender_id, :contact_id
   end
