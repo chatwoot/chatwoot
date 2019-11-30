31f7bf1dccacdbdd37f2239368fdd89400a2e24d
Mukesh Chaudhary <mukesh4139@gmail.com>
2019-11-30 19:09:55 +0530

    Annotations (#327)
    * Add annotate gem to the project
    * Annotate models, fixtures, factories and model_specs
    * Keep annotations only in Models
    * Remove unwanted changes in model specs
    * Exclude auto_annotate_models from rubocop

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: inboxes
+#
+#  id           :integer          not null, primary key
+#  channel_type :string
+#  name         :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  account_id   :integer          not null
+#  channel_id   :integer          not null
+#
+# Indexes
+#
+#  index_inboxes_on_account_id  (account_id)
+#
+
 class Inbox < ApplicationRecord
   validates :account_id, presence: true
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id              :integer          not null, primary key
+#  content         :text
+#  message_type    :integer          not null
+#  private         :boolean          default(FALSE)
+#  status          :integer          default("sent")
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  account_id      :integer          not null
+#  conversation_id :integer          not null
+#  fb_id           :string
+#  inbox_id        :integer          not null
+#  user_id         :integer
+#
+# Indexes
+#
+#  index_messages_on_conversation_id  (conversation_id)
+#
+
 class Message < ApplicationRecord
   include Events::Types
 
