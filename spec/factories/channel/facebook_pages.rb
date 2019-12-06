1164f0b59c258384a896c754584510055679980f
Pranav Raj S <pranavrajs@gmail.com>
2019-12-06 16:50:06 +0530

    Remove unnecessary slash in curl command (#349)
    * Remove unnecessary slash in curl command
    
    * bundle update
    
    * Fix rubocop errors

--- spec/factories/channel/facebook_pages.rb
+++ spec/factories/channel/facebook_pages.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :channel_facebook_page, class: Channel::FacebookPage do
+  factory :channel_facebook_page, class: 'Channel::FacebookPage' do
     name { Faker::Name.name }
     page_access_token { SecureRandom.uuid }
     user_access_token { SecureRandom.uuid }
