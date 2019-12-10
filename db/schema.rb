a0574eae630b0c457720d05cd5c87b078048247f
Sojan Jose <sojan@pepalo.com>
2019-12-10 10:29:35 +0530

    Feature: Profile API [#238] (#354)
    - api to update name and email
    - api to change password
    - api to  set profile pic
    - fixes update_attribute! deprecation warning
    - introducing active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_192301) do
+ActiveRecord::Schema.define(version: 2019_12_09_202758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,27 @@ ActiveRecord::Schema.define(version: 2019_12_04_192301) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "attachments", id: :serial, force: :cascade do |t|
@@ -207,7 +228,6 @@ ActiveRecord::Schema.define(version: 2019_12_04_192301) do
     t.string "unconfirmed_email"
     t.string "name", null: false
     t.string "nickname"
-    t.string "image"
     t.string "email"
     t.json "tokens"
     t.integer "account_id", null: false
@@ -223,6 +243,7 @@ ActiveRecord::Schema.define(version: 2019_12_04_192301) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "contact_inboxes", "contacts"
   add_foreign_key "contact_inboxes", "inboxes"
   add_foreign_key "users", "users", column: "inviter_id", on_delete: :nullify
