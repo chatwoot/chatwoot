cc25d6c1e7ee60c0a2fda5cba055fcf93d452760
Pranav Raj S <pranavrajs@gmail.com>
2019-12-14 18:14:35 +0530

    [Enhancement] Select widget_color while creating inbox (#362)
    * [Enhancement] Select widget_color while creating inbox
    
    * Fix codeclimate issues
    
    * Fix !important

--- app/javascript/dashboard/components/Modal.vue
+++ app/javascript/dashboard/components/Modal.vue
@@ -1,8 +1,8 @@
 <template>
   <transition name="modal-fade">
-    <div class="modal-mask" @click="close" v-if="show" transition="modal">
+    <div v-if="show" class="modal-mask" transition="modal" @click="close">
+      <i class="ion-android-close modal--close" @click="close"></i>
       <div class="modal-container" :class="className" @click.stop>
-        <i class="ion-android-close modal--close" @click="close"></i>
         <slot></slot>
       </div>
     </div>
@@ -10,24 +10,23 @@
 </template>
 
 <script>
-
 export default {
   props: {
     show: Boolean,
     onClose: Function,
     className: String,
   },
-  methods: {
-    close() {
-      this.onClose();
-    },
-  },
   mounted() {
-    document.addEventListener('keydown', (e) => {
+    document.addEventListener('keydown', e => {
       if (this.show && e.keyCode === 27) {
         this.onClose();
       }
     });
+  },
+  methods: {
+    close() {
+      this.onClose();
+    },
   },
 };
 </script>
