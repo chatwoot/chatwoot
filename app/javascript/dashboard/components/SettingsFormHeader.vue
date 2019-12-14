65ac811788006b18138f3f6d1281c7e3d41562c1
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2019-12-14 18:04:23 +0530

    [Enhancement] Select widget_color while creating inbox

---
+++ app/javascript/dashboard/components/SettingsFormHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="row settings--form--header">
+    <div class="medium-8">
+      <p class="title">
+        {{ title }}
+      </p>
+      <p class="sub-head">
+        {{ subTitle }}
+      </p>
+    </div>
+    <div v-if="buttonText" class="medium-4 text-right">
+      <woot-submit-button
+        class="small"
+        :button-text="buttonText"
+        :loading="isUpdating"
+        @click="onClick()"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subTitle: {
+      type: String,
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    isUpdating: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit('update');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '~dashboard/assets/scss/variables';
+
+.settings--form--header {
+  border-bottom: 1px solid $color-border;
+  margin-bottom: $space-normal;
+  padding: $space-normal 0;
+}
+</style>
