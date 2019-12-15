e7fffe033cfb08e397a8928834bc426077aa0c8d
Pranav Raj S <pranavrajs@gmail.com>
2019-12-15 16:59:12 +0530

    [Performance] Optimize queries in conversation/message finders (#364)
    * [Performance] Optimize queries in conversation/message finders
    
    * Add message_finder spec
    
    * Fix message_finder spec

---
+++ spec/finders/message_finder_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe ::MessageFinder do
+  subject(:message_finder) { described_class.new(conversation, params) }
+
+  let!(:account) { create(:account) }
+  let!(:user) { create(:user, account: account) }
+  let!(:inbox) { create(:inbox, account: account) }
+  let!(:conversation) { create(:complete_conversation, account: account, inbox: inbox, assignee: user) }
+
+  before do
+    create(:message, account: account, inbox: inbox, conversation: conversation)
+    create(:message, message_type: 'activity', account: account, inbox: inbox, conversation: conversation)
+    create(:message, message_type: 'activity', account: account, inbox: inbox, conversation: conversation)
+    create(:message, message_type: 'outgoing', account: account, inbox: inbox, conversation: conversation)
+  end
+
+  describe '#perform' do
+    context 'with filter_internal_messages false' do
+      let(:params) { { filter_internal_messages: false } }
+
+      it 'filter conversations by status' do
+        result = message_finder.perform
+        expect(result.count).to be 4
+      end
+    end
+
+    context 'with filter_internal_messages true' do
+      let(:params) { { filter_internal_messages: true } }
+
+      it 'filter conversations by status' do
+        result = message_finder.perform
+        expect(result.count).to be 2
+      end
+    end
+
+    context 'with before attribute' do
+      let!(:outgoing) { create(:message, message_type: 'outgoing', account: account, inbox: inbox, conversation: conversation) }
+      let(:params) { { before: outgoing.id } }
+
+      it 'filter conversations by status' do
+        result = message_finder.perform
+        expect(result.count).to be 4
+      end
+    end
+  end
+end
