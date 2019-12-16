e0845f7dcc5b6c061e145a1c11047f60e0c65498
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-12-16 09:24:56 +0530

    refactor: make component self closing

--- app/javascript/dashboard/components/widgets/SearchBox.vue
+++ app/javascript/dashboard/components/widgets/SearchBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="search">
-    <i class="icon ion-ios-search-strong"></i>
+    <i class="icon ion-ios-search-strong" />
     <input class="input" type="email" v-bind:placeholder="$t('CHAT_LIST.SEARCH.INPUT')">
   </div>
 </template>
