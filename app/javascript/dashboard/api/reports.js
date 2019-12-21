8346946e5ff54fdf03c86e7acbbe931d1f04d905
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2019-12-21 22:52:31 +0530

    Fix duplicate

--- app/javascript/dashboard/api/reports.js
+++ app/javascript/dashboard/api/reports.js
@@ -4,8 +4,8 @@ import endPoints from './endPoints';
 
 export default {
   getAccountReports(metric, from, to) {
-    const urlData = endPoints('reports').account(metric, from, to);
-    return axios.get(urlData.url);
+    const { url } = endPoints('reports').account(metric, from, to);
+    return axios.get(url);
   },
   getAccountSummary(accountId, from, to) {
     const urlData = endPoints('reports').accountSummary(accountId, from, to);
