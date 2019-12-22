1ee17cc8262d98589f462f0de3b9b160aa019d21
Anto Dominic <antodoms@gmail.com>
2019-12-22 22:53:18 +0530

    [Enhancement] Docker support Debugging (byebug & pry) and receive emails to MailHog via smtp (#371)
    * updated development docker setup
    
    * turned on yarn integrity check
    
    * create test docker compose and update development dockerfile
    
    * create docker Readme.md file
    
    * fix README.md file
    
    * create docker-copmose and dockerfile for production/staging environment setup
    
    * added mailhog to catch email
    
    * remove yarn integrity check in development mode!
    
    * Update Readme.md file to support mailhog inbox
    
    * update link to docker development guide from mail README
    
    * remove .env.development and use .env.example for docker as .env.development was mixing with circleci config
    
    * make the dockerfile configurable like before
    
    * update docker-compose as per suggesion in PR
    
    * create docker setup docs
    
    * Revert accidental deletion
    
    * fix: typo for branding consistency
    
    * fix typo
    
    * update the code as per recommendation in PR
    
    * remove package-lock and add gitignore
    
    * fix vulnerability AND update env for tests to pass
    
    * Fix yarn integrity check across different docker services
    
    * update code based on rubocop and PR suggestions
    
    * remove redundant test docker-compose and dockerfile
    
    * update rails entrypoint, update docs and Gemfile
    
    * [Rubocop] order Gemfile alphabetically
    
    * Reordering Gemfile

---
+++ docker/entrypoints/rails.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -x
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -rf /app/tmp/pids/server.pid
+rm -rf /app/tmp/cache/*
+
+echo "Waiting for postgres to become ready...."
+
+PGPASSWORD=$POSTGRES_PASSWORD
+PSQL="pg_isready -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USERNAME"
+until $PSQL
+do
+  sleep 2;
+done
+
+echo "Database ready to accept connections."
+
+YARN="yarn check --integrity"
+BUNDLE="bundle check"
+
+until $YARN && $BUNDLE
+do
+  sleep 2;
+done
+
+# Execute the main process of the container
+exec "$@"
