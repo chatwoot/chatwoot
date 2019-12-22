82dafba7543881755011a5ee4a3d45f9adaaa9cf
Anto Dominic <antodoms@gmail.com>
2019-12-22 18:00:23 +0530

    [Cleanup] Remove unnecessary Capistrano config (#375)

--- deploy/before_migrate.rb
+++
@@ -1,7 +0,0 @@
-Chef::Log.debug '==================> inside before_migrate <=================='
-
-shared_path = '/srv/www/chatwoot/shared'
-
-# yml files
-run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
-run "ln -nfs #{shared_path}/config/reports_redis.yml #{release_path}/config/reports_redis.yml"
