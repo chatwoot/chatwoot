2cd4052671a35f3550e8a9d884435a95bb785224
Anto Dominic <antodoms@gmail.com>
2019-12-23 16:17:01 +0530

    add sidekiq setup configuration and support

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  config.active_job.queue_adapter = :sidekiq
+  
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
