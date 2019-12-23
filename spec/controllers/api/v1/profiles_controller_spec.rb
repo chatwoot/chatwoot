4f936783bbef2461e9ce870dc483fdd08bcd5f6e
Sojan <sojan@pepalo.com>
2019-12-24 00:30:36 +0530

    Fix tests

--- spec/controllers/api/v1/profiles_controller_spec.rb
+++ spec/controllers/api/v1/profiles_controller_spec.rb
@@ -41,8 +41,9 @@ RSpec.describe 'Profile API', type: :request do
       let(:agent) { create(:user, account: account, role: :agent) }
 
       it 'updates the name & email' do
+        new_email = Faker::Internet.email
         put '/api/v1/profile',
-            params: { profile: { name: 'test', 'email': 'test@test.com' } },
+            params: { profile: { name: 'test', 'email': new_email } },
             headers: agent.create_new_auth_token,
             as: :json
 
@@ -51,7 +52,7 @@ RSpec.describe 'Profile API', type: :request do
         agent.reload
         expect(json_response['id']).to eq(agent.id)
         expect(json_response['email']).to eq(agent.email)
-        expect(agent.email).to eq('test@test.com')
+        expect(agent.email).to eq(new_email)
       end
 
       it 'updates the password' do
@@ -68,7 +69,7 @@ RSpec.describe 'Profile API', type: :request do
         expect(agent.avatar.attached?).to eq(false)
         file = fixture_file_upload(Rails.root.join('spec/assets/avatar.png'), 'image/png')
         put '/api/v1/profile',
-            params: { profile: { name: 'test', 'email': 'test@test.com', avatar: file } },
+            params: { profile: { avatar: file } },
             headers: agent.create_new_auth_token
 
         expect(response).to have_http_status(:success)
