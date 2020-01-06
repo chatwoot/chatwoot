99b70a20ead27e6a98f3580be139d4ef530d3b34
mbronek7 <mbronek7@gmail.com>
2020-01-06 16:44:55 +0100

    CR changes

--- app/jobs/pusher_call_job.rb
+++
@@ -1,7 +0,0 @@
-class PusherCallJob < ApplicationJob
-  queue_as :default
-
-  def perform(member, event_name, data)
-    ActionCable.server.broadcast(member, event: event_name, data: data)
-  end
-end
