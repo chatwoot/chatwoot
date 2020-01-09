05e49a853738f5cdc100b20d6ea54b2e72c72609
Sojan <sojan@pepalo.com>
2020-01-09 11:59:31 +0530

    Bugfix: Sidekiq queues getting stuck [#411]
    ref: https://github.com/rails/rails/issues/36810
    closes: #411

--- app/controllers/api/v1/widget/messages_controller.rb
+++ app/controllers/api/v1/widget/messages_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::Widget::MessagesController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   before_action :set_conversation, only: [:create]
 
   def index
