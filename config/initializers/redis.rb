d4b3ba4baa7137848343c24141b481a7af8ad6f6
Sony Mathew <ynos1234@gmail.com>
2020-01-23 23:14:07 +0545

    [#139] Send conversation emails (#442)
    * [#139] Delayed emails for conversations
    
    * Added the setex and get methods to Redis wrapper
    * Set the priorities for the sidekiq queues
    * Was not able to use mailhog for testing email in local, switched back to letter opener and added comments on using the SMTP settings
    * Added after create hood in messages to queue the sending of mail after 2 minutes using sidekiq worker and also set the redis key for the conversation to avoid the email sending for every message
    * Added the sidekiq worker to send the email and delete the conversation redis key
    * Added the mailer and mail template
    * mailer sends the last 10 messages along with the new messages from the time it was queued
    
    * Send email only in development or if smtp config is set
    
    * Send email only in development or if smtp config is set
    * Set the SMTP_PORT in production variable
    
    * Adding redis to circle CI
    
    * Specs for the conversation email changes
    
    * Added specs for conversation email sidekiq worker
    * Added specs for conversation mailer
    * Added specs in message model for the after create hook for notify email
    
    * Send emails only when there is a reply from agent
    
    * set development to use mailhog
    
    * Adding comments for using letter opener

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,6 @@ uri = URI.parse(ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'))
 redis = Rails.env.test? ? MockRedis.new : Redis.new(url: uri)
 Nightfury.redis = Redis::Namespace.new('reports', redis: redis)
 
-# Alfred - Used currently for Round Robin. Add here as you use it for more features
+# Alfred - Used currently for round robin and conversation emails.
+# Add here as you use it for more features
 $alfred = Redis::Namespace.new('alfred', redis: redis, warning: true)
