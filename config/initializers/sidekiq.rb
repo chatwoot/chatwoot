e45ab334e95a8738ba043969f6b33df23a8e7f39
sony-mathew <ynos1234@gmail.com>
2020-01-27 00:11:02 +0545

    Pass sidekiq cofig file
    * passed the env file in base image rather than separately in rails, web packer and sidekiq in docker-compose
    * removed un-necessary changes in schema
    * changed concurrency to finer values
    * removed default size set in sidekiq redis config
    * Added the sidekiq config option in Procfile.dev

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 sidekiq_redis_config = {
     url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'),
-    password: ENV.fetch('REDIS_PASSWORD', nil),
-    size: 10
+    password: ENV.fetch('REDIS_PASSWORD', nil)
 }
+
 Sidekiq.configure_client do |config|
     config.redis = sidekiq_redis_config.merge(size: 5)
 end
