1643ec9e3efc55c9fdb90d8f256ca8dea1562234
Subin T P <subinthattaparambil@gmail.com>
2020-02-22 22:55:04 +0530

    Add message to builder to send message from bot

---
+++ lib/integrations/answer_wise/outgoing_message_builder.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+class Integrations::AnswerWise::OutgoingMessageBuilder
+  # params = {
+  #   agent_bot_id: 1,
+  #   inbox_id: 1,
+  #   conversation_id: 2,
+  #   message: {
+  #     type: 'text'
+  #     content: 'Hello world'
+  #   }
+  # }
+  attr_accessor :options, :message
+
+  def initialize(options)
+    @options = options
+  end
+
+  def perform
+    ActiveRecord::Base.transaction do
+      build_message
+    end
+  end
+
+  private
+
+  def agent_bot
+    @agent_bot ||= AgentBot.find(options[:agent_bot_id])
+  end
+
+  def inbox
+    agent_bot.inboxes.find(options[:inbox_id])
+  end
+
+  def conversation
+    @conversation ||= inbox.conversations.find(options[:conversation_id])
+  end
+
+  def build_message
+    @message = conversation.messages.new(message_params)
+    @message.save!
+  end
+
+  def message_params
+    {
+      account_id: conversation.account_id,
+      inbox_id: inbox.id,
+      message_type: 1,
+      content: options[:message][:content]
+      # TODO: Add agent bot as user for this message
+    }
+  end
+end
