e8b5dbca1536e5a761b25ad36d987922a6571bf7
Sojan Jose <sojan@pepalo.com>
2020-02-29 11:20:33 +0530

    Chore: Making Account Signup Optional (#563)
    Introduce new environment variable that lets you control account signups
    ENABLE_ACCOUNT_SIGNUP :( true | false | api_only )
    Fixes: #406
    
    Co-authored-by: Pranav Raj S <pranavrajs@gmail.com>

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::AccountsController < Api::BaseController
   skip_before_action :verify_authenticity_token, only: [:create]
   skip_before_action :authenticate_user!, :set_current_user, :check_subscription, :handle_with_exception,
                      only: [:create], raise: false
+  before_action :check_signup_enabled
 
   rescue_from CustomExceptions::Account::InvalidEmail,
               CustomExceptions::Account::UserExists,
@@ -29,5 +30,9 @@ class Api::V1::AccountsController < Api::BaseController
 
   def account_params
     params.permit(:account_name, :email)
+  end
+
+  def check_signup_enabled
+    raise ActionController::RoutingError, 'Not Found' if ENV.fetch('ENABLE_ACCOUNT_SIGNUP', true) == 'false'
   end
 end
