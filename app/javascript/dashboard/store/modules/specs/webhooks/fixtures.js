a4078aface6b3665a6d0cec00caf7ec7bd6660e1
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2020-02-29 17:34:22 +0530

    Update fixtures

--- app/javascript/dashboard/store/modules/specs/webhooks/fixtures.js
+++ app/javascript/dashboard/store/modules/specs/webhooks/fixtures.js
@@ -6,12 +6,12 @@ export default [
   },
   {
     id: 5,
-    url: 'https://1.chatwoot.com',
+    url: 'https://2.chatwoot.com',
     account_id: 1,
   },
   {
     id: 6,
-    url: 'https://1.chatwoot.com',
+    url: 'https://3.chatwoot.com',
     account_id: 1,
   },
 ];
