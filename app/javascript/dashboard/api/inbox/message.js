065c5c5d95504dca71b5da9764d0123c93b469ea
Nithin David <webofnithin@gmail.com>
2020-03-03 15:57:36 +0530

    Init

--- app/javascript/dashboard/api/inbox/message.js
+++ app/javascript/dashboard/api/inbox/message.js
@@ -1,5 +1,5 @@
 /* eslint no-console: 0 */
-/* global axios */
+/* global axios, FormData */
 import ApiClient from '../ApiClient';
 
 class MessageApi extends ApiClient {
@@ -17,6 +17,17 @@ class MessageApi extends ApiClient {
   getPreviousMessages({ conversationId, before }) {
     return axios.get(`${this.url}/${conversationId}/messages`, {
       params: { before },
+    });
+  }
+
+  sendAttachment([conversationId, { file, file_type }]) {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('file_type', file_type);
+    return axios({
+      method: 'post',
+      url: `${this.url}/${conversationId}/messages`,
+      data: formData,
     });
   }
 }
