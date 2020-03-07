ea484835cf92970ec3cbba7fb2d62691a9e8a965
Sojan Jose <sojan@pepalo.com>
2020-03-07 12:18:16 +0530

    Chore: Enable Users to create multiple accounts (#440)
    Addresses: #402
    - migrations to split roles and other attributes from users table
    - make changes in code to accommodate this change
    
    Co-authored-by: Sojan Jose <sojan@pepalo.com>
    Co-authored-by: Pranav Raj Sreepuram <pranavrajs@gmail.com>

--- spec/policies/contact_policy_spec.rb
+++ spec/policies/contact_policy_spec.rb
@@ -5,8 +5,10 @@ require 'rails_helper'
 RSpec.describe ContactPolicy, type: :policy do
   subject(:contact_policy) { described_class }
 
-  let(:administrator) { create(:user, :administrator) }
-  let(:agent) { create(:user) }
+  let(:account) { create(:account) }
+
+  let(:administrator) { create(:user, :administrator, account: account) }
+  let(:agent) { create(:user, account: account) }
   let(:contact) { create(:contact) }
 
   permissions :index?, :show?, :update? do
