96819bef8d1f9f8e61375a40e1bced698bc322d7
Sojan <sojan@pepalo.com>
2020-03-08 23:50:10 +0530

    fix endpoint urls

---
+++ app/controllers/api/v1/accounts/conversations_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::Accounts::ConversationsController < Api::BaseController
+  before_action :conversation, except: [:index]
+
+  def index
+    result = conversation_finder.perform
+    @conversations = result[:conversations]
+    @conversations_count = result[:count]
+  end
+
+  def show; end
+
+  def toggle_status
+    @status = @conversation.toggle_status
+  end
+
+  def update_last_seen
+    @conversation.agent_last_seen_at = parsed_last_seen_at
+    @conversation.save!
+    head :ok
+  end
+
+  private
+
+  def parsed_last_seen_at
+    DateTime.strptime(params[:agent_last_seen_at].to_s, '%s')
+  end
+
+  def conversation
+    @conversation ||= current_account.conversations.find_by(display_id: params[:id])
+  end
+
+  def conversation_finder
+    @conversation_finder ||= ConversationFinder.new(current_user, params)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,12 +19,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :accounts, only: [:create]
-
       # ----------------------------------
       # start of account scoped api routes
-
-      scope 'account/:account_id', as: 'account', module: 'account' do
+      resources :accounts, only: [:create], module: :accounts do
         namespace :actions do
           resource :contact_merge, only: [:create]
         end
