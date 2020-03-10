f6dbb6a7efb242dda0d03a105e3eef9e3ae031ef
Sojan Jose <sojan@pepalo.com>
2020-03-11 00:02:15 +0530

    Feature: Access tokens for API access (#604)
    Co-authored-by: Pranav Raj Sreepuram <pranavrajs@gmail.com>

--- app/models/agent_bot_inbox.rb
+++ app/models/agent_bot_inbox.rb
@@ -6,6 +6,7 @@
 #  status       :integer          default("active")
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
+#  account_id   :integer
 #  agent_bot_id :integer
 #  inbox_id     :integer
 #
@@ -13,8 +14,16 @@
 class AgentBotInbox < ApplicationRecord
   validates :inbox_id, presence: true
   validates :agent_bot_id, presence: true
+  before_validation :ensure_account_id
 
   belongs_to :inbox
   belongs_to :agent_bot
+  belongs_to :account
   enum status: { active: 0, inactive: 1 }
+
+  private
+
+  def ensure_account_id
+    self.account_id = inbox&.account_id
+  end
 end
--- spec/controllers/api/v1/accounts/widget/inboxes_controller_spec.rb
+++ spec/controllers/api/v1/accounts/widget/inboxes_controller_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe '/api/v1/widget/inboxes', type: :request do
+RSpec.describe '/api/v1/accounts/{account.id}/widget/inboxes', type: :request do
   let(:account) { create(:account) }
   let(:inbox) { create(:inbox, account: account) }
   let(:admin) { create(:user, account: account, role: :administrator) }
   let(:agent) { create(:user, account: account, role: :agent) }
 
-  describe 'POST /api/v1/widget/inboxes' do
+  describe 'POST /api/v1/accounts/{account.id}/widget/inboxes' do
     let(:params) { { website: { website_name: 'test', website_url: 'test.com', widget_color: '#eaeaea' } } }
 
     context 'when unauthenticated user' do
       it 'returns unauthorized' do
-        post '/api/v1/widget/inboxes', params: params
+        post "/api/v1/accounts/#{account.id}/widget/inboxes", params: params
         expect(response).to have_http_status(:unauthorized)
       end
     end
@@ -19,7 +19,7 @@ RSpec.describe '/api/v1/widget/inboxes', type: :request do
     context 'when user is logged in' do
       context 'with user as administrator' do
         it 'creates inbox and returns website_token' do
-          post '/api/v1/widget/inboxes', params: params, headers: admin.create_new_auth_token
+          post "/api/v1/accounts/#{account.id}/widget/inboxes", params: params, headers: admin.create_new_auth_token
 
           expect(response).to have_http_status(:success)
           json_response = JSON.parse(response.body)
@@ -31,7 +31,7 @@ RSpec.describe '/api/v1/widget/inboxes', type: :request do
 
       context 'with user as agent' do
         it 'returns unauthorized' do
-          post '/api/v1/widget/inboxes',
+          post "/api/v1/accounts/#{account.id}/widget/inboxes",
                params: params,
                headers: agent.create_new_auth_token
 
@@ -41,12 +41,12 @@ RSpec.describe '/api/v1/widget/inboxes', type: :request do
     end
   end
 
-  describe 'PATCH /api/v1/widget/inboxes/:id' do
+  describe 'PATCH /api/v1/accounts/{account.id}/widget/inboxes/:id' do
     let(:update_params) { { website: { widget_color: '#eaeaea' } } }
 
     context 'when unauthenticated user' do
       it 'returns unauthorized' do
-        patch "/api/v1/widget/inboxes/#{inbox.channel_id}", params: update_params
+        patch "/api/v1/accounts/#{account.id}/widget/inboxes/#{inbox.channel_id}", params: update_params
         expect(response).to have_http_status(:unauthorized)
       end
     end
@@ -54,7 +54,7 @@ RSpec.describe '/api/v1/widget/inboxes', type: :request do
     context 'when user is logged in' do
       context 'with user as administrator' do
         it 'updates website channel' do
-          patch "/api/v1/widget/inboxes/#{inbox.channel_id}",
+          patch "/api/v1/accounts/#{account.id}/widget/inboxes/#{inbox.channel_id}",
                 params: update_params,
                 headers: admin.create_new_auth_token
 
@@ -67,7 +67,7 @@ RSpec.describe '/api/v1/widget/inboxes', type: :request do
 
       context 'with user as agent' do
         it 'returns unauthorized' do
-          patch "/api/v1/widget/inboxes/#{inbox.channel_id}",
+          patch "/api/v1/accounts/#{account.id}/widget/inboxes/#{inbox.channel_id}",
                 params: update_params,
                 headers: agent.create_new_auth_token
 
