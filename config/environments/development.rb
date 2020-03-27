55de92d9e57cd4f31b309fc87acbe1e3928eca74
Sojan <sojan@pepalo.com>
2020-03-28 03:17:22 +0530

    Fix twitter callbacks in local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,17 +32,14 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
   config.active_job.queue_adapter = :sidekiq
 
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: ENV['FRONTEND_URL'] }
-
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: ENV['FRONTEND_URL'] }
+
+  Rails.application.routes.default_url_options = { host: ENV['FRONTEND_URL'] }
 
   # If you want to use letter opener instead of mailhog for testing emails locally,
   # uncomment the following line L49 and comment lines L51 through to L65
