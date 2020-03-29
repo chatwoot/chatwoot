c4e676dcf3f0d140f41f47bf863ebe96daddaef5
Subin T P <subinthattaparambil@gmail.com>
2020-03-29 18:23:06 +0530

    Dispatch async events via sidekiq

--- app/jobs/event_dispatcher_job.rb
+++ app/jobs/event_dispatcher_job.rb
@@ -2,6 +2,6 @@ class EventDispatcherJob < ApplicationJob
   queue_as :events
 
   def perform(event_name, timestamp, data)
-    Rails.configuration.dispatcher.async_dispatcher.dispatch(event_name, timestamp, data)
+    Rails.configuration.dispatcher.async_dispatcher.publish_event(event_name, timestamp, data)
   end
 end
