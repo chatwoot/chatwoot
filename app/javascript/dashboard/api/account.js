bd1bef5dc8c80ac7a97f2f2a815fda11ccf88dd3
Nithin David <webofnithin@gmail.com>
2020-04-05 21:09:58 +0530

    Review cleanups

--- app/javascript/dashboard/api/account.js
+++ app/javascript/dashboard/api/account.js
@@ -1,13 +1,8 @@
-/* global axios */
 import ApiClient from './ApiClient';
 
 class AccountAPI extends ApiClient {
   constructor() {
     super('', { accountScoped: true });
-  }
-
-  getAccount() {
-    return axios.get(`${this.url}`);
   }
 }
 
