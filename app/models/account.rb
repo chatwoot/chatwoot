7bc7b3f9fa178975f0bcaf1a780795bd48670a41
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2020-04-07 10:12:24 +0530

    Update swagger documentation

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,7 +3,7 @@
 # Table name: accounts
 #
 #  id         :integer          not null, primary key
-#  locale     :integer          default("eng")
+#  locale     :integer          default("en")
 #  name       :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
