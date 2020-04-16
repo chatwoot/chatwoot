c8ffd455305bb6a9656292b375e0d3502ab80a3e
Sojan Jose <sojan@pepalo.com>
2020-04-16 15:05:01 +0530

    Chore: Housekeeping tasks (#721)
    - End point to return api version
    - Move agent bot listener to sync dispatcher
    - gem update

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   resource :widget, only: [:show]
 
+  get '/api', to: 'api#index'
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       # ----------------------------------
