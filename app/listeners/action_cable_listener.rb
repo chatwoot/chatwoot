aab44725c06bccb9e9d4a1d237c1935e8be4912a
Sojan Jose <sojan@pepalo.com>
2020-04-18 20:25:58 +0530

    Chore: Events Cleanup (#739)
    * Chore: Events Cleanup
    
    - fix failing tests
    - add additional webhook events
    - clean up event logic
    
    * Fix conversation status update in action cable
    
    Co-authored-by: Pranav Raj Sreepuram <pranavrajs@gmail.com>

--- app/listeners/action_cable_listener.rb
+++ app/listeners/action_cable_listener.rb
@@ -3,21 +3,21 @@ class ActionCableListener < BaseListener
 
   def conversation_created(event)
     conversation, account, timestamp = extract_conversation_and_account(event)
-    send_to_administrators(account.administrators, CONVERSATION_CREATED, conversation.push_event_data)
-    send_to_agents(conversation.inbox.members, CONVERSATION_CREATED, conversation.push_event_data)
+
+    send_to_agents(account, conversation.inbox.members, CONVERSATION_CREATED, conversation.push_event_data)
   end
 
   def conversation_read(event)
     conversation, account, timestamp = extract_conversation_and_account(event)
-    send_to_administrators(account.administrators, CONVERSATION_READ, conversation.push_event_data)
-    send_to_agents(conversation.inbox.members, CONVERSATION_READ, conversation.push_event_data)
+
+    send_to_agents(account, conversation.inbox.members, CONVERSATION_READ, conversation.push_event_data)
   end
 
   def message_created(event)
     message, account, timestamp = extract_message_and_account(event)
     conversation = message.conversation
-    send_to_administrators(account.administrators, MESSAGE_CREATED, message.push_event_data)
-    send_to_agents(conversation.inbox.members, MESSAGE_CREATED, message.push_event_data)
+
+    send_to_agents(account, conversation.inbox.members, MESSAGE_CREATED, message.push_event_data)
     send_to_contact(conversation.contact, MESSAGE_CREATED, message)
   end
 
@@ -25,55 +25,58 @@ class ActionCableListener < BaseListener
     message, account, timestamp = extract_message_and_account(event)
     conversation = message.conversation
     contact = conversation.contact
-    members = conversation.inbox.members.pluck(:pubsub_token)
-    send_to_members(members, MESSAGE_UPDATED, message.push_event_data)
+
+    send_to_agents(account, conversation.inbox.members, MESSAGE_UPDATED, message.push_event_data)
     send_to_contact(contact, MESSAGE_UPDATED, message)
   end
 
-  def conversation_reopened(event)
+  def conversation_resolved(event)
     conversation, account, timestamp = extract_conversation_and_account(event)
-    send_to_administrators(account.administrators, CONVERSATION_REOPENED, conversation.push_event_data)
-    send_to_agents(conversation.inbox.members, CONVERSATION_REOPENED, conversation.push_event_data)
+
+    send_to_agents(account, conversation.inbox.members, CONVERSATION_RESOLVED, conversation.push_event_data)
+  end
+
+  def conversation_opened(event)
+    conversation, account, timestamp = extract_conversation_and_account(event)
+
+    send_to_agents(account, conversation.inbox.members, CONVERSATION_OPENED, conversation.push_event_data)
   end
 
   def conversation_lock_toggle(event)
     conversation, account, timestamp = extract_conversation_and_account(event)
-    send_to_administrators(account.administrators, CONVERSATION_LOCK_TOGGLE, conversation.lock_event_data)
-    send_to_agents(conversation.inbox.members, CONVERSATION_LOCK_TOGGLE, conversation.lock_event_data)
+
+    send_to_agents(account, conversation.inbox.members, CONVERSATION_LOCK_TOGGLE, conversation.lock_event_data)
   end
 
   def assignee_changed(event)
     conversation, account, timestamp = extract_conversation_and_account(event)
-    send_to_administrators(account.administrators, ASSIGNEE_CHANGED, conversation.push_event_data)
-    send_to_agents(conversation.inbox.members, ASSIGNEE_CHANGED, conversation.push_event_data)
+
+    send_to_agents(account, conversation.inbox.members, ASSIGNEE_CHANGED, conversation.push_event_data)
   end
 
   def contact_created(event)
     contact, account, timestamp = extract_contact_and_account(event)
-    send_to_administrators(account.administrators, CONTACT_CREATED, contact.push_event_data)
-    send_to_agents(account.agents, CONTACT_CREATED, contact.push_event_data)
+
+    send_to_agents(account, account.agents, CONTACT_CREATED, contact.push_event_data)
   end
 
   def contact_updated(event)
     contact, account, timestamp = extract_contact_and_account(event)
-    send_to_administrators(account.administrators, CONTACT_UPDATED, contact.push_event_data)
-    send_to_agents(account.agents, CONTACT_UPDATED, contact.push_event_data)
+
+    send_to_agents(account, account.agents, CONTACT_UPDATED, contact.push_event_data)
   end
 
   private
 
-  def send_to_administrators(admins, event_name, data)
-    admin_tokens = admins.pluck(:pubsub_token)
-    return if admin_tokens.blank?
-
-    ::ActionCableBroadcastJob.perform_later(admin_tokens, event_name, data)
-  end
-
-  def send_to_agents(agents, event_name, data)
+  def send_to_agents(account, agents, event_name, data)
     agent_tokens = agents.pluck(:pubsub_token)
-    return if agent_tokens.blank?
+    admin_tokens = account.administrators.pluck(:pubsub_token)
 
-    ::ActionCableBroadcastJob.perform_later(agent_tokens, event_name, data)
+    pubsub_tokens = (agent_tokens + admin_tokens).uniq
+
+    return if pubsub_tokens.blank?
+
+    ::ActionCableBroadcastJob.perform_later(pubsub_tokens, event_name, data)
   end
 
   def send_to_contact(contact, event_name, message)
--- app/listeners/agent_bot_listener.rb
+++ app/listeners/agent_bot_listener.rb
@@ -1,4 +1,28 @@
 class AgentBotListener < BaseListener
+  def conversation_resolved(event)
+    conversation = extract_conversation_and_account(event)[0]
+    inbox = conversation.inbox
+    return if inbox.agent_bot_inbox.blank?
+    return unless inbox.agent_bot_inbox.active?
+
+    agent_bot = inbox.agent_bot_inbox.agent_bot
+
+    payload = conversation.webhook_data.merge(event: __method__.to_s)
+    AgentBotJob.perform_later(agent_bot.outgoing_url, payload)
+  end
+
+  def conversation_opened(event)
+    conversation = extract_conversation_and_account(event)[0]
+    inbox = conversation.inbox
+    return if inbox.agent_bot_inbox.blank?
+    return unless inbox.agent_bot_inbox.active?
+
+    agent_bot = inbox.agent_bot_inbox.agent_bot
+
+    payload = conversation.webhook_data.merge(event: __method__.to_s)
+    AgentBotJob.perform_later(agent_bot.outgoing_url, payload)
+  end
+
   def message_created(event)
     message = extract_message_and_account(event)[0]
     inbox = message.inbox
