645d53db1c8a15759ccc2e5bb291b8435b61cc12
Pranav Raj S <pranavrajs@gmail.com>
2020-04-22 13:14:09 +0530

    Bug: Fix webwidget.triggered event (#750)

--- app/javascript/sdk/bubbleHelpers.js
+++ app/javascript/sdk/bubbleHelpers.js
@@ -1,4 +1,5 @@
 import { addClass, toggleClass, wootOn } from './DOMHelpers';
+import { IFrameHelper } from './IFrameHelper';
 
 export const bubbleImg =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAUVBMVEUAAAD///////////////////////////////////////////////////////////////////////////////////////////////////////8IN+deAAAAGnRSTlMAAwgJEBk0TVheY2R5eo+ut8jb5OXs8fX2+cjRDTIAAADsSURBVHgBldZbkoMgFIThRgQv8SKKgGf/C51UnJqaRI30/9zfe+NQUQ3TvG7bOk9DVeCmshmj/CuOTYnrdBfkUOg0zlOtl9OWVuEk4+QyZ3DIevmSt/ioTvK1VH/s5bY3YdM9SBZ/mUUyWgx+U06ycgp7D8msxSvtc4HXL9BLdj2elSEfhBJAI0QNgJEBI1BEBsQClVBVGDgwYOLAhJkDM1YOrNg4sLFAsLJgZsHEgoEFFQt0JAFGFjQsKAMJ0LFAexKgZYFyJIDxJIBNJEDNAtSJBLCeBDCOBFAPzwFA94ED+zmhwDO9358r8ANtIsMXi7qVAwAAAABJRU5ErkJggg==';
@@ -36,6 +37,9 @@ export const onBubbleClick = () => {
   toggleClass(chatBubble, 'woot--hide');
   toggleClass(closeBubble, 'woot--hide');
   toggleClass(widgetHolder, 'woot--hide');
+  if (window.$chatwoot.isOpen) {
+    IFrameHelper.pushEvent('webwidget.triggered');
+  }
 };
 
 export const onClickChatBubble = () => {
