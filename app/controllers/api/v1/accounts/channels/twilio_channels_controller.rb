0cb733397766cf3c5ae095f45f7af52a9b016576
Pranav Raj S <pranavrajs@gmail.com>
2020-04-30 01:41:13 +0530

    Feature: Twilio Whatsapp Integration (#779)
    Twilio Whatsapp Integration
    
    Co-authored-by: Sojan <sojan@pepalo.com>

--- app/controllers/api/v1/accounts/channels/twilio_channels_controller.rb
+++ app/controllers/api/v1/accounts/channels/twilio_channels_controller.rb
@@ -2,13 +2,15 @@ class Api::V1::Accounts::Channels::TwilioChannelsController < Api::BaseControlle
   before_action :authorize_request
 
   def create
-    authenticate_twilio
-    build_inbox
-    setup_webhooks
-  rescue Twilio::REST::TwilioError => e
-    render_could_not_create_error(e.message)
-  rescue StandardError => e
-    render_could_not_create_error(e.message)
+    ActiveRecord::Base.transaction do
+      authenticate_twilio
+      build_inbox
+      setup_webhooks if @twilio_channel.sms?
+    rescue Twilio::REST::TwilioError => e
+      render_could_not_create_error(e.message)
+    rescue StandardError => e
+      render_could_not_create_error(e.message)
+    end
   end
 
   private
@@ -26,25 +28,30 @@ class Api::V1::Accounts::Channels::TwilioChannelsController < Api::BaseControlle
     ::Twilio::WebhookSetupService.new(inbox: @inbox).perform
   end
 
+  def phone_number
+    medium == 'sms' ? permitted_params[:phone_number] : "whatsapp:#{permitted_params[:phone_number]}"
+  end
+
+  def medium
+    permitted_params[:medium]
+  end
+
   def build_inbox
-    ActiveRecord::Base.transaction do
-      twilio_sms = current_account.twilio_sms.create(
-        account_sid: permitted_params[:account_sid],
-        auth_token: permitted_params[:auth_token],
-        phone_number: permitted_params[:phone_number]
-      )
-      @inbox = current_account.inboxes.create(
-        name: permitted_params[:name],
-        channel: twilio_sms
-      )
-    rescue StandardError => e
-      render_could_not_create_error(e.message)
-    end
+    @twilio_channel = current_account.twilio_sms.create!(
+      account_sid: permitted_params[:account_sid],
+      auth_token: permitted_params[:auth_token],
+      phone_number: phone_number,
+      medium: medium
+    )
+    @inbox = current_account.inboxes.create(
+      name: permitted_params[:name],
+      channel: @twilio_channel
+    )
   end
 
   def permitted_params
     params.require(:twilio_channel).permit(
-      :account_id, :phone_number, :account_sid, :auth_token, :name
+      :account_id, :phone_number, :account_sid, :auth_token, :name, :medium
     )
   end
 end
