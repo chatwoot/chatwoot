3548bd2c5d3b1dc2a74bf03ecd4d9432952d780b
Subin T P <subinthattaparambil@gmail.com>
2020-04-30 01:48:24 +0530

    Add integration app model

---
+++ app/models/integration/app.rb
@@ -0,0 +1,40 @@
+class Integration::App
+  attr_accessor :params
+
+  def initialize(params)
+    @params = params
+  end
+
+  def id
+    params[:id]
+  end
+
+  def name
+    params[:name]
+  end
+
+  def description
+    params[:description]
+  end
+
+  def fields
+    params[:fields]
+  end
+
+  class << self
+    def apps
+      Hashie::Mash.new(APPS_CONFIG)
+    end
+
+    def all
+      apps.values.inject([]) do |result,app|
+        result << self.new(app)
+        result
+      end
+    end
+
+    def find_by_id(id)
+      all.detect { |app| app.id == id }
+    end
+  end
+end
--- config/initializers/00_init.rb
+++ config/initializers/00_init.rb
@@ -1,2 +1,3 @@
 PLAN_CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'plans.yml'))
+APPS_CONFIG = YAML.load_file(File.join(Rails.root, 'config/integration', 'apps.yml'))
 $chargebee = ChargeBee.configure(site: ENV['CHARGEBEE_SITE'], api_key: ENV['CHARGEBEE_API_KEY'])
