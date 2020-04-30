304f41f6e3679f1d77d2803164904a31911d5e2c
Sony Mathew <ynos1234@gmail.com>
2020-04-30 20:20:26 +0530

    Feature: Conversation Continuity with Email (#770)
    * Added POC for mail inbox reply email
    * created mailbox and migratuion for the same
    * cleaned up sidekiq queues and added the queues for action mailbox and active storage
    * created conversation mailbox and functionlaity to create a message on the conversation when it's replied
    
    * Added UUID to conversation to be used in email replies
    
    * added migration to add uuid for conversation
    * changed parsing and resource fetching to reflect matching uuid and
      loading conversation alone
    * cleaned up conversation mailbox.rb
    
    * Added content type & attribute for message
    
    * Added the new reply email to outgoing emails
    * Added migration to accounts for adding domain and settings
    * Modified seeds to reflect this changes
    * Added the flag based column on account for boolean settings
    * Added the new reply to email in outgoing conversation emails based on conditions
    
    * Added dynamic email routing in application mailbox
    * Added dynamic email routing in application mailbox
    * Added a catch all deafult empty mailbox
    * Added annotation for account
    
    * Added the complete email details & attachments to the message
    * Added the complete email details to the message in content_attributes, like subject, to, cc, bcc etc
    * Modified the mail extractor to give a serilaized version of email
    * Handled storing attachments of email on the message
    
    * Added incoming email settings, env variables
    
    * [#138] Added documentation regarding different email settings and variables
    
    * Fixed the mail attachments blob issue (#138)
    * Decoided attachments were strings and had to construct blobs out fo them to work with active storage
    * Fixed the content encoding issue with mail body
    * Fixed issue with Proc used in apllication mailbox routing
    * Fixed couple of typos and silly mistakes
    
    * Set appropriate from email for conversation reply mails (#138)
    * From email was taken from a env variable, changed it to take from account settings if enabled
    * Set the reply to email correctly based on conversation UUID
    * Added commented config ind development.rb for mailbox ingress
    
    * Added account settings for domain and support email (#138)
    * Added the new attributes in accounts controller params whitelisting, api responses
    * Added options for the the new fields in account settings
    
    * Fixed typos in email continuity docs and warnings
    
    * Added specs for conversation reply mailer changes (#138)
    * Added specs for
      * conversation reply mailer
      * Accounts controller
      * Account and Conversation models
    
    * Added tests for email presenter (#138)
    
    * Specs for inbound email routing and mailboxes (#138)

--- spec/mailers/conversation_reply_mailer_spec.rb
+++ spec/mailers/conversation_reply_mailer_spec.rb
@@ -5,9 +5,6 @@ require 'rails_helper'
 RSpec.describe ConversationReplyMailer, type: :mailer do
   describe 'reply_with_summary' do
     let(:agent) { create(:user, email: 'agent1@example.com') }
-    let(:conversation) { create(:conversation, assignee: agent) }
-    let(:message) { create(:message, conversation: conversation) }
-    let(:mail) { described_class.reply_with_summary(message.conversation, Time.zone.now).deliver_now }
     let(:class_instance) { described_class.new }
 
     before do
@@ -15,16 +12,45 @@ RSpec.describe ConversationReplyMailer, type: :mailer do
       allow(class_instance).to receive(:smtp_config_set_or_development?).and_return(true)
     end
 
-    it 'renders the subject' do
-      expect(mail.subject).to eq("[##{message.conversation.display_id}] #{message.content.truncate(30)}")
+    context 'when custom domain and email is not enabled' do
+      let(:conversation) { create(:conversation, assignee: agent) }
+      let(:message) { create(:message, conversation: conversation) }
+      let(:mail) { described_class.reply_with_summary(message.conversation, Time.zone.now).deliver_now }
+
+      it 'renders the subject' do
+        expect(mail.subject).to eq("[##{message.conversation.display_id}] #{message.content.truncate(30)}")
+      end
+
+      it 'renders the receiver email' do
+        expect(mail.to).to eq([message&.conversation&.contact&.email])
+      end
+
+      it 'renders the reply to email' do
+        expect(mail.reply_to).to eq([message&.conversation&.assignee&.email])
+      end
     end
 
-    it 'renders the receiver email' do
-      expect(mail.to).to eq([message&.conversation&.contact&.email])
-    end
+    context 'when the cutsom domain emails are enabled' do
+      let(:conversation) { create(:conversation, assignee: agent) }
+      let(:message) { create(:message, conversation: conversation) }
+      let(:mail) { described_class.reply_with_summary(message.conversation, Time.zone.now).deliver_now }
 
-    it 'renders the reply to email' do
-      expect(mail.reply_to).to eq([message&.conversation&.assignee&.email])
+      before do
+        account = conversation.account
+        account.domain = 'example.com'
+        account.support_email = 'support@example.com'
+        account.domain_emails_enabled = true
+        account.save
+      end
+
+      it 'sets reply to email to be based on the domain' do
+        reply_to_email = "reply+to+#{message.conversation.uuid}@#{conversation.account.domain}"
+        expect(mail.reply_to).to eq([reply_to_email])
+      end
+
+      it 'sets the from email to be the support email' do
+        expect(mail.from).to eq([conversation.account.support_email])
+      end
     end
   end
 end
