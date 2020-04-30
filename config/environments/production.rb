c08c4134773195b7cdae8c70722d8ff4bab12cb6
Sojan <sojan@pepalo.com>
2020-04-30 20:59:44 +0530

    Add action cable configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  if ENV['FRONTEND_URL'].present?
+    config.action_cable.allowed_request_origins = [ENV['FRONTEND_URL'], %r{https?://#{URI.parse(ENV['FRONTEND_URL']).host}(:[0-9]+)?}]
+  end
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = ActiveModel::Type::Boolean.new.cast(ENV.fetch('FORCE_SSL', false))
