f0bc260e817f19dc501842f206af37b7c94323bc
Muhsin K <muhsinkeramam@gmail.com>
2020-05-02 10:23:33 +0530

    Change the date format in widget

--- app/javascript/shared/helpers/DateHelper.js
+++ app/javascript/shared/helpers/DateHelper.js
@@ -3,9 +3,17 @@ import moment from 'moment';
 class DateHelper {
   constructor(date) {
     this.date = moment(date * 1000);
+    this.today = moment();
+    this.yesterday = moment().subtract(1, 'day');
   }
 
   format(dateFormat = 'MMM DD, YYYY') {
+    if (moment(this.date).isSame(this.today, 'day')) {
+      return 'TODAY';
+    }
+    if (moment(this.date).isSame(this.yesterday, 'day')) {
+      return 'YESTERDAY';
+    }
     return this.date.format(dateFormat);
   }
 }
