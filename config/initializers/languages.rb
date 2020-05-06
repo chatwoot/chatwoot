f16607dc31ef96dc9e50386ac90eba340420b4e6
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2020-05-06 14:21:40 +0530

    Fix romaninan, french, pt_br

--- config/initializers/languages.rb
+++ config/initializers/languages.rb
@@ -5,7 +5,7 @@ LANGUAGES_CONFIG = {
   0 => { name: 'English', iso_639_3_code: 'eng', iso_639_1_code: 'en', enabled: true },
   1 => { name: 'Arabic', iso_639_3_code: 'ara', iso_639_1_code: 'ar', enabled: false },
   2 => { name: 'Dutch', iso_639_3_code: 'nld', iso_639_1_code: 'nl', enabled: false },
-  3 => { name: 'French', iso_639_3_code: 'fra', iso_639_1_code: 'fr', enabled: false },
+  3 => { name: 'French', iso_639_3_code: 'fra', iso_639_1_code: 'fr', enabled: true },
   4 => { name: 'German', iso_639_3_code: 'deu', iso_639_1_code: 'de', enabled: true },
   5 => { name: 'Hindi', iso_639_3_code: 'hin', iso_639_1_code: 'hi', enabled: false },
   6 => { name: 'Italian', iso_639_3_code: 'ita', iso_639_1_code: 'it', enabled: false },
@@ -17,7 +17,9 @@ LANGUAGES_CONFIG = {
   12 => { name: 'Spanish', iso_639_3_code: 'spa', iso_639_1_code: 'es', enabled: false },
   13 => { name: 'Malayalam', iso_639_3_code: 'mal', iso_639_1_code: 'ml', enabled: true },
   14 => { name: 'Catalan', iso_639_3_code: 'cat', iso_639_1_code: 'ca', enabled: true },
-  15 => { name: 'Greek', iso_639_3_code: 'ell', iso_639_1_code: 'el', enabled: true }
+  15 => { name: 'Greek', iso_639_3_code: 'ell', iso_639_1_code: 'el', enabled: true },
+  16 => { name: 'Portuguese, Brazilian', iso_639_3_code: '', iso_639_1_code: 'pt_BR', enabled: true },
+  17 => { name: 'Romanian', iso_639_3_code: 'ron', iso_639_1_code: 'ro', enabled: true }
 }.filter { |_key, val| val[:enabled] }.freeze
 
 Rails.configuration.i18n.available_locales = LANGUAGES_CONFIG.map { |_index, lang| lang[:iso_639_1_code].to_sym }
