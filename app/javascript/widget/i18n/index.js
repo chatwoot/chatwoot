cebd1ba80532bedc5546801602840471d0cf75d3
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2020-05-06 13:35:25 +0530

    Chore: Fix translations in widget

--- app/javascript/widget/i18n/index.js
+++ app/javascript/widget/i18n/index.js
@@ -1,9 +1,9 @@
-import ca from './ca';
-import de from './de';
-import el from './el';
-import en from './en';
-import ml from './ml';
-import pt from './pt';
+import { default as ca } from './locale/ca.json';
+import { default as de } from './locale/de.json';
+import { default as el } from './locale/el.json';
+import { default as en } from './locale/en.json';
+import { default as ml } from './locale/ml.json';
+import { default as pt } from './locale/pt.json';
 
 export default {
   ca,
