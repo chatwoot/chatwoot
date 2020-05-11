70341df9b84bfeb1d9cfbefcb171a760da3d86ed
Pranav Raj S <pranavrajs@gmail.com>
2020-05-11 11:40:14 +0530

    Bug: Use v2 reports instead of v1 reports (#843)

---
+++ app/javascript/dashboard/api/specs/reports.spec.js
@@ -0,0 +1,16 @@
+import reports from '../reports';
+import ApiClient from '../ApiClient';
+
+describe('#Reports API', () => {
+  it('creates correct instance', () => {
+    expect(reports).toBeInstanceOf(ApiClient);
+    expect(reports.apiVersion).toBe('/api/v2');
+    expect(reports).toHaveProperty('get');
+    expect(reports).toHaveProperty('show');
+    expect(reports).toHaveProperty('create');
+    expect(reports).toHaveProperty('update');
+    expect(reports).toHaveProperty('delete');
+    expect(reports).toHaveProperty('getAccountReports');
+    expect(reports).toHaveProperty('getAccountSummary');
+  });
+});
