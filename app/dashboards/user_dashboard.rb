4e9b9e5521468f581004cf123451bf2ef3665e17
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2020-05-11 12:33:52 +0530

    Fix user attributes

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -50,62 +50,21 @@ class UserDashboard < Administrate::BaseDashboard
   SHOW_PAGE_ATTRIBUTES = %i[
     accounts
     id
-    provider
-    uid
-    reset_password_token
-    reset_password_sent_at
-    remember_created_at
-    sign_in_count
-    current_sign_in_at
-    last_sign_in_at
-    current_sign_in_ip
-    last_sign_in_ip
-    confirmation_token
-    confirmed_at
-    confirmation_sent_at
     unconfirmed_email
     name
     nickname
     email
-    tokens
     created_at
     updated_at
-    pubsub_token
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    events
-    account_users
-    accounts
-    assigned_conversations
-    inbox_members
-    assigned_inboxes
-    messages
-    invitees
-    notification_settings
-    provider
-    uid
-    encrypted_password
-    reset_password_token
-    reset_password_sent_at
-    remember_created_at
-    sign_in_count
-    current_sign_in_at
-    last_sign_in_at
-    current_sign_in_ip
-    last_sign_in_ip
-    confirmation_token
-    confirmed_at
-    confirmation_sent_at
-    unconfirmed_email
     name
     nickname
     email
-    tokens
-    pubsub_token
   ].freeze
 
   # COLLECTION_FILTERS
