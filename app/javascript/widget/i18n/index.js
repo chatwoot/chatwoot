785e65fd0cdce816c77949c47f292738f076ffd5
Pranav Raj S <pranavrajs@gmail.com>
2020-05-17 16:15:53 +0530

    Feature: Introduce locale in web-widget SDK (#871)
    * Introduce setLocale in SDK
    
    Co-authored-by: Sojan <sojan@pepalo.com>

--- app/javascript/widget/i18n/index.js
+++ app/javascript/widget/i18n/index.js
@@ -1,15 +1,23 @@
+import { default as ar } from './locale/ar.json';
 import { default as ca } from './locale/ca.json';
 import { default as de } from './locale/de.json';
 import { default as el } from './locale/el.json';
 import { default as en } from './locale/en.json';
+import { default as fr } from './locale/fr.json';
 import { default as ml } from './locale/ml.json';
+import { default as nl } from './locale/nl.json';
 import { default as pt } from './locale/pt.json';
+import { default as pt_BR } from './locale/pt_BR.json';
 
 export default {
+  ar,
   ca,
   de,
   el,
   en,
+  fr,
   ml,
+  nl,
   pt,
+  pt_BR,
 };
