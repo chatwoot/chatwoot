f5acab669a773f3d48c644f977e64d6169bf0ba4
Pranav Raj S <pranavrajs@gmail.com>
2020-05-19 19:05:10 +0530

    Fix missing translations, enable dutch language (#878)
    * Fix missing translations, enable dutch language

--- app/javascript/dashboard/i18n/index.js
+++ app/javascript/dashboard/i18n/index.js
@@ -1,12 +1,13 @@
 import ca from './locale/ca';
-import ro from './locale/ro';
-import fr from './locale/fr';
-import pt_BR from './locale/pt_BR';
 import de from './locale/de';
 import el from './locale/el';
 import en from './locale/en';
+import fr from './locale/fr';
+import nl from './locale/nl';
 import ml from './locale/ml';
 import pt from './locale/pt';
+import pt_BR from './locale/pt_BR';
+import ro from './locale/ro';
 
 export default {
   ca,
@@ -14,6 +15,7 @@ export default {
   el,
   en,
   fr,
+  nl,
   ml,
   pt_BR,
   pt,
