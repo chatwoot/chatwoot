b54cf329ce6c241bd21792d435ec5b3387e2d20c
Sony Mathew <ynos1234@gmail.com>
2020-05-22 18:14:18 +0530

    Enhancement: Threaded email replies for a conversation (#885) (#886)
    * Added custom Message-ID and In-Reply-To headers for conversation reply emails
    * Added new global config for the default domain (This is used in the above headers)
    * Added migration to run the config loader to load the new global config value
    * The subject of the conversation reply mailer was made static (This is required for threaded emails)
    * Added required tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_112339) do
+ActiveRecord::Schema.define(version: 2020_05_22_115645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
