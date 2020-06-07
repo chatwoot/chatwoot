0545178ce51ab3814621fa3925cc52cf5b647fba
Sojan <sojan@pepalo.com>
2020-06-07 11:53:48 +0530

    Fix callback loop

--- spec/services/message_templates/template/greeting_spec.rb
+++ spec/services/message_templates/template/greeting_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe ::MessageTemplates::Template::EmailCollect do
+describe ::MessageTemplates::Template::Greeting do
   context 'when this hook is called' do
     let(:conversation) { create(:conversation) }
 
     it 'creates the email collect messages' do
       described_class.new(conversation: conversation).perform
-      expect(conversation.messages.count).to eq(2)
+      expect(conversation.messages.count).to eq(1)
     end
   end
 end
