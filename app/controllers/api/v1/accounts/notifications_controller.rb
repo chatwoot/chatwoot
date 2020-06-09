71523a218f7d1f4697c82dcea89b295e26d2abe2
Sojan Jose <sojan@pepalo.com>
2020-06-09 17:42:18 +0530

    Chore: FCM Push Fixes (#934)

--- app/controllers/api/v1/accounts/notifications_controller.rb
+++ app/controllers/api/v1/accounts/notifications_controller.rb
@@ -4,8 +4,13 @@ class Api::V1::Accounts::NotificationsController < Api::V1::Accounts::BaseContro
   before_action :fetch_notification, only: [:update]
 
   def index
-    @notifications = current_user.notifications.where(account_id: Current.account.id)
-    render json: @notifications
+    @unread_count = current_user.notifications.where(account_id: current_account.id, read_at: nil).count
+    @notifications = current_user.notifications.where(account_id: current_account.id).page params[:page]
+  end
+
+  def read_all
+    current_user.notifications.where(account_id: current_account.id, read_at: nil).update(read_at: DateTime.now.utc)
+    head :ok
   end
 
   def update
