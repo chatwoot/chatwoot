7a0d5f4ee686e2321ad9a05f22d190531819c4ab
Subin T P <subinthattaparambil@gmail.com>
2020-06-10 20:18:34 +0530

    Fix rubocop

--- db/migrate/20200610143132_rename_reference_id.rb
+++ db/migrate/20200610143132_rename_reference_id.rb
@@ -1,5 +1,5 @@
 class RenameReferenceId < ActiveRecord::Migration[6.0]
   def change
-  	rename_column :conversations, :reference_id, :identifier
+    rename_column :conversations, :reference_id, :identifier
   end
 end
