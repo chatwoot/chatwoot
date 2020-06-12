d301e7b8337cce4d7cf8774cdb1cf7490d611b2a
Sojan <sojan@pepalo.com>
2020-06-13 00:00:35 +0530

    Chore: Send browser language in webwidget events

--- app/javascript/widget/api/events.js
+++ app/javascript/widget/api/events.js
@@ -1,7 +1,16 @@
 import { API } from 'widget/helpers/axios';
+import Vue from 'vue';
 
 export default {
   create(name) {
-    return API.post(`/api/v1/widget/events${window.location.search}`, { name });
+    const locale = Vue.config.lang;
+    let search = window.location.search;
+    if (search) {
+      search = `${search}&locale=${locale}`;
+    } else {
+      search = `?locale=${locale}`;
+    }
+
+    return API.post(`/api/v1/widget/events${search}`, { name });
   },
 };
