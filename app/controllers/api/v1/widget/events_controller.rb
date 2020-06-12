f9e8bb8e104f405a360b8de046fa3367d6237cf6
Sojan Jose <sojan@pepalo.com>
2020-06-13 00:19:43 +0530

    Chore: Send browser language in webwidget events (#952)
    Co-authored-by: Pranav Raj Sreepuram <pranavrajs@gmail.com>

--- app/controllers/api/v1/widget/events_controller.rb
+++ app/controllers/api/v1/widget/events_controller.rb
@@ -2,11 +2,18 @@ class Api::V1::Widget::EventsController < Api::V1::Widget::BaseController
   include Events::Types
 
   def create
-    Rails.configuration.dispatcher.dispatch(permitted_params[:name], Time.zone.now, contact_inbox: @contact_inbox)
+    Rails.configuration.dispatcher.dispatch(permitted_params[:name], Time.zone.now, contact_inbox: @contact_inbox, event_info: event_info)
     head :no_content
   end
 
   private
+
+  def event_info
+    {
+      widget_language: params[:locale],
+      browser_language: browser.accept_language.first&.code
+    }
+  end
 
   def permitted_params
     params.permit(:name, :website_token)
