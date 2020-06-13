fb3b9ff6e7688b67b451a8b78ad9839e501ca98a
Nithin David <webofnithin@gmail.com>
2020-06-13 14:24:45 +0530

    Fix boken tests

--- app/javascript/shared/helpers/AudioNotificationHelper.js
+++ app/javascript/shared/helpers/AudioNotificationHelper.js
@@ -3,6 +3,7 @@ const notificationAudio = require('shared/assets/audio/ding.mp3');
 export const playNotificationAudio = () => {
   try {
     new Audio(notificationAudio).play();
+    if (window.bus) window.bus.$emit('on-agent-message-recieved');
   } catch (error) {
     console.log(error);
   }
--- app/javascript/widget/store/modules/conversation.js
+++ app/javascript/widget/store/modules/conversation.js
@@ -66,7 +66,6 @@ export const onNewMessageCreated = data => {
 
   if (isIncomingMessage) {
     playNotificationAudio();
-    window.bus.$emit('on-agent-message-recieved');
   }
 };
 
