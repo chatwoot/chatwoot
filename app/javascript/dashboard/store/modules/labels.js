6eb4d61c7e7455ffdc6f47e09118abbc2f38d943
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2020-06-21 20:34:48 +0530

    Add label module

---
+++ app/javascript/dashboard/store/modules/labels.js
@@ -0,0 +1,93 @@
+import * as MutationHelpers from 'shared/helpers/vuex/mutationHelpers';
+import types from '../mutation-types';
+import LabelsAPI from '../../api/labels';
+
+const state = {
+  records: [],
+  uiFlags: {
+    isFetching: false,
+    isFetchingItem: false,
+    isCreating: false,
+    isDeleting: false,
+  },
+};
+
+const getters = {
+  getLabels(_state) {
+    return _state.records;
+  },
+  getUIFlags(_state) {
+    return _state.uiFlags;
+  },
+};
+
+const actions = {
+  get: async function getLabels({ commit }) {
+    commit(types.SET_LABEL_UI_FLAG, { isFetching: true });
+    try {
+      const response = await LabelsAPI.get();
+      commit(types.SET_LABELS, response.data.payload);
+      commit(types.SET_LABEL_UI_FLAG, { isFetching: false });
+    } catch (error) {
+      commit(types.SET_LABEL_UI_FLAG, { isFetching: false });
+    }
+  },
+
+  create: async function createLabels({ commit }, cannedObj) {
+    commit(types.SET_LABEL_UI_FLAG, { isFetchingItem: true });
+    try {
+      const response = await LabelsAPI.create(cannedObj);
+      commit(types.ADD_LABEL, response.data);
+      commit(types.SET_LABEL_UI_FLAG, { isFetchingItem: false });
+    } catch (error) {
+      commit(types.SET_LABEL_UI_FLAG, { isFetchingItem: false });
+      throw new Error(error);
+    }
+  },
+
+  update: async function updateLabels({ commit }, { id, ...updateObj }) {
+    commit(types.SET_LABEL_UI_FLAG, { isCreating: true });
+    try {
+      const response = await LabelsAPI.update(id, updateObj);
+      commit(types.EDIT_LABEL, response.data);
+      commit(types.SET_LABEL_UI_FLAG, { isCreating: false });
+    } catch (error) {
+      commit(types.SET_LABEL_UI_FLAG, { isCreating: false });
+      throw new Error(error);
+    }
+  },
+
+  delete: async function deleteLabels({ commit }, id) {
+    commit(types.SET_LABEL_UI_FLAG, { isDeleting: true });
+    try {
+      await LabelsAPI.delete(id);
+      commit(types.DELETE_LABEL, id);
+      commit(types.SET_LABEL_UI_FLAG, { isDeleting: true });
+    } catch (error) {
+      commit(types.SET_LABEL_UI_FLAG, { isDeleting: true });
+      throw new Error(error);
+    }
+  },
+};
+
+const mutations = {
+  [types.SET_LABEL_UI_FLAG](_state, data) {
+    _state.uiFlags = {
+      ..._state.uiFlags,
+      ...data,
+    };
+  },
+
+  [types.SET_LABELS]: MutationHelpers.set,
+  [types.ADD_LABEL]: MutationHelpers.create,
+  [types.EDIT_LABEL]: MutationHelpers.update,
+  [types.DELETE_LABEL]: MutationHelpers.destroy,
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
