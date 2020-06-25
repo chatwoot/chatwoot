ec4cb56613c5fd032b4f795e10f8d85d220bd6a1
Sojan <sojan@pepalo.com>
2020-06-26 01:13:00 +0530

    Fix migration

--- db/migrate/20200418124534_add_sender_to_messages.rb
+++ db/migrate/20200418124534_add_sender_to_messages.rb
@@ -10,13 +10,15 @@ class AddSenderToMessages < ActiveRecord::Migration[6.0]
 
   def add_sender_from_message
     ::Message.find_in_batches do |messages_batch|
+      Rails.logger.info "migrated till #{messages_batch.first.id}\n"
       messages_batch.each do |message|
-        message.sender = message.user if message.user.present?
-        message.sender = message.contact if message.contact.present?
+        # rubocop:disable Rails/SkipsModelValidations
+        message.update_columns(sender_id: message.user.id, sender_type: 'User') if message.user.present?
+        message.update_columns(sender_id: message.contact.id, sender_type: 'Contact') if message.contact.present?
         if message.sender.nil?
-          message.sender = message.conversation.contact if message.incoming?
+          message.update_columns(sender_id: message.conversation.contact.id, sender_type: 'Contact') if message.incoming?
         end
-        message.save!
+        # rubocop:enable Rails/SkipsModelValidations
       end
     end
   end
