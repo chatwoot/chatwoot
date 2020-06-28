49c2056f3c444a872e3788dbf6662ccb976f7e28
Divyesh <dkothari@box8.in>
2020-06-28 13:08:57 +0530

    refactor: move create account from common api client to specific folder

--- app/javascript/dashboard/api/ApiClient.js
+++ app/javascript/dashboard/api/ApiClient.js
@@ -41,10 +41,6 @@ class ApiClient {
     return axios.post(this.url, data);
   }
 
-  createAccount(data) {
-    return axios.post(this.apiVersion + '/accounts', data);
-  }
-
   update(id, data) {
     return axios.patch(`${this.url}/${id}`, data);
   }
