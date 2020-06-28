7bcc0516b785e6c0e8384c1621da497ecb15cc57
Sojan <sojan@pepalo.com>
2020-06-28 21:41:28 +0530

    set presense

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,7 +1,7 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from params[:pubsub_token]
-    ::OnlineStatusTracker.set_presence(params[:pubsub_token])
+    ::OnlineStatusTracker.update_presence(params[:pubsub_token])
   end
 
   def unsubscribed
