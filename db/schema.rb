e41bd56f41bd9dc110ae2a042718c080553e4b43
Sojan Jose <sojan@pepalo.com>
2020-07-06 11:16:19 +0530

    Chore: Feature Flags for Channels (#1010)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_122646) do
+ActiveRecord::Schema.define(version: 2020_07_04_173104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
