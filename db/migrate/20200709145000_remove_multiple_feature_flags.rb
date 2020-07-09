3ca15d0acbdaa0e5692c13d5d39afb7c5e9f2c8d
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2020-07-10 00:01:03 +0530

    Add ConfigLoader.new.process

--- db/migrate/20200709145000_remove_multiple_feature_flags.rb
+++ db/migrate/20200709145000_remove_multiple_feature_flags.rb
@@ -2,6 +2,7 @@ class RemoveMultipleFeatureFlags < ActiveRecord::Migration[6.0]
   def change
     current_config = InstallationConfig.where(name: 'ACCOUNT_LEVEL_FEATURE_DEFAULTS').last
     InstallationConfig.where(name: 'ACCOUNT_LEVEL_FEATURE_DEFAULTS').where.not(id: current_config.id).destroy_all
+    ConfigLoader.new.process
     update_existing_accounts
   end
 
