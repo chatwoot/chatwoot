2219c7425f908cb1b42776d89f74178ebd4a12ea
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2020-07-10 19:31:37 +0530

    Fix languages.rb

--- config/initializers/languages.rb
+++ config/initializers/languages.rb
@@ -20,7 +20,7 @@ LANGUAGES_CONFIG = {
   15 => { name: 'Greek', iso_639_3_code: 'ell', iso_639_1_code: 'el', enabled: true },
   16 => { name: 'Portuguese, Brazilian', iso_639_3_code: '', iso_639_1_code: 'pt_BR', enabled: true },
   17 => { name: 'Romanian', iso_639_3_code: 'ron', iso_639_1_code: 'ro', enabled: true },
-  18 => { name: 'Tamil', iso_639_3_code: 'tam', iso_639_1_code: 'ta', enabled: true }
+  18 => { name: 'Tamil', iso_639_3_code: 'tam', iso_639_1_code: 'ta', enabled: true },
   19 => { name: 'Persian', iso_639_3_code: 'fas', iso_639_1_code: 'fa', enabled: true }
 }.filter { |_key, val| val[:enabled] }.freeze
 
