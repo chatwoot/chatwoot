2bdd2828135f8d8fe58a4b949a6e6374cda80ba5
Sojan <sojan@pepalo.com>
2020-07-19 22:07:17 +0530

    Chore: Adding browser info to web widget triggered event
    fixes: #970

--- app/controllers/api/v1/widget/base_controller.rb
+++ app/controllers/api/v1/widget/base_controller.rb
@@ -30,4 +30,14 @@ class Api::V1::Widget::BaseController < ApplicationController
     )
     @contact = @contact_inbox.contact
   end
+
+  def browser_params
+    {
+      browser_name: browser.name,
+      browser_version: browser.full_version,
+      device_name: browser.device.name,
+      platform_name: browser.platform.name,
+      platform_version: browser.platform.version
+    }
+  end
 end
