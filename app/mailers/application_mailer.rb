3bfb11f0aed04d06a95d817c6ef4b412e46d06b9
Pranav Raj S <pranav@thoughtwoot.com>
2020-08-14 23:50:41 +0530

    Update application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -45,7 +45,7 @@ class ApplicationMailer < ActionMailer::Base
   def liquid_locals
     # expose variables you want to be exposed in liquid
     {
-      global_config: GlobalConfig.get('INSTALLATION_NAME', 'BRAND_URL'),
+      global_config: GlobalConfig.get('BRAND_NAME', 'BRAND_URL'),
       action_url: @action_url
     }
   end
