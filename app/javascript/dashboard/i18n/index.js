859ad84ea2a7c9afc6dc2fd871f08345d09ae63c
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2020-08-22 17:45:46 +0530

    Enable Russian

--- app/javascript/dashboard/i18n/index.js
+++ app/javascript/dashboard/i18n/index.js
@@ -1,31 +1,63 @@
+import ar from './locale/ar';
 import ca from './locale/ca';
+import cs from './locale/cs';
+import da from './locale/da';
 import de from './locale/de';
 import el from './locale/el';
 import en from './locale/en';
 import es from './locale/es';
+import fa from './locale/fa';
+import fi from './locale/fi';
 import fr from './locale/fr';
-import nl from './locale/nl';
+import hi from './locale/hi';
+import hu from './locale/hu';
+import it from './locale/it';
+import ja from './locale/ja';
+import ko from './locale/ko';
 import ml from './locale/ml';
+import nl from './locale/nl';
+import pl from './locale/pl';
 import pt from './locale/pt';
 import pt_BR from './locale/pt_BR';
 import ro from './locale/ro';
-import fa from './locale/fa';
+import ru from './locale/ru';
+import sk from './locale/sk';
+import sv from './locale/sv';
 import ta from './locale/ta';
-import it from './locale/it';
+import tr from './locale/tr';
+import uk from './locale/uk';
+import vi from './locale/vi';
+import zh from './locale/zh';
 
 export default {
+  ar,
   ca,
+  cs,
+  da,
   de,
   el,
   en,
   es,
-  fr,
-  nl,
-  ml,
-  pt_BR,
-  pt,
-  ro,
   fa,
-  ta,
+  fi,
+  fr,
+  hi,
+  hu,
   it,
+  ja,
+  ko,
+  ml,
+  nl,
+  pl,
+  pt,
+  pt_BR,
+  ro,
+  ru,
+  sk,
+  sv,
+  ta,
+  tr,
+  uk,
+  vi,
+  zh,
 };
