1f48e66512f6ec378e1a44f21a6bf4120fb551b3
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2020-08-23 00:46:57 +0530

    Introduce TailwindCSS

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,8 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
+    require('tailwindcss'),
+    require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009',
