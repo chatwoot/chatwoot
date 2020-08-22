b712854e4b919e3eff90532433ef456874cfbfff
Pranav Raj S <pranav@thoughtwoot.com>
2020-08-22 22:20:22 +0530

    feat: Use order_by_name as default sort order for inbox/user (#1160)

--- app/controllers/api/v1/accounts/agents_controller.rb
+++ app/controllers/api/v1/accounts/agents_controller.rb
@@ -64,6 +64,6 @@ class Api::V1::Accounts::AgentsController < Api::V1::Accounts::BaseController
   end
 
   def agents
-    @agents ||= Current.account.users
+    @agents ||= Current.account.users.order_by_full_name
   end
 end
