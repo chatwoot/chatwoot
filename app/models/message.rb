85ae6d92b901bbcc92f5c75de152875ebe2668cb
Sojan Jose <sojan@pepalo.com>
2020-08-29 01:39:41 +0530

    feat: Enable Slack Integration in all channels (#1175)

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,21 +2,22 @@
 #
 # Table name: messages
 #
-#  id                 :integer          not null, primary key
-#  content            :text
-#  content_attributes :json
-#  content_type       :integer          default("text")
-#  message_type       :integer          not null
-#  private            :boolean          default(FALSE)
-#  sender_type        :string
-#  status             :integer          default("sent")
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#  account_id         :integer          not null
-#  conversation_id    :integer          not null
-#  inbox_id           :integer          not null
-#  sender_id          :bigint
-#  source_id          :string
+#  id                  :integer          not null, primary key
+#  content             :text
+#  content_attributes  :json
+#  content_type        :integer          default("text")
+#  external_source_ids :jsonb
+#  message_type        :integer          not null
+#  private             :boolean          default(FALSE)
+#  sender_type         :string
+#  status              :integer          default("sent")
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  account_id          :integer          not null
+#  conversation_id     :integer          not null
+#  inbox_id            :integer          not null
+#  sender_id           :bigint
+#  source_id           :string
 #
 # Indexes
 #
@@ -52,6 +53,8 @@ class Message < ApplicationRecord
   # [:email] : Used by conversation_continuity incoming email messages
   # [:in_reply_to] : Used to reply to a particular tweet in threads
   store :content_attributes, accessors: [:submitted_email, :items, :submitted_values, :email, :in_reply_to], coder: JSON
+
+  store :external_source_ids, accessors: [:slack], coder: JSON, prefix: :external_source_id
 
   # .succ is a hack to avoid https://makandracards.com/makandra/1057-why-two-ruby-time-objects-are-not-equal-although-they-appear-to-be
   scope :unread_since, ->(datetime) { where('EXTRACT(EPOCH FROM created_at) > (?)', datetime.to_i.succ) }
