6c723ab492fa1db980e9a7a8574065887eedd17f
Pranav Raj S <pranav@thoughtwoot.com>
2020-09-03 13:17:45 +0530

    chore: Improve widget loading performance (#1191)
    * Frontend assets url
    
    * Remove font files from widget
    
    * Rename variable to ASSET_CDN_HOST

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,8 +31,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
+  config.action_controller.asset_host = ENV.fetch('ASSET_CDN_HOST') if ENV['ASSET_CDN_HOST'].present?
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
