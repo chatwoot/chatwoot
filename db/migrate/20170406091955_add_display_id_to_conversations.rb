8ddeabd0fff3392ba790c74a6248069434294762
Sojan Jose <sojan@pepalo.com>
2020-09-07 15:19:41 +0530

    Chore: Squash previous migrations (#1107)
    Fixes: #784
    Co-authored-by: Pranav Raj S <pranav@thoughtwoot.com>

--- db/migrate/20170406091955_add_display_id_to_conversations.rb
+++
@@ -1,6 +0,0 @@
-class AddDisplayIdToConversations < ActiveRecord::Migration[5.0]
-  def change
-    add_column :conversations, :display_id, :integer
-    add_index :conversations, [:account_id, :display_id]
-  end
-end
--- db/migrate/20170426130050_add_agent_last_seen_at_to_conversation.rb
+++
@@ -1,5 +0,0 @@
-class AddAgentLastSeenAtToConversation < ActiveRecord::Migration[5.0]
-  def change
-    add_column :conversations, :agent_last_seen_at, :datetime
-  end
-end
