93366d65ceb95965493bf3d0fe8a6f5b07a022c3
Sojan Jose <sojan@pepalo.com>
2020-09-07 15:19:41 +0530

    Chore: Squash previous migrations (#1107)
    Fixes: #784
    Co-authored-by: Pranav Raj S <pranav@thoughtwoot.com>

--- db/migrate/20191020085608_rename_old_tables.rb
+++
@@ -1,7 +0,0 @@
-class RenameOldTables < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :channels
-    rename_table :facebook_pages, :channel_facebook_pages
-    rename_table :channel_widgets, :channel_web_widgets
-  end
-end
--- db/migrate/20200121220431_add_uniqueness_constraint_to_account_users.rb
+++
@@ -1,5 +0,0 @@
-class AddUniquenessConstraintToAccountUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_index :account_users, [:account_id, :user_id], unique: true, name: 'uniq_user_id_per_account_id'
-  end
-end
