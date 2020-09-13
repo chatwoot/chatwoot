27ee7c889b72d5b642aba5e95d6d60e40a1cd49b
Sojan <sojan@pepalo.com>
2020-09-13 16:16:52 +0530

    chore: Enable an echo id for message create end points

--- app/controllers/api/v1/widget/messages_controller.rb
+++ app/controllers/api/v1/widget/messages_controller.rb
@@ -48,6 +48,7 @@ class Api::V1::Widget::MessagesController < Api::V1::Widget::BaseController
       sender: @contact,
       content: permitted_params[:message][:content],
       inbox_id: conversation.inbox_id,
+      echo_id: permitted_params[:message][:echo_id],
       message_type: :incoming
     }
   end
@@ -116,7 +117,7 @@ class Api::V1::Widget::MessagesController < Api::V1::Widget::BaseController
   end
 
   def permitted_params
-    params.permit(:id, :before, :website_token, contact: [:email], message: [:content, :referer_url, :timestamp])
+    params.permit(:id, :before, :website_token, contact: [:email], message: [:content, :referer_url, :timestamp, :echo_id])
   end
 
   def set_message
