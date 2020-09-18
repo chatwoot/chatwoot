8791d0696e39483845ff7a7188041a872d9f3365
Sojan Jose <sojan@pepalo.com>
2020-09-18 18:50:53 +0530

    chore: Fix RestClient::GatewayTimeout, label_list of NilClass (#1243)

--- lib/webhooks/trigger.rb
+++ lib/webhooks/trigger.rb
@@ -1,8 +1,8 @@
 class Webhooks::Trigger
   def self.execute(url, payload)
     RestClient.post(url, payload.to_json, { content_type: :json, accept: :json })
-  rescue RestClient::NotFound => e
-    Rails.logger.info "invalid url #{url} : #{e.message}"
+  rescue RestClient::NotFound, RestClient::GatewayTimeout, SocketError => e
+    Rails.logger.info "Exception: invalid webhook url #{url} : #{e.message}"
   rescue StandardError => e
     Raven.capture_exception(e)
   end
