0a9dffa60339d6b42406fb6d45d1b26cf8529c68
Francisco Fernandes <1180964@isep.ipp.pt>
2020-10-03 00:48:10 +0100

    Fixed broken tests

--- app/javascript/dashboard/routes/dashboard/settings/labels/specs/validations.spec.js
+++ app/javascript/dashboard/routes/dashboard/settings/labels/specs/validations.spec.js
@@ -1,10 +1,16 @@
-import { validLabelCharacters } from '../validations';
+import { validLabelCharacters, notEmptyInput } from '../validations';
 
 describe('#validLabelCharacters', () => {
   it('validates the label', () => {
-    expect(validLabelCharacters('')).toEqual(false);
     expect(validLabelCharacters('str str')).toEqual(false);
     expect(validLabelCharacters('str_str')).toEqual(true);
     expect(validLabelCharacters('str-str')).toEqual(true);
+  });
+});
+
+describe('#notEmptyInput', () => {
+  it('verifies if the imput is empty or not', () => {
+    expect(notEmptyInput('')).toEqual(false);
+    expect(notEmptyInput('str_str')).toEqual(true);
   });
 });
