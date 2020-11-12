dc941c2a44b9b8ec5215107645f811c424c7f30f
Ravi Teja Gannavarapu <69030640+ravi-deepsource@users.noreply.github.com>
2020-11-12 18:26:52 +0530

    Update azure_storage_service_patch.rb

--- config/initializers/azure_storage_service_patch.rb
+++ config/initializers/azure_storage_service_patch.rb
@@ -145,7 +145,7 @@ module ActiveStorage
         chunk_size = 5.megabytes
         offset = 0
 
-        raise ActiveStorage::FileNotFoundError unless blob.present?
+        raise ActiveStorage::FileNotFoundError if blob.blank?
 
         while offset < blob.properties[:content_length]
           _, chunk = client.get_blob(container, key, start_range: offset, end_range: offset + chunk_size - 1)
