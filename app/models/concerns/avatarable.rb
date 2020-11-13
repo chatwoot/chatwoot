00ba8e7707242b177818a48e49258b6c9b67db4c
Sojan Jose <sojan@pepalo.com>
2020-11-13 19:56:30 +0530

    feat: Use gravatar URL for contacts and users if no image available (#1407)

--- app/models/concerns/avatarable.rb
+++ app/models/concerns/avatarable.rb
@@ -9,10 +9,13 @@ module Avatarable
   end
 
   def avatar_url
-    if avatar.attached? && avatar.representable?
-      url_for(avatar.representation(resize: '250x250'))
-    else
-      ''
+    return url_for(avatar.representation(resize: '250x250')) if avatar.attached? && avatar.representable?
+
+    if [User, Contact].include?(self.class) && email.present?
+      hash = Digest::MD5.hexdigest(email)
+      return "https://www.gravatar.com/avatar/#{hash}?d=404"
     end
+
+    ''
   end
 end
