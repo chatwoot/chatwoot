a151128bb1ff11b8e088067f1bbd8f5ed6c31501
Pranav Raj Sreepuram <pranavrajs@gmail.com>
2020-12-05 00:45:12 +0530

    Remove vue-highlight, fix css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,15 +8,13 @@ import VueI18n from 'vue-i18n';
 import VueRouter from 'vue-router';
 import axios from 'axios';
 // Global Components
+import hljs from 'highlight.js';
 import Multiselect from 'vue-multiselect';
 import WootSwitch from 'components/ui/Switch';
 import WootWizard from 'components/ui/Wizard';
 import { sync } from 'vuex-router-sync';
 import Vuelidate from 'vuelidate';
 import VTooltip from 'v-tooltip';
-import VueHighlightJS from 'vue-highlight.js';
-import javascript from 'highlight.js/lib/languages/javascript';
-
 import WootUiKit from '../dashboard/components';
 import App from '../dashboard/App';
 import i18n from '../dashboard/i18n';
@@ -38,11 +36,7 @@ Vue.use(VueI18n);
 Vue.use(WootUiKit);
 Vue.use(Vuelidate);
 Vue.use(VTooltip);
-Vue.use(VueHighlightJS, {
-  languages: {
-    javascript,
-  },
-});
+Vue.use(hljs.vuePlugin);
 
 Vue.component('multiselect', Multiselect);
 Vue.component('woot-switch', WootSwitch);
