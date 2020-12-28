a1413d2e07fd9b30e021d0d680d4c986b1a05ac8
Sojan Jose <sojan@pepalo.com>
2020-12-29 00:11:14 +0530

    chore: Fix typos

--- db/migrate/20201125123131_conv_dpid_seq_for_existing_accnts.rb
+++ db/migrate/20201125123131_conv_dpid_seq_for_existing_accnts.rb
@@ -1,9 +1,9 @@
 class ConvDpidSeqForExistingAccnts < ActiveRecord::Migration[6.0]
   def up
-    ::Acccount.find_in_batches do |accounts_batch|
+    ::Account.find_in_batches do |accounts_batch|
       Rails.logger.info "migrated till #{accounts_batch.first.id}\n"
       accounts_batch.each do |account|
-        display_id = Conversation.where(account_id: account_id).maximum('display_id')
+        display_id = Conversation.where(account_id: account.id).maximum('display_id')
         display_id ||= 0 # for accounts with out conversations
         ActiveRecord::Base.connection.exec_query("create sequence IF NOT EXISTS conv_dpid_seq_#{account.id} START #{display_id + 1}")
       end
@@ -11,7 +11,7 @@ class ConvDpidSeqForExistingAccnts < ActiveRecord::Migration[6.0]
   end
 
   def down
-    ::Acccount.find_in_batches do |accounts_batch|
+    ::Account.find_in_batches do |accounts_batch|
       Rails.logger.info "migrated till #{accounts_batch.first.id}\n"
       accounts_batch.each do |account|
         ActiveRecord::Base.connection.exec_query("drop sequence IF EXISTS conv_dpid_seq_#{account.id}")
