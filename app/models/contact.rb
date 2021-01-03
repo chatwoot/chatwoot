431a4496c3f8bafe69908d942bdbe187c54822d3
Pranav Raj S <pranav@chatwoot.com>
2021-01-03 20:07:57 +0530

    feat: API to add label to contacts (#1563)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -26,6 +26,7 @@ class Contact < ApplicationRecord
   include Pubsubable
   include Avatarable
   include AvailabilityStatusable
+  include Labelable
 
   validates :account_id, presence: true
   validates :email, allow_blank: true, uniqueness: { scope: [:account_id], case_sensitive: false }
