3e67d21b8b81de9225c1709bea48bc28733cc09a
Sojan <sojan@pepalo.com>
2021-01-06 00:28:26 +0530

    chore: enable pg stat statements

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_123131) do
+ActiveRecord::Schema.define(version: 2021_01_05_185632) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
