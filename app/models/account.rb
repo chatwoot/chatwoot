a0c33254e7302a51ccf15cb7b721d02762fd45e9
Sojan Jose <sojan@pepalo.com>
2021-01-17 23:56:56 +0530

    feat: Team APIs (#1654)
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>

--- app/models/account.rb
+++ app/models/account.rb
@@ -55,6 +55,7 @@ class Account < ApplicationRecord
   has_many :kbase_portals, dependent: :destroy, class_name: '::Kbase::Portal'
   has_many :kbase_categories, dependent: :destroy, class_name: '::Kbase::Category'
   has_many :kbase_articles, dependent: :destroy, class_name: '::Kbase::Article'
+  has_many :teams, dependent: :destroy
   has_flags ACCOUNT_SETTINGS_FLAGS.merge(column: 'settings_flags').merge(DEFAULT_QUERY_SETTING)
 
   enum locale: LANGUAGES_CONFIG.map { |key, val| [val[:iso_639_1_code], key] }.to_h
