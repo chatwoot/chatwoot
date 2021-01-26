be61125734b04fd966a62a424cb36b7ef82ba35a
Sojan <sojan@pepalo.com>
2021-01-26 17:21:44 +0530

    feat: Improvements to Team APIs

--- app/controllers/api/v1/accounts/team_members_controller.rb
+++ app/controllers/api/v1/accounts/team_members_controller.rb
@@ -7,16 +7,27 @@ class Api::V1::Accounts::TeamMembersController < Api::V1::Accounts::BaseControll
   end
 
   def create
-    record = @team.team_members.find_or_create_by(user_id: params[:user_id])
-    @team_member = record.user
+    ActiveRecord::Base.transaction do
+      @team_members = params[:user_ids].map { |user_id| create_team_member(user_id) }
+    end
   end
 
   def destroy
-    @team.team_members.find_by(user_id: params[:user_id])&.destroy
+    ActiveRecord::Base.transaction do
+      params[:user_ids].map { |user_id| remove_team_member(user_id) }
+    end
     head :ok
   end
 
   private
+
+  def create_team_member(user_id)
+    @team.team_members.find_or_create_by(user_id: user_id)&.user
+  end
+
+  def remove_team_member(user_id)
+    @team.team_members.find_by(user_id: user_id)&.destroy
+  end
 
   def fetch_team
     @team = Current.account.teams.find(params[:team_id])
