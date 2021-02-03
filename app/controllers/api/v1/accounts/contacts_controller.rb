4072c1df2ac9746f59419b18d4acb6b494c6f261
Sojan Jose <sojan@pepalo.com>
2021-02-03 19:24:51 +0530

    feat: Add bulk imports API for contacts (#1724)

--- app/controllers/api/v1/accounts/contacts_controller.rb
+++ app/controllers/api/v1/accounts/contacts_controller.rb
@@ -22,6 +22,14 @@ class Api::V1::Accounts::ContactsController < Api::V1::Accounts::BaseController
     @contacts = fetch_contact_last_seen_at(contacts)
   end
 
+  def import
+    ActiveRecord::Base.transaction do
+      import = Current.account.data_imports.create!(data_type: 'contacts')
+      import.import_file.attach(params[:import_file])
+    end
+    head :ok
+  end
+
   # returns online contacts
   def active
     contacts = Current.account.contacts.where(id: ::OnlineStatusTracker
---
+++ app/models/data_import.rb
@@ -0,0 +1,37 @@
+# == Schema Information
+#
+# Table name: data_imports
+#
+#  id                :bigint           not null, primary key
+#  data_type         :string           not null
+#  processed_records :integer
+#  processing_errors :text
+#  status            :integer          default("pending"), not null
+#  total_records     :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  account_id        :bigint           not null
+#
+# Indexes
+#
+#  index_data_imports_on_account_id  (account_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (account_id => accounts.id)
+#
+class DataImport < ApplicationRecord
+  belongs_to :account
+  validates :data_type, inclusion: { in: ['contacts'], message: '%<value>s is an invalid data type' }
+  enum status: { pending: 0, processing: 1, completed: 2, failed: 3 }
+
+  has_one_attached :import_file
+
+  after_create_commit :process_data_import
+
+  private
+
+  def process_data_import
+    DataImportJob.perform_later(self)
+  end
+end
