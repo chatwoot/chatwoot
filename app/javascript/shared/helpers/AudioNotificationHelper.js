42257a3772708d25e71944fd42052fd9a6cebbc5
Nithin David <webofnithin@gmail.com>
2021-02-07 12:46:36 +0530

    Add alert for incoming messages

--- app/javascript/shared/helpers/AudioNotificationHelper.js
+++ app/javascript/shared/helpers/AudioNotificationHelper.js
@@ -7,3 +7,31 @@ export const playNotificationAudio = () => {
     console.log(error);
   }
 };
+
+export const playAudioFromIframe = () => {
+  try {
+    const audioEl = document.querySelector('#notif-audio');
+    audioEl.currentTime = 0;
+    audioEl.play();
+    audioEl.muted = false;
+
+    setTimeout(() => {
+      audioEl.muted = true;
+      audioEl.pause();
+    }, 300);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const newMessageNotification = data => {
+  if (document.hidden) {
+    playAudioFromIframe();
+  } else {
+    const { conversation_id: currentPageId } = window.WOOT.$route.params;
+    const { conversation_id: incomingConvId } = data;
+    if (currentPageId !== incomingConvId) {
+      playAudioFromIframe();
+    }
+  }
+};
