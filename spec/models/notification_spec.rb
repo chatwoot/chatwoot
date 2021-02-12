3ceaaa9b0e509fd3e7ddb6ad94e3a0e9cd76f932
Sojan Jose <sojan@pepalo.com>
2021-02-12 22:56:12 +0530

    chore: Update notification Message Copy (#1685)

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -18,4 +18,33 @@ RSpec.describe Notification do
       expect(described_class.all.last).to eq notification1
     end
   end
+
+  context 'when push_title is called' do
+    it 'returns appropriate title suited for the notification type conversation_creation' do
+      notification = create(:notification, notification_type: 'conversation_creation')
+
+      expect(notification.push_message_title).to eq "[New conversation] - ##{notification.primary_actor.display_id} has\
+ been created in #{notification.primary_actor.inbox.name}"
+    end
+
+    it 'returns appropriate title suited for the notification type conversation_assignment' do
+      notification = create(:notification, notification_type: 'conversation_assignment')
+
+      expect(notification.push_message_title).to eq "[Assigned to you] - ##{notification.primary_actor.display_id} has been assigned to you"
+    end
+
+    it 'returns appropriate title suited for the notification type assigned_conversation_new_message' do
+      notification = create(:notification, notification_type: 'assigned_conversation_new_message')
+
+      expect(notification.push_message_title).to eq "[New message] - ##{notification.primary_actor.display_id} "
+    end
+
+    it 'returns appropriate title suited for the notification type conversation_mention' do
+      message = create(:message, sender: create(:user))
+      notification = create(:notification, notification_type: 'conversation_mention', primary_actor: message, secondary_actor: message.sender)
+
+      expect(notification.push_message_title).to eq "You have been mentioned in conversation [ID - #{message.conversation.display_id}] \
+by #{message.sender.name}"
+    end
+  end
 end
