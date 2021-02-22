87592687d620271dd9da78c5383f57e1a674fe23
Sojan <sojan@pepalo.com>
2021-02-22 23:54:02 +0530

    chore: add timezone

--- app/controllers/api/v1/accounts/inboxes_controller.rb
+++ app/controllers/api/v1/accounts/inboxes_controller.rb
@@ -81,7 +81,7 @@ class Api::V1::Accounts::InboxesController < Api::V1::Accounts::BaseController
 
   def inbox_update_params
     params.permit(:enable_auto_assignment, :name, :avatar, :greeting_message, :greeting_enabled,
-                  :working_hours_enabled, :out_of_office_message,
+                  :working_hours_enabled, :out_of_office_message, :timezone,
                   channel: [
                     :website_url,
                     :widget_color,
