59734ab2ec0ec3d55687b9fdb6b7793ef51b4ea7
Nithin David <webofnithin@gmail.com>
2021-03-01 16:55:28 +0530

    Review fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
+  
   if ActiveModel::Type::Boolean.new.cast(ENV.fetch('RAILS_LOG_TO_STDOUT', true))
     logger           = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
