c9e1b0eee166f60821f814c3e30ac594b8a781b9
Sojan <sojan@pepalo.com>
2021-03-08 12:03:36 +0530

    chore: mailer changes

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   # We need the application frontend url to be used in our emails
-  config.action_mailer.default_url_options = { host: ENV['FRONTEND_URL'] }
+  config.action_mailer.default_url_options = { host: ENV['FRONTEND_URL'] } if ENV['FRONTEND_URL'].present?
   # We load certain mailer templates from our database. This ensures changes to it is reflected immediately
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
@@ -21,9 +21,9 @@ Rails.application.configure do
   if ENV.fetch('SMTP_AUTHENTICATION', '').present?
     smtp_settings[:user_name] = ENV.fetch('SMTP_USERNAME', '')
     smtp_settings[:password] = ENV.fetch('SMTP_PASSWORD', '')
-    smtp_settings[:authentication] = ENV.fetch('SMTP_AUTHENTICATION', '')
+    smtp_settings[:authentication] = ENV.fetch('SMTP_AUTHENTICATION', 'login')
     smtp_settings[:enable_starttls_auto] = ActiveModel::Type::Boolean.new.cast(ENV.fetch('SMTP_ENABLE_STARTTLS_AUTO', true))
-    smtp_settings[:openssl_verify_mode] = ENV.fetch('SMTP_OPENSSL_VERIFY_MODE', '')
+    smtp_settings[:openssl_verify_mode] = ENV.fetch('SMTP_OPENSSL_VERIFY_MODE', 'peer')
   end
 
   smtp_settings[:ssl] = ActiveModel::Type::Boolean.new.cast(ENV.fetch('SMTP_SSL', true)) if ENV['SMTP_SSL']
