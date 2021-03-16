769c9508fc2cd0fb240f27913fc6af5bd8618b5b
sivin-git <iamsivin@gmail.com>
2021-03-16 14:13:28 +0530

    Added button component

---
+++ app/javascript/shared/components/ui/WootButton.vue
@@ -0,0 +1,65 @@
+<template>
+  <button
+    class="button"
+    :class="[type, size, color, style, classNames, disabled ? 'disabled' : '']"
+    :disabled="disabled || loading"
+    :autofocus="autofocus"
+    :type="nativeType"
+    @click="handleClick"
+  >
+    <spinner v-if="loading" size="small" />
+    <i v-if="icon" :class="icon"></i>
+    <span v-if="$slots.default"><slot></slot></span>
+  </button>
+</template>
+<script>
+import Spinner from 'shared/components/Spinner.vue';
+
+export default {
+  name: 'WootButton',
+  components: { Spinner },
+  props: {
+    type: {
+      type: String,
+      default: 'default',
+    },
+    size: {
+      type: String,
+      default: '',
+    },
+    icon: {
+      type: String,
+      default: '',
+    },
+    color: {
+      type: String,
+      default: 'primary',
+    },
+    classNames: {
+      type: String,
+      default: '',
+    },
+    style: {
+      type: String,
+      default: '',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    circle: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit('click', evt);
+    },
+  },
+};
+</script>
