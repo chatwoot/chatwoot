4e6cfb371f5ce70dd259722b87e51284d2a13310
Sojan Jose <sojan@pepalo.com>
2021-03-30 21:06:31 +0530

    chore: Handle Sentry Errors (#2020)
    If a contract contains an invalid email address, the sidekiq jobs for conversation continuity would error out. handling this exception gracefully.

--- lib/exception_list.rb
+++ lib/exception_list.rb
@@ -1,5 +1,8 @@
 module ExceptionList
-  URI_EXCEPTIONS = [Errno::ETIMEDOUT, Errno::ECONNREFUSED, URI::InvalidURIError, Net::OpenTimeout, SocketError].freeze
+  URI_EXCEPTIONS = [Errno::ETIMEDOUT, Errno::ECONNREFUSED, URI::InvalidURIError, Net::OpenTimeout, SocketError, OpenURI::HTTPError].freeze
   REST_CLIENT_EXCEPTIONS = [RestClient::NotFound, RestClient::GatewayTimeout, RestClient::BadRequest,
-                            RestClient::MethodNotAllowed, RestClient::Forbidden].freeze
+                            RestClient::MethodNotAllowed, RestClient::Forbidden, RestClient::InternalServerError, RestClient::PayloadTooLarge].freeze
+  SMTP_EXCEPTIONS = [
+    Net::SMTPSyntaxError
+  ].freeze
 end
