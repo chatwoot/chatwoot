79d7493832c13a2ea3f5aa1d97b00a9b39c2ab68
Pranav Raj S <pranav@chatwoot.com>
2021-04-15 17:50:02 +0530

    Update the migration

--- db/migrate/20210306170117_add_last_activity_at_to_contacts.rb
+++ db/migrate/20210306170117_add_last_activity_at_to_contacts.rb
@@ -1,13 +1,19 @@
 class AddLastActivityAtToContacts < ActiveRecord::Migration[6.0]
   def up
+    # rubocop:disable Rails/SkipsModelValidations
     add_column :contacts, :last_activity_at, :datetime, index: true, default: nil
-    # Contact.find_in_batches do |contact_batch|
-    #   Rails.logger.debug "Migrated till #{contact_batch.first.id}\n"
-    #   contact_batch.each do |contact|
-    #     last_activity_at = contact.messages.last&.created_at
-    #     contact.update_columns(last_activity_at: last_activity_at)
-    #   end
-    # end
+    Conversation.find_in_batches do |conversation_batch|
+      conversation_batch.each do |conversation|
+        contact = conversation.contact
+        if contact.last_activity_at.nil? || conversation.updated_at > contact.last_activity_at
+          contact.update_columns(last_activity_at: conversation.updated_at)
+        end
+      end
+    end
+
+    Contact.where(additional_attributes: nil).update_all(additional_attributes: {})
+    Contact.where(phone_number: '').update_all(phone_number: nil)
+    # rubocop:enable Rails/SkipsModelValidations
   end
 
   def down
