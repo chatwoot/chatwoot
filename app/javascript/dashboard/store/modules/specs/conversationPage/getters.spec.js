5b04b1a3b4a833daf77b871dd5c6a7fa1a500e90
Niklas Haug <niklas@nhaug.de>
2021-04-18 14:19:09 +0200

    added test for getCurrentPage function

--- app/javascript/dashboard/store/modules/specs/conversationPage/getters.spec.js
+++ app/javascript/dashboard/store/modules/specs/conversationPage/getters.spec.js
@@ -9,12 +9,25 @@ describe('#getters', () => {
         all: 3,
       },
     };
+    expect(getters.getCurrentPage(state)).toHaveProperty('me');
+    expect(getters.getCurrentPage(state)).toHaveProperty('unassigned');
+    expect(getters.getCurrentPage(state)).toHaveProperty('all');
+  });
+
+  it('getCurrentPageFilter', () => {
+    const state = {
+      currentPage: {
+        me: 1,
+        unassigned: 2,
+        all: 3,
+      },
+    };
     expect(getters.getCurrentPageFilter(state)('me')).toEqual(1);
     expect(getters.getCurrentPageFilter(state)('unassigned')).toEqual(2);
     expect(getters.getCurrentPageFilter(state)('all')).toEqual(3);
   });
 
-  it('getCurrentPage', () => {
+  it('getHasEndReached', () => {
     const state = {
       hasEndReached: {
         me: false,
