65c7d3ceea26abb3bd3004346cd5da820151a6d9
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2021-04-22 23:17:29 +0530

    Enhancement: Ability to assign administrators as conversation assignees (#2142)
    * Enhancement: Ability to assign administrators as conversation assignee
    
    Co-authored-by: Nithin David Thomas <webofnithin@gmail.com>
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>

---
+++ app/javascript/dashboard/store/modules/inboxAssignableAgents.js
@@ -0,0 +1,62 @@
+import Vue from 'vue';
+
+import InboxesAPI from 'dashboard/api/inboxes.js';
+
+const state = {
+  records: {},
+  uiFlags: {
+    isFetching: false,
+  },
+};
+
+export const types = {
+  SET_INBOX_ASSIGNABLE_AGENTS_UI_FLAG: 'SET_INBOX_ASSIGNABLE_AGENTS_UI_FLAG',
+  SET_INBOX_ASSIGNABLE_AGENTS: 'SET_INBOX_ASSIGNABLE_AGENTS',
+};
+
+export const getters = {
+  getAssignableAgents: $state => inboxId => {
+    const allAgents = $state.records[inboxId] || [];
+    const verifiedAgents = allAgents.filter(record => record.confirmed);
+    return verifiedAgents;
+  },
+  getUIFlags($state) {
+    return $state.uiFlags;
+  },
+};
+
+export const actions = {
+  async fetch({ commit }, { inboxId }) {
+    commit(types.SET_INBOX_ASSIGNABLE_AGENTS_UI_FLAG, { isFetching: true });
+    try {
+      const {
+        data: { payload },
+      } = await InboxesAPI.getAssignableAgents(inboxId);
+      commit(types.SET_INBOX_ASSIGNABLE_AGENTS, { inboxId, members: payload });
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      commit(types.SET_INBOX_ASSIGNABLE_AGENTS_UI_FLAG, { isFetching: false });
+    }
+  },
+};
+
+export const mutations = {
+  [types.SET_INBOX_ASSIGNABLE_AGENTS_UI_FLAG]($state, data) {
+    $state.uiFlags = {
+      ...$state.uiFlags,
+      ...data,
+    };
+  },
+  [types.SET_INBOX_ASSIGNABLE_AGENTS]: ($state, { inboxId, members }) => {
+    Vue.set($state.records, inboxId, members);
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
