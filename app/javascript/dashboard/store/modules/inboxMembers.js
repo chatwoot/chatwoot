025894542c732ba00ff70e8e09aa3b0be94473c3
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2021-04-27 20:59:00 +0530

    chore: inbox member clean up (#2144)
    Delete unused logic and tests
    Co-authored-by: Nithin David Thomas <webofnithin@gmail.com>
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>

--- app/javascript/dashboard/store/modules/inboxMembers.js
+++ app/javascript/dashboard/store/modules/inboxMembers.js
@@ -1,44 +1,6 @@
-import Vue from 'vue';
-
 import InboxMembersAPI from '../../api/inboxMembers';
 
-const state = {
-  records: {},
-  uiFlags: {
-    isFetching: false,
-  },
-};
-
-export const types = {
-  SET_INBOX_MEMBERS_UI_FLAG: 'SET_INBOX_MEMBERS_UI_FLAG',
-  SET_INBOX_MEMBERS: 'SET_INBOX_MEMBERS',
-};
-
-export const getters = {
-  getMembersByInbox: $state => inboxId => {
-    const allAgents = $state.records[inboxId] || [];
-    const verifiedAgents = allAgents.filter(record => record.confirmed);
-    return verifiedAgents;
-  },
-  getUIFlags($state) {
-    return $state.uiFlags;
-  },
-};
-
 export const actions = {
-  async fetch({ commit }, { inboxId }) {
-    commit(types.SET_INBOX_MEMBERS_UI_FLAG, { isFetching: true });
-    try {
-      const {
-        data: { payload },
-      } = await InboxMembersAPI.show(inboxId);
-      commit(types.SET_INBOX_MEMBERS, { inboxId, members: payload });
-    } catch (error) {
-      throw new Error(error);
-    } finally {
-      commit(types.SET_INBOX_MEMBERS_UI_FLAG, { isFetching: false });
-    }
-  },
   get(_, { inboxId }) {
     return InboxMembersAPI.show(inboxId);
   },
@@ -47,22 +9,7 @@ export const actions = {
   },
 };
 
-export const mutations = {
-  [types.SET_INBOX_MEMBERS_UI_FLAG]($state, data) {
-    $state.uiFlags = {
-      ...$state.uiFlags,
-      ...data,
-    };
-  },
-  [types.SET_INBOX_MEMBERS]: ($state, { inboxId, members }) => {
-    Vue.set($state.records, inboxId, members);
-  },
-};
-
 export default {
   namespaced: true,
-  state,
-  getters,
   actions,
-  mutations,
 };
