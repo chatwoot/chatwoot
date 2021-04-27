55b363617313d82c0033a64c6077073de7e56f01
Pranav Raj S <pranav@chatwoot.com>
2021-04-27 11:49:06 +0530

    fix: Add a default email subject line if no subject is present

--- app/presenters/mail_presenter.rb
+++ app/presenters/mail_presenter.rb
@@ -8,7 +8,7 @@ class MailPresenter < SimpleDelegator
   end
 
   def subject
-    encode_to_unicode(@mail.subject)
+    encode_to_unicode(@mail.subject || I18n.t('email.no_subject'))
   end
 
   def text_content
