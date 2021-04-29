cebcf4518c88b0c24ad1c194c86e54e7675ec62f
Sojan <sojan@pepalo.com>
2021-04-29 18:22:56 +0530

    chore: migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_04_26_191914) do
+ActiveRecord::Schema.define(version: 2021_04_28_151147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
