3c0d6d36b5baf578586e8816a70c10299ae1ac9f
Muhsin <muhsinkeramam@gmail.com>
2021-04-29 11:37:00 +0530

    add add campaign modal

---
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/components/AddCampaign.vue
@@ -0,0 +1,163 @@
+<template>
+  <modal :show.sync="show" :on-close="onClose">
+    <div class="column content-box">
+      <woot-modal-header
+        :header-title="$t('CAMPAIGN.ADD.TITLE')"
+        :header-content="$t('CAMPAIGN.ADD.DESC')"
+      />
+      <form class="row" @submit.prevent="addCampaign()">
+        <div class="medium-12 columns">
+          <label :class="{ error: $v.title.$error }">
+            {{ $t('CAMPAIGN.ADD.FORM.TITLE.LABEL') }}
+            <input
+              v-model.trim="title"
+              type="text"
+              :placeholder="$t('CAMPAIGN.ADD.FORM.TITLE.PLACEHOLDER')"
+              @input="$v.title.$touch"
+            />
+          </label>
+        </div>
+
+        <div class="medium-12 columns">
+          <label :class="{ error: $v.content.$error }">
+            {{ $t('CAMPAIGN.ADD.FORM.CONTENT.LABEL') }}
+            <textarea
+              v-model.trim="content"
+              rows="5"
+              type="text"
+              :placeholder="$t('CAMPAIGN.ADD.FORM.CONTENT.PLACEHOLDER')"
+              @input="$v.content.$touch"
+            />
+          </label>
+        </div>
+
+        <div class="medium-12 columns">
+          <label :class="{ error: $v.selectedAgent.$error }">
+            {{ $t('CAMPAIGN.ADD.FORM.SENT_BY.LABEL') }}
+            <select v-model="selectedAgent">
+              <option
+                v-for="agent in agentsList"
+                :key="agent.name"
+                :value="agent.name"
+              >
+                {{ agent.name }}
+              </option>
+            </select>
+            <span v-if="$v.selectedAgent.$error" class="message">
+              {{ $t('AGENT_MGMT.ADD.FORM.AGENT_TYPE.ERROR') }}
+            </span>
+          </label>
+        </div>
+
+        <div class="medium-12 columns">
+          <label :class="{ error: $v.endPoint.$error }">
+            {{ $t('CAMPAIGN.ADD.FORM.END_POINT.LABEL') }}
+            <input
+              v-model.trim="endPoint"
+              type="text"
+              :placeholder="$t('CAMPAIGN.ADD.FORM.END_POINT.PLACEHOLDER')"
+              @input="$v.endPoint.$touch"
+            />
+            <span v-if="$v.endPoint.$error" class="message">
+              {{ $t('CAMPAIGN.ADD.FORM.END_POINT.ERROR') }}
+            </span>
+          </label>
+        </div>
+        <div class="medium-12 columns">
+          <label :class="{ error: $v.timeOnPage.$error }">
+            {{ $t('CAMPAIGN.ADD.FORM.TIME_ON_PAGE.LABEL') }}
+            <input
+              v-model.trim="timeOnPage"
+              type="number"
+              :placeholder="$t('CAMPAIGN.ADD.FORM.TIME_ON_PAGE.PLACEHOLDER')"
+              @input="$v.timeOnPage.$touch"
+            />
+            <span v-if="$v.timeOnPage.$error" class="message">
+              {{ $t('CAMPAIGN.ADD.FORM.TIME_ON_PAGE.ERROR') }}
+            </span>
+          </label>
+        </div>
+
+        <div class="modal-footer">
+          <div class="medium-12 columns">
+            <woot-submit-button
+              :disabled="
+                $v.content.$invalid ||
+                  $v.title.$invalid ||
+                  $v.selectedAgent.$invalid ||
+                  $v.endPoint.$invalid ||
+                  $v.timeOnPage.$invalid
+              "
+              :button-text="$t('CAMPAIGN.ADD.CREATE_BUTTON_TEXT')"
+            />
+            <button class="button clear" @click.prevent="onClose">
+              {{ $t('CAMPAIGN.ADD.CANCEL_BUTTON_TEXT') }}
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </modal>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import { required, url, minLength } from 'vuelidate/lib/validators';
+
+import WootSubmitButton from '../../../../../components/buttons/FormSubmitButton';
+import Modal from '../../../../../components/Modal';
+import alertMixin from 'shared/mixins/alertMixin';
+
+export default {
+  components: {
+    WootSubmitButton,
+    Modal,
+  },
+  mixins: [alertMixin],
+  props: {
+    onClose: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      title: '',
+      content: '',
+      selectedAgent: '',
+      endPoint: '',
+      timeOnPage: null,
+      show: true,
+    };
+  },
+  validations: {
+    title: {
+      required,
+    },
+    content: {
+      required,
+    },
+    selectedAgent: {
+      required,
+    },
+    endPoint: {
+      required,
+      minLength: minLength(7),
+      url,
+    },
+    timeOnPage: {
+      required,
+    },
+  },
+
+  computed: {
+    ...mapGetters({
+      agentList: 'agents/getAgents',
+    }),
+
+    agentsList() {
+      return this.agentList;
+    },
+  },
+};
+</script>
