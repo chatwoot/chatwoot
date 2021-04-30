f86e37b29082ba98d515b40f5e467b22155ce04b
Nithin David <webofnithin@gmail.com>
2021-04-30 14:40:05 +0530

    chore: Add i18n and store into storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,28 @@
+import { addDecorator } from '@storybook/vue';
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+
+import WootUiKit from '../app/javascript/dashboard/components';
+import i18n from '../app/javascript/dashboard/i18n';
+import store from '../app/javascript/dashboard/store';
+
 import '../app/javascript/dashboard/assets/scss/storybook.scss';
+
+Vue.use(VueI18n);
+Vue.use(WootUiKit);
+const i18nConfig = new VueI18n({
+  locale: 'en',
+  messages: i18n,
+});
+
+addDecorator(() => ({
+  template: '<story/>',
+  i18n: i18nConfig,
+  store,
+  beforeCreate: function() {
+    this.$root._i18n = this.$i18n;
+  },
+}));
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
