7c8ca617e80f60c5e5fe8c30e4bb098a70e64725
Sojan <sojan@pepalo.com>
2021-04-30 23:31:06 +0530

    chore: minore fixes

--- app/controllers/api/v1/widget/events_controller.rb
+++ app/controllers/api/v1/widget/events_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::Widget::EventsController < Api::V1::Widget::BaseController
 
   def create
     Rails.configuration.dispatcher.dispatch(permitted_params[:name], Time.zone.now, contact_inbox: @contact_inbox, 
-      event_info: permitted_params[:event_info].merge(event_info))
+      event_info: permitted_params[:event_info].to_h.merge(event_info))
     head :no_content
   end
 
@@ -18,6 +18,6 @@ class Api::V1::Widget::EventsController < Api::V1::Widget::BaseController
   end
 
   def permitted_params
-    params.permit(:name, :website_token, :event_info)
+    params.permit(:name, :website_token, event_info: {})
   end
 end
