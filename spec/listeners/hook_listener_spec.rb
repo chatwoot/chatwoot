cbdbda7e7511db23132436a9162e222a97fa3e9e
Sojan <sojan@pepalo.com>
2021-05-04 18:11:33 +0530

    chore: listener spec

--- spec/listeners/hook_listener_spec.rb
+++ spec/listeners/hook_listener_spec.rb
@@ -29,4 +29,23 @@ describe HookListener do
       end
     end
   end
+
+  describe '#message_updated' do
+    let(:event_name) { 'message.updated' }
+
+    context 'when hook is not configured' do
+      it 'does not trigger hook job' do
+        expect(HookJob).to receive(:perform_later).exactly(0).times
+        listener.message_updated(event)
+      end
+    end
+
+    context 'when hook is configured' do
+      it 'triggers hook job' do
+        hook = create(:integrations_hook, account: account)
+        expect(HookJob).to receive(:perform_later).with(hook, 'message.updated', message: message).once
+        listener.message_updated(event)
+      end
+    end
+  end
 end
