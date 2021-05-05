05310e25d44c9883ca0d82fb75db05de93cd4229
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2021-05-05 13:36:45 +0530

    feat: Creates notes list component for CRM (#2195)
    * Feat: Creates note list component for CRM
    
    Co-authored-by: Nithin David Thomas <webofnithin@gmail.com>

--- app/javascript/dashboard/routes/dashboard/contacts/components/ContactNote.vue
+++ app/javascript/dashboard/routes/dashboard/contacts/components/ContactNote.vue
@@ -50,6 +50,10 @@ export default {
   mixins: [timeMixin],
 
   props: {
+    id: {
+      type: Number,
+      default: 0,
+    },
     note: {
       type: String,
       default: '',
@@ -76,10 +80,10 @@ export default {
 
   methods: {
     onEdit() {
-      this.$emit('edit');
+      this.$emit('edit', this.id);
     },
     onDelete() {
-      this.$emit('delete');
+      this.$emit('delete', this.id);
     },
   },
 };
@@ -90,6 +94,7 @@ export default {
   padding: var(--space-small);
   border: 1px solid var(--color-border);
   border-radius: var(--border-radius-small);
+  margin-bottom: var(--space-smaller);
 
   .text {
     padding-bottom: var(--space-small);
