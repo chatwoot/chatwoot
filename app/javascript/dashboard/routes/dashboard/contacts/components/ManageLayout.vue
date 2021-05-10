a33617cadd5987f98ed7a6271bde72daa7701e12
Nithin David Thomas <webofnithin@gmail.com>
2021-05-10 17:11:08 +0530

    feat: Adds 3 column layout for contact page (#2174)
    * Feat: Adds 3 column layout for contact page
    
    * Makes a mixin for 3 column layouts
    
    * Fixes story meta
    
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>

---
+++ app/javascript/dashboard/routes/dashboard/contacts/components/ManageLayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrap">
+    <div class="left"></div>
+    <div class="center"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  components: {},
+  props: {
+    contactId: {
+      type: [String, Number],
+      default: 0,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters({
+      uiFlags: 'contacts/getUIFlags',
+    }),
+    showEmptySearchResult() {
+      const hasEmptyResults = !!this.searchQuery && this.records.length === 0;
+      return hasEmptyResults;
+    },
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~dashboard/assets/scss/mixins';
+
+.wrap {
+  @include three-column-grid(27.2rem);
+  background: var(--color-background);
+  border-top: 1px solid var(--color-border);
+}
+
+.center {
+  border-right: 1px solid var(--color-border);
+  border-left: 1px solid var(--color-border);
+}
+</style>
