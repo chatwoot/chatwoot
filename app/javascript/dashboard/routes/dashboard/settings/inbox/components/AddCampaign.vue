c4eff83a099fa21a6afae9e07ee9b3a156977379
Muhsin Keloth <muhsinkeramam@gmail.com>
2021-05-10 21:41:43 +0530

    chore: Add bot in campaign add/edit form (#2246)
    * fix empty message condition in campaign table
    
    * add bot in campaign
    
    * locale changes
    
    * review fixes

--- app/javascript/dashboard/routes/dashboard/settings/inbox/components/AddCampaign.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/components/AddCampaign.vue
@@ -36,7 +36,7 @@
             {{ $t('CAMPAIGN.ADD.FORM.SENT_BY.LABEL') }}
             <select v-model="selectedSender">
               <option
-                v-for="sender in senderList"
+                v-for="sender in sendersAndBotList"
                 :key="sender.name"
                 :value="sender.id"
               >
@@ -132,7 +132,7 @@ export default {
     return {
       title: '',
       message: '',
-      selectedSender: '',
+      selectedSender: 0,
       endPoint: '',
       timeOnPage: 10,
       show: true,
@@ -173,7 +173,17 @@ export default {
         this.uiFlags.isCreating
       );
     },
+    sendersAndBotList() {
+      return [
+        {
+          id: 0,
+          name: 'Bot',
+        },
+        ...this.senderList,
+      ];
+    },
   },
+
   methods: {
     async addCampaign() {
       try {
@@ -181,7 +191,7 @@ export default {
           title: this.title,
           message: this.message,
           inbox_id: this.$route.params.inboxId,
-          sender_id: this.selectedSender,
+          sender_id: this.selectedSender || null,
           enabled: this.enabled,
           trigger_rules: {
             url: this.endPoint,
