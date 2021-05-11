873b74eae20adbb6c17db5d54bb44faad19e3ae9
Muhsin <muhsinkeramam@gmail.com>
2021-05-11 19:40:15 +0530

    update start campaigns helper

---
+++ app/javascript/widget/helpers/campaignHelper.js
@@ -0,0 +1,35 @@
+import { triggerCampaign } from 'widget/api/campaign';
+window.campaignTimers = [];
+const startCampaigns = async ({ allCampaigns, URL }) => {
+  const matchedURL = URL || window?.parent?.location?.href;
+  // Clear all timeouts
+  window.campaignTimers.forEach(timerId => {
+    clearTimeout(timerId);
+  });
+  // Find all campaigns matches the URL
+  const filteredCampaigns = allCampaigns
+    .map(item => {
+      return {
+        ...item,
+        timeOnpage: item?.trigger_rules?.time_on_page,
+        url: item?.trigger_rules?.url,
+      };
+    })
+    .filter(item => {
+      return item.url === matchedURL;
+    });
+
+  // Execute campaigns
+  filteredCampaigns.forEach(campaign => {
+    const {
+      trigger_rules: { time_on_page: timeOnPage },
+      id: campaignId,
+    } = campaign;
+    const timeoutID = setTimeout(async () => {
+      await triggerCampaign({ campaignId });
+    }, timeOnPage * 1000);
+    window.campaignTimers.push(timeoutID);
+  });
+};
+
+export { startCampaigns };
