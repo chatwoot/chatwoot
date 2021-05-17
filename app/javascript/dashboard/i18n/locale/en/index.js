17caef032309f7993bb9f19b4c7d97c9508a2819
Sojan Jose <sojan@pepalo.com>
2021-05-17 23:53:19 +0530

    chore: Update language translations (#2280)
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>

--- app/javascript/dashboard/i18n/locale/en/index.js
+++ app/javascript/dashboard/i18n/locale/en/index.js
@@ -1,38 +1,37 @@
 import { default as _agentMgmt } from './agentMgmt.json';
-import { default as _labelsMgmt } from './labelsMgmt.json';
+import { default as _campaign } from './campaign.json';
 import { default as _cannedMgmt } from './cannedMgmt.json';
 import { default as _chatlist } from './chatlist.json';
 import { default as _contact } from './contact.json';
 import { default as _conversation } from './conversation.json';
+import { default as _generalSettings } from './generalSettings.json';
 import { default as _inboxMgmt } from './inboxMgmt.json';
+import { default as _integrations } from './integrations.json';
+import { default as _labelsMgmt } from './labelsMgmt.json';
 import { default as _login } from './login.json';
 import { default as _report } from './report.json';
 import { default as _resetPassword } from './resetPassword.json';
 import { default as _setNewPassword } from './setNewPassword.json';
 import { default as _settings } from './settings.json';
 import { default as _signup } from './signup.json';
-import { default as _integrations } from './integrations.json';
-import { default as _generalSettings } from './generalSettings.json';
 import { default as _teamsSettings } from './teamsSettings.json';
-
-import { default as _campaign } from './campaign.json';
 
 export default {
   ..._agentMgmt,
+  ..._campaign,
   ..._cannedMgmt,
   ..._chatlist,
   ..._contact,
   ..._conversation,
+  ..._generalSettings,
   ..._inboxMgmt,
+  ..._integrations,
+  ..._labelsMgmt,
   ..._login,
   ..._report,
-  ..._labelsMgmt,
   ..._resetPassword,
   ..._setNewPassword,
   ..._settings,
   ..._signup,
-  ..._integrations,
-  ..._generalSettings,
   ..._teamsSettings,
-  ..._campaign,
 };
