53a78db3d593c55f4660c7c56cad387d9fbd4f5a
sivin-git <iamsivin@gmail.com>
2021-05-18 14:01:28 +0530

    feat: Creates section header component.

---
+++ app/javascript/dashboard/routes/dashboard/contacts/components/SectionHeader.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="wrap">
+    <div class="header">
+      <h5 class="block-title">
+        Activities
+      </h5>
+    </div>
+    <div class="button-wrap">
+      <woot-button
+        variant="hollow"
+        size="tiny"
+        color-scheme="secondary"
+        class-names="buttons"
+        label="notes"
+        @click="onClickNotes"
+      >
+        notes
+      </woot-button>
+      <woot-button
+        variant="hollow"
+        size="tiny"
+        color-scheme="secondary"
+        class-names="buttons"
+        label="events"
+        @click="onClickEvents"
+      >
+        events
+      </woot-button>
+      <woot-button
+        variant="hollow"
+        size="tiny"
+        color-scheme="secondary"
+        class-names="buttons"
+        label="conversations"
+        @click="onClickConversation"
+      >
+        conversations
+      </woot-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClickNotes() {
+      this.$emit('notes');
+    },
+
+    onClickEvents() {
+      this.$emit('events');
+    },
+
+    onClickConversation() {
+      this.$emit('conversation');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  width: 100%;
+  padding: var(--space-small);
+
+  .header {
+    padding: var(--space-smaller) 0;
+  }
+
+  .button-wrap {
+    display: flex;
+    margin-top: var(--space-smaller);
+
+    .buttons {
+      margin-right: var(--space-one);
+    }
+  }
+}
+</style>
