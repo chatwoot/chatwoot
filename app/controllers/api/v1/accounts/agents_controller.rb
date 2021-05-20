c95163cb4b6099265bc6444fd2c3f14a73bbeb84
Sojan <sojan@pepalo.com>
2021-05-20 20:59:32 +0530

    chore: fix rubocop

--- app/controllers/api/v1/accounts/agents_controller.rb
+++ app/controllers/api/v1/accounts/agents_controller.rb
@@ -39,7 +39,7 @@ class Api::V1::Accounts::AgentsController < Api::V1::Accounts::BaseController
   end
 
   # TODO: move this to a builder and combine the save account user method into a builder
-  # ensure the account user association is also created in a single transaction 
+  # ensure the account user association is also created in a single transaction
   def create_user
     return if @user
 
