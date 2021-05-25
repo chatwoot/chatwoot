dd0a8146909e4f7d594420f1629e8a77101096dd
Muhsin Keloth <muhsinkeramam@gmail.com>
2021-05-25 14:00:21 +0530

    chore: Add missing frontend specs (#2329)
    * complete dshboard api specs
    
    * code cleanup
    
    * add conversation mixin spec
    
    * add isadmin mixin spec
    
    * add agent details component spec
    
    * add notification badge spec
    
    * spec for thumbnail exist in agent details
    
    * fix the deprecation warnings
    
    * add agent details spec
    
    * add account selector specs
    
    * code cleanup
    
    * refactor contact spec
    
    * review fixes
    
    * review fixes
    
    * add shared spec helper
    
    * update api spec helper
    
    * review fixes

--- app/javascript/dashboard/api/specs/inboxes.spec.js
+++ app/javascript/dashboard/api/specs/inboxes.spec.js
@@ -1,13 +1,31 @@
-import inboxes from '../inboxes';
+import inboxesAPI from '../inboxes';
 import ApiClient from '../ApiClient';
+import describeWithAPIMock from './apiSpecHelper';
 
 describe('#InboxesAPI', () => {
   it('creates correct instance', () => {
-    expect(inboxes).toBeInstanceOf(ApiClient);
-    expect(inboxes).toHaveProperty('get');
-    expect(inboxes).toHaveProperty('show');
-    expect(inboxes).toHaveProperty('create');
-    expect(inboxes).toHaveProperty('update');
-    expect(inboxes).toHaveProperty('delete');
+    expect(inboxesAPI).toBeInstanceOf(ApiClient);
+    expect(inboxesAPI).toHaveProperty('get');
+    expect(inboxesAPI).toHaveProperty('show');
+    expect(inboxesAPI).toHaveProperty('create');
+    expect(inboxesAPI).toHaveProperty('update');
+    expect(inboxesAPI).toHaveProperty('delete');
+    expect(inboxesAPI).toHaveProperty('getAssignableAgents');
+    expect(inboxesAPI).toHaveProperty('getCampaigns');
+  });
+  describeWithAPIMock('API calls', context => {
+    it('#getAssignableAgents', () => {
+      inboxesAPI.getAssignableAgents(1);
+      expect(context.axiosMock.get).toHaveBeenCalledWith(
+        '/api/v1/inboxes/1/assignable_agents'
+      );
+    });
+
+    it('#getCampaigns', () => {
+      inboxesAPI.getCampaigns(2);
+      expect(context.axiosMock.get).toHaveBeenCalledWith(
+        '/api/v1/inboxes/2/campaigns'
+      );
+    });
   });
 });
