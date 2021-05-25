16c7bda876cd48250e13a03556e15adc8a4cd42e
Tim Johnson <jhnsntmthy@gmail.com>
2021-05-25 11:27:18 -0400

    cors for everything

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins '*'
-      resource '/packs/*', headers: :any, methods: [:get, :options]
+      resource '*', headers: :any, methods: [:get, :options]
     end
   end
 end
