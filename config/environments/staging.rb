eb76d58c2193181d9a1b3394798ed63ed623b8e0
Tim Johnson <jhnsntmthy@gmail.com>
2021-05-25 11:28:06 -0400

    fix more CORS methods

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins '*'
-      resource '*', headers: :any, methods: [:get, :options]
+      resource '*', headers: :any, methods: [:get, :post, :put, :options]
     end
   end
 end
