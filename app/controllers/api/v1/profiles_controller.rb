05d6e7ea103b17193f53d73f13fe94b8bae1c051
Sojan <sojan@pepalo.com>
2021-05-28 01:01:43 +0530

    chore: update password API

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -6,13 +6,18 @@ class Api::V1::ProfilesController < Api::BaseController
   end
 
   def update
+    if password_params[:password].present?
+      render_could_not_create_error('Invalid current password') and return unless @user.valid_password?(password_params[:current_password])
+      @user.update!(password_params.except(:current_password))
+    end
+
     @user.update!(profile_params)
   end
 
   private
 
   def set_user
-    @user = current_user
+    @user = current_user 
   end
 
   def profile_params
@@ -20,11 +25,17 @@ class Api::V1::ProfilesController < Api::BaseController
       :email,
       :name,
       :display_name,
-      :password,
-      :password_confirmation,
       :avatar,
       :availability,
       ui_settings: {}
+    )
+  end
+
+  def password_params
+    params.require(:profile).permit(
+      :current_password,
+      :password,
+      :password_confirmation
     )
   end
 end
