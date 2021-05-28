5fd08e3ad648948d055866623b268077f0022adf
Pranav Raj S <pranav@chatwoot.com>
2021-05-28 13:33:15 +0530

    fix ESLint error

--- app/javascript/dashboard/routes/auth/ResetPassword.vue
+++ app/javascript/dashboard/routes/auth/ResetPassword.vue
@@ -30,7 +30,6 @@
 <script>
 import { required, minLength, email } from 'vuelidate/lib/validators';
 import Auth from '../../api/auth';
-import { frontendURL } from '../../helper/URLHelper';
 
 export default {
   data() {
