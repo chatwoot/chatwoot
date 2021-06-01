d898972dc728693338cbc9f447c729eb7cc55fe6
Sojan Jose <sojan@pepalo.com>
2021-06-01 22:34:25 +0530

    feat: Add AgentBot APIs (#2323)

--- app/models/agent_bot.rb
+++ app/models/agent_bot.rb
@@ -2,13 +2,21 @@
 #
 # Table name: agent_bots
 #
-#  id                               :bigint           not null, primary key
-#  description                      :string
-#  hide_input_for_bot_conversations :boolean          default(FALSE)
-#  name                             :string
-#  outgoing_url                     :string
-#  created_at                       :datetime         not null
-#  updated_at                       :datetime         not null
+#  id           :bigint           not null, primary key
+#  description  :string
+#  name         :string
+#  outgoing_url :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  account_id   :bigint
+#
+# Indexes
+#
+#  index_agent_bots_on_account_id  (account_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (account_id => accounts.id)
 #
 
 class AgentBot < ApplicationRecord
@@ -18,6 +26,7 @@ class AgentBot < ApplicationRecord
   has_many :agent_bot_inboxes, dependent: :destroy
   has_many :inboxes, through: :agent_bot_inboxes
   has_many :messages, as: :sender, dependent: :restrict_with_exception
+  belongs_to :account, dependent: :destroy, optional: true
 
   def available_name
     name
