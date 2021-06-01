1cb3b3342c7fb9050e1989df4107f69bbd27ad0f
Jacob Ierome <34082168+ierome@users.noreply.github.com>
2021-06-01 10:23:19 -0700

    Config RB modification redis

--- lib/redis/config.rb
+++ lib/redis/config.rb
@@ -20,7 +20,8 @@ module Redis::Config
         url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'),
         password: ENV.fetch('REDIS_PASSWORD', nil).presence,
         reconnect_attempts: 2,
-        network_timeout: 5
+        network_timeout: 5,
+        ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
       }
     end
 
