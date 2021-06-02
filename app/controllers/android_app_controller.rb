dd599c27ed29fb8f1fc48f76d418c60524b3b74a
Muhsin <muhsinkeramam@gmail.com>
2021-06-02 16:11:06 +0530

    minor fixes

--- app/controllers/android_app_controller.rb
+++ app/controllers/android_app_controller.rb
@@ -1,6 +1,5 @@
 class AndroidAppController < ApplicationController
   def assetlinks
-    assetlinks_json = render_to_string action: 'assetlinks', layout: false
-    render assetlinks_json, filename: 'assetlinks', type: 'application/json'
+    render layout: false
   end
 end
