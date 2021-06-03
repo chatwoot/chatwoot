2fd08aaf3555dff8ee179bb8735cfa3ae621742f
Sivin Varghese <iamsivin@gmail.com>
2021-06-03 23:25:55 +0530

    Adds back select field example.

--- lib/woot_message_seeder.rb
+++ lib/woot_message_seeder.rb
@@ -79,7 +79,9 @@ module WootMessageSeeder
           { "name": 'email', "placeholder": 'Please enter your email', "type": 'email', "label": 'Email', "required": 'required',
             "regex": '^[^\s@]+@[^\s@]+\.[^\s@]+$' },
           { "name": 'text_area', "placeholder": 'Please enter text', "type": 'text_area', "label": 'Large Text', "required": 'required' },
-          { "name": 'text', "placeholder": 'Please enter text', "type": 'text', "label": 'text', "default": 'defaut value', "required": 'required' }
+          { "name": 'text', "placeholder": 'Please enter text', "type": 'text', "label": 'text', "default": 'defaut value', "required": 'required' },
+          { "name": 'select', "label": 'Select Option', "type": 'select', "options": [{ "label": '🌯 Burito', "value": 'Burito' },
+                                                                                      { "label": '🍝 Pasta', "value": 'Pasta' }] }
         ]
       }
     )
