9d755f122b60955aa8979fd63c703e8f066ca098
Sojan <sojan@pepalo.com>
2021-06-04 17:28:07 +0530

    chore: rubocop fixes

--- lib/woot_message_seeder.rb
+++ lib/woot_message_seeder.rb
@@ -74,19 +74,23 @@ module WootMessageSeeder
       message_type: :template,
       content_type: 'form',
       content: 'form',
-      content_attributes: {
-        "items": [
-          { "name": 'email', "placeholder": 'Please enter your email', "type": 'email', "label": 'Email', "required": 'required',
-            "error": 'Please fill this field', "pattern": '^[^\s@]+@[^\s@]+\.[^\s@]+$' },
-          { "name": 'text_area', "placeholder": 'Please enter text', "type": 'text_area', "label": 'Large Text', "required": 'required',
-            "error": 'Please fill this field' },
-          { "name": 'text', "placeholder": 'Please enter text', "type": 'text', "label": 'text', "default": 'defaut value', "required": 'required',
-            "pattern": '^[a-zA-Z ]*$' },
-          { "name": 'select', "label": 'Select Option', "type": 'select', "options": [{ "label": '🌯 Burito', "value": 'Burito' },
-                                                                                      { "label": '🍝 Pasta', "value": 'Pasta' }] }
-        ]
-      }
+      content_attributes: sample_form
     )
+  end
+
+  def self.sample_form
+    {
+      "items": [
+        { "name": 'email', "placeholder": 'Please enter your email', "type": 'email', "label": 'Email', "required": 'required',
+          "error": 'Please fill this field', "pattern": '^[^\s@]+@[^\s@]+\.[^\s@]+$' },
+        { "name": 'text_area', "placeholder": 'Please enter text', "type": 'text_area', "label": 'Large Text', "required": 'required',
+          "error": 'Please fill this field' },
+        { "name": 'text', "placeholder": 'Please enter text', "type": 'text', "label": 'text', "default": 'defaut value', "required": 'required',
+          "pattern": '^[a-zA-Z ]*$' },
+        { "name": 'select', "label": 'Select Option', "type": 'select', "options": [{ "label": '🌯 Burito', "value": 'Burito' },
+                                                                                    { "label": '🍝 Pasta', "value": 'Pasta' }] }
+      ]
+    }
   end
 
   def self.create_sample_articles_message(conversation)
