c0fcc339467aacf184c44289bc6136d71bdae24f
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2021-06-08 16:32:01 +0530

    feat: Add regex validation on form message inputs (#2317)
    Co-authored-by: Nithin David Thomas <webofnithin@gmail.com>
    Co-authored-by: Sojan <sojan@pepalo.com>

--- app/models/concerns/content_attribute_validator.rb
+++ app/models/concerns/content_attribute_validator.rb
@@ -2,7 +2,7 @@ class ContentAttributeValidator < ActiveModel::Validator
   ALLOWED_SELECT_ITEM_KEYS = [:title, :value].freeze
   ALLOWED_CARD_ITEM_KEYS = [:title, :description, :media_url, :actions].freeze
   ALLOWED_CARD_ITEM_ACTION_KEYS = [:text, :type, :payload, :uri].freeze
-  ALLOWED_FORM_ITEM_KEYS = [:type, :placeholder, :label, :name, :options, :default].freeze
+  ALLOWED_FORM_ITEM_KEYS = [:type, :placeholder, :label, :name, :options, :default, :required, :pattern, :title, :pattern_error].freeze
   ALLOWED_ARTICLE_KEYS = [:title, :description, :link].freeze
 
   def validate(record)
