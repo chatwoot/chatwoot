557e9aaca6b2833fea55c79a273fbf8e8114f5ef
Muhsin <muhsinkeramam@gmail.com>
2021-06-09 11:02:43 +0530

    add more specs

--- app/javascript/dashboard/routes/dashboard/settings/labels/specs/validationMixin.spec.js
+++ app/javascript/dashboard/routes/dashboard/settings/labels/specs/validationMixin.spec.js
@@ -1,30 +1,76 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import VueI18n from 'vue-i18n';
-import validationMixin from '../validationMixin';
+import Vuelidate from 'vuelidate';
 
+import validationMixin from '../validationMixin';
+import validations from '../validations';
 import i18n from 'dashboard/i18n';
 const localVue = createLocalVue();
-localVue.use(VueI18n);
 
+localVue.use(VueI18n);
+localVue.use(Vuelidate);
 const i18nConfig = new VueI18n({
   locale: 'en',
   messages: i18n,
 });
+const Component = {
+  render() {},
+  title: 'TestComponent',
+  mixins: [validationMixin],
+  validations,
+};
 
 describe('validationMixin', () => {
-  it('should return label required error message', () => {
-    const Component = {
-      render() {},
-      title: 'TestComponent',
-      mixins: [validationMixin],
+  it('it should return empty error message if valid label name passed', async () => {
+    const wrapper = shallowMount(Component, {
+      i18n: i18nConfig,
+      localVue,
+      data() {
+        return {
+          title: 'sales',
+        };
+      },
+    });
+    expect(wrapper.vm.getLabelTitleErrorMessage).toBe('');
+  });
+  it('it should return label required error message if empty name is passed', async () => {
+    const wrapper = shallowMount(Component, {
+      i18n: i18nConfig,
+      localVue,
       data() {
         return {
           title: '',
         };
       },
-    };
-
-    const wrapper = shallowMount(Component, { i18n: i18nConfig, localVue });
+    });
     expect(wrapper.vm.getLabelTitleErrorMessage).toBe('Label Name is required');
+  });
+  it('it should return label minimum length error message if one charceter label name is passed', async () => {
+    const wrapper = shallowMount(Component, {
+      i18n: i18nConfig,
+      localVue,
+      data() {
+        return {
+          title: 's',
+        };
+      },
+    });
+    expect(wrapper.vm.getLabelTitleErrorMessage).toBe(
+      'Miniumum length 2 is required'
+    );
+  });
+  it('it should return invalid character error message if invalid lable name passed', async () => {
+    const wrapper = shallowMount(Component, {
+      i18n: i18nConfig,
+      localVue,
+      data() {
+        return {
+          title: 'sales enquiry',
+        };
+      },
+    });
+    expect(wrapper.vm.getLabelTitleErrorMessage).toBe(
+      'Only Alphabets, Numbers, Hyphen and Underscore are allowed'
+    );
   });
 });
