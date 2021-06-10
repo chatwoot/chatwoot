822c00707b7f7f1dfab3a29f0ebffb60e5304a39
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2021-06-10 09:35:15 +0530

    chore: Cleanup page-top-bar style conflicts (#2407)
    * chore: Class name cleanup page-top-bar.
    
    * prettier spacing fixes
    
    * Review fixes and adds storybook
    
    * Circleci fixes

---
+++ app/javascript/dashboard/components/widgets/SettingIntroBanner.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="column page-top-banner">
+    <h2 class="page-sub-title">
+      {{ headerTitle }}
+    </h2>
+    <p v-if="headerContent" class="small-12 column">
+      {{ headerContent }}
+    </p>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    headerTitle: {
+      type: String,
+      default: '',
+    },
+    headerContent: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.page-top-banner {
+  border-bottom: 1px solid var(--color-border);
+  background: var(--color-background-light);
+  padding: var(--space-normal) var(--space-large) 0;
+}
+</style>
