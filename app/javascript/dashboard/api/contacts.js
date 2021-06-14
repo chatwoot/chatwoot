d21c1c773b26eb87cdc938a3e8bcf596a778c3ab
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2021-06-14 10:36:00 +0530

    feat: Ability to add label for contact page (#2350)
    * feat: Ability to add label for contact page
    
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>
    Co-authored-by: Nithin David Thomas <webofnithin@gmail.com>

--- app/javascript/dashboard/api/contacts.js
+++ app/javascript/dashboard/api/contacts.js
@@ -18,6 +18,14 @@ class ContactAPI extends ApiClient {
     return axios.get(`${this.url}/${contactId}/contactable_inboxes`);
   }
 
+  getContactLabels(contactId) {
+    return axios.get(`${this.url}/${contactId}/labels`);
+  }
+
+  updateContactLabels(contactId, labels) {
+    return axios.post(`${this.url}/${contactId}/labels`, { labels });
+  }
+
   search(search = '', page = 1, sortAttr = 'name') {
     return axios.get(
       `${this.url}/search?q=${search}&page=${page}&sort=${sortAttr}`
