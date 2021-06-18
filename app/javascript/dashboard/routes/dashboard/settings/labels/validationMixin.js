6c836311f5dc45da95849a0b0f4a0441c78e9b7f
Pranav Raj S <pranav@chatwoot.com>
2021-06-18 17:41:52 +0530

    Reduce return

--- app/javascript/dashboard/routes/dashboard/settings/labels/validationMixin.js
+++ app/javascript/dashboard/routes/dashboard/settings/labels/validationMixin.js
@@ -1,17 +1,16 @@
 export default {
   computed: {
     getLabelTitleErrorMessage() {
-      if (!this.$v.title.$error) {
-        return '';
-      }
-      if (!this.title) {
-        return this.$t('LABEL_MGMT.FORM.NAME.REQUIRED_ERROR');
-      }
-      if (!this.$v.title.minLength) {
-        return this.$t('LABEL_MGMT.FORM.NAME.MINIMUM_LENGTH_ERROR');
-      }
-      if (!this.$v.title.validLabelCharacters) {
-        return this.$t('LABEL_MGMT.FORM.NAME.VALID_ERROR');
+      if (this.$v.title.$error) {
+        if (!this.$v.title.required) {
+          return this.$t('LABEL_MGMT.FORM.NAME.REQUIRED_ERROR');
+        }
+        if (!this.$v.title.minLength) {
+          return this.$t('LABEL_MGMT.FORM.NAME.MINIMUM_LENGTH_ERROR');
+        }
+        if (!this.$v.title.validLabelCharacters) {
+          return this.$t('LABEL_MGMT.FORM.NAME.VALID_ERROR');
+        }
       }
       return '';
     },
