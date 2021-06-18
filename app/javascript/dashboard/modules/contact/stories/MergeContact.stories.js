9eb5a7bc24ee9ec229cf0b9f69e864c347d56f45
Nithin David <1277421+nithindavid@users.noreply.github.com>
2021-06-18 19:35:03 +0530

    Fixes broken stories

--- app/javascript/dashboard/modules/contact/stories/MergeContact.stories.js
+++ app/javascript/dashboard/modules/contact/stories/MergeContact.stories.js
@@ -1,17 +1,32 @@
 import { action } from '@storybook/addon-actions';
-import MergeContact from './MergeContact';
+import MergeContact from 'dashboard/modules/contact/components/MergeContact';
 
 export default {
   title: 'Components/Contact/MergeContacts',
   component: MergeContact,
-  argTypes: {},
+  argTypes: {
+    'primary-contact': {
+      defaultValue: '{}',
+      control: {
+        type: 'object',
+      },
+    },
+  },
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { MergeContact },
-  template: '<merge-contact v-bind="$props"></merge-contact>',
+  template:
+    '<merge-contact v-bind="$props" @submit="onSearch" @search="onSubmit"></merge-contact>',
 });
 
 export const List = Template.bind({});
-List.args = {};
+List.args = {
+  primaryContact: {
+    id: 12,
+    name: 'Mason Mount',
+  },
+  onSearch: action('Search'),
+  onSubmit: action('Submit'),
+};
