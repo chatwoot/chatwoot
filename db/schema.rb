e4e1186d05dcba32ff36491fdbf737b5aa32faf9
thundersparkf <devagastya0@gmail.com>
2021-06-20 00:02:06 +0530

    Added model for Gupshup

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -169,6 +169,15 @@ ActiveRecord::Schema.define(version: 2021_06_23_155413) do
     t.datetime "updated_at", null: false
     t.index ["page_id", "account_id"], name: "index_channel_facebook_pages_on_page_id_and_account_id", unique: true
     t.index ["page_id"], name: "index_channel_facebook_pages_on_page_id"
+  end
+
+  create_table "channel_gupshup", force: :cascade do |t|
+    t.string "account_id", null: false
+    t.string "app", null: false
+    t.string "apikey", null: false
+    t.integer "phone_number", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "channel_twilio_sms", force: :cascade do |t|
