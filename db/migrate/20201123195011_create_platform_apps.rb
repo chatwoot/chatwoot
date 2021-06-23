31e37d29a5612a56c069308d99b079a8460d08b6
Pranav Raj S <pranav@chatwoot.com>
2021-06-23 22:15:32 +0530

    feat: Add APIs to create custom views on the dashboard

--- db/migrate/20201123195011_create_platform_apps.rb
+++ db/migrate/20201123195011_create_platform_apps.rb
@@ -2,7 +2,9 @@ class CreatePlatformApps < ActiveRecord::Migration[6.0]
   def change
     create_table :platform_apps do |t|
       t.string :name, null: false
-      t.timestamps
+      t.integer :type, null: false, default: 0
+      t
+        .t.timestamps
     end
   end
 end
