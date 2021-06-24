8964d80b04e4e0b7735b770df5c252943c2741d3
Sivin Varghese <iamsivin@gmail.com>
2021-06-24 10:42:34 +0530

    Review fixes

--- app/javascript/shared/helpers/AudioNotificationHelper.js
+++ app/javascript/shared/helpers/AudioNotificationHelper.js
@@ -75,13 +75,13 @@ export const newMessageNotification = data => {
   const { conversation_id: incomingConvId } = data;
   const currentConv =
     window.WOOT.$store.getters.getConversationById(incomingConvId) || {};
-  const assignee = currentConv.meta.assignee;
   let id;
-  let userId;
-  if (assignee) {
-    id = assignee.id;
+  if (currentConv.meta) {
+    const assignee = currentConv.meta.assignee;
+    if (assignee) {
+      id = assignee.id;
+    }
   }
-  userId = assignee.id;
   const isDocHiddden = document.hidden;
   const {
     enable_audio_alerts: enableAudioAlerts = false,
@@ -94,7 +94,7 @@ export const newMessageNotification = data => {
   );
   const playAudioByUserSettings = shouldPlayByUserSettings(
     enableAudioAlerts,
-    userId,
+    currentUserId,
     id
   );
   if (playAudio && playAudioByUserSettings) {
