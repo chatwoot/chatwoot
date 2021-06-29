28027acabd9d0ab0b33118e0d814e5d65b3a512c
Pranav Raj S <pranav@chatwoot.com>
2021-06-29 19:08:55 +0530

    revert type

--- db/migrate/20201123195011_create_platform_apps.rb
+++ db/migrate/20201123195011_create_platform_apps.rb
@@ -2,7 +2,6 @@ class CreatePlatformApps < ActiveRecord::Migration[6.0]
   def change
     create_table :platform_apps do |t|
       t.string :name, null: false
-      t.integer :type, null: false, default: 0
       t.timestamps
     end
   end
