55e0457ffb07f6cbed335005e168674e1cb350de
Sojan Jose <sojan@pepalo.com>
2021-07-06 18:45:52 +0530

    chore: clean up

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -3,6 +3,3 @@ sed -i -e '/REDIS_URL/ s/=.*/=redis:\/\/localhost:6379/' .env
 sed -i -e '/POSTGRES_HOST/ s/=.*/=localhost/' .env
 sed -i -e '/SMTP_ADDRESS/ s/=.*/=localhost/' .env
 sed -i -e "/FRONTEND_URL/ s/=.*/=https:\/\/$CODESPACE_NAME-3000.githubpreview.dev/" .env
-
-
-
