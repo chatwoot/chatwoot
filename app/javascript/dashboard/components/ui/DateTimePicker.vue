78fae4c332eb8bf830ff2808a9f835e60ab56173
Muhsin <muhsinkeramam@gmail.com>
2021-07-14 01:33:02 +0530

    add date time picker

---
+++ app/javascript/dashboard/components/ui/DateTimePicker.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="woot-time-range-picker">
+    <date-picker
+      type="datetime"
+      :confirm="true"
+      :clearable="false"
+      :editable="false"
+      :confirm-text="confirmText"
+      :placeholder="placeholder"
+      :value="value"
+      @change="handleChange"
+    />
+  </div>
+</template>
+
+<script>
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+export default {
+  components: { DatePicker },
+  props: {
+    confirmText: {
+      type: String,
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    value: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    handleChange(value) {
+      this.$emit('change', value);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.mx-datepicker-popup {
+  z-index: 99999;
+}
+.woot-time-range-picker {
+  .mx-datepicker {
+    width: 100%;
+  }
+  .mx-input {
+    display: flex;
+    border: 1px solid var(--color-border);
+    border-radius: var(--border-radius-normal);
+    box-shadow: none;
+    height: 4.6rem;
+  }
+
+  .mx-input:disabled,
+  .mx-input[readonly] {
+    background-color: var(--white);
+    cursor: pointer;
+  }
+}
+</style>
