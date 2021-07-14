cb44eb2964b31a657b2483a5f57080deb74a47bc
Pranav Raj S <pranav@chatwoot.com>
2021-07-14 10:20:06 +0530

    feat: Add CSAT reports (#2608)

--- app/javascript/dashboard/routes/dashboard/settings/reports/reports.routes.js
+++ app/javascript/dashboard/routes/dashboard/settings/reports/reports.routes.js
@@ -1,4 +1,5 @@
 import Index from './Index';
+import CsatResponses from './CsatResponses';
 import SettingsContent from '../Wrapper';
 import { frontendURL } from '../../../../helper/URLHelper';
 
@@ -9,15 +10,34 @@ export default {
       component: SettingsContent,
       props: {
         headerTitle: 'REPORT.HEADER',
-        headerButtonText: 'REPORT.HEADER_BTN_TXT',
         icon: 'ion-arrow-graph-up-right',
       },
       children: [
         {
           path: '',
+          redirect: 'overview',
+        },
+        {
+          path: 'overview',
           name: 'settings_account_reports',
           roles: ['administrator'],
           component: Index,
+        },
+      ],
+    },
+    {
+      path: frontendURL('accounts/:accountId/reports'),
+      component: SettingsContent,
+      props: {
+        headerTitle: 'CSAT_REPORTS.HEADER',
+        icon: 'ion-happy-outline',
+      },
+      children: [
+        {
+          path: 'csat',
+          name: 'csat_reports',
+          roles: ['administrator'],
+          component: CsatResponses,
         },
       ],
     },
