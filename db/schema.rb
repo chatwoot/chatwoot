be242b88f3d2250b9bfe5abab155051ecad7e6e8
Mrunal Selokar <mrunal@idyllic.co>
2021-07-15 15:24:38 +0530

    chore: Removing duplicate callback to create auth token (#2624)
    Removed after create callback in user model to create a new access token. This is already being taken care of by AccessTokenable module.
    
    Fixes #2620

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_140842) do
+ActiveRecord::Schema.define(version: 2021_07_14_110714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
