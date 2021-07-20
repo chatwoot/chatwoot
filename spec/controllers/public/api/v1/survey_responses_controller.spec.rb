b97bf191c96cd330a1b288618e32da296c72362f
Muhsin <muhsinkeramam@gmail.com>
2021-07-21 00:16:29 +0530

    code cleanup

--- spec/controllers/public/api/v1/survey_responses_controller.spec.rb
+++ spec/controllers/public/api/v1/survey_responses_controller.spec.rb
@@ -6,10 +6,11 @@ RSpec.describe 'Public Survey Responses API', type: :request do
   describe 'GET public/api/v1/survey_responses/{uuid}' do
     let!(:conversation) { create(:conversation, account: account) }
     let!(:message) { create(:message, conversation: conversation, account: account, content_type: 'input_csat') }
-     context 'when it is an authenticated user' do
-       it 'return the survey response for that conversation' do
-         get "/public/api/v1/survey_responses/#{conversation.uuid}"
-          expect(response).to have_http_status(:success)
+
+    context 'when it is an authenticated user' do
+      it 'return the survey response for that conversation' do
+        get "/public/api/v1/survey_responses/#{conversation.uuid}"
+        expect(response).to have_http_status(:success)
       end
     end
   end
