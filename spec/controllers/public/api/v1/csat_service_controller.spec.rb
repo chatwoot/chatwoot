34f2746d539788653a4497012358169723c79102
Muhsin <muhsinkeramam@gmail.com>
2021-07-21 00:29:21 +0530

    fix specs

--- spec/controllers/public/api/v1/csat_service_controller.spec.rb
+++ spec/controllers/public/api/v1/csat_service_controller.spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Public Survey Responses API', type: :request do
   let(:account) { create(:account) }
+  let!(:conversation) { create(:conversation, account: account) }
+  let!(:message) { create(:message, conversation: conversation, account: account, content_type: 'input_csat') }
 
-  describe 'GET public/api/v1/survey_responses/{uuid}' do
-    let!(:conversation) { create(:conversation, account: account) }
-    let!(:message) { create(:message, conversation: conversation, account: account, content_type: 'input_csat') }
+
+  describe 'GET public/api/v1/csat_service/{uuid}' do
 
     context 'when it is an authenticated user' do
       it 'return the survey response for that conversation' do
-        get "/public/api/v1/survey_responses/#{conversation.uuid}"
+        get "/public/api/v1/csat_service/#{conversation.uuid}"
         expect(response).to have_http_status(:success)
       end
     end
