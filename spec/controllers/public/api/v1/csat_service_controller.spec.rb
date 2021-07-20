0ff6de7ca78fd7550bfb11d75376f70ab97f252f
Muhsin <muhsinkeramam@gmail.com>
2021-07-21 00:29:55 +0530

    format code

--- spec/controllers/public/api/v1/csat_service_controller.spec.rb
+++ spec/controllers/public/api/v1/csat_service_controller.spec.rb
@@ -5,9 +5,7 @@ RSpec.describe 'Public Survey Responses API', type: :request do
   let!(:conversation) { create(:conversation, account: account) }
   let!(:message) { create(:message, conversation: conversation, account: account, content_type: 'input_csat') }
 
-
   describe 'GET public/api/v1/csat_service/{uuid}' do
-
     context 'when it is an authenticated user' do
       it 'return the survey response for that conversation' do
         get "/public/api/v1/csat_service/#{conversation.uuid}"
