8fc72ba36d76bc6b3e799966466ccbe5f3e264fa
Pascal Jufer <pascal-jufer@bluewin.ch>
2021-08-03 17:08:10 +0200

    Use single-quote and remove trailing whitespace

--- docker/entrypoints/helpers/pg_database_url.rb
+++ docker/entrypoints/helpers/pg_database_url.rb
@@ -6,5 +6,5 @@ if !ENV['DATABASE_URL'].nil? && ENV['DATABASE_URL'] != ''
   uri = URI(ENV['DATABASE_URL'])
   puts "export POSTGRES_HOST=#{uri.host} POSTGRES_PORT=#{uri.port} POSTGRES_USERNAME=#{uri.user}"
 elsif ENV['POSTGRES_PORT'].nil? || ENV['POSTGRES_PORT'] == ''
-  puts "export POSTGRES_PORT=5432" 
+  puts 'export POSTGRES_PORT=5432'
 end
