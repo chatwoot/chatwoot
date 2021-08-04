0afda75b24c7695da751f4539fd013a6c40bcddd
Muhsin Keloth <muhsinkeramam@gmail.com>
2021-08-04 16:54:30 +0530

    code cleanup

--- app/javascript/dashboard/routes/dashboard/settings/campaigns/Index.vue
+++ app/javascript/dashboard/routes/dashboard/settings/campaigns/Index.vue
@@ -6,7 +6,7 @@
       icon="ion-android-add-circle"
       @click="openAddPopup"
     >
-      {{ $t('CAMPAIGN.HEADER_BTN_TXT.ONE_OFF') }}
+      {{ buttonText }}
     </woot-button>
     <campaign />
     <woot-modal :show.sync="showAddPopup" :on-close="hideAddPopup">
@@ -16,6 +16,7 @@
 </template>
 
 <script>
+import campaignMixin from 'shared/mixins/campaignMixin';
 import Campaign from './Campaign.vue';
 import AddCampaign from './AddCampaign';
 
@@ -24,8 +25,17 @@ export default {
     Campaign,
     AddCampaign,
   },
+  mixins: [campaignMixin],
   data() {
     return { showAddPopup: false };
+  },
+  computed: {
+    buttonText() {
+      if (this.isOngoingType) {
+        return this.$t('CAMPAIGN.HEADER_BTN_TXT.ONGOING');
+      }
+      return this.$t('CAMPAIGN.HEADER_BTN_TXT.ONE_OFF');
+    },
   },
   mounted() {
     this.$store.dispatch('campaigns/get');
