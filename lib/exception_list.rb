57120f135da7059dadfc17e47d9354bc6ea7e2ed
Sojan <sojan@pepalo.com>
2021-08-07 01:09:19 +0530

    chore: add more errors

--- lib/exception_list.rb
+++ lib/exception_list.rb
@@ -1,6 +1,6 @@
 module ExceptionList
   REST_CLIENT_EXCEPTIONS = [RestClient::NotFound, RestClient::GatewayTimeout, RestClient::BadRequest,
-                            RestClient::MethodNotAllowed, RestClient::Forbidden, RestClient::InternalServerError, RestClient::PayloadTooLarge].freeze
+                            RestClient::MethodNotAllowed, RestClient::Forbidden, RestClient::InternalServerError, RestClient::PayloadTooLarge, SocketError].freeze
   SMTP_EXCEPTIONS = [
     Net::SMTPSyntaxError
   ].freeze
