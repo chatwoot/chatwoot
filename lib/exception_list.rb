468a3ee9cb22b239fea958fcbf83a31af900e9d4
Sojan <sojan@pepalo.com>
2021-08-07 13:27:36 +0530

    chore: fix rubocap exceptions

--- lib/exception_list.rb
+++ lib/exception_list.rb
@@ -1,6 +1,7 @@
 module ExceptionList
   REST_CLIENT_EXCEPTIONS = [RestClient::NotFound, RestClient::GatewayTimeout, RestClient::BadRequest,
-                            RestClient::MethodNotAllowed, RestClient::Forbidden, RestClient::InternalServerError, RestClient::PayloadTooLarge, SocketError].freeze
+                            RestClient::MethodNotAllowed, RestClient::Forbidden, RestClient::InternalServerError,
+                            RestClient::PayloadTooLarge, SocketError].freeze
   SMTP_EXCEPTIONS = [
     Net::SMTPSyntaxError
   ].freeze
