4f21d36d940b72d5d02a166de5b9e08f8198bb16
Sojan Jose <sojan@pepalo.com>
2021-08-12 01:32:22 +0530

    chore: handle more exceptions (#2793)

--- lib/exception_list.rb
+++ lib/exception_list.rb
@@ -1,7 +1,7 @@
 module ExceptionList
   REST_CLIENT_EXCEPTIONS = [RestClient::NotFound, RestClient::GatewayTimeout, RestClient::BadRequest,
                             RestClient::MethodNotAllowed, RestClient::Forbidden, RestClient::InternalServerError,
-                            RestClient::PayloadTooLarge, SocketError].freeze
+                            RestClient::Exceptions::OpenTimeout, RestClient::Exceptions::ReadTimeout, SocketError].freeze
   SMTP_EXCEPTIONS = [
     Net::SMTPSyntaxError
   ].freeze
