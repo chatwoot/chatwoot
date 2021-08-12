473777cffa7f3cb5e50b052a57c469f0a0bd91da
Sojan <sojan@pepalo.com>
2021-08-13 01:42:37 +0530

    chore: fix rubocop

--- lib/exception_list.rb
+++ lib/exception_list.rb
@@ -1,7 +1,7 @@
 module ExceptionList
   REST_CLIENT_EXCEPTIONS = [RestClient::NotFound, RestClient::GatewayTimeout, RestClient::BadRequest,
                             RestClient::MethodNotAllowed, RestClient::Forbidden, RestClient::InternalServerError,
-                            RestClient::Exceptions::OpenTimeout, RestClient::Exceptions::ReadTimeout, 
+                            RestClient::Exceptions::OpenTimeout, RestClient::Exceptions::ReadTimeout,
                             RestClient::MovedPermanently, SocketError].freeze
   SMTP_EXCEPTIONS = [
     Net::SMTPSyntaxError
