6b7055729f8c44528ad2dc5d409c97d411f524f1
Sojan <sojan@pepalo.com>
2021-08-13 20:13:34 +0530

    chore: event for contact merged

--- app/actions/contact_merge_action.rb
+++ app/actions/contact_merge_action.rb
@@ -1,4 +1,5 @@
 class ContactMergeAction
+  include Events::Types
   pattr_initialize [:account!, :base_contact!, :mergee_contact!]
 
   def perform
@@ -44,9 +45,14 @@ class ContactMergeAction
     mergable_attribute_keys = %w[identifier name email phone_number custom_attributes]
     base_contact_attributes = base_contact.attributes.slice(*mergable_attribute_keys).compact_blank
     mergee_contact_attributes = mergee_contact.attributes.slice(*mergable_attribute_keys).compact_blank
+
     # attributes in base contact are given preference
     merged_attributes = mergee_contact_attributes.deep_merge(base_contact_attributes)
+    # retaining old pubsub token to notify the contacts that are listening
+    mergee_pubsub_token = mergee_contact.pubsub_token
+
     @mergee_contact.destroy!
+    Rails.configuration.dispatcher.dispatch(CONTACT_MERGED, Time.zone.now, contact: @base_contact, tokens: [mergee_pubsub_token])
     @base_contact.update!(merged_attributes)
   end
 end
