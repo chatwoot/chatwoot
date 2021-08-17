1136872640a54afa5b94f43c3f3539a98a55df9e
Tejaswini <tejaswini@chatwoot.com>
2021-08-17 21:44:00 +0530

    Action mailer format and updated style for the template

--- app/drops/message_drop.rb
+++ app/drops/message_drop.rb
@@ -1,12 +1,12 @@
 class MessageDrop < BaseDrop
+  include MessageFormatHelper
+
   def sender_display_name
     @obj.sender.try(:display_name)
   end
 
   def text_content
     content = @obj.try(:content)
-    # This will remove /[@user](mention://user/3/user)/ from the content
-    text = content.sub(%r{\[@.+\]\(mention://(user|team)/(\d+)/(.+)\) }, '') if content.present?
-    text
+    transform_user_mention_content content
   end
 end
