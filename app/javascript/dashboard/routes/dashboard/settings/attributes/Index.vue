6ae750da699d23107c564b29608fec0fd849d600
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2021-08-25 20:30:34 +0530

    feat: Creates custom attributes route (#2877)

---
+++ app/javascript/dashboard/routes/dashboard/settings/attributes/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="column content-box">
+    <woot-button
+      color-scheme="success"
+      class-names="button--fixed-right-top"
+      icon="ion-android-add-circle"
+    >
+      {{ $t('ATTRIBUTES_MGMT.HEADER_BTN_TXT') }}
+    </woot-button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
