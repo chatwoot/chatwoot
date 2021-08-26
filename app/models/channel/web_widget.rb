9632e55462225fd9c2f753c5a23ab4c518bcc09b
Fayaz Ahmed <fayazara@gmail.com>
2021-08-26 15:42:12 +0530

    feat: Adds async and defer attributes to chatwoot script tag

--- app/models/channel/web_widget.rb
+++ app/models/channel/web_widget.rb
@@ -59,6 +59,8 @@ class Channel::WebWidget < ApplicationRecord
         var BASE_URL=\"#{ENV.fetch('FRONTEND_URL', '')}\";
         var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
         g.src=BASE_URL+\"/packs/js/sdk.js\";
+        g.defer = true;
+        g.async = true;
         s.parentNode.insertBefore(g,s);
         g.onload=function(){
           window.chatwootSDK.run({
