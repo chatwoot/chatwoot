c763adda6e16ba1b4e301541c96cf5b138524a9e
Sojan Jose <sojan@pepalo.com>
2021-08-31 13:17:00 +0530

    chore: Strip unneccesary data from push payload

--- app/services/notification/push_notification_service.rb
+++ app/services/notification/push_notification_service.rb
@@ -96,7 +96,7 @@ class Notification::PushNotificationService
         title: notification.notification_type.titleize,
         body: notification.push_message_title
       },
-      data: { notification: notification.push_event_data.to_json },
+      data: { notification: notification.fcm_push_data.to_json },
       collapse_key: "chatwoot_#{notification.primary_actor_type.downcase}_#{notification.primary_actor_id}"
     }
   end
