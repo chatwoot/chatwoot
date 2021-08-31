d47dd935fc3721293f18fa8b9f82c7027070be14
Muhsin Keloth <muhsinkeramam@gmail.com>
2021-08-31 19:45:11 +0530

    fix: Handle more rest client exceptions (#2930)

--- lib/exception_list.rb
+++ lib/exception_list.rb
@@ -2,7 +2,7 @@ module ExceptionList
   REST_CLIENT_EXCEPTIONS = [RestClient::NotFound, RestClient::GatewayTimeout, RestClient::BadRequest,
                             RestClient::MethodNotAllowed, RestClient::Forbidden, RestClient::InternalServerError,
                             RestClient::Exceptions::OpenTimeout, RestClient::Exceptions::ReadTimeout,
-                            RestClient::MovedPermanently, SocketError].freeze
+                            RestClient::MovedPermanently, RestClient::ServiceUnavailable, Errno::ECONNREFUSED, SocketError].freeze
   SMTP_EXCEPTIONS = [
     Net::SMTPSyntaxError
   ].freeze
