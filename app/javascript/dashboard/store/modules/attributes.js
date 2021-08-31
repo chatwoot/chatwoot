9f6f044a430900c873bc7eee59fa29fd21db1d55
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2021-08-31 13:54:34 +0530

    feat: Add the ability to create custom attribute (#2903)

---
+++ app/javascript/dashboard/store/modules/attributes.js
@@ -0,0 +1,91 @@
+import * as MutationHelpers from 'shared/helpers/vuex/mutationHelpers';
+import types from '../mutation-types';
+import AttributeAPI from '../../api/attributes';
+
+export const state = {
+  records: [],
+  uiFlags: {
+    isFetching: false,
+    isCreating: false,
+  },
+};
+
+export const getters = {
+  getUIFlags(_state) {
+    return _state.uiFlags;
+  },
+  getAttributes: _state => attributeType => {
+    return _state.records.filter(
+      record => record.attribute_display_type === attributeType
+    );
+  },
+};
+
+export const actions = {
+  get: async function getAttributes({ commit }) {
+    commit(types.SET_CUSTOM_ATTRIBUTE_UI_FLAG, { isFetching: true });
+    try {
+      const response = await AttributeAPI.get();
+      commit(types.SET_CUSTOM_ATTRIBUTE, response.data);
+    } catch (error) {
+      // Ignore error
+    } finally {
+      commit(types.SET_CUSTOM_ATTRIBUTE_UI_FLAG, { isFetching: false });
+    }
+  },
+  create: async function createAttribute({ commit }, attributeObj) {
+    commit(types.SET_CUSTOM_ATTRIBUTE_UI_FLAG, { isCreating: true });
+    try {
+      const response = await AttributeAPI.create(attributeObj);
+      commit(types.ADD_CUSTOM_ATTRIBUTE, response.data);
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      commit(types.SET_CUSTOM_ATTRIBUTE_UI_FLAG, { isCreating: false });
+    }
+  },
+  update: async ({ commit }, { id, ...updateObj }) => {
+    commit(types.SET_CUSTOM_ATTRIBUTE_UI_FLAG, { isUpdating: true });
+    try {
+      const response = await AttributeAPI.update(id, updateObj);
+      commit(types.EDIT_CUSTOM_ATTRIBUTE, response.data);
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      commit(types.SET_CUSTOM_ATTRIBUTE_UI_FLAG, { isUpdating: false });
+    }
+  },
+  delete: async ({ commit }, id) => {
+    commit(types.SET_CUSTOM_ATTRIBUTE_UI_FLAG, { isDeleting: true });
+    try {
+      await AttributeAPI.delete(id);
+      commit(types.DELETE_CUSTOM_ATTRIBUTE, id);
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      commit(types.SET_CUSTOM_ATTRIBUTE_UI_FLAG, { isDeleting: false });
+    }
+  },
+};
+
+export const mutations = {
+  [types.SET_CUSTOM_ATTRIBUTE_UI_FLAG](_state, data) {
+    _state.uiFlags = {
+      ..._state.uiFlags,
+      ...data,
+    };
+  },
+
+  [types.ADD_CUSTOM_ATTRIBUTE]: MutationHelpers.create,
+  [types.SET_CUSTOM_ATTRIBUTE]: MutationHelpers.set,
+  [types.EDIT_CUSTOM_ATTRIBUTE]: MutationHelpers.update,
+  [types.DELETE_CUSTOM_ATTRIBUTE]: MutationHelpers.destroy,
+};
+
+export default {
+  namespaced: true,
+  actions,
+  state,
+  getters,
+  mutations,
+};
