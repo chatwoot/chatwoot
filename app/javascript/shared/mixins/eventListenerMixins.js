b31cb0a548edfdbe8a6ba5a57a8fca0b65cf462c
Pranav Raj S <pranav@chatwoot.com>
2021-09-08 17:27:05 +0530

    fix: Reject keyboard shortcut listeners if input box is active

--- app/javascript/shared/mixins/eventListenerMixins.js
+++ app/javascript/shared/mixins/eventListenerMixins.js
@@ -1,6 +1,22 @@
+import { isEscape } from '../helpers/KeyboardHelpers';
+
 export default {
   mounted() {
-    document.addEventListener('keydown', this.handleKeyEvents);
+    document.addEventListener('keydown', e => {
+      const isEventFromAnInputBox = e.target?.tagName === 'INPUT';
+      const isEventFromProseMirror = e.target?.className?.includes(
+        'ProseMirror'
+      );
+
+      if (isEventFromAnInputBox || isEventFromProseMirror) {
+        if (isEscape(e)) {
+          e.target.blur();
+        }
+        return;
+      }
+
+      this.handleKeyEvents(e);
+    });
   },
   destroyed() {
     document.removeEventListener('keydown', this.handleKeyEvents);
