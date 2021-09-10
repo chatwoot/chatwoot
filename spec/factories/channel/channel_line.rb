b1d5c3ff77fe2a9415da8935e04fd6cf846e3d0a
Sojan <sojan@pepalo.com>
2021-09-10 19:51:52 +0530

    chore: fix

--- spec/factories/channel/channel_line.rb
+++ spec/factories/channel/channel_line.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :channel_facebook_page, class: 'Channel::Line' do
+  factory :channel_line, class: 'Channel::Line' do
     line_channel_id { SecureRandom.uuid }
     line_channel_secret { SecureRandom.uuid }
     line_channel_token { SecureRandom.uuid }
