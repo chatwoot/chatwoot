a8520eff57374a22a66a777a54fe212de35b39ec
Sojan <sojan@pepalo.com>
2021-09-15 00:58:57 +0530

    chore: Sidebar icons for new inboxes

--- app/javascript/dashboard/helper/inbox.js
+++ app/javascript/dashboard/helper/inbox.js
@@ -21,8 +21,11 @@ export const getInboxClassByType = (type, phoneNumber) => {
 
     case INBOX_TYPES.EMAIL:
       return 'ion-ios-email';
+    
+    case INBOX_TYPES.TELEGRAM:
+      return 'ion-ios-navigate';
 
     default:
-      return '';
+      return 'ion-ios-chatbubble';
   }
 };
