c230df265269dc56501530aa848857bb3b5bd862
Nithin David <1277421+nithindavid@users.noreply.github.com>
2021-09-15 12:51:57 +0530

    feat: Adds public API endpoints for widget

--- app/javascript/widget/api/contacts.js
+++ app/javascript/widget/api/contacts.js
@@ -3,15 +3,27 @@ import { API } from 'widget/helpers/axios';
 const buildUrl = endPoint => `/api/v1/${endPoint}${window.location.search}`;
 
 export default {
-  get() {
-    return API.get(buildUrl('widget/contact'));
-  },
-  update(identifier, userObject) {
-    return API.patch(buildUrl('widget/contact'), {
-      identifier,
+  create(inboxIdentifier, userObject) {
+    return API.post(buildUrl(`inboxes/${inboxIdentifier}/contacts`), {
       ...userObject,
     });
   },
+
+  get(inboxIdentifier, contactIdentifier) {
+    return API.get(
+      buildUrl(`inboxes/${inboxIdentifier}/contacts/${contactIdentifier}`)
+    );
+  },
+
+  update(inboxIdentifier, contactIdentifier, userObject) {
+    return API.patch(
+      buildUrl(`inboxes/${inboxIdentifier}/contacts/${contactIdentifier}`),
+      {
+        ...userObject,
+      }
+    );
+  },
+
   setCustomAttibutes(customAttributes = {}) {
     return API.patch(buildUrl('widget/contact'), {
       custom_attributes: customAttributes,
---
+++ app/javascript/widget/api/conversationPublic.js
@@ -0,0 +1,35 @@
+import { API } from 'widget/helpers/axios';
+
+const buildUrl = endPoint => `/api/v1/${endPoint}${window.location.search}`;
+
+/*
+ *  Refer: https://www.chatwoot.com/developers/api#tag/Conversations-API
+ */
+
+export default {
+  create(inboxIdentifier, contactIdentifier) {
+    return API.post(
+      buildUrl(
+        `inboxes/${inboxIdentifier}/contacts/${contactIdentifier}/conversations`
+      )
+    );
+  },
+
+  get(inboxIdentifier, contactIdentifier) {
+    return API.get(
+      buildUrl(
+        `inboxes/${inboxIdentifier}/contacts/${contactIdentifier}/conversations`
+      )
+    );
+  },
+
+  // https://www.chatwoot.com/developers/api#operation/update-a-contact
+  update(inboxIdentifier, contactIdentifier, userObject) {
+    return API.patch(
+      buildUrl(`inboxes/${inboxIdentifier}/contacts/${contactIdentifier}`),
+      {
+        ...userObject,
+      }
+    );
+  },
+};
