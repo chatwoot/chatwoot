db1d2d35beeb02198098fe40898dde0b4e6a70a4
murtaza-bagwala <bagwalamurtaza@gmail.com>
2021-09-21 19:23:53 +0530

    disable rubocop check for the migration

--- db/migrate/20210921121815_set_content_type_text_for_the_old_messages.rb
+++ db/migrate/20210921121815_set_content_type_text_for_the_old_messages.rb
@@ -1,5 +1,7 @@
 class SetContentTypeTextForTheOldMessages < ActiveRecord::Migration[6.1]
   def change
+    # rubocop:disable Rails/SkipsModelValidations
     Message.where(content_type: nil).update_all(content_type: 0)
+    # rubocop:enable Rails/SkipsModelValidations
   end
 end
