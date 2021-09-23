05fc0e6ea4731096e0a5db40cb211a48babe6d85
Tejaswini <tejaswini@chatwoot.com>
2021-09-23 20:33:01 +0530

    feat: Fix use in reply

--- app/mailboxes/reply_mailbox.rb
+++ app/mailboxes/reply_mailbox.rb
@@ -6,10 +6,11 @@ class ReplyMailbox < ApplicationMailbox
   # Last part is the regex for the UUID
   # Eg: email should be something like : reply+6bdc3f4d-0bec-4515-a284-5d916fdde489@domain.com
   EMAIL_PART_PATTERN = /^reply\+([0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12})$/i
+  CONVERSATION_UUID_PATTERN = %r{^<account/(\d+?)/conversation/([a-zA-Z0-9\-]*?)@(\w+\.\w+)>$}
 
   before_processing :conversation_uuid_from_to_address,
+                    :find_relative_conversation,
                     :verify_decoded_params,
-                    :find_conversation,
                     :decorate_mail
 
   def process
@@ -18,6 +19,16 @@ class ReplyMailbox < ApplicationMailbox
   end
 
   private
+
+  def find_relative_conversation
+    if @conversation_uuid
+      find_conversation_with_uuid
+    elsif recipient_email_channel_conversations.present?
+      find_conversation_with_recipient
+    else
+      find_conversation_with_id
+    end
+  end
 
   def conversation_uuid_from_to_address
     mail.to.each do |email|
@@ -35,9 +46,27 @@ class ReplyMailbox < ApplicationMailbox
     raise 'Conversation uuid not found' if conversation_uuid.nil?
   end
 
-  def find_conversation
+  def find_conversation_with_uuid
     @conversation = Conversation.find_by(uuid: conversation_uuid)
     validate_resource @conversation
+  end
+
+  def find_conversation_with_recipient
+    @conversation = @conversations.last
+    @conversation_uuid = @conversation.uuid
+    validate_resource @conversation
+  end
+
+  def find_conversation_with_id
+    in_reply_to_email = mail['In-Reply-To'].value
+    match_result = in_reply_to_email.match(CONVERSATION_UUID_PATTERN)
+    return unless match_result
+
+    @account = Account.find_by(id: match_result.captures[0])
+    return unless @account.inbound_email_domain == match_result.captures[2]
+
+    @conversation_uuid = match_result.captures[1]
+    find_conversation_with_uuid
   end
 
   def validate_resource(resource)
@@ -48,5 +77,29 @@ class ReplyMailbox < ApplicationMailbox
 
   def decorate_mail
     @processed_mail = MailPresenter.new(mail, @conversation.account)
+  end
+
+  def conversation_with_recipient
+    @conversation = Conversation.find_by(uuid: conversation_uuid)
+  end
+
+  def recipient_email_channel_conversations
+    @conversations = []
+    mail.to.each do |email|
+      inbox = Channel::Email.find_by(email: email).try(:inbox)
+      @conversations = collect_conversations(inbox)
+
+      break if @conversations.any?
+    end
+    @conversations
+  end
+
+  def collect_conversations(inbox)
+    return if inbox.nil?
+
+    inbox.conversations.joins(:contact).where(
+      "conversations.additional_attributes ->> 'mail_subject' = ? AND contacts.email IN (?)",
+      mail.subject, mail.from
+    )
   end
 end
