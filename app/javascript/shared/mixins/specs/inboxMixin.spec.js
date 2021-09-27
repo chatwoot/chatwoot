1a36d290778fa44bb90c6e4232708af6bc85bce6
iamsivin <iamsivin@gmail.com>
2021-09-27 13:40:18 +0530

    Review fixes

--- app/javascript/shared/mixins/specs/inboxMixin.spec.js
+++ app/javascript/shared/mixins/specs/inboxMixin.spec.js
@@ -127,4 +127,78 @@ describe('inboxMixin', () => {
     const wrapper = shallowMount(Component);
     expect(wrapper.vm.isAnEmailChannel).toBe(true);
   });
+
+  it('isTwitterInboxTweet returns true if Twitter channel type is tweet', () => {
+    const Component = {
+      render() {},
+      mixins: [inboxMixin],
+      data() {
+        return {
+          chat: {
+            channel_type: 'Channel::TwitterProfile',
+            additional_attributes: {
+              type: 'tweet',
+            },
+          },
+        };
+      },
+    };
+    const wrapper = shallowMount(Component);
+    expect(wrapper.vm.isTwitterInboxTweet).toBe(true);
+  });
+
+  it('twilioBadge returns string sms if channel type is Twilio and medium is sms', () => {
+    const Component = {
+      render() {},
+      mixins: [inboxMixin],
+      data() {
+        return {
+          inbox: {
+            channel_type: 'Channel::TwilioSms',
+            medium: 'sms',
+          },
+        };
+      },
+    };
+    const wrapper = shallowMount(Component);
+    expect(wrapper.vm.isATwilioSMSChannel).toBe(true);
+    expect(wrapper.vm.twilioBadge).toBe('sms');
+  });
+
+  it('twitterBadge returns string twitter-tweet if Twitter channel type is tweet', () => {
+    const Component = {
+      render() {},
+      mixins: [inboxMixin],
+      data() {
+        return {
+          chat: {
+            id: 1,
+            additional_attributes: {
+              type: 'tweet',
+            },
+          },
+        };
+      },
+    };
+    const wrapper = shallowMount(Component);
+    expect(wrapper.vm.twitterBadge).toBe('twitter-tweet');
+  });
+
+  it('inboxBadge returns string Channel::Telegram if isATwilioChannel and isATwitterInbox is false', () => {
+    const Component = {
+      render() {},
+      mixins: [inboxMixin],
+      data() {
+        return {
+          inbox: {
+            channel_type: 'Channel::Telegram',
+          },
+        };
+      },
+    };
+    const wrapper = shallowMount(Component);
+    expect(wrapper.vm.isATwilioChannel).toBe(false);
+    expect(wrapper.vm.isATwitterInbox).toBe(false);
+    expect(wrapper.vm.channelType).toBe('Channel::Telegram');
+  });
 });
