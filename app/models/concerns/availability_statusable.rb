41eea04a9022f53f9dec4f013418d4afca74f61f
Sojan <sojan@pepalo.com>
2021-09-27 17:53:22 +0530

    chore: fixes

--- app/models/concerns/availability_statusable.rb
+++ app/models/concerns/availability_statusable.rb
@@ -7,10 +7,23 @@ module AvailabilityStatusable
   end
 
   def availability_status
-    return availability  if self.is_a?(AccountUser) && !auto_offline
-    return 'offline' unless online_presence?
-    return 'online' if is_a? Contact
+    if is_a? Contact
+      contact_availability_status
+    else
+      user_availability_status
+    end
+  end
 
-    ::OnlineStatusTracker.get_status(account_id, user_id) || availability
+  private
+  
+  def contact_availability_status
+    online_presence? ? 'online' : 'offline'
+  end
+
+  def user_availability_status
+    # we are not considering presence in this case. Just returns the availability
+    return availability if !auto_offline
+    # availability as a fallback in case the status is not present in redis
+    online_presence?  ? (::OnlineStatusTracker.get_status(account_id, user_id) || availability) : 'offline'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_09_23_190418) do
 
   # These are extensions that must be enabled in order to support this database
