651d71ec130d6c354413693b72298c8d9fe6f6af
Pranav Raj S <pranav@chatwoot.com>
2021-09-30 12:13:13 +0530

    Fix specs - add restoreAllMocks

--- app/javascript/dashboard/store/modules/specs/reports/actions.spec.js
+++ app/javascript/dashboard/store/modules/specs/reports/actions.spec.js
@@ -5,22 +5,29 @@ global.open = jest.fn();
 global.axios = axios;
 jest.mock('axios');
 
+const createElementSpy = () => {
+  const element = document.createElement('a');
+  jest.spyOn(document, 'createElement').mockImplementation(() => element);
+  return element;
+};
+
 describe('#actions', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   describe('#downloadAgentReports', () => {
     it('open CSV download prompt if API is success', async () => {
       axios.get.mockResolvedValue({
         data: `Agent name,Conversations count,Avg first response time (Minutes),Avg resolution time (Minutes)
-          Pranav,36,114,28411`,
+        Pranav,36,114,28411`,
       });
       const param = {
         from: 1630504922510,
         to: 1630504922510,
         fileName: 'agent-report-01-09-2021.csv',
       };
-      const mockAgentDownloadElement = document.createElement('a');
-      jest
-        .spyOn(document, 'createElement')
-        .mockImplementation(() => mockAgentDownloadElement);
+      const mockAgentDownloadElement = createElementSpy();
       await actions.downloadAgentReports(1, param);
       expect(mockAgentDownloadElement.href).toEqual(
         'data:text/csv;charset=utf-8,Agent%20name,Conversations%20count,Avg%20first%20response%20time%20(Minutes),Avg%20resolution%20time%20(Minutes)%0A%20%20%20%20%20%20%20%20Pranav,36,114,28411'
@@ -41,10 +48,7 @@ describe('#actions', () => {
         type: 'label',
         fileName: 'label-report-01-09-2021.csv',
       };
-      const mockLabelDownloadElement = document.createElement('a');
-      jest
-        .spyOn(document, 'createElement')
-        .mockImplementation(() => mockLabelDownloadElement);
+      const mockLabelDownloadElement = createElementSpy();
       await actions.downloadLabelReports(1, param);
       expect(mockLabelDownloadElement.href).toEqual(
         'data:text/csv;charset=utf-8,Label%20Title,Conversations%20count,Avg%20first%20response%20time%20(Minutes),Avg%20resolution%20time%20(Minutes)%0A%20%20%20%20%20%20%20%20website,0,0,0'
@@ -66,10 +70,7 @@ describe('#actions', () => {
         to: 1635013800,
         fileName: 'inbox-report-24-10-2021.csv',
       };
-      const mockInboxDownloadElement = document.createElement('a');
-      jest
-        .spyOn(document, 'createElement')
-        .mockImplementation(() => mockInboxDownloadElement);
+      const mockInboxDownloadElement = createElementSpy();
       await actions.downloadInboxReports(1, param);
       expect(mockInboxDownloadElement.href).toEqual(
         'data:text/csv;charset=utf-8,Inbox%20name,Conversations%20count,Avg%20first%20response%20time%20(Minutes),Avg%20resolution%20time%20(Minutes)%0A%20%20%20%20%20%20%20%20Fayaz,2,127,0%0A%20%20%20%20%20%20%20%20EMa,0,0,0%0A%20%20%20%20%20%20%20%20Twillio%20WA,0,0,0'
