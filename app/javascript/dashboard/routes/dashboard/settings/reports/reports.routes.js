1e1f48ed2fd4579910b95b163fda12184949032b
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2021-09-30 13:13:45 +0530

    feat - Add filter for reports by agent, label and inboxes (#3084)
    * Adds filter for agents, labels and inboxes
    
    * Added Inboxes Reports Feature
    
    * Fixed populating of filter dropdown issue
    
    * If applied, fixes code climate style-lint warnings
    
    * Fixes codeclimate warnings
    
    * if applied, Refactors sidebar file to fix codclimate warnings
    
    * if applied, fixes the download reports button for filtered report-data
    
    * If applied, replaces native img tag with thumbnail component
    
    * If applied, replaces hardcoded color string with variable
    
    * If applied, adds a11y labels to multiselect dropdowns
    
    * If applied, Renames reports methods to generic names
    
    * If applied, Adds test cases for Labels and Inboxes
    
    * If applied, write a test spec for fileDownload helper
    
    * if applied, Moves fileDownload method to a utils folder
    
    * If applied, Fixes the report file name type
    
    * Test Spec for Reports Store module
    
    * Fix specs - add restoreAllMocks
    
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>

--- app/javascript/dashboard/routes/dashboard/settings/reports/reports.routes.js
+++ app/javascript/dashboard/routes/dashboard/settings/reports/reports.routes.js
@@ -1,4 +1,7 @@
 import Index from './Index';
+import AgentReports from './AgentReports';
+import LabelReports from './LabelReports';
+import InboxReports from './InboxReports';
 import CsatResponses from './CsatResponses';
 import SettingsContent from '../Wrapper';
 import { frontendURL } from '../../../../helper/URLHelper';
@@ -38,6 +41,54 @@ export default {
           name: 'csat_reports',
           roles: ['administrator'],
           component: CsatResponses,
+        },
+      ],
+    },
+    {
+      path: frontendURL('accounts/:accountId/reports'),
+      component: SettingsContent,
+      props: {
+        headerTitle: 'AGENT_REPORTS.HEADER',
+        icon: 'ion-people',
+      },
+      children: [
+        {
+          path: 'agent',
+          name: 'agent_reports',
+          roles: ['administrator'],
+          component: AgentReports,
+        },
+      ],
+    },
+    {
+      path: frontendURL('accounts/:accountId/reports'),
+      component: SettingsContent,
+      props: {
+        headerTitle: 'LABEL_REPORTS.HEADER',
+        icon: 'ion-pricetags',
+      },
+      children: [
+        {
+          path: 'label',
+          name: 'label_reports',
+          roles: ['administrator'],
+          component: LabelReports,
+        },
+      ],
+    },
+    {
+      path: frontendURL('accounts/:accountId/reports'),
+      component: SettingsContent,
+      props: {
+        headerTitle: 'INBOX_REPORTS.HEADER',
+        icon: 'ion-archive',
+      },
+      children: [
+        {
+          path: 'inboxes',
+          name: 'inbox_reports',
+          roles: ['administrator'],
+          component: InboxReports,
         },
       ],
     },
