474f348b2d59094c817ab57ee7a53e78a1ad1b71
Tejaswini <tejaswini@chatwoot.com>
2021-10-11 17:42:33 +0530

    feat: Conversation and contact endpoint > Added the contact policy > Added the routes and jbuilder and specs

--- app/controllers/api/v1/accounts/conversations_controller.rb
+++ app/controllers/api/v1/accounts/conversations_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Accounts::ConversationsController < Api::V1::Accounts::BaseContro
   include Events::Types
   include DateRangeHelper
 
-  before_action :conversation, except: [:index, :meta, :search, :create]
+  before_action :conversation, except: [:index, :meta, :search, :create, :filter]
   before_action :contact_inbox, only: [:create]
 
   def index
@@ -30,6 +30,10 @@ class Api::V1::Accounts::ConversationsController < Api::V1::Accounts::BaseContro
   end
 
   def show; end
+
+  def filter
+    @conversations = Current.account.conversations.limit(10)
+  end
 
   def mute
     @conversation.mute!
