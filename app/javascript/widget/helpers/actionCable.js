726a865a1fb968b25dcdb7fd2dd74a141dcf2750
iamsivin <iamsivin@gmail.com>
2021-10-11 11:35:16 +0530

    feat: Adds sound alert notification for new messages on the widget

--- app/javascript/widget/helpers/actionCable.js
+++ app/javascript/widget/helpers/actionCable.js
@@ -1,4 +1,5 @@
 import BaseActionCableConnector from '../../shared/helpers/BaseActionCableConnector';
+import { newMessageNotification } from './widgetAudioNotificationHelper';
 
 class ActionCableConnector extends BaseActionCableConnector {
   constructor(app, pubsubToken) {
@@ -36,6 +37,9 @@ class ActionCableConnector extends BaseActionCableConnector {
       .then(() => {
         window.bus.$emit('on-agent-message-recieved');
       });
+    if (data.sender_type === 'User') {
+      newMessageNotification();
+    }
   };
 
   onMessageUpdated = data => {
