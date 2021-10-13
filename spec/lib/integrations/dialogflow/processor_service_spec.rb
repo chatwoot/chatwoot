10cb0b7c33363cce7503db00e540f4f7e3ecf07b
Sojan <sojan@pepalo.com>
2021-10-13 19:24:54 +0530

    chore: rubocop

--- spec/lib/integrations/dialogflow/processor_service_spec.rb
+++ spec/lib/integrations/dialogflow/processor_service_spec.rb
@@ -91,11 +91,14 @@ describe Integrations::Dialogflow::ProcessorService do
     end
 
     context 'when message updated' do
-      let(:message) { create(:message, account: account, conversation: conversation, private: true, submitted_values: [{"title"=>"Support", "value"=>"selected_gas"}]) }
+      let(:message) do
+        create(:message, account: account, conversation: conversation, private: true,
+                         submitted_values: [{ 'title' => 'Support', 'value' => 'selected_gas' }])
+      end
       let(:event_name) { 'message.updated' }
 
       it 'returns submitted value for message content' do
-         expect(processor.send(:message_content, message)).to eql('selected_gas')
+        expect(processor.send(:message_content, message)).to eql('selected_gas')
       end
     end
   end
