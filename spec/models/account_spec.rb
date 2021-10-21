f2129fd2eee710c4cebd2c4b9604c2bd75423670
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2021-10-21 23:42:57 +0530

    Rubocop issues fixed

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -10,15 +10,15 @@ RSpec.describe Account do
   it { is_expected.to have_many(:account_users) }
   it { is_expected.to have_many(:inboxes).dependent(:destroy_async) }
   it { is_expected.to have_many(:conversations).dependent(:destroy_async) }
-  it { is_expected.to have_many(:contacts).dependent(:destroy_async)  }
-  it { is_expected.to have_many(:telegram_bots).dependent(:destroy_async)  }
-  it { is_expected.to have_many(:canned_responses).dependent(:destroy_async)  }
-  it { is_expected.to have_many(:facebook_pages).class_name('::Channel::FacebookPage').dependent(:destroy_async)  }
-  it { is_expected.to have_many(:web_widgets).class_name('::Channel::WebWidget').dependent(:destroy_async)  }
-  it { is_expected.to have_many(:webhooks).dependent(:destroy_async)  }
-  it { is_expected.to have_many(:notification_settings).dependent(:destroy_async)  }
+  it { is_expected.to have_many(:contacts).dependent(:destroy_async) }
+  it { is_expected.to have_many(:telegram_bots).dependent(:destroy_async) }
+  it { is_expected.to have_many(:canned_responses).dependent(:destroy_async) }
+  it { is_expected.to have_many(:facebook_pages).class_name('::Channel::FacebookPage').dependent(:destroy_async) }
+  it { is_expected.to have_many(:web_widgets).class_name('::Channel::WebWidget').dependent(:destroy_async) }
+  it { is_expected.to have_many(:webhooks).dependent(:destroy_async) }
+  it { is_expected.to have_many(:notification_settings).dependent(:destroy_async) }
   it { is_expected.to have_many(:events) }
-  it { is_expected.to have_many(:kbase_portals).dependent(:destroy_async)  }
-  it { is_expected.to have_many(:kbase_categories).dependent(:destroy_async)  }
-  it { is_expected.to have_many(:teams).dependent(:destroy_async)  }
+  it { is_expected.to have_many(:kbase_portals).dependent(:destroy_async) }
+  it { is_expected.to have_many(:kbase_categories).dependent(:destroy_async) }
+  it { is_expected.to have_many(:teams).dependent(:destroy_async) }
 end
