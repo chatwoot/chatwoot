45a1c97f3b15e9a1674364af2f3202a10cee7637
Pranav Raj S <pranav@chatwoot.com>
2021-10-25 18:35:58 +0530

    feat: Add notes for Contacts (#3273)
    Fixes #2275

--- app/javascript/dashboard/api/contactNotes.js
+++ app/javascript/dashboard/api/contactNotes.js
@@ -2,7 +2,27 @@ import ApiClient from './ApiClient';
 
 class ContactNotes extends ApiClient {
   constructor() {
-    super('contact_notes', { accountScoped: true });
+    super('notes', { accountScoped: true });
+    this.contactId = null;
+  }
+
+  get url() {
+    return `${this.baseUrl()}/contacts/${this.contactId}/notes`;
+  }
+
+  get(contactId) {
+    this.contactId = contactId;
+    return super.get();
+  }
+
+  create(contactId, content) {
+    this.contactId = contactId;
+    return super.create({ content });
+  }
+
+  delete(contactId, id) {
+    this.contactId = contactId;
+    return super.delete(id);
   }
 }
 
