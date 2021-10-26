86911f59bf974837269040ea94a99ee460120dc9
Vishnu Narayanan <vishnu@chatwoot.com>
2021-10-26 19:41:54 +0530

    fix rubocop errors

--- lib/global_config_service.rb
+++ lib/global_config_service.rb
@@ -1,5 +1,4 @@
 class GlobalConfigService
-  
   def self.load(config_key, default_value)
     config = GlobalConfig.get(config_key)
     return config[config_key] if config[config_key].present?
@@ -11,5 +10,4 @@ class GlobalConfigService
     i = InstallationConfig.where(name: config_key).first_or_create(value: config_value)
     i.value
   end
-
 end
