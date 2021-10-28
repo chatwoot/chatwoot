0b8eeeb9f64cff6233ff85f0ed648f8668477eb7
Vishnu Narayanan <vishnu@chatwoot.com>
2021-10-28 18:25:25 +0530

    add spec for global_config_service

--- spec/lib/global_config_service_spec.rb
+++ spec/lib/global_config_service_spec.rb
@@ -9,16 +9,25 @@ describe GlobalConfigService do
         GlobalConfig.clear_cache
       end
 
-      it 'get value from DB if found' do
-        pending 'add some checks'
+      it 'set default value if not found on db nor env var' do
+        described_class.load('ENABLE_ACCOUNT_SIGNUP', 'true')
+        value = GlobalConfig.get('ENABLE_ACCOUNT_SIGNUP')
+        expect(value).to be 'true'
       end
 
       it 'get value from env variable if not found on DB' do
-        pending 'add some checks'
+        with_modified_env ENABLE_ACCOUNT_SIGNUP: 'false' do
+          GlobalConfig.clear_cache
+          described_class.load('ENABLE_ACCOUNT_SIGNUP', 'true')
+          expect(value).to be 'false'
+        end
       end
 
-      it 'set default value if not found on db nor env var' do
-        pending 'add some checks'
+      it 'get value from DB if found' do
+        # Not clearing the GlobalConfig and as such its value should
+        # be `false` in the DB from the test above
+        described_class.load('ENABLE_ACCOUNT_SIGNUP', 'true')
+        expect(value).to be 'false'
       end
     end
   end
