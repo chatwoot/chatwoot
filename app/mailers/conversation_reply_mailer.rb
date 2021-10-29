26df9661d368178d97f7457afe2f3bf083b9df4d
Aswin Dev P S <aswin@chatwoot.com>
2021-10-29 16:52:27 +0530

    Add helper module for conversation reply mailer.
    To reduce code duplication and method complexity.

--- app/mailers/conversation_reply_mailer.rb
+++ app/mailers/conversation_reply_mailer.rb
@@ -1,4 +1,5 @@
 class ConversationReplyMailer < ApplicationMailer
+  include ConversationReplyMailerHelper
   default from: ENV.fetch('MAILER_SENDER_EMAIL', 'Chatwoot <accounts@chatwoot.com>')
   layout :choose_layout
 
@@ -13,33 +14,7 @@ class ConversationReplyMailer < ApplicationMailer
     @messages = recap_messages + new_messages
     @messages = @messages.select(&:email_reply_summarizable?)
 
-    data = {
-      to: @contact&.email,
-      from: email_imap_enabled ? @channel.imap_email : from_email_with_name,
-      reply_to: email_imap_enabled ? @channel.imap_email : reply_email,
-      subject: mail_subject,
-      message_id: email_imap_enabled ? @conversation.additional_attributes['message_id'] : custom_message_id,
-      in_reply_to: in_reply_to_email,
-      cc: cc_bcc_emails[0],
-      bcc: cc_bcc_emails[1]
-    }
-
-    if @inbox.inbox_type == 'Email' && @channel.smtp_enabled
-      smtp_settings = {
-        address: @channel.smtp_address,
-        port: @channel.smtp_port,
-        user_name: @channel.smtp_email,
-        password: @channel.smtp_password,
-        domain: @channel.smtp_domain,
-        enable_starttls_auto: @channel.smtp_enable_starttls_auto,
-        authentication: @channel.smtp_authentication
-      }
-
-      data[:delivery_method] = :smtp
-      data[:delivery_method_options] = smtp_settings
-    end
-
-    mail(data)
+    mail(prepare_mail)
   end
 
   def reply_without_summary(conversation, message_queued_time)
@@ -52,33 +27,7 @@ class ConversationReplyMailer < ApplicationMailer
     @messages = @messages.reject { |m| m.template? && !m.input_csat? }
     return false if @messages.count.zero?
 
-    data = {
-      to: @contact&.email,
-      from: email_imap_enabled ? @channel.imap_email : from_email_with_name,
-      reply_to: email_imap_enabled ? @channel.imap_email : reply_email,
-      subject: mail_subject,
-      message_id: email_imap_enabled ? @conversation.additional_attributes['message_id'] : custom_message_id,
-      in_reply_to: in_reply_to_email,
-      cc: cc_bcc_emails[0],
-      bcc: cc_bcc_emails[1]
-    }
-
-    if @inbox.inbox_type == 'Email' && @channel.smtp_enabled
-      smtp_settings = {
-        address: @channel.smtp_address,
-        port: @channel.smtp_port,
-        user_name: @channel.smtp_email,
-        password: @channel.smtp_password,
-        domain: @channel.smtp_domain,
-        enable_starttls_auto: @channel.smtp_enable_starttls_auto,
-        authentication: @channel.smtp_authentication
-      }
-
-      data[:delivery_method] = :smtp
-      data[:delivery_method_options] = smtp_settings
-    end
-
-    mail(data)
+    mail(prepare_mail)
   end
 
   def conversation_transcript(conversation, to_email)
