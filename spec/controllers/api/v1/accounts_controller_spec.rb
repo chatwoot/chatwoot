ab568eacb911dd86aab08619eaa5388033a45d71
Vishnu Narayanan <vishnu@chatwoot.com>
2021-11-03 23:04:42 +0530

    feat: add GlobalConfigService to support env vars migration  (#3288)

--- spec/controllers/api/v1/accounts_controller_spec.rb
+++ spec/controllers/api/v1/accounts_controller_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe 'Accounts API', type: :request do
     let(:email) { Faker::Internet.email }
     let(:user_full_name) { Faker::Name.name_with_middle }
 
+    before do
+      # to clear redis cache
+      GlobalConfig.clear_cache
+    end
+
     context 'when posting to accounts with correct parameters' do
       let(:account_builder) { double }
       let(:account) { create(:account) }
@@ -15,62 +20,56 @@ RSpec.describe 'Accounts API', type: :request do
       end
 
       it 'calls account builder' do
-        with_modified_env ENABLE_ACCOUNT_SIGNUP: nil do
-          allow(account_builder).to receive(:perform).and_return([user, account])
+        GlobalConfigService.load('ENABLE_ACCOUNT_SIGNUP', 'nil')
+        allow(account_builder).to receive(:perform).and_return([user, account])
 
-          params = { account_name: 'test', email: email, user: nil, user_full_name: user_full_name, password: 'Password1!' }
+        params = { account_name: 'test', email: email, user: nil, user_full_name: user_full_name, password: 'Password1!' }
 
-          post api_v1_accounts_url,
-               params: params,
-               as: :json
+        post api_v1_accounts_url,
+             params: params,
+             as: :json
 
-          expect(AccountBuilder).to have_received(:new).with(params.except(:password).merge(user_password: params[:password]))
-          expect(account_builder).to have_received(:perform)
-          expect(response.headers.keys).to include('access-token', 'token-type', 'client', 'expiry', 'uid')
-        end
+        expect(AccountBuilder).to have_received(:new).with(params.except(:password).merge(user_password: params[:password]))
+        expect(account_builder).to have_received(:perform)
+        expect(response.headers.keys).to include('access-token', 'token-type', 'client', 'expiry', 'uid')
       end
 
       it 'renders error response on invalid params' do
-        with_modified_env ENABLE_ACCOUNT_SIGNUP: nil do
-          allow(account_builder).to receive(:perform).and_return(nil)
+        GlobalConfigService.load('ENABLE_ACCOUNT_SIGNUP', 'nil')
+        allow(account_builder).to receive(:perform).and_return(nil)
 
-          params = { account_name: nil, email: nil, user: nil, user_full_name: nil }
+        params = { account_name: nil, email: nil, user: nil, user_full_name: nil }
 
-          post api_v1_accounts_url,
-               params: params,
-               as: :json
+        post api_v1_accounts_url,
+             params: params,
+             as: :json
 
-          expect(AccountBuilder).to have_received(:new).with(params.merge(user_password: params[:password]))
-          expect(account_builder).to have_received(:perform)
-          expect(response).to have_http_status(:forbidden)
-          expect(response.body).to eq({ message: I18n.t('errors.signup.failed') }.to_json)
-        end
+        expect(AccountBuilder).to have_received(:new).with(params.merge(user_password: params[:password]))
+        expect(account_builder).to have_received(:perform)
+        expect(response).to have_http_status(:forbidden)
+        expect(response.body).to eq({ message: I18n.t('errors.signup.failed') }.to_json)
       end
     end
 
     context 'when ENABLE_ACCOUNT_SIGNUP env variable is set to false' do
       it 'responds 404 on requests' do
-        params = { account_name: 'test', email: email, user_full_name: user_full_name }
-        with_modified_env ENABLE_ACCOUNT_SIGNUP: 'false' do
-          post api_v1_accounts_url,
-               params: params,
-               as: :json
-
-          expect(response).to have_http_status(:not_found)
-        end
+        params = { account_name: 'test', email: email, user_full_name: user_full_name, password: 'Password1!' }
+        GlobalConfigService.load('ENABLE_ACCOUNT_SIGNUP', 'false')
+        post api_v1_accounts_url,
+             params: params,
+             as: :json
+        expect(response).to have_http_status(:not_found)
       end
     end
 
     context 'when ENABLE_ACCOUNT_SIGNUP env variable is set to api_only' do
       it 'does not respond 404 on requests' do
         params = { account_name: 'test', email: email, user_full_name: user_full_name, password: 'Password1!' }
-        with_modified_env ENABLE_ACCOUNT_SIGNUP: 'api_only' do
-          post api_v1_accounts_url,
-               params: params,
-               as: :json
-
-          expect(response).to have_http_status(:success)
-        end
+        GlobalConfigService.load('ENABLE_ACCOUNT_SIGNUP', 'api_only')
+        post api_v1_accounts_url,
+             params: params,
+             as: :json
+        expect(response).to have_http_status(:success)
       end
     end
   end
