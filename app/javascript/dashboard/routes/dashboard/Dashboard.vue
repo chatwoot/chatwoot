07c4308b42dc46e864344ca82dfd41757b204c8b
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2021-11-09 12:25:17 +0530

    If applied, adds shortcuts command bar feature

--- app/javascript/dashboard/routes/dashboard/Dashboard.vue
+++ app/javascript/dashboard/routes/dashboard/Dashboard.vue
@@ -3,16 +3,18 @@
     <sidebar :route="currentRoute" :class="sidebarClassName"></sidebar>
     <section class="app-content columns" :class="contentClassName">
       <router-view></router-view>
+      <commandbar />
     </section>
   </div>
 </template>
 
 <script>
 import Sidebar from '../../components/layout/Sidebar';
-
+import commandbar from './commands/commandbar.vue';
 export default {
   components: {
     Sidebar,
+    commandbar,
   },
   data() {
     return {
