58621b1221f64141abcee43cdc9f16c2491d96f4
Nithin David <1277421+nithindavid@users.noreply.github.com>
2021-11-09 16:47:20 +0530

    Removes old modules

--- app/javascript/widget/store/index.js
+++ app/javascript/widget/store/index.js
@@ -2,13 +2,10 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import agent from 'widget/store/modules/agent';
 import appConfig from 'widget/store/modules/appConfig';
-import contacts from 'widget/store/modules/contacts';
-import conversation from 'widget/store/modules/conversation';
-import conversationAttributes from 'widget/store/modules/conversationAttributes';
+
 import conversationLabels from 'widget/store/modules/conversationLabels';
 import events from 'widget/store/modules/events';
 import globalConfig from 'shared/store/globalConfig';
-import message from 'widget/store/modules/message';
 import campaign from 'widget/store/modules/campaign';
 
 // New store modules
@@ -21,13 +18,9 @@ export default new Vuex.Store({
   modules: {
     agent,
     appConfig,
-    contacts,
-    conversation,
-    conversationAttributes,
     conversationLabels,
     events,
     globalConfig,
-    message,
     campaign,
     contactV2,
     conversationV2,
