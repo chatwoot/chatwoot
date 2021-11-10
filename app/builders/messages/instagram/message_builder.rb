c0f5a59cf88b65e9a30c650158f0f80cc49706d3
Tejaswini Chile <tejaswini@chatwoot.com>
2021-11-10 18:32:46 +0530

    Bug: Instagram render template message twice (#3313)

--- app/builders/messages/instagram/message_builder.rb
+++ app/builders/messages/instagram/message_builder.rb
@@ -38,6 +38,10 @@ class Messages::Instagram::MessageBuilder < Messages::Messenger::MessageBuilder
     @outgoing_echo ? :outgoing : :incoming
   end
 
+  def message_identifier
+    message[:mid]
+  end
+
   def message_source_id
     @outgoing_echo ? recipient_id : sender_id
   end
@@ -64,10 +68,6 @@ class Messages::Instagram::MessageBuilder < Messages::Messenger::MessageBuilder
 
   def message_content
     @messaging[:message][:text]
-  end
-
-  def content_attributes
-    { message_id: @messaging[:message][:mid] }
   end
 
   def build_message
@@ -103,22 +103,17 @@ class Messages::Instagram::MessageBuilder < Messages::Messenger::MessageBuilder
       account_id: conversation.account_id,
       inbox_id: conversation.inbox_id,
       message_type: message_type,
-      source_id: message_source_id,
+      source_id: message_identifier,
       content: message_content,
-      content_attributes: content_attributes,
       sender: @outgoing_echo ? nil : contact
     }
   end
 
   def already_sent_from_chatwoot?
     cw_message = conversation.messages.where(
-      source_id: nil,
-      message_type: 'outgoing',
-      content: message_content,
-      private: false,
-      status: :sent
+      source_id: @messaging[:message][:mid]
     ).first
-    cw_message.update(content_attributes: content_attributes) if cw_message.present?
+
     cw_message.present?
   end
 
