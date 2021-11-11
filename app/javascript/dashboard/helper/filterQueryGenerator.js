c0bdb9bc249411969bcc42a10146e03adbd7a7de
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2021-11-11 22:29:43 +0530

    FIx codeclimate warnings

--- app/javascript/dashboard/helper/filterQueryGenerator.js
+++ app/javascript/dashboard/helper/filterQueryGenerator.js
@@ -1,11 +1,7 @@
 const generatePayload = data => {
   let payload = data.map(item => {
     if (Array.isArray(item.values)) {
-      if (!item.values.length) {
-        item.values = [];
-      } else {
-        item.values = item.values.map(val => val.id);
-      }
+      item.values = item.values.map(val => val.id);
     } else if (typeof item.values === 'object') {
       item.values = [item.values.id];
     } else if (!item.values) {
