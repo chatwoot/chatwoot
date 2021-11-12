0946873a957c027a9b2558079709f461f89d467c
Vishnu Narayanan <vishnu@chatwoot.com>
2021-11-12 19:48:37 +0530

    feat: migrate SMTP, active storage and remaining channels

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -37,7 +37,30 @@ class DashboardController < ActionController::Base
       SMTP_PASSWORD: GlobalConfigService.load('SMTP_PASSWORD',''),
       SMTP_AUTHENTICATION: GlobalConfigService.load('SMTP_AUTHENTICATION',''),
       SMTP_ENABLE_STARTTLS_AUTO: GlobalConfigService.load('SMTP_ENABLE_STARTTLS_AUTO','true'),
-      SMTP_OPENSSL_VERIFY_MODE: GlobalConfigService.load('SMTP_OPENSSL_VERIFY_MODE','peer')
+      SMTP_OPENSSL_VERIFY_MODE: GlobalConfigService.load('SMTP_OPENSSL_VERIFY_MODE','peer'),
+      MAILER_INBOUND_EMAIL_DOMAIN: GlobalConfigService.load('MAILER_INBOUND_EMAIL_DOMAIN',''),
+      RAILS_INBOUND_EMAIL_SERVICE: GlobalConfigService.load('RAILS_INBOUND_EMAIL_SERVICE',''),
+      RAILS_INBOUND_EMAIL_PASSWORD: GlobalConfigService.load('RAILS_INBOUND_EMAIL_PASSWORD',''),
+      MAILGUN_INGRESS_SIGNING_KEY: GlobalConfigService.load('MAILGUN_INGRESS_SIGNING_KEY',''),
+      MANDRILL_INGRESS_API_KEY: GlobalConfigService.load('MANDRILL_INGRESS_API_KEY',''),
+      ACTIVE_STORAGE_SERVICE: GlobalConfigService.load('ACTIVE_STORAGE_SERVICE','local'),
+      S3_BUCKET_NAME: GlobalConfigService.load('S3_BUCKET_NAME',''),
+      AWS_ACCESS_KEY_ID: GlobalConfigService.load('AWS_ACCESS_KEY_ID',''),
+      AWS_SECRET_ACCESS_KEY: GlobalConfigService.load('AWS_SECRET_ACCESS_KEY',''),
+      AWS_REGION: GlobalConfigService.load('AWS_REGION',''),
+      FB_VERIFY_TOKEN: GlobalConfigService.load('FB_VERIFY_TOKEN',''),
+      FB_APP_SECRET: GlobalConfigService.load('FB_APP_SECRET',''),
+      FB_APP_ID: GlobalConfigService.load('FB_APP_ID',''),
+      TWITTER_APP_ID: GlobalConfigService.load('TWITTER_APP_ID',''),
+      TWITTER_CONSUMER_KEY: GlobalConfigService.load('TWITTER_CONSUMER_KEY',''),
+      TWITTER_CONSUMER_SECRET: GlobalConfigService.load('TWITTER_CONSUMER_SECRET',''),
+      TWITTER_ENVIRONMENT: GlobalConfigService.load('TWITTER_ENVIRONMENT',''),
+      SLACK_CLIENT_ID: GlobalConfigService.load('SLACK_CLIENT_ID',''),
+      SLACK_CLIENT_SECRET: GlobalConfigService.load('SLACK_CLIENT_SECRET',''),
+      IOS_APP_ID: GlobalConfigService.load('IOS_APP_ID','6C953F3RX2.com.chatwoot.app'),
+      IOS_APP_IDENTIFIER: GlobalConfigService.load('IOS_APP_IDENTIFIER','1495796682'),
+      ANDROID_BUNDLE_ID: GlobalConfigService.load('ANDROID_BUNDLE_ID','com.chatwoot.app'),
+      ANDROID_SHA256_CERT_FINGERPRINT: GlobalConfigService.load('ANDROID_SHA256_CERT_FINGERPRINT','AC:73:8E:DE:EB:56:EA:CC:10:87:02:A7:65:37:7B:38:D4:5D:D4:53:F8:3B:FB:D3:C6:28:64:1D:AA:08:1E:D8')
     )
   end
 
