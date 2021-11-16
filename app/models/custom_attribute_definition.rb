dead8431bba6a9d4801e17120cee7c143a2ae9a1
Muhsin Keloth <muhsinkeramam@gmail.com>
2021-11-16 18:55:29 +0530

    Add list and checkbox in attribute_display_type

--- app/models/custom_attribute_definition.rb
+++ app/models/custom_attribute_definition.rb
@@ -31,7 +31,7 @@ class CustomAttributeDefinition < ApplicationRecord
   validates :attribute_model, presence: true
 
   enum attribute_model: { conversation_attribute: 0, contact_attribute: 1 }
-  enum attribute_display_type: { text: 0, number: 1, currency: 2, percent: 3, link: 4, date: 5 }
+  enum attribute_display_type: { text: 0, number: 1, currency: 2, percent: 3, link: 4, date: 5, list: 6, checkbox: 7}
 
   belongs_to :account
 end
