399a2ab06b7acad631c71b8804f2779125688b94
Pranav Raj S <pranav@chatwoot.com>
2021-11-22 15:05:29 +0530

    chore: Move referrerHost to appConfig store (#3433)

---
+++ app/javascript/widget/store/modules/specs/appConfig/actions.spec.js
@@ -0,0 +1,22 @@
+import { actions } from '../../appConfig';
+
+const commit = jest.fn();
+describe('#actions', () => {
+  describe('#setReferrerHost', () => {
+    it('creates actions properly', () => {
+      actions.setReferrerHost({ commit }, 'www.chatwoot.com');
+      expect(commit.mock.calls).toEqual([
+        ['SET_REFERRER_HOST', 'www.chatwoot.com'],
+      ]);
+    });
+  });
+
+  describe('#setWidgetColor', () => {
+    it('creates actions properly', () => {
+      actions.setWidgetColor({ commit }, { widgetColor: '#eaeaea' });
+      expect(commit.mock.calls).toEqual([
+        ['SET_WIDGET_COLOR', { widgetColor: '#eaeaea' }],
+      ]);
+    });
+  });
+});
