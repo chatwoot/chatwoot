f1a27fd4e14923602beabc80a3182bc5c04eb447
Pranav Raj S <pranav@chatwoot.com>
2021-11-23 10:39:24 +0530

    Fix mutation specs

--- app/javascript/widget/store/modules/specs/appConfig/mutations.spec.js
+++ app/javascript/widget/store/modules/specs/appConfig/mutations.spec.js
@@ -12,7 +12,7 @@ describe('#mutations', () => {
   describe('#SET_WIDGET_COLOR', () => {
     it('sets widget color properly', () => {
       const state = { widgetColor: '' };
-      mutations.SET_WIDGET_COLOR(state, { widgetColor: '#00bcd4' });
+      mutations.SET_WIDGET_COLOR(state, '#00bcd4');
       expect(state.widgetColor).toEqual('#00bcd4');
     });
   });
