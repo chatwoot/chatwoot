b326da7d94312ca585c61d6b677fda7a4e960ed5
Santhosh C <csesanthosh15@gmail.com>
2021-11-27 00:56:07 +0530

    feat: Ability to remove user profile picture (#3156)
    Fixes #3107

--- config/routes.rb
+++ config/routes.rb
@@ -166,10 +166,12 @@ Rails.application.routes.draw do
       end
 
       resource :profile, only: [:show, :update] do
+        delete :avatar, on: :collection
         member do
           post :availability
         end
       end
+
       resource :notification_subscriptions, only: [:create]
 
       namespace :widget do
