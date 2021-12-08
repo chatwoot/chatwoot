31bb0c3c2bff5b817b0cbcc1700d4300cbc97023
Tejaswini Chile <tejaswini@chatwoot.com>
2021-12-08 16:17:49 +0530

    Fix: Added facebook login scope with new advanced access for page read (#3518)

--- app/javascript/dashboard/routes/dashboard/settings/inbox/channels/Facebook.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/channels/Facebook.vue
@@ -163,7 +163,7 @@ export default {
           FB.init({
             appId: window.chatwootConfig.fbAppId,
             xfbml: true,
-            version: 'v7.0',
+            version: 'v12.0',
             status: true,
           });
           window.fbSDKLoaded = true;
@@ -207,7 +207,7 @@ export default {
         },
         {
           scope:
-            'pages_manage_metadata,pages_messaging,instagram_basic,pages_show_list,instagram_manage_messages',
+            'pages_manage_metadata,pages_messaging,instagram_basic,pages_show_list,pages_read_engagement,instagram_manage_messages',
         }
       );
     },
