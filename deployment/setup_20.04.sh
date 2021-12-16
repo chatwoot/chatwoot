9d4f70932fb984cc15acafd9cf698402f10c1e91
Shauli Mizrahi <95334569+shaulirep@users.noreply.github.com>
2021-12-16 14:13:17 +1100

    Update setup_20.04.sh
    removed ngnix config and ssl

--- deployment/setup_20.04.sh
+++ deployment/setup_20.04.sh
@@ -5,20 +5,6 @@
 # Script Version: 0.7
 # Run this script as root
 
-read -p 'Would you like to configure a domain and SSL for Chatwoot?(yes or no): ' configure_webserver
-
-if [ $configure_webserver == "yes" ]
-then
-read -p 'Enter your sub-domain to be used for Chatwoot (chatwoot.domain.com for example) : ' domain_name
-echo -e "\nThis script will try to generate SSL certificates via LetsEncrypt and serve chatwoot at
-"https://$domain_name". Proceed further once you have pointed your DNS to the IP of the instance.\n"
-read -p 'Do you wish to proceed? (yes or no): ' exit_true
-if [ $exit_true == "no" ]
-then
-exit 1
-fi
-fi
-
 
 apt update && apt upgrade -y
 apt install -y curl
@@ -35,10 +21,6 @@ apt install -y \
     libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev
 
 
-if [ $configure_webserver == "yes" ]
-then
-apt install -y nginx nginx-full certbot python3-certbot-nginx
-fi
 
 adduser --disabled-login --gecos "" chatwoot
 
@@ -79,14 +61,6 @@ echo -en "\nINSTALLATION_ENV=linux_script" >> ".env"
 rake assets:precompile RAILS_ENV=production
 EOF
 
-if [ $install_pg_redis != "no" ]
-then
-sudo -i -u chatwoot << EOF
-cd chatwoot
-RAILS_ENV=production bundle exec rake db:create
-RAILS_ENV=production bundle exec rake db:reset
-EOF
-fi
 
 cp /home/chatwoot/chatwoot/deployment/chatwoot-web.1.service /etc/systemd/system/chatwoot-web.1.service
 cp /home/chatwoot/chatwoot/deployment/chatwoot-worker.1.service /etc/systemd/system/chatwoot-worker.1.service
@@ -97,37 +71,15 @@ systemctl start chatwoot.target
 
 public_ip=$(curl http://checkip.amazonaws.com -s)
 
-if [ $configure_webserver != "yes" ]
-then
+
 echo -en "\n\n***************************************************************************\n"
 echo "Woot! Woot!! Chatwoot server installation is complete"
 echo "The server will be accessible at http://$public_ip:3000"
 echo "To configure a domain and SSL certificate, follow the guide at https://www.chatwoot.com/docs/deployment/deploy-chatwoot-in-linux-vm"
 echo "***************************************************************************"
-else
-curl https://ssl-config.mozilla.org/ffdhe4096.txt >> /etc/ssl/dhparam
-wget https://raw.githubusercontent.com/shaulirep/rep-live-chat/develop/deployment/nginx_chatwoot.conf
-cp nginx_chatwoot.conf /etc/nginx/sites-available/nginx_chatwoot.conf
-certbot certonly --nginx -d $domain_name
-sed -i "s/chatwoot.domain.com/$domain_name/g" /etc/nginx/sites-available/nginx_chatwoot.conf
-ln -s /etc/nginx/sites-available/nginx_chatwoot.conf /etc/nginx/sites-enabled/nginx_chatwoot.conf
-systemctl restart nginx
-sudo -i -u chatwoot << EOF
-cd chatwoot
-sed -i "s/http:\/\/0.0.0.0:3000/https:\/\/$domain_name/g" .env
-EOF
-systemctl restart chatwoot.target
-echo -en "\n\n***************************************************************************\n"
-echo "Woot! Woot!! Chatwoot server installation is complete"
-echo "The server will be accessible at https://$domain_name"
-echo "***************************************************************************"
-fi
 
 
-if [ $install_pg_redis == "no" ]
-then
 echo -en "\n\n***************************************************************************\n"
 echo "DB migrations are not run as pg and redis is not installed."
 echo "After modifying .env with your external db creds, run db migrations !!!"
 echo "***************************************************************************"
-fi
