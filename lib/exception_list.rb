aaf2cefb8fb70f1a0871397ae4dfecf27ba32c88
Sojan <sojan@pepalo.com>
2021-12-22 22:51:52 +0530

    chore: exception list

--- lib/exception_list.rb
+++ lib/exception_list.rb
@@ -2,6 +2,8 @@ module ExceptionList
   REST_CLIENT_EXCEPTIONS = [RestClient::NotFound, RestClient::GatewayTimeout, RestClient::BadRequest,
                             RestClient::MethodNotAllowed, RestClient::Forbidden, RestClient::InternalServerError,
                             RestClient::Exceptions::OpenTimeout, RestClient::Exceptions::ReadTimeout,
+                            RestClient::TemporaryRedirect, RestClient::SSLCertificateNotVerified, RestClient::PaymentRequired,
+                            RestClient::BadGateway, RestClient::Unauthorized, RestClient::PayloadTooLarge,
                             RestClient::MovedPermanently, RestClient::ServiceUnavailable, Errno::ECONNREFUSED, SocketError].freeze
   SMTP_EXCEPTIONS = [
     Net::SMTPSyntaxError
