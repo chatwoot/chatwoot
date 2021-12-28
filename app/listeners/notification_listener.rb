756d3424be44252cf637eaf49e0043325858f62b
Sojan <sojan@pepalo.com>
2021-12-28 21:01:51 +0530

    chore: Fix issue with conversation assignment notifications

--- app/listeners/notification_listener.rb
+++ app/listeners/notification_listener.rb
@@ -16,7 +16,7 @@ class NotificationListener < BaseListener
   def assignee_changed(event)
     conversation, account = extract_conversation_and_account(event)
     assignee = conversation.assignee
-    return unless conversation.notifiable_assignee_change?
+    return unless event.data[:notifiable_assignee_change].present?
     return if conversation.pending?
 
     NotificationBuilder.new(
