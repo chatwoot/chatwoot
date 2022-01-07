6bbf3e07590cc474d349f5addd028aac158067ce
Aswin Dev P S <aswin@chatwoot.com>
2022-01-07 11:56:24 +0530

    Add data migration script for csat enabled inboxes

--- db/migrate/20211216110209_add_allow_messages_after_resolved_to_inbox.rb
+++ db/migrate/20211216110209_add_allow_messages_after_resolved_to_inbox.rb
@@ -1,5 +1,16 @@
 class AddAllowMessagesAfterResolvedToInbox < ActiveRecord::Migration[6.1]
   def change
     add_column :inboxes, :allow_messages_after_resolved, :boolean, default: true
+
+    update_csat_enabled_inboxes
+  end
+
+  def update_csat_enabled_inboxes
+    ::Inbox.find_in_batches do |inboxes_batch|
+      inboxes_batch.each do |inbox|
+        inbox.allow_messages_after_resolved = false if inbox.channel_type == 'Channel::WebWidget' && inbox.csat_survey_enabled?
+        inbox.save!
+      end
+    end
   end
 end
