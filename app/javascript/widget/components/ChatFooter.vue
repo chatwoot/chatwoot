7ee70628433185407eb3558e5fbf8230aa372ae3
Aswin Dev P.S <aswin@chatwoot.com>
2022-01-11 00:32:03 -0800

    feat: Toggle to disallow users to send messages after a conversation is resolved (#3605)

--- app/javascript/widget/components/ChatFooter.vue
+++ app/javascript/widget/components/ChatFooter.vue
@@ -58,9 +58,9 @@ export default {
       return getContrastingTextColor(this.widgetColor);
     },
     hideReplyBox() {
-      const { csatSurveyEnabled } = window.chatwootWebChannel;
+      const { allowMessagesAfterResolved } = window.chatwootWebChannel;
       const { status } = this.conversationAttributes;
-      return csatSurveyEnabled && status === 'resolved';
+      return !allowMessagesAfterResolved && status === 'resolved';
     },
     showEmailTranscriptButton() {
       return this.currentUser && this.currentUser.email;
