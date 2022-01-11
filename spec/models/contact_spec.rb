4ee24b3f59592b5e798947f59b78803158e4bb1d
Sojan Jose <sojan@pepalo.com>
2022-01-12 04:44:55 +0530

    chore: Prevent null in Contact JSONB attributes (#3730)

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -11,4 +11,36 @@ RSpec.describe Contact do
     it { is_expected.to belong_to(:account) }
     it { is_expected.to have_many(:conversations).dependent(:destroy_async) }
   end
+
+  context 'pepare contact attributes before validation' do
+    it 'sets email to lowercase' do
+      contact = create(:contact, email: 'Test@test.com')
+      expect(contact.email).to eq('test@test.com')
+    end
+
+    it 'sets email to nil when empty string' do
+      contact = create(:contact, email: '')
+      expect(contact.email).to be_nil
+    end
+
+    it 'sets custom_attributes to {} when nil' do
+      contact = create(:contact, custom_attributes: nil)
+      expect(contact.custom_attributes).to eq({})
+    end
+
+    it 'sets custom_attributes to {} when empty string' do
+      contact = create(:contact, custom_attributes: '')
+      expect(contact.custom_attributes).to eq({})
+    end
+
+    it 'sets additional_attributes to {} when nil' do
+      contact = create(:contact, additional_attributes: nil)
+      expect(contact.additional_attributes).to eq({})
+    end
+
+    it 'sets additional_attributes to {} when empty string' do
+      contact = create(:contact, additional_attributes: '')
+      expect(contact.additional_attributes).to eq({})
+    end
+  end
 end
