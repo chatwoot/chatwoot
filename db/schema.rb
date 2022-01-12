7e544b0483305c779019d70d933677efb0a276c1
Sojan Jose <sojan@pepalo.com>
2022-01-12 08:30:00 +0530

    fix: SuperAdmin Improvements (#3733)
    - Fix broken access tokens pages
    - Fix broken avatar images in the super admin users tab
    - Fix broken pagination links in super admin
    - Add agent bot tabs
    
    Fixes: #2021

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_11_200105) do
+ActiveRecord::Schema.define(version: 2022_01_11_223630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
