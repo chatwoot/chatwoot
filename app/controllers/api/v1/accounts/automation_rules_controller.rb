3da55d14708066cd9bcd87a872055015188b11ca
Tejaswini <tejaswini@chatwoot.com>
2022-01-17 14:03:42 +0530

    Feat: Clone and update automation rules

--- app/controllers/api/v1/accounts/automation_rules_controller.rb
+++ app/controllers/api/v1/accounts/automation_rules_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Accounts::AutomationRulesController < Api::V1::Accounts::BaseController
   before_action :check_authorization
+  before_action :fetch_automation_rule, only: [:show, :edit, :update, :destroy, :clone]
 
   def index
     @automation_rules = Current.account.automation_rules.active
@@ -7,6 +8,26 @@ class Api::V1::Accounts::AutomationRulesController < Api::V1::Accounts::BaseCont
 
   def create
     @automation_rule = Current.account.automation_rules.create(automation_rules_permit)
+  end
+
+  def edit; end
+
+  def show; end
+
+  def update
+    @automation_rule.update(automation_rules_permit)
+  end
+
+  def destroy
+    @automation_rule.destroy!
+    head :ok
+  end
+
+  def clone
+    automation_rule = Current.account.automation_rules.find_by(id: params[:automation_rule_id])
+    new_rule = automation_rule.dup
+    new_rule.save
+    @automation_rule = new_rule
   end
 
   private
@@ -17,5 +38,9 @@ class Api::V1::Accounts::AutomationRulesController < Api::V1::Accounts::BaseCont
       conditions: [:attribute_key, :filter_operator, :query_operator, { values: [] }],
       actions: [:action_name, { action_params: [:intiated_at] }]
     )
+  end
+
+  def fetch_automation_rule
+    @automation_rule = Current.account.automation_rules.find_by(id: params[:id])
   end
 end
