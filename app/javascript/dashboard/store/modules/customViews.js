b3382c4306184bf030dfd745b9f330a5d6da6559
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2022-01-17 09:18:54 +0530

    feat: Adds the ability to have custom view for conversations  (#3666)
    * feat: Adds the ability to save custom filters and display folders on the sidebar
    
    * Minor fixes
    
    * Review fixes
    
    * Review fixes
    
    * i18n fixes
    
    * Shows conversations when the user click on the folder sidebar item
    
    * Spacing fixes
    
    * Review fixes
    
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>

---
+++ app/javascript/dashboard/store/modules/customViews.js
@@ -0,0 +1,79 @@
+import * as MutationHelpers from 'shared/helpers/vuex/mutationHelpers';
+import types from '../mutation-types';
+import CustomViewsAPI from '../../api/customViews';
+
+export const state = {
+  records: [],
+  uiFlags: {
+    isFetching: false,
+    isCreating: false,
+    isDeleting: false,
+  },
+};
+
+export const getters = {
+  getUIFlags(_state) {
+    return _state.uiFlags;
+  },
+  getCustomViews(_state) {
+    return _state.records;
+  },
+};
+
+export const actions = {
+  get: async function getCustomViews({ commit }) {
+    commit(types.SET_CUSTOM_VIEW_UI_FLAG, { isFetching: true });
+    try {
+      const response = await CustomViewsAPI.getCustomViews();
+      commit(types.SET_CUSTOM_VIEW, response.data);
+    } catch (error) {
+      // Ignore error
+    } finally {
+      commit(types.SET_CUSTOM_VIEW_UI_FLAG, { isFetching: false });
+    }
+  },
+  create: async function createCustomViews({ commit }, obj) {
+    commit(types.SET_CUSTOM_VIEW_UI_FLAG, { isCreating: true });
+    try {
+      const response = await CustomViewsAPI.create(obj);
+      commit(types.ADD_CUSTOM_VIEW, response.data);
+    } catch (error) {
+      const errorMessage = error?.response?.data?.message;
+      throw new Error(errorMessage);
+    } finally {
+      commit(types.SET_CUSTOM_VIEW_UI_FLAG, { isCreating: false });
+    }
+  },
+  delete: async ({ commit }, id) => {
+    commit(types.SET_CUSTOM_VIEW_UI_FLAG, { isDeleting: true });
+    try {
+      await CustomViewsAPI.delete(id);
+      commit(types.DELETE_CUSTOM_VIEW, id);
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      commit(types.SET_CUSTOM_VIEW_UI_FLAG, { isDeleting: false });
+    }
+  },
+};
+
+export const mutations = {
+  [types.SET_CUSTOM_VIEW_UI_FLAG](_state, data) {
+    _state.uiFlags = {
+      ..._state.uiFlags,
+      ...data,
+    };
+  },
+
+  [types.ADD_CUSTOM_VIEW]: MutationHelpers.create,
+  [types.SET_CUSTOM_VIEW]: MutationHelpers.set,
+  [types.DELETE_CUSTOM_VIEW]: MutationHelpers.destroy,
+};
+
+export default {
+  namespaced: true,
+  actions,
+  state,
+  getters,
+  mutations,
+};
