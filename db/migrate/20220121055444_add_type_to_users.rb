43f4fdc1b7ca5a2ed47959ad451c1eaeccfe7404
Sojan <sojan@pepalo.com>
2022-01-21 16:40:35 -0800

    chore: migrating existing super admins

--- db/migrate/20220121055444_add_type_to_users.rb
+++ db/migrate/20220121055444_add_type_to_users.rb
@@ -1,5 +1,30 @@
 class AddTypeToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :type, :string
+    migrate_existing_super_admins
+  end
+
+  private
+
+  def old_super_admins
+    ActiveRecord::Base.connection.execute('SELECT * from super_admins').to_a
+  end
+
+  def create_user_account_for_super_admin(super_admin)
+    u = User.new(email: super_admin['email'], name: "SuperUser #{super_admin['id']}", encrypted_password: super_admin['encrypted_password'],
+                 confirmed_at: DateTime.now, type: 'SuperAdmin')
+    u.save(validate: false)
+  end
+
+  def migrate_existing_super_admins
+    old_super_admins.each do |super_admin|
+      user = User.find(super_admin['email'])
+      if user.present?
+        user.update(type: 'SuperAdmin')
+      else
+        Rails.logger.debug { "User with email #{super_admin['email']} not found" }
+        create_user_account_for_super_admin(super_admin)
+      end
+    end
   end
 end
