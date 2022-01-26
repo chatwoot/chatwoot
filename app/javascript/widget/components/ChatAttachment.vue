f18ca537d2cfe77ceba5748007008306ee64f687
Tejaswini <tejaswini@chatwoot.com>
2022-01-26 18:30:59 +0530

    PR comments resolved

--- app/javascript/widget/components/ChatAttachment.vue
+++ app/javascript/widget/components/ChatAttachment.vue
@@ -59,7 +59,7 @@ export default {
           upload.create((error, blob) => {
             if (error) {
               window.bus.$emit(BUS_EVENTS.SHOW_ALERT, {
-                message: this.$t(error),
+                message: error,
               });
             } else {
               this.onAttach({
