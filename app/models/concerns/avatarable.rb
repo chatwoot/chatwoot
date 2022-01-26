2ec85255648ab7098fe03f98212c11220dc8e4cb
Sojan Jose <sojan@pepalo.com>
2022-01-25 16:58:49 -0800

    feat: Unify user and super admin credentials (#3830)
    Fixes: #3061, #3489

--- app/models/concerns/avatarable.rb
+++ app/models/concerns/avatarable.rb
@@ -12,7 +12,7 @@ module Avatarable
   def avatar_url
     return url_for(avatar.representation(resize: '250x250')) if avatar.attached? && avatar.representable?
 
-    if [User, Contact].include?(self.class) && email.present?
+    if [SuperAdmin, User, Contact].include?(self.class) && email.present?
       hash = Digest::MD5.hexdigest(email)
       return "https://www.gravatar.com/avatar/#{hash}?d=404"
     end
