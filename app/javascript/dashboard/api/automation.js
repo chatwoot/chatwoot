8426a9b7a3eb814508daf30570cc0196f853dac3
fayazara <fayazara@gmail.com>
2022-02-02 19:47:47 +0530

    clone automation

--- app/javascript/dashboard/api/automation.js
+++ app/javascript/dashboard/api/automation.js
@@ -4,6 +4,10 @@ class AutomationsAPI extends ApiClient {
   constructor() {
     super('automation_rules', { accountScoped: true });
   }
+
+  clone(automationId) {
+    return axios.post(`${this.url}/${automationId}/clone`);
+  }
 }
 
 export default new AutomationsAPI();
