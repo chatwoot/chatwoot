82502e4c4ec94c097709bd44d2562f02efa105a3
fayazara <fayazara@gmail.com>
2022-02-07 20:20:35 +0530

    Fix codeclimate issues

--- app/javascript/dashboard/helper/filterQueryGenerator.js
+++ app/javascript/dashboard/helper/filterQueryGenerator.js
@@ -1,3 +1,15 @@
+function lowerCaseValues(operator, values) {
+  if (operator === 'equal_to' || operator === 'not_equal_to') {
+    values = values.map(val => {
+      if (typeof val === 'string') {
+        return val.toLowerCase();
+      }
+      return val;
+    });
+  }
+  return values;
+}
+
 const generatePayload = data => {
   // Make a copy of data to avoid vue data reactivity issues
   const filters = JSON.parse(JSON.stringify(data));
@@ -12,17 +24,7 @@ const generatePayload = data => {
       item.values = [item.values];
     }
     // Convert all values to lowerCase if operator_type is 'equal_to' or 'not_equal_to'
-    if (
-      item.filter_operator === 'equal_to' ||
-      item.filter_operator === 'not_equal_to'
-    ) {
-      item.values = item.values.map(val => {
-        if (typeof val === 'string') {
-          return val.toLowerCase();
-        }
-        return val;
-      });
-    }
+    item.values = lowerCaseValues(item.filter_operator, item.values);
     return item;
   });
   // For every query added, the query_operator is set default to and so the
