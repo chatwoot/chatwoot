7594cb29cf9459ac1bb0008fd03702909e964c45
fayazara <fayazara@gmail.com>
2022-02-07 17:07:48 +0530

    Fix filter payload issue

--- app/javascript/dashboard/helper/filterQueryGenerator.js
+++ app/javascript/dashboard/helper/filterQueryGenerator.js
@@ -1,5 +1,7 @@
 const generatePayload = data => {
-  let payload = data.map(item => {
+  // Make a copy of data to avoid vue data reactivity issues
+  const filters = JSON.parse(JSON.stringify(data));
+  let payload = filters.map(item => {
     if (Array.isArray(item.values)) {
       item.values = item.values.map(val => val.id);
     } else if (typeof item.values === 'object') {
@@ -8,6 +10,13 @@ const generatePayload = data => {
       item.values = [];
     } else {
       item.values = [item.values];
+    }
+    // Convert all values to lowerCase if operator_type is 'equal_to' or 'not_equal_to'
+    if (
+      item.filter_operator === 'equal_to' ||
+      item.filter_operator === 'not_equal_to'
+    ) {
+      item.values = item.values.map(val => val.toLowerCase());
     }
     return item;
   });
