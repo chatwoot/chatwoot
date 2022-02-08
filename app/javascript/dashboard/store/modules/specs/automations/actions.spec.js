2f6d909bc6385acd24afedb3097986abd31858fe
fayazara <fayazara@gmail.com>
2022-02-08 16:58:31 +0530

    Fix test specs

--- app/javascript/dashboard/store/modules/specs/automations/actions.spec.js
+++ app/javascript/dashboard/store/modules/specs/automations/actions.spec.js
@@ -30,11 +30,11 @@ describe('#actions', () => {
 
   describe('#create', () => {
     it('sends correct actions if API is success', async () => {
-      axios.post.mockResolvedValue({ data: automationsList[0] });
-      await actions.create({ commit }, automationsList[0]);
+      axios.post.mockResolvedValue({ data: automationsList.payload[0] });
+      await actions.create({ commit }, automationsList.payload[0]);
       expect(commit.mock.calls).toEqual([
         [types.default.SET_AUTOMATION_UI_FLAG, { isCreating: true }],
-        [types.default.ADD_AUTOMATION, automationsList[0]],
+        [types.default.ADD_AUTOMATION, automationsList.payload[0]],
         [types.default.SET_AUTOMATION_UI_FLAG, { isCreating: false }],
       ]);
     });
@@ -50,18 +50,19 @@ describe('#actions', () => {
 
   describe('#update', () => {
     it('sends correct actions if API is success', async () => {
-      axios.patch.mockResolvedValue({ data: automationsList[0] });
-      await actions.update({ commit }, automationsList[0]);
+      axios.patch.mockResolvedValue({ data: automationsList.payload[0] });
+
+      await actions.update({ commit }, automationsList.payload[0]);
       expect(commit.mock.calls).toEqual([
         [types.default.SET_AUTOMATION_UI_FLAG, { isUpdating: true }],
-        [types.default.EDIT_AUTOMATION, automationsList[0]],
+        [types.default.EDIT_AUTOMATION, automationsList.payload[0]],
         [types.default.SET_AUTOMATION_UI_FLAG, { isUpdating: false }],
       ]);
     });
     it('sends correct actions if API is error', async () => {
       axios.patch.mockRejectedValue({ message: 'Incorrect header' });
       await expect(
-        actions.update({ commit }, automationsList[0])
+        actions.update({ commit }, automationsList.payload[0])
       ).rejects.toThrow(Error);
       expect(commit.mock.calls).toEqual([
         [types.default.SET_AUTOMATION_UI_FLAG, { isUpdating: true }],
@@ -72,18 +73,18 @@ describe('#actions', () => {
 
   describe('#delete', () => {
     it('sends correct actions if API is success', async () => {
-      axios.delete.mockResolvedValue({ data: automationsList[0] });
-      await actions.delete({ commit }, automationsList[0].id);
+      axios.delete.mockResolvedValue({ data: automationsList.payload[0] });
+      await actions.delete({ commit }, automationsList.payload[0].id);
       expect(commit.mock.calls).toEqual([
         [types.default.SET_AUTOMATION_UI_FLAG, { isDeleting: true }],
-        [types.default.DELETE_AUTOMATION, automationsList[0].id],
+        [types.default.DELETE_AUTOMATION, automationsList.payload[0].id],
         [types.default.SET_AUTOMATION_UI_FLAG, { isDeleting: false }],
       ]);
     });
     it('sends correct actions if API is error', async () => {
       axios.delete.mockRejectedValue({ message: 'Incorrect header' });
       await expect(
-        actions.delete({ commit }, automationsList[0].id)
+        actions.delete({ commit }, automationsList.payload[0].id)
       ).rejects.toThrow(Error);
       expect(commit.mock.calls).toEqual([
         [types.default.SET_AUTOMATION_UI_FLAG, { isDeleting: true }],
@@ -94,8 +95,8 @@ describe('#actions', () => {
 
   describe('#clone', () => {
     it('clones the automation', async () => {
-      axios.post.mockResolvedValue({ data: automationsList[0] });
-      await actions.clone({ commit }, automationsList[0]);
+      axios.post.mockResolvedValue({ data: automationsList.payload[0] });
+      await actions.clone({ commit }, automationsList.payload[0]);
       expect(commit.mock.calls).toEqual([
         [types.default.SET_AUTOMATION_UI_FLAG, { isCloning: true }],
         [types.default.SET_AUTOMATION_UI_FLAG, { isCloning: false }],
