a49b587698b0faccf5ae68631f4f4d3177272e7f
Pranav Raj S <pranav@chatwoot.com>
2022-02-09 14:37:41 +0530

    Message Contents -> Message Content

--- app/javascript/dashboard/routes/dashboard/settings/automation/constants.js
+++ app/javascript/dashboard/routes/dashboard/settings/automation/constants.js
@@ -59,7 +59,7 @@ export const AUTOMATIONS = {
       },
       {
         key: 'content',
-        name: 'Message Contents',
+        name: 'Message Content',
         attributeI18nKey: 'MESSAGE_CONTAINS',
         inputType: 'plain_text',
         filterOperators: OPERATOR_TYPES_2,
