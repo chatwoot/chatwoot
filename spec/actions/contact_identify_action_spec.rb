f5b4ea0b6420585afb43148246a7c65e97b36d30
Muhsin Keloth <muhsinkeramam@gmail.com>
2022-02-10 15:37:50 +0530

    Add additional attributes in contact update API

--- spec/actions/contact_identify_action_spec.rb
+++ spec/actions/contact_identify_action_spec.rb
@@ -6,7 +6,10 @@ describe ::ContactIdentifyAction do
   let!(:account) { create(:account) }
   let(:custom_attributes) { { test: 'test', test1: 'test1' } }
   let!(:contact) { create(:contact, account: account, custom_attributes: custom_attributes) }
-  let(:params) { { name: 'test', identifier: 'test_id', custom_attributes: { test: 'new test', test2: 'test2' } } }
+  let(:params) do
+    { name: 'test', identifier: 'test_id', additional_attributes: { location: 'Bengaulru', company_name: 'Meta' },
+      custom_attributes: { test: 'new test', test2: 'test2' } }
+  end
 
   describe '#perform' do
     it 'updates the contact' do
@@ -15,6 +18,7 @@ describe ::ContactIdentifyAction do
       expect(contact.reload.name).to eq 'test'
       # custom attributes are merged properly without overwriting existing ones
       expect(contact.custom_attributes).to eq({ 'test' => 'new test', 'test1' => 'test1', 'test2' => 'test2' })
+      expect(contact.additional_attributes).to eq({ 'company_name' => 'Meta', 'location' => 'Bengaulru' })
       expect(contact.reload.identifier).to eq 'test_id'
     end
 
