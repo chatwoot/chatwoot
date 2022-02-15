ae1545991ced5bd8277057f01aaa54bb6a4fc2db
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2022-02-15 23:36:29 +0530

    fix: Automation Bugs and minor enhancements (#3936)

--- app/javascript/dashboard/store/modules/specs/automations/fixtures.js
+++ app/javascript/dashboard/store/modules/specs/automations/fixtures.js
@@ -1,64 +1,36 @@
 export default [
   {
-    id: 12,
+    name: 'Test 5',
+    description: 'Hello',
+    id: 46,
     account_id: 1,
-    name: 'Test',
-    description: 'This is a test',
     event_name: 'conversation_created',
     conditions: [
       {
         values: ['open'],
         attribute_key: 'status',
-        query_operator: null,
         filter_operator: 'equal_to',
       },
     ],
-    actions: [
-      {
-        action_name: 'add_label',
-        action_params: [{}],
-      },
-    ],
-    created_on: '2022-01-14T09:17:55.689Z',
+    actions: [{ action_name: 'add_label', action_params: ['testlabel'] }],
+    created_on: '2022-02-08T10:46:32.387Z',
     active: true,
   },
   {
-    id: 13,
+    id: 47,
     account_id: 1,
-    name: 'Auto resolve conversation',
-    description: 'Auto resolves conversation',
-    event_name: 'conversation_updated',
+    name: 'Snooze',
+    description: 'Test Description',
+    event_name: 'conversation_created',
     conditions: [
       {
-        values: ['resolved'],
+        values: ['pending'],
         attribute_key: 'status',
-        query_operator: null,
         filter_operator: 'equal_to',
       },
     ],
-    actions: [
-      {
-        action_name: 'add_label',
-        action_params: [{}],
-      },
-    ],
-    created_on: '2022-01-14T13:06:31.843Z',
-    active: true,
-  },
-  {
-    id: 14,
-    account_id: 1,
-    name: 'Fayaz',
-    description: 'This is a test',
-    event_name: 'conversation_created',
-    conditions: {},
-    actions: [
-      {
-        action_name: 'add_label',
-        action_params: [{}],
-      },
-    ],
-    created_on: '2022-01-17T06:46:08.098Z',
+    actions: [{ action_name: 'assign_team', action_params: [1] }],
+    created_on: '2022-02-08T11:19:44.714Z',
     active: true,
   },
 ];
