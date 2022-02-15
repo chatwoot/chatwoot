f32d379a4a8432360c10e8e19db7ce5cc62f8f3f
Pranav Raj S <pranav@chatwoot.com>
2022-02-15 12:12:27 +0530

    chore: Fix timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_124741) do
+ActiveRecord::Schema.define(version: 2022_02_15_060751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
