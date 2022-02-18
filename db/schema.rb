50b4fd2245c343d5d7fd5a3fac9502dbc377060e
Pranav Raj S <pranav@chatwoot.com>
2022-02-18 18:13:22 +0530

    Revert unnecessary changes in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -396,7 +396,7 @@ ActiveRecord::Schema.define(version: 2022_02_18_120357) do
     t.text "attribute_description"
     t.jsonb "attribute_values", default: []
     t.index ["account_id"], name: "index_custom_attribute_definitions_on_account_id"
-    t.index ["attribute_key", "attribute_model"], name: "attribute_key_model_index", unique: true
+    t.index ["attribute_key", "attribute_model", "account_id"], name: "attribute_key_model_index", unique: true
   end
 
   create_table "custom_filters", force: :cascade do |t|
