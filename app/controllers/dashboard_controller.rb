04b1fa434a0fea56751cfac012b21ccbfcc8ab2b
Pranav Raj S <pranav@chatwoot.com>
2022-02-25 15:28:30 +0530

    Add customizable logout links

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -27,7 +27,8 @@ class DashboardController < ActionController::Base
       'ANALYTICS_TOKEN',
       'ANALYTICS_HOST',
       'DIRECT_UPLOADS_ENABLED',
-      'HCAPTCHA_SITE_KEY'
+      'HCAPTCHA_SITE_KEY',
+      'LOGOUT_REDIRECT_LINK',
     ).merge(app_config)
   end
 
