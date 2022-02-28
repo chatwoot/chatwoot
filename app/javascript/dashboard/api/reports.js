e095b76691085460af268503a1839f097752ee1c
Pranav Raj S <pranav@chatwoot.com>
2022-02-28 10:56:24 +0530

    fix: Consider timezone in the reports (#4027)
    Co-authored-by: Sojan Jose <sojan@pepalo.com>

--- app/javascript/dashboard/api/reports.js
+++ app/javascript/dashboard/api/reports.js
@@ -1,6 +1,8 @@
 /* global axios */
 import ApiClient from './ApiClient';
 
+const getTimeOffset = () => -new Date().getTimezoneOffset() / 60;
+
 class ReportsAPI extends ApiClient {
   constructor() {
     super('reports', { accountScoped: true, apiVersion: 'v2' });
@@ -8,13 +10,27 @@ class ReportsAPI extends ApiClient {
 
   getReports(metric, since, until, type = 'account', id, group_by) {
     return axios.get(`${this.url}`, {
-      params: { metric, since, until, type, id, group_by },
+      params: {
+        metric,
+        since,
+        until,
+        type,
+        id,
+        group_by,
+        timezone_offset: getTimeOffset(),
+      },
     });
   }
 
   getSummary(since, until, type = 'account', id, group_by) {
     return axios.get(`${this.url}/summary`, {
-      params: { since, until, type, id, group_by },
+      params: {
+        since,
+        until,
+        type,
+        id,
+        group_by,
+      },
     });
   }
 
