06b9f3ce31dbef7ae9943f5280eb588029693485
Muhsin Keloth <muhsinkeramam@gmail.com>
2022-03-09 11:13:55 +0530

    Add api changes

--- app/javascript/dashboard/routes/dashboard/settings/inbox/PreChatForm/Settings.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/PreChatForm/Settings.vue
@@ -26,17 +26,47 @@
           "
         />
       </label>
-      <div>
-        <input
-          v-model="preChatFieldOptions"
-          type="checkbox"
-          value="requireEmail"
-          @input="handlePreChatFieldOptions"
-        />
-        <label for="requireEmail">
-          {{ $t('INBOX_MGMT.PRE_CHAT_FORM.REQUIRE_EMAIL.LABEL') }}
-        </label>
-      </div>
+      <label> Set your fields </label>
+      <table class="table table-striped">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Field</th>
+            <th scope="col">Required</th>
+          </tr>
+        </thead>
+        <draggable v-model="preChatFields" tag="tbody">
+          <tr v-for="(item, index) in preChatFields" :key="index">
+            <td scope="row">
+              <button
+                type="button"
+                class="toggle-button"
+                :class="{ active: item['enabled'] }"
+                role="switch"
+                @click="handlePreChatFieldOptions($event, 'enabled', item)"
+              >
+                <span
+                  aria-hidden="true"
+                  :class="{ active: item['enabled'] }"
+                ></span>
+              </button>
+            </td>
+            <td :class="{ 'disabled-text': !item['enabled'] }">
+              {{ item.label }}
+            </td>
+            <td>
+              <input
+                v-if="item['enabled']"
+                v-model="item['required']"
+                type="checkbox"
+                :value="`${item.name}-required`"
+                @click="handlePreChatFieldOptions($event, 'required', item)"
+              />
+            </td>
+          </tr>
+        </draggable>
+      </table>
+
       <woot-submit-button
         :button-text="$t('INBOX_MGMT.SETTINGS_POPUP.UPDATE')"
         :loading="uiFlags.isUpdatingInbox"
@@ -47,8 +77,12 @@
 <script>
 import { mapGetters } from 'vuex';
 import alertMixin from 'shared/mixins/alertMixin';
+import draggable from 'vuedraggable';
 
 export default {
+  components: {
+    draggable,
+  },
   mixins: [alertMixin],
   props: {
     inbox: {
@@ -61,6 +95,7 @@ export default {
       preChatFormEnabled: false,
       preChatMessage: '',
       preChatFieldOptions: [],
+      preChatFields: [],
     };
   },
   computed: {
@@ -81,19 +116,20 @@ export default {
         pre_chat_form_options: preChatFormOptions,
       } = this.inbox;
       this.preChatFormEnabled = preChatFormEnabled;
-      const { pre_chat_message: preChatMessage, require_email: requireEmail } =
-        preChatFormOptions || {};
+      const {
+        pre_chat_message: preChatMessage,
+        pre_chat_fields: preChatFields,
+      } = preChatFormOptions || {};
       this.preChatMessage = preChatMessage;
-      if (requireEmail) {
-        this.preChatFieldOptions = ['requireEmail'];
-      }
+      this.preChatFields = preChatFields;
     },
-    handlePreChatFieldOptions(event) {
-      if (this.preChatFieldOptions.includes(event.target.value)) {
-        this.preChatFieldOptions = [];
-      } else {
-        this.preChatFieldOptions = [event.target.value];
-      }
+    handlePreChatFieldOptions(event, type, item) {
+      // eslint-disable-next-line
+      this.preChatFields.map((field, index) => {
+        if (field.name === item.name) {
+          this.preChatFields[index][type] = !item[type];
+        }
+      });
     },
     async updateInbox() {
       try {
@@ -104,7 +140,7 @@ export default {
             pre_chat_form_enabled: this.preChatFormEnabled,
             pre_chat_form_options: {
               pre_chat_message: this.preChatMessage,
-              require_email: this.preChatFieldOptions.includes('requireEmail'),
+              pre_chat_fields: this.preChatFields,
             },
           },
         };
@@ -124,5 +160,46 @@ export default {
 
 .prechat--title {
   margin: var(--space-medium) 0 var(--space-slab);
+}
+.toggle-button {
+  background-color: var(--s-200);
+  position: relative;
+  display: inline-flex;
+  height: 19px;
+  width: 34px;
+  border: 2px solid transparent;
+  border-radius: var(--border-radius-large);
+  cursor: pointer;
+  transition-property: background-color;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 200ms;
+  flex-shrink: 0;
+}
+
+.toggle-button.active {
+  background-color: var(--w-500);
+}
+
+.toggle-button span {
+  --space-one-point-five: 1.5rem;
+  height: var(--space-one-point-five);
+  width: var(--space-one-point-five);
+  display: inline-block;
+  background-color: var(--white);
+  box-shadow: rgb(255, 255, 255) 0px 0px 0px 0px,
+    rgba(59, 130, 246, 0.5) 0px 0px 0px 0px, rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
+    rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+  transform: translate(0, 0);
+  border-radius: 100%;
+  transition-property: transform;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 200ms;
+}
+.toggle-button span.active {
+  transform: translate(var(--space-one-point-five), var(--space-zero));
+}
+.disabled-text {
+  font-size: var(--font-size-small);
+  color: var(--s-500);
 }
 </style>
