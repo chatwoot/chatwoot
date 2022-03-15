665c1c0034440a5591962e4063d02c252287bc5f
Pranav <pranav@chatwoot.com>
2022-03-15 16:50:33 +0530

    Fix display issues in smaller screens

--- app/javascript/dashboard/App.vue
+++ app/javascript/dashboard/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app" class="app-wrapper app-root">
-    <banner
-      v-if="shouldShowBanner"
-      class="update-banner"
-      color-scheme="primary"
-      :banner-message="bannerMessage"
-      href-link="https://github.com/chatwoot/chatwoot/releases"
-      :href-link-text="$t('GENERAL_SETTINGS.LEARN_MORE')"
-      has-close-button
-      @close="dismissUpdateBanner"
-    />
+    <update-banner :latest-chatwoot-version="latestChatwootVersion" />
     <transition name="fade" mode="out-in">
       <router-view></router-view>
     </transition>
@@ -23,26 +14,21 @@
 </template>
 
 <script>
-/* eslint-disable prettier/prettier */
-import { mapGetters } from 'vuex';
-import Banner from 'dashboard/components/ui/Banner.vue';
-import AddAccountModal from '../dashboard/components/layout/sidebarComponents/AddAccountModal';
-import WootSnackbarBox from './components/SnackbarContainer';
-import LocalStorage from './helper/localStorage';
-import NetworkNotification from './components/NetworkNotification';
 import { accountIdFromPathname } from './helper/URLHelper';
-const semver = require('semver');
-
-const dismissedUpdates = new LocalStorage('dismissedUpdates');
+import { mapGetters } from 'vuex';
+import AddAccountModal from '../dashboard/components/layout/sidebarComponents/AddAccountModal';
+import NetworkNotification from './components/NetworkNotification';
+import UpdateBanner from './components/app/UpdateBanner.vue';
+import WootSnackbarBox from './components/SnackbarContainer';
 
 export default {
   name: 'App',
 
   components: {
-    WootSnackbarBox,
     AddAccountModal,
     NetworkNotification,
-    Banner,
+    UpdateBanner,
+    WootSnackbarBox,
   },
 
   data() {
@@ -63,30 +49,6 @@ export default {
         this.currentUser &&
         this.currentUser.accounts &&
         this.currentUser.accounts.length !== 0
-      );
-    },
-    hasAnUpdateAvailable() {
-      if (!semver.valid(this.latestChatwootVersion)) {
-        return false;
-      }
-      return (
-        semver.lt(this.globalConfig.appVersion, this.latestChatwootVersion) &&
-        this.checkUpdateDismissedOrNot(this.latestChatwootVersion)
-      );
-    },
-    bannerMessage() {
-      return this.$t('GENERAL_SETTINGS.UPDATE_CHATWOOT', {
-        latestChatwootVersion: this.latestChatwootVersion,
-      });
-    },
-    isUserAdmin() {
-      return this.currentUser.role === 'administrator';
-    },
-    shouldShowBanner() {
-      return (
-        this.globalConfig.displayManifest &&
-        this.hasAnUpdateAvailable &&
-        this.isUserAdmin
       );
     },
   },
@@ -115,21 +77,13 @@ export default {
 
       if (accountId) {
         await this.$store.dispatch('accounts/get');
-        const { locale, latest_chatwoot_version: latestChatwootVersion } =
-          this.getAccount(accountId);
+        const {
+          locale,
+          latest_chatwoot_version: latestChatwootVersion,
+        } = this.getAccount(accountId);
         this.setLocale(locale);
         this.latestChatwootVersion = latestChatwootVersion;
       }
-    },
-    checkUpdateDismissedOrNot(version) {
-      return !dismissedUpdates.get().includes(version);
-    },
-    dismissUpdateBanner() {
-      const updatedDismissedItems = dismissedUpdates.get();
-      updatedDismissedItems.push(this.latestChatwootVersion);
-      dismissedUpdates.store(updatedDismissedItems);
-      this.showUpdateBanner = false;
-      this.latestChatwootVersion = this.globalConfig.appVersion;
     },
   },
 };
