b5baba4847dd1de2036c2d2b5fa81a982d77c4da
Tejaswini <tejaswini@chatwoot.com>
2022-03-17 18:22:32 +0530

    Feat: Add contact based conditions on automation

--- app/services/automation_rules/conditions_filter_service.rb
+++ app/services/automation_rules/conditions_filter_service.rb
@@ -11,10 +11,17 @@ class AutomationRules::ConditionsFilterService < FilterService
 
   def perform
     conversation_filters = @filters['conversations']
+    contact_filters = @filters['contacts']
 
     @rule.conditions.each_with_index do |query_hash, current_index|
-      current_filter = conversation_filters[query_hash['attribute_key']]
-      @query_string += conversation_query_string(current_filter, query_hash.with_indifferent_access, current_index)
+      conversation_filter = conversation_filters[query_hash['attribute_key']]
+      contact_filter = contact_filters[query_hash['attribute_key']]
+
+      if conversation_filter
+        @query_string += query_string('conversations', conversation_filter, query_hash.with_indifferent_access, current_index)
+      elsif contact_filter
+        @query_string += query_string('contacts', contact_filter, query_hash.with_indifferent_access, current_index)
+      end
     end
 
     records = base_relation.where(@query_string, @filter_values.with_indifferent_access)
@@ -57,7 +64,7 @@ class AutomationRules::ConditionsFilterService < FilterService
     records.any?
   end
 
-  def conversation_query_string(current_filter, query_hash, current_index)
+  def query_string(table_name, current_filter, query_hash, current_index)
     attribute_key = query_hash['attribute_key']
     query_operator = query_hash['query_operator']
 
@@ -65,17 +72,17 @@ class AutomationRules::ConditionsFilterService < FilterService
 
     case current_filter['attribute_type']
     when 'additional_attributes'
-      " conversations.additional_attributes ->> '#{attribute_key}' #{filter_operator_value} #{query_operator} "
+      " #{table_name}.additional_attributes ->> '#{attribute_key}' #{filter_operator_value} #{query_operator} "
     when 'standard'
       if attribute_key == 'labels'
         " tags.id #{filter_operator_value} #{query_operator} "
       else
-        " conversations.#{attribute_key} #{filter_operator_value} #{query_operator} "
+        " #{table_name}.#{attribute_key} #{filter_operator_value} #{query_operator} "
       end
     end
   end
 
   def base_relation
-    Conversation.where(id: @conversation.id)
+    Conversation.where(id: @conversation.id).joins('LEFT OUTER JOIN contacts on conversations.contact_id = contacts.id')
   end
 end
