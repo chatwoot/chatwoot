8296e7668543608644c808d4a01b6e0b7cbcdc16
Pranav Raj S <pranav@chatwoot.com>
2022-03-18 18:40:23 +0530

    Remove contactConversations

--- app/javascript/dashboard/store/modules/contactConversations.js
+++ app/javascript/dashboard/store/modules/contactConversations.js
@@ -66,14 +66,6 @@ export const actions = {
         id: contactId,
         data: response.data.payload,
       });
-      commit(
-        types.default.SET_ALL_CONVERSATION,
-        {},
-        {
-          root: true,
-        }
-      );
-
       commit(types.default.SET_CONTACT_CONVERSATIONS_UI_FLAG, {
         isFetching: false,
       });
