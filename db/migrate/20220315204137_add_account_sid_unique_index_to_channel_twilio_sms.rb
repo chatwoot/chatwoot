a39f3ab90baefe5712b88b662b8886c9004fa8f3
Jordan Brough <git.j@brgh.net>
2022-03-22 08:41:01 -0600

    Add warning message if duplicates are found

--- db/migrate/20220315204137_add_account_sid_unique_index_to_channel_twilio_sms.rb
+++ db/migrate/20220315204137_add_account_sid_unique_index_to_channel_twilio_sms.rb
@@ -1,5 +1,21 @@
 class AddAccountSidUniqueIndexToChannelTwilioSms < ActiveRecord::Migration[6.1]
   def change
+    has_duplicates = Channel::TwilioSms
+      .select(1)
+      .where.not(account_sid: nil)
+      .where.not(phone_number: nil)
+      .group(:account_sid, :phone_number)
+      .having('count(*) > 1')
+      .exists?
+
+    if has_duplicates
+      raise <<~ERR.squish
+        ERROR: You have duplicate values for account_sid + phone_number in "channel_twilio_sms".
+        This causes Twilio account lookup to behave unpredictably. Please eliminate the duplicates
+        and then re-run this migration.
+      ERR
+    end
+
     add_index :channel_twilio_sms, [:account_sid, :phone_number], unique: true
   end
 end
