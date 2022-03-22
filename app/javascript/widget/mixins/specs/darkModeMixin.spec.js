27e5eb706421d63c42a4bba76577d55dbc3daffa
iamsivin <iamsivin@gmail.com>
2022-03-22 12:40:28 +0530

    Adds tests for dark mode mixin

---
+++ app/javascript/widget/mixins/specs/darkModeMixin.spec.js
@@ -0,0 +1,57 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import darkModeMixin from '../darkModeMixin';
+import Vuex from 'vuex';
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const darkModeValues = ['light', 'dark', 'auto'];
+
+describe('darkModeMixin', () => {
+  let getters;
+  let store;
+  beforeEach(() => {
+    getters = {
+      'appConfig/darkMode': () => darkModeValues[0],
+    };
+    store = new Vuex.Store({ getters });
+  });
+
+  it('if light theme', () => {
+    const Component = {
+      render() {},
+      mixins: [darkModeMixin],
+    };
+    const wrapper = shallowMount(Component, { store, localVue });
+    expect(
+      wrapper.vm.isDarkOrWhiteOrAutoMode({
+        dark: 'bg-600',
+        light: 'bg-100',
+      })
+    ).toBe('bg-100');
+  });
+
+  it('if dark theme', () => {
+    const Component = {
+      render() {},
+      mixins: [darkModeMixin],
+    };
+    const wrapper = shallowMount(Component, { store, localVue });
+    expect(
+      wrapper.vm.isDarkOrWhiteOrAutoMode({
+        dark: 'bg-600',
+        light: 'bg-100',
+      })
+    ).toBe('bg-100');
+  });
+
+  it('if auto theme', () => {
+    const Component = {
+      render() {},
+      mixins: [darkModeMixin],
+    };
+    const wrapper = shallowMount(Component, { store, localVue });
+    expect(
+      wrapper.vm.isDarkOrWhiteOrAutoMode({ dark: 'bg-600', light: 'bg-100' })
+    ).toBe('bg-100');
+  });
+});
