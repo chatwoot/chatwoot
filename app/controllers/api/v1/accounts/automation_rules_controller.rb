370cfa82be345e2ba6112f964932b69a14ff6947
Tejaswini <tejaswini@chatwoot.com>
2022-03-23 23:38:50 +0530

    Feat: Added automation for attachments

--- app/controllers/api/v1/accounts/automation_rules_controller.rb
+++ app/controllers/api/v1/accounts/automation_rules_controller.rb
@@ -7,7 +7,6 @@ class Api::V1::Accounts::AutomationRulesController < Api::V1::Accounts::BaseCont
   end
 
   def create
-    binding.pry
     @automation_rule = Current.account.automation_rules.create(automation_rules_permit)
   end
 
