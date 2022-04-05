13ae48db21b3f2b4369714e2e93e661524217042
fayazara <fayazara@gmail.com>
2022-04-05 15:02:34 +0530

    Fix codeclimate issues

--- app/javascript/dashboard/helper/actionQueryGenerator.js
+++ app/javascript/dashboard/helper/actionQueryGenerator.js
@@ -1,14 +1,19 @@
+const formatArray = params => {
+  if (params.length <= 0) {
+    params = [];
+  } else if (params.every(elem => typeof elem === 'string')) {
+    params = [...params];
+  } else {
+    params = params.map(val => val.id);
+  }
+  return params;
+};
+
 const generatePayload = data => {
   const actions = JSON.parse(JSON.stringify(data));
   let payload = actions.map(item => {
     if (Array.isArray(item.action_params)) {
-      if (item.action_params.length <= 0) {
-        item.action_params = [];
-      } else if (item.action_params.every(elem => typeof elem === 'string')) {
-        item.action_params = [...item.action_params];
-      } else {
-        item.action_params = item.action_params.map(val => val.id);
-      }
+      item.action_params = formatArray(item.action_params);
     } else if (typeof item.values === 'object') {
       item.action_params = [item.action_params.id];
     } else if (!item.action_params) {
