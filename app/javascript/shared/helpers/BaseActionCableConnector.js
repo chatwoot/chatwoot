e8aef31b6d05a7c9cb1df223ec45019cb889c41c
Muhsin Keloth <muhsinkeramam@gmail.com>
2022-04-06 15:19:29 +0530

    Remove logs

--- app/javascript/shared/helpers/BaseActionCableConnector.js
+++ app/javascript/shared/helpers/BaseActionCableConnector.js
@@ -41,8 +41,6 @@ class BaseActionCableConnector {
   }
 
   onReceived = ({ event, data } = {}) => {
-    console.log('event, data', event, data);
-
     if (this.isAValidEvent(data)) {
       if (this.events[event] && typeof this.events[event] === 'function') {
         this.events[event](data);
