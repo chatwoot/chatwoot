f7b8ce48945363fc38c841d33d53b81bf50375ba
Muhsin Keloth <muhsinkeramam@gmail.com>
2022-04-08 14:53:37 +0530

    feat: Add `phone_number` & `custom_attributes` in create conversation end point (#4421)

--- app/controllers/api/v1/widget/conversations_controller.rb
+++ app/controllers/api/v1/widget/conversations_controller.rb
@@ -7,10 +7,16 @@ class Api::V1::Widget::ConversationsController < Api::V1::Widget::BaseController
 
   def create
     ActiveRecord::Base.transaction do
-      update_contact(contact_email) if @contact.email.blank? && contact_email.present?
+      process_update_contact
       @conversation = create_conversation
       conversation.messages.create(message_params)
     end
+  end
+
+  def process_update_contact
+    update_contact(contact_email) if @contact.email.blank? && contact_email.present?
+    update_contact_phone_number(contact_phone_number) if @contact.phone_number.blank? && contact_phone_number.present?
+    @contact.update!(name: contact_name) if contact_name.present?
   end
 
   def update_last_seen
@@ -63,6 +69,7 @@ class Api::V1::Widget::ConversationsController < Api::V1::Widget::BaseController
   end
 
   def permitted_params
-    params.permit(:id, :typing_status, :website_token, :email, contact: [:name, :email], message: [:content, :referer_url, :timestamp, :echo_id])
+    params.permit(:id, :typing_status, :website_token, :email, contact: [:name, :email], message: [:content, :referer_url, :timestamp, :echo_id],
+                                                               custom_attributes: {})
   end
 end
