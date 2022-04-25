5ca7d48cfd59472f734cc188c58f8017bfd5822a
Pranav Raj S <pranav@chatwoot.com>
2022-04-25 17:44:42 +0530

    feat: Add event subscription option to webhooks (#4540)
    Co-authored-by: Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>

--- app/listeners/webhook_listener.rb
+++ app/listeners/webhook_listener.rb
@@ -1,22 +1,4 @@
 class WebhookListener < BaseListener
-  # FIXME: deprecate the opened and resolved events in future in favor of status changed event.
-  def conversation_resolved(event)
-    conversation = extract_conversation_and_account(event)[0]
-    changed_attributes = extract_changed_attributes(event)
-    inbox = conversation.inbox
-    payload = conversation.webhook_data.merge(event: __method__.to_s, changed_attributes: changed_attributes)
-    deliver_webhook_payloads(payload, inbox)
-  end
-
-  # FIXME: deprecate the opened and resolved events in future in favor of status changed event.
-  def conversation_opened(event)
-    conversation = extract_conversation_and_account(event)[0]
-    changed_attributes = extract_changed_attributes(event)
-    inbox = conversation.inbox
-    payload = conversation.webhook_data.merge(event: __method__.to_s, changed_attributes: changed_attributes)
-    deliver_webhook_payloads(payload, inbox)
-  end
-
   def conversation_status_changed(event)
     conversation = extract_conversation_and_account(event)[0]
     changed_attributes = extract_changed_attributes(event)
@@ -71,15 +53,23 @@ class WebhookListener < BaseListener
 
   private
 
-  def deliver_webhook_payloads(payload, inbox)
-    # Account webhooks
+  def deliver_account_webhooks(payload, inbox)
     inbox.account.webhooks.account.each do |webhook|
+      next unless webhook.subscriptions.include?(payload[:event])
+
       WebhookJob.perform_later(webhook.url, payload)
     end
+  end
 
+  def deliver_api_inbox_webhooks(payload, inbox)
     return unless inbox.channel_type == 'Channel::Api'
     return if inbox.channel.webhook_url.blank?
 
     WebhookJob.perform_later(inbox.channel.webhook_url, payload)
+  end
+
+  def deliver_webhook_payloads(payload, inbox)
+    deliver_account_webhooks(payload, inbox)
+    deliver_api_inbox_webhooks(payload, inbox)
   end
 end
