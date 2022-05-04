896f2fa9e4f885b3060dc7e52f6cfb20406e0863
Sojan <sojan@pepalo.com>
2022-05-05 00:10:53 +0530

    chore: sentry fix

--- spec/models/inbox_member_spec.rb
+++ spec/models/inbox_member_spec.rb
@@ -9,13 +9,11 @@ RSpec.describe InboxMember do
     let(:inbox_member) { create(:inbox_member) }
 
     # ref: https://github.com/chatwoot/chatwoot/issues/4616
-    context 'when parent inbox is destroyed' do 
+    context 'when parent inbox is destroyed' do
       it 'enques and processes DestroyAssociationAsyncJob' do
-
         perform_enqueued_jobs do
           inbox_member.inbox.destroy!
         end
-  
       end
     end
   end
