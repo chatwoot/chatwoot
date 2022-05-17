ceb10d0623900f3dc4ad4ac54e338b8b0e5bfd3a
Pranav Raj S <pranav@chatwoot.com>
2022-05-17 22:58:39 +0530

    fix: Update report method to fix issues in characters

--- app/javascript/dashboard/helper/specs/downloadHelper.spec.js
+++ app/javascript/dashboard/helper/specs/downloadHelper.spec.js
@@ -1,24 +1,4 @@
-import { downloadCsvFile, generateFileName } from '../downloadHelper';
-
-const fileName = 'test.csv';
-const fileData = `Agent name,Conversations count,Avg first response time (Minutes),Avg resolution time (Minutes)
-Pranav,36,114,28411`;
-
-describe('#downloadCsvFile', () => {
-  it('should download the csv file', () => {
-    const link = {
-      click: jest.fn(),
-    };
-    jest.spyOn(document, 'createElement').mockImplementation(() => link);
-
-    downloadCsvFile(fileName, fileData);
-    expect(link.download).toEqual(fileName);
-    expect(link.href).toEqual(
-      `data:text/csv;charset=utf-8,${encodeURI(fileData)}`
-    );
-    expect(link.click).toHaveBeenCalledTimes(1);
-  });
-});
+import { generateFileName } from '../downloadHelper';
 
 describe('#generateFileName', () => {
   it('should generate the correct file name', () => {
