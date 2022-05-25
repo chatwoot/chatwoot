4595e4bc530c80b864ada29b170a36cba395dd15
Sojan Jose <sojan@chatwoot.com>
2022-05-25 14:19:01 +0530

    chore: rubocop fix

--- config/initializers/actioncable.rb
+++ config/initializers/actioncable.rb
@@ -2,7 +2,7 @@
 
 require 'action_cable/subscription_adapter/redis'
 
-ActionCable::SubscriptionAdapter::Redis.redis_connector = ->(config) do
+ActionCable::SubscriptionAdapter::Redis.redis_connector = lambda do |config|
   config[:id] = nil
   ::Redis.new(config.except(:adapter, :channel_prefix))
 end
