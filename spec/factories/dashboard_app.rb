a0afff91daddebd093ef720dcc1fe7ebbc4573ff
Pranav Raj S <pranav@chatwoot.com>
2022-06-01 11:13:10 +0530

    feat: Allow users to create dashboard apps to give agents more context (#4761)

---
+++ spec/factories/dashboard_app.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :dashboard_app do
+    sequence(:title) { |n| "Dashboard App #{n}" }
+    content { [{ type: 'frame', url: 'https://chatwoot.com' }] }
+    user
+    account
+  end
+end
