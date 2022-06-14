c5c1d5db458285692d1c97e4000223beb2a3ad92
Pranav Raj S <pranav@chatwoot.com>
2022-06-14 14:47:42 +0530

    Fix specs

--- spec/factories/channel/channel_api.rb
+++ spec/factories/channel/channel_api.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :channel_api, class: 'Channel::Api' do
     webhook_url { 'http://example.com' }
     account
-    additional_attributes { additional_attributes }
     after(:create) do |channel_api|
       create(:inbox, channel: channel_api, account: channel_api.account)
     end
