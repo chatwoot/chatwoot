d310ba116271e4fe929e82059f68fb6202dafe06
Aswin Dev P.S <aswindevps@gmail.com>
2022-06-17 12:24:58 +0530

    Fix smtp sentry issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_10_091206) do
+ActiveRecord::Schema.define(version: 2022_06_16_154502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
