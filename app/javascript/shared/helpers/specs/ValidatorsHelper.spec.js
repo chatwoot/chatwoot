adceea9b520473f0bddf5a04bd5d3e3e6b6e62d3
iamsivin <iamsivin@gmail.com>
2022-06-22 10:10:32 +0530

    chore: Adds specs

--- app/javascript/shared/helpers/specs/ValidatorsHelper.spec.js
+++ app/javascript/shared/helpers/specs/ValidatorsHelper.spec.js
@@ -1,5 +1,6 @@
 import { shouldBeUrl } from '../Validators';
 import { isValidPassword } from '../Validators';
+import { isValidEmail } from '../Validators';
 
 describe('#shouldBeUrl', () => {
   it('should return correct url', () => {
@@ -20,5 +21,19 @@ describe('#isValidPassword', () => {
     expect(isValidPassword('testPass4')).toEqual(false);
     expect(isValidPassword('testpass4!')).toEqual(false);
     expect(isValidPassword('testPass!')).toEqual(false);
+  });
+});
+
+describe('isValidEmail', () => {
+  it('should return correct email', () => {
+    expect(isValidEmail('test@mail.com')).toEqual(true);
+    expect(isValidEmail('test@chatwoot.co')).toEqual(true);
+  });
+
+  it('should return wrong email', () => {
+    expect(isValidEmail('test@mail')).toEqual(false);
+    expect(isValidEmail('test@mail.')).toEqual(false);
+    expect(isValidEmail('test@mail.c')).toEqual(false);
+    expect(isValidEmail('.test@mail.c0')).toEqual(false);
   });
 });
