62a9edbcc5e1bd18cc362c8c4cd377611e8f68f4
Sojan <sojan@pepalo.com>
2022-06-29 15:57:26 +0200

    chore: CodeClimate

--- app/jobs/webhooks/whatsapp_events_job.rb
+++ app/jobs/webhooks/whatsapp_events_job.rb
@@ -2,15 +2,22 @@ class Webhooks::WhatsappEventsJob < ApplicationJob
   queue_as :default
 
   def perform(params = {})
+    channel = find_channel(params)
+    return if channel.blank?
+
+    case channel.provider
+    when 'whatsapp_cloud'
+      Whatsapp::IncomingMessageWhatsappCloudService.new(channel: channel, params: params).perform
+    else
+      Whatsapp::IncomingMessageWhatsappService.new(channel: channel, params: params).perform
+    end
+  end
+
+  private
+
+  def find_channel(params)
     return unless params[:phone_number]
 
-    channel = Channel::Whatsapp.find_by(phone_number: params[:phone_number])
-    return unless channel
-
-    if channel.provider == 'whatsapp_cloud'
-      Whatsapp::IncomingMessageWhatsappCloudService.new(inbox: channel.inbox, params: params['whatsapp'].with_indifferent_access).perform
-    else
-      Whatsapp::IncomingMessageService.new(inbox: channel.inbox, params: params['whatsapp'].with_indifferent_access).perform
-    end
+    Channel::Whatsapp.find_by(phone_number: params[:phone_number])
   end
 end
