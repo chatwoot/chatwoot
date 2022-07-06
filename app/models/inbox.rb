1079598763dfca6857afc782bbfdb9f5ad2141db
Sojan Jose <sojan@pepalo.com>
2022-07-06 21:45:03 +0200

    feat: Support for Whatsapp Cloud API (#4938)
    Ability to configure Whatsapp Cloud API Inboxes
    
    fixes: #4712

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -120,6 +120,8 @@ class Inbox < ApplicationRecord
       "#{ENV['FRONTEND_URL']}/webhooks/sms/#{channel.phone_number.delete_prefix('+')}"
     when 'Channel::Line'
       "#{ENV['FRONTEND_URL']}/webhooks/line/#{channel.line_channel_id}"
+    when 'Channel::Whatsapp'
+      "#{ENV['FRONTEND_URL']}/webhooks/whatsapp/#{channel.phone_number}"
     end
   end
 
