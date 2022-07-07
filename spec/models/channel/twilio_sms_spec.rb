4f81d1f2f47bb2d0a33f12f921e9f75b364566ac
Sojan Jose <sojan@chatwoot.com>
2022-07-07 20:24:55 +0200

    chore: fix spec

--- spec/models/channel/twilio_sms_spec.rb
+++ spec/models/channel/twilio_sms_spec.rb
@@ -6,19 +6,19 @@ RSpec.describe Channel::TwilioSms do
   describe '#has_24_hour_messaging_window?' do
     context 'with medium whatsapp' do
       let!(:whatsapp_channel) { create(:channel_twilio_sms, medium: :whatsapp) }
-
+  
       it 'returns true' do
-        expect(whatsapp_channel.has_24_hour_messaging_window?).to eq true
+        expect(whatsapp_channel.messaging_window_enabled?).to eq true
         expect(whatsapp_channel.name).to eq 'Whatsapp'
         expect(whatsapp_channel.medium).to eq 'whatsapp'
       end
     end
-
+  
     context 'with medium sms' do
       let!(:sms_channel) { create(:channel_twilio_sms, medium: :sms) }
-
+  
       it 'returns false' do
-        expect(sms_channel.has_24_hour_messaging_window?).to eq false
+        expect(sms_channel.messaging_window_enabled?).to eq false
         expect(sms_channel.name).to eq 'Twilio SMS'
         expect(sms_channel.medium).to eq 'sms'
       end
