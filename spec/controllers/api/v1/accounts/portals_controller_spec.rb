7d998bef2478ea78c1c27b8483e92cd362ab189e
tejaswini chile <tejaswini@chatwoot.com>
2022-07-07 23:49:41 +0530

    feat: Added portal logo and the updated the JSON

--- spec/controllers/api/v1/accounts/portals_controller_spec.rb
+++ spec/controllers/api/v1/accounts/portals_controller_spec.rb
@@ -67,11 +67,14 @@ RSpec.describe 'Api::V1::Accounts::Portals', type: :request do
 
     context 'when it is an authenticated user' do
       it 'creates portal' do
+        file = fixture_file_upload(Rails.root.join('spec/assets/avatar.png'), 'image/png')
+
         portal_params = {
           portal: {
             name: 'test_portal',
             slug: 'test_kbase'
-          }
+          },
+          logo: file
         }
         post "/api/v1/accounts/#{account.id}/portals",
              params: portal_params,
@@ -80,6 +83,7 @@ RSpec.describe 'Api::V1::Accounts::Portals', type: :request do
         expect(response).to have_http_status(:success)
         json_response = JSON.parse(response.body)
         expect(json_response['name']).to eql('test_portal')
+        expect(json_response['logo']['filename']).to eql('avatar.png')
       end
     end
   end
