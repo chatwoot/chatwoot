a8839e3eb7de83d85d4d31290aa82659546cffd4
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2022-07-08 14:25:32 +0530

    feat: Add the ability to create dashboard apps from the UI (#4924)
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>

---
+++ app/javascript/dashboard/routes/dashboard/settings/integrations/DashboardApps/DashboardAppsRow.vue
@@ -0,0 +1,45 @@
+<template>
+  <tr>
+    <td>{{ app.title }}</td>
+    <td>{{ app.content[0].url }}</td>
+    <td class="button-wrapper">
+      <woot-button
+        v-tooltip.top="
+          $t('INTEGRATION_SETTINGS.DASHBOARD_APPS.LIST.EDIT_TOOLTIP')
+        "
+        variant="smooth"
+        size="tiny"
+        color-scheme="secondary"
+        class-names="grey-btn"
+        icon="edit"
+        @click="$emit('edit', app)"
+      />
+      <woot-button
+        v-tooltip.top="
+          $t('INTEGRATION_SETTINGS.DASHBOARD_APPS.LIST.DELETE_TOOLTIP')
+        "
+        variant="smooth"
+        color-scheme="alert"
+        size="tiny"
+        icon="dismiss-circle"
+        class-names="grey-btn"
+        @click="$emit('delete', app)"
+      />
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    app: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+};
+</script>
