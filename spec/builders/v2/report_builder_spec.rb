032e0b118e59090aece931d7eefbd3612bda64b7
Sojan <sojan@pepalo.com>
2022-07-12 21:49:01 +0200

    chore: fix specs

--- spec/builders/v2/report_builder_spec.rb
+++ spec/builders/v2/report_builder_spec.rb
@@ -9,24 +9,23 @@ describe ::V2::ReportBuilder do
   let!(:label_1) { create(:label, title: 'Label_1', account: account) }
   let!(:label_2) { create(:label, title: 'Label_2', account: account) }
 
-
   describe '#timeseries' do
     before do
-      gravatar_url = "https://www.gravatar.com"
+      gravatar_url = 'https://www.gravatar.com'
       stub_request(:get, /#{gravatar_url}.*/).to_return(status: 404)
 
       perform_enqueued_jobs do
         10.times do
           conversation = create(:conversation, account: account,
-                                              inbox: inbox, assignee: user,
-                                              created_at: Time.zone.today)
+                                               inbox: inbox, assignee: user,
+                                               created_at: Time.zone.today)
           create_list(:message, 5, message_type: 'outgoing',
-                                  account: account, inbox: inbox,
-                                  conversation: conversation, created_at: Time.zone.today + 2.hours)
+                                   account: account, inbox: inbox,
+                                   conversation: conversation, created_at: Time.zone.today + 2.hours)
           create_list(:message, 2, message_type: 'incoming',
-                                  account: account, inbox: inbox,
-                                  conversation: conversation,
-                                  created_at: Time.zone.today + 3.hours)
+                                   account: account, inbox: inbox,
+                                   conversation: conversation,
+                                   created_at: Time.zone.today + 3.hours)
           conversation.update_labels('label_1')
           conversation.label_list
           conversation.save!
@@ -34,16 +33,16 @@ describe ::V2::ReportBuilder do
 
         5.times do
           conversation = create(:conversation, account: account,
-                                              inbox: inbox, assignee: user,
-                                              created_at: (Time.zone.today - 2.days))
+                                               inbox: inbox, assignee: user,
+                                               created_at: (Time.zone.today - 2.days))
           create_list(:message, 3, message_type: 'outgoing',
-                                  account: account, inbox: inbox,
-                                  conversation: conversation,
-                                  created_at: (Time.zone.today - 2.days))
+                                   account: account, inbox: inbox,
+                                   conversation: conversation,
+                                   created_at: (Time.zone.today - 2.days))
           create_list(:message, 1, message_type: 'incoming',
-                                  account: account, inbox: inbox,
-                                  conversation: conversation,
-                                  created_at: (Time.zone.today - 2.days))
+                                   account: account, inbox: inbox,
+                                   conversation: conversation,
+                                   created_at: (Time.zone.today - 2.days))
           conversation.update_labels('label_2')
           conversation.label_list
           conversation.save!
