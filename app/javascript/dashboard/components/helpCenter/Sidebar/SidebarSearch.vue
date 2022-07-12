f4cab86a708e2070ee7b0b97b1d8995e3b1ffb36
iamsivin <iamsivin@gmail.com>
2022-07-12 10:25:12 +0530

    feat: New sidebar component for help-center

---
+++ app/javascript/dashboard/components/helpCenter/Sidebar/SidebarSearch.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="search-input--wrap">
+    <div class="search-icon--wrap">
+      <fluent-icon icon="search" class="search-icon" />
+    </div>
+    <input
+      v-model="searchValue"
+      class="search-input"
+      placeholder="Search for articles"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    searchValue: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.search-input--wrap {
+  display: flex;
+  padding: var(--space-small) var(--space-zero);
+  width: 100%;
+}
+
+.search-input {
+  width: 100%;
+  height: 3.6rem;
+  border-radius: var(--border-radius-normal);
+  background: var(--s-50);
+  font-size: var(--font-size-small);
+  padding: var(--space-small) var(--space-small) var(--space-small)
+    var(--space-large);
+  border: 1px solid var(--s-100);
+
+  &:focus {
+    border-color: var(--w-500);
+  }
+}
+
+.search-icon--wrap {
+  position: relative;
+}
+
+.search-icon {
+  position: absolute;
+  color: var(--s-200);
+  top: var(--space-small);
+  left: var(--space-small);
+}
+</style>
