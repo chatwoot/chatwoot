d7a3342a15c4f9168b48443146a60b4b4d34b41c
Muhsin Keloth <muhsinkeramam@gmail.com>
2022-07-20 00:21:00 +0530

    Complete actions

--- app/javascript/dashboard/store/modules/helpCenterArticles/actions.js
+++ app/javascript/dashboard/store/modules/helpCenterArticles/actions.js
@@ -1,25 +1,88 @@
 import helpCenterArticlesAPI from 'dashboard/api/helpcenter/article.js';
 import types from '../../mutation-types';
 export const actions = {
-  fetchAllArticles: async ({ commit }) => {
+  get: async ({ commit }) => {
     try {
       commit(types.SET_UI_FLAG, { isFetching: true });
       const { data } = await helpCenterArticlesAPI.get();
 
-      data.payload.forEach(helpCenter => {
-        const { id: helpCenterId } = helpCenter;
+      data.payload.forEach(article => {
+        const { id: articleId } = article;
 
-        commit(types.ADD_HELP_CENTER_ARTICLE, helpCenter);
-        commit(types.ADD_HELP_CENTER_ARTICLE_ID, helpCenterId);
+        commit(types.ADD_HELP_CENTER_ARTICLE, article);
+        commit(types.ADD_HELP_CENTER_ARTICLE_ID, articleId);
         commit(types.ADD_HELP_CENTER_ARTICLE_FLAG, {
           uiFlags: {},
-          helpCenterId,
+          articleId,
         });
       });
     } catch (error) {
       throw new Error(error);
     } finally {
       commit(types.SET_UI_FLAG, { isFetching: false });
+    }
+  },
+
+  create: async ({ commit }, params) => {
+    commit(types.SET_UI_FLAG, { isCreating: true });
+    try {
+      const { data } = await helpCenterArticlesAPI.create(params);
+      const { id: articleId } = data;
+      commit(types.ADD_HELP_CENTER_ARTICLE, data);
+      commit(types.ADD_HELP_CENTER_ARTICLE_ID, articleId);
+      return articleId;
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      commit(types.SET_UI_FLAG, { isCreating: false });
+    }
+  },
+  update: async ({ commit }, params) => {
+    const articleId = params.id;
+    commit(types.SET_UI_FLAG, {
+      uiFlags: {
+        isUpdating: true,
+      },
+      articleId,
+    });
+    try {
+      const { data } = await helpCenterArticlesAPI.update(params);
+
+      commit(types.UPDATE_HELP_CENTER_ARTICLE, data);
+
+      return articleId;
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      commit(types.SET_UI_FLAG, {
+        uiFlags: {
+          isUpdating: false,
+        },
+        articleId,
+      });
+    }
+  },
+  delete: async ({ commit }, articleId) => {
+    commit(types.SET_UI_FLAG, {
+      uiFlags: {
+        isDeleting: true,
+      },
+      articleId,
+    });
+    try {
+      await helpCenterArticlesAPI.delete(articleId);
+
+      commit(types.REMOVE_HELP_CENTER_ARTICLE, articleId);
+      commit(types.REMOVE_HELP_CENTER_ARTICLE_ID, articleId);
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      commit(types.SET_UI_FLAG, {
+        uiFlags: {
+          isDeleting: false,
+        },
+        articleId,
+      });
     }
   },
 };
--- app/javascript/dashboard/store/modules/helpCenterArticles/specs/action.spec.js
+++ app/javascript/dashboard/store/modules/helpCenterArticles/specs/action.spec.js
@@ -16,7 +16,7 @@ describe('#actions', () => {
   describe('#get', () => {
     it('sends correct actions if API is success', async () => {
       axios.get.mockResolvedValue({ data: { payload: articleList } });
-      await actions.fetchAllArticles({ commit });
+      await actions.get({ commit });
       expect(commit.mock.calls).toEqual([
         [types.default.SET_UI_FLAG, { isFetching: true }],
         [
@@ -30,17 +30,109 @@ describe('#actions', () => {
         [types.default.ADD_HELP_CENTER_ARTICLE_ID, 1],
         [
           types.default.ADD_HELP_CENTER_ARTICLE_FLAG,
-          { uiFlags: {}, helpCenterId: 1 },
+          { uiFlags: {}, articleId: 1 },
         ],
         [types.default.SET_UI_FLAG, { isFetching: false }],
       ]);
     });
     it('sends correct actions if API is error', async () => {
       axios.get.mockRejectedValue({ message: 'Incorrect header' });
-      await expect(actions.fetchAllArticles({ commit })).rejects.toThrow(Error);
+      await expect(actions.get({ commit })).rejects.toThrow(Error);
       expect(commit.mock.calls).toEqual([
         [types.default.SET_UI_FLAG, { isFetching: true }],
         [types.default.SET_UI_FLAG, { isFetching: false }],
+      ]);
+    });
+  });
+
+  describe('#create', () => {
+    it('sends correct actions if API is success', async () => {
+      axios.post.mockResolvedValue({ data: articleList[0] });
+      await actions.create({ commit }, articleList[0]);
+      expect(commit.mock.calls).toEqual([
+        [types.default.SET_UI_FLAG, { isCreating: true }],
+        [types.default.ADD_HELP_CENTER_ARTICLE, articleList[0]],
+        [types.default.ADD_HELP_CENTER_ARTICLE_ID, 1],
+        [types.default.SET_UI_FLAG, { isCreating: false }],
+      ]);
+    });
+    it('sends correct actions if API is error', async () => {
+      axios.post.mockRejectedValue({ message: 'Incorrect header' });
+      await expect(actions.create({ commit }, articleList[0])).rejects.toThrow(
+        Error
+      );
+      expect(commit.mock.calls).toEqual([
+        [types.default.SET_UI_FLAG, { isCreating: true }],
+        [types.default.SET_UI_FLAG, { isCreating: false }],
+      ]);
+    });
+  });
+
+  describe('#update', () => {
+    it('sends correct actions if API is success', async () => {
+      axios.patch.mockResolvedValue({ data: articleList[0] });
+      await actions.update({ commit }, articleList[0]);
+      expect(commit.mock.calls).toEqual([
+        [
+          types.default.SET_UI_FLAG,
+          { uiFlags: { isUpdating: true }, articleId: 1 },
+        ],
+        [types.default.UPDATE_HELP_CENTER_ARTICLE, articleList[0]],
+        [
+          types.default.SET_UI_FLAG,
+          { uiFlags: { isUpdating: false }, articleId: 1 },
+        ],
+      ]);
+    });
+    it('sends correct actions if API is error', async () => {
+      axios.patch.mockRejectedValue({ message: 'Incorrect header' });
+      await expect(actions.update({ commit }, articleList[0])).rejects.toThrow(
+        Error
+      );
+      expect(commit.mock.calls).toEqual([
+        [
+          types.default.SET_UI_FLAG,
+          { uiFlags: { isUpdating: true }, articleId: 1 },
+        ],
+        [
+          types.default.SET_UI_FLAG,
+          { uiFlags: { isUpdating: false }, articleId: 1 },
+        ],
+      ]);
+    });
+  });
+
+  describe('#delete', () => {
+    it('sends correct actions if API is success', async () => {
+      axios.delete.mockResolvedValue({ data: articleList[0] });
+      await actions.delete({ commit }, articleList[0].id);
+      expect(commit.mock.calls).toEqual([
+        [
+          types.default.SET_UI_FLAG,
+          { uiFlags: { isDeleting: true }, articleId: 1 },
+        ],
+        [types.default.REMOVE_HELP_CENTER_ARTICLE, articleList[0].id],
+        [types.default.REMOVE_HELP_CENTER_ARTICLE_ID, articleList[0].id],
+        [
+          types.default.SET_UI_FLAG,
+          { uiFlags: { isDeleting: false }, articleId: 1 },
+        ],
+      ]);
+    });
+    it('sends correct actions if API is error', async () => {
+      axios.delete.mockRejectedValue({ message: 'Incorrect header' });
+      await expect(
+        actions.delete({ commit }, articleList[0].id)
+      ).rejects.toThrow(Error);
+      expect(commit.mock.calls).toEqual([
+        [
+          types.default.SET_UI_FLAG,
+          { uiFlags: { isDeleting: true }, articleId: 1 },
+        ],
+        [
+          types.default.SET_UI_FLAG,
+          { uiFlags: { isDeleting: false }, articleId: 1 },
+        ],
       ]);
     });
   });
