8ec1cebca9acd7d37e613b8242d9a2ea8b6a3505
Tejaswini Chile <tejaswini@chatwoot.com>
2022-07-19 17:37:00 +0530

    feat: Macros CRUD api (#5047)

--- app/models/account.rb
+++ app/models/account.rb
@@ -40,7 +40,8 @@ class Account < ApplicationRecord
   has_many :agent_bots, dependent: :destroy_async
   has_many :api_channels, dependent: :destroy_async, class_name: '::Channel::Api'
   has_many :articles, dependent: :destroy_async, class_name: '::Article'
-  has_many :automation_rules, dependent: :destroy
+  has_many :automation_rules, dependent: :destroy_async
+  has_many :macros, dependent: :destroy_async
   has_many :campaigns, dependent: :destroy_async
   has_many :canned_responses, dependent: :destroy_async
   has_many :categories, dependent: :destroy_async, class_name: '::Category'
