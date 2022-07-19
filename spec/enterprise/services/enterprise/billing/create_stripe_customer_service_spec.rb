38b36675fc2d6937c42efd1fb44e2d6a2df8598e
Pranav Raj S <pranav@chatwoot.com>
2022-07-19 17:54:51 +0530

    Fix specs

--- spec/enterprise/services/enterprise/billing/create_stripe_customer_service_spec.rb
+++ spec/enterprise/services/enterprise/billing/create_stripe_customer_service_spec.rb
@@ -21,10 +21,30 @@ describe Enterprise::Billing::CreateStripeCustomerService do
       account.update!(custom_attributes: { stripe_customer_id: 'cus_random_number' })
 
       allow(Stripe::Customer).to receive(:create)
+      allow(Stripe::Subscription).to receive(:create)
+        .and_return(
+          {
+            plan: { id: 'price_random_number', product: 'prod_random_number' },
+            quantity: 2
+          }.with_indifferent_access
+        )
 
       create_stripe_customer_service.new(account: account).perform
 
       expect(Stripe::Customer).not_to have_received(:create)
+      expect(Stripe::Subscription)
+        .to have_received(:create)
+        .with({ customer: 'cus_random_number', items: [{ price: 'price_hacker_random', quantity: 2 }] })
+
+      expect(account.reload.custom_attributes).to eq(
+        {
+          stripe_customer_id: 'cus_random_number',
+          stripe_price_id: 'price_random_number',
+          stripe_product_id: 'prod_random_number',
+          subscribed_quantity: 2,
+          plan_name: 'A Plan Name'
+        }.with_indifferent_access
+      )
     end
 
     it 'calls stripe methods to create a customer and updates the account' do
