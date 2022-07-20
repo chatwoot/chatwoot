8a885be42d546a4b05d78db493f860a390144da7
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2022-07-20 14:04:03 +0530

    Update HelpCenterLayout.vue

--- app/javascript/dashboard/routes/dashboard/helpcenter/components/HelpCenterLayout.vue
+++ app/javascript/dashboard/routes/dashboard/helpcenter/components/HelpCenterLayout.vue
@@ -61,7 +61,7 @@ export default {
     ...mapGetters({
       accountId: 'getCurrentAccountId',
     }),
-    // dummy datas for testing
+    // For testing
     accessibleMenuItems() {
       return [
         {
