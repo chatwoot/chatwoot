60fe5e8b3c2e5e262a6100e9dd24979f189e74e5
Sojan <sojan@pepalo.com>
2022-07-21 17:56:59 +0200

    chore: Add rubocop

--- spec/enterprise/services/enterprise/billing/handle_stripe_event_service_spec.rb
+++ spec/enterprise/services/enterprise/billing/handle_stripe_event_service_spec.rb
@@ -2,35 +2,36 @@ require 'rails_helper'
 
 describe Enterprise::Billing::HandleStripeEventService do
   subject(:stripe_event_service) { described_class }
+
   let(:event) { double }
   let(:data) { double }
   let(:subscription) { double }
   let!(:account) { create(:account, custom_attributes: { stripe_customer_id: 'cus_123' }) }
-  
+
   before do
     allow(event).to receive(:data).and_return(data)
     allow(data).to receive(:object).and_return(subscription)
-    allow(subscription).to receive(:[]).with('plan').
-      and_return({
-        'id' => 'test', 'product' => 'plan_id', 'name' => 'plan_name'
-      })
+    allow(subscription).to receive(:[]).with('plan')
+                                       .and_return({
+                                                     'id' => 'test', 'product' => 'plan_id', 'name' => 'plan_name'
+                                                   })
     allow(subscription).to receive(:[]).with('quantity').and_return('10')
     allow(subscription).to receive(:customer).and_return('cus_123')
-    create(:installation_config, { 
-      name: 'CHATWOOT_CLOUD_PLANS', 
-      value: [
-        {
-          "name"=>"Hacker",
-          "product_id"=>["plan_id"],
-          "price_ids"=>["price_1"]
-        },
-        {
-          "name"=>"Startups",
-          "product_id"=>["plan_id_2"],
-          "price_ids"=>["price_2"]
-        }
-      ] 
-    }) 
+    create(:installation_config, {
+             name: 'CHATWOOT_CLOUD_PLANS',
+             value: [
+               {
+                 'name' => 'Hacker',
+                 'product_id' => ['plan_id'],
+                 'price_ids' => ['price_1']
+               },
+               {
+                 'name' => 'Startups',
+                 'product_id' => ['plan_id_2'],
+                 'price_ids' => ['price_2']
+               }
+             ]
+           })
   end
 
   describe '#perform' do
@@ -39,12 +40,12 @@ describe Enterprise::Billing::HandleStripeEventService do
       allow(subscription).to receive(:customer).and_return('cus_123')
       stripe_event_service.new.perform(event: event)
       expect(account.reload.custom_attributes).to eq({
-        'stripe_customer_id' => 'cus_123',
-        'stripe_price_id' => 'test',
-        'stripe_product_id' => 'plan_id',
-        'plan_name' => 'Hacker',
-        'subscribed_quantity' => '10'
-      })
+                                                       'stripe_customer_id' => 'cus_123',
+                                                       'stripe_price_id' => 'test',
+                                                       'stripe_product_id' => 'plan_id',
+                                                       'plan_name' => 'Hacker',
+                                                       'subscribed_quantity' => '10'
+                                                     })
     end
 
     it 'handles customer.subscription.deleted' do
