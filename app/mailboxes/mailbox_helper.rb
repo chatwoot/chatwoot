0e128bb938d5c951bf124f7a08772c459553ef6f
Developer <support@tiniyo.com>
2022-07-23 12:53:17 +0530

    chatwoot replaced with siprtc

--- app/mailboxes/mailbox_helper.rb
+++ app/mailboxes/mailbox_helper.rb
@@ -50,7 +50,7 @@ module MailboxHelper
 
   def notification_email_from_chatwoot?
     # notification emails are send via mailer sender email address. so it should match
-    @processed_mail.original_sender == Mail::Address.new(ENV.fetch('MAILER_SENDER_EMAIL', 'Chatwoot <accounts@chatwoot.com>')).address
+    @processed_mail.original_sender == Mail::Address.new(ENV.fetch('MAILER_SENDER_EMAIL', 'Chatwoot <admin@siprtc.io>')).address
   end
 
   def mail_content
