6295f5fd616ed8512a600792fdde5e853766dc20
Muhsin Keloth <muhsinkeramam@gmail.com>
2022-07-27 12:29:11 +0530

    feat: Add store to manage Articles (#5048)
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>

---
+++ app/javascript/dashboard/store/modules/helpCenterArticles/index.js
@@ -0,0 +1,30 @@
+import { getters } from './getters';
+import { actions } from './actions';
+import { mutations } from './mutations';
+
+export const defaultHelpCenterFlags = {
+  isFetching: false,
+  isUpdating: false,
+  isDeleting: false,
+};
+const state = {
+  articles: {
+    byId: {},
+    allIds: [],
+    uiFlags: {
+      byId: {},
+    },
+  },
+  uiFlags: {
+    allFetched: false,
+    isFetching: false,
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
