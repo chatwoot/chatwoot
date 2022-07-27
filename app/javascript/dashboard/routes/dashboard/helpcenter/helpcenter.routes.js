51af119c552b4dc21adff9d7aed27e6fe23d44ff
Muhsin Keloth <muhsinkeramam@gmail.com>
2022-07-27 13:08:27 +0530

    feat: Article list view page (#5122)

--- app/javascript/dashboard/routes/dashboard/helpcenter/helpcenter.routes.js
+++ app/javascript/dashboard/routes/dashboard/helpcenter/helpcenter.routes.js
@@ -14,10 +14,7 @@ const ListCategoryArticles = () =>
   import('./pages/articles/ListCategoryArticles');
 
 const ListAllArticles = () => import('./pages/articles/ListAllArticles');
-const ListArchivedArticles = () =>
-  import('./pages/articles/ListArchivedArticles');
-const ListDraftArticles = () => import('./pages/articles/ListDraftArticles');
-const ListMyArticles = () => import('./pages/articles/ListMyArticles');
+
 const NewArticle = () => import('./pages/articles/NewArticle');
 const EditArticle = () => import('./pages/articles/EditArticle');
 
@@ -56,30 +53,31 @@ const articleRoutes = [
     component: ListAllArticles,
   },
   {
+    path: getPortalRoute(':portalSlug/:locale/articles/new'),
+    name: 'new_article',
+    roles: ['administrator', 'agent'],
+    component: NewArticle,
+  },
+  {
+    path: getPortalRoute(':portalSlug/:locale/articles/mine'),
+    name: 'list_mine_articles',
+    roles: ['administrator', 'agent'],
+    component: ListAllArticles,
+  },
+  {
     path: getPortalRoute(':portalSlug/:locale/articles/archived'),
     name: 'list_archived_articles',
     roles: ['administrator', 'agent'],
-    component: ListArchivedArticles,
+    component: ListAllArticles,
   },
 
   {
     path: getPortalRoute(':portalSlug/:locale/articles/draft'),
     name: 'list_draft_articles',
     roles: ['administrator', 'agent'],
-    component: ListDraftArticles,
+    component: ListAllArticles,
   },
-  {
-    path: getPortalRoute(':portalSlug/:locale/articles/mine'),
-    name: 'list_mine_articles',
-    roles: ['administrator', 'agent'],
-    component: ListMyArticles,
-  },
-  {
-    path: getPortalRoute(':portalSlug/:locale/articles/new'),
-    name: 'new_article',
-    roles: ['administrator', 'agent'],
-    component: NewArticle,
-  },
+
   {
     path: getPortalRoute(':portalSlug/:locale/articles/:articleSlug'),
     name: 'edit_article',
