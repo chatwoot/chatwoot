5ec8e14ad32b58b42bf941c7aeabda8f530211a3
Aswin Dev P.S <aswindevps@gmail.com>
2022-08-01 17:20:56 +0530

    update input radio group

--- app/javascript/dashboard/routes/dashboard/settings/inbox/components/InputRadioGroup.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/components/InputRadioGroup.vue
@@ -3,13 +3,16 @@
     <label class="radio-group-label">{{ label }}</label>
     <div class="radio-group">
       <div v-for="item in items" :key="item.id" class="radio-group-item">
-        <input
-          :name="`${name} -radio-input`"
-          type="radio"
-          :checked="item.checked"
-          @change="action({ ...item, checked: true })"
-        />
-        <label>{{ item.title }}</label>
+        <label class="radio-group-item-label">
+          <input
+            class="radio-input"
+            :name="`${name} -radio-input`"
+            type="radio"
+            :checked="item.checked"
+            @change="action({ ...item, checked: true })"
+          />
+          <span>{{ item.title }}</span>
+        </label>
       </div>
     </div>
   </div>
@@ -40,12 +43,23 @@ export default {
 
 <style lang="scss" scoped>
 .radio-group-label {
-  margin-bottom: var(--space-small);
+  margin-bottom: var(--space-smaller);
 }
 .radio-group {
   display: inline-block;
+  margin-bottom: var(--space-small);
 }
 .radio-group-item {
   float: left;
+  margin-right: var(--space-one);
+  .radio-group-item-label {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    .radio-input {
+      margin: 0 var(--space-one) 0 0;
+    }
+  }
 }
 </style>
