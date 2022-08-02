581ce15564c0a07e1c0aceb03b798b15327b1465
fayazara <fayazara@gmail.com>
2022-08-02 16:18:10 +0530

    Udpate endpoints

--- app/javascript/dashboard/api/helpCenter/categories.js
+++ app/javascript/dashboard/api/helpCenter/categories.js
@@ -1,8 +1,24 @@
-import ApiClient from '../ApiClient';
+/* global axios */
 
-class CategoriesAPI extends ApiClient {
-  constructor() {
-    super('categories', { accountScoped: true });
+import PortalsAPI from './portals';
+
+class CategoriesAPI extends PortalsAPI {
+  get({ pageNumber, portalSlug, locale }) {
+    return axios.get(
+      `${this.url}/${portalSlug}/categories?page=${pageNumber}&locale=${locale}`
+    );
+  }
+
+  create({ portalSlug, categoryObj }) {
+    return axios.post(`${this.url}/${portalSlug}/categories`, categoryObj);
+  }
+
+  update({ portalSlug, categoryObj }) {
+    return axios.patch(`${this.url}/${portalSlug}/categories`, categoryObj);
+  }
+
+  delete({ portalSlug, categoryId }) {
+    return axios.delete(`${this.url}/${portalSlug}/categories/${categoryId}`);
   }
 }
 
--- app/javascript/dashboard/store/modules/helpCenterCategories/actions.js
+++ app/javascript/dashboard/store/modules/helpCenterCategories/actions.js
@@ -2,10 +2,10 @@ import categoriesAPI from 'dashboard/api/helpCenter/categories.js';
 import { throwErrorMessage } from 'dashboard/store/utils/api';
 import types from '../../mutation-types';
 export const actions = {
-  index: async ({ commit }) => {
+  index: async ({ commit }, portalSlug, pageNumber, locale) => {
     try {
       commit(types.SET_UI_FLAG, { isFetching: true });
-      const { data } = await categoriesAPI.get();
+      const { data } = await categoriesAPI.get(pageNumber, portalSlug, locale);
       const categoryIds = data.map(category => category.id);
       commit(types.ADD_MANY_CATEGORIES, data);
       commit(types.ADD_MANY_CATEGORIES_ID, categoryIds);
@@ -17,10 +17,10 @@ export const actions = {
     }
   },
 
-  create: async ({ commit }, params) => {
+  create: async ({ commit }, categoryObj) => {
     commit(types.SET_UI_FLAG, { isCreating: true });
     try {
-      const { data } = await categoriesAPI.create(params);
+      const { data } = await categoriesAPI.create(categoryObj);
       const { id: categoryId } = data;
       commit(types.ADD_CATEGORY, data);
       commit(types.ADD_CATEGORY_ID, categoryId);
@@ -31,19 +31,15 @@ export const actions = {
       commit(types.SET_UI_FLAG, { isCreating: false });
     }
   },
-  update: async ({ commit }, params) => {
-    const categoryId = params.id;
+  update: async ({ commit }, categoryObj) => {
+    const categoryId = categoryObj.id;
     commit(types.ADD_CATEGORY_FLAG, {
-      uiFlags: {
-        isUpdating: true,
-      },
+      uiFlags: { isUpdating: true },
       categoryId,
     });
     try {
-      const { data } = await categoriesAPI.update(params);
-
+      const { data } = await categoriesAPI.update(categoryObj);
       commit(types.UPDATE_CATEGORY, data);
-
       return categoryId;
     } catch (error) {
       return throwErrorMessage(error);
@@ -65,7 +61,6 @@ export const actions = {
     });
     try {
       await categoriesAPI.delete(categoryId);
-
       commit(types.REMOVE_CATEGORY, categoryId);
       commit(types.REMOVE_CATEGORY_ID, categoryId);
       return categoryId;
