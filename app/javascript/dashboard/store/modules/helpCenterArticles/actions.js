f7df142e39fa81be0c1d52bac8e69641bd2487db
Muhsin Keloth <muhsinkeramam@gmail.com>
2022-08-04 15:18:56 +0530

    Fixed all the spec issues

--- app/javascript/dashboard/store/modules/helpCenterArticles/actions.js
+++ app/javascript/dashboard/store/modules/helpCenterArticles/actions.js
@@ -1,7 +1,6 @@
-import PortalAPI from 'dashboard/api/helpCenter/portals';
 import articlesAPI from 'dashboard/api/helpCenter/articles';
 import { throwErrorMessage } from 'dashboard/store/utils/api';
-const portalAPIs = new PortalAPI();
+
 import types from '../../mutation-types';
 export const actions = {
   index: async (
@@ -12,7 +11,7 @@ export const actions = {
       commit(types.SET_UI_FLAG, { isFetching: true });
       const {
         data: { payload, meta },
-      } = await portalAPIs.getArticles({
+      } = await articlesAPI.getArticles({
         pageNumber,
         portalSlug,
         locale,
