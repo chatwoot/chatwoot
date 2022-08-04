f5684f3facc0ce07835f64cb90fc5c281d145a32
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2022-08-04 15:11:29 +0530

    feat: Add store to manage categories (#5127)

---
+++ app/javascript/dashboard/api/helpCenter/categories.js
@@ -0,0 +1,27 @@
+/* global axios */
+
+import PortalsAPI from './portals';
+
+class CategoriesAPI extends PortalsAPI {
+  constructor() {
+    super('categories', { accountScoped: true });
+  }
+
+  get({ portalSlug }) {
+    return axios.get(`${this.url}/${portalSlug}/categories`);
+  }
+
+  create({ portalSlug, categoryObj }) {
+    return axios.post(`${this.url}/${portalSlug}/categories`, categoryObj);
+  }
+
+  update({ portalSlug, categoryObj }) {
+    return axios.patch(`${this.url}/${portalSlug}/categories`, categoryObj);
+  }
+
+  delete({ portalSlug, categoryId }) {
+    return axios.delete(`${this.url}/${portalSlug}/categories/${categoryId}`);
+  }
+}
+
+export default new CategoriesAPI();
