2d36d3879d3a443c8f02b1c9644ed1150af33c52
Sojan Jose <sojan@pepalo.com>
2022-08-10 12:41:58 +0200

    chore: fix the job

--- app/jobs/migration/remove_stale_notifications_job.rb
+++ app/jobs/migration/remove_stale_notifications_job.rb
@@ -3,25 +3,18 @@ class Migration::RemoveStaleNotificationsJob < ApplicationJob
   queue_as :scheduled_jobs
 
   def perform
-    remove_conversation_notifications
-    remove_message_notifications
+    remove_invalid_messages
   end
 
   private
 
-  def remove_conversation_notifications
+  def remove_invalid_messages
     deleted_ids = []
-    Notification.where(primary_actor_type: 'Conversation').pluck(:primary_actor_id).uniq.each_slice(1000) do |id_list|
-      deleted_ids << (id_list - Conversation.where(id: id_list).pluck(:id))
+    Message.pluck(:inbox_id).uniq.each_slice(1000) do |id_list|
+      deleted_ids << (id_list - Inbox.where(id: id_list).pluck(:id))
     end
-    Notification.where(primary_actor_type: 'Conversation', primary_actor_id: deleted_ids).destroy_all
-  end
-
-  def remove_message_notifications
-    deleted_ids = []
-    Notification.where(primary_actor_type: 'Message').pluck(:primary_actor_id).uniq.each_slice(1000) do |id_list|
-      deleted_ids << (id_list - Message.where(id: id_list).pluck(:id))
-    end
-    Notification.where(primary_actor_type: 'Message', primary_actor_id: deleted_ids).destroy_all
+    
+    Message.where(inbox_id: deleted_ids.flatten).destroy_all
   end
 end
+
