74b9a2f61ad6977dc30c9c265fe1ab709417c612
Sojan Jose <sojan@pepalo.com>
2022-08-10 13:46:46 +0200

    fix: Notification page breakages (#5236)
    - Remove the cascading foreign key indexes
    - Add migration to clean up existing objects
    
    fixes: #4285

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -581,12 +581,17 @@ RSpec.describe Conversation, type: :model do
   end
 
   describe '#delete conversation' do
+    include ActiveJob::TestHelper
+
     let!(:conversation) { create(:conversation) }
 
     let!(:notification) { create(:notification, notification_type: 'conversation_creation', primary_actor: conversation) }
 
     it 'delete associated notifications if conversation is deleted' do
-      conversation.destroy!
+      perform_enqueued_jobs do
+        conversation.destroy!
+      end
+
       expect { notification.reload }.to raise_error ActiveRecord::RecordNotFound
     end
   end
