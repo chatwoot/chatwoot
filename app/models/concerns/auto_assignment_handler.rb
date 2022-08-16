a242803374f658ee3ef78243c0a613e45bf50613
Sojan Jose <sojan@pepalo.com>
2022-08-16 16:58:23 +0530

    feat: Enable Capacity Config UI (#5164)
    - Enables Capacity Config in UI
    - Rewrite auto assignment Logic to consider only online agents
    
    fixes: #4990

--- app/models/concerns/auto_assignment_handler.rb
+++ app/models/concerns/auto_assignment_handler.rb
@@ -1,23 +1,23 @@
-module RoundRobinHandler
+module AutoAssignmentHandler
   extend ActiveSupport::Concern
   include Events::Types
 
   included do
-    after_save :run_round_robin
+    after_save :run_auto_assignment
   end
 
   private
 
-  def run_round_robin
+  def run_auto_assignment
     # Round robin kicks in on conversation create & update
     # run it only when conversation status changes to open
     return unless conversation_status_changed_to_open?
-    return unless should_round_robin?
+    return unless should_run_auto_assignment?
 
-    ::RoundRobin::AssignmentService.new(conversation: self, allowed_member_ids: inbox.member_ids_with_assignment_capacity).perform
+    ::AutoAssignment::AgentAssignmentService.new(conversation: self, allowed_agent_ids: inbox.member_ids_with_assignment_capacity).perform
   end
 
-  def should_round_robin?
+  def should_run_auto_assignment?
     return false unless inbox.enable_auto_assignment?
 
     # run only if assignee is blank or doesn't have access to inbox
