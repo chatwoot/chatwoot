f930832d62fd1c70cb4c96a398bdf6c62d804c8b
Sojan <sojan@pepalo.com>
2022-08-30 17:48:23 +0530

    chore: Clean up

--- lib/seeders/account_seeder.rb
+++ lib/seeders/account_seeder.rb
@@ -55,10 +55,14 @@ class Seeders::AccountSeeder
       AccountUser.create_with(role: (user['role'] || 'agent')).find_or_create_by!(account_id: @account.id, user_id: user_record.id)
       next if user['team'].blank?
 
-      user['team'].each do |team|
-        team_record = @account.teams.where('name LIKE ?', "%#{team.downcase}%").first if team.present?
-        TeamMember.find_or_create_by!(team_id: team_record.id, user_id: user_record.id) unless team_record.nil?
-      end
+      add_user_to_teams(user: user_record, teams: user['team'])
+    end
+  end
+
+  def add_user_to_teams(user:, teams:)
+    teams.each do |team|
+      team_record = @account.teams.where('name LIKE ?', "%#{team.downcase}%").first if team.present?
+      TeamMember.find_or_create_by!(team_id: team_record.id, user_id: user.id) unless team_record.nil?
     end
   end
 
@@ -75,96 +79,27 @@ class Seeders::AccountSeeder
       contact_data['conversations'].each do |conversation_data|
         inbox = @account.inboxes.find_by(channel_type: conversation_data['channel'])
         contact_inbox = inbox.contact_inboxes.create!(contact: contact, source_id: (conversation_data['source_id'] || SecureRandom.hex))
-        assignee = User.find_by(email: conversation_data['assignee']) if conversation_data['assignee'].present?
-        conversation = contact_inbox.conversations.create!(account: inbox.account, contact: contact, inbox: inbox, assignee: assignee)
-        conversation_data['messages'].each do |message_data|
-          sender = User.find_by(email: message_data['sender']) if message_data['sender'].present?
-          conversation.messages.create!(message_data.slice('content', 'message_type').merge(account: inbox.account, sender: sender, inbox: inbox))
-        end
+        create_conversation(contact_inbox: contact_inbox, conversation_data: conversation_data)
       end
     end
   end
 
+  def create_conversation(contact_inbox:, conversation_data:)
+    assignee = User.find_by(email: conversation_data['assignee']) if conversation_data['assignee'].present?
+    conversation = contact_inbox.conversations.create!(account: contact_inbox.inbox.account, contact: contact_inbox.contact,
+                                                       inbox: contact_inbox.inbox, assignee: assignee)
+    create_messages(conversation: conversation, messages: conversation_data['messages'])
+  end
+
+  def create_messages(conversation:, messages:)
+    messages.each do |message_data|
+      sender = User.find_by(email: message_data['sender']) if message_data['sender'].present?
+      conversation.messages.create!(message_data.slice('content', 'message_type').merge(account: conversation.inbox.account, sender: sender,
+                                                                                        inbox: conversation.inbox))
+    end
+  end
+
   def seed_inboxes
-    seed_website_inbox
-    seed_facebook_inbox
-    seed_twitter_inbox
-    seed_whatsapp_inbox
-    seed_sms_inbox
-    seed_email_inbox
-    seed_api_inbox
-    seed_telegram_inbox
-    seed_line_inbox
-  end
-
-  def seed_website_inbox
-    channel = Channel::WebWidget.create!(account: @account, website_url: "https://#{@account_data['company_name'].downcase}.inc")
-    Inbox.create!(channel: channel, account: @account, name: "#{@account_data['company_name']} Website")
-  end
-
-  def seed_facebook_inbox
-    channel = Channel::FacebookPage.create!(account: @account, user_access_token: 'test', page_access_token: 'test', page_id: 'test')
-    Inbox.create!(channel: channel, account: @account, name: "#{@account_data['company_name']} Facebook")
-  end
-
-  def seed_twitter_inbox
-    channel = Channel::TwitterProfile.create!(account: @account, twitter_access_token: 'test', twitter_access_token_secret: 'test', profile_id: '123')
-    Inbox.create!(channel: channel, account: @account, name: "#{@account_data['company_name']} Twitter")
-  end
-
-  def seed_whatsapp_inbox
-    Channel::Whatsapp.insert(
-      {
-        account_id: @account.id,
-        phone_number: Faker::PhoneNumber.cell_phone_in_e164,
-        created_at: Time.now.utc,
-        updated_at: Time.now.utc
-      },
-      returning: %w[id]
-    )
-
-    channel = Channel::Whatsapp.find_by(account_id: @account.id)
-
-    Inbox.create!(channel: channel, account: @account, name: "#{@account_data['company_name']} Whatsapp")
-  end
-
-  def seed_sms_inbox
-    channel = Channel::Sms.create!(account: @account, phone_number: Faker::PhoneNumber.cell_phone_in_e164)
-    Inbox.create!(channel: channel, account: @account, name: "#{@account_data['company_name']} Mobile")
-  end
-
-  def seed_email_inbox
-    channel = Channel::Email.create!(account: @account, email: "test#{SecureRandom.hex}@#{@account_data['company_name'].downcase}.inc",
-                                     forward_to_email: "test_fwd#{SecureRandom.hex}@#{@account_data['company_name'].downcase}.inc")
-    Inbox.create!(channel: channel, account: @account, name: "#{@account_data['company_name']} Email")
-  end
-
-  def seed_api_inbox
-    channel = Channel::Api.create!(account: @account)
-    Inbox.create!(channel: channel, account: @account, name: "#{@account_data['company_name']} API")
-  end
-
-  def seed_telegram_inbox
-    # rubocop:disable Rails/SkipsModelValidations
-    bot_token = SecureRandom.hex
-    Channel::Telegram.insert(
-      {
-        account_id: @account.id,
-        bot_name: (@account_data['company_name']).to_s,
-        bot_token: bot_token,
-        created_at: Time.now.utc,
-        updated_at: Time.now.utc
-      },
-      returning: %w[id]
-    )
-    channel = Channel::Telegram.find_by(bot_token: bot_token)
-    Inbox.create!(channel: channel, account: @account, name: "#{@account_data['company_name']} Telegram")
-    # rubocop:enable Rails/SkipsModelValidations
-  end
-
-  def seed_line_inbox
-    channel = Channel::Line.create!(account: @account, line_channel_id: SecureRandom.hex, line_channel_secret: SecureRandom.hex,
-                                    line_channel_token: SecureRandom.hex)
-    Inbox.create!(channel: channel, account: @account, name: "#{@account_data['company_name']} Line")
+    Seeders::InboxSeeder.new(account: @account, company_data: @account_data[:company]).perform!
   end
 end
