50a6e91142f904671af0849c91d86e5a6cb3064d
iamsivin <iamsivin@gmail.com>
2022-09-02 19:41:30 +0530

    chore: Adds empty text

--- app/javascript/dashboard/routes/dashboard/helpcenter/pages/categories/CategoryListItem.vue
+++ app/javascript/dashboard/routes/dashboard/helpcenter/pages/categories/CategoryListItem.vue
@@ -1,66 +1,73 @@
 <template>
-  <table>
-    <thead>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th scope="col">
+            {{ $t('HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.NAME') }}
+          </th>
+          <th scope="col">
+            {{ $t('HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.DESCRIPTION') }}
+          </th>
+          <th scope="col">
+            {{ $t('HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.LOCALE') }}
+          </th>
+          <th scope="col">
+            {{ $t('HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.ARTICLE_COUNT') }}
+          </th>
+          <th scope="col" />
+        </tr>
+      </thead>
       <tr>
-        <th scope="col">
-          {{ $t('HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.NAME') }}
-        </th>
-        <th scope="col">
-          {{ $t('HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.DESCRIPTION') }}
-        </th>
-        <th scope="col">
-          {{ $t('HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.LOCALE') }}
-        </th>
-        <th scope="col">
-          {{ $t('HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.ARTICLE_COUNT') }}
-        </th>
-        <th scope="col" />
+        <td colspan="100%" class="horizontal-line" />
       </tr>
-    </thead>
-    <tr>
-      <td colspan="100%" class="horizontal-line" />
-    </tr>
-    <tbody>
-      <tr v-for="category in categories" :key="category.id">
-        <td>
-          <span>{{ category.name }}</span>
-        </td>
-        <td>
-          <span>{{ category.description }}</span>
-        </td>
-        <td>
-          <span>{{ category.locale }}</span>
-        </td>
-        <td>
-          <span>{{ category.meta.articles_count }}</span>
-        </td>
-        <td>
-          <woot-button
-            v-tooltip.top-end="
-              $t('HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.ACTION_BUTTON.EDIT')
-            "
-            size="tiny"
-            variant="smooth"
-            icon="edit"
-            color-scheme="secondary"
-            @click="editCategory(category)"
-          />
-          <woot-button
-            v-tooltip.top-end="
-              $t(
-                'HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.ACTION_BUTTON.DELETE'
-              )
-            "
-            size="tiny"
-            variant="smooth"
-            icon="delete"
-            color-scheme="alert"
-            @click="deleteCategory(category.id)"
-          />
-        </td>
-      </tr>
-    </tbody>
-  </table>
+      <tbody>
+        <tr v-for="category in categories" :key="category.id">
+          <td>
+            <span>{{ category.name }}</span>
+          </td>
+          <td>
+            <span>{{ category.description }}</span>
+          </td>
+          <td>
+            <span>{{ category.locale }}</span>
+          </td>
+          <td>
+            <span>{{ category.meta.articles_count }}</span>
+          </td>
+          <td>
+            <woot-button
+              v-tooltip.top-end="
+                $t(
+                  'HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.ACTION_BUTTON.EDIT'
+                )
+              "
+              size="tiny"
+              variant="smooth"
+              icon="edit"
+              color-scheme="secondary"
+              @click="editCategory(category)"
+            />
+            <woot-button
+              v-tooltip.top-end="
+                $t(
+                  'HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.ACTION_BUTTON.DELETE'
+                )
+              "
+              size="tiny"
+              variant="smooth"
+              icon="delete"
+              color-scheme="alert"
+              @click="deleteCategory(category.id)"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-if="categories.length === 0" class="empty-text">
+      {{ $t('HELP_CENTER.PORTAL.EDIT.CATEGORIES.TABLE.EMPTY_TEXT') }}
+    </p>
+  </div>
 </template>
 
 <script>
@@ -104,5 +111,13 @@ table {
 }
 .horizontal-line {
   border-bottom: 1px solid var(--color-border);
+}
+
+.empty-text {
+  display: flex;
+  justify-content: center;
+  color: var(--s-500);
+  font-size: var(--font-size-default);
+  margin-top: var(--space-three);
 }
 </style>
