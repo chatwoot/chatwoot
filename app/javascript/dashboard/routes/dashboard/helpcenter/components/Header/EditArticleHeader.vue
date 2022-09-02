03c8251cc3606891181ea882dd63f10bed47ee17
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2022-09-02 12:53:18 +0530

    feat: Adds the ability to publish an article (#5365)
    * feat: Adds the ability to publish an article
    
    * chore: Disabled publish button and dropdown when there is no article id
    
    * chore: Review fixes
    
    * chore: Review fixes
    
    * Update app/javascript/dashboard/routes/dashboard/helpcenter/components/Header/EditArticleHeader.vue
    
    * chore: Review fixes
    
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>

--- app/javascript/dashboard/routes/dashboard/helpcenter/components/Header/EditArticleHeader.vue
+++ app/javascript/dashboard/routes/dashboard/helpcenter/components/Header/EditArticleHeader.vue
@@ -59,19 +59,58 @@
         color-scheme="secondary"
         @click="closeSidebar"
       />
-      <woot-button
-        class-names="article--buttons"
-        size="small"
-        color-scheme="primary"
-      >
-        {{ $t('HELP_CENTER.EDIT_HEADER.PUBLISH_BUTTON') }}
-      </woot-button>
+      <div class="article--buttons">
+        <div class="button-group">
+          <woot-button
+            class-names="publish-button"
+            size="small"
+            color-scheme="primary"
+            :is-disabled="!articleSlug || isPublishedArticle"
+            @click="updateArticleStatus(ARTICLE_STATUS_TYPES.PUBLISH)"
+          >
+            {{ $t('HELP_CENTER.EDIT_HEADER.PUBLISH_BUTTON') }}
+          </woot-button>
+          <woot-button
+            ref="arrowDownButton"
+            size="small"
+            icon="chevron-down"
+            :is-disabled="!articleSlug || isArchivedArticle"
+            @click="openActionsDropdown"
+          />
+        </div>
+        <div
+          v-if="showActionsDropdown"
+          v-on-clickaway="closeActionsDropdown"
+          class="dropdown-pane dropdown-pane--open"
+        >
+          <woot-dropdown-menu>
+            <woot-dropdown-item>
+              <woot-button
+                variant="clear"
+                color-scheme="secondary"
+                size="small"
+                icon="book-clock"
+                @click="updateArticleStatus(ARTICLE_STATUS_TYPES.ARCHIVE)"
+              >
+                {{ $t('HELP_CENTER.EDIT_HEADER.MOVE_TO_ARCHIVE_BUTTON') }}
+              </woot-button>
+            </woot-dropdown-item>
+          </woot-dropdown-menu>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import alertMixin from 'shared/mixins/alertMixin';
+import { mixin as clickaway } from 'vue-clickaway';
+import wootConstants from 'dashboard/constants.js';
+
+const { ARTICLE_STATUS_TYPES } = wootConstants;
+
 export default {
+  mixins: [alertMixin, clickaway],
   props: {
     backButtonLabel: {
       type: String,
@@ -97,6 +136,9 @@ export default {
   data() {
     return {
       isSidebarOpen: false,
+      showActionsDropdown: false,
+      alertMessage: '',
+      ARTICLE_STATUS_TYPES: ARTICLE_STATUS_TYPES,
     };
   },
   computed: {
@@ -104,6 +146,21 @@ export default {
       return this.isUpdating
         ? this.$t('HELP_CENTER.EDIT_HEADER.SAVING')
         : this.$t('HELP_CENTER.EDIT_HEADER.SAVED');
+    },
+    articleSlug() {
+      return this.$route.params.articleSlug;
+    },
+    currentPortalSlug() {
+      return this.$route.params.portalSlug;
+    },
+    currentArticleStatus() {
+      return this.$store.getters['articles/articleStatus'](this.articleSlug);
+    },
+    isPublishedArticle() {
+      return this.currentArticleStatus === 'published';
+    },
+    isArchivedArticle() {
+      return this.currentArticleStatus === 'archived';
     },
   },
   methods: {
@@ -116,6 +173,36 @@ export default {
     onClickAdd() {
       this.$emit('add');
     },
+    async updateArticleStatus(status) {
+      try {
+        await this.$store.dispatch('articles/update', {
+          portalSlug: this.currentPortalSlug,
+          articleId: this.articleSlug,
+          status: status,
+        });
+        this.statusUpdateSuccessMessage(status);
+        this.closeActionsDropdown();
+      } catch (error) {
+        this.alertMessage =
+          error?.message || this.statusUpdateErrorMessage(status);
+      } finally {
+        this.showAlert(this.alertMessage);
+      }
+    },
+    statusUpdateSuccessMessage(status) {
+      if (status === this.ARTICLE_STATUS_TYPES.PUBLISH) {
+        this.alertMessage = this.$t('HELP_CENTER.PUBLISH_ARTICLE.API.SUCCESS');
+      } else if (status === this.ARTICLE_STATUS_TYPES.ARCHIVE) {
+        this.alertMessage = this.$t('HELP_CENTER.ARCHIVE_ARTICLE.API.SUCCESS');
+      }
+    },
+    statusUpdateErrorMessage(status) {
+      if (status === this.ARTICLE_STATUS_TYPES.PUBLISH) {
+        this.alertMessage = this.$t('HELP_CENTER.PUBLISH_ARTICLE.API.ERROR');
+      } else if (status === this.ARTICLE_STATUS_TYPES.ARCHIVE) {
+        this.alertMessage = this.$t('HELP_CENTER.ARCHIVE_ARTICLE.API.ERROR');
+      }
+    },
     openSidebar() {
       this.isSidebarOpen = true;
       this.$emit('open');
@@ -123,6 +210,12 @@ export default {
     closeSidebar() {
       this.isSidebarOpen = false;
       this.$emit('close');
+    },
+    openActionsDropdown() {
+      this.showActionsDropdown = !this.showActionsDropdown;
+    },
+    closeActionsDropdown() {
+      this.showActionsDropdown = false;
     },
   },
 };
@@ -149,6 +242,9 @@ export default {
 }
 .article--buttons {
   margin-left: var(--space-smaller);
+  .dropdown-pane {
+    right: var(--space-smaller);
+  }
 }
 .draft-status {
   margin-right: var(--space-smaller);
