db73d033b718f2cd7b400f3ce5c58d06299b9dda
Tejaswini Chile <tejaswini@chatwoot.com>
2022-09-07 12:22:24 +0530

    feat: Fetching the portal data related to a specific custom domain (#5249)

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,4 +3,19 @@
 class PublicController < ActionController::Base
   include RequestExceptionHandler
   skip_before_action :verify_authenticity_token
+
+  private
+
+  def ensure_custom_domain_request
+    custom_domain = request.host
+
+    @portals = ::Portal.where(custom_domain: custom_domain)
+
+    return if @portals.present?
+
+    render json: {
+      error: "Domain: #{custom_domain} is not registered with us. \
+      Please send us an email at support@chatwoot.com with the custom domain name and account API key"
+    }, status: :unauthorized and return
+  end
 end
