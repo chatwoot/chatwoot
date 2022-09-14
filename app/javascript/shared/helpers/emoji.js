e585b227f1152be6d60311422579a4f1b66f1876
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2022-09-14 19:45:04 +0530

    chore: Check for empty strings in name formatter (#5434)

--- app/javascript/shared/helpers/emoji.js
+++ app/javascript/shared/helpers/emoji.js
@@ -34,11 +34,14 @@ export const hasEmojiSupport = () => {
 };
 
 export const removeEmoji = text => {
-  return text
-    .replace(
-      /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
-      ''
-    )
-    .replace(/\s+/g, ' ')
-    .trim();
+  if (text) {
+    return text
+      .replace(
+        /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+        ''
+      )
+      .replace(/\s+/g, ' ')
+      .trim();
+  }
+  return '';
 };
