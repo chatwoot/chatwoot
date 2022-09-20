3b7cae19e6d882e54dd06ee5f638d9b7f4af8078
Sojan Jose <sojan@pepalo.com>
2022-09-20 08:09:41 +0530

    chore: Enable help center for self-hosted accounts (#5458)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_225556) do
+ActiveRecord::Schema.define(version: 2022_09_20_014549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
