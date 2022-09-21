b20f6b571676029cb5c67e002a492e1717f533fe
Pranav Raj S <pranav@chatwoot.com>
2022-09-20 17:55:56 -0700

    chore: Fix link generation logic in help-center

--- app/controllers/public/api/v1/portals_controller.rb
+++ app/controllers/public/api/v1/portals_controller.rb
@@ -1,7 +1,7 @@
 class Public::Api::V1::PortalsController < PublicController
   before_action :ensure_custom_domain_request, only: [:show]
-  before_action :redirect_to_portal_with_locale, only: [:show]
   before_action :portal
+  before_action :redirect_to_portal_with_locale, only: [:show]
   layout 'portal'
 
   def show; end
