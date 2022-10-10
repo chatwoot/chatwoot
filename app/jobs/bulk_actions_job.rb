50a87ec7dd9caadda7f6dac3a88d01e5c6aed003
tejaswini chile <tejaswini@chatwoot.com>
2022-10-10 20:22:17 +0530

    fix: Bulk update assignee id to null

--- app/jobs/bulk_actions_job.rb
+++ app/jobs/bulk_actions_job.rb
@@ -36,7 +36,7 @@ class BulkActionsJob < ApplicationJob
   def available_params(params)
     return unless params[:fields]
 
-    params[:fields].delete_if { |_k, v| v.nil? }
+    params[:fields].delete_if { |k, _v| k.nil? }
   end
 
   def bulk_add_labels(conversation)
