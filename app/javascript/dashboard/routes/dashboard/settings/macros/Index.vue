63ee8bb88a89b53be555af2bbea0304540ac7499
fayazara <fayazara@gmail.com>
2022-10-12 13:36:24 +0530

    Add macros list and editor for CRUD

--- app/javascript/dashboard/routes/dashboard/settings/macros/Index.vue
+++ app/javascript/dashboard/routes/dashboard/settings/macros/Index.vue
@@ -1,11 +1,123 @@
 <template>
-  <div>
-    Macros
+  <div class="column content-box">
+    <router-link
+      :to="addAccountScoping('settings/macros/new')"
+      class="button success button--fixed-right-top"
+    >
+      <fluent-icon icon="add-circle" />
+      <span class="button__content">
+        {{ $t('MACROS.HEADER_BTN_TXT') }}
+      </span>
+    </router-link>
+    <div class="row">
+      <div class="small-8 columns with-right-space">
+        <p
+          v-if="!uiFlags.isFetching && !records.length"
+          class="no-items-error-message"
+        >
+          {{ $t('MACROS.LIST.404') }}
+        </p>
+        <woot-loading-state
+          v-if="uiFlags.isFetching"
+          :message="$t('MACROS.LOADING')"
+        />
+        <table v-if="!uiFlags.isFetching && records.length" class="woot-table">
+          <thead>
+            <th
+              v-for="thHeader in $t('MACROS.LIST.TABLE_HEADER')"
+              :key="thHeader"
+            >
+              {{ thHeader }}
+            </th>
+          </thead>
+          <tbody>
+            <macros-table-row
+              v-for="(macro, index) in records"
+              :key="index"
+              :macro="macro"
+              @delete="openDeletePopup(macro, index)"
+            />
+          </tbody>
+        </table>
+      </div>
+      <div class="small-4 columns">
+        <span v-dompurify-html="$t('MACROS.SIDEBAR_TXT')" />
+      </div>
+    </div>
+    <woot-delete-modal
+      :show.sync="showDeleteConfirmationPopup"
+      :on-close="closeDeletePopup"
+      :on-confirm="confirmDeletion"
+      :title="$t('LABEL_MGMT.DELETE.CONFIRM.TITLE')"
+      :message="$t('MACROS.DELETE.CONFIRM.MESSAGE')"
+      :message-value="deleteMessage"
+      :confirm-text="deleteConfirmText"
+      :reject-text="deleteRejectText"
+    />
   </div>
 </template>
 
 <script>
-export default {};
+import { mapGetters } from 'vuex';
+import alertMixin from 'shared/mixins/alertMixin';
+import accountMixin from 'dashboard/mixins/account.js';
+import MacrosTableRow from './MacrosTableRow';
+export default {
+  components: {
+    MacrosTableRow,
+  },
+  mixins: [alertMixin, accountMixin],
+  data() {
+    return {
+      showDeleteConfirmationPopup: false,
+      selectedResponse: {},
+      loading: {},
+    };
+  },
+  computed: {
+    ...mapGetters({
+      records: ['macros/getMacros'],
+      uiFlags: 'macros/getUIFlags',
+    }),
+    deleteConfirmText() {
+      return `${this.$t('MACROS.DELETE.CONFIRM.YES')} ${
+        this.selectedResponse.name
+      }`;
+    },
+    deleteRejectText() {
+      return `${this.$t('MACROS.DELETE.CONFIRM.NO')} ${
+        this.selectedResponse.name
+      }`;
+    },
+    deleteMessage() {
+      return ` ${this.selectedResponse.name}?`;
+    },
+  },
+  mounted() {
+    this.$store.dispatch('macros/get');
+  },
+  methods: {
+    openDeletePopup(response) {
+      this.showDeleteConfirmationPopup = true;
+      this.selectedResponse = response;
+    },
+    closeDeletePopup() {
+      this.showDeleteConfirmationPopup = false;
+    },
+    confirmDeletion() {
+      this.loading[this.selectedResponse.id] = true;
+      this.closeDeletePopup();
+      this.deleteMacro(this.selectedResponse.id);
+    },
+    async deleteMacro(id) {
+      try {
+        await this.$store.dispatch('macros/delete', id);
+        this.showAlert(this.$t('MACROS.DELETE.API.SUCCESS_MESSAGE'));
+        this.loading[this.selectedResponse.id] = false;
+      } catch (error) {
+        this.showAlert(this.$t('MACROS.DELETE.API.ERROR_MESSAGE'));
+      }
+    },
+  },
+};
 </script>
-
-<style></style>
--- app/javascript/dashboard/routes/dashboard/settings/macros/MacroEditor.vue
+++ app/javascript/dashboard/routes/dashboard/settings/macros/MacroEditor.vue
@@ -1,9 +1,144 @@
 <template>
-  <div>MacrosEditor</div>
+  <div class="column content-box">
+    <p v-if="!uiFlags.isFetching && !macro" class="no-items-error-message">
+      {{ $t('MACROS.LIST.404') }}
+    </p>
+    <woot-loading-state
+      v-if="uiFlags.isFetching"
+      :message="$t('MACROS.LOADING')"
+    />
+    <macro-form v-if="macro" :macro-data.sync="macro" @submit="saveMacro" />
+  </div>
 </template>
 
 <script>
-export default {};
+import MacroForm from './MacroForm';
+import { MACRO_ACTION_TYPES } from './constants';
+import { mapGetters } from 'vuex';
+import { emptyMacro } from './macroHelper';
+import actionQueryGenerator from 'dashboard/helper/actionQueryGenerator.js';
+import alertMixin from 'shared/mixins/alertMixin';
+import macrosMixin from './macrosMixin';
+
+export default {
+  components: {
+    MacroForm,
+  },
+  mixins: [alertMixin, macrosMixin],
+  provide() {
+    return {
+      macroActionTypes: this.macroActionTypes,
+    };
+  },
+  data() {
+    return {
+      macro: {},
+      mode: 'CREATE',
+      macroActionTypes: MACRO_ACTION_TYPES,
+    };
+  },
+  computed: {
+    ...mapGetters({
+      uiFlags: 'macros/getUIFlags',
+      labels: 'labels/getLabels',
+      teams: 'teams/getTeams',
+    }),
+    macroId() {
+      return this.$route.params.macroId;
+    },
+  },
+  watch: {
+    $route: {
+      handler() {
+        if (this.$route.params.macroId) {
+          this.fetchMacro();
+        } else {
+          this.initNewMacro();
+        }
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    fetchMacro() {
+      this.mode = 'EDIT';
+      this.$store.dispatch('agents/get');
+      this.$store.dispatch('teams/get');
+      this.$store.dispatch('labels/get');
+      this.manifestMacro();
+    },
+    async manifestMacro() {
+      const isMacroAvailable = this.$store.getters['macros/getMacro'](
+        this.macroId
+      );
+      if (isMacroAvailable) this.macro = this.formatMacro(isMacroAvailable);
+      else {
+        const unserializedMacro = await this.getSingleMacro();
+        this.macro = this.formatMacro(unserializedMacro);
+      }
+    },
+    async getSingleMacro() {
+      return this.$store.dispatch('macros/getSingleMacro', this.macroId);
+    },
+    formatMacro(macro) {
+      const formattedActions = macro.actions.map(action => {
+        let actionParams = [];
+        if (action.action_params.length) {
+          const inputType = this.macroActionTypes.find(
+            item => item.key === action.action_name
+          ).inputType;
+          if (inputType === 'multi_select') {
+            actionParams = [
+              ...this.getDropdownValues(action.action_name, this.$store),
+            ].filter(item => [...action.action_params].includes(item.id));
+          } else if (inputType === 'team_message') {
+            actionParams = {
+              team_ids: [
+                ...this.getDropdownValues(action.action_name, this.$store),
+              ].filter(item =>
+                [...action.action_params[0].team_ids].includes(item.id)
+              ),
+              message: action.action_params[0].message,
+            };
+          } else actionParams = [...action.action_params];
+        }
+        return {
+          ...action,
+          action_params: actionParams,
+        };
+      });
+      return {
+        ...macro,
+        actions: formattedActions,
+      };
+    },
+    initNewMacro() {
+      this.mode = 'CREATE';
+      this.macro = emptyMacro;
+    },
+    async saveMacro(macro) {
+      try {
+        const action = this.mode === 'EDIT' ? 'macros/update' : 'macros/create';
+        const successMessage =
+          this.mode === 'EDIT'
+            ? this.$t('MACROS.EDIT.API.SUCCESS_MESSAGE')
+            : this.$t('MACROS.ADD.API.SUCCESS_MESSAGE');
+        let serializeMacro = JSON.parse(JSON.stringify(macro));
+        serializeMacro.actions = actionQueryGenerator(serializeMacro.actions);
+        await this.$store.dispatch(action, serializeMacro);
+        this.showAlert(this.$t(successMessage));
+        this.$router.push({ name: 'macros_wrapper' });
+      } catch (error) {
+        this.showAlert(this.$t('MACROS.ERROR'));
+      }
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.content-box {
+  padding: 0;
+  height: 100vh;
+}
+</style>
