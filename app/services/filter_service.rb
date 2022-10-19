ee37b221ceaefe73161302b71b26bd0dd8b1a369
Tejaswini Chile <tejaswini@chatwoot.com>
2022-10-19 09:17:46 +0530

    fix: cognitive complexity

--- app/services/filter_service.rb
+++ app/services/filter_service.rb
@@ -99,26 +99,42 @@ class FilterService
   end
 
   def custom_attribute_query(query_hash, custom_attribute_type, current_index)
-    attribute_key = query_hash[:attribute_key]
-    query_operator = query_hash[:query_operator]
-    attribute_model = custom_attribute_type.presence || self.class::ATTRIBUTE_MODEL
+    @attribute_key = query_hash[:attribute_key]
+    @custom_attribute_type = custom_attribute_type
 
-    attribute_type = custom_attribute(attribute_key, @account, attribute_model).try(:attribute_display_type)
-    filter_operator_value = filter_operation(query_hash, current_index)
-    attribute_data_type = self.class::ATTRIBUTE_TYPES[attribute_type]
+    attribute_data_type
 
     return ' ' if @custom_attribute.blank?
 
-    table_name = attribute_model == 'conversation_attribute' ? 'conversations' : 'contacts'
-
-    if attribute_data_type == 'text'
-      "  LOWER(#{table_name}.custom_attributes ->> '#{attribute_key}')::#{attribute_data_type} #{filter_operator_value} #{query_operator} "
-    else
-      "  (#{table_name}.custom_attributes ->> '#{attribute_key}')::#{attribute_data_type} #{filter_operator_value} #{query_operator} "
-    end
+    build_custom_attr_query(query_hash, current_index)
   end
 
   private
+
+  def table_name
+    @table_name = attribute_model == 'conversation_attribute' ? 'conversations' : 'contacts'
+  end
+
+  def attribute_model
+    @attribute_model = @custom_attribute_type.presence || self.class::ATTRIBUTE_MODEL
+  end
+
+  def attribute_data_type
+    attribute_type = custom_attribute(@attribute_key, @account, attribute_model).try(:attribute_display_type)
+    @attribute_data_type = self.class::ATTRIBUTE_TYPES[attribute_type]
+  end
+
+  def build_custom_attr_query(query_hash, current_index)
+    filter_operator_value = filter_operation(query_hash, current_index)
+    query_operator = query_hash[:query_operator]
+
+    if attribute_data_type == 'text'
+      "  LOWER(#{table_name}.custom_attributes ->> '#{@attribute_key}')::#{attribute_data_type} #{filter_operator_value} #{query_operator} "
+    else
+      "  (#{table_name}.custom_attributes ->> '#{@attribute_key}')::#{attribute_data_type} #{filter_operator_value} #{query_operator} "
+    end
+  end
+
 
   def custom_attribute(attribute_key, account, custom_attribute_type)
     current_account = account || Current.account
