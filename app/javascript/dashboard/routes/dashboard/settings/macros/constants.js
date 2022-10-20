91564ad48294df61b15af48bcd67783bdbe8eaea
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2022-10-20 05:43:13 +0530

    feat: Macros listing and Editor (#5606)
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>

---
+++ app/javascript/dashboard/routes/dashboard/settings/macros/constants.js
@@ -0,0 +1,42 @@
+export const MACRO_ACTION_TYPES = [
+  {
+    key: 'assign_team',
+    label: 'Assign a team',
+    inputType: 'multi_select',
+  },
+  {
+    key: 'add_label',
+    label: 'Add a label',
+    inputType: 'multi_select',
+  },
+  {
+    key: 'send_email_transcript',
+    label: 'Send an email transcript',
+    inputType: 'email',
+  },
+  {
+    key: 'mute_conversation',
+    label: 'Mute conversation',
+    inputType: null,
+  },
+  {
+    key: 'snooze_conversation',
+    label: 'Snooze conversation',
+    inputType: null,
+  },
+  {
+    key: 'resolve_conversation',
+    label: 'Resolve conversation',
+    inputType: null,
+  },
+  {
+    key: 'send_attachment',
+    label: 'Send Attachment',
+    inputType: 'attachment',
+  },
+  {
+    key: 'send_message',
+    label: 'Send a message',
+    inputType: 'textarea',
+  },
+];
