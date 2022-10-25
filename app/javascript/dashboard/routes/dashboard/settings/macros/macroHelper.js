c3ec1d4f8abbab7650bafda726dc0f4d6eb84340
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2022-10-25 09:03:59 +0530

    feat: Add the ability for the agents to execute a macro (#5698)
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>

--- app/javascript/dashboard/routes/dashboard/settings/macros/macroHelper.js
+++ app/javascript/dashboard/routes/dashboard/settings/macros/macroHelper.js
@@ -1,3 +1,4 @@
+import { MACRO_ACTION_TYPES as macroActionTypes } from 'dashboard/routes/dashboard/settings/macros/constants.js';
 export const emptyMacro = {
   name: '',
   actions: [
@@ -7,4 +8,26 @@ export const emptyMacro = {
     },
   ],
   visibility: 'global',
+};
+
+export const resolveActionName = key => {
+  return macroActionTypes.find(i => i.key === key).label;
+};
+
+export const resolveTeamIds = (teams, ids) => {
+  return ids
+    .map(id => {
+      const team = teams.find(i => i.id === id);
+      return team ? team.name : '';
+    })
+    .join(', ');
+};
+
+export const resolveLabels = (labels, ids) => {
+  return ids
+    .map(id => {
+      const label = labels.find(i => i.title === id);
+      return label ? label.title : '';
+    })
+    .join(', ');
 };
