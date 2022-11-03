780e4832b3d4ae328ae65878fc671ddb8ea3c197
Sojan <sojan@pepalo.com>
2022-11-02 18:48:23 -0700

    chore: add environment variable to control the behaviour

--- config/initializers/actioncable.rb
+++ config/initializers/actioncable.rb
@@ -3,6 +3,13 @@
 require 'action_cable/subscription_adapter/redis'
 
 ActionCable::SubscriptionAdapter::Redis.redis_connector = lambda do |config|
-  config[:id] = nil
+  # For supporting GCP Memorystore where `client` command is disabled. 
+  # You can configure the follwoing ENV variable to get your installation working.
+  # ref: 
+  # https://github.com/mperham/sidekiq/issues/3518#issuecomment-595611673
+  # https://github.com/redis/redis-rb/issues/767
+  # https://gitlab.com/gitlab-org/gitlab/-/merge_requests/75173
+  # https://github.com/rails/rails/blob/4a23cb3415eac03d76623112576559a722d1f23d/actioncable/lib/action_cable/subscription_adapter/base.rb#L30
+  config[:id] = nil if ENV['REDIS_DISABLE_CLIENT_COMMAND'].present?
   ::Redis.new(config.except(:adapter, :channel_prefix))
 end
