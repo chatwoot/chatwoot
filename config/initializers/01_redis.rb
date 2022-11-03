197c46f8eaeb0ecaf3c81409748a130df4c15a58
Sojan Jose <sojan@pepalo.com>
2022-11-02 17:31:20 -0700

    chore:  Use connection_pool for redis (#5790)
    fixes: #3199

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -2,15 +2,15 @@ Rails.application.reloader.to_prepare do
   # Alfred
   # Add here as you use it for more features
   # Used for Round Robin, Conversation Emails & Online Presence
-  $alfred = ConnectionPool::Wrapper.new(size: 5, timeout: 3) do
+  $alfred = ConnectionPool.new(size: 5, timeout: 3) do
     redis = Rails.env.test? ? MockRedis.new : Redis.new(Redis::Config.app)
     Redis::Namespace.new('alfred', redis: redis, warning: true)
   end
 
   # Velma : Determined protector
   # used in rack attack
-  $velma = ConnectionPool::Wrapper.new(size: 5, timeout: 3) do
-    redis = Rails.env.test? ? MockRedis.new : Redis.new(Redis::Config.app)
-    Redis::Namespace.new('velma', redis: redis, warning: true)
+  $velma = ConnectionPool.new(size: 5, timeout: 3) do
+    config = Rails.env.test? ? MockRedis.new : Redis.new(Redis::Config.app)
+    Redis::Namespace.new('velma', redis: config, warning: true)
   end
 end
