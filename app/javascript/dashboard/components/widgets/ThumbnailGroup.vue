7a6efcacce6b6ed3dfc6d4c946ed9146e0becd87
Nithin David <1277421+nithindavid@users.noreply.github.com>
2022-11-03 20:51:20 -0700

    feat: Adds component for grouped thumbnails

---
+++ app/javascript/dashboard/components/widgets/ThumbnailGroup.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="overlapping-thumbnails">
+    <thumbnail
+      v-for="user in usersListShort"
+      :key="user.id"
+      :src="user.thumbnail"
+      :username="user.name"
+      :has-border="true"
+      :size="size"
+      class="overlapping-thumbnail"
+    />
+    <span v-if="showMoreThumbnailsCount" class="thumbnail-more-text">
+      {{ moreThumbnailsText }}
+    </span>
+  </div>
+</template>
+<script>
+import Thumbnail from './Thumbnail';
+
+export default {
+  components: {
+    Thumbnail,
+  },
+  props: {
+    usersList: {
+      type: Array,
+      default: () => [],
+    },
+    size: {
+      type: String,
+      default: '24px',
+    },
+    maxThumbnailCount: {
+      type: Number,
+      default: 4,
+    },
+    moreThumbnailsText: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      imgError: false,
+    };
+  },
+  computed: {
+    usersListShort() {
+      return this.usersList.slice(0, this.maxThumbnailCount);
+    },
+    showMoreThumbnailsCount() {
+      return this.usersList.length > this.usersListShort.length;
+    },
+  },
+  methods: {
+    onImgError() {
+      this.imgError = true;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.overlapping-thumbnails {
+  display: flex;
+}
+
+.overlapping-thumbnail {
+  position: relative;
+  box-shadow: var(--shadow-small);
+
+  &:not(:first-child) {
+    margin-left: var(--space-minus-small);
+  }
+}
+
+.thumbnail-more-text {
+  display: inline-flex;
+  align-items: center;
+  position: relative;
+
+  margin-left: var(--space-minus-small);
+  padding: 0 var(--space-small);
+  box-shadow: var(--shadow-small);
+  background: var(--color-background);
+  border-radius: var(--space-giga);
+  border: 1px solid var(--white);
+
+  color: var(--s-600);
+  font-size: var(--font-size-mini);
+  font-weight: var(--font-weight-medium);
+}
+</style>
