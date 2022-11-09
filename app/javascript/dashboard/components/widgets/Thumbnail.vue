67e824e40a90a9cff7025cdd495be477957c16fe
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2022-11-08 21:05:13 -0800

    feat: Improve image loading for thumbnails (#5823)

--- app/javascript/dashboard/components/widgets/Thumbnail.vue
+++ app/javascript/dashboard/components/widgets/Thumbnail.vue
@@ -1,13 +1,15 @@
 <template>
   <div :class="thumbnailBoxClass" :style="{ height: size, width: size }">
+    <!-- Using v-show instead of v-if to avoid flickering as v-if removes dom elements.  -->
     <img
-      v-if="!imgError && src"
+      v-show="shouldShowImage"
       :src="src"
       :class="thumbnailClass"
+      @load="onImgLoad"
       @error="onImgError"
     />
     <Avatar
-      v-else
+      v-show="!shouldShowImage"
       :username="userNameWithoutEmoji"
       :class="thumbnailClass"
       :size="avatarSize"
@@ -77,6 +79,7 @@ export default {
   },
   data() {
     return {
+      hasImageLoaded: false,
       imgError: false,
     };
   },
@@ -124,6 +127,15 @@ export default {
       const boxClass = this.variant === 'circle' ? 'is-rounded' : '';
       return `user-thumbnail-box ${boxClass}`;
     },
+    shouldShowImage() {
+      if (!this.src) {
+        return false;
+      }
+      if (this.hasImageLoaded) {
+        return !this.imgError;
+      }
+      return false;
+    },
   },
   watch: {
     src(value, oldValue) {
@@ -135,6 +147,9 @@ export default {
   methods: {
     onImgError() {
       this.imgError = true;
+    },
+    onImgLoad() {
+      this.hasImageLoaded = true;
     },
   },
 };
@@ -159,6 +174,7 @@ export default {
     width: 100%;
     box-sizing: border-box;
     object-fit: cover;
+    vertical-align: initial;
 
     &.border {
       border: 1px solid white;
