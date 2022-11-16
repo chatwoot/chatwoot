606faaa026e9b9a429a73ffeebf76f3f757ef49b
iamsivin <iamsivin@gmail.com>
2022-11-16 13:21:03 +0530

    feat: Setup cypress

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000',
+    // fixturesFolder: 'spec/cypress/fixtures',
+    // downloadsFolder: 'spec/cypress/downloads',
+    // fileServerFolder: 'spec/cypress/public',
+    // screenshotsFolder: 'spec/cypress/screenshots',
+    // videosFolder: 'spec/cypress/videos',
+  },
+});
