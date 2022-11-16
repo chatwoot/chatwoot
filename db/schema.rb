7881cfcf38ca396010d140fee831851c8f3f6422
Sojan <sojan@pepalo.com>
2022-11-16 00:24:15 +0000

    chore: Enable Macros for all accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_02_082737) do
+ActiveRecord::Schema.define(version: 2022_11_16_000514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
