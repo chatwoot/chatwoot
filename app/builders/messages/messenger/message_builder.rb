6be9c138896c099653a0a598a4a71579d05ab3a0
Tejaswini Chile <tejaswini@chatwoot.com>
2022-11-22 13:14:42 +0530

    Fix: destroy the attachments records

--- app/builders/messages/messenger/message_builder.rb
+++ app/builders/messages/messenger/message_builder.rb
@@ -74,6 +74,7 @@ class Messages::Messenger::MessageBuilder
     raise
   rescue Koala::Facebook::ClientError => e
     # The exception occurs when we are trying fetch the deleted story or blocked story.
+    @message.attachments.destroy_all
     @message.update(content: I18n.t('conversations.messages.instagram_deleted_story_content'))
     Rails.logger.error e
     {}
