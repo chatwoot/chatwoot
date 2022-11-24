8ea06608623126bacfb8086bbb3c829f1ff40287
Sojan Jose <sojan@pepalo.com>
2022-11-24 11:50:32 +0000

    chore: Add reauthorization prompt for Whatsapp Channel (#5929)
    - Add reauthorization prompt for Whatsapp Channel
    
    fixes: #5782

--- app/mailers/administrator_notifications/channel_notifications_mailer.rb
+++ app/mailers/administrator_notifications/channel_notifications_mailer.rb
@@ -15,6 +15,14 @@ class AdministratorNotifications::ChannelNotificationsMailer < ApplicationMailer
     send_mail_with_liquid(to: admin_emails, subject: subject) and return
   end
 
+  def whatsapp_disconnect(inbox)
+    return unless smtp_config_set_or_development?
+
+    subject = 'Your Whatsapp connection has expired'
+    @action_url = "#{ENV.fetch('FRONTEND_URL', nil)}/app/accounts/#{Current.account.id}/settings/inboxes/#{inbox.id}"
+    send_mail_with_liquid(to: admin_emails, subject: subject) and return
+  end
+
   def email_disconnect(inbox)
     return unless smtp_config_set_or_development?
 
