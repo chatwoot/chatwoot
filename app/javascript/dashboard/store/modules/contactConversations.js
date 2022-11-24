dcb75b730c251c491e9323d96cf55e6f78bd3cc7
Nithin David <1277421+nithindavid@users.noreply.github.com>
2022-11-24 21:27:28 +0530

    Adds a new conversation to list

--- app/javascript/dashboard/store/modules/contactConversations.js
+++ app/javascript/dashboard/store/modules/contactConversations.js
@@ -89,11 +89,18 @@ export const mutations = {
   },
   [types.default.ADD_CONTACT_CONVERSATION]: ($state, { id, data }) => {
     const conversations = $state.records[id] || [];
-    const updatedConversations = conversations.map(conversation => {
-      return conversation.id === data.id
-        ? { ...conversation, ...data }
-        : conversation;
-    });
+
+    const updatedConversations = [...conversations];
+    const index = conversations.findIndex(
+      conversation => conversation.id === data.id
+    );
+
+    if (index !== -1) {
+      updatedConversations[index] = { ...conversations[index], ...data };
+    } else {
+      updatedConversations.push(data);
+    }
+
     Vue.set($state.records, id, updatedConversations);
   },
   [types.default.DELETE_CONTACT_CONVERSATION]: ($state, id) => {
