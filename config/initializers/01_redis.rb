ddd79affecd68a08532969cffc62d9b0fa175572
Sojan <sojan@pepalo.com>
2022-11-25 16:35:59 +0300

    chore: debug heorku redis issue

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -1,16 +1,14 @@
-Rails.application.reloader.to_prepare do
-  # Alfred
-  # Add here as you use it for more features
-  # Used for Round Robin, Conversation Emails & Online Presence
-  $alfred = ConnectionPool::Wrapper.new(size: 5, timeout: 3) do
-    redis = Rails.env.test? ? MockRedis.new : Redis.new(Redis::Config.app)
-    Redis::Namespace.new('alfred', redis: redis, warning: true)
-  end
+# Alfred
+# Add here as you use it for more features
+# Used for Round Robin, Conversation Emails & Online Presence
+$alfred = ConnectionPool::Wrapper.new(size: 5, timeout: 3) do
+  redis = Rails.env.test? ? MockRedis.new : Redis.new(Redis::Config.app)
+  Redis::Namespace.new('alfred', redis: redis, warning: true)
+end
 
-  # Velma : Determined protector
-  # used in rack attack
-  $velma = ConnectionPool::Wrapper.new(size: 5, timeout: 3) do
-    redis = Rails.env.test? ? MockRedis.new : Redis.new(Redis::Config.app)
-    Redis::Namespace.new('velma', redis: redis, warning: true)
-  end
+# Velma : Determined protector
+# used in rack attack
+$velma = ConnectionPool::Wrapper.new(size: 5, timeout: 3) do
+  redis = Rails.env.test? ? MockRedis.new : Redis.new(Redis::Config.app)
+  Redis::Namespace.new('velma', redis: redis, warning: true)
 end
