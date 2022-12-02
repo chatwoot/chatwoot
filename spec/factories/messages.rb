dd744301582e951c365f2a7038da03f0bf97f1dc
Tejaswini Chile <tejaswini@chatwoot.com>
2022-12-02 17:50:48 +0530

    fix: update specs

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -14,4 +14,23 @@ FactoryBot.define do
       message.conversation ||= create(:conversation, account: message.account, inbox: message.inbox)
     end
   end
+
+  factory :instagram_message, class: 'Message' do
+    content { 'Incoming Message' }
+    status { 'sent' }
+    message_type { 'incoming' }
+    content_type { 'text' }
+    account { create(:account) }
+    source_id { 'instagram-message-id-1234' }
+
+    after(:build) do |message|
+      message.class.skip_callback(:commit, :after, :execute_after_create_commit_callbacks, raise: false)
+      message.class.skip_callback(:create, :after)
+      channel ||= create(:channel_instagram_fb_page, account: message.account, instagram_id: 'instagram-message-id-1234')
+      message.content_attributes = { image_type: 'story_mention' }
+      message.sender ||= message.outgoing? ? create(:user, account: message.account) : create(:contact, account: message.account)
+      message.inbox ||=  create(:inbox, account: message.account, channel: channel)
+      message.conversation ||= create(:conversation, account: message.account, inbox: message.inbox)
+    end
+  end
 end
