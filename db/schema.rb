b456cec6a67d4b7ff64a478789c772e056fb0548
Tejaswini Chile <tejaswini@chatwoot.com>
2022-12-12 11:57:01 +0530

    fix: migration for rebuilding multi model search

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_05_081737) do
+ActiveRecord::Schema.define(version: 2022_12_12_061802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
