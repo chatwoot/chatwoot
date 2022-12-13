9fc4c2e49f703d338f6366ec49a76b5b18cebf52
Muhsin <muhsinkeramam@gmail.com>
2022-12-13 13:19:27 +0530

    Allow editing standard fields label & placeholder

--- app/javascript/widget/components/PreChat/Form.vue
+++ app/javascript/widget/components/PreChat/Form.vue
@@ -211,14 +211,10 @@ export default {
     isContactFieldRequired(field) {
       return this.preChatFields.find(option => option.name === field).required;
     },
-    getLabel({ name, label }) {
-      if (this.labels[name])
-        return this.$t(`PRE_CHAT_FORM.FIELDS.${this.labels[name]}.LABEL`);
+    getLabel({ label }) {
       return label;
     },
-    getPlaceHolder({ name, placeholder }) {
-      if (this.labels[name])
-        return this.$t(`PRE_CHAT_FORM.FIELDS.${this.labels[name]}.PLACEHOLDER`);
+    getPlaceHolder({ placeholder }) {
       return placeholder;
     },
     getValue({ name, type }) {
@@ -229,11 +225,7 @@ export default {
       return this.formValues[name] || null;
     },
 
-    getRequiredErrorMessage({ name, label }) {
-      if (this.labels[name])
-        return this.$t(
-          `PRE_CHAT_FORM.FIELDS.${this.labels[name]}.REQUIRED_ERROR`
-        );
+    getRequiredErrorMessage({ label }) {
       return `${label} ${this.$t('PRE_CHAT_FORM.IS_REQUIRED')}`;
     },
     getValidation({ type, name }) {
