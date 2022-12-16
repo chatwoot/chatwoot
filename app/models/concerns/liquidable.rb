faaf409d0995f78b66d88cd96c6602c54bd6d78a
Sojan <sojan@pepalo.com>
2022-12-16 18:29:26 +0530

    fix: codeblocks

--- app/models/concerns/liquidable.rb
+++ app/models/concerns/liquidable.rb
@@ -24,7 +24,19 @@ module Liquidable
   def process_liquid_in_content
     return unless liquid_processable_message?
 
-    template = Liquid::Template.parse(content)
+    template = Liquid::Template.parse(modified_liquid_content)
     self.content = template.render(message_drops)
+  end
+
+  # def get_liquid_content
+  #   content.gsub(/{{([^}]*)}}/) do |_match|
+  #     "{{#{$1} | default: ''}}"
+  #   end
+  # end
+
+  def modified_liquid_content
+    # This regex is used to match the code blocks in the content
+    # We don't want to process liquid in code blocks
+    content.gsub(/(```)/, '{% raw %}\\1').gsub(/(```)/, '\\1{% endraw %}')
   end
 end
