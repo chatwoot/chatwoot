4d763b36a36dbc1d0d33f164e8e9668fd88f6865
Muhsin <muhsinkeramam@gmail.com>
2022-12-23 15:36:55 +0530

    Add message helpers

---
+++ app/javascript/dashboard/helper/messageHelper.js
@@ -0,0 +1,42 @@
+export const replaceVariablesInMessage = ({ message, variables }) => {
+  const regex = /{{(.*?)}}/g;
+  return message.replace(regex, (match, replace) => {
+    return variables[replace.trim()]
+      ? variables[replace.trim().toLowerCase()]
+      : '';
+  });
+};
+
+export const getFirstName = ({ name }) => {
+  return name.split(' ')[0];
+};
+
+export const getLastName = ({ name }) => {
+  return name.split(' ').length > 1
+    ? name.split(' ')[name.split(' ').length - 1]
+    : '';
+};
+
+export const getMessageVariables = ({ conversation }) => {
+  const {
+    meta: { assignee = {}, sender = {} },
+    id,
+  } = conversation;
+
+  return {
+    'contact.name': sender?.name,
+    'contact.first_name': getFirstName({ name: sender?.name }),
+    'contact.last_name': getLastName({ name: sender?.name }),
+    'contact.email': sender?.email,
+    'contact.phone': sender?.phone_number,
+    'conversation.id': id,
+    'agent.name': assignee?.name ? assignee?.name : '',
+    'agent.first_name': assignee?.name
+      ? getFirstName({ name: assignee?.name })
+      : '',
+    'agent.last_name': assignee?.name
+      ? getLastName({ name: assignee?.name })
+      : '',
+    'agent.email': assignee?.email ? assignee?.email : '',
+  };
+};
