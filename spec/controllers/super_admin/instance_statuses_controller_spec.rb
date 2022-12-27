970c740eef02ac8685656e16b34efb3c323696d9
Vishnu Narayanan <vishnu@chatwoot.com>
2022-12-27 13:41:40 +0530

    chore: fix specs

---
+++ spec/controllers/super_admin/instance_statuses_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+#
+# TODO add spec
+#
--- spec/helpers/super_admin/instance_statuses_helper_spec.rb
+++ spec/helpers/super_admin/instance_statuses_helper_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 # Specs in this file have access to a helper object that includes
 # the SuperAdmin::InstanceStatusHelper. For example:
 #
-# describe SuperAdmin::InstanceStatusHelper do
+# describe SuperAdmin::InstanceStatusesHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
 # end
-RSpec.describe SuperAdmin::InstanceStatusHelper, type: :helper do
+RSpec.describe SuperAdmin::InstanceStatusesHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
