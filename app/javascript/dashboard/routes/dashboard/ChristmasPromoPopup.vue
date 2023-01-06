e9777c80b0491fe6be6799215602ef8f602dbeb0
fayazara <fayazara@gmail.com>
2023-01-06 13:07:37 +0530

    Add a link to the billing page

--- app/javascript/dashboard/routes/dashboard/ChristmasPromoPopup.vue
+++ app/javascript/dashboard/routes/dashboard/ChristmasPromoPopup.vue
@@ -6,15 +6,23 @@
       <fluent-icon icon="copy" class="copy--icon" @click.native="onCopy" />
       during checkout
     </p>
+    <router-link
+      :to="addAccountScoping('settings/billing')"
+      class="link"
+      @click.native="$emit('close')"
+    >
+      Go to the billing page
+    </router-link>
   </div>
 </template>
 
 <script>
 import { copyTextToClipboard } from 'shared/helpers/clipboard';
 import alertMixin from 'shared/mixins/alertMixin';
+import accountMixin from 'dashboard/mixins/account.js';
 
 export default {
-  mixins: [alertMixin],
+  mixins: [alertMixin, accountMixin],
   methods: {
     async onCopy(e) {
       e.preventDefault();
@@ -51,5 +59,14 @@ export default {
       cursor: pointer;
     }
   }
+}
+
+.link {
+  background-color: var(--white);
+  padding: var(--space-slab) var(--space-normal);
+  border-radius: var(--border-radius-normal);
+  display: inline-block;
+  margin-top: var(--space-slab);
+  text-decoration: none;
 }
 </style>
