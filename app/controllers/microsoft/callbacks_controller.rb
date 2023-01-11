5f99ffa286f453f0b5a484e6a7a71c51a70b1ca7
Tejaswini Chile <tejaswini@chatwoot.com>
2023-01-11 13:29:49 +0530

    feat: code clean up and refactoring

--- app/controllers/microsoft/callbacks_controller.rb
+++ app/controllers/microsoft/callbacks_controller.rb
@@ -1,34 +1,26 @@
 class Microsoft::CallbacksController < ApplicationController
-  include Api::V1::MicrosoftGraphHelper
+  include MicrosoftConcern
 
   def show
-    client = ::OAuth2::Client.new(
-      ENV.fetch('AZURE_APP_ID', nil),
-      ENV.fetch('AZURE_APP_SECRET', nil),
-      {
-        site: 'https://login.microsoftonline.com',
-        authorize_url: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-        token_url: 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
-      }
-    )
-
-    @response = client.auth_code.get_token(
+    @response = microsoft_client.auth_code.get_token(
       oauth_code,
       redirect_uri: "#{base_utl}/microsoft/callback"
     )
 
     ActiveRecord::Base.transaction do
-      inbox = create_inbox
-      ::Redis::Alfred.delete(permitted_params['current_account_id'])
-      # ::microsoft::WebhookSubscribeService.new(inbox_id: inbox.id).perform
+      inbox = find_or_create_inbox
+      ::Redis::Alfred.delete('current_account_id')
       redirect_to app_twitter_inbox_agents_url(account_id: account.id, inbox_id: inbox.id)
+    rescue StandardError => e
+      Rails.logger.error e
+      redirect_to microsoft_app_redirect_url
     end
   end
 
   private
 
   def base_url
-    'http://localhost:3000/'
+    ENV.fetch('FRONTEND_URL', 'http://localhost:3000')
   end
 
   def oauth_code
@@ -56,25 +48,8 @@ class Microsoft::CallbacksController < ApplicationController
     app_new_microsoft_inbox_url(account_id: account.id)
   end
 
-  def ensure_access_token
-    microsoft_client.access_token(
-      oauth_token: permitted_params[:oauth_token],
-      oauth_verifier: permitted_params[:oauth_verifier]
-    )
-  end
-
-  def create_inbox
-    channel_email = Channel::Email.create!(
-      { account: account, email: users_data['email'],
-        imap_login: users_data['email'], imap_address: 'outlook.office365.com',
-        imap_port: '993', imap_enabled: true,
-        provider: 'microsoft',
-        provider_config: {
-          access_token: parsed_body['access_token'],
-          refresh_token: parsed_body['refresh_token'],
-          expires_on: (Time.current.utc + 1.hour).to_s
-        } }
-    )
+  def find_or_create_inbox
+    channel_email = create_imap_email_channel(users_data)
 
     account.inboxes.create!(
       account: account,
@@ -83,7 +58,16 @@ class Microsoft::CallbacksController < ApplicationController
     )
   end
 
-  def permitted_params
-    params.permit(:oauth_token, :oauth_verifier, :denied)
+  def create_imap_email_channel
+    channel_email = Channel::Email.find_or_initialize_by(email: users_data['email'], account: account)
+    channel_email.imap_login = users_data['email']
+    channel_email.imap_address = 'outlook.office365.com'
+    channel_email.imap_port = '993'
+    channel_email.imap_enabled = true,
+                                 channel_email.provider = 'microsoft'
+    channel_email.provider_config = { access_token: parsed_body['access_token'], refresh_token: parsed_body['refresh_token'],
+                                      expires_on: (Time.current.utc + 1.hour).to_s }
+    channel_email.save!
+    channel_email
   end
 end
