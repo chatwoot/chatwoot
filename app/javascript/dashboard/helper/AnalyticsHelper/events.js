065531d24a1e2feb75e5b24bed97856d6c0cc130
Shivam Mishra <scm.mymail@gmail.com>
2023-01-12 12:38:31 +0530

    refactor: use past tense for event names

--- app/javascript/dashboard/helper/AnalyticsHelper/events.js
+++ app/javascript/dashboard/helper/AnalyticsHelper/events.js
@@ -1,4 +1,3 @@
-// CONVERSATION EVENTS
 export const CONVERSATION_EVENTS = {
   EXECUTED_A_MACRO: 'Executed a macro',
   SENT_MESSAGE: 'Sent a message',
@@ -12,10 +11,10 @@ export const CONVERSATION_EVENTS = {
 };
 
 export const LABEL_EVENTS = {
-  CREATE: 'Created a new label',
+  CREATE: 'Created a label',
   UPDATE: 'Updated a label',
   DELETED: 'Deleted a label',
-  APPLY_LABEL: 'Apply a label',
+  APPLY_LABEL: 'Applied a label',
 };
 
 // REPORTS EVENTS
@@ -27,43 +26,43 @@ export const REPORTS_EVENTS = {
 // CONTACTS PAGE EVENTS
 export const CONTACTS_EVENTS = {
   // TODO: Determine if these events are required
-  // APPLY_FILTER: 'Filter contacts list',
-  // APPLY_SORT: 'Sort contacts list',
-  // SEARCH: 'Search contacts list',
-  CREATE_CONTACT: 'Create a contact',
-  IMPORT_MODAL_OPEN: 'Open import contacts modal',
+  APPLY_FILTER: 'Filtered contacts list',
+  APPLY_SORT: 'Sorted contacts list',
+  SEARCH: 'Searched contacts list',
+  SAVE_FILTER: 'Saved filter',
+  CREATE_CONTACT: 'Created a contact',
+  IMPORT_MODAL_OPEN: 'Opened import contacts modal',
   IMPORT_FAILURE: 'Import contacts failed',
-  IMPORT_SUCCESS: 'Import contacts success',
+  IMPORT_SUCCESS: 'Imported contacts',
 };
 
 // CAMPAIGN EVENTS
 export const CAMPAIGNS_EVENTS = {
-  OPEN_NEW_CAMPAIGN_MODAL: 'Open new campaign modal',
-  CREATE_CAMPAIGN: 'Create new campaign',
-  UPDATE_CAMPAIGN: 'Update campaign',
-  DELETE_CAMPAIGN: 'Delete campaign',
+  OPEN_NEW_CAMPAIGN_MODAL: 'Opened new campaign modal',
+  CREATE_CAMPAIGN: 'Created a campaign',
+  UPDATE_CAMPAIGN: 'Updated a campaign',
+  DELETE_CAMPAIGN: 'Deleted a campaign',
 };
 
 // PORTAL EVENTS
 export const PORTALS_EVENTS = {
-  CREATE_PORTAL: 'Create a new portal',
-  DELETE_PORTAL: 'Delete a portal',
-  PREVIEW_PORTAL: 'Preview a portal',
-  UPDATE_PORTAL: 'Update a portal',
+  CREATE_PORTAL: 'Created a portal',
+  DELETE_PORTAL: 'Deleted a portal',
+  UPDATE_PORTAL: 'Updated a portal',
 
-  CREATE_LOCALE: 'Create a new locale',
+  CREATE_LOCALE: 'Created a locale',
   SET_DEFAULT_LOCALE: 'Set default locale',
-  DELETE_LOCALE: 'Delete a locale',
-  SWITCH_LOCALE: 'Switch locale',
+  DELETE_LOCALE: 'Deleted a locale',
+  SWITCH_LOCALE: 'Switched locale',
 
-  CREATE_CATEGORY: 'Create a new category',
-  DELETE_CATEGORY: 'Delete a category',
-  EDIT_CATEGORY: 'Edit a category',
+  CREATE_CATEGORY: 'Created a category',
+  DELETE_CATEGORY: 'Deleted a category',
+  EDIT_CATEGORY: 'Edited a category',
 
-  CREATE_ARTICLE: 'Create a new article',
-  PUBLISH_ARTICLE: 'Publish an article',
-  UPDATE_ARTICLE_SETTINGS: 'Update article settings',
-  ARCHIVE_ARTICLE: 'Archive an article',
-  DELETE_ARTICLE: 'Delete an article',
-  PREVIEW_ARTICLE: 'Preview article',
+  CREATE_ARTICLE: 'Created an article',
+  PUBLISH_ARTICLE: 'Published an article',
+  UPDATE_ARTICLE_SETTINGS: 'Updated article settings',
+  ARCHIVE_ARTICLE: 'Archived an article',
+  DELETE_ARTICLE: 'Deleted an article',
+  PREVIEW_ARTICLE: 'Previewed article',
 };
