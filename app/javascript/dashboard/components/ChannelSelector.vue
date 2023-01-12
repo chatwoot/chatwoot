b555805921a93f15a66d79ceb5033aa6fdab4512
Pranav Raj S <pranav@chatwoot.com>
2023-01-11 19:18:55 -0800

    chore: Create a resuable component for selecting a channel

---
+++ app/javascript/dashboard/components/ChannelSelector.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="small-6 medium-4 large-3 channel" @click="$emit('click')">
+    <img :src="src" />
+    <h3 class="channel__title">
+      {{ title }}
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+$channel-hover-color: rgba(0, 0, 0, 0.1);
+
+.inactive {
+  filter: grayscale(100%);
+}
+
+.channel {
+  background: var(--white);
+  border: 1px solid var(--color-border-light);
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  margin: -1px;
+  padding: var(--space-normal) 0;
+  transition: all 0.2s ease-in;
+
+  &:hover {
+    border: 1px solid var(--w-500);
+    box-shadow: var(--shadow-small);
+    z-index: var(--z-index-high);
+  }
+
+  &.disabled {
+    opacity: 0.6;
+  }
+
+  img {
+    margin: var(--space-normal) auto;
+    width: 50%;
+  }
+
+  .channel__title {
+    color: var(--color-body);
+    font-size: var(--font-size-default);
+    text-align: center;
+    text-transform: capitalize;
+  }
+
+  p {
+    color: var(--b-500);
+    width: 100%;
+  }
+}
+</style>
