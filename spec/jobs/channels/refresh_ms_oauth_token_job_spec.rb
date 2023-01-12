de416ad0a0f64a5706d2d668af1a195725ca6a2e
Tejaswini Chile <tejaswini@chatwoot.com>
2023-01-12 17:04:16 +0530

    fix: rubocop

--- spec/jobs/channels/refresh_ms_oauth_token_job_spec.rb
+++ spec/jobs/channels/refresh_ms_oauth_token_job_spec.rb
@@ -6,16 +6,18 @@ RSpec.describe Channels::RefreshMsOauthTokenJob do
   let(:expires_on) { Time.zone.now + 3600 }
   let(:graph_endpoint) { 'https://graph.microsoft.com' }
   let(:azure_ad_endpoint) { 'https://login.microsoftonline.com' }
+  let(:microsoft_client) do
+    OAuth2::Client.new('client_id', 'client_secret', {
+                         site: 'https://login.microsoftonline.com',
+                         authorize_url: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
+                       })
+  end
   let!(:microsoft_email_channel) do
     create(:channel_email, provider_config: { access_token: access_token, refresh_token: refresh_token, expires_on: expires_on })
   end
+  let(:new_tokens) { { access_token: access_token, refresh_token: refresh_token, expires_at: expires_on } }
 
   before do
-    stub_request(:post, "#{azure_ad_endpoint}/common/oauth2/v2.0/token").to_return(
-      body: { 'access_token' => access_token, 'refresh_token' => refresh_token, 'expires_at' => expires_on }.to_json,
-      headers: { 'Content-Type' => 'application/json' }
-    )
-
     stub_request(:post, 'https://login.microsoftonline.com/common/oauth2/v2.0/token').with(
       body: { 'grant_type' => 'refresh_token', 'refresh_token' => refresh_token },
       headers: {
@@ -30,7 +32,13 @@ RSpec.describe Channels::RefreshMsOauthTokenJob do
 
   describe '#refresh_token' do
     it 'when refresh token present' do
+      token = double
+
+      allow(::OAuth2::AccessToken).to receive(:new).and_return(token)
+      allow(token).to receive(:refresh!).and_return(new_tokens)
+
       response = described_class.new.refresh_tokens(microsoft_email_channel, microsoft_email_channel.provider_config)
+
       expect(response['refresh_token']).to be_present
       expect(response['access_token']).to be_present
     end
