9505080dff6dabbf20514d9b7ade70980c89dc5a
Tejaswini Chile <tejaswini@chatwoot.com>
2023-01-17 21:02:21 +0530

    fix: added specs

---
+++ spec/controllers/microsoft_indentity_association_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe '.well-known/microsoft-identity-association.json', type: :request do
+  describe 'GET /.well-known/microsoft-identity-association.json' do
+    it 'successfully retrieves assetlinks.json file' do
+      get '/.well-known/microsoft-identity-association.json'
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include "applicationId"
+    end
+  end
+end
