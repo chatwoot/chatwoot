8bf09962bf3a495c9548504dad53287dad9f23de
Shivam Mishra <scm.mymail@gmail.com>
2023-01-18 16:15:07 +0530

    feat: add omniauth callbacks controller

---
+++ app/controllers/auth/omniauth_callbacks_controller.rb
@@ -0,0 +1,13 @@
+class Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def google_oauth2
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
+  end
+end
