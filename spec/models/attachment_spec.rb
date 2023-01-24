242c6172fd67ff645ac5bf461b004e262786de56
Tejaswini Chile <tejaswini@chatwoot.com>
2023-01-24 22:53:07 +0530

    fix: specs

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Attachment, type: :model do
     end
 
     it 'returns external url as data and thumb urls' do
-      external_url = instagram_message.attachments.first.file_url
+      external_url = instagram_message.attachments.first.external_url
       expect(instagram_message.attachments.first.push_event_data[:data_url]).to eq external_url
     end
   end
