b5cec20ca8f764ee70c86c8d0a818626abe4ed19
Shivam Mishra <scm.mymail@gmail.com>
2023-01-24 16:14:43 +0530

    refactor: omniauth success callback to use sso auth route

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,24 +1,34 @@
 class Users::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
   def omniauth_success
     get_resource_from_auth_hash
-    set_token_on_resource
-    create_auth_params
 
     if confirmable_enabled?
       # don't send confirmation email!!!
       @resource.skip_confirmation!
     end
-    
-    sign_in(:user, @resource, store: false, bypass: false)
+
     @resource.save!
-
-    yield @resource if block_given?
-
-    render partial: 'devise/auth', formats: [:json], locals: { resource: @resource }
+    encoded_email = ERB::Util.url_encode(@resource.email)
+    redirect_to "#{ENV.fetch('FRONTEND_URL', nil)}/app/login?email=#{encoded_email}&sso_auth_token=#{@resource.generate_sso_auth_token}"
   end
 
   def resource_class(mapping = nil)
     User
+  end
+
+  def get_resource_from_auth_hash
+    # find or create user by provider and provider uid
+    @resource = resource_class.where(
+      email: auth_hash['info']['email']
+    ).first
+
+    if @resource.nil?
+      # raise invalid login error instead of user not found
+      # this is to prevent enumeration attacks
+      raise StandardError, 'Invalid Login'
+    end
+
+    @resource
   end
 
   def default_devise_mapping
