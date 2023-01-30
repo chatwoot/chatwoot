a7387b60d5abb1cdda648293de135ca76276ad8d
Vishnu Narayanan <vishnu@chatwoot.com>
2023-01-30 15:31:47 +0530

    chore: refactor

--- app/controllers/super_admin/instance_statuses_controller.rb
+++ app/controllers/super_admin/instance_statuses_controller.rb
@@ -1,40 +1,39 @@
 class SuperAdmin::InstanceStatusesController < SuperAdmin::ApplicationController
   def show
-    @instance_version = get_version
-    @instance_sha =  get_sha
-    @instance_postgres = get_postgres_status
-    @instance_redis = get_redis_status
-    @redis_metrics = {}
+    @metrics = {}
+    get_chatwoot_version
+    get_sha
+    get_postgres_status
     get_redis_metrics
   end
 
-  def get_version
-    Chatwoot.config[:version]
+  def get_chatwoot_version
+    @metrics["Chatwoot version"] = Chatwoot.config[:version]
   end
 
   def get_sha
     sha = `git rev-parse HEAD`
     if sha.blank?
-      return "undefined"
+      @metrics["Git SHA"] = "n/a"
     else
-      return zsha
+      @metrics["Git SHA"] = sha
     end
   end
 
   def get_postgres_status
     if ActiveRecord::Base.connection.active?
-      return true
+      @metrics["Postgres alive"] = "true"
     else
-      return false
+      @metrics["Postgres alive"] = "false"
     end
   end
 
   def get_redis_status
     r = Redis.new(Redis::Config.app)
     if r.ping == "PONG"
-      return true
+      @metrics["Redis alive"] = "false"
     else
-      return false
+      @metrics["Redis alive"] = "false"
     end
   end
 
@@ -42,14 +41,17 @@ class SuperAdmin::InstanceStatusesController < SuperAdmin::ApplicationController
     r = Redis.new(Redis::Config.app)
     if r.ping == "PONG"
       redis_server = r.info()
-      @redis_metrics["Redis version"] = redis_server["redis_version"]
-      @redis_metrics["Redis number of connected clients"] = redis_server["connected_clients"]
-      @redis_metrics["Redis 'maxclients' setting"] = redis_server["maxclients"]
-      @redis_metrics["Redis memory used"] = redis_server["used_memory_human"]
-      @redis_metrics["Redis memory peak"] =  redis_server["used_memory_peak_human"]
-      @redis_metrics["Redis total memory available"] = redis_server["total_system_memory_human"]
-      @redis_metrics["Redis 'maxmemory' setting"] = redis_server["maxmemory"]
-      @redis_metrics["Redis 'maxmemory_policy' setting"] = redis_server["maxmemory_policy"]
+      @metrics["Redis alive"] = "false"
+      @metrics["Redis version"] = redis_server["redis_version"]
+      @metrics["Redis number of connected clients"] = redis_server["connected_clients"]
+      @metrics["Redis 'maxclients' setting"] = redis_server["maxclients"]
+      @metrics["Redis memory used"] = redis_server["used_memory_human"]
+      @metrics["Redis memory peak"] =  redis_server["used_memory_peak_human"]
+      @metrics["Redis total memory available"] = redis_server["total_system_memory_human"]
+      @metrics["Redis 'maxmemory' setting"] = redis_server["maxmemory"]
+      @metrics["Redis 'maxmemory_policy' setting"] = redis_server["maxmemory_policy"]
+    else
+      @metrics["Redis alive"] = "false"
     end
   end
 
