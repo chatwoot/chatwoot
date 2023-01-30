d4558f987844b80434bfd136099810997ab57a03
Vishnu Narayanan <vishnu@chatwoot.com>
2023-01-30 17:55:23 +0530

    feat: add instance health to /api

--- lib/redis/config.rb
+++ lib/redis/config.rb
@@ -11,7 +11,7 @@ module Redis::Config
 
     def base_config
       {
-        url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'),
+        # url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'),
         password: ENV.fetch('REDIS_PASSWORD', nil).presence,
         ssl_params: { verify_mode: Chatwoot.redis_ssl_verify_mode },
         reconnect_attempts: 2,
