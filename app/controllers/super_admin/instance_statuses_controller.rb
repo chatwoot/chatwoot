764922df8d24f0a42ffa66049bdcbdb35e11b38f
Vishnu Narayanan <vishnu@chatwoot.com>
2023-01-30 15:21:05 +0530

    feat: add redis metrics to instance health page

--- app/controllers/super_admin/instance_statuses_controller.rb
+++ app/controllers/super_admin/instance_statuses_controller.rb
@@ -4,6 +4,8 @@ class SuperAdmin::InstanceStatusesController < SuperAdmin::ApplicationController
     @instance_sha =  get_sha
     @instance_postgres = get_postgres_status
     @instance_redis = get_redis_status
+    @redis_metrics = {}
+    get_redis_metrics
   end
 
   def get_version
@@ -15,7 +17,7 @@ class SuperAdmin::InstanceStatusesController < SuperAdmin::ApplicationController
     if sha.blank?
       return "undefined"
     else
-      return sha
+      return zsha
     end
   end
 
@@ -35,5 +37,21 @@ class SuperAdmin::InstanceStatusesController < SuperAdmin::ApplicationController
       return false
     end
   end
+
+  def get_redis_metrics
+    r = Redis.new(Redis::Config.app)
+    if r.ping == "PONG"
+      redis_server = r.info()
+      @redis_metrics["Redis version"] = redis_server["redis_version"]
+      @redis_metrics["Redis number of connected clients"] = redis_server["connected_clients"]
+      @redis_metrics["Redis 'maxclients' setting"] = redis_server["maxclients"]
+      @redis_metrics["Redis memory used"] = redis_server["used_memory_human"]
+      @redis_metrics["Redis memory peak"] =  redis_server["used_memory_peak_human"]
+      @redis_metrics["Redis total memory available"] = redis_server["total_system_memory_human"]
+      @redis_metrics["Redis 'maxmemory' setting"] = redis_server["maxmemory"]
+      @redis_metrics["Redis 'maxmemory_policy' setting"] = redis_server["maxmemory_policy"]
+    end
+  end
+
 
 end
