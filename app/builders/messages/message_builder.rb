5997957c1cd35feb1f74405964b4e6f66eaf40e6
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-02-01 13:01:11 +0530

    Apply suggestions from code review

--- app/builders/messages/message_builder.rb
+++ app/builders/messages/message_builder.rb
@@ -51,17 +51,15 @@ class Messages::MessageBuilder
     cc_emails = @params[:cc_emails].split(',') if @params[:cc_emails]
     bcc_emails = @params[:bcc_emails].split(',') if @params[:bcc_emails]
 
-    validate_email_addresses(cc_emails, bcc_emails)
+    all_email_addresses = cc_emails + bcc_emails
+    validate_email_addresses(all_email_addresses)
 
     @message.content_attributes[:cc_emails] = cc_emails
     @message.content_attributes[:bcc_emails] = bcc_emails
   end
 
-  def validate_email_addresses(cc_emails, bcc_emails)
-    cc_emails&.each do |email|
-      raise StandardError, 'Invalid email address' unless email.match?(URI::MailTo::EMAIL_REGEXP)
-    end
-    bcc_emails&.each do |email|
+  def validate_email_addresses(all_emails)
+    all_emails&.each do |email|
       raise StandardError, 'Invalid email address' unless email.match?(URI::MailTo::EMAIL_REGEXP)
     end
   end
