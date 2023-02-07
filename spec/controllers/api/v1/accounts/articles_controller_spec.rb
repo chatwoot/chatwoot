493dbfc5601fc8971d95d525d7a6fc86deff75fa
Tejaswini Chile <tejaswini@chatwoot.com>
2023-02-07 21:43:33 +0530

    Upload file for articles irrespective of the association

--- spec/controllers/api/v1/accounts/articles_controller_spec.rb
+++ spec/controllers/api/v1/accounts/articles_controller_spec.rb
@@ -247,19 +247,7 @@ RSpec.describe 'Api::V1::Accounts::Articles', type: :request do
 
         blob = JSON.parse(response.body)
 
-        expect(blob['blob_key']).to be_present
-        expect(blob['blob_id']).to be_present
-
-        params = { blob_id: blob['blob_id'] }
-
-        expect(article.background_image.attachment).not_to be_present
-
-        patch "/api/v1/accounts/#{account.id}/portals/#{article.portal.slug}/articles/#{article.id}",
-              headers: agent.create_new_auth_token,
-              params: params
-
-        expect(article.reload.background_image.presence).to be_truthy
-        expect(article.reload.background_image.attachment).to be_present
+        expect(blob['file_url']).to be_present
       end
     end
   end
