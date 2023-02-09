30aa2c52da95ff9cc7ed6e7394272d0ff2138eb5
Tejaswini Chile <tejaswini@chatwoot.com>
2023-02-09 14:05:45 +0530

    Added one more endpoint to attach tempfile and get logo (#6407)

--- spec/controllers/api/v1/accounts/articles_controller_spec.rb
+++ spec/controllers/api/v1/accounts/articles_controller_spec.rb
@@ -232,5 +232,23 @@ RSpec.describe 'Api::V1::Accounts::Articles', type: :request do
         expect(json_response['payload']['id']).to eq(root_article.id)
       end
     end
+
+    describe 'Upload an image' do
+      let(:article) { create(:article, account_id: account.id, category_id: category.id, portal_id: portal.id, author_id: agent.id) }
+
+      it 'update the article with an image' do
+        file = fixture_file_upload(Rails.root.join('spec/assets/avatar.png'), 'image/png')
+
+        post "/api/v1/accounts/#{account.id}/portals/#{article.portal.slug}/articles/attach_file",
+             headers: agent.create_new_auth_token,
+             params: { background_image: file }
+
+        expect(response).to have_http_status(:success)
+
+        blob = JSON.parse(response.body)
+
+        expect(blob['file_url']).to be_present
+      end
+    end
   end
 end
