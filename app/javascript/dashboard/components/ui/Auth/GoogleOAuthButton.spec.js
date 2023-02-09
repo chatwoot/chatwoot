9c9b437419412c336b65b1f12a73b84495bb6781
Shivam Mishra <scm.mymail@gmail.com>
2023-02-09 17:01:01 +0530

    feat: add prop tests

--- app/javascript/dashboard/components/ui/Auth/GoogleOAuthButton.spec.js
+++ app/javascript/dashboard/components/ui/Auth/GoogleOAuthButton.spec.js
@@ -1,9 +1,9 @@
 import { shallowMount } from '@vue/test-utils';
 import GoogleOAuthButton from './GoogleOAuthButton.vue';
 
-function getWrapper(showSeparator) {
+function getWrapper(showSeparator, buttonSize) {
   return shallowMount(GoogleOAuthButton, {
-    propsData: { showSeparator: showSeparator },
+    propsData: { showSeparator: showSeparator, buttonSize: buttonSize },
     methods: {
       $t(text) {
         return text;
@@ -47,5 +47,23 @@ describe('GoogleOAuthButton.vue', () => {
     );
     expect(params.get('response_type')).toBe('code');
     expect(params.get('scope')).toBe('email profile');
+  });
+
+  it('responds to buttonSize prop properly', () => {
+    let wrapper = getWrapper(true, 'tiny');
+    expect(wrapper.find('.button.tiny').exists()).toBe(true);
+
+    wrapper = getWrapper(true, 'small');
+    expect(wrapper.find('.button.small').exists()).toBe(true);
+
+    wrapper = getWrapper(true, 'large');
+    expect(wrapper.find('.button.large').exists()).toBe(true);
+
+    // should not render either
+    wrapper = getWrapper(true, 'default');
+    expect(wrapper.find('.button.small').exists()).toBe(false);
+    expect(wrapper.find('.button.tiny').exists()).toBe(false);
+    expect(wrapper.find('.button.large').exists()).toBe(false);
+    expect(wrapper.find('.button').exists()).toBe(true);
   });
 });
