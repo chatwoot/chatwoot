7f36983af5e25c732eab35ccdfe8de64bbfa42a6
Shivam Mishra <scm.mymail@gmail.com>
2023-02-13 17:01:09 +0530

    refactor: method defn order

--- app/listeners/reporting_event_listener.rb
+++ app/listeners/reporting_event_listener.rb
@@ -1,24 +1,6 @@
 class ReportingEventListener < BaseListener
   include ReportingEventHelper
 
-  def conversation_bot_handoff(event)
-    conversation = extract_conversation_and_account(event)[0]
-    time_to_handoff = conversation.updated_at.to_i - conversation.created_at.to_i
-
-    reporting_event = ReportingEvent.new(
-      name: 'conversation_bot_handoff',
-      value: time_to_handoff,
-      value_in_business_hours: business_hours(conversation.inbox, conversation.created_at, conversation.updated_at),
-      account_id: conversation.account_id,
-      inbox_id: conversation.inbox_id,
-      user_id: conversation.assignee_id,
-      conversation_id: conversation.id,
-      event_start_time: conversation.created_at,
-      event_end_time: conversation.updated_at
-    )
-    reporting_event.save!
-  end
-
   def conversation_resolved(event)
     conversation = extract_conversation_and_account(event)[0]
     time_to_resolve = conversation.updated_at.to_i - conversation.created_at.to_i
@@ -58,6 +40,24 @@ class ReportingEventListener < BaseListener
 
     conversation.update(first_reply_created_at: message.created_at)
 
+    reporting_event.save!
+  end
+
+  def conversation_bot_handoff(event)
+    conversation = extract_conversation_and_account(event)[0]
+    time_to_handoff = conversation.updated_at.to_i - conversation.created_at.to_i
+
+    reporting_event = ReportingEvent.new(
+      name: 'conversation_bot_handoff',
+      value: time_to_handoff,
+      value_in_business_hours: business_hours(conversation.inbox, conversation.created_at, conversation.updated_at),
+      account_id: conversation.account_id,
+      inbox_id: conversation.inbox_id,
+      user_id: conversation.assignee_id,
+      conversation_id: conversation.id,
+      event_start_time: conversation.created_at,
+      event_end_time: conversation.updated_at
+    )
     reporting_event.save!
   end
 end
