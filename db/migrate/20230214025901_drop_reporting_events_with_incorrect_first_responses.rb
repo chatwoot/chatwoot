0e89600fb72416b8f00c8b33b0802116ef7e9e4e
Shivam Mishra <scm.mymail@gmail.com>
2023-02-16 11:06:31 +0530

    feat: add migration to recompute first_responses

---
+++ db/migrate/20230214025901_drop_reporting_events_with_incorrect_first_responses.rb
@@ -0,0 +1,47 @@
+class DropReportingEventsWithIncorrectFirstResponses < ActiveRecord::Migration[6.1]
+  include ReportingEventHelper
+
+  def change
+    recompute_first_responses
+  end
+
+  def recompute_first_responses
+    Reporting::Event.where(name: 'first_response', user_id: nil).find_in_batches do |bot_responses|
+      bot_responses.each do |event|
+        conversation = event.conversation
+        next if conversation.nil?
+
+        last_bot_reply = conversation.messages.where(sender_type: 'AgentBot').last
+        first_human_reply = conversation.message.where(sender_type: 'User').first
+
+        # accomodate for campaign if required
+        # new_value = difference between the first_human_reply and the first_bot_reply if it exists or first_human_reply and created at
+        #
+        # conversation       bot                         conversation
+        # start              handoff                     resolved
+        # |                  |                           |
+        # |____|___|_________|____|_______|_____|________|
+        #      bot reply     ^    ^  human reply
+        #                    |    |
+        #                    |    |
+        #       last_bot_reply    first_human_reply
+        #
+        #
+        # bot handoff happens at the last_bot_reply created time
+        # the response time is the time between last bot reply created and the first human reply created
+
+        next if last_bot_reply.blank?
+
+        # this means a bot replied existed, so we need to update the event_start_time
+        # rubocop:disable Rails/SkipsModelValidations
+        event.update_columns(event_start_time: last_bot_reply.created_at,
+                             event_end_time: first_human_reply.created_at,
+                             value: first_human_reply.created_at.to_i - last_bot_reply.created_at.to_i,
+                             value_in_business_hours: business_hours(conversation.inbox, last_bot_reply.created_at, first_human_reply.created_at),
+                             user_id: first_human_reply.sender_id)
+
+        # rubocop:enable Rails/SkipsModelValidations
+      end
+    end
+  end
+end
