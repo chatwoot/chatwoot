9add8c23190d457af8e635aa13eea7312d1ffb59
Pranav Raj S <pranav@chatwoot.com>
2023-02-15 16:33:31 -0800

    chore: Add controllers for conversation participants  (#6462)
    Co-authored-by: Aswin Dev P.S <aswindevps@gmail.com>
    Co-authored-by: Sojan Jose <sojan@chatwoot.com>

--- app/services/messages/new_message_notification_service.rb
+++ app/services/messages/new_message_notification_service.rb
@@ -4,6 +4,7 @@ class Messages::NewMessageNotificationService
   def perform
     return unless message.notifiable?
 
+    notify_participating_users
     notify_conversation_assignee
   end
 
@@ -11,8 +12,23 @@ class Messages::NewMessageNotificationService
 
   delegate :conversation, :sender, :account, to: :message
 
+  def notify_participating_users
+    participating_users = conversation.conversation_participants.map(&:user)
+    participating_users -= [sender] if sender.is_a?(User)
+
+    participating_users.uniq.each do |participant|
+      NotificationBuilder.new(
+        notification_type: 'participating_conversation_new_message',
+        user: participant,
+        account: account,
+        primary_actor: message
+      ).perform
+    end
+  end
+
   def notify_conversation_assignee
     return if conversation.assignee.blank?
+    return if assignee_already_notified_via_participation?
     return if conversation.assignee == sender
 
     NotificationBuilder.new(
@@ -21,5 +37,14 @@ class Messages::NewMessageNotificationService
       account: account,
       primary_actor: message
     ).perform
+  end
+
+  def assignee_already_notified_via_participation?
+    return unless conversation.conversation_participants.map(&:user).include?(conversation.assignee)
+
+    # check whether participation notifcation is disabled for assignee
+    notification_setting = conversation.assignee.notification_settings.find_by(account_id: account.id)
+    notification_setting.public_send(:email_participating_conversation_new_message?) || notification_setting
+      .public_send(:push_participating_conversation_new_message?)
   end
 end
