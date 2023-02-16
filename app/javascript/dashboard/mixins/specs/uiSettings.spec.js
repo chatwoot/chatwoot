bacb528dc54d5e3f6501c26c3902eb639345bf31
Sojan <sojan@pepalo.com>
2023-02-16 12:50:59 +0530

    chore: fix spec

--- app/javascript/dashboard/mixins/specs/uiSettings.spec.js
+++ app/javascript/dashboard/mixins/specs/uiSettings.spec.js
@@ -116,11 +116,11 @@ describe('uiSettingsMixin', () => {
       const wrapper = shallowMount(Component, { store, localVue });
       expect(wrapper.vm.conversationSidebarItemsOrder).toEqual([
         { name: 'conversation_actions' },
-        { name: 'conversation_participants' },
         { name: 'macros' },
         { name: 'conversation_info' },
         { name: 'contact_attributes' },
         { name: 'previous_conversation' },
+        { name: 'conversation_participants' }
       ]);
     });
   });
