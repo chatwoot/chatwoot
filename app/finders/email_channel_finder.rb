3aa9f28ed057f3ddbe1803ae4b4836f7135a321c
Tejaswini Chile <tejaswini@chatwoot.com>
2023-02-17 20:14:07 +0530

    fix: mail finder for email+@domain.com

--- app/finders/email_channel_finder.rb
+++ app/finders/email_channel_finder.rb
@@ -7,7 +7,11 @@ class EmailChannelFinder
     channel = nil
     recipient_mails = @email_object.to.to_a + @email_object.cc.to_a
     recipient_mails.each do |email|
-      channel = Channel::Email.find_by('lower(email) = ? OR lower(forward_to_email) = ?', email.downcase, email.downcase)
+      mail_id, domain = email.split('@')
+      original_mail_address = mail_id.split('+')[0]
+      channel = Channel::Email.where('lower(email) ~* ? OR lower(forward_to_email) ~* ?',
+                                     "#{original_mail_address}\+\.*\@#{domain}",
+                                     "#{original_mail_address}\+\.*\@#{domain}").last
       break if channel.present?
     end
     channel
