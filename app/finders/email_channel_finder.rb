df2747d46855d6f84e593034b14c48e5e370de8d
Sojan <sojan@pepalo.com>
2023-03-01 15:26:04 +0530

    chore: Fix specs

--- app/finders/email_channel_finder.rb
+++ app/finders/email_channel_finder.rb
@@ -1,4 +1,6 @@
 class EmailChannelFinder
+  include EmailHelper
+
   def initialize(email_object)
     @email_object = email_object
   end
@@ -7,7 +9,7 @@ class EmailChannelFinder
     channel = nil
     recipient_mails = @email_object.to.to_a + @email_object.cc.to_a
     recipient_mails.each do |email|
-      normalized_email = EmailHelper.normalize_email_with_plus_addressing(email)
+      normalized_email = normalize_email_with_plus_addressing(email)
       channel = Channel::Email.find_by('lower(email) = ? OR lower(forward_to_email) = ?', normalized_email, normalized_email)
       break if channel.present?
     end
