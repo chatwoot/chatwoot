2b9b307ffc3e76f24e0001a5590a00882c8510a9
Vishnu Narayanan <vishnu@chatwoot.com>
2023-03-01 14:35:57 +0530

    chore: improve spec for audit logs controller

--- spec/enterprise/controllers/api/v1/accounts/audit_logs_controller_spec.rb
+++ spec/enterprise/controllers/api/v1/accounts/audit_logs_controller_spec.rb
@@ -3,11 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Enterprise Audit API', type: :request do
   let(:account) { create(:account) }
   let(:admin) { create(:user, account: account, role: :administrator) }
+  let(:inbox) { create(:inbox, account: account) }
+  let(:webhook) { create(:webhook, account: account) }
 
   describe 'GET /api/v1/accounts/{account.id}/audit_logs' do
-    let(:inbox) { create(:inbox, account: account) }
-    let(:webhook) { create(:webhook, account: account) }
-
     context 'when it is an un-authenticated user' do
       it 'does not fetch audit logs associated with the account' do
         get "/api/v1/accounts/#{account.id}/audit_logs",
@@ -16,14 +15,34 @@ RSpec.describe 'Enterprise Audit API', type: :request do
       end
     end
 
-    context 'when it is an authenticated user' do
+    context 'when it is an authenticated normal user' do
+      let(:user) { create(:user, account: account) }
+      let(:inbox) { create(:inbox, account: account) }
+
+      it 'fetches audit logs associated with the account' do
+        get "/api/v1/accounts/#{account.id}/audit_logs",
+            headers: user.create_new_auth_token,
+            as: :json
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    # check for response in parse
+    context 'when it is an authenticated admin user' do
       let(:admin) { create(:user, account: account, role: :administrator) }
+      let(:inbox) { create(:inbox, account: account) }
 
       it 'fetches audit logs associated with the account' do
         get "/api/v1/accounts/#{account.id}/audit_logs",
             headers: admin.create_new_auth_token,
             as: :json
+
         expect(response).to have_http_status(:success)
+        json_response = JSON.parse(response.body)
+        expect(json_response['auditable_type']).to eql('Inbox')
+        expect(json_response['action']).to eql('create')
+        expect(json_response['associated_id']).to eql(account.id)
+        expect(json_response['username']).to eql(user.email)
       end
     end
   end
