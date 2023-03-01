a3ec80e524842271e48654c0375f7a019a63a039
Nithin David <1277421+nithindavid@users.noreply.github.com>
2023-03-01 18:03:34 +0530

    feat: Creates component to display conversation search results

---
+++ app/javascript/dashboard/modules/search/components/SearchResultConversationItem.vue
@@ -0,0 +1,136 @@
+<template>
+  <router-link :to="navigateTo" class="contact-item">
+    <div class="icon-wrap">
+      <fluent-icon icon="chat-multiple" :size="14" />
+    </div>
+    <div class="conversation-details">
+      <div class="meta-wrap">
+        <div class="flex-container ">
+          <woot-label
+            class="conversation-id"
+            :title="`#${id}`"
+            :show-close="false"
+            small
+          />
+          <div class="inbox-name-wrap">
+            <inbox-name :inbox="inbox" class="margin-right-1" />
+          </div>
+        </div>
+        <div>
+          <span>{{ createdAtTime }}</span>
+        </div>
+      </div>
+      <h5 class="text-block-title name">
+        <span class="pre-text">from:</span>
+        {{ name }}
+      </h5>
+      <slot />
+    </div>
+  </router-link>
+</template>
+
+<script>
+import { frontendURL } from 'dashboard/helper/URLHelper.js';
+import timeMixin from 'dashboard/mixins/time';
+import InboxName from 'dashboard/components/widgets/InboxName.vue';
+
+export default {
+  components: {
+    InboxName,
+  },
+  mixins: [timeMixin],
+  props: {
+    id: {
+      type: String,
+      default: '',
+    },
+    email: {
+      type: String,
+      default: '',
+    },
+    inbox: {
+      type: Object,
+      default: () => ({}),
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    thumbnail: {
+      type: String,
+      default: '',
+    },
+    accountId: {
+      type: String,
+      default: '',
+    },
+    createdAt: {
+      type: [String, Date, Number],
+      default: '',
+    },
+  },
+  computed: {
+    navigateTo() {
+      return frontendURL(`accounts/${this.accountId}/contacts/${this.id}`);
+    },
+    createdAtTime() {
+      return this.dynamicTime(this.createdAt);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.contact-item {
+  cursor: pointer;
+  display: flex;
+  padding: var(--space-small);
+  border-radius: var(--border-radius-small);
+
+  &:hover {
+    background-color: var(--s-25);
+  }
+}
+
+.meta-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: var(--space-smaller);
+}
+.icon-wrap {
+  width: var(--space-medium);
+  height: var(--space-medium);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: var(--w-600);
+  border-radius: var(--border-radius-small);
+  background-color: var(--w-75);
+}
+
+.inbox-name-wrap {
+  background-color: var(--s-25);
+  height: var(--space-two);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: var(--border-radius-small);
+  width: fit-content;
+  margin-left: var(--space-smaller);
+}
+.conversation-details {
+  margin-left: var(--space-normal);
+  flex-grow: 1;
+}
+.conversation-id,
+.name {
+  margin: 0;
+}
+
+.pre-text {
+  color: var(--s-600);
+  font-size: var(--font-size-mini);
+  font-weight: var(--font-weight-normal);
+}
+</style>
