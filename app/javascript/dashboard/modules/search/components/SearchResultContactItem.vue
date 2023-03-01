fb0e30fcfe87939cfd955c06e1632010da8b4cf1
Nithin David <1277421+nithindavid@users.noreply.github.com>
2023-03-01 11:45:07 +0530

    feat: Creates component to show contact search results

---
+++ app/javascript/dashboard/modules/search/components/SearchResultContactItem.vue
@@ -0,0 +1,84 @@
+<template>
+  <router-link :to="navigateTo" class="contact-item">
+    <woot-thumbnail :src="thumbnail" :username="name" size="24px" />
+    <div class="contact-details">
+      <h5 class="text-block-title name">{{ name }}</h5>
+      <p class="details-meta">
+        <span v-if="email" class="email">{{ email }}</span>
+        <span v-if="phone" class="separator">·</span>
+        <span v-if="phone" class="phone">
+          {{ phone }}
+        </span>
+      </p>
+    </div>
+  </router-link>
+</template>
+
+<script>
+import { frontendURL } from 'dashboard/helper/URLHelper.js';
+export default {
+  props: {
+    id: {
+      type: String,
+      default: '',
+    },
+    email: {
+      type: String,
+      default: '',
+    },
+    phone: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    thumbnail: {
+      type: String,
+      default: '',
+    },
+    accountId: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    navigateTo() {
+      return frontendURL(`accounts/${this.accountId}/contacts/${this.id}`);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.contact-item {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: var(--space-small);
+  border-radius: var(--border-radius-small);
+
+  &:hover {
+    background-color: var(--s-25);
+  }
+}
+.contact-details {
+  margin-left: var(--space-normal);
+}
+.name {
+  margin: 0;
+}
+.details-meta {
+  margin: 0;
+  color: var(--s-600);
+  font-size: var(--font-size-small);
+  display: flex;
+  align-items: center;
+}
+
+.details-meta > :not([hidden]) ~ :not([hidden]) {
+  margin-right: calc(1rem * 0);
+  margin-left: calc(1rem * calc(1 - 0));
+}
+</style>
