747404a34f5d925c467447bf63b4bfe41f30458d
Shivam Mishra <scm.mymail@gmail.com>
2023-03-06 15:41:36 +0530

    feat: add trigram index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_02_054408) do
+ActiveRecord::Schema.define(version: 2023_03_06_100327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -816,6 +816,7 @@ ActiveRecord::Schema.define(version: 2023_03_02_054408) do
   create_table "tags", id: :serial, force: :cascade do |t|
     t.string "name"
     t.integer "taggings_count", default: 0
+    t.index "lower((name)::text) gin_trgm_ops", name: "tags_name_trgm_idx", using: :gin
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
