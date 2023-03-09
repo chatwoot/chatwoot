d0ba688653ec4e016bd748ef1ad33152c4d1cb5c
Tejaswini Chile <tejaswini@chatwoot.com>
2023-03-09 14:21:35 +0530

    Fix: skip invalid access token from sentry

--- app/services/facebook/send_on_facebook_service.rb
+++ app/services/facebook/send_on_facebook_service.rb
@@ -9,9 +9,12 @@ class Facebook::SendOnFacebookService < Base::SendOnChannelService
     send_message_to_facebook fb_text_message_params if message.content.present?
     send_message_to_facebook fb_attachment_message_params if message.attachments.present?
   rescue Facebook::Messenger::FacebookError => e
-    ChatwootExceptionTracker.new(e, account: message.account, user: message.sender).capture_exception
     # TODO : handle specific errors or else page will get disconnected
-    # channel.authorization_error!
+    if e.message.include?('The session has been invalidated') || e.message.include?('Error validating access token')
+      channel.authorization_error!
+    else
+      ChatwootExceptionTracker.new(e, account: message.account, user: message.sender).capture_exception
+    end
   end
 
   def send_message_to_facebook(delivery_params)
