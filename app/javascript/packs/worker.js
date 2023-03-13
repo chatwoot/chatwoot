2c3f55d0d48ffa4dce6aa142cba5cb70af9d9408
Shivam Mishra <scm.mymail@gmail.com>
2023-03-13 16:41:50 +0530

    feat: setup basic architecture for workers

--- app/javascript/packs/worker.js
+++ app/javascript/packs/worker.js
@@ -1,3 +1,23 @@
-onmessage = e => {
-  postMessage(e);
+const methods = {
+  async syncStorage({ modelName, data }) {
+    return [modelName, data];
+  },
+};
+
+onmessage = async payload => {
+  const { event, params, accountId } = payload.data;
+
+  if (!methods[event]) {
+    throw new Error(`Unknown event: ${event}`);
+  }
+
+  if (!accountId) {
+    throw new Error(`Account ID is not defined`);
+  }
+
+  const response = await methods[event](params);
+  postMessage({
+    event,
+    response,
+  });
 };
