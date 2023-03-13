6b2c7151ed36b8fbdb7958701cc46140f9d185d5
Shivam Mishra <scm.mymail@gmail.com>
2023-03-13 21:44:21 +0530

    feat: cache enabled API client

---
+++ app/javascript/dashboard/api/CacheEnabledApiClient.js
@@ -0,0 +1,59 @@
+/* global axios */
+import { DataManager } from '../../worker/data-manager';
+import ApiClient from './ApiClient';
+
+class CacheEnabledApiClient extends ApiClient {
+  constructor(resource, options = {}) {
+    super(resource, options);
+    this.dataManager = new DataManager(this.accountIdFromRoute);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  get cacheModelName() {
+    throw new Error('cacheModelName is not defined');
+  }
+
+  get(cache = false) {
+    if (cache) {
+      return this.getFromCache();
+    }
+
+    return axios.get(this.url);
+  }
+
+  async getFromCache() {
+    await this.dataManager.initDb();
+    const cachekey = await this.dataManager.getCacheKey(this.cacheModelName);
+    const { data } = await axios.get(
+      `/api/v1/accounts/${this.accountIdFromRoute}`
+    );
+
+    const cacheKeyFromApi = data.cache_keys[this.cacheModelName];
+
+    let localData = [];
+
+    if (cacheKeyFromApi === cachekey) {
+      localData = await this.dataManager.get({
+        modelName: this.cacheModelName,
+      });
+    }
+
+    if (localData.length === 0) {
+      this.dataManager.setCacheKeys({ [this.cacheModelName]: cacheKeyFromApi });
+      return this.refetchAndCommit();
+    }
+    return { data: { payload: localData } };
+  }
+
+  async refetchAndCommit() {
+    await this.dataManager.initDb();
+    const response = await axios.get(this.url);
+    this.dataManager.replace({
+      modelName: this.cacheModelName,
+      data: response.data.payload,
+    });
+    return response;
+  }
+}
+
+export default CacheEnabledApiClient;
