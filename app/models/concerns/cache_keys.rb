704b084173f7f1c2fd94aebca2ed8d63c6ac8c2b
Shivam Mishra <scm.mymail@gmail.com>
2023-03-13 23:14:57 +0530

    feat: add a generic listener

--- app/models/concerns/cache_keys.rb
+++ app/models/concerns/cache_keys.rb
@@ -1,5 +1,6 @@
 module CacheKeys
   extend ActiveSupport::Concern
+  include Events::Types
 
   def get_prefixed_cache_key(key)
     "idb-cache-key-#{self.class.name.underscore}_#{id}_#{key}"
@@ -26,10 +27,18 @@ module CacheKeys
   def invalidate_cache_key_for(key)
     prefixed_cache_key = get_prefixed_cache_key(key)
     Redis::Alfred.del(prefixed_cache_key)
+    dispatch_cache_udpate_event
   end
 
   def update_cache_key(key)
     prefixed_cache_key = get_prefixed_cache_key(key)
     Redis::Alfred.set(prefixed_cache_key, Time.now.utc.to_i)
+    dispatch_cache_udpate_event
+  end
+
+  private
+
+  def dispatch_cache_udpate_event
+    Rails.configuration.dispatcher.dispatch(ACCOUNT_CACHE_INVALIDATED, Time.zone.now, data: cache_keys, account: self)
   end
 end
