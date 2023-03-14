75e67c94f07b252368a5cfa14166635be1200bf1
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2023-03-14 13:09:43 +0530

    feat: Search improvements for conversations  (#6645)
    * feat: Shows search as a popover
    
    * Reverts search from popover to page
    
    * Fixes review comments on usability
    
    * Fixes keyboard navigation issues

--- app/javascript/dashboard/modules/search/components/SearchTabs.vue
+++ app/javascript/dashboard/modules/search/components/SearchTabs.vue
@@ -34,7 +34,7 @@ export default {
 </script>
 <style lang="scss" scoped>
 .tab-container {
-  margin-left: var(--space-minus-normal);
-  margin-top: var(--space-small);
+  margin-top: var(--space-smaller);
+  border-bottom: 1px solid var(--s-50);
 }
 </style>
