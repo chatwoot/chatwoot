2e1dcf8450dc30372021b6f7081b1f86760b0672
Sojan <sojan@pepalo.com>
2023-03-15 16:52:10 +0530

    chore: Add additional indexes

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -26,7 +26,8 @@
 #
 # Indexes
 #
-#  index_inboxes_on_account_id  (account_id)
+#  index_inboxes_on_account_id                   (account_id)
+#  index_inboxes_on_channel_id_and_channel_type  (channel_id,channel_type)
 #
 
 class Inbox < ApplicationRecord
