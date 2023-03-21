1bd65165c68b46bc11a6226496e2baacf7690470
Tejaswini Chile <tejaswini@chatwoot.com>
2023-03-21 22:49:20 +0530

    Fix: Added instagram dashboard icon

--- app/services/instagram/webhooks_base_service.rb
+++ app/services/instagram/webhooks_base_service.rb
@@ -9,23 +9,21 @@ class Instagram::WebhooksBaseService
   def find_or_create_contact(user)
     @contact_inbox = @inbox.contact_inboxes.where(source_id: user['id']).first
     @contact = @contact_inbox.contact if @contact_inbox
+    update_instagram_profile_link(user)
     return if @contact
 
     @contact_inbox = @inbox.channel.create_contact_inbox(
       user['id'], user['name']
     )
 
-    update_instagram_profile_link(user)
     @contact = @contact_inbox.contact
     Avatar::AvatarFromUrlJob.perform_later(@contact, user['profile_pic']) if user['profile_pic']
   end
 
-  def update_instagram_profile_link
+  def update_instagram_profile_link(user)
     return unless user['username']
 
-    @contact.additional_attributes = @contact.additional_attributes.merge({
-                                                                            social_profiles: { instagram: user['username'] }
-                                                                          })
+    @contact.additional_attributes = @contact.additional_attributes.merge({ 'social_profiles': { 'instagram': user['username'] } })
     @contact.save
   end
 end
