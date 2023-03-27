ef797990854410f699b87c1987e61b9fbbab9981
Shivam Mishra <scm.mymail@gmail.com>
2023-03-27 12:24:18 +0530

    feat: annotate with comments

--- app/helpers/api/v2/accounts/heatmap_helper.rb
+++ app/helpers/api/v2/accounts/heatmap_helper.rb
@@ -15,22 +15,51 @@ module Api::V2::Accounts::HeatmapHelper
   private
 
   def group_traffic_data(data)
+    # start with an empty array
     result_arr = []
 
-    dates = data.pluck(:date).uniq.sort # Get unique dates in ascending order
+    # pick all the unique dates from the data in ascending order
+    dates = data.pluck(:date).uniq.sort
 
+    # add the dates as the first row, leave an empty cell for the hour column
+    # e.g. [nil, '2023-01-01', '2023-1-02', '2023-01-03']
     result_arr << ([nil] + dates)
 
-    data.group_by { |d| d[:hour] }.each do |hour, items| # Group data by hour
+    # group the data by hour, we do not need to sort it, because the data is already sorted
+    # given it starts from the beginning of the day
+    # here each hour is a key, and the value is an array of all the items for that hour at each date
+    # e.g. hour = 1
+    # value = [{date: 2023-01-01, value: 1}, {date: 2023-01-02, value: 1}, {date: 2023-01-03, value: 1}, ...]
+    data.group_by { |d| d[:hour] }.each do |hour, items|
+      # create a new row for each hour
       row = [hour]
+
+      # group the items by date, so we can easily access the value for each date
+      # grouped values will be a hasg with the date as the key, and the value as the value
+      # e.g. { '2023-01-01' => [{date: 2023-01-01, value: 1}], '2023-01-02' => [{date: 2023-01-02, value: 1}], ... }
       grouped_values = items.group_by { |d| d[:date] }
+
+      # now for each unique date we have, we can access the value for that date and append it to the array
       dates.each do |date|
         row << (grouped_values[date][0][:value] if grouped_values[date].is_a?(Array))
       end
 
+      # row will look like [22, 0, 0, 1, 4, 6, 7, 4]
+      # add the row to the result array
+
       result_arr << row
     end
 
+    # return the resultant array
+    # the result looks like this
+    # [
+    #   [nil, '2023-01-01', '2023-1-02', '2023-01-03'],
+    #   [0, 0, 0, 0],
+    #   [1, 0, 0, 0],
+    #   [2, 0, 0, 0],
+    #   [3, 0, 0, 0],
+    #   [4, 0, 0, 0],
+    # ]
     result_arr
   end
 
