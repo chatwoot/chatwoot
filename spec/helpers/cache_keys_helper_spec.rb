9b00ce682671c28e4cd0e9b3d1694958c60c133f
Shivam Mishra <scm.mymail@gmail.com>
2023-03-27 12:16:25 +0530

    feat: IndexedDB based caching for labels, inboxes and teams [CW-50] (#6710)
    * feat: allow caching of labels in the account scope
    
    * feat: send cache keys in account json response
    
    * feat: kickstart web worker
    
    * feat: setup basic architecture for workers
    
    * feat: install idb
    
    * feat: add datamanger
    
    * fix: typos
    
    * refactor: rename method
    
    * feat: make init db a manual step
    
    * refactor: separate accountIdFromRoute
    
    * feat: cache enabled API client
    
    * feat: enable caching for inboxes and labels
    
    * feat: enable cache for team
    
    * feat: manage exceptions for team
    
    * feat: add team to data manager
    
    * feat: add a generic listener
    
    * refactor: send only cache keys
    
    * refactor: separate validate method
    
    * feat: add listeners
    
    * feat: add event for revalidate
    
    * feat: add cache keys endpoint
    
    * refactor: fetch cache keys instead of full account data
    
    * fix: key pattern
    
    * feat: don't fetch account for cache_keys
    
    * fix: cache key base class
    
    * refactor: cache keys helper
    
    * feat: add helper
    
    * fix: cache-key update logic
    
    * feat: delete indexeddb on logout
    
    * feat: remove worker.js
    
    * refactor: move data-manager
    
    * refactor: name of file
    
    * feat: add test for DataManager
    
    * refactor: add fake idb to jest setup
    
    * test: cache keys helper
    
    * test: cache keys helper
    
    * test: cache_keys in accounts controller
    
    * refactor: remove cache_keys context
    
    * feat: add policy for cache-keys

---
+++ spec/helpers/cache_keys_helper_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe CacheKeysHelper, type: :helper do
+  let(:account_id) { 1 }
+  let(:key) { 'example_key' }
+
+  describe '#get_prefixed_cache_key' do
+    it 'returns a string with the correct prefix, account ID, and key' do
+      expected_key = "idb-cache-key-account-#{account_id}-#{key}"
+      result = helper.get_prefixed_cache_key(account_id, key)
+
+      expect(result).to eq(expected_key)
+    end
+  end
+
+  describe '#fetch_value_for_key' do
+    it 'returns the zero epoch time if no value is cached' do
+      result = helper.fetch_value_for_key(account_id, 'another-key')
+
+      expect(result).to eq('0000000000')
+    end
+
+    it 'returns a cached value if it exists' do
+      value = Time.now.to_i
+      prefixed_cache_key = helper.get_prefixed_cache_key(account_id, key)
+
+      Redis::Alfred.set(prefixed_cache_key, value)
+
+      result = helper.fetch_value_for_key(account_id, key)
+
+      expect(result).to eq(value.to_s)
+    end
+  end
+end
