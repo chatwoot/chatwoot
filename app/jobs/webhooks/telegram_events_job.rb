3280e7250933f7dd187a8609111e5c9f38a9d9b5
Sojan <sojan@pepalo.com>
2023-03-28 19:05:49 +0530

    chore: clean up logic and specs

--- app/jobs/webhooks/telegram_events_job.rb
+++ app/jobs/webhooks/telegram_events_job.rb
@@ -7,15 +7,6 @@ class Webhooks::TelegramEventsJob < ApplicationJob
     channel = Channel::Telegram.find_by(bot_token: params[:bot_token])
     return unless channel
 
-    if params['telegram']['callback_query'].present?
-      param = params['telegram']['callback_query']
-      params['telegram']['callback_query']['message']['text'] = params['telegram']['callback_query']['data']
-      params['telegram']['callback_query']['message']['username'] = params['telegram']['callback_query']['from']['username']
-      params['telegram']['callback_query']['message']['from']['id'] = params['telegram']['callback_query']['from']['id']
-
-    else
-      param = params['telegram']
-    end
-    Telegram::IncomingMessageService.new(inbox: channel.inbox, params: param.with_indifferent_access).perform
+    Telegram::IncomingMessageService.new(inbox: channel.inbox, params: params['telegram'].with_indifferent_access).perform
   end
 end
