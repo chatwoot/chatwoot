1fa30e58b3601abe4cddaceca727064d206771fa
Sebastián Arjona <sebasarjona@outlook.com>
2023-03-28 21:07:49 -0600

    Feature | Add Slack notification to Staging deployment (#26)

---
+++ scripts/build_slack_deployment_status_notification_payload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get deploy environmet
+if [[ $BRANCH_NAME = 'trunk' ]]; then
+    deploy_environment="production"
+elif [[ $BRANCH_NAME = 'develop' ]]; then
+    deploy_environment="staging"
+else
+    deploy_environment="development"
+fi
+
+# Get GithubActionURL
+github_action_url="https://github.com/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
+
+echo "::set-output name=payload::{ \"repository_name\": \"$GITHUB_REPOSITORY\", \"enviroment\": \"$deploy_environment\", \"status\": \"$JOB_STATUS\", \"action_url\": \"$github_action_url\" }"
