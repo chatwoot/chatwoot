aa75666ad9e1f930047bb37d02dce64d23b40c3d
Jordan Brough <jordan-brough@users.noreply.github.com>
2023-03-29 00:54:14 -0600

    chore: Remove extra audit column (#6769)
    - Drop unintentional "audits.integer" column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_27_081350) do
+ActiveRecord::Schema.define(version: 2023_03_28_131926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
