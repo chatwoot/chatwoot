027226c61081fca4fd9de09e11717341fec246e9
Sojan <sojan@pepalo.com>
2023-03-29 18:28:03 +0530

    chore: code climate fix

--- app/jobs/webhooks/telegram_events_job.rb
+++ app/jobs/webhooks/telegram_events_job.rb
@@ -3,7 +3,6 @@ class Webhooks::TelegramEventsJob < ApplicationJob
 
   def perform(params = {})
     return unless params[:bot_token]
-    return unless params[:telegram]
 
     channel = Channel::Telegram.find_by(bot_token: params[:bot_token])
     return unless channel
@@ -14,6 +13,8 @@ class Webhooks::TelegramEventsJob < ApplicationJob
   private
 
   def process_event_params(channel, params)
+    return unless params[:telegram]
+
     if params.dig(:telegram, :edited_message).present?
       Telegram::UpdateMessageService.new(inbox: channel.inbox, params: params['telegram'].with_indifferent_access).perform
     else
