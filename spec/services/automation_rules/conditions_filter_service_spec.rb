63c9ed764535d59a371c0c78a331de7f546934c8
Tejaswini Chile <tejaswini@chatwoot.com>
2023-03-30 22:28:23 +0530

    fix: Specs

--- spec/services/automation_rules/conditions_filter_service_spec.rb
+++ spec/services/automation_rules/conditions_filter_service_spec.rb
@@ -6,21 +6,45 @@ RSpec.describe AutomationRules::ConditionsFilterService do
   let(:rule) { create(:automation_rule, account: account) }
 
   describe '#perform' do
-    before do
-      rule.conditions = [{ 'values': ['open'], 'attribute_key': 'status', 'query_operator': nil, 'filter_operator': 'equal_to' }]
-      rule.save
-    end
+    context 'when conditions based on status' do
+      before do
+        rule.conditions = [{ 'values': ['open'], 'attribute_key': 'status', 'query_operator': nil, 'filter_operator': 'equal_to' }]
+        rule.save
+      end
 
-    context 'when conditions in rule matches with object' do
-      it 'will return true' do
-        expect(described_class.new(rule, conversation, { changed_attributes: { status: [nil, 'open'] } }).perform).to be(true)
+      context 'when conditions in rule matches with object' do
+        it 'will return true' do
+          expect(described_class.new(rule, conversation, { changed_attributes: { status: [nil, 'open'] } }).perform).to be(true)
+        end
+      end
+
+      context 'when conditions in rule does not match with object' do
+        it 'will return false' do
+          conversation.update(status: 'resolved')
+          expect(described_class.new(rule, conversation, { changed_attributes: { status: %w[open resolved] } }).perform).to be(false)
+        end
       end
     end
 
-    context 'when conditions in rule does not match with object' do
-      it 'will return false' do
-        conversation.update(status: 'resolved')
-        expect(described_class.new(rule, conversation, { changed_attributes: { status: %w[open resolved] } }).perform).to be(false)
+    context 'when conditions based on phone_number' do
+      before do
+        contact = conversation.contact
+        contact.update(phone_number: '+918484848484')
+        rule.conditions = [{ 'values': ['+918484'], 'attribute_key': 'phone_number', 'query_operator': nil, 'filter_operator': 'starts_with' }]
+        rule.save
+      end
+
+      context 'when conditions in rule matches with object' do
+        it 'will return true' do
+          expect(described_class.new(rule, conversation, { changed_attributes: {} }).perform).to be(true)
+        end
+      end
+
+      context 'when conditions in rule does not match with object' do
+        it 'will return false' do
+          conversation.contact.update(phone_number: '+918585858585')
+          expect(described_class.new(rule, conversation, { changed_attributes: {} }).perform).to be(false)
+        end
       end
     end
   end
