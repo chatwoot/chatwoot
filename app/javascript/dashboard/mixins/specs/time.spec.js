48ac38acb75a95a91565a34fa2dc2abfd887a677
iamsivin <iamsivin@gmail.com>
2023-04-05 13:18:55 +0530

    feat: Shows year in message timestamp if the message is not from the current year

--- app/javascript/dashboard/mixins/specs/time.spec.js
+++ app/javascript/dashboard/mixins/specs/time.spec.js
@@ -9,6 +9,20 @@ describe('#messageStamp', () => {
   });
 });
 
+describe('#conversationMessageStamp', () => {
+  it('should return the message date in the specified format if the message was sent in the current year', () => {
+    const now = new Date();
+    expect(
+      TimeMixin.methods.conversationMessageStamp(now.getTime() / 1000)
+    ).toEqual('Apr 5, 2023');
+  });
+  it('should return the message date and time in a different format if the message was sent in a different year', () => {
+    expect(TimeMixin.methods.conversationMessageStamp(1612971343)).toEqual(
+      'Feb 10 2021, 3:35 PM'
+    );
+  });
+});
+
 describe('#dynamicTime', () => {
   it('returns correct value', () => {
     expect(TimeMixin.methods.dynamicTime(1612971343)).toEqual(
