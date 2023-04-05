df2a31c39a97e29ec8dab79a16c9acbd27787aa0
Shivam Mishra <scm.mymail@gmail.com>
2023-04-05 16:24:14 +0530

    feat: add migration to reorder all articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_28_131926) do
+ActiveRecord::Schema.define(version: 2023_04_05_104405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
