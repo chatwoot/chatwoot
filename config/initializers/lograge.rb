6ec24bf1317f7d8a4d5b09aa48af447d8913997a
Sojan <sojan@pepalo.com>
2023-04-07 13:14:57 +0530

    chore: refactor

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,24 +1,28 @@
-Rails.application.configure do
-  config.lograge.enabled = true
-  config.lograge.keep_original_rails_log = true
-  config.lograge.formatter = Lograge::Formatters::Json.new
-  config.colorize_logging = false
-  config.lograge.custom_payload do |controller|
-    {
-      host: controller.request.host,
-      remote_ip: controller.request.remote_ip,
-      user_id: controller.current_user.try(:id)
-    }
-  end
+if ActiveModel::Type::Boolean.new.cast(ENV.fetch('LOGRAGE_ENABLED', false)).present?
+  require 'lograge'
 
-  config.lograge.custom_options = lambda do |event|
-    {
-      level: event.payload[:level]
-    }
-  end
+  Rails.application.configure do
+    config.lograge.enabled = true
+    config.lograge.formatter = Lograge::Formatters::Json.new
 
-  config.lograge.ignore_custom = lambda do |event|
-    # ignore update_presence  events in log
-    return true if event.payload[:channel_class] == 'RoomChannel'
+    config.lograge.custom_payload do |controller|
+      {
+        host: controller.request.host,
+        remote_ip: controller.request.remote_ip,
+        user_id: controller.current_user.try(:id)
+      }
+    end
+
+    config.lograge.custom_options = lambda do |event|
+      param_exceptions = %w[controller action format id]
+      {
+        params: event.payload[:params]&.except(*param_exceptions)
+      }
+    end
+
+    config.lograge.ignore_custom = lambda do |event|
+      # ignore update_presence  events in log
+      return true if event.payload[:channel_class] == 'RoomChannel'
+    end
   end
 end
