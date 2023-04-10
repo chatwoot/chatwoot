dd17da8d50299353dec82b060bda8cca4c5ab1f4
Tejaswini Chile <tejaswini@chatwoot.com>
2023-04-10 16:31:28 +0530

    Fix: Added specs

--- app/models/message.rb
+++ app/models/message.rb
@@ -257,7 +257,7 @@ class Message < ApplicationRecord
     # mark resolved bot conversation as pending to be reopened by bot processor service
     if conversation.inbox.active_bot?
       conversation.pending!
-    else
+    elsif conversation.inbox.api?
       Current.executed_by = sender if reopened_by_contact?
       conversation.open!
     end
