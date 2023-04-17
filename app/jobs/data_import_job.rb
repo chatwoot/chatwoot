65a70ceedf0e19f0de6e39d5d10899c8765a2bf0
Sojan <sojan@pepalo.com>
2023-04-17 15:54:27 +0530

    chore: refactor the import jobs

--- app/jobs/data_import_job.rb
+++ app/jobs/data_import_job.rb
@@ -5,10 +5,26 @@ class DataImportJob < ApplicationJob
   queue_as :low
 
   def perform(data_import)
+    @data_import = data_import
+    process_import_file
+    send_failed_records_to_admin
+  end
+
+  private
+
+  def process_import_file
+    @data_import.update!(status: :processing)
+
+    contacts, rejected_contacts = parse_csv_and_build_contacts
+
+    import_contacts(contacts)
+    update_data_import_status(contacts.length, rejected_contacts.length)
+    save_failed_records_csv(rejected_contacts)
+  end
+
+  def parse_csv_and_build_contacts
     contacts = []
     rejected_contacts = []
-    @data_import = data_import
-    @data_import.update!(status: :processing)
     csv = CSV.parse(@data_import.import_file.download, headers: true)
 
     csv.each do |row|
@@ -16,103 +32,91 @@ class DataImportJob < ApplicationJob
       current_contact.valid? ? contacts << current_contact : rejected_contacts << current_contact
     end
 
-    # <struct ActiveRecord::Import::Result failed_instances=[], num_inserts=1, ids=[444, 445], results=[]>
-    Contact.import(contacts, synchronise: contacts, on_duplicate_key_ignore: true, track_validation_failures: true, validate: true,
-                             batch_size: 1000)
-
-    rejected_contacts = rejected_contacts.flatten
-
-    @data_import.update!(status: :completed, processed_records: (csv.length - rejected_contacts.length), total_records: csv.length)
-    save_failed_records_csv(rejected_contacts)
+    [contacts, rejected_contacts]
   end
 
-  private
+  def import_contacts(contacts)
+    # <struct ActiveRecord::Import::Result failed_instances=[], num_inserts=1, ids=[444, 445], results=[]>
+    Contact.import(contacts, synchronize: contacts, on_duplicate_key_ignore: true, track_validation_failures: true, validate: true, batch_size: 1000)
+  end
+
+  def update_data_import_status(processed_records, rejected_records)
+    @data_import.update!(status: :completed, processed_records: processed_records, total_records: processed_records + rejected_records)
+  end
 
   def build_contact(params, account)
-    # TODO: rather than doing the find or initialize individually lets fetch objects in bulk and update them in memory
-    contact = init_contact(params, account)
-
+    contact = find_or_initialize_contact(params, account)
     contact.name = params[:name] if params[:name].present?
     contact.assign_attributes(custom_attributes: contact.custom_attributes.merge(params.except(:identifier, :email, :name, :phone_number)))
     contact
   end
 
-  # add the phone number check here
-  def get_exisiting_contacts(params, account)
-    identifier_contact = account.contacts.find_by(identifier: params[:identifier]) if params[:identifier]
-    email_contact = account.contacts.find_by(email: params[:email]) if params[:email]
-    phone_number_contact = account.contacts.find_by(phone_number: params[:phone_number]) if params[:phone_number]
-    contact = merged_contact_record(params, [identifier_contact, email_contact, phone_number_contact])
-
-    # On, already existing records, we merge contact but import! reject those contact so we are saving those merged contacts here
-    contact.save if contact.present? && contact.valid?
+  def find_or_initialize_contact(params, account)
+    contact = find_existing_contact(params, account)
+    contact ||= account.contacts.new(params.slice(:email, :identifier, :phone_number))
     contact
   end
 
-  def merged_contact_record(params, available_contacts)
-    identifier_contact, email_contact, phone_number_contact = available_contacts
-    contact = merge_identified_contact(params, [identifier_contact, email_contact])
-    contact ||= merge_email_contact(params, email_contact)
-    contact ||= merge_phone_number_contact(params, phone_number_contact)
+  def find_existing_contact(params, account)
+    contact = find_contact_by_identifier(params, account)
+    contact ||= find_contact_by_email(params, account)
+    contact ||= find_contact_by_phone_number(params, account)
+
+    update_contact_with_merged_attributes(params, contact) if contact.present? && contact.valid?
     contact
   end
 
-  # Find contact with email and update it with phone number
-  def merge_email_contact(params, email_contact)
-    contact = email_contact
-    contact&.phone_number = params[:phone_number] if params[:phone_number].present?
-    contact
+  def find_contact_by_identifier(params, account)
+    return unless params[:identifier]
+
+    account.contacts.find_by(identifier: params[:identifier])
   end
 
-  # Find contact with phone_number and update it with email
-  def merge_phone_number_contact(params, phone_number_contact)
-    contact = phone_number_contact
-    contact&.email = params[:email] if params[:email].present?
-    contact
+  def find_contact_by_email(params, account)
+    return unless params[:email]
+
+    account.contacts.find_by(email: params[:email])
   end
 
-  # intiating the new contact / contact attributes only by ensuring the identifier, email or phone_number duplication errors won't occur
-  def merge_identified_contact(params, available_contacts)
-    identifier_contact, email_contact = available_contacts
-    contact = identifier_contact
-    contact&.email = params[:email] if params[:email].present? && email_contact.blank?
-    contact&.phone_number = params[:phone_number] if params[:phone_number].present?
-    contact
+  def find_contact_by_phone_number(params, account)
+    return unless params[:phone_number]
+
+    account.contacts.find_by(phone_number: params[:phone_number])
+  end
+
+  def update_contact_with_merged_attributes(params, contact)
+    contact.email = params[:email] if params[:email].present?
+    contact.phone_number = params[:phone_number] if params[:phone_number].present?
+    contact.save
   end
 
   def save_failed_records_csv(rejected_contacts)
-    if rejected_contacts.any?
-      csv_data = CSV.generate do |csv|
-        csv << %w[id first_name last_name email phone_number identifier gender errors]
-        rejected_contacts.each do |record|
-          csv << [
-            record['id'],
-            record.custom_attributes['first_name'],
-            record.custom_attributes['last_name'],
-            record['email'],
-            record['phone_number'],
-            record['identifier'],
-            record.custom_attributes['gender'],
-            record.errors.full_messages.join(',')
-          ]
-        end
+    return unless rejected_contacts.any?
+
+    csv_data = generate_csv_data(rejected_contacts)
+    @data_import.failed_records.attach(io: StringIO.new(csv_data), filename: "#{Time.zone.today.strftime('%Y%m%d')}_contacts.csv",
+                                       content_type: 'text/csv')
+  end
+
+  def generate_csv_data(rejected_contacts)
+    CSV.generate do |csv|
+      csv << %w[id first_name last_name email phone_number identifier gender errors]
+      rejected_contacts.each do |record|
+        csv << [
+          record['id'],
+          record.custom_attributes['first_name'],
+          record.custom_attributes['last_name'],
+          record['email'],
+          record['phone_number'],
+          record['identifier'],
+          record.custom_attributes['gender'],
+          record.errors.full_messages.join(',')
+        ]
       end
     end
-
-    send_failed_records_to_admin(csv_data)
   end
 
-  def send_failed_records_to_admin(csv_data)
-    if csv_data.present?
-      @data_import.failed_records.attach(io: StringIO.new(csv_data), filename: "#{Time.zone.today.strftime('%Y%m%d')}_contacts.csv",
-                                         content_type: 'text/csv')
-    end
+  def send_failed_records_to_admin
     AdministratorNotifications::ChannelNotificationsMailer.with(account: @data_import.account).failed_records(@data_import).deliver_later
-  end
-
-  def init_contact(params, account)
-    contact = get_exisiting_contacts(params, account)
-    contact ||= account.contacts.new(params.slice(:email, :identifier, :phone_number))
-    contact
   end
 end
