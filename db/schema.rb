b8fe8cf424efad7bfe6df3adf8baaa6b48b83219
Shivam Mishra <scm.mymail@gmail.com>
2023-04-17 14:43:10 +0530

    feat: allow sorting of articles (#6833)
    * feat: sort by position
    
    * chore: whitespace change
    
    * feat: add border bottom color to list item
    
    * feat: allow dragging articles
    
    * feat: add migration to reorder all articles
    
    * feat: add onsort method
    
    * feat: finish UI sorting
    
    * feat: show 50 per page in articles list
    
    * feat: add article sorting methods
    
    * feat: patch up reorder action with the API
    
    * refactor: better naming
    
    * chore: add comments
    
    * feat: attach position to article before create
    
    * feat: move article to end if moved between categories
    
    * chore: add comments
    
    * chore: update version
    
    * fix: don't change position if previous category was nil
    
    * fix: condition to trigger update on category change
    
    * refactor: store new_position
    
    * refactor: use grid instead of table
    
    * feat: add snug spacing
    
    * feat: add grab-icon
    
    * feat: add grab icon to list
    
    * refactor: show draggable only for category page
    
    * feat: add update_positions as a class method
    
    ---------
    
    Co-authored-by: Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -437,7 +437,7 @@ ActiveRecord::Schema.define(version: 2023_04_07_191457) do
     t.datetime "agent_last_seen_at"
     t.jsonb "additional_attributes", default: {}
     t.bigint "contact_inbox_id"
-    t.uuid "uuid", default: -> { "public.gen_random_uuid()" }, null: false
+    t.uuid "uuid", default: -> { "gen_random_uuid()" }, null: false
     t.string "identifier"
     t.datetime "last_activity_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.bigint "team_id"
