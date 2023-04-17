ff9f868e14b349aa630fcac2c7dc017ad01ae765
Vishnu Narayanan <vishnu@chatwoot.com>
2023-04-17 17:28:06 +0530

    chore: fix dockerbuild

--- config/initializers/git_sha.rb
+++ config/initializers/git_sha.rb
@@ -3,8 +3,8 @@ def fetch_git_sha
   sha = `git rev-parse HEAD`
   if sha.present?
     sha.strip
-  elsif File.exist?('.git-sha')
-    File.read('.git-sha').strip
+  elsif File.exist?('.git_sha')
+    File.read('.git_sha').strip
   else
     'unknown'
   end
