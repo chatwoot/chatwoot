8404f8c4ad8e55da5e8dff6beb73a58f4bd4ba9b
Sojan <sojan@pepalo.com>
2023-04-17 15:37:02 +0530

    chore: Improve the specs

--- spec/factories/data_import.rb
+++ spec/factories/data_import.rb
@@ -3,13 +3,5 @@ FactoryBot.define do
     data_type { 'contacts' }
     import_file { Rack::Test::UploadedFile.new(Rails.root.join('spec/assets/contacts.csv'), 'text/csv') }
     account
-
-    trait :invalid_data_import do
-      import_file { Rack::Test::UploadedFile.new(Rails.root.join('spec/assets/invalid_contacts.csv'), 'text/csv') }
-    end
-
-    trait :existing_data_import do
-      import_file { Rack::Test::UploadedFile.new(Rails.root.join('spec/assets/existing_contacts.csv'), 'text/csv') }
-    end
   end
 end
