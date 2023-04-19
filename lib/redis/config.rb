ccbdeba22bb599149dcfa898043280c38636dd58
Sojan <sojan@pepalo.com>
2023-04-19 22:25:04 +0530

    chore: update timeouts

--- lib/redis/config.rb
+++ lib/redis/config.rb
@@ -14,7 +14,8 @@ module Redis::Config
         url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'),
         password: ENV.fetch('REDIS_PASSWORD', nil).presence,
         ssl_params: { verify_mode: Chatwoot.redis_ssl_verify_mode },
-        reconnect_attempts: 2
+        reconnect_attempts: 2,
+        timeout: 1
       }
     end
 
