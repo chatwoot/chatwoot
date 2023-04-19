092e4d1523561177108dfd5b882b61225cf2e06d
Pranav Raj S <pranav@chatwoot.com>
2023-04-18 18:30:01 -0700

    fix: Remove fallback phone_number search in WhatsApp event processing (#6904)

--- spec/factories/channel/channel_whatsapp.rb
+++ spec/factories/channel/channel_whatsapp.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :channel_whatsapp, class: 'Channel::Whatsapp' do
     sequence(:phone_number) { |n| "+123456789#{n}1" }
     account
-    provider_config { { 'api_key' => 'test_key' } }
+    provider_config { { 'api_key' => 'test_key', 'phone_number_id' => 'random_id' } }
     message_templates do
       [{ 'name' => 'sample_shipping_confirmation',
          'status' => 'approved',
