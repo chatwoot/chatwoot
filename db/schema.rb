36bee21ab94e8e17c8deac8e66370687f30f06ee
Sojan <sojan@pepalo.com>
2023-04-19 16:39:05 +0530

    chore: bump ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_18_100944) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_100944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
