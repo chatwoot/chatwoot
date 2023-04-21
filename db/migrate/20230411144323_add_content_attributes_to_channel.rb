2a2490ec9a4bde4e667b44392b16e4346a321193
Sojan Jose <sojan@pepalo.com>
2023-04-21 17:39:15 +0530

    Delete 20230411144323_add_content_attributes_to_channel.rb

--- db/migrate/20230411144323_add_content_attributes_to_channel.rb
+++
@@ -1,9 +0,0 @@
-class AddContentAttributesToChannel < ActiveRecord::Migration[6.1]
-  def up
-    add_column :channel_facebook_pages, :content_attributes, :jsonb, default: {}
-  end
-
-  def down
-    remove_column :channel_facebook_pages, :content_attributes, :jsonb
-  end
-end
