bed39f26a4154cb5ef8736647248ebd657106d10
Tejaswini Chile <tejaswini@chatwoot.com>
2023-04-21 17:10:46 +0530

    Greeting enabled with contact name string interpolation

--- app/services/message_templates/template/greeting.rb
+++ app/services/message_templates/template/greeting.rb
@@ -17,6 +17,7 @@ class MessageTemplates::Template::Greeting
 
   def greeting_message_params
     content = @conversation.inbox&.greeting_message
+    content = string_interpolation_on_content
 
     {
       account_id: @conversation.account_id,
@@ -24,5 +25,16 @@ class MessageTemplates::Template::Greeting
       message_type: :template,
       content: content
     }
+  end
+
+  def string_interpolation_on_content
+    binding.pry
+    content.match?/{{.*?}}/
+    email_text = content.gsub(/\{\{(.*?)\}\}/) { Regexp.last_match(1) }
+    association, attribute = email_text.split('.')
+    record = conversation.send(association)
+    contacts_name = record.try(attribute)
+
+    content.gsub(/{{.*?}}/, contacts_name)
   end
 end
