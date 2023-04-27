d5ff7e7ce6215837c08f6b24c9f4ed122680b6e9
Tejaswini Chile <tejaswini@chatwoot.com>
2023-04-27 20:43:28 +0530

    remove migration

--- db/migrate/20230426195724_add_message_source_identifier_to_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddMessageSourceIdentifierToMessages < ActiveRecord::Migration[6.1]
-  def change
-    add_column :messages, :message_source_identifier, :string, default: nil, null: true
-  end
-end
