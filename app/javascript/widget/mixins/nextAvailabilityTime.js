4d5a1e4733261dfb2ab369de8d3892e4652001a7
iamsivin <iamsivin@gmail.com>
2023-05-02 19:54:36 +0530

    chore: Review fixes

--- app/javascript/widget/mixins/nextAvailabilityTime.js
+++ app/javascript/widget/mixins/nextAvailabilityTime.js
@@ -7,15 +7,15 @@ const { utcToZonedTime } = require('date-fns-tz');
 export default {
   data() {
     return {
-      dayNames: {
-        0: 'Sunday',
-        1: 'Monday',
-        2: 'Tuesday',
-        3: 'Wednesday',
-        4: 'Thursday',
-        5: 'Friday',
-        6: 'Saturday',
-      },
+      dayNames: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ],
       timeSlots: [...defaultTimeSlot],
       timeSlot: {},
     };
--- app/javascript/widget/mixins/specs/nextAvailabilityTime.spec.js
+++ app/javascript/widget/mixins/specs/nextAvailabilityTime.spec.js
@@ -64,15 +64,15 @@ describe('nextAvailabilityTimeMixin', () => {
   });
 
   it('should return day names', () => {
-    expect(wrapper.vm.dayNames).toEqual({
-      0: 'Sunday',
-      1: 'Monday',
-      2: 'Tuesday',
-      3: 'Wednesday',
-      4: 'Thursday',
-      5: 'Friday',
-      6: 'Saturday',
-    });
+    expect(wrapper.vm.dayNames).toEqual([
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ]);
   });
 
   it('should return channelConfig', () => {
