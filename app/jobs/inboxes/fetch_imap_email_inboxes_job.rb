c98b85da612ecc72615ba4e7e6e9b6f46541df5c
Tejaswini Chile <tejaswini@chatwoot.com>
2023-05-02 08:47:35 +0530

    minor fixes for multi tenant app

--- app/jobs/inboxes/fetch_imap_email_inboxes_job.rb
+++ app/jobs/inboxes/fetch_imap_email_inboxes_job.rb
@@ -2,7 +2,10 @@ class Inboxes::FetchImapEmailInboxesJob < ApplicationJob
   queue_as :low
 
   def perform
-    Inbox.where(channel_type: 'Channel::Email', imap_enabled: true).all.find_each(batch_size: 100) do |inbox|
+    # check imap_enabled for channel
+    Inbox.where(channel_type: 'Channel::Email').all.find_each(batch_size: 100) do |inbox|
+      next unless inbox.channel.imap_enabled?
+
       if inbox.channel.microsoft? && ENV.fetch('AZURE_TENANT_ID', false)
         ::Inboxes::FetchMsGraphEmailsJob.perform_later(inbox.channel)
       else
