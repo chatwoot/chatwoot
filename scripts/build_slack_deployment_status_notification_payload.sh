55fda9401ad26f6aa0d2878d6234d9fc865f2002
Sebastián Arjona <sebasarjona@outlook.com>
2023-05-02 11:54:13 -0600

    Maintenance | Update Slack script output (#69)

--- scripts/build_slack_deployment_status_notification_payload.sh
+++ scripts/build_slack_deployment_status_notification_payload.sh
@@ -12,4 +12,4 @@ fi
 # Get GithubActionURL
 github_action_url="https://github.com/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
 
-echo "::set-output name=payload::{ \"repository_name\": \"$GITHUB_REPOSITORY\", \"enviroment\": \"$deploy_environment\", \"status\": \"$JOB_STATUS\", \"action_url\": \"$github_action_url\" }"
+echo "payload={ \"repository_name\": \"$GITHUB_REPOSITORY\", \"enviroment\": \"$deploy_environment\", \"status\": \"$JOB_STATUS\", \"action_url\": \"$github_action_url\" }" >> $GITHUB_OUTPUT
