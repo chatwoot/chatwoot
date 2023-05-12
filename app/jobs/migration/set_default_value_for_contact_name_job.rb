18d9271891cea5d78ea255a3046382b688ce59b7
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-05-12 09:56:49 +0530

    Remove the comment

--- app/jobs/migration/set_default_value_for_contact_name_job.rb
+++ app/jobs/migration/set_default_value_for_contact_name_job.rb
@@ -3,7 +3,6 @@ class Migration::SetDefaultValueForContactNameJob < ApplicationJob
 
   def perform(account)
     account.contacts.each do |contact|
-      # Set default value for contact name if it is blank
       # rubocop:disable Rails/SkipsModelValidations
       contact.update_columns(name: '') if contact.name.blank?
       # rubocop:enable Rails/SkipsModelValidations
