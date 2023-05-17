37fad2a3e40086df6a7e1cba70b18394f171c9fe
Tejaswini Chile <tejaswini@chatwoot.com>
2023-05-17 15:34:37 +0530

    Send on slack service

--- lib/integrations/slack/send_on_slack_service.rb
+++ lib/integrations/slack/send_on_slack_service.rb
@@ -79,6 +79,7 @@ class Integrations::Slack::SendOnSlackService < Base::SendOnChannelService
 
   def send_message
     post_message if message_content.present?
+    sleep(3.seconds)
     upload_file if message.attachments.any?
   rescue Slack::Web::Api::Errors::AccountInactive => e
     Rails.logger.error e
