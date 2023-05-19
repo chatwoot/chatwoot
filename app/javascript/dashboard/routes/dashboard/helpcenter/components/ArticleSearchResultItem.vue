f3d477cd2ac47c6223ff67ae06dc85e66eab717e
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2023-05-19 11:24:15 +0530

    Adds story for the component

--- app/javascript/dashboard/routes/dashboard/helpcenter/components/ArticleSearchResultItem.vue
+++ app/javascript/dashboard/routes/dashboard/helpcenter/components/ArticleSearchResultItem.vue
@@ -1,42 +1,63 @@
 <template>
-  <div>
-    <h4 class="text-block-title">{{ title }}</h4>
-    <p>{{ body }}</p>
-    <div>
-      <p class="text-small">{{ locale }}/{{ category || 'Uncategorized' }}</p>
+  <div class="article-item">
+    <h4 class="text-block-title margin-bottom-0">{{ title }}</h4>
+    <p class="margin-bottom-0 text-truncate">{{ body }}</p>
+    <div class="footer">
+      <p class="text-small meta">
+        {{ locale }}
+        {{ ` / ` }}
+        {{
+          category ||
+            $t('HELP_CENTER.ARTICLE_SEARCH_RESULT.ARTICLE_SEARCH_RESULT')
+        }}
+      </p>
       <div class="action-buttons">
         <woot-button
-          :variant="hollow"
-          :color-scheme="secondary"
+          :title="$t('HELP_CENTER.ARTICLE_SEARCH_RESULT.COPY_LINK')"
+          variant="hollow"
+          color-scheme="secondary"
           size="tiny"
           icon="copy"
           @click="handleCopy"
         />
+
+        <a
+          :href="url"
+          class="button hollow button--only-icon tiny secondary"
+          rel="noopener noreferrer nofollow"
+          target="_blank"
+          :title="$t('HELP_CENTER.ARTICLE_SEARCH_RESULT.OPEN_LINK')"
+        >
+          <fluent-icon size="12" icon="arrow-up-right" />
+          <span class="show-for-sr">{{ url }}</span>
+        </a>
         <woot-button
-          :variant="hollow"
-          :color-scheme="secondary"
-          size="tiny"
-          icon="arrow-up-right"
-          @click="handleOpen"
-        />
-        <woot-button
-          :variant="hollow"
-          :color-scheme="secondary"
+          variant="hollow"
+          color-scheme="secondary"
           size="tiny"
           icon="preview-link"
+          :title="$t('HELP_CENTER.ARTICLE_SEARCH_RESULT.PREVIEW_LINK')"
           @click="handlePreview"
         />
-        <woot-button @click="handleClick">Insert</woot-button>
+        <woot-button
+          class="insert-button"
+          variant="smooth"
+          color-scheme="secondary"
+          size="tiny"
+          @click="handleClick"
+        >
+          {{ $t('HELP_CENTER.ARTICLE_SEARCH_RESULT.INSERT_ARTICLE') }}
+        </woot-button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import WootButton from '../../../../components/ui/WootButton.vue';
+import { copyTextToClipboard } from 'shared/helpers/clipboard';
+
 export default {
   name: 'ArticleSearchResultItem',
-  components: { WootButton },
   props: {
     title: {
       type: String,
@@ -66,9 +87,44 @@ export default {
     handlePreview() {
       this.$emit('preview');
     },
-    handleOpen() {
-      this.$emit('open');
+    async handleCopy() {
+      await copyTextToClipboard(this.url);
+      this.showAlert(this.$t('CONTACT_PANEL.COPY_SUCCESSFUL'));
     },
   },
 };
 </script>
+<style lang="scss" scoped>
+.article-item {
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-micro);
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: var(--space-micro);
+}
+
+.meta {
+  color: var(--s-600);
+  margin-bottom: 0;
+}
+
+.action-buttons {
+  display: flex;
+  gap: var(--space-micro);
+}
+.action-buttons .button:not(.insert-button) {
+  visibility: hidden;
+  opacity: 0;
+  transition: all 0.1s ease-in;
+}
+
+.article-item:hover .action-buttons .button:not(.insert-button) {
+  visibility: visible;
+  opacity: 1;
+}
+</style>
