db2c3ae973acdb2a08e597c3378630c1e809c976
Tejaswini Chile <tejaswini@chatwoot.com>
2023-05-23 20:45:49 +0530

    fix: for rails 7 upgrade, in action-mailer initializer

--- config/initializers/delivery_methods.rb
+++ config/initializers/delivery_methods.rb
@@ -1,3 +1,3 @@
-require './lib/delivery_methods/microsoft_graph.rb'
+require './lib/delivery_methods/microsoft_graph'
 
 ActionMailer::Base.add_delivery_method :microsoft_graph, MicrosoftGraph, location: '../../lib/delivery_methods/microsoft_graph.rb'
