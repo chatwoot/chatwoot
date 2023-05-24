ff5b7e2ede056b2736f19cd58f6043650131fc4c
Vishnu Narayanan <vishnu@chatwoot.com>
2023-05-24 21:20:43 +0530

    feat: review comments

--- enterprise/app/models/enterprise/audit/user.rb
+++ enterprise/app/models/enterprise/audit/user.rb
@@ -2,29 +2,12 @@ module Enterprise::Audit::User
   extend ActiveSupport::Concern
 
   included do
-    audited except: [
-      :confirmation_sent_at,
-      :confirmation_token,
-      :confirmed_at,
-      :current_sign_in_at,
-      :current_sign_in_ip,
-      :encrypted_password,
-      :last_sign_in_at,
-      :last_sign_in_ip,
-      :message_signature,
-      :provider,
-      :pubsub_token,
-      :remember_created_at,
-      :reset_password_sent_at,
-      :reset_password_token,
-      :sign_in_count,
-      :tokens,
-      :type,
-      :ui_settings,
-      :uid,
-      :unconfirmed_email
+    audited only: [
+      :availability,
+      :display_name,
+      :email,
+      :name
     ]
-
     audited associated_with: :account
   end
 end
