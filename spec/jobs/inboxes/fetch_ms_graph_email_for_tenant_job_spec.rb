eaaba7d8f30855c590e4b8e5b8179a62f184d758
Tejaswini Chile <tejaswini@chatwoot.com>
2023-05-24 17:23:14 +0530

    fix: job specs with stubs

--- spec/jobs/inboxes/fetch_ms_graph_email_for_tenant_job_spec.rb
+++ spec/jobs/inboxes/fetch_ms_graph_email_for_tenant_job_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe Inboxes::FetchMsGraphEmailForTenantJob do
   end
   let(:ms_email_inbox) { create(:inbox, channel: microsoft_imap_email_channel, account: account) }
   let(:inbound_mail) { create_inbound_email_from_mail(from: 'testemail@gmail.com', to: 'imap@outlook.com', subject: 'Hello!') }
+  let(:yesterday) { (Time.zone.today - 1).strftime('%FT%TZ') }
+  let(:tomorrow) { (Time.zone.today + 1).strftime('%FT%TZ') }
 
   it 'enqueues the job' do
     expect { described_class.perform_later }.to have_enqueued_job(described_class)
@@ -19,7 +21,18 @@ RSpec.describe Inboxes::FetchMsGraphEmailForTenantJob do
 
   context 'when imap fetch new emails for microsoft mailer' do
     before do
-      stub_request(:get, 'https://graph.microsoft.com/v1.0/me/messages?$filter=receivedDateTime%20ge%202023-05-23T00:00:00Z%20and%20receivedDateTime%20le%202023-05-25T00:00:00Z&$select=id&$top=1000')
+      stub_request(:get, "https://graph.microsoft.com/v1.0/me/messages?$filter=receivedDateTime%20ge%20#{yesterday}%20and%20receivedDateTime%20le%20#{tomorrow}&$select=id&$top=1000")
+        .with(
+          headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Authorization' => 'Bearer access_token',
+            'User-Agent' => 'Ruby'
+          }
+        )
+        .to_return(status: 200, body: '{"value":[{"id":"1"}]}', headers: {})
+
+      stub_request(:get, 'https://graph.microsoft.com/v1.0/me/messages/1/$value')
         .with(
           headers: {
             'Accept' => '*/*',
@@ -33,26 +46,19 @@ RSpec.describe Inboxes::FetchMsGraphEmailForTenantJob do
 
     it 'fetch and process all emails' do
       with_modified_env AZURE_TENANT_ID: 'azure_tenant_id' do
+        ms_imap_email_inbox = double
+
         email = Mail.new do
           to 'test@outlook.com'
           from 'test@gmail.com'
           subject :test.to_s
           body 'hello'
         end
+
         imap_fetch_mail = Net::IMAP::FetchData.new
         imap_fetch_mail.attr = { RFC822: email }.with_indifferent_access
 
-        ms_imap = double
-
-        allow(Net::IMAP).to receive(:new).and_return(ms_imap)
-        allow(ms_imap).to receive(:authenticate)
-        allow(ms_imap).to receive(:select)
-        allow(ms_imap).to receive(:search).and_return([1])
-        allow(ms_imap).to receive(:fetch).and_return([imap_fetch_mail])
         allow(Mail).to receive(:read_from_string).and_return(inbound_mail)
-
-        ms_imap_email_inbox = double
-
         allow(Imap::ImapMailbox).to receive(:new).and_return(ms_imap_email_inbox)
         expect(ms_imap_email_inbox).to receive(:process).with(inbound_mail, microsoft_imap_email_channel).once
 
