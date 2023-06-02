a867cce4caa224da3f3dc0ebd7591d319a08562c
Shivam Mishra <scm.mymail@gmail.com>
2023-06-02 15:30:16 +0530

    feat: remove update-banner classes

--- app/javascript/dashboard/App.vue
+++ app/javascript/dashboard/App.vue
@@ -6,6 +6,7 @@
     :class="{ 'app-rtl--wrapper': isRTLView }"
   >
     <update-banner :latest-chatwoot-version="latestChatwootVersion" />
+    <payment-pending-banner />
     <transition name="fade" mode="out-in">
       <router-view />
     </transition>
@@ -25,6 +26,7 @@ import AddAccountModal from '../dashboard/components/layout/sidebarComponents/Ad
 import LoadingState from './components/widgets/LoadingState.vue';
 import NetworkNotification from './components/NetworkNotification';
 import UpdateBanner from './components/app/UpdateBanner.vue';
+import PaymentPendingBanner from './components/app/PaymentPendingBanner.vue';
 import vueActionCable from './helper/actionCable';
 import WootSnackbarBox from './components/SnackbarContainer';
 import rtlMixin from 'shared/mixins/rtlMixin';
@@ -41,6 +43,7 @@ export default {
     LoadingState,
     NetworkNotification,
     UpdateBanner,
+    PaymentPendingBanner,
     WootSnackbarBox,
   },
 
@@ -115,11 +118,6 @@ export default {
 
 <style lang="scss">
 @import './assets/scss/app';
-.update-banner {
-  height: var(--space-larger);
-  align-items: center;
-  font-size: var(--font-size-small) !important;
-}
 </style>
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
