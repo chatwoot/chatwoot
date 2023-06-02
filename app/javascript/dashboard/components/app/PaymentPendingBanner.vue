54128df2bf05d0e36ad827b1fcc3db9c829c5afb
Shivam Mishra <scm.mymail@gmail.com>
2023-06-02 16:17:32 +0530

    feat: update banner visibility and actions

--- app/javascript/dashboard/components/app/PaymentPendingBanner.vue
+++ app/javascript/dashboard/components/app/PaymentPendingBanner.vue
@@ -6,6 +6,7 @@
     :action-button-label="actionButtonMessage"
     has-close-button
     has-action-button
+    @click="routeToBilling"
     @close="dismissUpdateBanner"
   />
 </template>
@@ -16,15 +17,24 @@ import { LOCAL_STORAGE_KEYS } from 'dashboard/constants/localStorage';
 import { LocalStorage } from 'shared/helpers/localStorage';
 import { mapGetters } from 'vuex';
 import adminMixin from 'dashboard/mixins/isAdmin';
+import accountMixin from 'dashboard/mixins/account';
+
+const EMPTY_SUBSCRIPTION_INFO = {
+  status: null,
+  endsOn: null,
+};
 
 export default {
   components: { Banner },
-  mixins: [adminMixin],
+  mixins: [adminMixin, accountMixin],
   data() {
     return { userDismissedBanner: false };
   },
   computed: {
-    ...mapGetters({ globalConfig: 'globalConfig/get' }),
+    ...mapGetters({
+      isOnChatwootCloud: 'globalConfig/isOnChatwootCloud',
+      getAccount: 'accounts/getAccount',
+    }),
     bannerMessage() {
       return this.$t('GENERAL_SETTINGS.PAYMENT_PENDING');
     },
@@ -32,6 +42,10 @@ export default {
       return this.$t('GENERAL_SETTINGS.OPEN_BILLING');
     },
     shouldShowBanner() {
+      if (!this.isOnChatwootCloud) {
+        return false;
+      }
+
       if (!this.isAdmin) {
         return false;
       }
@@ -40,27 +54,65 @@ export default {
         return false;
       }
 
-      return true;
+      if (this.isBannerDismissed()) {
+        return false;
+      }
+
+      return this.isPaymentPending();
     },
   },
   methods: {
-    isBannerDismissed(version) {
-      const dismissedVersions =
-        LocalStorage.get(LOCAL_STORAGE_KEYS.DISMISSED_PAYMENT) || [];
-      return dismissedVersions.includes(version);
+    routeToBilling() {
+      this.$router.push({
+        name: 'billing_settings_index',
+        params: { accountId: this.accountId },
+      });
+    },
+    isPaymentPending() {
+      const { status, endsOn } = this.getSubscriptionInfo();
+
+      if (status && endsOn) {
+        const now = new Date();
+        if (status === 'past_due' && endsOn < now) {
+          return true;
+        }
+      }
+
+      return false;
+    },
+    getSubscriptionInfo() {
+      const account = this.getAccount(this.accountId);
+      if (!account) return EMPTY_SUBSCRIPTION_INFO;
+
+      const { custom_attributes: subscription } = account;
+      if (!subscription) return EMPTY_SUBSCRIPTION_INFO;
+
+      const {
+        subscription_status: status,
+        subscription_ends_on: endsOn,
+      } = subscription;
+
+      return { status, endsOn: new Date(endsOn) };
+    },
+    isBannerDismissed() {
+      const bannerHiddenUntil = LocalStorage.get(
+        LOCAL_STORAGE_KEYS.HIDE_PAYMENT_TILL
+      );
+
+      const now = new Date();
+
+      if (bannerHiddenUntil && now < new Date(bannerHiddenUntil)) {
+        return true;
+      }
+
+      return false;
     },
     dismissUpdateBanner() {
-      let updatedDismissedItems =
-        LocalStorage.get(LOCAL_STORAGE_KEYS.DISMISSED_PAYMENT) || [];
-      if (updatedDismissedItems instanceof Array) {
-        updatedDismissedItems.push(this.latestChatwootVersion);
-      } else {
-        updatedDismissedItems = [this.latestChatwootVersion];
-      }
-      LocalStorage.set(
-        LOCAL_STORAGE_KEYS.DISMISSED_PAYMENT,
-        updatedDismissedItems
-      );
+      // dismiss the banner for 24 hours
+      const tomorrow = new Date();
+      tomorrow.setDate(tomorrow.getDate() + 1);
+
+      LocalStorage.set(LOCAL_STORAGE_KEYS.HIDE_PAYMENT_TILL, tomorrow);
       this.userDismissedBanner = true;
     },
   },
