bdf59b1a9f40a7c49b68ca6f6ea7ddd2e6075551
Shivam Mishra <scm.mymail@gmail.com>
2023-06-09 18:27:19 +0530

    feat: update cache_while_revalidate time to 15 mins

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -37,7 +37,7 @@ class Api::V1::AccountsController < Api::BaseController
   end
 
   def cache_keys
-    expires_in 10.seconds, public: false, stale_while_revalidate: 5.minutes
+    expires_in 10.seconds, public: false, stale_while_revalidate: 15.minutes
     render json: { cache_keys: get_cache_keys }, status: :ok
   end
 
