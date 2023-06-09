cc4f652e3003db17ccb2d4d9a5d7356c66327b39
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2023-06-09 13:50:54 +0530

    fixes code climate issue

--- lib/superscript_renderer.rb
+++ lib/superscript_renderer.rb
@@ -1,26 +1,28 @@
 class SuperscriptRenderer < CommonMarker::HtmlRenderer
-  def initialize
-    super()
-  end
-
   def text(node)
     content = node.string_content
 
     # Check for presence of '^' in the content
     if content.include?('^')
       # Split the text and insert <sup> tags where necessary
-      split_content = content.split(/(\^[^\^]+\^)/).map do |segment|
-        if segment.start_with?('^') && segment.end_with?('^')
-          "<sup>#{escape_html(segment[1..-2])}</sup>"
-        else
-          escape_html(segment)
-        end
-      end
+      split_content = parse_sup(content)
       # Output the transformed content
       out(split_content.join)
     else
       # Output the original content
       out(escape_html(content))
+    end
+  end
+
+  private
+
+  def parse_sup(content)
+    content.split(/(\^[^\^]+\^)/).map do |segment|
+      if segment.start_with?('^') && segment.end_with?('^')
+        "<sup>#{escape_html(segment[1..-2])}</sup>"
+      else
+        escape_html(segment)
+      end
     end
   end
 end
