157ba9b56b1d5045dd0263237d5f8a341d6179e9
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2023-06-09 19:57:24 +0530

    fixes rubocop issue

--- spec/lib/superscript_renderer_spec.rb
+++ spec/lib/superscript_renderer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe SuperscriptRenderer do
-  let(:renderer) { SuperscriptRenderer.new }
+  let(:renderer) { described_class.new }
 
   def render_markdown(markdown)
     doc = CommonMarker.render_doc(markdown, :DEFAULT)
@@ -10,17 +10,17 @@ describe SuperscriptRenderer do
 
   describe '#text' do
     it 'converts text wrapped in ^ to superscript' do
-      markdown = "This is an example of a superscript: ^superscript^."
+      markdown = 'This is an example of a superscript: ^superscript^.'
       expect(render_markdown(markdown)).to include('<sup>superscript</sup>')
     end
 
     it 'does not convert text not wrapped in ^' do
-      markdown = "This is an example without superscript."
+      markdown = 'This is an example without superscript.'
       expect(render_markdown(markdown)).not_to include('<sup>')
     end
 
     it 'converts multiple superscripts in the same text' do
-      markdown = "This is an example with ^multiple^ ^superscripts^."
+      markdown = 'This is an example with ^multiple^ ^superscripts^.'
       rendered_html = render_markdown(markdown)
       expect(rendered_html.scan('<sup>').length).to eq(2)
       expect(rendered_html).to include('<sup>multiple</sup>')
