14d96ff22b8fb3ddbc485e2e2ed1127308d22660
Sojan <sojan@pepalo.com>
2023-06-14 14:26:16 +0530

    chore: fix renderer

--- spec/lib/chatwoot_markdown_renderer_spec.rb
+++ spec/lib/chatwoot_markdown_renderer_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
-require 'chatwoot_markdown_renderer'
 
 RSpec.describe ChatwootMarkdownRenderer, type: :model do
   let(:superscript_renderer) { instance_double(SuperscriptRenderer) }
   let(:renderer) { described_class.new(superscript_renderer) }
   let(:markdown_content) { 'This is a *test* content with markdown' }
-  let(:doc) { instance_double('CommonMarker doc instance') }
+  let(:doc) { instance_double(CommonMarker::Node) }
   let(:html_content) { '<p>This is a <em>test</em> content with markdown</p>' }
 
   before do
