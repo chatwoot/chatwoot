32a492a164d357de6f2268f5ff5146621f813ced
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-06-16 09:25:41 +0530

    Update app/javascript/dashboard/components/buttons/ResolveAction.vue
    Co-authored-by: Sivin Varghese <64252451+iamsivin@users.noreply.github.com>

--- app/javascript/dashboard/components/buttons/ResolveAction.vue
+++ app/javascript/dashboard/components/buttons/ResolveAction.vue
@@ -88,7 +88,7 @@ import { getUnixTime } from 'date-fns';
 import { mapGetters } from 'vuex';
 import { mixin as clickaway } from 'vue-clickaway';
 import alertMixin from 'shared/mixins/alertMixin';
-import CustomSnoozeModal from 'dashboard/components/CustomSnoozeModal.vue';
+import CustomSnoozeModal from 'dashboard/components/CustomSnoozeModal';
 import eventListenerMixins from 'shared/mixins/eventListenerMixins';
 import {
   hasPressedAltAndEKey,
