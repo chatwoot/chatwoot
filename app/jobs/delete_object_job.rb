ab844d999b6805084d90f9ba193146f36a077e12
Vishnu Narayanan <vishnu@chatwoot.com>
2023-06-26 14:52:58 +0530

    feat: capture user and ip details on Inbox delete

--- app/jobs/delete_object_job.rb
+++ app/jobs/delete_object_job.rb
@@ -1,7 +1,28 @@
 class DeleteObjectJob < ApplicationJob
   queue_as :low
 
-  def perform(object)
+  def perform(object, user = nil, ip = nil)
+    audited_changes = object.attributes.except('id', 'created_at', 'updated_at')
     object.destroy!
+    create_audit_entry(object, user, ip, audited_changes)
+  end
+
+  def create_audit_entry(object, user, ip, audited_changes)
+    return unless object.is_a?(Inbox) && user.present?
+
+    account = object.account
+    associated_type = 'Account'
+    Enterprise::AuditLog.create(
+      auditable_id: object.id,
+      auditable_type: object.class.name,
+      audited_changes: audited_changes,
+      action: 'destroy',
+      user_id: user.id,
+      user_type: 'User',
+      associated_id: account.id,
+      associated_type: associated_type,
+      remote_address: ip,
+      comment: 'Deleted Inbox'
+    )
   end
 end
--- enterprise/app/models/enterprise/audit/inbox.rb
+++ enterprise/app/models/enterprise/audit/inbox.rb
@@ -2,6 +2,7 @@ module Enterprise::Audit::Inbox
   extend ActiveSupport::Concern
 
   included do
+    audited on: [:create, :update]
     audited associated_with: :account
   end
 end
