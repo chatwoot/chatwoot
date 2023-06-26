8cb072b98aba1b4f5f78d5033cb72f08f0a0cad8
Vishnu Narayanan <vishnu@chatwoot.com>
2023-06-26 18:04:11 +0530

    chore: audit only on create and update

--- enterprise/app/models/enterprise/audit/inbox.rb
+++ enterprise/app/models/enterprise/audit/inbox.rb
@@ -2,7 +2,6 @@ module Enterprise::Audit::Inbox
   extend ActiveSupport::Concern
 
   included do
-    audited associated_with: :account
-    audited on: [:create, :update]
+    audited associated_with: :account, on: [:create, :update]
   end
 end
