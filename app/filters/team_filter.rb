1a3189893d14ca998fe7b733bebd85cf3a7559ff
RubyFreez <RubyFreez@proton.me>
2023-06-27 20:40:01 +0200

    feature/7398 private or restricted teams

--- app/filters/team_filter.rb
+++ app/filters/team_filter.rb
@@ -25,7 +25,6 @@ class TeamFilter
 
   def team_filter
     raise Pundit::NotAuthorizedError if excluded_team_ids.to_set.include?(@team.id)
-    raise Pundit::NotAuthorizedError if excluded_team_ids.to_set.include?(@team.id)
 
     @conversations.where(team: @team)
   end
