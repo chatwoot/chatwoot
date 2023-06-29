169b21e2c8a5485437ed3366b0cdb154d07840bc
Pranav Raj S <pranav@chatwoot.com>
2023-06-29 14:31:05 -0700

    Fix

--- app/javascript/v3/views/auth/signup/Index.vue
+++ app/javascript/v3/views/auth/signup/Index.vue
@@ -24,7 +24,7 @@
             </h2>
           </div>
           <signup-form />
-          <div class="text-sm text-slate-800 dark:text-woot-50">
+          <div class="text-sm text-slate-800 dark:text-woot-50 px-1">
             <span>{{ $t('REGISTER.HAVE_AN_ACCOUNT') }}</span>
             <router-link class="text-link" to="/app/login">
               {{
