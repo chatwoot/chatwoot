00cab6de81f0833d64787e9c6507a2638aab2ea9
Tejaswini Chile <tejaswini@chatwoot.com>
2023-07-10 23:05:48 +0530

    sentiment helper update

--- enterprise/app/models/enterprise/sentiment_analysis_helper.rb
+++ enterprise/app/models/enterprise/sentiment_analysis_helper.rb
@@ -13,33 +13,33 @@ module Enterprise::SentimentAnalysisHelper
       records = incoming_messages.last(average_message_count)
       average_sentiment(records)
     end
-  end
 
-  def average_sentiment(records)
-    {
-      label: average_sentiment_label(records),
-      score: average_sentiment_score(records)
-    }
-  end
+    def average_sentiment(records)
+      {
+        label: average_sentiment_label(records),
+        score: average_sentiment_score(records)
+      }
+    end
 
-  private
+    private
 
-  def average_sentiment_label(records)
-    value = records.pluck(:sentiment).sum { |a| a['value'].to_i }
-    value.negative? ? 'negative' : 'positive'
-  end
+    def average_sentiment_label(records)
+      value = records.pluck(:sentiment).sum { |a| a['value'].to_i }
+      value.negative? ? 'negative' : 'positive'
+    end
 
-  def average_sentiment_score(records)
-    total = records.pluck(:sentiment).sum { |a| a['score'].to_f }
-    total / average_message_count
-  end
+    def average_sentiment_score(records)
+      total = records.pluck(:sentiment).sum { |a| a['score'].to_f }
+      total / average_message_count
+    end
 
-  def average_message_count
-    # incoming_messages.count >= 10 ? 5 : ((incoming_messages.count / 2) - 1)
-    5
-  end
+    def average_message_count
+      # incoming_messages.count >= 10 ? 5 : ((incoming_messages.count / 2) - 1)
+      5
+    end
 
-  def incoming_messages
-    messages.incoming.where(private: false)
+    def incoming_messages
+      messages.incoming.where(private: false)
+    end
   end
 end
