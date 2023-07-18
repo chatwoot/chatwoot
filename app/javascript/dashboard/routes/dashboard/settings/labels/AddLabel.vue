65c476162ada86c8aa67262cd5a9348fdd3e77ba
iamsivin <iamsivin@gmail.com>
2023-07-18 20:09:08 +0530

    chore: More files

--- app/javascript/dashboard/routes/dashboard/settings/labels/AddLabel.vue
+++ app/javascript/dashboard/routes/dashboard/settings/labels/AddLabel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="column content-box">
+  <div class="h-auto overflow-auto flex flex-col">
     <woot-modal-header
       :header-title="$t('LABEL_MGMT.ADD.TITLE')"
       :header-content="$t('LABEL_MGMT.ADD.DESC')"
@@ -8,7 +8,7 @@
       <woot-input
         v-model.trim="title"
         :class="{ error: $v.title.$error }"
-        class="medium-12 columns label-name--input"
+        class="w-full label-name--input"
         :label="$t('LABEL_MGMT.FORM.NAME.LABEL')"
         :placeholder="$t('LABEL_MGMT.FORM.NAME.PLACEHOLDER')"
         :error="getLabelTitleErrorMessage"
@@ -19,38 +19,36 @@
       <woot-input
         v-model.trim="description"
         :class="{ error: $v.description.$error }"
-        class="medium-12 columns"
+        class="w-full"
         :label="$t('LABEL_MGMT.FORM.DESCRIPTION.LABEL')"
         :placeholder="$t('LABEL_MGMT.FORM.DESCRIPTION.PLACEHOLDER')"
         data-testid="label-description"
         @input="$v.description.$touch"
       />
 
-      <div class="medium-12">
+      <div class="w-full">
         <label>
           {{ $t('LABEL_MGMT.FORM.COLOR.LABEL') }}
           <woot-color-picker v-model="color" />
         </label>
       </div>
-      <div class="medium-12">
+      <div class="w-full">
         <input v-model="showOnSidebar" type="checkbox" :value="true" />
         <label for="conversation_creation">
           {{ $t('LABEL_MGMT.FORM.SHOW_ON_SIDEBAR.LABEL') }}
         </label>
       </div>
-      <div class="modal-footer">
-        <div class="medium-12 columns">
-          <woot-button
-            :is-disabled="$v.title.$invalid || uiFlags.isCreating"
-            :is-loading="uiFlags.isCreating"
-            data-testid="label-submit"
-          >
-            {{ $t('LABEL_MGMT.FORM.CREATE') }}
-          </woot-button>
-          <woot-button class="button clear" @click.prevent="onClose">
-            {{ $t('LABEL_MGMT.FORM.CANCEL') }}
-          </woot-button>
-        </div>
+      <div class="flex justify-end items-center py-2 px-0 gap-2 w-full">
+        <woot-button
+          :is-disabled="$v.title.$invalid || uiFlags.isCreating"
+          :is-loading="uiFlags.isCreating"
+          data-testid="label-submit"
+        >
+          {{ $t('LABEL_MGMT.FORM.CREATE') }}
+        </woot-button>
+        <woot-button class="button clear" @click.prevent="onClose">
+          {{ $t('LABEL_MGMT.FORM.CANCEL') }}
+        </woot-button>
       </div>
     </form>
   </div>
