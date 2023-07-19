9a57a718994d1b02761df7aa86e2d5ede942698d
iamsivin <iamsivin@gmail.com>
2023-07-19 21:42:06 +0530

    chore: More files

--- app/javascript/dashboard/components/widgets/TableFooter.vue
+++ app/javascript/dashboard/components/widgets/TableFooter.vue
@@ -4,7 +4,7 @@
     class="bg-white dark:bg-slate-800 h-[60px] border-t border-solid border-slate-75 dark:border-slate-600 flex items-center justify-between py-0 px-4"
   >
     <div class="left-aligned-wrap">
-      <div class="text-xs">
+      <div class="text-xs text-slate-600 dark:text-slate-200">
         <strong>{{ firstIndex }}</strong>
         - <strong>{{ lastIndex }}</strong> of
         <strong>{{ totalCount }}</strong> items
