b15026e4a300f7f9badef241bf301e904c0105a0
iamsivin <iamsivin@gmail.com>
2023-07-20 21:19:42 +0530

    chore: More files

--- app/javascript/dashboard/routes/dashboard/settings/macros/MacroForm.vue
+++ app/javascript/dashboard/routes/dashboard/settings/macros/MacroForm.vue
@@ -132,7 +132,7 @@ export default {
     background-image: radial-gradient(#ebf0f5 1.2px, transparent 0);
   }
   .macro-dark-gradient-radial {
-    background-image: radial-gradient(#1b2836 1.2px, transparent 0);
+    background-image: radial-gradient(#293f51 1.2px, transparent 0);
   }
   .macro-gradient-radial-size {
     background-size: 1rem 1rem;
