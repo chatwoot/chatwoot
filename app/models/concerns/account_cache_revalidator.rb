29afe12325aed2bc8695ea3943d5e026dcabb116
Shivam Mishra <scm.mymail@gmail.com>
2023-07-21 16:18:09 +0530

    chore: change cache update trigger conditions

--- app/models/concerns/account_cache_revalidator.rb
+++ app/models/concerns/account_cache_revalidator.rb
@@ -2,10 +2,10 @@ module AccountCacheRevalidator
   extend ActiveSupport::Concern
 
   included do
-    after_commit :update_account_cache
+    after_commit :update_account_cache, on: [:create, :update]
   end
 
   def update_account_cache
-    account&.update_cache_key(self.class.name.underscore)
+    account.update_cache_key(self.class.name.underscore)
   end
 end
