33515eac331f1c9973bbc98333b0dde27204b7cb
Sojan <sojan@pepalo.com>
2023-07-21 20:31:51 +0300

    chore: Fix response associations for inbox

--- enterprise/app/models/enterprise/concerns/inbox.rb
+++ enterprise/app/models/enterprise/concerns/inbox.rb
@@ -4,8 +4,8 @@ module Enterprise::Concerns::Inbox
   included do
     def self.add_response_related_associations
       has_many :response_sources, dependent: :destroy_async
-      has_many :response_documents, dependent: :destroy_async
-      has_many :responses, dependent: :destroy_async
+      has_many :response_documents, through: :response_sources
+      has_many :responses, through: :response_documents
     end
 
     add_response_related_associations if Features::ResponseBotService.new.vector_extension_enabled?
