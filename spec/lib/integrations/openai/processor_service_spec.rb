0ef43807d624dc34af66a858cfbc9b70169629dd
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-07-24 10:42:52 +0800

    Fix the specs

--- spec/lib/integrations/openai/processor_service_spec.rb
+++ spec/lib/integrations/openai/processor_service_spec.rb
@@ -64,10 +64,7 @@ RSpec.describe Integrations::Openai::ProcessorService do
           'model' => 'gpt-3.5-turbo',
           'messages' => [
             { role: 'system',
-              # content: Rails.root.join('lib/integrations/openai/openai_prompts/reply.txt').read },
-              content: 'Please suggest a reply to the following conversation between support agents and customer. ' \
-                       'Don\'t expose that you are an AI model, respond "Couldn\'t generate the reply" in cases where you can\'t answer. ' \
-                       'Reply in the user\'s language.' },
+              content: Rails.root.join('lib/integrations/openai/openai_prompts/reply.txt').read },
             { role: 'user', content: customer_message.content },
             { role: 'assistant', content: agent_message.content }
           ]
