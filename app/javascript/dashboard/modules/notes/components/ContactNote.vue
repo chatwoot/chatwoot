0f090417f350e9c4fc7c21ab9a5ab94193649c6b
iamsivin <iamsivin@gmail.com>
2023-07-24 17:01:48 +0530

    fix: Contact note page

--- app/javascript/dashboard/modules/notes/components/ContactNote.vue
+++ app/javascript/dashboard/modules/notes/components/ContactNote.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class="card note-wrap">
-    <div class="header">
-      <div class="meta">
+  <div
+    class="flex flex-col mb-2 p-4 border border-solid border-slate-75 dark:border-slate-700 overflow-hidden rounded-md flex-grow shadow-sm bg-white dark:bg-slate-900 text-slate-700 dark:text-slate-100 note-wrap"
+  >
+    <div class="flex justify-between items-end text-xs gap-1">
+      <div class="flex items-center">
         <thumbnail
           :title="noteAuthorName"
           :src="noteAuthor.thumbnail"
           :username="noteAuthorName"
           size="20px"
         />
-        <div class="date-wrap">
-          <span class="fw-medium"> {{ noteAuthorName }} </span>
-          <span> {{ $t('NOTES.LIST.LABEL') }} </span>
-          <span class="fw-medium time-stamp"> {{ readableTime }} </span>
+        <div class="my-0 mx-1 p-0.5 flex flex-row gap-1">
+          <span class="font-medium text-slate-800 dark:text-slate-100">
+            {{ noteAuthorName }}
+          </span>
+          <span class="text-slate-700 dark:text-slate-100">
+            {{ $t('NOTES.LIST.LABEL') }}
+          </span>
+          <span class="font-medium text-slate-700 dark:text-slate-100">
+            {{ readableTime }}
+          </span>
         </div>
       </div>
-      <div class="actions">
+      <div class="actions flex invisible">
         <woot-button
           v-tooltip="$t('NOTES.CONTENT_HEADER.DELETE')"
           variant="smooth"
@@ -35,7 +43,10 @@
         :reject-text="$t('DELETE_NOTE.CONFIRM.NO')"
       />
     </div>
-    <p v-dompurify-html="formatMessage(note || '')" class="note__content" />
+    <p
+      v-dompurify-html="formatMessage(note || '')"
+      class="note__content mt-4"
+    />
   </div>
 </template>
 
@@ -116,39 +127,9 @@ export default {
   }
 }
 
-.note__content {
-  margin-top: var(--space-normal);
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
-  font-size: var(--font-size-mini);
-
-  .meta {
-    display: flex;
-    align-items: center;
-
-    .date-wrap {
-      margin: 0 var(--space-smaller);
-      padding: var(--space-micro);
-      color: var(--color-body);
-    }
-  }
-  .actions {
-    display: flex;
-    visibility: hidden;
-
-    .button {
-      margin-left: var(--space-small);
-    }
-  }
-}
-
 .note-wrap:hover {
   .actions {
-    visibility: visible;
+    @apply visible;
   }
 }
 </style>
