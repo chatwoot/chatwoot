6b93e4c4594d1215eee9d70ef583192a656bbeec
iamsivin <iamsivin@gmail.com>
2023-07-27 10:53:40 +0530

    chore: Minor fix

--- app/javascript/dashboard/components/layout/sidebarComponents/NotificationBell.vue
+++ app/javascript/dashboard/components/layout/sidebarComponents/NotificationBell.vue
@@ -15,7 +15,7 @@
       />
       <span
         v-if="unreadCount"
-        class="text-black-900 bg-yellow-300 absolute -top-0.5 -right-1 p-1 text-xxs min-w-[1rem] rounded-full"
+        class="text-black-900 bg-yellow-300 absolute -top-0.5 -right-1 px-1 py-0.5 text-xxs min-w-[1rem] rounded-full"
       >
         {{ unreadCount }}
       </span>
