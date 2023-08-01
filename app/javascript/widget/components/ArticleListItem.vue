ed480b9113759dc867bb99ac712d5d56118ca1a5
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2023-08-01 21:32:44 +0530

    feat: Show popular articles on widget home (#7604)

--- app/javascript/widget/components/ArticleListItem.vue
+++ app/javascript/widget/components/ArticleListItem.vue
@@ -1,20 +1,24 @@
 <template>
   <li
-    class="py-1 flex items-center justify-between  -mx-1 px-1 hover:bg-slate-25"
+    class="py-1 flex items-center justify-between -mx-1 px-1 hover:bg-slate-75 dark:hover:bg-slate-600 rounded cursor-pointer text-slate-700 dark:text-slate-50 dark:hover:text-slate-25 hover:text-slate-900 "
+    @click="onClick"
   >
     <button
-      class="text-slate-700 hover:text-slate-900 underline-offset-2 text-sm leading-6"
+      class="underline-offset-2 text-sm leading-6 text-left"
       @click="onClick"
     >
       {{ title }}
     </button>
-    <span class="pl-1 text-slate-700 arrow">
+    <span class="pl-1 first-letter arrow">
       <fluent-icon icon="arrow-right" size="14" />
     </span>
   </li>
 </template>
 <script>
+import FluentIcon from 'shared/components/FluentIcon/Index.vue';
+
 export default {
+  components: { FluentIcon },
   props: {
     link: {
       type: String,
