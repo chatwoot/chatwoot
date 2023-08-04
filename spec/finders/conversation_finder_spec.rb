7f836aa551513e71b5cd11573dd0d0b3c3bcee7f
Sojan <sojan@pepalo.com>
2023-08-04 15:59:24 -0700

    chore: add specs

--- spec/finders/conversation_finder_spec.rb
+++ spec/finders/conversation_finder_spec.rb
@@ -160,9 +160,15 @@ describe ConversationFinder do
       let(:params) { { status: 'open', assignee_type: 'me', conversation_type: 'unattended' } }
 
       it 'returns unattended conversations' do
-        create_list(:conversation, 25, account: account, inbox: inbox, assignee: user_1)
+        create(:conversation, account: account, first_reply_created_at: Time.now.utc, assignee: user_1) # attended_conversation
+        # to ensure that waiting since value is populated
+        # create(:message, message_type: :outgoing, conversation: attended_conversation, account: account)
+
+        create(:conversation, account: account, first_reply_created_at: nil, assignee: user_1) # unattended_conversation_no_first_reply
+        create(:conversation, account: account, first_reply_created_at: Time.now.utc, assignee: user_1) # unattended_conversation_waiting_since
+
         result = conversation_finder.perform
-        expect(result[:conversations].length).to be 25
+        expect(result[:conversations].length).to be 2
       end
     end
   end
