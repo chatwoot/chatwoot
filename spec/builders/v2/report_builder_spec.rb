9d8615f4f017f880778f5644df7dd592dce6b4b2
Sojan <sojan@pepalo.com>
2023-08-07 12:38:54 -0700

    chore: fix flaky spec

--- spec/builders/v2/report_builder_spec.rb
+++ spec/builders/v2/report_builder_spec.rb
@@ -9,46 +9,48 @@ describe V2::ReportBuilder do
   # Update this spec to use travel_to
   # This spec breaks in certain timezone
   describe '#timeseries' do
-    before_all do
-      user = create(:user, account: account)
-      inbox = create(:inbox, account: account)
-      create(:inbox_member, user: user, inbox: inbox)
+    before do
+      travel_to(Time.zone.today) do
+        user = create(:user, account: account)
+        inbox = create(:inbox, account: account)
+        create(:inbox_member, user: user, inbox: inbox)
 
-      gravatar_url = 'https://www.gravatar.com'
-      stub_request(:get, /#{gravatar_url}.*/).to_return(status: 404)
+        gravatar_url = 'https://www.gravatar.com'
+        stub_request(:get, /#{gravatar_url}.*/).to_return(status: 404)
 
-      perform_enqueued_jobs do
-        10.times do
-          conversation = create(:conversation, account: account,
-                                               inbox: inbox, assignee: user,
-                                               created_at: Time.zone.today)
-          create_list(:message, 5, message_type: 'outgoing',
-                                   account: account, inbox: inbox,
-                                   conversation: conversation, created_at: Time.zone.today + 2.hours)
-          create_list(:message, 2, message_type: 'incoming',
-                                   account: account, inbox: inbox,
-                                   conversation: conversation,
-                                   created_at: Time.zone.today + 3.hours)
-          conversation.update_labels('label_1')
-          conversation.label_list
-          conversation.save!
-        end
+        perform_enqueued_jobs do
+          10.times do
+            conversation = create(:conversation, account: account,
+                                                 inbox: inbox, assignee: user,
+                                                 created_at: Time.zone.today)
+            create_list(:message, 5, message_type: 'outgoing',
+                                     account: account, inbox: inbox,
+                                     conversation: conversation, created_at: Time.zone.today + 2.hours)
+            create_list(:message, 2, message_type: 'incoming',
+                                     account: account, inbox: inbox,
+                                     conversation: conversation,
+                                     created_at: Time.zone.today + 3.hours)
+            conversation.update_labels('label_1')
+            conversation.label_list
+            conversation.save!
+          end
 
-        5.times do
-          conversation = create(:conversation, account: account,
-                                               inbox: inbox, assignee: user,
-                                               created_at: (Time.zone.today - 2.days))
-          create_list(:message, 3, message_type: 'outgoing',
-                                   account: account, inbox: inbox,
-                                   conversation: conversation,
-                                   created_at: (Time.zone.today - 2.days))
-          create_list(:message, 1, message_type: 'incoming',
-                                   account: account, inbox: inbox,
-                                   conversation: conversation,
-                                   created_at: (Time.zone.today - 2.days))
-          conversation.update_labels('label_2')
-          conversation.label_list
-          conversation.save!
+          5.times do
+            conversation = create(:conversation, account: account,
+                                                 inbox: inbox, assignee: user,
+                                                 created_at: (Time.zone.today - 2.days))
+            create_list(:message, 3, message_type: 'outgoing',
+                                     account: account, inbox: inbox,
+                                     conversation: conversation,
+                                     created_at: (Time.zone.today - 2.days))
+            create_list(:message, 1, message_type: 'incoming',
+                                     account: account, inbox: inbox,
+                                     conversation: conversation,
+                                     created_at: (Time.zone.today - 2.days))
+            conversation.update_labels('label_2')
+            conversation.label_list
+            conversation.save!
+          end
         end
       end
     end
