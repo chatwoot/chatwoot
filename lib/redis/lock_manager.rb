28f7b1d3d592ecf8403061403e906c7a16df3f4b
Shivam Mishra <scm.mymail@gmail.com>
2023-08-16 10:31:19 +0700

    feat: update lock duration and retry delay

--- lib/redis/lock_manager.rb
+++ lib/redis/lock_manager.rb
@@ -12,10 +12,10 @@
 # end
 #
 class Redis::LockManager
-  # Default lock timeout set to 15 seconds. This means that if the lock isn't released
-  # within 15 seconds, it will automatically expire.
+  # Default lock timeout set to 5 seconds. This means that if the lock isn't released
+  # within 5 seconds, it will automatically expire.
   # This helps to avoid deadlocks in case the process holding the lock crashes or fails to release it.
-  LOCK_TIMEOUT = 15.seconds
+  LOCK_TIMEOUT = 5.seconds
 
   # Attempts to acquire a lock for the given key.
   #
