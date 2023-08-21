79a76bc9df2188d213d2195420a250b5cbee7ff1
Shivam Mishra <scm.mymail@gmail.com>
2023-08-21 15:11:51 +0700

    refactor: use MutexApplicationJob

--- app/jobs/webhooks/facebook_events_job.rb
+++ app/jobs/webhooks/facebook_events_job.rb
@@ -1,8 +1,12 @@
 class Webhooks::FacebookEventsJob < ApplicationJob
   queue_as :default
+  retry_on LockAcquisitionError, wait: 2.seconds, attempts: 5
 
   def perform(message)
     response = ::Integrations::Facebook::MessageParser.new(message)
-    ::Integrations::Facebook::MessageCreator.new(response).perform
+
+    with_lock(::Redis::Alfred::FACEBOOK_MESSAGE_MUTEX, sender_id: response.sender_id, recipient_id: response.recipient_id) do
+      ::Integrations::Facebook::MessageCreator.new(response).perform
+    end
   end
 end
