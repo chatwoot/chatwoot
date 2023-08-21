611ec0f5577246e7bcc97ef31d327fd68cae3b63
Shivam Mishra <scm.mymail@gmail.com>
2023-08-21 15:02:43 +0700

    feat: add mutex for slack send job

--- app/jobs/send_on_slack_job.rb
+++ app/jobs/send_on_slack_job.rb
@@ -1,7 +1,24 @@
 class SendOnSlackJob < ApplicationJob
+  class LockAcquisitionError < StandardError; end
+
   queue_as :medium
+  retry_on LockAcquisitionError, wait: 3.seconds, attempts: 5
 
   def perform(message, hook)
-    Integrations::Slack::SendOnSlackService.new(message: message, hook: hook).perform
+    lock_key = format(::Redis::Alfred::SLACK_MESSAGE_MUTEX, sender_id: message.sender_id, reference_id: hook.reference_id)
+    lock_manager = Redis::LockManager.new
+
+    if lock_manager.locked?(lock_key)
+      Rails.logger.error "[SendOnSlackJob] Failed to acquire lock on attempt #{executions + 1}: #{lock_key}"
+      raise LockAcquisitionError, "Failed to acquire lock for key: #{lock_key}"
+    end
+
+    begin
+      Rails.logger.info "[SendOnSlackJob] Acquired lock for: #{lock_key}"
+      Integrations::Slack::SendOnSlackService.new(message: message, hook: hook).perform
+    ensure
+      # Ensure that the lock is released even if there's an error in processing
+      lock_manager.unlock(lock_key)
+    end
   end
 end
