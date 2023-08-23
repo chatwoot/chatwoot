5d5ef2a92c4eab25428080433c6a0a3d093c93a7
Rishbh Pandram <rishabh@onehash.ai>
2023-08-24 01:25:19 +0530

    ch: development.yml

--- app/services/enterprise/billing/handle_stripe_event_service.rb
+++ app/services/enterprise/billing/handle_stripe_event_service.rb
@@ -8,8 +8,8 @@ class Enterprise::Billing::HandleStripeEventService
         subscription_price = Enterprise::BillingProductPrice.find_by(price_stripe_id: subscription.plan.id)
         active_subscription = account.account_billing_subscriptions.where(account_id: subscription.metadata.account_id)&.last
         active_subscription&.update(billing_product_price: subscription_price, subscription_stripe_id: subscription.id,
-          current_period_end: Time.at(subscription.current_period_end).utc.to_datetime)
-       
+                                    current_period_end: Time.at(subscription.current_period_end).utc.to_datetime)
+
       when 'customer.subscription.updated'
         subscription = event.data.object
         account = Account.where("custom_attributes->>'stripe_customer_id' = ?", subscription.customer).first
