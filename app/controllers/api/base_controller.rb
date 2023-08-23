9676cad13e242d20dad4794c58d074963d6541c0
Rishbh Pandram <rishabh@onehash.ai>
2023-08-23 18:22:17 +0530

    fix: billing

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -4,7 +4,7 @@ class Api::BaseController < ApplicationController
   before_action :authenticate_access_token!, if: :authenticate_by_access_token?
   before_action :validate_bot_access_token!, if: :authenticate_by_access_token?
   before_action :authenticate_user!, unless: :authenticate_by_access_token?
-  # before_action :verify_subscription, if: :current_user
+  before_action :verify_subscription, if: :current_user
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -388,6 +388,7 @@ Rails.application.routes.draw do
   post 'webhooks/whatsapp/:phone_number', to: 'webhooks/whatsapp#process_payload'
   get 'webhooks/instagram', to: 'webhooks/instagram#verify'
   post 'webhooks/instagram', to: 'webhooks/instagram#events'
+  post 'webhooks/stripe', to: 'webhooks/stripe#process_payload'
 
   namespace :twitter do
     resource :callback, only: [:show]
@@ -425,6 +426,13 @@ Rails.application.routes.draw do
       end
       resources :users, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
         delete :avatar, on: :member, action: :destroy_avatar
+      end
+
+      # added
+      namespace :enterprise do
+        resources :billing_products
+        resources :billing_product_prices
+        resources :account_billing_subscriptions
       end
 
       resources :access_tokens, only: [:index, :show]
