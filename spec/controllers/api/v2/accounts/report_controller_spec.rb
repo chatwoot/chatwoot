6f4045caf8a4116a825f0dabcef0a1a90fcd074d
Sojan <sojan@pepalo.com>
2023-08-24 16:34:26 -0700

    chore: fix spec

--- spec/controllers/api/v2/accounts/report_controller_spec.rb
+++ spec/controllers/api/v2/accounts/report_controller_spec.rb
@@ -14,10 +14,8 @@ RSpec.describe 'Reports API', type: :request do
   let(:new_account) { create(:account) }
 
   before do
-    travel_to(Time.current.in_time_zone(default_timezone)) do
-      create_list(:conversation, 10, account: account, inbox: inbox,
-                                     assignee: user, created_at: Time.current.in_time_zone(default_timezone).to_date)
-    end
+    create_list(:conversation, 10, account: account, inbox: inbox,
+                                   assignee: user, created_at: Time.current.in_time_zone(default_timezone).to_date)
   end
 
   describe 'GET /api/v2/accounts/:account_id/reports/account' do
