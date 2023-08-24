8e84010bbd3db5761931407512466a8459d08920
Shivam Mishra <scm.mymail@gmail.com>
2023-08-24 12:13:19 +0700

    chore: update retry config

--- app/jobs/webhooks/facebook_events_job.rb
+++ app/jobs/webhooks/facebook_events_job.rb
@@ -3,7 +3,7 @@ class Webhooks::FacebookEventsJob < ApplicationJob
 
   queue_as :default
   # https://edgeapi.rubyonrails.org/classes/ActiveJob/Exceptions/ClassMethods.html#method-i-retry_on
-  retry_on LockAcquisitionError, wait: proc { |attempt| 2**attempt }, attempts: 5
+  retry_on LockAcquisitionError, wait: 1.seconds, attempts: 6
 
   def perform(message)
     response = ::Integrations::Facebook::MessageParser.new(message)
