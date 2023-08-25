3f8f4177d9e8fcf64fae2da01a70ad648fbb1858
Sojan <sojan@pepalo.com>
2023-08-24 20:26:03 -0700

    chore: fix specs

--- spec/factories/team_members.rb
+++ spec/factories/team_members.rb
@@ -4,12 +4,5 @@ FactoryBot.define do
   factory :team_member do
     user
     team
-
-    after(:create) do |team_member|
-      unless team_member.user.account_users.exists?(account: team_member.team.account)
-        create(:account_user, user: team_member.user,
-                              account: team_member.team.account)
-      end
-    end
   end
 end
