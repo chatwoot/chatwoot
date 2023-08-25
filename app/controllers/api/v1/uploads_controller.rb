febd8bc560782ff27f83d511cb8c2e4ea796fa4a
Shivam Mishra <scm.mymail@gmail.com>
2023-08-25 14:53:54 +0700

    feat: add upload endpoint

---
+++ app/controllers/api/v1/uploads_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::UploadsController < ApplicationController
+  def create
+    file_blob = ActiveStorage::Blob.create_and_upload!(
+      key: nil,
+      io: params[:attachment].tempfile,
+      filename: params[:attachment].original_filename,
+      content_type: params[:attachment].content_type
+    )
+    file_blob.save!
+    render json: { file_url: url_for(file_blob) }
+  end
+end
