d32610c3cc0af6f95c5af0ff354238800606bc2d
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2023-09-06 21:08:49 +0530

    Update app/javascript/shared/components/Branding.vue
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>

--- app/javascript/shared/components/Branding.vue
+++ app/javascript/shared/components/Branding.vue
@@ -1,8 +1,7 @@
 <template>
   <div
     v-if="globalConfig.brandName && !disableBranding"
-    class="px-0 py-3 flex justify-center"
-    :class="$dm('bg-slate-25', 'dark:bg-slate-800')"
+    class="px-0 py-3 flex justify-center bg-slate-25 dark:bg-slate-800"
   >
     <a
       :href="brandRedirectURL"
