bfd192ebb24017730f320b13be6a787be8ed9d7d
Sojan Jose <sojan@pepalo.com>
2023-09-07 22:46:24 -0700

    chore: Improve the behaviour of lock to single conversation (#7863)

--- app/services/sms/incoming_message_service.rb
+++ app/services/sms/incoming_message_service.rb
@@ -57,7 +57,8 @@ class Sms::IncomingMessageService
   end
 
   def set_conversation
-    @conversation = @contact_inbox.conversations.last
+    # if lock to single conversation is disabled, we will create a new conversation if previous conversation is resolved
+    @conversation = @inbox.lock_to_single_conversation ? @contact_inbox.conversations.last : @contact_inbox.conversations.open.last
     return if @conversation
 
     @conversation = ::Conversation.create!(conversation_params)
