291f193a30c6ae936876944c3a2c7bd6b47ac107
Shivam Mishra <scm.mymail@gmail.com>
2023-09-21 11:27:42 +0530

    chore: revert import style

--- app/javascript/dashboard/components/widgets/WootWriter/FullEditor.vue
+++ app/javascript/dashboard/components/widgets/WootWriter/FullEditor.vue
@@ -218,7 +218,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import '@chatwoot/prosemirror-schema/src/styles/article.scss';
+@import '~@chatwoot/prosemirror-schema/src/styles/article.scss';
 
 .ProseMirror-menubar-wrapper {
   display: flex;
