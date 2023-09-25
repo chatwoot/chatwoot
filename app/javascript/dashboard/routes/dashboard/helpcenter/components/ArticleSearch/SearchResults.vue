db355b8811188b3f96d5abe5b3e98b53aca51840
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2023-09-25 14:55:13 +0530

    Fixes animation width issue in loader

--- app/javascript/dashboard/routes/dashboard/helpcenter/components/ArticleSearch/SearchResults.vue
+++ app/javascript/dashboard/routes/dashboard/helpcenter/components/ArticleSearch/SearchResults.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-end gap-1 py-6 bg-white dark:bg-slate-900">
+  <div class="flex justify-end gap-1 py-4 bg-white dark:bg-slate-900">
     <div class="flex flex-col gap-1 w-full">
       <div v-if="isLoading" class="empty-state-message">
         <spinner size="" />
--- app/javascript/shared/components/ArticleSkeletonLoader.vue
+++ app/javascript/shared/components/ArticleSkeletonLoader.vue
@@ -1,26 +1,41 @@
 <template>
-  <div class="p-4 space-y-6 bg-white">
-    <div class="space-y-2 animate-pulse ">
-      <div class="h-6 bg-slate-100 rounded w-1/5" />
-      <div class="h-10 bg-slate-100 rounded w-3/5" />
+  <div class="p-4 space-y-6 bg-white dark:bg-slate-900">
+    <div class="space-y-2 pulse-animation">
+      <div class="h-6 bg-slate-100 dark:bg-slate-700 rounded w-1/5" />
+      <div class="h-10 bg-slate-100 dark:bg-slate-700 rounded w-3/5" />
     </div>
-    <div class="space-y-2 animate-pulse ">
-      <div class="h-5 bg-slate-100 rounded" />
-      <div class="h-5 bg-slate-100 rounded" />
-      <div class="h-5 bg-slate-100 rounded" />
-      <div class="h-5 bg-slate-100 rounded  w-4/5" />
+    <div class="space-y-2 pulse-animation">
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded" />
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded" />
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded" />
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded  w-4/5" />
     </div>
-    <div class="space-y-2 animate-pulse ">
-      <div class="h-5 bg-slate-100 rounded" />
-      <div class="h-5 bg-slate-100 rounded" />
-      <div class="h-5 bg-slate-100 rounded w-3/5" />
-      <div class="h-5 bg-slate-100 rounded" />
+    <div class="space-y-2 pulse-animation">
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded" />
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded" />
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded w-3/5" />
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded" />
     </div>
-    <div class="space-y-2 animate-pulse ">
-      <div class="h-5 bg-slate-100 rounded" />
-      <div class="h-5 bg-slate-100 rounded" />
-      <div class="h-5 bg-slate-100 rounded" />
-      <div class="h-5 bg-slate-100 rounded w-3/5" />
+    <div class="space-y-2 pulse-animation">
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded" />
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded" />
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded" />
+      <div class="h-5 bg-slate-100 dark:bg-slate-700 rounded w-3/5" />
     </div>
   </div>
 </template>
+<style scoped>
+.pulse-animation {
+  animation: pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+}
+
+@keyframes pulse {
+  0%,
+  100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
+}
+</style>
