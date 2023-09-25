85daa6addb0b606add8715af3162c50721cd5c9d
Shivam Mishra <scm.mymail@gmail.com>
2023-09-25 11:22:50 +0530

    fix: reset mocks

--- jest.api-mock.js
+++ jest.api-mock.js
@@ -2,4 +2,10 @@ import mockAxios from 'jest-mock-axios';
 
 beforeEach(() => {
   window.axios = mockAxios;
+  global.axios = mockAxios;
+});
+
+afterEach(() => {
+  // cleaning up the mess left behind the previous test
+  mockAxios.reset();
 });
