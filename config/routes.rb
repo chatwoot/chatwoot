cd3510272abc7e2cb0ee9a769da6dc420c860dc2
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-09-26 09:38:14 +0530

    fix: Set avatar for users(agent/contact) in slack channels (#7960)

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
     namespace :survey do
       resources :responses, only: [:show]
     end
+    resource :slack_uploads, only: [:show]
   end
 
   get '/api', to: 'api#index'
