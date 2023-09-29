b595adbf595122c01be0831789d7862aea661756
Pranav Raj S <pranav@chatwoot.com>
2023-09-29 12:38:48 +0530

    Update app/controllers/api/v1/integrations/webhooks_controller.rb

--- app/controllers/api/v1/integrations/webhooks_controller.rb
+++ app/controllers/api/v1/integrations/webhooks_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::Integrations::WebhooksController < ApplicationController
   private
 
   # TODO: This is a temporary solution to permit all params for slack unfurling job.
-  # We should only permit the params that we need  Handle all the params based on events and send it to the respective services
+  # We should only permit the params that we use. Handle all the params based on events and send it to the respective services.
   def permitted_params
     params.permit!
   end
