c5bb10aad3f821a45d69bf1d023d1a9d1ba705a5
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-10-03 17:30:28 +0530

    chore: code cleanup

--- app/jobs/slack_unfurl_job.rb
+++ app/jobs/slack_unfurl_job.rb
@@ -3,23 +3,26 @@ class SlackUnfurlJob < ApplicationJob
 
   def perform(params)
     integration_hook = find_integration_hook(params)
-    return unless integration_hook  # Handle if integration_hook is nil
+    return unless integration_hook
 
     slack_client = build_slack_client(integration_hook)
-    unfurl_service = build_unfurl_service(params, integration_hook)
-    # Check the channel has access to the bot to unfurl the links
-    response = slack_client.conversations_members(channel: params.dig(:event, :channel))
 
-    unfurl_service.perform if response['ok']
+    return unless channel_has_access?(slack_client, params)
+
+    unfurl_service = build_unfurl_service(params, integration_hook)
+
+    unfurl_service.perform
   end
 
   private
 
+  # Find the integration hook by taking first link from array of links
+  # Assume that all the links are from the same account, how ever there is a possibility that the links are from different accounts.
+  # TODO: Fix this edge case later
   def find_integration_hook(params)
-    # Find the integration hook by taking first link from the event params,
-    # Assume that all the links are from the same account, how ever there is a possibility that the links are from different accounts.
-    # Fix this issue later.
     url = extract_url(params)
+    return unless url
+
     account_id = extract_account_id(url)
     Integrations::Hook.find_by(account_id: account_id)
   end
@@ -40,5 +43,11 @@ class SlackUnfurlJob < ApplicationJob
 
   def build_unfurl_service(params, integration_hook)
     Integrations::Slack::SlackLinkUnfurlService.new(params: params, integration_hook: integration_hook)
+  end
+
+  # Check the channel has access to the bot to unfurl the links
+  def channel_has_access?(slack_client, params)
+    response = slack_client.conversations_members(channel: params.dig(:event, :channel))
+    response['ok']
   end
 end
