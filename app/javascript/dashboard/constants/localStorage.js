e4620291a72f29afe43efb25eb35d3652d115525
Shivam Mishra <scm.mymail@gmail.com>
2023-10-04 17:46:57 +0530

    feat: setup inReplyTo store

--- app/javascript/dashboard/constants/localStorage.js
+++ app/javascript/dashboard/constants/localStorage.js
@@ -2,6 +2,7 @@ export const LOCAL_STORAGE_KEYS = {
   DISMISSED_UPDATES: 'dismissedUpdates',
   WIDGET_BUILDER: 'widgetBubble_',
   DRAFT_MESSAGES: 'draftMessages',
+  MESSAGE_IN_REPLY_TO: 'messageInReplyTo',
   COLOR_SCHEME: 'color_scheme',
   DISMISSED_LABEL_SUGGESTIONS: 'labelSuggestionsDismissed',
 };
--- app/javascript/dashboard/store/modules/draftMessages.js
+++ app/javascript/dashboard/store/modules/draftMessages.js
@@ -7,12 +7,16 @@ import { LOCAL_STORAGE_KEYS } from 'dashboard/constants/localStorage';
 
 const state = {
   records: LocalStorage.get(LOCAL_STORAGE_KEYS.DRAFT_MESSAGES) || {},
+  inReplyTo: LocalStorage.get(LOCAL_STORAGE_KEYS.MESSAGE_IN_REPLY_TO) || {},
   replyEditorMode: REPLY_EDITOR_MODES.REPLY,
 };
 
 export const getters = {
   get: _state => key => {
     return _state.records[key] || '';
+  },
+  getInReplyTo: _state => key => {
+    return _state.inReplyTo[key];
   },
   getReplyEditorMode: _state => _state.replyEditorMode,
 };
@@ -23,6 +27,12 @@ export const actions = {
   },
   delete: ({ commit }, { key }) => {
     commit(types.SET_DRAFT_MESSAGES, { key });
+  },
+  setInReplyTo({ commit }, { key, inReplyTo }) {
+    commit(types.SET_IN_REPLY_TO, { key, inReplyTo });
+  },
+  deleteInReplyTo({ commit }, { key }) {
+    commit(types.REMOVE_IN_REPLY_TO, { key });
   },
   setReplyEditorMode: ({ commit }, { mode }) => {
     commit(types.SET_REPLY_EDITOR_MODE, { mode });
@@ -38,6 +48,15 @@ export const mutations = {
     const { [key]: draftToBeRemoved, ...updatedRecords } = $state.records;
     Vue.set($state, 'records', updatedRecords);
     LocalStorage.set(LOCAL_STORAGE_KEYS.DRAFT_MESSAGES, $state.records);
+  },
+  [types.SET_IN_REPLY_TO]($state, { key, inReplyTo }) {
+    Vue.set($state.inReplyTo, key, inReplyTo);
+    LocalStorage.set(LOCAL_STORAGE_KEYS.MESSAGE_IN_REPLY_TO, $state.inReplyTo);
+  },
+  [types.REMOVE_IN_REPLY_TO]($state, { key }) {
+    const { [key]: draftToBeRemoved, ...newRecords } = $state.inReplyTo;
+    Vue.set($state, 'inReplyTo', newRecords);
+    LocalStorage.set(LOCAL_STORAGE_KEYS.DRAFT_MESSAGES, $state.inReplyTo);
   },
   [types.SET_REPLY_EDITOR_MODE]($state, { mode }) {
     Vue.set($state, 'replyEditorMode', mode);
