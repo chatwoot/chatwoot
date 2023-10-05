72ba42915999abe3f9d4a9d7ae76a244e787d8d8
Sojan Jose <sojan@pepalo.com>
2023-10-05 00:11:01 -0700

    fix: Show sources in bot response only if documents are present (#8055)
    - show sources section in bot only based on appropriate conditions

--- spec/enterprise/services/enterprise/message_templates/response_bot_service_spec.rb
+++ spec/enterprise/services/enterprise/message_templates/response_bot_service_spec.rb
@@ -30,6 +30,36 @@ RSpec.describe Enterprise::MessageTemplates::ResponseBotService, type: :service 
         last_message = conversation.messages.last
         expect(last_message.content).to include('some_response')
         expect(last_message.content).to include(Response.first.question)
+        expect(last_message.content).to include('**Sources**')
+      end
+    end
+
+    context 'when context_ids are not present' do
+      it 'creates an outgoing message without article references' do
+        allow(chat_gpt_double).to receive(:generate_response).and_return({ 'response' => 'some_response' })
+
+        expect do
+          service.perform
+        end.to change { conversation.messages.where(message_type: :outgoing).count }.by(1)
+
+        last_message = conversation.messages.last
+        expect(last_message.content).to include('some_response')
+        expect(last_message.content).not_to include('**Sources**')
+      end
+    end
+
+    context 'when response doesnt have response document' do
+      it 'creates an outgoing message without article references' do
+        response = create(:response, response_source: response_source, response_document: nil)
+        allow(chat_gpt_double).to receive(:generate_response).and_return({ 'response' => 'some_response', 'context_ids' => [response.id] })
+
+        expect do
+          service.perform
+        end.to change { conversation.messages.where(message_type: :outgoing).count }.by(1)
+
+        last_message = conversation.messages.last
+        expect(last_message.content).to include('some_response')
+        expect(last_message.content).not_to include('**Sources**')
       end
     end
 
