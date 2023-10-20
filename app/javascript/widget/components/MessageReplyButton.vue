bb171fb11045d5babd19f1c50589af826a358d34
Shivam Mishra <scm.mymail@gmail.com>
2023-10-20 16:00:03 +0530

    feat: update message size

--- app/javascript/widget/components/MessageReplyButton.vue
+++ app/javascript/widget/components/MessageReplyButton.vue
@@ -1,9 +1,9 @@
 <template>
   <button
-    class="p-1.5 mb-1 rounded-full dark:text-slate-500 dark:bg-slate-900 text-slate-600 bg-slate-100 hover:text-slate-800"
+    class="p-1 mb-1 rounded-full dark:text-slate-500 dark:bg-slate-900 text-slate-600 bg-slate-100 hover:text-slate-800"
     @click="toggleReply"
   >
-    <FluentIcon icon="arrow-reply" size="12" class="flex-shrink-0" />
+    <FluentIcon icon="arrow-reply" size="11" class="flex-shrink-0" />
   </button>
 </template>
 
