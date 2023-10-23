5d20b5f6a3851b60e0c39a27c842a0bd966e8e0a
iamsivin <iamsivin@gmail.com>
2023-10-23 10:27:35 +0530

    chore: Revert code

--- app/helpers/portal_helper.rb
+++ app/helpers/portal_helper.rb
@@ -1,8 +1,7 @@
-# require 'redcarpet'
 module PortalHelper
   def generate_portal_bg_color(portal_color, theme)
     base_color = theme == 'dark' ? 'black' : 'white'
-    "color-mix(in srgb, #{portal_color} 20%, #{base_color})"
+    "color-mix(in srgb, #{portal_color} 0%, #{base_color})"
   end
 
   def generate_portal_bg(portal_color, theme)
@@ -28,11 +27,4 @@ module PortalHelper
   def generate_gradient_to_bottom(theme)
     "background-image: linear-gradient(to bottom, transparent, #{theme == 'dark' ? '#151718' : 'white'})"
   end
-
-  # def markdown_to_html(text)
-  #   renderer = Redcarpet::Render::HTML.new
-  #   markdown = Redcarpet::Markdown.new(renderer)
-  #   sanitized_output = sanitize(markdown.render(text))
-  #   sanitized_output.html_safe
-  # end
 end
