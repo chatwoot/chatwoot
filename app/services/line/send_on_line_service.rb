4c38259060d45d31d933083ccbd9ca5ea2c17e32
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-10-24 23:18:23 +0530

    chore: Add some comments

--- app/services/line/send_on_line_service.rb
+++ app/services/line/send_on_line_service.rb
@@ -8,12 +8,15 @@ class Line::SendOnLineService < Base::SendOnChannelService
   def perform_reply
     response = channel.client.push_message(message.conversation.contact_inbox.source_id, [{ type: 'text', text: nil }])
     parsed_json = JSON.parse(response.body)
+    # If the request not success, update the message status to failed and save the external error
     message.update!(status: :failed, external_error: external_error(parsed_json)) unless response.code == '200'
   end
 
   # https://developers.line.biz/en/reference/messaging-api/#error-responses
   def external_error(error)
+    # Message containing information about the error. See https://developers.line.biz/en/reference/messaging-api/#error-messages
     message = error['message']
+    # An array of error details. If the array is empty, this property will not be included in the response.
     details = error['details']
 
     return message if details.empty?
