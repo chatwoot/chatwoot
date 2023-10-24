4ab4052ad416e48440b30d447d4a78c70440ddce
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-10-25 00:16:14 +0530

    chore: Add DeliveryStatusService

--- app/jobs/webhooks/sms_events_job.rb
+++ app/jobs/webhooks/sms_events_job.rb
@@ -1,13 +1,22 @@
 class Webhooks::SmsEventsJob < ApplicationJob
   queue_as :default
 
+  SUPPORTED_EVENTS = %w[message-received message-delivered message-failed].freeze
+
   def perform(params = {})
-    return unless params[:type] == 'message-received'
+    return unless SUPPORTED_EVENTS.include?(params[:event])
 
     channel = Channel::Sms.find_by(phone_number: params[:to])
     return unless channel
 
-    # TODO: pass to appropriate provider service from here
+    Sms::DeliveryStatusService.new(channel: channel, params: params[:message].with_indifferent_access).perform if delivery_event?
+
     Sms::IncomingMessageService.new(inbox: channel.inbox, params: params[:message].with_indifferent_access).perform
+  end
+
+  private
+
+  def delivery_event?
+    params[:event] == 'message-delivered' || params[:event] == 'message-failed'
   end
 end
