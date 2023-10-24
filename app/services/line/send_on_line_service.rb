584c671c2737c0248bd33ed58928ca423296723a
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-10-24 19:50:17 +0530

    chore: handle error code

--- app/services/line/send_on_line_service.rb
+++ app/services/line/send_on_line_service.rb
@@ -6,7 +6,22 @@ class Line::SendOnLineService < Base::SendOnChannelService
   end
 
   def perform_reply
-    response = channel.client.push_message(message.conversation.contact_inbox.source_id, [{ type: 'text', text: message.content }])
-    message.update!(status: :failed, external_error: "#{response.code} - #{response.message}") unless response.code == 200
+    response = channel.client.push_message(message.conversation.contact_inbox.source_id, [{ type: 'text', text: nil }])
+    parsed_json = JSON.parse(response.body)
+    message.update!(status: :failed, external_error: external_error(parsed_json)) unless response.code == '200'
+  end
+
+  # https://developers.line.biz/en/reference/messaging-api/#error-responses
+  def external_error(error)
+    message = error['message']
+    details = error['details']
+
+    return message if details.empty?
+
+    detail_messages = details.map do |detail|
+      "#{detail['property']}: #{detail['message']}"
+    end
+
+    [message, detail_messages].flatten.join(', ')
   end
 end
