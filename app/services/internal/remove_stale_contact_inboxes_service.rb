ec2cd26f266bf73418042b1fff6373374036e5a6
Vishnu Narayanan <vishnu@chatwoot.com>
2023-10-26 11:43:42 +0530

    chore: refactor and add env var to toggle deletion job

--- app/services/internal/remove_stale_contact_inboxes_service.rb
+++ app/services/internal/remove_stale_contact_inboxes_service.rb
@@ -1,21 +1,37 @@
 class Internal::RemoveStaleContactInboxesService
   def perform
-    ninety_days_ago = 90.days.ago
-    # time_period = ENV.fetch('STALE_CONTACT_INBOX_TIME_PERIOD', ninety_days_ago)
-    time_period = ninety_days_ago
-    contact_inboxes_to_delete = ContactInbox.left_joins(:conversations)
-                                            .where('contact_inboxes.created_at < ?', time_period)
-                                            .where(conversations: { contact_id: nil })
+    return unless remove_stale_contact_inbox_job_enabled?
 
-    Rails.logger.info "Deleting #{contact_inboxes_to_delete.count} stale contact inboxes older than #{time_period}"
+    time_period = 90.days.ago
+    contact_inboxes_to_delete = stale_contact_inboxes(time_period)
+
+    log_stale_contact_inboxes_deletion(contact_inboxes_to_delete, time_period)
+
+    # since the number of record is very high, delete_all would be faster than destroy_all
+    contact_inboxes_to_delete.delete_all
+  end
+
+  private
+
+  def remove_stale_contact_inbox_job_enabled?
+    job_status = ENV.fetch('REMOVE_STALE_CONTACT_INBOX_JOB_STATUS', false)
+    return false unless ActiveModel::Type::Boolean.new.cast(job_status)
+
+    true
+  end
+
+  def stale_contact_inboxes(time_period)
+    ContactInbox.left_joins(:conversations)
+                .where('contact_inboxes.created_at < ?', time_period)
+                .where(conversations: { contact_id: nil })
+  end
+
+  def log_stale_contact_inboxes_deletion(contact_inboxes, time_period)
+    count = contact_inboxes.count
+    Rails.logger.info "Deleting #{count} stale contact inboxes older than #{time_period}"
 
     # Log the SQL query without executing it
-    sql_query = contact_inboxes_to_delete.to_sql
+    sql_query = contact_inboxes.to_sql
     Rails.logger.info("SQL Query: #{sql_query}")
-
-    # commenting out the delete for now
-    # since the number of record is very high, delete_all would be faster
-    # contact_inboxes_to_delete.destroy_all
-    contact_inboxes_to_delete.delete_all
   end
 end
--- spec/services/internal/remove_stale_contact_inboxes_service_spec.rb
+++ spec/services/internal/remove_stale_contact_inboxes_service_spec.rb
@@ -4,17 +4,26 @@ require 'rails_helper'
 
 RSpec.describe Internal::RemoveStaleContactInboxesService do
   describe '#perform' do
+    it 'does not delete stale contact inboxes if REMOVE_STALE_CONTACT_INBOX_JOB_STATUS is false' do
+      # default value of REMOVE_STALE_CONTACT_INBOX_JOB_STATUS is false
+      create(:contact_inbox, created_at: 3.days.ago)
+      create(:contact_inbox, created_at: 91.days.ago)
+      create(:contact_inbox, created_at: 92.days.ago)
+      create(:contact_inbox, created_at: 93.days.ago)
+      create(:contact_inbox, created_at: 94.days.ago)
+
+      service = described_class.new
+      expect { service.perform }.not_to change(ContactInbox, :count)
+    end
+
     it 'deletes stale contact inboxes' do
-      with_modified_env STALE_CONTACT_INBOX_TIME_PERIOD: '120.days.ago' do
-        # Create some sample ContactInbox records for testing
-        # Replace this with your own setup as needed
+      with_modified_env REMOVE_STALE_CONTACT_INBOX_JOB_STATUS: 'true' do
         create(:contact_inbox, created_at: 3.days.ago)
         create(:contact_inbox, created_at: 91.days.ago)
         create(:contact_inbox, created_at: 92.days.ago)
         create(:contact_inbox, created_at: 93.days.ago)
         create(:contact_inbox, created_at: 94.days.ago)
 
-        # Create an instance of the service and call the perform method
         service = described_class.new
         expect { service.perform }.to change(ContactInbox, :count).by(-4)
       end
