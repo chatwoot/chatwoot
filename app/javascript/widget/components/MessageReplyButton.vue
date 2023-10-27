d671533bca86ffdff7ed47b3211882df6d460bf4
Shivam Mishra <scm.mymail@gmail.com>
2023-10-27 13:35:02 +0530

    feat: show ReplyTo in widget UI (#8094)
    Co-authored-by: Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>

---
+++ app/javascript/widget/components/MessageReplyButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="p-1 mb-1 rounded-full dark:text-slate-500 dark:bg-slate-900 text-slate-600 bg-slate-100 hover:text-slate-800"
+    @click="$emit('click')"
+  >
+    <FluentIcon icon="arrow-reply" size="11" class="flex-shrink-0" />
+  </button>
+</template>
+
+<script>
+import FluentIcon from 'shared/components/FluentIcon/Index.vue';
+
+export default {
+  name: 'MessageReplyButton',
+  components: { FluentIcon },
+};
+</script>
