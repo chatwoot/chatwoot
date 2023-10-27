954e8bd28f19ee7eb11507d35da6479035a0d661
iamsivin <iamsivin@gmail.com>
2023-10-27 22:17:40 +0530

    chore: Fix and adds spec

--- app/javascript/portal/specs/portal.spec.js
+++ app/javascript/portal/specs/portal.spec.js
@@ -3,12 +3,14 @@ import {
   generatePortalBgColor,
   generatePortalBg,
   generateGradientToBottom,
-  setPortalStyles,
+  setPortalBackgroundStyles,
+  setPortalHoverStyles,
   setPortalClass,
   updateThemeStyles,
   toggleAppearanceDropdown,
   updateURLParameter,
   removeURLParameter,
+  switchTheme,
 } from '../portalHelpers';
 
 describe('#navigateToLocalePage', () => {
@@ -50,14 +52,14 @@ describe('Theme Functions', () => {
     it('returns background for dark theme', () => {
       const result = generatePortalBg('#FF5733', 'dark');
       expect(result).toBe(
-        'background: url(/assets/images/hc/hexagon-dark.svg) color-mix(in srgb, #FF5733 20%, black)'
+        'url(/assets/images/hc/hexagon-dark.svg) color-mix(in srgb, #FF5733 20%, black)'
       );
     });
 
     it('returns background for light theme', () => {
       const result = generatePortalBg('#FF5733', 'light');
       expect(result).toBe(
-        'background: url(/assets/images/hc/hexagon-light.svg) color-mix(in srgb, #FF5733 20%, white)'
+        'url(/assets/images/hc/hexagon-light.svg) color-mix(in srgb, #FF5733 20%, white)'
       );
     });
   });
@@ -65,52 +67,73 @@ describe('Theme Functions', () => {
   describe('#generateGradientToBottom', () => {
     it('returns gradient for dark theme', () => {
       const result = generateGradientToBottom('dark');
-      expect(result).toBe(
-        'background-image: linear-gradient(to bottom, transparent, #151718)'
-      );
+      expect(result).toBe('linear-gradient(to bottom, transparent, #151718)');
     });
 
     it('returns gradient for light theme', () => {
       const result = generateGradientToBottom('light');
-      expect(result).toBe(
-        'background-image: linear-gradient(to bottom, transparent, white)'
-      );
+      expect(result).toBe('linear-gradient(to bottom, transparent, white)');
     });
   });
 
-  describe('#setPortalStyles', () => {
-    let mockPortalBgDiv;
-    let mockPortalBgGradientDiv;
-
+  describe('#setPortalBackgroundStyles', () => {
     beforeEach(() => {
-      // Mocking portal background div
-      mockPortalBgDiv = document.createElement('div');
-      mockPortalBgDiv.id = 'portal-bg';
-      document.body.appendChild(mockPortalBgDiv);
-
-      // Mocking portal background gradient div
-      mockPortalBgGradientDiv = document.createElement('div');
-      mockPortalBgGradientDiv.id = 'portal-bg-gradient';
-      document.body.appendChild(mockPortalBgGradientDiv);
+      window.portalConfig = { portalColor: '#FF5733' };
     });
 
     afterEach(() => {
-      document.body.innerHTML = '';
+      document.documentElement.style.removeProperty('--dynamic-portal-bg');
+      document.documentElement.style.removeProperty(
+        '--dynamic-portal-bg-gradient'
+      );
     });
 
     it('sets styles for portal background based on theme', () => {
-      window.portalConfig = { portalColor: '#FF5733' };
+      setPortalBackgroundStyles('dark');
 
-      setPortalStyles('dark');
+      const portalBgStyle = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue('--dynamic-portal-bg');
+      const portalBgGradientStyle = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue('--dynamic-portal-bg-gradient');
+
       const expectedPortalBgStyle =
-        'background: url(/assets/images/hc/hexagon-dark.svg) color-mix(in srgb, #FF5733 20%, black)';
+        'url(/assets/images/hc/hexagon-dark.svg) color-mix(in srgb, #FF5733 20%, black)';
       const expectedGradientStyle =
-        'background-image: linear-gradient(to bottom, transparent, #151718)';
+        'linear-gradient(to bottom, transparent, #151718)';
 
-      expect(mockPortalBgDiv.getAttribute('style')).toBe(expectedPortalBgStyle);
-      expect(mockPortalBgGradientDiv.getAttribute('style')).toBe(
-        expectedGradientStyle
-      );
+      expect(portalBgStyle.trim()).toBe(expectedPortalBgStyle);
+      expect(portalBgGradientStyle.trim()).toBe(expectedGradientStyle);
+    });
+  });
+
+  describe('#setPortalHoverStyles', () => {
+    beforeEach(() => {
+      window.portalConfig = { portalColor: '#ff5733' };
+    });
+
+    afterEach(() => {
+      document.documentElement.style.removeProperty('--dynamic-hover-bg-color');
+      document.documentElement.style.removeProperty('--dynamic-hover-color');
+    });
+
+    it('sets styles for portal hover based on theme', () => {
+      setPortalHoverStyles('dark');
+
+      const hoverBgColorStyle = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue('--dynamic-hover-bg-color');
+      const hoverColorStyle = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue('--dynamic-hover-color');
+
+      const expectedHoverBgColorStyle =
+        'color-mix(in srgb, #ff5733 5%, #1B1B1B)';
+      const expectedHoverColorStyle = '#ff5733';
+
+      expect(hoverBgColorStyle.trim()).toBe(expectedHoverBgColorStyle);
+      expect(hoverColorStyle.trim()).toBe(expectedHoverColorStyle);
     });
   });
 
@@ -203,26 +226,47 @@ describe('Theme Functions', () => {
     });
   });
 
-  describe('#updateThemeStyles', () => {
+  describe('switchTheme', () => {
     let mockPortalDiv;
-    let mockPortalBgDiv;
-    let mockPortalBgGradientDiv;
 
     beforeEach(() => {
-      // Mocking portal div
       mockPortalDiv = document.createElement('div');
       mockPortalDiv.id = 'portal';
       document.body.appendChild(mockPortalDiv);
+    });
 
-      // Mocking portal background div
-      mockPortalBgDiv = document.createElement('div');
-      mockPortalBgDiv.id = 'portal-bg';
-      document.body.appendChild(mockPortalBgDiv);
+    afterEach(() => {
+      document.body.innerHTML = '';
+    });
 
-      // Mocking portal background gradient div
-      mockPortalBgGradientDiv = document.createElement('div');
-      mockPortalBgGradientDiv.id = 'portal-bg-gradient';
-      document.body.appendChild(mockPortalBgGradientDiv);
+    it('updates theme based on theme', () => {
+      window.portalConfig = { portalColor: '#FF5733' };
+      switchTheme('dark');
+
+      const portalBgStyle = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue('--dynamic-portal-bg');
+      const portalBgGradientStyle = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue('--dynamic-portal-bg-gradient');
+
+      const expectedPortalBgStyle =
+        'url(/assets/images/hc/hexagon-dark.svg) color-mix(in srgb, #FF5733 20%, black)';
+      const expectedGradientStyle =
+        'linear-gradient(to bottom, transparent, #151718)';
+      expect(portalBgStyle.trim()).toBe(expectedPortalBgStyle);
+      expect(portalBgGradientStyle.trim()).toBe(expectedGradientStyle);
+      expect(mockPortalDiv.classList.contains('dark')).toBe(true);
+    });
+  });
+
+  describe('#updateThemeStyles', () => {
+    let mockPortalDiv;
+
+    beforeEach(() => {
+      mockPortalDiv = document.createElement('div');
+      mockPortalDiv.id = 'portal';
+      document.body.appendChild(mockPortalDiv);
     });
 
     afterEach(() => {
@@ -231,19 +275,23 @@ describe('Theme Functions', () => {
 
     it('updates theme styles based on theme', () => {
       window.portalConfig = { portalColor: '#FF5733' };
-
       updateThemeStyles('dark');
 
-      const expectedPortalBgStyle =
-        'background: url(/assets/images/hc/hexagon-dark.svg) color-mix(in srgb, #FF5733 20%, black)';
-      const expectedGradientStyle =
-        'background-image: linear-gradient(to bottom, transparent, #151718)';
+      const portalBgStyle = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue('--dynamic-portal-bg');
+      const portalBgGradientStyle = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue('--dynamic-portal-bg-gradient');
 
+      const expectedPortalBgStyle =
+        'url(/assets/images/hc/hexagon-dark.svg) color-mix(in srgb, #FF5733 20%, black)';
+      const expectedGradientStyle =
+        'linear-gradient(to bottom, transparent, #151718)';
+
+      expect(portalBgStyle.trim()).toBe(expectedPortalBgStyle);
+      expect(portalBgGradientStyle.trim()).toBe(expectedGradientStyle);
       expect(mockPortalDiv.classList.contains('dark')).toBe(true);
-      expect(mockPortalBgDiv.getAttribute('style')).toBe(expectedPortalBgStyle);
-      expect(mockPortalBgGradientDiv.getAttribute('style')).toBe(
-        expectedGradientStyle
-      );
     });
   });
 
@@ -277,19 +325,20 @@ describe('Theme Functions', () => {
       expect(mockPortalDiv.classList.contains('dark')).toBe(true);
     });
 
-    it('does not update theme if themeFromServer is not "system"', () => {
-      const mediaQueryList = {
-        matches: true,
-        addEventListener: jest.fn(),
-      };
-      window.matchMedia = jest.fn().mockReturnValue(mediaQueryList);
+    it('updates theme based on portal theme dark', () => {
       window.portalConfig = { theme: 'dark' };
 
       InitializationHelpers.initializeTheme();
 
-      expect(mediaQueryList.addEventListener).not.toBeCalled();
-      expect(mockPortalDiv.classList.contains('dark')).toBe(false);
-      expect(mockPortalDiv.classList.contains('light')).toBe(false);
+      expect(mockPortalDiv.classList.contains('dark')).toBe(true);
+    });
+
+    it('updates theme based on portal theme light', () => {
+      window.portalConfig = { theme: 'light' };
+
+      InitializationHelpers.initializeTheme();
+
+      expect(mockPortalDiv.classList.contains('light')).toBe(true);
     });
   });
 
