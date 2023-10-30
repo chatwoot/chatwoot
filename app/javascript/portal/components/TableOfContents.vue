deee18f3a1f7376eb8d052fb6815be47e22384e3
iamsivin <iamsivin@gmail.com>
2023-10-30 11:27:06 +0530

    chore: Minor fix

--- app/javascript/portal/components/TableOfContents.vue
+++ app/javascript/portal/components/TableOfContents.vue
@@ -6,7 +6,7 @@
           id="on-this-page-title"
           class="text-slate-800 pl-6 dark:text-slate-50 font-semibold tracking-wide py-3 leading-7 border-l-2 border-solid border-slate-100 dark:border-slate-800"
           :class="{
-            'border-slate-400 dark:border-slate-100': !currentSlug,
+            '!border-slate-400 dark:!border-slate-100': !currentSlug,
           }"
         >
           {{ tocHeader }}
@@ -15,21 +15,15 @@
           <li
             v-for="element in rows"
             :key="element.slug"
-            class="leading-6 py-2 pl-6 border-l-2 border-solid border-slate-100 dark:border-slate-800"
-            :class="{
-              'border-slate-400 dark:border-slate-50 transition-colors duration-200':
-                getActiveSlug(element),
-            }"
+            class="leading-6 py-2 pl-6 border-l-2 border-solid"
+            :class="elementBorderStyles(element)"
           >
             <p :class="getClassName(element)">
               <a
                 :href="`#${element.slug}`"
                 data-turbolinks="false"
-                class="text-base text-slate-700 dark:text-slate-100 cursor-pointer"
-                :class="{
-                  'font-semibold text-slate-900 dark:text-slate-25':
-                    getActiveSlug(element),
-                }"
+                class="text-base cursor-pointer"
+                :class="elementTextStyles(element)"
               >
                 {{ element.title }}
               </a>
@@ -94,8 +88,26 @@ export default {
     onURLHashChange() {
       this.currentSlug = window.location?.hash?.substring(1) || '';
     },
-    getActiveSlug(el) {
+    isElementActive(el) {
       return this.currentSlug === el.slug;
+    },
+    tocHeaderBorderStyles() {
+      if (this.currentSlug === '') {
+        return 'border-slate-400 dark:border-slate-100';
+      }
+      return 'border-slate-100 dark:border-slate-800';
+    },
+    elementBorderStyles(el) {
+      if (this.isElementActive(el)) {
+        return 'border-slate-400 dark:border-slate-50 transition-colors duration-200';
+      }
+      return 'border-slate-100 dark:border-slate-800';
+    },
+    elementTextStyles(el) {
+      if (this.isElementActive(el)) {
+        return 'font-semibold text-slate-900 dark:text-slate-25';
+      }
+      return 'text-slate-700 dark:text-slate-100';
     },
   },
 };
