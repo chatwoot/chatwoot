c22769b483c9331740b1312c6d05ab871ca3bb80
Pranav Raj S <pranav@chatwoot.com>
2023-10-31 19:38:28 -0700

    Remove unused file

--- spec/cypress.config.js
+++ spec/cypress.config.js
@@ -5,12 +5,6 @@ module.exports = defineConfig({
   viewportWidth: 1250,
   viewportHeight: 800,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      // eslint-disable-next-line
-      return require('./cypress/plugins/index.js')(on, config);
-    },
     baseUrl: 'http://localhost:5050',
   },
 });
