20a8c614001099ffd0489d937f1da12883b6245e
Pranav Raj S <pranav@chatwoot.com>
2023-11-06 13:37:05 -0800

    Remove unnecessary change

--- app/controllers/api/v1/widget/configs_controller.rb
+++ app/controllers/api/v1/widget/configs_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::Widget::ConfigsController < Api::V1::Widget::BaseController
   def create
     build_contact
     set_token
-    set_campaigns
   end
 
   private
@@ -25,10 +24,6 @@ class Api::V1::Widget::ConfigsController < Api::V1::Widget::BaseController
 
     @contact_inbox = @web_widget.create_contact_inbox(additional_attributes)
     @contact = @contact_inbox.contact
-  end
-
-  def set_campaigns
-    @campaigns = @web_widget.inbox.campaigns.where(enabled: true)
   end
 
   def set_token
