ba2dad387e3b8468a051cc74039f709c5b3d47a7
iamsivin <iamsivin@gmail.com>
2023-11-09 14:25:14 +0530

    chore: Remove un used helper

--- app/helpers/portal_helper.rb
+++ app/helpers/portal_helper.rb
@@ -24,26 +24,6 @@ module PortalHelper
     language_map[locale] || locale
   end
 
-  def get_theme_names(theme)
-    if theme == 'light'
-      I18n.t('public_portal.header.appearance.light')
-    elsif theme == 'dark'
-      I18n.t('public_portal.header.appearance.dark')
-    else
-      I18n.t('public_portal.header.appearance.system')
-    end
-  end
-
-  def get_theme_icon(theme)
-    if theme == 'light'
-      'icons/sun'
-    elsif theme == 'dark'
-      'icons/moon'
-    else
-      'icons/monitor'
-    end
-  end
-
   def generate_home_link(portal_slug, portal_locale, theme, is_plain_layout_enabled)
     if is_plain_layout_enabled
       "/hc/#{portal_slug}/#{portal_locale}#{theme.present? && theme != 'system' ? "?theme=#{theme}" : ''}"
