c4af97ad5e2b5dea7a23c6f9e77355814f669459
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2023-11-09 07:29:03 +0530

    feat: Updated home page design (#8106)
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>

--- app/helpers/portal_helper.rb
+++ app/helpers/portal_helper.rb
@@ -1,11 +1,11 @@
 module PortalHelper
   def generate_portal_bg_color(portal_color, theme)
     base_color = theme == 'dark' ? 'black' : 'white'
-    "color-mix(in srgb, #{portal_color} 10%, #{base_color})"
+    "color-mix(in srgb, #{portal_color} 20%, #{base_color})"
   end
 
   def generate_portal_bg(portal_color, theme)
-    bg_image = theme == 'dark' ? 'grid_dark.svg' : 'grid.svg'
+    bg_image = theme == 'dark' ? 'hexagon-dark.svg' : 'hexagon-light.svg'
     "background: url(/assets/images/hc/#{bg_image}) #{generate_portal_bg_color(portal_color, theme)}"
   end
 
@@ -32,5 +32,9 @@ module PortalHelper
     else
       'icons/monitor'
     end
+  end
+
+  def generate_gradient_to_bottom(theme)
+    "background-image: linear-gradient(to bottom, transparent, #{theme == 'dark' ? '#151718' : 'white'})"
   end
 end
