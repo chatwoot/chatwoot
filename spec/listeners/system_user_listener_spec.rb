924628156557450d91094639db887325e9e87653
Douglas Lara <douglasdelaras@gmail.com>
2023-11-17 15:24:25 -0300

    Improve tests

--- spec/listeners/system_user_listener_spec.rb
+++ spec/listeners/system_user_listener_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 describe SystemUserListener do
   include ActiveJob::TestHelper
-  # let(:listener) { described_class.instance }
 
   describe '#account_created' do
-    let!(:system_user_1) { create(:system_user) }
-    let!(:system_user_2) { create(:system_user) }
+    let(:system_user_1) { create(:system_user) }
+    let(:system_user_2) { create(:system_user) }
+    let(:account) { create(:account) }
 
     it 'adds system users' do
       perform_enqueued_jobs(only: EventDispatcherJob) do
-        Account.create(name: 'test')
+        expect(system_user_1.accounts).to eq([account])
+        expect(system_user_2.accounts).to eq([account])
       end
-      expect(Account.last.users.count).to eq(2)
     end
   end
 
   describe '#system_user_created' do
-    let!(:account_1) { create(:account) }
-    let!(:account_2) { create(:account) }
+    let(:system_user_1) { create(:system_user) }
+    let(:system_user_2) { create(:system_user) }
+    let(:account) { create(:account) }
 
     it 'adds accounts to system user' do
       perform_enqueued_jobs(only: EventDispatcherJob) do
-        create(:system_user)
-        create(:system_user)
+        expect(system_user_1.accounts).to eq([account])
+        expect(system_user_2.accounts).to eq([account])
       end
-      expect(Account.last.users.count).to eq(2)
     end
   end
 end
