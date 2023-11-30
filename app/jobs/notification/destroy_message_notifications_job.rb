d0fcf7fa1c76675396e02acb6f6bb38fb18276f3
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-11-30 14:57:00 +0530

    chore: add DestoryMessageNotificationsJob

---
+++ app/jobs/notification/destroy_message_notifications_job.rb
@@ -0,0 +1,14 @@
+class Notification::DestoryMessageNotificationsJob < ApplicationJob
+  queue_as :low
+
+  def perform(notification)
+    # Ensure the notification object is passed correctly
+    return unless notification.is_a?(Notification)
+
+    # Destroy all the existing notifications where primary actor is Message and do it in batches
+    Notification.where(user_id: notification.user_id)
+                .where(primary_actor_type: 'Message')
+                .where.not(id: notification.id)
+                .find_each(batch_size: 100, &:destroy)
+  end
+end
