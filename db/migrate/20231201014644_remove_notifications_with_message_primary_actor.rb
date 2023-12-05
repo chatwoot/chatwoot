248d2e548cce0f99ed3b62433dbf8d948bf55e13
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-12-05 14:29:17 +0530

    chore: cleanup the migration

--- db/migrate/20231201014644_remove_notifications_with_message_primary_actor.rb
+++ db/migrate/20231201014644_remove_notifications_with_message_primary_actor.rb
@@ -1,9 +1,5 @@
 class RemoveNotificationsWithMessagePrimaryActor < ActiveRecord::Migration[7.0]
-  def up
-    Notification.where(primary_actor_type: 'Message').in_batches(of: 100).each_record(&:destroy)
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
+  def change
+    Migration::RemoveMessageNotifications.perform_later
   end
 end
