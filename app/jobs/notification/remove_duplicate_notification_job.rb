68ff48b33641aabb6c2627e8f2ab6d921743763a
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-12-06 11:36:50 +0530

    chore: code cleanup

--- app/jobs/notification/remove_duplicate_notification_job.rb
+++ app/jobs/notification/remove_duplicate_notification_job.rb
@@ -2,15 +2,15 @@ class Notification::RemoveDuplicateNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(notification)
-    # Ensure the notification object is passed correctly
     return unless notification.is_a?(Notification)
 
     user_id = notification.user_id
     primary_actor_id = notification.primary_actor_id
 
-    # Find and destroy older notifications with the same user and primary_actor_id, excluding the new one
-    Notification.where(user_id: user_id, primary_actor_id: primary_actor_id)
-                .where.not(id: notification.id)
-                .destroy_all
+    # Find older notifications with the same user and primary_actor_id, excluding the new one
+    duplicate_notifications = Notification.where(user_id: user_id, primary_actor_id: primary_actor_id)
+                                          .where.not(id: notification.id)
+
+    duplicate_notifications.each(&:destroy)
   end
 end
