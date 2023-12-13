55d56c88bfe8fad76d7b6db5251fced1c66a8871
iamsivin <iamsivin@gmail.com>
2023-12-13 16:59:47 +0530

    chore: Minor fix

--- app/javascript/dashboard/components/ConversationItem.vue
+++ app/javascript/dashboard/components/ConversationItem.vue
@@ -9,15 +9,15 @@
     :selected="isConversationSelected(source.id)"
     :show-assignee="showAssignee"
     :enable-context-menu="true"
-    @select-conversation="onSelectConversation"
-    @de-select-conversation="onDeSelectConversation"
-    @assign-agent="onAssignAgent"
-    @assign-team="onAssignTeam"
-    @assign-label="onAssignLabel"
-    @update-conversation-status="toggleConversationStatus"
-    @context-menu-toggle="onContextMenuToggle"
-    @mark-as-unread="onClickMarkAsUnread"
-    @assign-priority="onAssignPriority"
+    @select-conversation="selectConversation"
+    @de-select-conversation="deSelectConversation"
+    @assign-agent="assignAgent"
+    @assign-team="assignTeam"
+    @assign-label="assignLabels"
+    @update-conversation-status="updateConversationStatus"
+    @context-menu-toggle="toggleContextMenu"
+    @mark-as-unread="markAsUnread"
+    @assign-priority="assignPriority"
   />
 </template>
 
@@ -66,35 +66,6 @@ export default {
     showAssignee: {
       type: Boolean,
       default: false,
-    },
-  },
-  methods: {
-    onSelectConversation(conversationId, inboxId) {
-      this.selectConversation(conversationId, inboxId);
-    },
-    onDeSelectConversation(conversationId, inboxId) {
-      this.deSelectConversation(conversationId, inboxId);
-    },
-    onAssignAgent(agent, conversationId) {
-      this.assignAgent(agent, conversationId);
-    },
-    onAssignTeam(team, conversationId) {
-      this.assignTeam(team, conversationId);
-    },
-    onAssignLabel(labelTitle, conversationId) {
-      this.assignLabels(labelTitle, conversationId);
-    },
-    toggleConversationStatus(conversationId, status, snoozedUntil) {
-      this.updateConversationStatus(conversationId, status, snoozedUntil);
-    },
-    onContextMenuToggle(value) {
-      this.toggleContextMenu(value);
-    },
-    onClickMarkAsUnread(conversationId) {
-      this.markAsUnread(conversationId);
-    },
-    onAssignPriority(priority, conversationId) {
-      this.assignPriority(priority, conversationId);
     },
   },
 };
