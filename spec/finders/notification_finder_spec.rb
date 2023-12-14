340ba9bc5391f0c34702350108c135d3230cd4b7
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-12-14 19:44:10 +0530

    Update notification_finder_spec.rb

--- spec/finders/notification_finder_spec.rb
+++ spec/finders/notification_finder_spec.rb
@@ -7,9 +7,9 @@ describe NotificationFinder do
   let!(:user) { create(:user, account: account) }
 
   before do
-    create(:notification, account: account, user: user)
-    create(:notification, account: account, user: user)
-    create(:notification, account: account, user: user, snoozed_until: DateTime.now.utc + 1.day)
+    create(:notification, account: account, user: user, updated_at: DateTime.now.utc + 1.day)
+    create(:notification, account: account, user: user, snoozed_until: DateTime.now.utc + 3.days, updated_at: DateTime.now.utc)
+    create(:notification, account: account, user: user, updated_at: DateTime.now.utc + 2.days)
   end
 
   describe '#perform' do
@@ -28,6 +28,15 @@ describe NotificationFinder do
       it 'returns all notifications' do
         result = notification_finder.perform
         expect(result.length).to be 3
+      end
+    end
+
+    context 'when order by updated_at' do
+      let(:params) { { status: 'open' } }
+
+      it 'returns all notifications' do
+        result = notification_finder.perform
+        expect(result.first.updated_at).to be > result.last.updated_at
       end
     end
   end
