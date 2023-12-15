049d84d6a9f8539ff8db54d7c250dd0dc56ba996
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-12-15 09:39:35 +0530

    chore: review comments

--- app/jobs/notification/reopen_snoozed_notifications_job.rb
+++ app/jobs/notification/reopen_snoozed_notifications_job.rb
@@ -2,8 +2,10 @@ class Notification::ReopenSnoozedNotificationsJob < ApplicationJob
   queue_as :low
 
   def perform
-    Notification.where.not(snoozed_until: nil).where(snoozed_until: 3.days.ago..Time.current).find_each(batch_size: 100) do |notification|
-      notification.update(snoozed_until: nil)
-    end
+    # rubocop:disable Rails/SkipsModelValidations
+    Notification.where.not(snoozed_until: nil)
+                .where(snoozed_until: 3.days.ago..Time.current)
+                .update_all(snoozed_until: nil, updated_at: Time.current)
+    # rubocop:enable Rails/SkipsModelValidations
   end
 end
