f177623500cf8d00a25b1bb2feb0c9e906b08d19
Sojan <sojan@pepalo.com>
2023-12-18 14:51:30 -0800

    chore: Billing Changes

--- app/jobs/internal/check_new_versions_job.rb
+++ app/jobs/internal/check_new_versions_job.rb
@@ -2,13 +2,14 @@ class Internal::CheckNewVersionsJob < ApplicationJob
   queue_as :scheduled_jobs
 
   def perform
-    return unless Rails.env.production?
+    # return unless Rails.env.production?
 
     instance_info = ChatwootHub.sync_with_hub
     return unless instance_info
 
     ::Redis::Alfred.set(::Redis::Alfred::LATEST_CHATWOOT_VERSION, instance_info['version'])
     update_installation_config(key: 'INSTALLATION_PRICING_PLAN', value: instance_info['plan'])
+    update_installation_config(key: 'INSTALLATION_PRICING_PLAN_QUANTITY', value: instance_info['plan_quantity'])
     update_installation_config(key: 'CHATWOOT_SUPPORT_WEBSITE_TOKEN', value: instance_info['chatwoot_support_website_token'])
     update_installation_config(key: 'CHATWOOT_SUPPORT_IDENTIFIER_HASH', value: instance_info['chatwoot_support_identifier_hash'])
     update_installation_config(key: 'CHATWOOT_SUPPORT_SCRIPT_URL', value: instance_info['chatwoot_support_script_url'])
