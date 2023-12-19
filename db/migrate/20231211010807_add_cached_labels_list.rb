2399f3fac8bb404b63df7aa382e07c273462b824
Pranav Raj S <pranav@chatwoot.com>
2023-12-18 16:21:41 -0800

    chore(migration): Re-run database migration job for caching.
    A support request that came to Chatwoot Cloud revealed that the job was timed
    prematurely. The default timeout for Sidekiq queues was set as 25 minutes in
    sidekiq.yml file. The cache was not created properly for the accounts with more
    than 100k conversations.
    
    This change removes the cache logic in the previous migration and creates a new
    migration with a new job which processes conversations in batch.

--- db/migrate/20231211010807_add_cached_labels_list.rb
+++ db/migrate/20231211010807_add_cached_labels_list.rb
@@ -3,17 +3,5 @@ class AddCachedLabelsList < ActiveRecord::Migration[7.0]
     add_column :conversations, :cached_label_list, :string
     Conversation.reset_column_information
     ActsAsTaggableOn::Taggable::Cache.included(Conversation)
-
-    update_exisiting_conversations
-  end
-
-  private
-
-  def update_exisiting_conversations
-    ::Account.find_in_batches do |account_batch|
-      account_batch.each do |account|
-        Migration::ConversationCacheLabelJob.perform_later(account)
-      end
-    end
   end
 end
