714404708860af744fdb1142aa345088589e8a5a
Muhsin Keloth <muhsinkeramam@gmail.com>
2023-12-20 11:27:24 +0530

    chore: Ensure that the value last_activity_at doesn't get saved as nil.

--- db/schema.rb
+++ db/schema.rb
@@ -731,7 +731,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_19_073832) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "snoozed_until"
-    t.datetime "last_activity_at"
+    t.datetime "last_activity_at", default: -> { "CURRENT_TIMESTAMP" }
     t.index ["account_id"], name: "index_notifications_on_account_id"
     t.index ["last_activity_at"], name: "index_notifications_on_last_activity_at"
     t.index ["primary_actor_type", "primary_actor_id"], name: "uniq_primary_actor_per_account_notifications"
