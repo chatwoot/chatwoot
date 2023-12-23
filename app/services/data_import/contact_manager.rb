94914b9e6a4f8b9af1719cd167d4998e8ead51b9
Alexander Shakhov <shakhov@vesama.com>
2023-12-23 11:27:30 +0200

    Find contact by id

--- app/services/data_import/contact_manager.rb
+++ app/services/data_import/contact_manager.rb
@@ -11,19 +11,26 @@ class DataImport::ContactManager
 
   def find_or_initialize_contact(params)
     contact = find_existing_contact(params)
-    contact_params = params.slice(:email, :identifier, :phone_number)
+    contact_params = params.slice(:id, :email, :identifier, :phone_number)
     contact_params[:phone_number] = format_phone_number(contact_params[:phone_number]) if contact_params[:phone_number].present?
     contact ||= @account.contacts.new(contact_params)
     contact
   end
 
   def find_existing_contact(params)
-    contact = find_contact_by_identifier(params)
+    contact = find_contact_by_id(params)
+    contact ||= find_contact_by_identifier(params)
     contact ||= find_contact_by_email(params)
     contact ||= find_contact_by_phone_number(params)
 
     update_contact_with_merged_attributes(params, contact) if contact.present? && contact.valid?
     contact
+  end
+
+  def find_contact_by_id(params)
+    return unless params[:id]
+
+    @account.contacts.find_by(id: params[:id])
   end
 
   def find_contact_by_identifier(params)
@@ -63,6 +70,6 @@ class DataImport::ContactManager
     contact.additional_attributes ||= {}
     contact.additional_attributes[:company] = params[:company] if params[:company].present?
     contact.additional_attributes[:city] = params[:city] if params[:city].present?
-    contact.assign_attributes(custom_attributes: contact.custom_attributes.merge(params.except(:identifier, :email, :name, :phone_number)))
+    contact.assign_attributes(custom_attributes: contact.custom_attributes.merge(params.except(:id, :identifier, :email, :name, :phone_number)))
   end
 end
