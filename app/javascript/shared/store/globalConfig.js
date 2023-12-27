efd0c1f0700ef2f0f8dbfb0018d0479786efd098
Parsaa404 <121176600+Parsaa404@users.noreply.github.com>
2023-12-27 20:03:34 +0800

    develop

--- app/javascript/shared/store/globalConfig.js
+++ app/javascript/shared/store/globalConfig.js
@@ -49,8 +49,8 @@ const state = {
 export const getters = {
   get: $state => $state,
   isOnChatwootCloud: $state => $state.deploymentEnv === 'cloud',
-  isACustomBrandedInstance: $state => $state.installationName !== 'Chatwoot',
-  isAChatwootInstance: $state => $state.installationName === 'Chatwoot',
+  isACustomBrandedInstance: $state => $state.installationName !== 'xping',
+  isAChatwootInstance: $state => $state.installationName === 'xping',
 };
 
 export const actions = {};
