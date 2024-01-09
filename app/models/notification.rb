cc83a5493096fbedf8d577565ec9436184822519
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-01-09 12:34:27 +0530

    chore: review comments

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -118,26 +118,22 @@ class Notification < ApplicationRecord
   private
 
   def process_notification_delivery
-    Notification::PushNotificationJob.perform_later(self) if user_subscribed_to_notification?
+    Notification::PushNotificationJob.perform_later(self) if user_subscribed_to_notification?('push')
 
     # Should we do something about the case where user subscribed to both push and email ?
     # In future, we could probably add condition here to enqueue the job for 30 seconds later
     # when push enabled and then check in email job whether notification has been read already.
-    Notification::EmailNotificationJob.perform_later(self) if user_subscribed_to_notification?
+    Notification::EmailNotificationJob.perform_later(self) if user_subscribed_to_notification?('email')
 
-    # Remove duplicate notifications
     Notification::RemoveDuplicateNotificationJob.perform_later(self)
   end
 
-  def user_subscribed_to_notification?
+  def user_subscribed_to_notification?(delivery_type)
     notification_setting = user.notification_settings.find_by(account_id: account.id)
-    # added for the case where an assignee might be removed from the account but remains in conversation
-    return if notification_setting.blank?
+    return false if notification_setting.blank?
 
-    return true if notification_setting.public_send("email_#{notification_type}?")
-    return true if notification_setting.public_send("push_#{notification_type}?")
-
-    false
+    # Check if the user has subscribed to the specified type of notification
+    notification_setting.public_send("#{delivery_type}_#{notification_type}?")
   end
 
   def dispatch_create_event
