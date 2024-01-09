4ede1754eb58aef7dd92dea8aed6fe929319ce03
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2024-01-09 11:16:06 -0800

    Delete lib/telegram_markdown_renderer.rb

--- lib/telegram_markdown_renderer.rb
+++
@@ -1,9 +0,0 @@
-class TelegramMarkdownRenderer < CommonMarker::HtmlRenderer
-  def emph(_node)
-    out('<em>', :children, '</em>')
-  end
-
-  def strong(_node)
-    out('<strong>', :children, '</strong>')
-  end
-end
