487532eabbb8e0c2837951ce3558345575051223
Sojan <sojan@pepalo.com>
2024-01-18 18:52:01 +0400

    chore: remove rubocop disable

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,8 +1,8 @@
 class ApplicationJob < ActiveJob::Base
   # https://api.rubyonrails.org/v5.2.1/classes/ActiveJob/Exceptions/ClassMethods.html
   discard_on ActiveJob::DeserializationError do |job, error|
-    # rubocop:disable Layout/LineLength
-    Rails.logger.info("Skipping #{job.class} with #{job.instance_variable_get(:@serialized_arguments)} because of ActiveJob::DeserializationError (#{error.message})")
-    # rubocop:enable Layout/LineLength
+    Rails.logger.info("Skipping #{job.class} with #{
+      job.instance_variable_get(:@serialized_arguments)
+    } because of ActiveJob::DeserializationError (#{error.message})")
   end
 end
