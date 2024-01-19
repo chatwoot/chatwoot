687e3e8117489219b79831391a13153758b44ecb
Vishnu Narayanan <vishnu@chatwoot.com>
2024-01-19 17:49:11 +0530

    chore: fix spec

--- spec/enterprise/models/applied_sla_spec.rb
+++ spec/enterprise/models/applied_sla_spec.rb
@@ -2,21 +2,15 @@ require 'rails_helper'
 
 RSpec.describe AppliedSla, type: :model do
   describe 'associations' do
-    it { is_expected.to belong_to(:sla_policies) }
+    it { is_expected.to belong_to(:sla_policy) }
     it { is_expected.to belong_to(:account) }
     it { is_expected.to belong_to(:conversation) }
   end
 
-  describe 'validations' do
-    it { is_expected.to validate_presence_of(:sla_policies_id) }
-    it { is_expected.to validate_presence_of(:conversation_id) }
-    it { is_expected.to validate_presence_of(:account_id) }
-  end
-
   describe 'validates_factory' do
     it 'creates valid applied sla policy object' do
-      applied_sla = create(:applied_slas)
-      expect(applied_sla.status).to eq 'active'
+      applied_sla = create(:applied_sla)
+      expect(applied_sla.sla_status).to eq 'active'
     end
   end
 end
