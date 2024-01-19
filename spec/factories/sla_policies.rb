21c413ade82918f96cc5c6e3dde933e85510bda9
Vishnu Narayanan <vishnu@chatwoot.com>
2024-01-19 17:42:12 +0530

    chore: fix sla_policy tests

--- spec/factories/sla_policies.rb
+++ spec/factories/sla_policies.rb
@@ -2,8 +2,9 @@ FactoryBot.define do
   factory :sla_policy do
     account
     name { 'sla_1' }
-    rt_threshold { 1000 }
-    frt_threshold { 2000 }
+    first_response_time { 2000 }
+    next_response_time { 1000 }
+    resolution_time { 3000 }
     only_during_business_hours { false }
   end
 end
