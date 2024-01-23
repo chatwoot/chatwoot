7979babf700f46bebd3c250e024c6fdf6aebda80
Vishnu Narayanan <vishnu@chatwoot.com>
2024-01-23 22:54:05 +0530

    chore: refactor models

--- db/migrate/20231223031803_add_description_to_sla_policy.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToSlaPolicy < ActiveRecord::Migration[7.0]
-  def change
-    add_column :sla_policies, :description, :string
-  end
-end
--- db/migrate/20231223033730_add_resolution_time_to_sla_policy.rb
+++
@@ -1,5 +0,0 @@
-class AddResolutionTimeToSlaPolicy < ActiveRecord::Migration[7.0]
-  def change
-    add_column :sla_policies, :resolution_time_threshold, :float
-  end
-end
