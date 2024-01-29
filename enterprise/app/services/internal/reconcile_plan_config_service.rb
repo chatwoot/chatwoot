f50bcebd1b4cae369192ba59549c351bd6e6768b
Sojan <sojan@pepalo.com>
2024-01-29 16:51:38 +0400

    chore: Reconcile Premium config and features

--- enterprise/app/services/internal/reconcile_plan_config_service.rb
+++ enterprise/app/services/internal/reconcile_plan_config_service.rb
@@ -1,3 +1,40 @@
 class Internal::ReconcilePlanConfigService
-  def perform; end
+  def perform
+    return if ChatwootHub.pricing_plan != 'community'
+
+    reconcile_premium_config
+    reconcile_premium_features
+  end
+
+  private
+
+  def config_path
+    @config_path ||= Rails.root.join('enterprise/config')
+  end
+
+  def premium_config
+    @premium_config ||= YAML.safe_load(File.read("#{config_path}/premium_installation_config.yml")).freeze
+  end
+
+  def reconcile_premium_config
+    premium_config.each do |config|
+      new_config = config.with_indifferent_access
+      existing_config = InstallationConfig.find_by(name: new_config[:name])
+      next if existing_config&.value == new_config[:value]
+
+      existing_config&.update!(value: new_config[:value])
+    end
+  end
+
+  def premium_features
+    @premium_features ||= YAML.safe_load(File.read("#{config_path}/premium_features.yml")).freeze
+  end
+
+  def reconcile_premium_features
+    Account.find_in_batches do |accounts|
+      accounts.each do |account|
+        account.disable_features!(*premium_features)
+      end
+    end
+  end
 end
