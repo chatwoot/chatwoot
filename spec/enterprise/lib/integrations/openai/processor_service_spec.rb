0302e63ad1118752c064d22273ec453ace38b91c
Sojan Jose <sojan@pepalo.com>
2024-01-29 20:09:17 +0400

    chore: Move EE OpenAI spec to correct folder (#8805)
    - We previously had this spec inside the enterprise folder which wouldn't be picked up by our build process, so moving to the correct folder instead.
    
    Co-authored-by: Shivam Mishra <scm.mymail@gmail.com>

--- spec/enterprise/lib/integrations/openai/processor_service_spec.rb
+++ spec/enterprise/lib/integrations/openai/processor_service_spec.rb
@@ -44,6 +44,8 @@ RSpec.describe Integrations::Openai::ProcessorService do
 
         create(:label, account: account)
         create(:label, account: account)
+
+        hook.settings['label_suggestion'] = 'true'
       end
 
       it 'returns the label suggestions' do
@@ -76,6 +78,27 @@ RSpec.describe Integrations::Openai::ProcessorService do
 
     context 'when event name is not one that can be processed' do
       let(:event) { { 'name' => 'unknown', 'data' => {} } }
+
+      it 'returns nil' do
+        expect(subject.perform).to be_nil
+      end
+    end
+
+    context 'when hook is not enabled' do
+      let(:event) { { 'name' => 'label_suggestion', 'data' => { 'conversation_display_id' => conversation.display_id } } }
+
+      before do
+        create(:message, account: account, conversation: conversation, message_type: :incoming, content: 'hello agent')
+        create(:message, account: account, conversation: conversation, message_type: :outgoing, content: 'hello customer')
+        create(:message, account: account, conversation: conversation, message_type: :incoming, content: 'hello agent 2')
+        create(:message, account: account, conversation: conversation, message_type: :incoming, content: 'hello agent 3')
+        create(:message, account: account, conversation: conversation, message_type: :incoming, content: 'hello agent 4')
+
+        create(:label, account: account)
+        create(:label, account: account)
+
+        hook.settings['label_suggestion'] = nil
+      end
 
       it 'returns nil' do
         expect(subject.perform).to be_nil
