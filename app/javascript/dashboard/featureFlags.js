6a493b574e51e1b763468402d88d19a8cfd80ce7
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-01-30 13:55:20 +0530

    feat: Add inbox view page (#8814)
    * feat: Add inbox view page
    
    * Update accounts.js
    
    * Update index.js

--- app/javascript/dashboard/featureFlags.js
+++ app/javascript/dashboard/featureFlags.js
@@ -17,4 +17,5 @@ export const FEATURE_FLAGS = {
   VOICE_RECORDER: 'voice_recorder',
   AUDIT_LOGS: 'audit_logs',
   INSERT_ARTICLE_IN_REPLY: 'insert_article_in_reply',
+  INBOX_VIEW: 'inbox_view',
 };
--- app/javascript/dashboard/store/modules/specs/account/getters.spec.js
+++ app/javascript/dashboard/store/modules/specs/account/getters.spec.js
@@ -4,6 +4,10 @@ const accountData = {
   id: 1,
   name: 'Company one',
   locale: 'en',
+  features: {
+    auto_resolve_conversations: false,
+    agent_management: false,
+  },
 };
 
 describe('#getters', () => {
@@ -28,5 +32,33 @@ describe('#getters', () => {
       isUpdating: false,
       isDeleting: false,
     });
+  });
+
+  it('isFeatureEnabledonAccount', () => {
+    const state = {
+      records: [accountData],
+    };
+    const rootGetters = {
+      getCurrentUser: {
+        type: 'SuperAdmin',
+      },
+    };
+    expect(
+      getters.isFeatureEnabledonAccount(
+        state,
+        null,
+        null,
+        rootGetters
+      )(1, 'auto_resolve_conversations')
+    ).toEqual(true);
+  });
+
+  it('isFeatureEnabledGlobally', () => {
+    const state = {
+      records: [accountData],
+    };
+    expect(
+      getters.isFeatureEnabledGlobally(state)(1, 'auto_resolve_conversations')
+    ).toEqual(false);
   });
 });
