707ad650892447bd43b87ae84efa682cf45249bd
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-01-31 13:09:59 +0530

    chore: code cleanup

--- app/javascript/dashboard/routes/dashboard/inbox/components/InboxItemHeader.vue
+++ app/javascript/dashboard/routes/dashboard/inbox/components/InboxItemHeader.vue
@@ -1,23 +1,24 @@
-<script setup>
+<script>
 import PaginationButton from './PaginationButton.vue';
 
-const props = defineProps({
-  totalLength: {
-    type: Number,
-    default: 0,
+export default {
+  components: {
+    PaginationButton,
   },
-  currentIndex: {
-    type: Number,
-    default: 0,
+  props: {
+    totalLength: {
+      type: Number,
+      default: 0,
+    },
+    currentIndex: {
+      type: Number,
+      default: 0,
+    },
   },
-});
-
-const onSnooze = () => {
-  // TODO: Implement snooze
-};
-
-const onDelete = () => {
-  // TODO: Implement delete
+  methods: {
+    onSnooze() {},
+    onDelete() {},
+  },
 };
 </script>
 
@@ -26,8 +27,8 @@ const onDelete = () => {
     class="flex gap-2 py-2 pl-4 h-14 pr-2 justify-between items-center w-full border-b border-slate-50 dark:border-slate-800/50"
   >
     <pagination-button
-      :total-length="props.totalLength"
-      :current-index="props.currentIndex"
+      :total-length="totalLength"
+      :current-index="currentIndex"
     />
     <div class="flex items-center gap-2">
       <woot-button
