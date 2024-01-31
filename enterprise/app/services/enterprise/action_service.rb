5a35116cb1c277943cf7468d09b114cc68e5d47d
Vishnu Narayanan <vishnu@chatwoot.com>
2024-01-31 18:36:48 +0530

    feat: create applied_sla table entry

--- enterprise/app/services/enterprise/action_service.rb
+++ enterprise/app/services/enterprise/action_service.rb
@@ -1,5 +1,15 @@
 module Enterprise::ActionService
   def add_sla(sla_policy)
     @conversation.update!(sla_policy_id: sla_policy.id)
+    create_applied_sla
+  end
+
+  def create_applied_sla
+    AppliedSla.create!(
+      account: @conversation.account,
+      sla_policy: sla_policy,
+      conversation: @conversation,
+      sla_status: 'active'
+    )
   end
 end
