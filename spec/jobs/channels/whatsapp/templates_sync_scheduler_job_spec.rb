971874f67466071edca75fac85ee34ee8073e9f1
Sojan Jose <sojan@pepalo.com>
2024-01-31 18:38:01 +0400

    fix: Template sync issue for older Whatsapp channels (#8830)
    - This PR introduces a modification to the channel fetching logic, ensuring that channels with older message_template_last_updated timestamps are prioritized during synchronization.

--- spec/jobs/channels/whatsapp/templates_sync_scheduler_job_spec.rb
+++ spec/jobs/channels/whatsapp/templates_sync_scheduler_job_spec.rb
@@ -23,5 +23,23 @@ RSpec.describe Channels::Whatsapp::TemplatesSyncSchedulerJob do
         have_been_enqueued.with(non_synced).on_queue('low')
       )
     end
+
+    it 'schedules templates_sync_job for oldest synced channels first' do
+      stub_const('Limits::BULK_EXTERNAL_HTTP_CALLS_LIMIT', 2)
+      stub_request(:post, 'https://waba.360dialog.io/v1/configs/webhook')
+      non_synced = create(:channel_whatsapp, sync_templates: false, message_templates_last_updated: nil)
+      synced_recently = create(:channel_whatsapp, sync_templates: false, message_templates_last_updated: 4.hours.ago)
+      synced_old = create(:channel_whatsapp, sync_templates: false, message_templates_last_updated: 6.hours.ago)
+      described_class.perform_now
+      expect(Channels::Whatsapp::TemplatesSyncJob).not_to(
+        have_been_enqueued.with(synced_recently).on_queue('low')
+      )
+      expect(Channels::Whatsapp::TemplatesSyncJob).to(
+        have_been_enqueued.with(synced_old).on_queue('low')
+      )
+      expect(Channels::Whatsapp::TemplatesSyncJob).to(
+        have_been_enqueued.with(non_synced).on_queue('low')
+      )
+    end
   end
 end
