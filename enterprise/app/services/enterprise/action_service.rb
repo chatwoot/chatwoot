01cf3c5921052e8f90c2d7297daa89a70493213c
Vishnu Narayanan <vishnu@chatwoot.com>
2024-01-31 19:00:25 +0530

    chore: add applied_sla spec

--- enterprise/app/services/enterprise/action_service.rb
+++ enterprise/app/services/enterprise/action_service.rb
@@ -1,14 +1,14 @@
 module Enterprise::ActionService
   def add_sla(sla_policy)
     @conversation.update!(sla_policy_id: sla_policy.id)
-    create_applied_sla
+    create_applied_sla(sla_policy)
   end
 
-  def create_applied_sla
+  def create_applied_sla(sla_policy)
     AppliedSla.create!(
-      account: @conversation.account,
-      sla_policy: sla_policy,
-      conversation: @conversation,
+      account_id: @conversation.account_id,
+      sla_policy_id: sla_policy.id,
+      conversation_id: @conversation.id,
       sla_status: 'active'
     )
   end
