1ff1a624b6111d564dbc6fae08f8952209b79756
Pranav Raj S <pranav@chatwoot.com>
2024-02-01 19:36:49 -0800

    reduce the code

--- app/services/imap/microsoft_fetch_email_service.rb
+++ app/services/imap/microsoft_fetch_email_service.rb
@@ -12,17 +12,6 @@ class Imap::MicrosoftFetchEmailService < Imap::BaseFetchEmailService
   end
 
   def imap_password
-    return if channel.provider_config['access_token'].blank?
-
-    access_token = valid_access_token channel
-
-    return unless access_token
-
-    access_token
-  end
-
-  # Refresh the token before the emails are fetched
-  def valid_access_token(channel)
     Microsoft::RefreshOauthTokenService.new(channel: channel).access_token
   end
 end
