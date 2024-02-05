5f2dd1f080fc2801f8c82da1b465f1561b798ff4
iamsivin <iamsivin@gmail.com>
2024-02-05 21:26:50 +0530

    chore: Minor fix

--- app/javascript/dashboard/routes/dashboard/inbox/components/InboxItemHeader.vue
+++ app/javascript/dashboard/routes/dashboard/inbox/components/InboxItemHeader.vue
@@ -3,11 +3,13 @@
     class="flex gap-2 py-2 pl-4 h-14 pr-2 justify-between items-center w-full border-b border-slate-50 dark:border-slate-800/50"
   >
     <pagination-button
+      v-if="totalLength > 1"
       :total-length="totalLength"
       :current-index="currentIndex"
       @next="onClickNext"
       @prev="onClickPrev"
     />
+    <div v-else />
     <div class="flex items-center gap-2">
       <woot-button
         variant="hollow"
