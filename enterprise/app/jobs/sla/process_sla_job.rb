7ca2951cbdb272020504c7da10cb7811f5b5c98c
Vishnu Narayanan <vishnu@chatwoot.com>
2024-02-06 23:13:01 +0530

    chore: refactor

--- enterprise/app/jobs/sla/process_sla_job.rb
+++ enterprise/app/jobs/sla/process_sla_job.rb
@@ -2,6 +2,6 @@ class Sla::ProcessSlaJob < ApplicationJob
   queue_as :high
 
   def perform(applied_sla)
-    Sla::EvaluateSlaService.new(applied_sla: applied_sla).perform
+    Sla::EvaluateSlaService.new(applied_sla: applied_sla).perform_later
   end
 end
