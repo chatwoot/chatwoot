9e0468cd737265a4bd1ab84dd36a6b9a7d3a7ca4
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2024-02-06 08:54:15 +0530

    feat: Inbox header actions (Snooze/Delete) (#8858)
    * feat: Inbox header actions (Snooze/Delete)
    
    * chore: Minor fix
    
    * chore: Fix eslint
    
    * Update inboxHotKeys.js
    
    * feat: custom snooze
    
    * Update actions.spec.js
    
    * chore: Clean up
    
    * chore: add snoozed_until to notification end point
    
    * chore: Minor fix
    
    * chore: Minor style fix
    
    * chore:Clean up
    
    * chore: review fixes
    
    * chore: Minor fix
    
    * chore: Adds alert
    
    ---------
    
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>

--- app/javascript/dashboard/store/modules/notifications/actions.js
+++ app/javascript/dashboard/store/modules/notifications/actions.js
@@ -119,6 +119,27 @@ export const actions = {
       commit(types.SET_NOTIFICATIONS_UI_FLAG, { isDeleting: false });
     }
   },
+
+  snooze: async ({ commit }, { id, snoozedUntil }) => {
+    commit(types.SET_NOTIFICATIONS_UI_FLAG, { isUpdating: true });
+    try {
+      const response = await NotificationsAPI.snooze({
+        id,
+        snoozedUntil,
+      });
+      const {
+        data: { snoozed_until = null },
+      } = response;
+      commit(types.SNOOZE_NOTIFICATION, {
+        id,
+        snoozed_until,
+      });
+      commit(types.SET_NOTIFICATIONS_UI_FLAG, { isUpdating: false });
+    } catch (error) {
+      commit(types.SET_NOTIFICATIONS_UI_FLAG, { isUpdating: false });
+    }
+  },
+
   addNotification({ commit }, data) {
     commit(types.ADD_NOTIFICATION, data);
   },
