0c8a9120e86deb39446f9a9923cf08aee5bc71ea
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-02-07 10:42:38 +0530

    feat: Add `notification.updated` event

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -46,6 +46,7 @@ class Notification < ApplicationRecord
   before_create :set_last_activity_at
   after_create_commit :process_notification_delivery, :dispatch_create_event
   after_destroy_commit :dispatch_destroy_event
+  after_update_commit :dispatch_update_event
 
   PRIMARY_ACTORS = ['Conversation'].freeze
 
@@ -140,6 +141,10 @@ class Notification < ApplicationRecord
 
   def dispatch_create_event
     Rails.configuration.dispatcher.dispatch(NOTIFICATION_CREATED, Time.zone.now, notification: self)
+  end
+
+  def dispatch_update_event
+    Rails.configuration.dispatcher.dispatch(NOTIFICATION_UPDATED, Time.zone.now, notification: self)
   end
 
   def dispatch_destroy_event
