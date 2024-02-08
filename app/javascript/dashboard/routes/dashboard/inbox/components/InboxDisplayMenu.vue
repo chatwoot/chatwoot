a57c183fab9e4f1078d6f77d16dff889a56a10ad
iamsivin <iamsivin@gmail.com>
2024-02-08 11:48:55 +0530

    chore: Minor fix

--- app/javascript/dashboard/routes/dashboard/inbox/components/InboxDisplayMenu.vue
+++ app/javascript/dashboard/routes/dashboard/inbox/components/InboxDisplayMenu.vue
@@ -115,7 +115,7 @@ export default {
         {
           name: this.$t('INBOX.DISPLAY_MENU.DISPLAY_OPTIONS.READ'),
           key: wootConstants.INBOX_DISPLAY_BY.READ,
-          selected: true,
+          selected: false,
           type: wootConstants.INBOX_FILTER_TYPE.TYPE,
         },
       ],
