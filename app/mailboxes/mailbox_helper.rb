f45ddfb212786043789f3b194cb1d7643c5cf347
Shivam Mishra <scm.mymail@gmail.com>
2024-02-12 17:40:50 +0530

    chore: add more logging to mailbox helpers

--- app/mailboxes/mailbox_helper.rb
+++ app/mailboxes/mailbox_helper.rb
@@ -2,6 +2,7 @@ module MailboxHelper
   private
 
   def create_message
+    Rails.logger.info "[MailboxHelper] Creating message #{processed_mail.message_id}"
     return if @conversation.messages.find_by(source_id: processed_mail.message_id).present?
 
     @message = @conversation.messages.create!(
@@ -23,7 +24,6 @@ module MailboxHelper
   def add_attachments_to_message
     return if @message.blank?
 
-    # ensure we don't add more than the permitted number of attachments
     all_attachments = processed_mail.attachments.last(Message::NUMBER_OF_PERMITTED_ATTACHMENTS)
 
     inline_attachments = all_attachments.select { |attachment| attachment[:original].inline? }
@@ -36,6 +36,7 @@ module MailboxHelper
   end
 
   def process_regular_attachments(attachments)
+    Rails.logger.info "[MailboxHelper] Processing regular attachments for message with ID: #{@message.id}"
     attachments.each do |mail_attachment|
       attachment = @message.attachments.new(
         account_id: @conversation.account_id,
@@ -46,8 +47,7 @@ module MailboxHelper
   end
 
   def process_inline_attachments(attachments)
-    # create an instance variable here, the `embed_inline_image_source`
-    # updates them directly. And then the value is eventaully used to update the message content
+    Rails.logger.info "[MailboxHelper] Processing inline attachments for message with ID: #{@message.id}"
     @html_content = processed_mail.serialized_data[:html_content][:full]
     @text_content = processed_mail.serialized_data[:text_content][:reply]
 
@@ -55,35 +55,8 @@ module MailboxHelper
       embed_inline_image_source(mail_attachment)
     end
 
-    # update the message content with the updated html and text content
     @message.content_attributes[:email][:html_content][:full] = @html_content
     @message.content_attributes[:email][:text_content][:full] = @text_content
-  end
-
-  def embed_inline_image_source(mail_attachment)
-    if @html_content.present?
-      upload_inline_image(mail_attachment)
-    elsif @text_content.present?
-      embed_plain_text_email_with_inline_image(mail_attachment)
-    end
-  end
-
-  def upload_inline_image(mail_attachment)
-    content_id = mail_attachment[:original].cid
-
-    @html_content = @html_content.gsub("cid:#{content_id}", inline_image_url(mail_attachment[:blob]).to_s)
-  end
-
-  def embed_plain_text_email_with_inline_image(mail_attachment)
-    attachment_name = mail_attachment[:original].filename
-
-    @text_content = @text_content.gsub(
-      "[image: #{attachment_name}]", "<img src=\"#{inline_image_url(mail_attachment[:blob])}\" alt=\"#{attachment_name}\">"
-    )
-  end
-
-  def inline_image_url(blob)
-    Rails.application.routes.url_helpers.url_for(blob)
   end
 
   def create_contact
@@ -99,18 +72,6 @@ module MailboxHelper
       }
     ).perform
     @contact = @contact_inbox.contact
-  end
-
-  def notification_email_from_chatwoot?
-    # notification emails are send via mailer sender email address. so it should match
-    @processed_mail.original_sender == Mail::Address.new(ENV.fetch('MAILER_SENDER_EMAIL', 'Chatwoot <accounts@chatwoot.com>')).address
-  end
-
-  def mail_content
-    if processed_mail.text_content.present?
-      processed_mail.text_content[:reply]
-    elsif processed_mail.html_content.present?
-      processed_mail.html_content[:reply]
-    end
+    Rails.logger.info "[MailboxHelper] Contact created with ID: #{@contact.id} for inbox with ID: #{@inbox.id}"
   end
 end
