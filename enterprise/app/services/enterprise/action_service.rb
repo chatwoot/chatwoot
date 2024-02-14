2e0177a8cbfe63fbed279b08dcd3b35957849f27
Vishnu Narayanan <vishnu@chatwoot.com>
2024-02-15 00:47:10 +0530

    feat: refactor fetching sla in action service

--- enterprise/app/services/enterprise/action_service.rb
+++ enterprise/app/services/enterprise/action_service.rb
@@ -1,10 +1,13 @@
 module Enterprise::ActionService
-  def add_sla(sla_policy)
+  def add_sla(sla_policy_id)
+    sla_policy = @account.sla_policies.find(sla_policy_id.first)
+    Rails.logger.info "SLA:: Adding SLA #{sla_policy.id} to conversation: #{@conversation.id}"
     @conversation.update!(sla_policy_id: sla_policy.id)
     create_applied_sla(sla_policy)
   end
 
   def create_applied_sla(sla_policy)
+    Rails.logger.info "SLA:: Creating Applied SLA for conversation: #{@conversation.id}"
     AppliedSla.create!(
       account_id: @conversation.account_id,
       sla_policy_id: sla_policy.id,
