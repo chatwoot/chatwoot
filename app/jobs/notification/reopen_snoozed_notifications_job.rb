d32b75ffd3d235850fbe30e10e7eb8bf68af5db6
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-02-14 15:47:16 +0530

    feat: add last snoozed at

--- app/jobs/notification/reopen_snoozed_notifications_job.rb
+++ app/jobs/notification/reopen_snoozed_notifications_job.rb
@@ -2,9 +2,17 @@ class Notification::ReopenSnoozedNotificationsJob < ApplicationJob
   queue_as :low
 
   def perform
-    # rubocop:disable Rails/SkipsModelValidations
-    Notification.where(snoozed_until: 3.days.ago..Time.current)
-                .update_all(snoozed_until: nil, updated_at: Time.current, last_activity_at: Time.current)
-    # rubocop:enable Rails/SkipsModelValidations
+    Notification.where(snoozed_until: 3.days.ago..Time.current).find_each do |notification|
+      # Ensure the meta field is a hash and merge the last_snoozed_at key
+      updated_meta = (notification.meta || {}).merge('last_snoozed_at' => notification.snoozed_until)
+
+      # Update the notification with the new values
+      notification.update(
+        snoozed_until: nil,
+        updated_at: Time.current,
+        last_activity_at: Time.current,
+        meta: updated_meta
+      )
+    end
   end
 end
