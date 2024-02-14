894d7f209a195f46c673701bbdd76e5f9e3eb054
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2024-02-14 11:08:01 -0800

    Update accounts_controller.rb

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -45,7 +45,8 @@ class Api::V1::AccountsController < Api::BaseController
 
   def update
     @account.assign_attributes(account_params.slice(:name, :locale, :domain, :support_email, :auto_resolve_duration))
-    @account.custom_attributes['onboarding_step'] = 'invite_team' if custom_attributes_params[:onboarding_step].present?
+    @account.custom_attributes.merge!(custom_attributes_params)
+    @account.custom_attributes['onboarding_step'] = 'invite_team' if @account.custom_attributes['onboarding_step'] == 'account_update'
     @account.save!
   end
 
@@ -86,7 +87,7 @@ class Api::V1::AccountsController < Api::BaseController
   end
 
   def custom_attributes_params
-    params.permit(:industry, :company_size, :timezone, :onboarding_step)
+    params.permit(:industry, :company_size, :timezone)
   end
 
   def check_signup_enabled
