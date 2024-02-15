9d82ebbb4365085c514d7c8812890849b1c0bd0c
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2024-02-14 21:26:41 -0800

    Update OnboardingHelper.js

--- app/javascript/v3/helpers/OnboardingHelper.js
+++ app/javascript/v3/helpers/OnboardingHelper.js
@@ -6,10 +6,12 @@ export const findMatchingOption = (value, options, defaultValue) => {
 };
 
 export const findCompanySizeMatch = (options, size) => {
+  if (!size) return undefined;
+  if (Number.isNaN(size)) return undefined;
   return (
     options.find(option => {
       const upperLimit = option.value.split('-')[1]?.split('+')[0];
       return size < (upperLimit ? Number(upperLimit) : Infinity);
-    })?.value || this.companySizeOptions[0].value
+    })?.value || options[0].value
   );
 };
