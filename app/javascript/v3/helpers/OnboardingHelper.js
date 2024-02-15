85e39e9c57e5b786bd156cfe93818b183fa19f31
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2024-02-14 23:11:38 -0800

    Update app/javascript/v3/helpers/OnboardingHelper.js
    Co-authored-by: Sivin Varghese <64252451+iamsivin@users.noreply.github.com>

--- app/javascript/v3/helpers/OnboardingHelper.js
+++ app/javascript/v3/helpers/OnboardingHelper.js
@@ -6,8 +6,7 @@ export const findMatchingOption = (value, options, defaultValue) => {
 };
 
 export const findCompanySizeMatch = (options, size) => {
-  if (!size) return undefined;
-  if (Number.isNaN(size)) return undefined;
+  if (!size || Number.isNaN(size)) return undefined;
   return (
     options.find(option => {
       const upperLimit = option.value.split('-')[1]?.split('+')[0];
