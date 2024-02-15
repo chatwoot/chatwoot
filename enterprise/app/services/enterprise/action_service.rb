31387632c3d786f50a3948c4d352284227e0816c
Vishnu Narayanan <vishnu@chatwoot.com>
2024-02-15 15:35:00 +0530

    chore: review fixes

--- enterprise/app/services/enterprise/action_service.rb
+++ enterprise/app/services/enterprise/action_service.rb
@@ -1,14 +1,14 @@
 module Enterprise::ActionService
   def add_sla(sla_policy_id)
     sla_policy = @account.sla_policies.find(sla_policy_id.first)
+    return if @conversation.sla_policy.present?
+
     Rails.logger.info "SLA:: Adding SLA #{sla_policy.id} to conversation: #{@conversation.id}"
     @conversation.update!(sla_policy_id: sla_policy.id)
     create_applied_sla(sla_policy)
   end
 
   def create_applied_sla(sla_policy)
-    return if AppliedSla.exists?(conversation_id: @conversation.id, account_id: @conversation.account_id, sla_policy_id: sla_policy.id)
-
     Rails.logger.info "SLA:: Creating Applied SLA for conversation: #{@conversation.id}"
     AppliedSla.create!(
       account_id: @conversation.account_id,
