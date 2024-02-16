a729cefc389961c0c69c0c05b676ad8ba41a6371
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-02-16 11:33:03 +0530

    chore: remove description

--- db/migrate/20240215065844_add_meta_and_description_to_notifications.rb
+++
@@ -1,6 +0,0 @@
-class AddMetaAndDescriptionToNotifications < ActiveRecord::Migration[7.0]
-  def change
-    add_column :notifications, :meta, :jsonb, default: {}
-    add_column :notifications, :description, :text, default: ''
-  end
-end
