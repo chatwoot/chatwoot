4d496ffb21c73626f0d0c61a1ce751df34c0ca82
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2024-02-16 20:55:41 +0530

    Updates helper

--- app/javascript/v3/helpers/BrowserHelper.js
+++ app/javascript/v3/helpers/BrowserHelper.js
@@ -1,3 +1,5 @@
+import timeZoneData from 'dashboard/routes/dashboard/settings/inbox/helpers/timezones.json';
+
 export const getBrowserLocale = enabledLanguages => {
   const [language, region] = navigator.language.split('-');
   const fullLocale = `${language}_${region || ''}`;
@@ -9,4 +11,21 @@ export const getBrowserLocale = enabledLanguages => {
 
 export const getBrowserTimezone = () => {
   return Intl.DateTimeFormat().resolvedOptions().timeZone;
+};
+
+export const getIANATimezoneFromOffset = () => {
+  const offsetMinutes = new Date().getTimezoneOffset();
+  const sign = offsetMinutes > 0 ? '-' : '+';
+  const hours = Math.floor(Math.abs(offsetMinutes) / 60)
+    .toString()
+    .padStart(2, '0');
+  const minutes = Math.abs(offsetMinutes % 60)
+    .toString()
+    .padStart(2, '0');
+  const gmtOffset = `GMT${sign}${hours}:${minutes}`;
+
+  const match =
+    Object.keys(timeZoneData).find(key => key.includes(gmtOffset)) || 'UTC';
+  // Loop through the keys and return the first one that matches the GMT offset
+  return timeZoneData[match];
 };
