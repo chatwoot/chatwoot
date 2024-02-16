b92a0565ee2609d16d0286f95573dfbd987587ae
Vishnu Narayanan <vishnu@chatwoot.com>
2024-02-16 12:00:11 +0530

    chore: add new specs to improve coverage

--- enterprise/app/services/enterprise/action_service.rb
+++ enterprise/app/services/enterprise/action_service.rb
@@ -2,7 +2,7 @@ module Enterprise::ActionService
   def add_sla(sla_policy_id)
     return if sla_policy_id.blank?
 
-    sla_policy = @account.sla_policies.find(sla_policy_id.first)
+    sla_policy = @account.sla_policies.find_by(id: sla_policy_id.first)
     return if sla_policy.nil?
     return if @conversation.sla_policy.present?
 
