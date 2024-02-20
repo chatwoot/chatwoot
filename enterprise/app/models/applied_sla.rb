5e1bc414a7583162e2bde79028b5fe554cc51ae7
Vishnu Narayanan <iamwishnu@gmail.com>
2024-02-20 21:59:49 +0530

    feat: sla-7 ensure applied_sla uniqueness (#8938)
    * feat: refactor fetching sla in action service
    
    * chore: modify spec
    
    * chore: ensure applied_sla uniqueness
    
    * chore: review fixes
    
    * feat: add unique index on applied_sla
    
    * chore: fix spec
    
    * chore: add new specs to improve coverage
    
    * chore: improve spec
    
    * Update spec/enterprise/services/enterprise/action_service_spec.rb
    
    ---------
    
    Co-authored-by: Sojan Jose <sojan@pepalo.com>

--- enterprise/app/models/applied_sla.rb
+++ enterprise/app/models/applied_sla.rb
@@ -12,14 +12,17 @@
 #
 # Indexes
 #
-#  index_applied_slas_on_account_id       (account_id)
-#  index_applied_slas_on_conversation_id  (conversation_id)
-#  index_applied_slas_on_sla_policy_id    (sla_policy_id)
+#  index_applied_slas_on_account_id                       (account_id)
+#  index_applied_slas_on_account_sla_policy_conversation  (account_id,sla_policy_id,conversation_id) UNIQUE
+#  index_applied_slas_on_conversation_id                  (conversation_id)
+#  index_applied_slas_on_sla_policy_id                    (sla_policy_id)
 #
 class AppliedSla < ApplicationRecord
   belongs_to :account
   belongs_to :sla_policy
   belongs_to :conversation
+
+  validates :account_id, uniqueness: { scope: %i[sla_policy_id conversation_id] }
 
   enum sla_status: { active: 0, hit: 1, missed: 2 }
 end
