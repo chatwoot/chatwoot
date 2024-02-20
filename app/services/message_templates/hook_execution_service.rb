f4117ab4e0eda3a246afca573a706c4cacc25c0a
John <jmanuel.derecho@gmail.com>
2024-02-20 02:26:16 +0000

    ticket replied toggle (staging)

--- app/services/message_templates/hook_execution_service.rb
+++ app/services/message_templates/hook_execution_service.rb
@@ -56,7 +56,7 @@ class MessageTemplates::HookExecutionService
 
   def csat_enabled_conversation?
     return false unless conversation.resolved? || ((message.outgoing? || message.input_csat?) && inbox.send_csat_on_all_reply?)
-    puts 'RAILS_NISUD_DRE'
+
     # should not sent since the link will be public
     return false if conversation.tweet?
     return false unless inbox.csat_survey_enabled?
--- lib/digitaltolk/toggle_ticket_replied.rb
+++ lib/digitaltolk/toggle_ticket_replied.rb
@@ -1,28 +1,6 @@
 class Digitaltolk::ToggleTicketReplied
-  attr_accessor :status
-
-  def initialize(status, conversation_id)
-    @status = status
-  end
-
-  def perform
-    return false # temporary disable
-
-    post_status
-  end
-
-  private
-
-  def post_status
-    url = '' # dt url
-    headers = {
-      # 'Authorization' => "Bearer #{ENV.fetch('OPENAI_API_KEY', '')}",
-      # 'Content-Type' => 'application/json'
-    }
-    data = {
-      is_ticket_replied: status
-    }
-
-    response = Net::HTTP.post(URI(url), data.to_json, headers)
-  end
+  INFOTEST = 32
+  TOLKSUPPORT = 39
+  BOKNING = 45
+  ALLOWED_INBOXES = [BOKNING, TOLKSUPPORT, INFOTEST]
 end
