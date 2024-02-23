52788645f86581ed63193bc0ebc8129c09c7ab88
Ricardo Andrade <r64bits@gmail.com>
2024-02-23 18:18:07 -0300

    log send facebook

--- app/services/facebook/send_on_facebook_service.rb
+++ app/services/facebook/send_on_facebook_service.rb
@@ -18,11 +18,11 @@ class Facebook::SendOnFacebookService < Base::SendOnChannelService
 
   def send_message_to_facebook(delivery_params)
     Rails.logger.info "Facebook::SendOnFacebookService: Page - #{channel.page_id}"
-    Rails.logger.info "params messagetype : #{delivery_params.messaging_type}"
     # Rails.logger.info "params recipient : #{delivery_params.recipient.id}"
 
     result = Facebook::Messenger::Bot.deliver(delivery_params, page_id: channel.page_id)
     parsed_result = JSON.parse(result)
+    Rails.logger.info "Facebook::RESPONSE: Page - #{JSON.stringify(result)}"
     if parsed_result['error'].present?
       message.update!(status: :failed, external_error: external_error(parsed_result))
       Rails.logger.info "Facebook::SendOnFacebookService: Error sending message to Facebook : Page - #{channel.page_id} : #{result}"
