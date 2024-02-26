f8eccf5aff7501145de7935f6b8b38e13cff2d5b
Pranav <pranav@chatwoot.com>
2024-02-25 20:31:17 -0800

    Update the UI

--- app/javascript/packs/superadmin_pages.js
+++ app/javascript/packs/superadmin_pages.js
@@ -1,1 +1,23 @@
 import 'chart.js';
+import Vue from 'vue';
+const PlaygroundIndex = () =>
+  import('../superadmin_pages/views/playground/Index.vue');
+
+const ComponentMapping = {
+  PlaygroundIndex: PlaygroundIndex,
+};
+
+const renderComponent = (componentName, props) => {
+  Vue.component(componentName, ComponentMapping[componentName]);
+  new Vue({
+    data: { props: props },
+    template: `<${componentName} :component-data="props"/>`,
+  }).$mount('#app');
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const element = document.getElementById('app');
+  const componentName = element.dataset.componentName;
+  const props = JSON.parse(element.dataset.props);
+  renderComponent(componentName, props);
+});
---
+++ app/javascript/superadmin_pages/components/playground/UserMessage.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="w-full mb-4 flex items-center justify-end">
+    <div
+      class="px-4 py-3 bg-woot-400 text-white text-sm rounded-md inline-block"
+    >
+      {{ message }}
+    </div>
+  </div>
+</template>
