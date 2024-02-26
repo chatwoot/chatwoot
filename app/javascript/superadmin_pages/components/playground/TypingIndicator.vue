a1e24ae1364410f6953158fe3ca0c9f5d6dd3a40
Sojan Jose <sojan@pepalo.com>
2024-02-26 20:20:12 +0530

    feat: Interface to validate response_source (#8894)
    - This PR adds a UI to validate the response source quality quickly. It also helps to test with sample questions and update responses in the database when missing.
    
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>

---
+++ app/javascript/superadmin_pages/components/playground/TypingIndicator.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TypingIndicator from './assets/typing.gif';
+</script>
+
+<template>
+  <div class="w-full mb-4 flex items-center justify-start">
+    <div
+      class="px-2 py-2 bg-white max-w-4xl text-slate-700 leading-6 text-sm rounded-md inline-block border border-slate-100"
+    >
+      <img :src="TypingIndicator" alt="TypingIndicator" class="h-4" />
+    </div>
+  </div>
+</template>
