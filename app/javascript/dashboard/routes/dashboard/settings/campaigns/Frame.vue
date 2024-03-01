0bde4350675eb3547afeba731f34c9e14587940c
João Jocowski <joao.jocowski@brainitsolutions.com.br>
2024-03-01 11:04:29 -0300

    Add App Integration to Campaigns

---
+++ app/javascript/dashboard/routes/dashboard/settings/campaigns/Frame.vue
@@ -0,0 +1,123 @@
+<template>
+  <div v-if="hasOpenedAtleastOnce" class="dashboard-app--container">
+    <div
+      v-for="(configItem, index) in config"
+      :key="index"
+      class="dashboard-app--list"
+    >
+      <loading-state
+        v-if="iframeLoading"
+        :message="$t('DASHBOARD_APPS.LOADING_MESSAGE')"
+        class="dashboard-app_loading-container"
+      />
+      <iframe
+        v-if="configItem.type === 'frame' && configItem.url"
+        :id="getFrameId(index)"
+        :src="configItem.url"
+        @load="() => onIframeLoad(index)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import LoadingState from 'dashboard/components/widgets/LoadingState.vue';
+export default {
+  components: {
+    LoadingState,
+  },
+  props: {
+    config: {
+      type: Array,
+      default: () => [],
+    },
+    currentChat: {
+      type: Object,
+      default: () => ({}),
+    },
+    isVisible: {
+      type: Boolean,
+      default: false,
+    },
+    position: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      hasOpenedAtleastOnce: false,
+      iframeLoading: true,
+    };
+  },
+  computed: {
+    dashboardAppContext() {
+      return {
+        conversation: this.currentChat,
+        contact: this.$store.getters['contacts/getContact'](this.contactId),
+        currentAgent: this.currentAgent,
+      };
+    },
+    contactId() {
+      return this.currentChat?.meta?.sender?.id;
+    },
+    currentAgent() {
+      const { id, name, email } = this.$store.getters.getCurrentUser;
+      return { id, name, email };
+    },
+  },
+  watch: {
+    isVisible() {
+      if (this.isVisible) {
+        this.hasOpenedAtleastOnce = true;
+      }
+    },
+  },
+
+  mounted() {
+    window.onmessage = e => {
+      if (
+        typeof e.data !== 'string' ||
+        e.data !== 'chatwoot-dashboard-app:fetch-info'
+      ) {
+        return;
+      }
+      this.onIframeLoad(0);
+    };
+  },
+  methods: {
+    getFrameId(index) {
+      return `dashboard-app--frame-${this.position}-${index}`;
+    },
+    onIframeLoad(index) {
+      // A possible alternative is to use ref instead of document.getElementById
+      // However, when ref is used together with v-for, the ref you get will be
+      // an array containing the child components mirroring the data source.
+      const frameElement = document.getElementById(this.getFrameId(index));
+      const eventData = { event: 'appContext', data: this.dashboardAppContext };
+      frameElement.contentWindow.postMessage(JSON.stringify(eventData), '*');
+      this.iframeLoading = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.dashboard-app--container,
+.dashboard-app--list,
+.dashboard-app--list iframe {
+  height: 100%;
+  width: 100%;
+}
+
+.dashboard-app--list iframe {
+  border: 0;
+}
+.dashboard-app_loading-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+}
+</style>
