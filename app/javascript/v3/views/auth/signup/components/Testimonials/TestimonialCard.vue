3c919f43764ae3e77606a2080c61da35044c72d0
Shivam Mishra <scm.mymail@gmail.com>
2024-03-13 16:09:29 +0530

    refactor: use setup syntax

--- app/javascript/v3/views/auth/signup/components/Testimonials/TestimonialCard.vue
+++ app/javascript/v3/views/auth/signup/components/Testimonials/TestimonialCard.vue
@@ -1,48 +1,50 @@
+<script setup>
+defineProps({
+  reviewContent: {
+    type: String,
+    default: '',
+  },
+  authorImage: {
+    type: String,
+    default: '',
+  },
+  authorName: {
+    type: String,
+    default: '',
+  },
+  authorDesignation: {
+    type: String,
+    default: '',
+  },
+  isLeftAligned: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
 <template>
   <div
-    class="flex items-end justify-center w-80 gap-3"
+    class="flex items-end justify-center gap-3 w-80"
     :class="{ 'flex-row-reverse ml-14': !isLeftAligned }"
   >
     <div class="bg-white rounded-full shrink-0">
-      <img :src="authorImage" class="h-12 w-12 rounded-full" />
+      <img
+        :src="authorImage"
+        class="w-12 h-12 border rounded-full shadow-md dark:border-none border-slate-200 dark:shadow-none"
+      />
     </div>
     <div class="text-woot-900 dark:text-woot-100">
       <div
-        class="w-[360px] shrink-0 flex flex-col mt-4 dark:bg-[#0F1720] bg-[##FBFDFF] p-4 rounded-2xl border border-solid border-woot-100 dark:border-woot-800"
+        class="w-[360px] shrink-0 flex flex-col mt-4 dark:bg-[#0F1720] bg-[#FBFDFF] p-4 rounded-2xl border border-solid border-woot-100 dark:border-woot-800"
       >
-        <p class="text-sm tracking-wide leading-6">
+        <p class="text-sm leading-6 tracking-wide">
           {{ reviewContent }}
         </p>
       </div>
-      <div class="mt-2 px-1" :class="{ 'text-right': !isLeftAligned }">
+      <div class="px-1 mt-2" :class="{ 'text-right': !isLeftAligned }">
         <div class="text-sm">{{ authorName }}, {{ authorDesignation }}</div>
       </div>
     </div>
   </div>
 </template>
-<script>
-export default {
-  props: {
-    reviewContent: {
-      type: String,
-      default: '',
-    },
-    authorImage: {
-      type: String,
-      default: '',
-    },
-    authorName: {
-      type: String,
-      default: '',
-    },
-    authorDesignation: {
-      type: String,
-      default: '',
-    },
-    isLeftAligned: {
-      type: Boolean,
-      default: false,
-    },
-  },
-};
-</script>
