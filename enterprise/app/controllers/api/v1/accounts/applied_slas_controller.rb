f8c7f545b149130a76afedf43e42d2e777765631
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-03-21 10:16:26 +0530

    chore: code beautification

--- enterprise/app/controllers/api/v1/accounts/applied_slas_controller.rb
+++ enterprise/app/controllers/api/v1/accounts/applied_slas_controller.rb
@@ -2,24 +2,24 @@ class Api::V1::Accounts::AppliedSlasController < Api::V1::Accounts::EnterpriseAc
   include Sift
   include DateRangeHelper
 
-  before_action :set_sla_responses, only: [:metrics]
+  before_action :set_appplied_slas, only: [:metrics]
   before_action :check_admin_authorization?
 
   def metrics
-    @total_applied_slas = @sla_responses.count
-    @number_of_sla_breaches = @sla_responses.missed.count
+    @total_applied_slas = @applied_slas.count
+    @number_of_sla_breaches = @applied_slas.missed.count
   end
 
   private
 
-  def set_sla_responses
-    @sla_responses = initial_query
-                     .filter_by_date_range(range)
-                     .filter_by_inbox_id(params[:inbox_id])
-                     .filter_by_team_id(params[:team_id])
-                     .filter_by_sla_policy_id(params[:sla_policy_id])
-                     .filter_by_label_list(params[:label_list])
-                     .filter_by_assigned_agent_id(params[:assigned_agent_id])
+  def set_appplied_slas
+    @applied_slas = initial_query
+                    .filter_by_date_range(range)
+                    .filter_by_inbox_id(params[:inbox_id])
+                    .filter_by_team_id(params[:team_id])
+                    .filter_by_sla_policy_id(params[:sla_policy_id])
+                    .filter_by_label_list(params[:label_list])
+                    .filter_by_assigned_agent_id(params[:assigned_agent_id])
   end
 
   def initial_query
