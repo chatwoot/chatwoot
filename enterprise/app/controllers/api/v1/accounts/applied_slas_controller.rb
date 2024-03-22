5c4154dc66f24d14749f74da2a16971bd7877a3c
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-03-22 14:28:27 +0530

    feat: Add API to list of the conversation that breached and SLA in the given time period (#9142)
    * feat: Add index route
    
    * fix: typo
    
    ---------
    
    Co-authored-by: Shivam Mishra <scm.mymail@gmail.com>

--- enterprise/app/controllers/api/v1/accounts/applied_slas_controller.rb
+++ enterprise/app/controllers/api/v1/accounts/applied_slas_controller.rb
@@ -2,8 +2,16 @@ class Api::V1::Accounts::AppliedSlasController < Api::V1::Accounts::EnterpriseAc
   include Sift
   include DateRangeHelper
 
-  before_action :set_appplied_slas, only: [:metrics]
+  RESULTS_PER_PAGE = 25
+
+  before_action :set_applied_slas, only: [:index, :metrics]
+  before_action :set_current_page, only: [:index]
+  before_action :set_current_page_appiled_slas, only: [:index]
   before_action :check_admin_authorization?
+
+  sort_on :created_at, type: :datetime
+
+  def index; end
 
   def metrics
     @total_applied_slas = total_applied_slas
@@ -29,7 +37,7 @@ class Api::V1::Accounts::AppliedSlasController < Api::V1::Accounts::EnterpriseAc
     ((total_applied_slas - number_of_sla_breaches) / total_applied_slas.to_f * 100).round(2)
   end
 
-  def set_appplied_slas
+  def set_applied_slas
     @applied_slas = initial_query
                     .filter_by_date_range(range)
                     .filter_by_inbox_id(params[:inbox_id])
@@ -41,5 +49,13 @@ class Api::V1::Accounts::AppliedSlasController < Api::V1::Accounts::EnterpriseAc
 
   def initial_query
     Current.account.applied_slas.includes(:conversation)
+  end
+
+  def set_current_page_appiled_slas
+    @applied_slas = @applied_slas.page(@current_page).per(RESULTS_PER_PAGE)
+  end
+
+  def set_current_page
+    @current_page = params[:page] || 1
   end
 end
