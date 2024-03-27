2fedec076d735176bd29bbe00209002209944726
Shivam Mishra <scm.mymail@gmail.com>
2024-03-27 11:04:07 +0530

    test: fix method names

--- spec/models/automation_rule_spec.rb
+++ spec/models/automation_rule_spec.rb
@@ -57,12 +57,12 @@ RSpec.describe AutomationRule do
     end
   end
 
-  describe '#disable_and_notify' do
+  describe '#disable_rule_and_notify' do
     let(:account) { create(:account) }
     let(:automation_rule) { create(:automation_rule, account: account) }
 
     it 'disables the automation rule' do
-      automation_rule.disable_and_notify
+      automation_rule.disable_rule_and_notify
       expect(automation_rule.reload.active).to be false
     end
 
@@ -73,7 +73,7 @@ RSpec.describe AutomationRule do
       allow(mailer).to receive(:automation_rule_disabled).and_return(mailer_action)
       allow(mailer_action).to receive(:deliver_later)
 
-      automation_rule.disable_and_notify
+      automation_rule.disable_rule_and_notify
 
       expect(mailer).to have_received(:automation_rule_disabled).with(automation_rule)
       expect(mailer_action).to have_received(:deliver_later)
@@ -86,20 +86,20 @@ RSpec.describe AutomationRule do
 
     it 'increments the error count' do
       automation_rule.invalid_condition_error!
-      expect(automation_rule.send(:condition_error_counts)).to eq(1)
+      expect(automation_rule.send(:authorization_error_count)).to eq(1)
     end
 
     it 'disables the automation rule if error count exceeds threshold' do
-      allow(automation_rule).to receive(:condition_error_counts).and_return(3)
+      allow(automation_rule).to receive(:authorization_error_count).and_return(3)
       automation_rule.invalid_condition_error!
       expect(automation_rule.active).to be false
     end
 
     it 'resets the error count if the rule is updated' do
       automation_rule.invalid_condition_error!
-      expect(automation_rule.send(:condition_error_counts)).to eq(1)
+      expect(automation_rule.send(:authorization_error_count)).to eq(1)
       automation_rule.update(name: 'New Name')
-      expect(automation_rule.send(:condition_error_counts)).to eq(0)
+      expect(automation_rule.send(:authorization_error_count)).to eq(0)
     end
   end
 end
