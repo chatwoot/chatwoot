8bc7ebb890e88a91646e563e56fbfb39a4b23ba1
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-03-27 11:46:34 +0530

    feat: Add SLA reports page

---
+++ app/javascript/dashboard/routes/dashboard/settings/reports/SLAReports.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex-1 overflow-auto p-4">
+    <h1>SLA Reports</h1>
+  </div>
+</template>
+<script>
+import { defineComponent, ref, watch, watchEffect } from 'vue';
+export default {
+  name: 'SLAReports',
+};
+</script>
