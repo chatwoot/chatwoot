af87f7c027f202514811ab43eb96ab325a5b6a9a
Sojan Jose <sojan@pepalo.com>
2024-03-28 19:12:28 +0530

    Update process_account_applied_slas_job_spec.rb
    clean up

--- spec/enterprise/jobs/sla/process_account_applied_slas_job_spec.rb
+++ spec/enterprise/jobs/sla/process_account_applied_slas_job_spec.rb
@@ -9,27 +9,21 @@ RSpec.describe Sla::ProcessAccountAppliedSlasJob do
     let!(:miss_applied_sla) { create(:applied_sla, account: account, sla_policy: sla_policy, sla_status: 'missed') }
     let!(:active_with_misses_applied_sla) { create(:applied_sla, account: account, sla_policy: sla_policy, sla_status: 'active_with_misses') }
     let!(:conversation) { create(:conversation, account: account, status: 'resolved') }
-    let!(:missed_applied_sla_with_resolved_conversation) do
-      create(:applied_sla, account: account,
-                           sla_policy: sla_policy, sla_status: 'missed', conversation: conversation)
-    end
 
     it 'enqueues the job' do
       expect { described_class.perform_later }.to have_enqueued_job(described_class)
         .on_queue('medium')
     end
 
-    it 'calls the ProcessAppliedSlaJob for both active/missed' do
-      # check if both applied_sla and miss_applied_sla are called
+    it 'calls the ProcessAppliedSlaJob for both active and active_with_misses' do
       expect(Sla::ProcessAppliedSlaJob).to receive(:perform_later).with(active_with_misses_applied_sla).and_call_original
       expect(Sla::ProcessAppliedSlaJob).to receive(:perform_later).with(applied_sla).and_call_original
       described_class.perform_now(account)
     end
 
-    it 'does not call the ProcessAppliedSlaJob for applied slas that are hit or if the conversation is resolved' do
+    it 'does not call the ProcessAppliedSlaJob for applied slas that are hit or miss' do
       expect(Sla::ProcessAppliedSlaJob).not_to receive(:perform_later).with(hit_applied_sla)
       expect(Sla::ProcessAppliedSlaJob).not_to receive(:perform_later).with(miss_applied_sla)
-      expect(Sla::ProcessAppliedSlaJob).not_to receive(:perform_later).with(missed_applied_sla_with_resolved_conversation)
       described_class.perform_now(account)
     end
   end
