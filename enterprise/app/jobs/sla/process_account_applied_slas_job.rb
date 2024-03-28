5089003264d9be29d346964e52f2ad70f164dab6
Vishnu Narayanan <iamwishnu@gmail.com>
2024-03-29 02:01:43 +1100

    feat: refactor SLA evaluation logic (#9133)
    * feat: update SLA evaluation logic
    
    * chore: handle nrt
    
    * chore: handle applied_sla status
    
    * chore: refactor spec to bring down expecations in a single block
    
    * chore: fix process_account_applied_sla spec
    
    * chore: add spec to test multiple nrt misses
    
    * feat: persist sla notifications
    
    * feat: revert persist sla notifications
    
    * chore: refactor sla_status to include active_with_misses
    
    * chore: refactor spec
    
    * Update evaluate_applied_sla_service.rb
    
    * minor refactors
    
    * clean up
    
    * move notification related spec
    
    * chore: refactor notifications spec to sla_event model
    
    ---------
    
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>
    Co-authored-by: Sojan Jose <sojan@pepalo.com>

--- enterprise/app/jobs/sla/process_account_applied_slas_job.rb
+++ enterprise/app/jobs/sla/process_account_applied_slas_job.rb
@@ -2,7 +2,7 @@ class Sla::ProcessAccountAppliedSlasJob < ApplicationJob
   queue_as :medium
 
   def perform(account)
-    account.applied_slas.where(sla_status: 'active').each do |applied_sla|
+    account.applied_slas.where(sla_status: %w[active active_with_misses]).each do |applied_sla|
       Sla::ProcessAppliedSlaJob.perform_later(applied_sla)
     end
   end
