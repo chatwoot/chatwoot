66c5ec6c037276194ce0cc4efe0cda41652a8c0b
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-03-29 18:05:22 -0300

    update status from last message

--- app/controllers/webhooks/notifica_me_controller.rb
+++ app/controllers/webhooks/notifica_me_controller.rb
@@ -1,5 +1,6 @@
 class Webhooks::NotificaMeController < ActionController::API
   def process_payload
+    Rails.logger.error(">>>>>> params.to_unsafe_hash #{params.to_unsafe_hash}")
     Webhooks::NotificaMeEventsJob.perform_later(params.to_unsafe_hash)
     head :ok
   end
--- app/jobs/webhooks/notifica_me_events_job.rb
+++ app/jobs/webhooks/notifica_me_events_job.rb
@@ -1,11 +1,45 @@
-class Webhooks::NotificaEventsJob < ApplicationJob
+class Webhooks::NotificaMeEventsJob < ApplicationJob
   queue_as :default
 
-  def perform(params = {})
-    return unless params[:channel_token]
+=begin
+  {
+    "type":"MESSAGE_STATUS",
+    "timestamp":"2024-03-29 07:55:58 pm",
+    "subscriptionId":"f44d13f2-fb66-4dc8-85fe-7973b296dd3a",
+    "channel":"telegram",
+    "messageId":"c09b633a-ceab-4bb5-854a-5ce5d27717cb",
+    "contentIndex":0,
+    "messageStatus":{
+       "timestamp":"2024-03-29 07:55:58 pm",
+       "code":"REJECTED",
+       "description":"The message was rejected by the provider"
+    },
+    "hub_access_token":"f20018fa-eb17-11ee-880c-0efa6ad28f4f.f44d13f2-fb66-4dc8-85fe-7973b296dd3a",
+    "controller":"webhooks/notifica_me",
+    "action":"process_payload",
+    "channel_id":"f44d13f2-fb66-4dc8-85fe-7973b296dd3a",
+    "notifica_me":{
+       "type":"MESSAGE_STATUS",
+       "timestamp":"2024-03-29 07:55:58 pm",
+       "subscriptionId":"f44d13f2-fb66-4dc8-85fe-7973b296dd3a",
+       "channel":"telegram",
+       "messageId":"c09b633a-ceab-4bb5-854a-5ce5d27717cb",
+       "contentIndex":0,
+       "messageStatus":{
+          "timestamp":"2024-03-29 07:55:58 pm",
+          "code":"REJECTED",
+          "description":"The message was rejected by the provider"
+       }
+    }
+  }
+=end
 
-    channel = Channel::Notification.find_by(channel_token: params[:channel_token])
-    return unless channel
+  def perform(params = {})
+    channel = Channel::NotificaMe.find_by(channel_id: params['channel_id'])
+    unless channel
+      Rails.logger.warn("NotificaMe Channel #{params['channel_id']} not found")
+      return
+    end
 
     process_event_params(channel, params)
   end
@@ -13,12 +47,16 @@ class Webhooks::NotificaEventsJob < ApplicationJob
   private
 
   def process_event_params(channel, params)
-    return unless params[:telegram]
+    if params['type'] == 'MESSAGE_STATUS'
+      message = Message.last #find_by(source_id: params['messageId'])
+      unless message
+        Rails.logger.warn("NotificaMe Message source id #{params['messageId']} not found")
+        return
+      end
 
-    # if params.dig(:telegram, :edited_message).present?
-    #   Telegram::UpdateMessageService.new(inbox: channel.inbox, params: params['telegram'].with_indifferent_access).perform
-    # else
-    #   Telegram::IncomingMessageService.new(inbox: channel.inbox, params: params['telegram'].with_indifferent_access).perform
-    # end
+      if params['messageStatus']['code'] == 'REJECTED'
+        message.update!(status: :failed, external_error: params['messageStatus']['description'])
+      end
+    end
   end
 end
