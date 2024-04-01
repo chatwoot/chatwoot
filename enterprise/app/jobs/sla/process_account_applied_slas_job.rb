76a22ee0f3cdbde7828f3aa92ee6297e4c92044d
Shivam Mishra <scm.mymail@gmail.com>
2024-04-01 10:19:14 +0530

    refactor: eager load associations

--- enterprise/app/jobs/sla/process_account_applied_slas_job.rb
+++ enterprise/app/jobs/sla/process_account_applied_slas_job.rb
@@ -2,7 +2,7 @@ class Sla::ProcessAccountAppliedSlasJob < ApplicationJob
   queue_as :medium
 
   def perform(account)
-    account.applied_slas.where(sla_status: %w[active active_with_misses]).each do |applied_sla|
+    account.applied_slas.where(sla_status: %w[active active_with_misses]).includes(:conversation, :sla_policy).each do |applied_sla|
       Sla::EvaluateAppliedSlaService.new(applied_sla: applied_sla).perform
     end
   end
