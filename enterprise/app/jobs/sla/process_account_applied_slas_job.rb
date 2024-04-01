7bcf35538fab7468336152778b20a52253539208
Shivam Mishra <scm.mymail@gmail.com>
2024-04-01 18:20:12 +0530

    feat: allow bubbling up missed slas

--- enterprise/app/jobs/sla/process_account_applied_slas_job.rb
+++ enterprise/app/jobs/sla/process_account_applied_slas_job.rb
@@ -2,8 +2,18 @@ class Sla::ProcessAccountAppliedSlasJob < ApplicationJob
   queue_as :medium
 
   def perform(account)
+    missed_slas = []
+
     account.applied_slas.where(sla_status: %w[active active_with_misses]).includes(:conversation, :sla_policy).each do |applied_sla|
       Sla::EvaluateAppliedSlaService.new(applied_sla: applied_sla).perform
+      applied_sla.reload
+      missed_slas << applied_sla if applied_sla.status == :missed || applied_sla.status == :active_with_misses
     end
+
+    notify_missed_slas(missed_slas, account)
+  end
+
+  def notify_missed_slas(missed_slas, account)
+    # notify_users = account.administrators
   end
 end
