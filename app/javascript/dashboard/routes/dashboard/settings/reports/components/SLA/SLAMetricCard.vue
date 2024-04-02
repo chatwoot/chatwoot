72bc258de47d0a080410d0070a1a5ff15df08823
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-04-02 15:01:28 +0530

    chore: redice the complexity

--- app/javascript/dashboard/routes/dashboard/settings/reports/components/SLA/SLAMetricCard.vue
+++ app/javascript/dashboard/routes/dashboard/settings/reports/components/SLA/SLAMetricCard.vue
@@ -1,35 +1,29 @@
 <template>
-  <SLA-metrics-layout :is-loading="isLoading">
-    <template #loading>
-      <div class="w-12 h-6 mb-0.5 rounded-md bg-slate-50 animate-pulse" />
-    </template>
-    <template #label>
-      <span
-        class="inline-flex items-center gap-1 text-sm text-slate-700 dark:text-slate-200 font-medium"
-      >
-        {{ label }}
-        <fluent-icon
-          v-tooltip.right="toolTip"
-          size="14"
-          icon="information"
-          type="outline"
-          class="flex-shrink-0 text-sm font-normal flex sm:font-medium text-slate-500 dark:text-slate-500"
-        />
-      </span>
-    </template>
-    <template #value>
-      <span class="text-2xl font-medium text-slate-900 dark:text-slate-25">
-        {{ value }}
-      </span>
-    </template>
-  </SLA-metrics-layout>
+  <div class="flex flex-col gap-2 items-start justify-center min-w-[10rem]">
+    <span
+      class="inline-flex items-center gap-1 text-sm text-slate-700 dark:text-slate-200 font-medium"
+    >
+      {{ label }}
+      <fluent-icon
+        v-tooltip.right="toolTip"
+        size="14"
+        icon="information"
+        type="outline"
+        class="flex-shrink-0 text-sm font-normal flex sm:font-medium text-slate-500 dark:text-slate-500"
+      />
+    </span>
+    <div
+      v-if="isLoading"
+      class="w-12 h-6 mb-0.5 rounded-md bg-slate-50 animate-pulse"
+    />
+
+    <span v-else class="text-2xl font-medium text-slate-900 dark:text-slate-25">
+      {{ value }}
+    </span>
+  </div>
 </template>
 <script>
-import SLAMetricsLayout from './SLAMetricsLayout.vue';
 export default {
-  components: {
-    SLAMetricsLayout,
-  },
   props: {
     label: {
       type: String,
