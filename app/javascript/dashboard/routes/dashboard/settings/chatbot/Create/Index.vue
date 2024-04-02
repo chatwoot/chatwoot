2f2e732f9ddac0f8370ef840ce789b1d9625347a
shivam kumar <20051252@kiit.ac.in>
2024-04-02 15:25:52 +0530

    chatbot: typing gif added

--- app/javascript/dashboard/routes/dashboard/settings/chatbot/Create/Index.vue
+++ app/javascript/dashboard/routes/dashboard/settings/chatbot/Create/Index.vue
@@ -1,19 +1,45 @@
 <template>
   <div class="wizard-body w-[75%] flex-shrink-0 flex-grow-0 max-w-[75%]">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+      rel="stylesheet"
+    />
     <back-button class="absolute top-[17px] left-[420px]" />
     <!-- sidebar icons -->
     <div class="sidebar">
-      <div class="icon" @click="showUploader('file')" :class="{ 'active': showFileUploader }">
-        <img width="30" height="30" src="https://img.icons8.com/ios/50/000000/file--v1.png" alt="file--v1"/>
+      <div
+        class="icon"
+        :class="{ active: showFileUploader }"
+        @click="showUploader('file')"
+      >
+        <img
+          width="30"
+          height="30"
+          src="https://img.icons8.com/ios/50/000000/file--v1.png"
+          alt="file--v1"
+        />
         <span>{{ $t('CHATBOT_SETTINGS.UPLOAD.FILE') }}</span>
       </div>
-      <div class="icon" @click="showText()" :class="{ 'active': showTextInput }">
-        <img width="30" height="30" src="https://img.icons8.com/ios/50/000000/edit-text-file.png" alt="edit-text-file"/>
+      <div class="icon" :class="{ active: showTextInput }" @click="showText()">
+        <img
+          width="30"
+          height="30"
+          src="https://img.icons8.com/ios/50/000000/edit-text-file.png"
+          alt="edit-text-file"
+        />
         <span>{{ $t('CHATBOT_SETTINGS.UPLOAD.TEXT') }}</span>
       </div>
-      <div class="icon" @click="showUploader('website')" :class="{ 'active': showWebsiteUploader }">
-        <img width="30" height="30" src="https://img.icons8.com/ios/50/000000/domain--v1.png" alt="domain--v1"/>
+      <div
+        class="icon"
+        :class="{ active: showWebsiteUploader }"
+        @click="showUploader('website')"
+      >
+        <img
+          width="30"
+          height="30"
+          src="https://img.icons8.com/ios/50/000000/domain--v1.png"
+          alt="domain--v1"
+        />
         <span>{{ $t('CHATBOT_SETTINGS.UPLOAD.WEBSITE') }}</span>
       </div>
     </div>
@@ -24,41 +50,65 @@
         <file-uploader />
         <div v-if="botFiles.length > 0" class="file-names-container">
           <ul class="file-names">
-            <li v-for="(file, index) in botFiles" :key="index" class="file-item">
+            <li
+              v-for="(file, index) in botFiles"
+              :key="index"
+              class="file-item"
+            >
               <input type="text" :value="file.name" readonly />
-              <span class="delete-icon" @click="deleteFile(index)"><i class="fas fa-trash-alt"></i></span>
+              <span class="delete-icon" @click="deleteFile(index)">
+                <i class="fas fa-trash-alt" />
+              </span>
             </li>
           </ul>
         </div>
       </div>
       <!-- input field component -->
-      <div class="text-input" v-if="showTextInput">
-        <textarea v-model="textInput" @input="setText" @focus="isTextInputFocused = true" @blur="isTextInputFocused = false" placeholder="Enter your text here"></textarea>
+      <div v-if="showTextInput" class="text-input">
+        <textarea
+          v-model="textInput"
+          placeholder="Enter your text here"
+          @input="setText"
+          @focus="isTextInputFocused = true"
+          @blur="isTextInputFocused = false"
+        />
       </div>
       <!-- website field component -->
       <div v-if="showWebsiteUploader" class="website-input">
-        <input type="text" placeholder="Enter website URL" v-model="websiteInput" />
+        <input
+          v-model="websiteInput"
+          type="text"
+          placeholder="Enter website URL"
+        />
         <button class="button" @click="addWebsite">
           {{ $t('CHATBOT_SETTINGS.FORM.ADD_URL') }}
         </button>
         <div class="website-urls-container">
           <ul class="website-urls">
-            <li v-for="(url, index) in websiteUrls" :key="index" class="url-item">
+            <li
+              v-for="(url, index) in websiteUrls"
+              :key="index"
+              class="url-item"
+            >
               <input type="text" :value="url" readonly />
-              <span class="delete-icon" @click="deleteUrl(index)"><i class="fas fa-trash-alt"></i></span>
+              <span class="delete-icon" @click="deleteUrl(index)">
+                <i class="fas fa-trash-alt" />
+              </span>
             </li>
           </ul>
         </div>
       </div>
     </div>
-    <router-link :to="addAccountScoping(`settings/chatbot/create/connect_inbox`)">
-        <woot-button
-          class-names="button--fixed-top"
-          :button-text="$t('CHATBOT_SETTINGS.FORM.CONNECT_INBOX')"
-        >
-          {{ $t('CHATBOT_SETTINGS.FORM.CONNECT_INBOX') }}
-        </woot-button>
-      </router-link>
+    <router-link
+      :to="addAccountScoping(`settings/chatbot/create/connect_inbox`)"
+    >
+      <woot-button
+        class-names="button--fixed-top"
+        :button-text="$t('CHATBOT_SETTINGS.FORM.CONNECT_INBOX')"
+      >
+        {{ $t('CHATBOT_SETTINGS.FORM.CONNECT_INBOX') }}
+      </woot-button>
+    </router-link>
   </div>
 </template>
 
@@ -103,7 +153,13 @@ export default {
     this.showFileUploader = true;
   },
   methods: {
-    ...mapActions('chatbot', ['addBotUrl', 'setBotText', 'addBotFiles', 'deleteBotFile', 'deleteBotUrl']),
+    ...mapActions('chatbot', [
+      'addBotUrl',
+      'setBotText',
+      'addBotFiles',
+      'deleteBotFile',
+      'deleteBotUrl',
+    ]),
     showUploader(type) {
       if (type === 'file') {
         this.showFileUploader = true;
@@ -125,8 +181,6 @@ export default {
       if (websiteUrl) {
         this.addBotUrl(websiteUrl);
         this.websiteInput = '';
-      } else {
-        console.error('Empty website URL');
       }
     },
     setText() {
@@ -144,6 +198,12 @@ export default {
 </script>
 
 <style scoped>
+/* fix chatbot UI from here ONLY */
+.content,
+.sidebar {
+  margin-top: -65%;
+}
+
 .file-item,
 .url-item {
   display: flex;
@@ -156,31 +216,36 @@ export default {
   color: red;
   margin-top: -17px;
 }
+
 .icon.active span {
   color: #488aec;
 }
+
 .file-names-container {
   margin-top: 50px;
   max-height: 330px;
-  overflow-y: auto; /* Enable vertical scrolling */
+  overflow-y: auto;
+  /* Enable vertical scrolling */
   background-color: #ffffff;
 }
 
-input{
+input {
   border: 1px solid #000000;
   background-color: #ffffff;
-
 }
+
 .website-urls-container {
   margin-top: 65px;
   max-height: 500px;
-  overflow-y: auto; /* Enable vertical scrolling */
+  overflow-y: auto;
+  /* Enable vertical scrolling */
 }
 
 .website-urls,
 .file-names {
   margin-top: 5px;
-  padding-right: 10px; /* Add some padding to prevent content from sticking to the scrollbar */
+  padding-right: 10px;
+  /* Add some padding to prevent content from sticking to the scrollbar */
 }
 
 .button {
@@ -199,22 +264,26 @@ input{
   border-radius: 0.5rem;
   user-select: none;
   gap: 0.75rem;
-  box-shadow: 0 4px 6px -1px #488aec31, 0 2px 4px -1px #488aec17;
-  transition: all .6s ease;
+  box-shadow:
+    0 4px 6px -1px #488aec31,
+    0 2px 4px -1px #488aec17;
+  transition: all 0.6s ease;
   width: auto;
   max-width: 100%;
-  margin: 0 auto; 
+  margin: 0 auto;
   position: fixed;
   z-index: 999;
 }
 
 .button:hover {
-  box-shadow: 0 10px 15px -3px #488aec4f, 0 4px 6px -2px #488aec17;
+  box-shadow:
+    0 10px 15px -3px #488aec4f,
+    0 4px 6px -2px #488aec17;
 }
 
 .button:focus,
 .button:active {
-  opacity: .85;
+  opacity: 0.85;
   box-shadow: none;
 }
 
@@ -267,12 +336,17 @@ input{
 .text-input textarea {
   width: 100%;
   height: 300px;
-  overflow: auto; /* Enable scrolling with automatic scrollbar display */
-  scrollbar-width: none; /* Hide scrollbar for Firefox */
-  -ms-overflow-style: none; /* Hide scrollbar for Internet Explorer and Edge */
+  overflow: auto;
+  /* Enable scrolling with automatic scrollbar display */
+  scrollbar-width: none;
+  /* Hide scrollbar for Firefox */
+  -ms-overflow-style: none;
+  /* Hide scrollbar for Internet Explorer and Edge */
 }
+
 .text-input textarea::-webkit-scrollbar {
-  display: none; /* Hide scrollbar for Webkit browsers (Chrome, Safari) */
+  display: none;
+  /* Hide scrollbar for Webkit browsers (Chrome, Safari) */
 }
 
 .uploader input,
@@ -281,6 +355,7 @@ input{
   height: 40px;
   padding: 0.5rem;
 }
+
 /* Hide scrollbar for webkit browsers (Chrome, Safari, etc.) */
 .file-names-container::-webkit-scrollbar,
 .website-urls-container::-webkit-scrollbar {
