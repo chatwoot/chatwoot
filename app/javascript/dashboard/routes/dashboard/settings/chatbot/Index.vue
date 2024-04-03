a95945af3a83d488c9fa4eb4cd80c5bc535456cc
shivam kumar <20051252@kiit.ac.in>
2024-04-03 11:47:23 +0530

    Revert "chatbot: typing gif added"
    This reverts commit 56718eda614b0b8817c80c07a404fa66da094981.

--- app/javascript/dashboard/routes/dashboard/settings/chatbot/Index.vue
+++ app/javascript/dashboard/routes/dashboard/settings/chatbot/Index.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="flex-1 overflow-auto p-4">
-    <!-- <div class="flex-grow flex-shrink min-w-0 p-6 overflow-auto"> -->
-    <router-link :to="addAccountScoping(`settings/chatbot/create`)">
-      <woot-button
-        class-names="button--fixed-top"
-        color-scheme="success"
-        icon="add-circle"
-        :button-text="$t('CHATBOT_SETTINGS.NEW_BOT')"
-      >
-        {{ $t('CHATBOT_SETTINGS.NEW_BOT') }}
-      </woot-button>
-    </router-link>
-    <!-- </div> -->
+    <div class="flex-grow flex-shrink min-w-0 p-6 overflow-auto">
+      <router-link :to="addAccountScoping(`settings/chatbot/create`)">
+        <woot-button
+          class-names="button--fixed-top"
+          color-scheme="success"
+          icon="add-circle"
+          :button-text="$t('CHATBOT_SETTINGS.NEW_BOT')"
+        >
+          {{ $t('CHATBOT_SETTINGS.NEW_BOT') }}
+        </woot-button>
+      </router-link>
+    </div>
     <div class="flex flex-row gap-4">
       <div class="w-[60%]">
         <p
@@ -19,10 +19,7 @@
           class="flex h-full items-center flex-col justify-center"
         >
           {{ $t('CHATBOT_SETTINGS.LIST.404') }}
-          <router-link
-            v-if="isAdmin"
-            :to="addAccountScoping('settings/chatbot/create')"
-          >
+          <router-link v-if="isAdmin" :to="addAccountScoping('settings/chatbot/create')">
             {{ $t('CHATBOT_SETTINGS.NEW_BOT') }}
           </router-link>
         </p>
@@ -32,16 +29,8 @@
             <tr v-for="chatbot in chatbotsList" :key="chatbot.chatbot_id">
               <td>
                 <span class="agent-name">
-                  <template
-                    v-if="
-                      chatbot.chatbot_name === null ||
-                      chatbot.chatbot_name === ''
-                    "
-                  >
-                    <span class="chatbot-id">
-                      {{ $t('CHATBOT_SETTINGS.CHATBOT') }} -
-                      {{ chatbot.chatbot_id.slice(-4) }}
-                    </span>
+                  <template v-if="chatbot.chatbot_name === null || chatbot.chatbot_name === ''">
+                    <span class="chatbot-id">{{ $t('CHATBOT_SETTINGS.CHATBOT') }} - {{ chatbot.chatbot_id.slice(-4) }}</span>
                   </template>
                   <template v-else>
                     {{ chatbot.chatbot_name }}
@@ -52,11 +41,7 @@
               <td>
                 <div class="button-wrapper">
                   <router-link
-                    :to="
-                      addAccountScoping(
-                        `settings/chatbot/edit/${chatbot.chatbot_id}@${chatbot.last_trained_at}`
-                      )
-                    "
+                    :to = "addAccountScoping(`settings/chatbot/edit/${chatbot.chatbot_id}@${chatbot.last_trained_at}`)"
                   >
                     <woot-button
                       v-if="isAdmin"
@@ -104,9 +89,7 @@
       :message="$t('CHATBOT_SETTINGS.DELETE.CONFIRM.MESSAGE')"
       :confirm-text="deleteConfirmText"
       :reject-text="deleteRejectText"
-      :confirm-value="
-        selectedChatbot.chatbot_name || selectedChatbot.chatbot_id
-      "
+      :confirm-value="selectedChatbot.chatbot_name || selectedChatbot.chatbot_id"
       :confirm-place-holder-text="confirmPlaceHolderText"
       @on-confirm="confirmDeletion"
       @on-close="closeDeleteModal"
@@ -136,29 +119,17 @@ export default {
       globalConfig: 'globalConfig/get',
       currentAccountId: 'getCurrentAccountId',
     }),
-    formattedLastTrainedAt() {
-      return lastTrainedAt => {
+    formattedLastTrainedAt(lastTrainedAt) {
+      return (lastTrainedAt) => {
         if (lastTrainedAt) {
           const lastTrainedAtUTC = new Date(lastTrainedAt);
-          const lastTrainedAtLocal = new Date(
-            lastTrainedAtUTC.getTime() +
-              lastTrainedAtUTC.getTimezoneOffset() * 60000
-          );
+          const lastTrainedAtLocal = new Date(lastTrainedAtUTC.getTime() + (lastTrainedAtUTC.getTimezoneOffset() * 60000));
           const year = lastTrainedAtLocal.getFullYear();
-          const month = String(lastTrainedAtLocal.getMonth() + 1).padStart(
-            2,
-            '0'
-          );
-          const day = String(lastTrainedAtLocal.getDate()).padStart(2, '0');
-          const hours = String(lastTrainedAtLocal.getHours()).padStart(2, '0');
-          const minutes = String(lastTrainedAtLocal.getMinutes()).padStart(
-            2,
-            '0'
-          );
-          const seconds = String(lastTrainedAtLocal.getSeconds()).padStart(
-            2,
-            '0'
-          );
+          const month = String(lastTrainedAtLocal.getMonth() + 1).padStart(2, "0");
+          const day = String(lastTrainedAtLocal.getDate()).padStart(2, "0");
+          const hours = String(lastTrainedAtLocal.getHours()).padStart(2, "0");
+          const minutes = String(lastTrainedAtLocal.getMinutes()).padStart(2, "0");
+          const seconds = String(lastTrainedAtLocal.getSeconds()).padStart(2, "0");
           return `${year}/${month}/${day}, ${hours}:${minutes}:${seconds}`;
         }
         return lastTrainedAt;
@@ -174,21 +145,17 @@ export default {
     },
     confirmDeleteTitle() {
       return this.$t('CHATBOT_SETTINGS.DELETE.CONFIRM.TITLE', {
-        name:
-          this.selectedChatbot.chatbot_name || this.selectedChatbot.chatbot_id,
+        name: this.selectedChatbot.chatbot_name || this.selectedChatbot.chatbot_id,
       });
     },
     confirmPlaceHolderText() {
       return `${this.$t('CHATBOT_SETTINGS.DELETE.CONFIRM.PLACE_HOLDER', {
-        name:
-          this.selectedChatbot.chatbot_name || this.selectedChatbot.chatbot_id,
+        name: this.selectedChatbot.chatbot_name || this.selectedChatbot.chatbot_id,
       })}`;
     },
   },
   async created() {
-    const res = await ChatbotAPI.fetchChatbotsWithAccountId(
-      this.currentAccountId
-    );
+    const res = await ChatbotAPI.fetchChatbotsWithAccountId(this.currentAccountId);
     this.chatbotsList = res.data;
   },
   methods: {
