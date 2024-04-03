42034819bae35fd5f1c008c7c627cd2a0dd8eeb6
shivam kumar <20051252@kiit.ac.in>
2024-04-03 11:47:23 +0530

    Revert "chatbot: typing gif added"
    This reverts commit 2f2e732f9ddac0f8370ef840ce789b1d9625347a.

--- app/controllers/api/v1/widget/messages_controller.rb
+++ app/controllers/api/v1/widget/messages_controller.rb
@@ -17,23 +17,18 @@ class Api::V1::Widget::MessagesController < Api::V1::Widget::BaseController
     conversation_id = conversation_id.to_s
     client_message = @message[:content]
     @message.save!
-    trigger_chatbot_typing_status(conversation_id, true) if conversation.present?
     bot_res = HTTP.post(
       ENV.fetch('MICROSERVICE_URL', nil) + '/prompt',
       form: { chatbot_id: chatbot_ID, user_message: client_message },
       headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
     )
     return unless ChatbotHelper::CONVERSATION_ID_TO_BOT_STATUS_MAPPING[conversation_id] != false
-
     response_body = bot_res.body.to_s
     response_hash = JSON.parse(response_body)
     bot_message = response_hash['message']
     ChatbotHelper::CONVERSATION_ID_TO_BOT_STATUS_MAPPING[conversation_id] = true
     email_collect = MessageTemplates::Template::EmailCollect.new(conversation: @conversation)
     email_collect.chatbot(bot_message)
-    return unless conversation.present?
-
-    trigger_chatbot_typing_status(conversation_id, false)
   end
 
   def update
@@ -52,21 +47,6 @@ class Api::V1::Widget::MessagesController < Api::V1::Widget::BaseController
   end
 
   private
-
-  def trigger_chatbot_typing_status(conversation_id, typing_status)
-    # Fetch the conversation object based on the conversation_id
-    @conversation = Conversation.find(conversation_id)
-    # Get the current_user object (or an appropriate user object)
-    @current_user = @conversation.assignee
-
-    typing_status_manager = ::Conversations::TypingStatusManager.new(
-      @conversation,
-      @current_user,
-      typing_status: typing_status ? 'on' : 'off',
-      is_private: false
-    )
-    typing_status_manager.toggle_typing_status
-  end
 
   def build_attachment
     return if params[:message][:attachments].blank?
