dcc26def81b01ed7e6668d362eab70bdd4059832
Jaideep Guntupalli <guntupallijaideep@gmail.com>
2024-04-03 21:07:29 +0530

    fix: bug in Instagram message builder

--- app/builders/messages/instagram/message_builder.rb
+++ app/builders/messages/instagram/message_builder.rb
@@ -86,7 +86,7 @@ class Messages::Instagram::MessageBuilder < Messages::Messenger::MessageBuilder
   end
 
   def find_or_build_for_multiple_conversations
-    last_conversation = instagram_direct_message_conversation..where.not(status: :resolved).order(created_at: :desc).first
+    last_conversation = instagram_direct_message_conversation.where.not(status: :resolved).order(created_at: :desc).first
 
     return build_conversation if last_conversation.nil?
 
