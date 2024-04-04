a0fc8d46cc8c45f920f14a0bf9b487a4833690d1
Shivam Mishra <scm.mymail@gmail.com>
2024-04-04 09:35:25 +0530

    refactor: move toggle styles to tailwind

--- app/javascript/dashboard/routes/dashboard/settings/sla/components/Toggle.vue
+++ app/javascript/dashboard/routes/dashboard/settings/sla/components/Toggle.vue
@@ -20,40 +20,17 @@ const toggleCheckbox = () => {
 <template>
   <label class="relative inline-block h-4 w-7">
     <input
-      class="hidden"
+      class="hidden peer"
       type="checkbox"
       :checked="isChecked"
       @click="toggleCheckbox"
     />
     <div
-      class="absolute inset-0 p-0.5 transition-all ease-in-out duration-200 rounded-full cursor-pointer slider bg-slate-100"
+      :class="[
+        'absolute inset-0 p-0.5 transition-all ease-in-out duration-200 rounded-full cursor-pointer slider bg-slate-100',
+        'peer-focus:shadow-sm peer-checked:bg-woot-500 peer-checked:before:translate-x-3',
+        'before:absolute before:rounded-full before:content-[ ] before:top-0.5 before:left-0.5 before:h-3 before:w-3 before:bg-white before:transition-all before:duration-200',
+      ]"
     />
   </label>
 </template>
-
-<style>
-.slider:before {
-  position: absolute;
-  border-radius: 50%;
-  content: '';
-  top: 2px;
-  left: 2px;
-  height: calc(16px - 4px); /* 16px - 2 * 2px padding */
-  width: calc(16px - 4px);
-  background-color: white;
-  -webkit-transition: 0.4s;
-  transition: 0.4s;
-}
-
-input:checked + .slider {
-  @apply bg-woot-500;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #101010;
-}
-
-input:checked + .slider:before {
-  transform: translateX(12px);
-}
-</style>
