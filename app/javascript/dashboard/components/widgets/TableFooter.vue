84108db3fa7db6df8ce220b29baa0db6831bd28d
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-04-05 11:34:27 +0530

    Update TableFooter.vue

--- app/javascript/dashboard/components/widgets/TableFooter.vue
+++ app/javascript/dashboard/components/widgets/TableFooter.vue
@@ -3,104 +3,101 @@
     v-if="isFooterVisible"
     class="bg-white dark:bg-slate-900 h-12 flex items-center justify-between px-6"
   >
-    <div>
-      <span class="text-sm text-slate-700 dark:text-slate-200 font-medium">
-        {{
-          $t('GENERAL.SHOWING_RESULTS', {
-            currentItemCount: `${firstIndex}-${lastIndex}`,
-            totalItems: totalCount,
-          })
-        }}
-      </span>
-    </div>
-    <div>
-      <div
-        v-if="totalCount"
-        class="flex items-center bg-slate-50 dark:bg-slate-800 h-8 rounded-lg"
-      >
-        <woot-button
-          size="small"
-          variant="smooth"
-          color-scheme="secondary"
-          :is-disabled="hasFirstPage"
-          class-names="dark:!bg-slate-800 !opacity-100 rounded-l-lg rounded-r-none"
-          :class="
-            hasFirstPage
-              ? 'hover:!bg-slate-50 dark:hover:!bg-slate-800'
-              : 'dark:hover:!bg-slate-700/50'
-          "
-          @click="onFirstPage"
-        >
-          <fluent-icon
-            icon="chevrons-left"
-            size="20"
-            icon-lib="lucide"
-            :class="hasFirstPage && 'opacity-40'"
-          />
-        </woot-button>
-        <div class="bg-slate-75 dark:bg-slate-700/50 w-px rounded-sm h-4" />
-        <woot-button
-          size="small"
-          variant="smooth"
-          color-scheme="secondary"
-          :is-disabled="hasPrevPage"
-          class-names="dark:!bg-slate-800 !opacity-100 rounded-none"
-          :class="buttonClass(hasPrevPage)"
-          @click="onPrevPage"
-        >
-          <fluent-icon
-            icon="chevron-left-single"
-            size="20"
-            icon-lib="lucide"
-            :class="hasPrevPage && 'opacity-40'"
-          />
-        </woot-button>
+    <span class="text-sm text-slate-700 dark:text-slate-200 font-medium">
+      {{
+        $t('GENERAL.SHOWING_RESULTS', {
+          currentItemCount: `${firstIndex}-${lastIndex}`,
+          totalItems: totalCount,
+        })
+      }}
+    </span>
 
-        <div
-          class="flex px-3 items-center gap-3 tabular-nums bg-slate-50 dark:bg-slate-800 text-slate-700 dark:text-slate-100"
-        >
-          <span class="text-sm text-slate-800 dark:text-slate-100">
-            {{ currentPage }}
-          </span>
-          <span>/</span>
-          <span class="text-sm text-slate-600 dark:text-slate-200">
-            {{ totalPages }}
-          </span>
-        </div>
-        <woot-button
-          size="small"
-          variant="smooth"
-          color-scheme="secondary"
-          :is-disabled="hasNextPage"
-          class-names="dark:!bg-slate-800 !opacity-100 rounded-none"
-          :class="buttonClass(hasNextPage)"
-          @click="onNextPage"
-        >
-          <fluent-icon
-            icon="chevron-right-single"
-            size="20"
-            icon-lib="lucide"
-            :class="hasNextPage && 'opacity-40'"
-          />
-        </woot-button>
-        <div class="bg-slate-75 dark:bg-slate-700/50 w-px rounded-sm h-4" />
-        <woot-button
-          size="small"
-          variant="smooth"
-          color-scheme="secondary"
-          class-names="dark:!bg-slate-800 rounded-r-lg rounded-l-none !opacity-100"
-          :class="buttonClass(hasLastPage)"
-          :is-disabled="hasLastPage"
-          @click="onLastPage"
-        >
-          <fluent-icon
-            icon="chevrons-right"
-            size="20"
-            icon-lib="lucide"
-            :class="hasLastPage && 'opacity-40'"
-          />
-        </woot-button>
+    <div
+      v-if="totalCount"
+      class="flex items-center bg-slate-50 dark:bg-slate-800 h-8 rounded-lg"
+    >
+      <woot-button
+        size="small"
+        variant="smooth"
+        color-scheme="secondary"
+        :is-disabled="hasFirstPage"
+        class-names="dark:!bg-slate-800 !opacity-100 rounded-l-lg rounded-r-none"
+        :class="
+          hasFirstPage
+            ? 'hover:!bg-slate-50 dark:hover:!bg-slate-800'
+            : 'dark:hover:!bg-slate-700/50'
+        "
+        @click="onFirstPage"
+      >
+        <fluent-icon
+          icon="chevrons-left"
+          size="20"
+          icon-lib="lucide"
+          :class="hasFirstPage && 'opacity-40'"
+        />
+      </woot-button>
+      <div class="bg-slate-75 dark:bg-slate-700/50 w-px rounded-sm h-4" />
+      <woot-button
+        size="small"
+        variant="smooth"
+        color-scheme="secondary"
+        :is-disabled="hasPrevPage"
+        class-names="dark:!bg-slate-800 !opacity-100 rounded-none"
+        :class="buttonClass(hasPrevPage)"
+        @click="onPrevPage"
+      >
+        <fluent-icon
+          icon="chevron-left-single"
+          size="20"
+          icon-lib="lucide"
+          :class="hasPrevPage && 'opacity-40'"
+        />
+      </woot-button>
+
+      <div
+        class="flex px-3 items-center gap-3 tabular-nums bg-slate-50 dark:bg-slate-800 text-slate-700 dark:text-slate-100"
+      >
+        <span class="text-sm text-slate-800 dark:text-slate-75">
+          {{ currentPage }}
+        </span>
+        <span class="text-slate-600 dark:bg-slate-500">/</span>
+        <span class="text-sm text-slate-600 dark:text-slate-500">
+          {{ totalPages }}
+        </span>
       </div>
+      <woot-button
+        size="small"
+        variant="smooth"
+        color-scheme="secondary"
+        :is-disabled="hasNextPage"
+        class-names="dark:!bg-slate-800 !opacity-100 rounded-none"
+        :class="buttonClass(hasNextPage)"
+        @click="onNextPage"
+      >
+        <fluent-icon
+          icon="chevron-right-single"
+          size="20"
+          icon-lib="lucide"
+          :class="hasNextPage && 'opacity-40'"
+        />
+      </woot-button>
+      <div class="bg-slate-75 dark:bg-slate-700/50 w-px rounded-sm h-4" />
+      <woot-button
+        size="small"
+        variant="smooth"
+        color-scheme="secondary"
+        class-names="dark:!bg-slate-800 rounded-r-lg rounded-l-none !opacity-100"
+        :class="buttonClass(hasLastPage)"
+        :is-disabled="hasLastPage"
+        @click="onLastPage"
+      >
+        <fluent-icon
+          icon="chevrons-right"
+          size="20"
+          icon-lib="lucide"
+          :class="hasLastPage && 'opacity-40'"
+        />
+      </woot-button>
     </div>
   </footer>
 </template>
