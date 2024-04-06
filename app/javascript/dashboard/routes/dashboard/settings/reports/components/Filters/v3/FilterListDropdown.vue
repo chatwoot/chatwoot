e31b532e26a257e0b1ab1681f62b4f62572804dc
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2024-04-06 18:52:12 +0530

    feat: Adds component level search

--- app/javascript/dashboard/routes/dashboard/settings/reports/components/Filters/v3/FilterListDropdown.vue
+++ app/javascript/dashboard/routes/dashboard/settings/reports/components/Filters/v3/FilterListDropdown.vue
@@ -1,24 +1,55 @@
 <script setup>
+import { ref, computed } from 'vue';
 import FilterListItemButton from './FilterListItemButton.vue';
+import FilterDropdownSearch from './FilterDropdownSearch.vue';
 
-defineProps({
+const props = defineProps({
   listItems: {
     type: Array,
     default: () => [],
   },
+  enableSearch: {
+    type: Boolean,
+    default: false,
+  },
+  inputButtonText: {
+    type: String,
+    default: '',
+  },
+});
+
+const searchTerm = ref('');
+
+const onSearch = value => {
+  searchTerm.value = value;
+};
+
+const filteredListItems = computed(() => {
+  return props.listItems.filter(item =>
+    item.name.toLowerCase().includes(searchTerm.value.toLowerCase())
+  );
 });
 </script>
 <template>
   <div
     class="z-20 w-40 bg-white border shadow dark:bg-slate-800 rounded-xl border-slate-50 dark:border-slate-700/50"
   >
-    <slot name="search" />
-    <slot name="listItem" />
-    <filter-list-item-button
-      v-for="item in listItems"
-      :key="item.id"
-      :button-text="item.name"
-      @click="$emit('click')"
-    />
+    <slot name="search">
+      <filter-dropdown-search
+        v-if="enableSearch && listItems.length"
+        :button-text="inputButtonText"
+        :input-value="searchTerm"
+        @input="onSearch"
+        @click="onSearch('')"
+      />
+    </slot>
+    <slot name="listItem">
+      <filter-list-item-button
+        v-for="item in filteredListItems"
+        :key="item.id"
+        :button-text="item.name"
+        @click="$emit('click')"
+      />
+    </slot>
   </div>
 </template>
