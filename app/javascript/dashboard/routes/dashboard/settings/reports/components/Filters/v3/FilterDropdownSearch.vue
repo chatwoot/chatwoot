30224b56f6b58f4d11237b523f36fd000236710a
iamsivin <iamsivin@gmail.com>
2024-04-06 17:58:49 +0530

    feat: Reports filter components

---
+++ app/javascript/dashboard/routes/dashboard/settings/reports/components/Filters/v3/FilterDropdownSearch.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  buttonText: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+<template>
+  <div
+    class="flex items-center justify-between h-10 gap-2 px-3 mt-px border-b rounded-t-xl border-slate-50 dark:border-slate-700"
+  >
+    <div class="flex items-center gap-2">
+      <fluent-icon
+        icon="search"
+        size="18"
+        class="text-slate-400 dark:text-slate-400"
+      />
+      <input
+        type="text"
+        class="w-full mb-0 text-sm bg-white dark:bg-slate-800 text-slate-800 dark:text-slate-75 reset-base"
+        placeholder="Search"
+        @input="$emit('input', $event.target.value)"
+      />
+    </div>
+    <woot-button
+      size="small"
+      variant="clear"
+      color-scheme="primary"
+      class="!px-1 !py-1.5"
+      @click="$emit('clear')"
+    >
+      {{ buttonText }}
+    </woot-button>
+  </div>
+</template>
---
+++ app/javascript/dashboard/routes/dashboard/settings/reports/components/Filters/v3/FilterListDropdown.vue
@@ -0,0 +1,24 @@
+<script setup>
+import FilterListItemButton from './FilterListItemButton.vue';
+
+defineProps({
+  listItems: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+<template>
+  <div
+    class="z-20 w-40 bg-white border shadow dark:bg-slate-800 rounded-xl border-slate-50 dark:border-slate-700/50"
+  >
+    <slot name="search" />
+    <slot name="listItem" />
+    <filter-list-item-button
+      v-for="item in listItems"
+      :key="item.id"
+      :button-text="item.name"
+      @click="$emit('click')"
+    />
+  </div>
+</template>
