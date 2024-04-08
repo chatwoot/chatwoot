f4ff232a2b2d5445c6ca1b1beca85eb80d824e97
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-04-08 12:58:17 +0530

    feat: Add TableHeaderCell component

---
+++ app/javascript/dashboard/components/widgets/TableHeaderCell.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    :class="`flex items-center px-0 py-2 text-xs font-medium text-left uppercase col-span-${span} text-slate-700 dark:text-slate-100 rtl:text-right`"
+  >
+    <slot v-if="labelKey">
+      {{ $t(labelKey) }}
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableHeaderCell',
+  props: {
+    span: {
+      type: Number,
+      required: true,
+    },
+    labelKey: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
