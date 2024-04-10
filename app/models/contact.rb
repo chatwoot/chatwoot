e08a296afc843d7387c62b20046c5ebc7470ea59
Rishabh Pandram <rishabh@onehash.ai>
2024-04-10 12:33:29 +0530

    new-dev init

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,10 +6,16 @@
 #
 #  id                    :integer          not null, primary key
 #  additional_attributes :jsonb
+#  blocked               :boolean          default(FALSE), not null
+#  contact_type          :integer          default(0)
+#  country_code          :string           default("")
 #  custom_attributes     :jsonb
 #  email                 :string
 #  identifier            :string
 #  last_activity_at      :datetime
+#  last_name             :string           default("")
+#  location              :string           default("")
+#  middle_name           :string           default("")
 #  name                  :string           default("")
 #  phone_number          :string
 #  created_at            :datetime         not null
@@ -19,6 +25,7 @@
 # Indexes
 #
 #  index_contacts_on_account_id                          (account_id)
+#  index_contacts_on_blocked                             (blocked)
 #  index_contacts_on_lower_email_account_id              (lower((email)::text), account_id)
 #  index_contacts_on_name_email_phone_number_identifier  (name,email,phone_number,identifier) USING gin
 #  index_contacts_on_nonempty_fields                     (account_id,email,phone_number,identifier) WHERE (((email)::text <> ''::text) OR ((phone_number)::text <> ''::text) OR ((identifier)::text <> ''::text))
