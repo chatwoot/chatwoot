7614fb824832b73046a593ef3c29117334b40a28
Sojan <sojan@pepalo.com>
2024-04-15 13:51:51 -0700

    chore: fixes

--- enterprise/app/services/features/base_service.rb
+++ enterprise/app/services/features/base_service.rb
@@ -1,8 +1,6 @@
 class Features::BaseService
   MIGRATION_VERSION = ActiveRecord::Migration[7.0]
 
-  private
-
   def vector_extension_enabled?
     ActiveRecord::Base.connection.extension_enabled?('vector')
   end
