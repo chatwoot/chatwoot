9ec87c8575f70f4dbc2f7d3dca3882359bfb36ed
Shivam Mishra <scm.mymail@gmail.com>
2024-04-16 12:12:18 +0530

    fix: dark mode

--- app/javascript/dashboard/routes/dashboard/settings/sla/components/SLAPaywallEnterprise.vue
+++ app/javascript/dashboard/routes/dashboard/settings/sla/components/SLAPaywallEnterprise.vue
@@ -12,20 +12,24 @@ defineProps({
 <template>
   <base-empty-state>
     <div
-      class="flex flex-col items-center max-w-md px-6 py-6 bg-white border shadow rounded-xl border-slate-100"
+      class="flex flex-col max-w-md px-6 py-6 bg-white border shadow dark:bg-slate-800 rounded-xl border-slate-100 dark:border-slate-900"
     >
-      <div class="flex items-center w-full gap-2">
+      <div class="flex items-center w-full gap-2 mb-4">
         <span
-          class="flex items-center justify-center w-6 h-6 rounded-full bg-woot-100"
+          class="flex items-center justify-center w-6 h-6 rounded-full bg-woot-75/70 dark:bg-woot-800/40"
         >
-          <fluent-icon size="14" class="text-woot-500" icon="lock-closed" />
+          <fluent-icon
+            size="14"
+            class="flex-shrink-0 text-woot-500 dark:text-woot-500"
+            icon="lock-closed"
+          />
         </span>
-        <span class="text-base font-medium text-slate-900">
+        <span class="text-base font-medium text-slate-900 dark:text-white">
           {{ $t('SLA.PAYWALL.TITLE') }}
         </span>
       </div>
       <p
-        class="text-sm font-normal tracking-[0.5%] mt-4"
+        class="text-sm font-normal tracking-[0.5%]"
         v-html="$t('SLA.ENTERPRISE_PAYWALL.AVAILABLE_ON')"
       />
       <template v-if="isAdmin">
