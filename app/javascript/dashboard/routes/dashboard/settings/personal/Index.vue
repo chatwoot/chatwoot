de5820f94393a712ae3928707cc0341c6f19a8fa
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-04-17 10:46:53 +0530

    feat: add routes

---
+++ app/javascript/dashboard/routes/dashboard/settings/personal/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center w-full overflow-y-auto">
+    <div
+      class="flex flex-col h-full p-5 pt-16 mx-auto my-0 bg-white dark:bg-slate-900 font-inter"
+    >
+      <div class="flex flex-col gap-8 sm:max-w-3xl">
+        <h1>Personal settings</h1>
+      </div>
+    </div>
+  </div>
+</template>
