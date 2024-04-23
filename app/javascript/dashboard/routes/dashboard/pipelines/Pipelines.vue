f582f3f82519533428f601712fe5150759368d56
CongWeploy <cong.nguyen@weployapp.com>
2024-04-23 22:08:39 +0700

    Change to another Kanban component

--- app/javascript/dashboard/routes/dashboard/pipelines/Pipelines.vue
+++ app/javascript/dashboard/routes/dashboard/pipelines/Pipelines.vue
@@ -1,145 +1,150 @@
 <template>
-  <div id="app">
-    <drag-drop
-      :dropzones="dropGroups"
-      :dropzones-title="'XYZ Company Teams'"
-      :original-data="stories"
-      :original-title="'Tasks to be distributed'"
-      :in-place="true"
-      :enable-save="false"
-      :enable-cancel="false"
-      @dropInOriginalBucket="originalBucketDropEvent"
-      @dropInDestinationBucket="destinationBucketDropEvent"
-    >
-      <template #dd-card="{ cardData }">
-        <custom-card :data="cardData" @done="doneMarked" />
-      </template>
-    </drag-drop>
+  <div
+    class="flex justify-between flex-col h-full m-0 flex-1 bg-white dark:bg-slate-900"
+  >
+    <Kanban :stages="statuses" :blocks="blocks" @update-block="updateBlock">
+      <div v-for="stage in statuses" :slot="stage" :key="stage">
+        <h2>
+          {{ stage }}
+          <a>+</a>
+        </h2>
+      </div>
+      <div v-for="item in blocks" :slot="item.id" :key="item.id">
+        <div v-if="item.status === 'on-hold'">
+          {{ item.title }}
+        </div>
+        <div v-else>
+          <strong>id:</strong> {{ item.id }}
+          <div>{{ item.title }}</div>
+        </div>
+      </div>
+      <div v-for="stage in statuses" :key="stage" :slot="`footer-${stage}`">
+        <a href="" @click.prevent="() => addBlock(stage)">+ Add new block</a>
+      </div>
+    </Kanban>
   </div>
 </template>
 
 <script>
-import DragDrop from 'vue-drag-n-drop';
-import CustomCard from './CustomCard.vue';
+import faker from 'faker';
+import { debounce } from 'lodash';
+import Kanban from '../../../components/Kanban.vue';
 
 export default {
+  name: 'app',
   components: {
-    DragDrop,
-    CustomCard,
+    Kanban,
   },
   data() {
     return {
-      stories: [
-        {
-          title: 'Strategy 101',
-          description: 'Create a draft of business plan',
-          time: '3 days',
-          done: false,
-        },
-        {
-          title: 'Strategy 102',
-          description: 'Finalize the plan',
-          time: '4 days',
-          done: false,
-        },
-        {
-          title: 'Tech diagram',
-          description: 'Draw the tech data',
-          time: '4 days',
-          done: false,
-        },
-        {
-          title: 'Place Holder',
-          description: 'Data Science Team',
-          time: '5 days',
-          done: false,
-        },
-        {
-          title: 'Strategy 104',
-          description: 'Create a draft of business plan',
-          time: '3 days',
-          done: false,
-        },
-        {
-          title: 'Strategy 105',
-          description: 'Finalize the plan',
-          time: '4 days',
-          done: false,
-        },
-        {
-          title: 'Tech diagram 1',
-          description: 'Draw the tech data',
-          time: '4 days',
-          done: false,
-        },
-        {
-          title: 'Place Holder 2',
-          description: 'Data Science Team',
-          time: '5 days',
-          done: false,
-        },
-      ],
-
-      dropGroups: [
-        {
-          name: 'Business Team',
-          children: [],
-        },
-        {
-          name: 'Tech Dept',
-          children: [],
-        },
-        {
-          name: 'Marketing Dept',
-          children: [],
-        },
-        {
-          name: 'CS Team',
-          children: [],
-        },
-        {
-          name: 'Support Dept',
-          children: [],
-        },
-        {
-          name: 'Finance Dept',
-          children: [],
-        },
-      ],
+      statuses: ['on-hold', 'in-progress', 'needs-review', 'approved'],
+      blocks: [],
     };
+  },
+  mounted() {
+    for (let i = 0; i <= 10; i += 1) {
+      this.blocks.push({
+        id: i,
+        status: this.statuses[Math.floor(Math.random() * 4)],
+        title: faker.company.bs(),
+      });
+    }
   },
 
   methods: {
-    save(received) {
-      console.log('Received:', received);
-    },
-
-    doneMarked(data) {
-      data.done = true;
-      alert(data.title);
-    },
-
-    originalBucketDropEvent(result) {
-      console.log('Original: ', result);
-    },
-
-    destinationBucketDropEvent(columnName, result) {
-      console.log('Destination: ', columnName, result);
-    },
-
-    cancel() {
-      console.log('Cancel hit');
-    },
+    updateBlock: debounce(function find(id, status) {
+      this.blocks.find(b => b.id === Number(id)).status = status;
+    }, 500),
+    addBlock: debounce(function push(stage) {
+      this.blocks.push({
+        id: this.blocks.length,
+        status: stage,
+        title: faker.company.bs(),
+      });
+    }, 500),
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss">
+@import '../../../assets/scss/kanban.scss';
+
+$on-hold: #fb7d44;
+$in-progress: #2a92bf;
+$needs-review: #f4ce46;
+$approved: #00b961;
+
+* {
+  box-sizing: border-box;
+}
+
+.drag-column {
+  .drag-column-header > div {
+    width: 100%;
+    h2 > a {
+      float: right;
+    }
+  }
+
+  .drag-column-footer > div {
+    margin-left: 10px;
+    a {
+      text-decoration: none;
+      color: white;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  &-on-hold {
+    .drag-column-header,
+    .is-moved,
+    .drag-options {
+      background: $on-hold;
+    }
+  }
+
+  &-in-progress {
+    .drag-column-header,
+    .is-moved,
+    .drag-options {
+      background: $in-progress;
+    }
+  }
+
+  &-needs-review {
+    .drag-column-header,
+    .is-moved,
+    .drag-options {
+      background: $needs-review;
+    }
+  }
+
+  &-approved {
+    .drag-column-header,
+    .is-moved,
+    .drag-options {
+      background: $approved;
+    }
+  }
+}
+
+.section {
+  padding: 20px;
   text-align: center;
-  color: #2c3e50;
+
+  a {
+    color: white;
+    text-decoration: none;
+    font-weight: 300;
+  }
+
+  h4 {
+    font-weight: 400;
+    a {
+      font-weight: 600;
+    }
+  }
 }
 </style>
