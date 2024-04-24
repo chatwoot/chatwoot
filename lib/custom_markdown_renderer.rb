bcd99a02fb2bcbec25974737df49e28424c5d8f7
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-04-24 11:04:01 +0530

    fix: responsiveness

--- lib/custom_markdown_renderer.rb
+++ lib/custom_markdown_renderer.rb
@@ -87,15 +87,12 @@ class CustomMarkdownRenderer < CommonMarker::HtmlRenderer
   def make_loom_embed(loom_match)
     video_id = loom_match[1]
     %(
+      <div style="position: relative; padding-bottom: 62.5%; height: 0;">
       <iframe
-        src="https://www.loom.com/embed/#{video_id}"
-        width="640"
-        height="360"
-        frameborder="0"
-        webkitallowfullscreen
-        mozallowfullscreen
-        allowfullscreen
-      ></iframe>
+         src="https://www.loom.com/embed/#{video_id}"
+         frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen
+         style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
+      </div>
     )
   end
 
