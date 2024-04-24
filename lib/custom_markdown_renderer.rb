c4ce02e12264ae9b106d419055ecaf2c99c5e514
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-04-24 11:22:13 +0530

    Update custom_markdown_renderer.rb

--- lib/custom_markdown_renderer.rb
+++ lib/custom_markdown_renderer.rb
@@ -105,7 +105,7 @@ class CustomMarkdownRenderer < CommonMarker::HtmlRenderer
         frameborder="0"
         allow="autoplay; fullscreen; picture-in-picture"
         allowfullscreen
-        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">></iframe>
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
        </div>
     )
   end
