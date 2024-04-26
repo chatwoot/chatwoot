dc5ff3408539f6cbcbd65894815e7d95cb8fbcec
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-04-26 14:18:51 +0530

    chore: revire comments

--- app/javascript/v3/components/Form/ProfileAvatar.vue
+++ app/javascript/v3/components/Form/ProfileAvatar.vue
@@ -1,9 +1,5 @@
 <template>
-  <div
-    class="relative rounded-xl h-[72px] w-[72px] cursor-pointer"
-    @mouseover="onImageOver"
-    @mouseleave="onImageOut"
-  >
+  <div class="relative rounded-xl h-[72px] w-[72px] cursor-pointe group">
     <img
       v-if="shouldShowImage"
       class="rounded-xl h-[72px] w-[72px]"
@@ -19,27 +15,24 @@
       ref="fileInputRef"
       type="file"
       accept="image/png, image/jpeg, image/jpg, image/gif, image/webp"
-      style="display: none"
+      hidden
       @change="onImageUpload"
     />
-    <button
-      v-if="src && shouldShowUploadIcon"
-      class="absolute z-10 flex items-center justify-center w-6 h-6 p-1 border border-white rounded-full select-none dark:border-ash-75 reset-base -top-2 -right-2 bg-ash-300"
-      @click="onAvatarDelete"
-    >
-      <fluent-icon
-        icon="dismiss"
-        size="16"
-        class="text-slate-800 dark:text-slate-200"
-      />
-    </button>
-    <button
-      v-if="shouldShowUploadIcon"
-      class="reset-base absolute h-[72px] w-[72px] top-0 left-0 rounded-xl select-none flex items-center justify-center bg-modal-backdrop-dark dark:bg-modal-backdrop-dark"
-      @click="openFileInput"
-    >
-      <fluent-icon icon="avatar-upload" size="32" class="text-white" />
-    </button>
+    <div class="hidden group-hover:block">
+      <button
+        v-if="src"
+        class="absolute z-10 flex items-center justify-center w-6 h-6 p-1 border border-white rounded-full select-none dark:border-ash-75 reset-base -top-2 -right-2 bg-ash-300"
+        @click="onAvatarDelete"
+      >
+        <fluent-icon icon="dismiss" size="16" class="text-ash-900" />
+      </button>
+      <button
+        class="reset-base absolute h-[72px] w-[72px] top-0 left-0 rounded-xl select-none flex items-center justify-center bg-modal-backdrop-dark dark:bg-modal-backdrop-dark"
+        @click="openFileInput"
+      >
+        <fluent-icon icon="avatar-upload" size="32" class="text-white" />
+      </button>
+    </div>
   </div>
 </template>
 <script setup>
@@ -60,7 +53,6 @@ const emits = defineEmits(['change', 'delete']);
 
 const hasImageLoaded = ref(false);
 const imageLoadedError = ref(false);
-const shouldShowUploadIcon = ref(false);
 const fileInputRef = ref(null);
 
 const shouldShowImage = computed(() => props.src && !imageLoadedError.value);
@@ -74,27 +66,19 @@ const onImageLoad = () => {
   imageLoadedError.value = false;
 };
 
-const onImageOver = () => {
-  shouldShowUploadIcon.value = true;
-};
-
-const onImageOut = () => {
-  shouldShowUploadIcon.value = false;
-};
-
 const openFileInput = () => {
   fileInputRef.value.click();
 };
 
 const onImageUpload = event => {
   const [file] = event.target.files;
-  emits('change-avatar', {
+  emits('change', {
     file,
     url: file ? URL.createObjectURL(file) : null,
   });
 };
 
 const onAvatarDelete = () => {
-  emits('delete-avatar');
+  emits('delete');
 };
 </script>
