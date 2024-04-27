836b93ce6720e9af561fd4fe1a1f9fb619dd8320
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-04-27 12:09:02 +0530

    chore: Replace form-input with woot-input

--- app/javascript/dashboard/routes/dashboard/settings/personal/ChangePassword.vue
+++ app/javascript/dashboard/routes/dashboard/settings/personal/ChangePassword.vue
@@ -1,62 +1,57 @@
 <template>
   <form @submit.prevent="changePassword()">
     <div class="flex flex-col w-full gap-4">
-      <form-input
+      <woot-input
         v-model="currentPassword"
         type="password"
-        name="currentPassword"
+        :styles="inputStyles"
         :class="{ error: $v.currentPassword.$error }"
         :label="$t('PROFILE_SETTINGS.FORM.CURRENT_PASSWORD.LABEL')"
         :placeholder="$t('PROFILE_SETTINGS.FORM.CURRENT_PASSWORD.PLACEHOLDER')"
-        :has-error="$v.currentPassword.$error"
-        :error-message="
+        :error="`${
           $v.currentPassword.$error
             ? $t('PROFILE_SETTINGS.FORM.CURRENT_PASSWORD.ERROR')
             : ''
-        "
-        @blur="$v.currentPassword.$touch"
+        }`"
+        @input="$v.currentPassword.$touch"
       />
-      <form-input
+
+      <woot-input
         v-model="password"
         type="password"
-        name="password"
+        :styles="inputStyles"
         :class="{ error: $v.password.$error }"
         :label="$t('PROFILE_SETTINGS.FORM.PASSWORD.LABEL')"
         :placeholder="$t('PROFILE_SETTINGS.FORM.PASSWORD.PLACEHOLDER')"
-        :has-error="$v.password.$error"
-        :error-message="
+        :error="`${
           $v.password.$error ? $t('PROFILE_SETTINGS.FORM.PASSWORD.ERROR') : ''
-        "
-        @blur="$v.password.$touch"
+        }`"
+        @input="$v.password.$touch"
       />
 
-      <form-input
+      <woot-input
         v-model="passwordConfirmation"
         type="password"
-        name="passwordConfirmation"
+        :styles="inputStyles"
         :class="{ error: $v.passwordConfirmation.$error }"
         :label="$t('PROFILE_SETTINGS.FORM.PASSWORD_CONFIRMATION.LABEL')"
         :placeholder="
           $t('PROFILE_SETTINGS.FORM.PASSWORD_CONFIRMATION.PLACEHOLDER')
         "
-        :has-error="$v.passwordConfirmation.$error"
-        :error-message="
+        :error="`${
           $v.passwordConfirmation.$error
             ? $t('PROFILE_SETTINGS.FORM.PASSWORD_CONFIRMATION.ERROR')
             : ''
-        "
-        @blur="$v.passwordConfirmation.$touch"
+        }`"
+        @input="$v.passwordConfirmation.$touch"
       />
+
       <form-button
         type="submit"
         color-scheme="primary"
         variant="solid"
         size="large"
-        :disabled="
-          !currentPassword ||
-          !passwordConfirmation ||
-          !$v.passwordConfirmation.isEqPassword
-        "
+        :disabled="isButtonDisabled"
       >
         {{ $t('PROFILE_SETTINGS.FORM.PASSWORD_SECTION.BTN_TEXT') }}
       </form-button>
@@ -69,11 +64,9 @@ import { required, minLength } from 'vuelidate/lib/validators';
 import alertMixin from 'shared/mixins/alertMixin';
 import { parseAPIErrorResponse } from 'dashboard/store/utils/api';
 import FormButton from 'v3/components/Form/Button.vue';
-import FormInput from 'v3/components/Form/Input.vue';
 export default {
   components: {
     FormButton,
-    FormInput,
   },
   mixins: [alertMixin],
   data() {
@@ -100,6 +93,23 @@ export default {
         }
         return true;
       },
+    },
+  },
+  computed: {
+    inputStyles() {
+      return {
+        borderRadius: '12px',
+        padding: '6px 12px',
+        fontSize: '14px',
+        marginBottom: '2px',
+      };
+    },
+    isButtonDisabled() {
+      return (
+        !this.currentPassword ||
+        !this.passwordConfirmation ||
+        !this.$v.passwordConfirmation.isEqPassword
+      );
     },
   },
   methods: {
