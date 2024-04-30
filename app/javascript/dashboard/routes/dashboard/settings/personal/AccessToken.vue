c9a575c31066a974da3ec2808f987df8a742f6b5
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-04-30 22:02:01 +0530

    feat: Revamp access token section in profile settings (#9328)
    Co-authored-by: Sivin Varghese <64252451+iamsivin@users.noreply.github.com>

---
+++ app/javascript/dashboard/routes/dashboard/settings/personal/AccessToken.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex flex-row justify-between gap-4">
+    <woot-input
+      name="access_token"
+      class="flex-1 focus:[&>input]:!border-slate-200 focus:[&>input]:dark:!border-slate-600 [&>input]:cursor-not-allowed"
+      :styles="{
+        borderRadius: '12px',
+        padding: '6px 12px',
+        fontSize: '14px',
+        marginBottom: '2px',
+      }"
+      type="password"
+      :value="value"
+      readonly
+    />
+    <form-button
+      type="submit"
+      size="large"
+      icon="text-copy"
+      variant="outline"
+      color-scheme="secondary"
+      @click="onClick"
+    >
+      {{ $t('PROFILE_SETTINGS.FORM.ACCESS_TOKEN.COPY') }}
+    </form-button>
+  </div>
+</template>
+<script setup>
+import FormButton from 'v3/components/Form/Button.vue';
+const props = defineProps({
+  value: {
+    type: String,
+    default: '',
+  },
+});
+const emit = defineEmits(['on-copy']);
+const onClick = () => {
+  emit('on-copy', props.value);
+};
+</script>
