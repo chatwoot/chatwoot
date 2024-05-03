9553942726d591dfdeffab3a260b0e4a5b64e894
Jaideep Guntupalli <guntupallijaideep@gmail.com>
2024-05-03 13:20:07 +0530

    fix: spec for delaydispatch job

--- spec/jobs/delay_dispatch_event_job_spec.rb
+++ spec/jobs/delay_dispatch_event_job_spec.rb
@@ -1,35 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe DelayDispatchEventJob do
-  subject(:job) { described_class.perform_later(event_name: event_name, timestamp: timestamp, message_id: message_id) }
+  subject(:job) { described_class.perform_later(event_name: event_name, timestamp: timestamp, message_id: message.id) }
 
-  let(:event_name) { 'test_event' }
+  let(:event_name) { 'message.created' }
   let(:timestamp) { Time.now.to_i }
-  let(:message_id) { 10 }
-  let(:message) { instance_double(Message) }
+  let!(:message) { create(:message, message_type: :incoming) }
 
   before do
-    allow(Message).to receive(:find_by).with(id: message_id).and_return(message)
     allow(Rails.configuration.dispatcher).to receive(:dispatch)
   end
 
   it 'enqueues the job on the high queue' do
     expect { job }.to have_enqueued_job(described_class)
-      .with(event_name: event_name, timestamp: timestamp, message_id: message_id)
+      .with(event_name: event_name, timestamp: timestamp, message_id: message.id)
       .on_queue('high')
   end
 
-  it 'when the job is performed and the message exists dispatches the event with the correct parameters' do
-    described_class.perform_now(event_name: event_name, timestamp: timestamp, message_id: message_id)
-    expected_data = { message: message }
-    expect(Rails.configuration.dispatcher).to have_received(:dispatch).with(event_name, timestamp, expected_data)
+  context 'when the job is performed and the message exists' do
+    it 'dispatches the event with the correct parameters' do
+      described_class.perform_now(event_name: event_name, timestamp: timestamp, message_id: message.id)
+      expected_data = { message: message }
+      expect(Rails.configuration.dispatcher).to have_received(:dispatch).with(event_name, timestamp, expected_data)
+    end
   end
 
   context 'when the job is performed and the message does not exist' do
-    let(:message) { nil }
+    before { message.destroy }
 
     it 'does not dispatch the event' do
-      described_class.perform_now(event_name: event_name, timestamp: timestamp, message_id: message_id)
+      described_class.perform_now(event_name: event_name, timestamp: timestamp, message_id: message.id)
       expect(Rails.configuration.dispatcher).not_to have_received(:dispatch)
     end
   end
