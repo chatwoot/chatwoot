3c1bffdcacb53eb5fb018440a114167a118c3aab
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-05-06 22:33:39 +0530

    feat: Revamp profile settings screen (#9352)
    Co-authored-by: Shivam Mishra <scm.mymail@gmail.com>
    Co-authored-by: iamsivin <iamsivin@gmail.com>

--- app/javascript/v3/components/Form/CheckBox.vue
+++ app/javascript/v3/components/Form/CheckBox.vue
@@ -4,7 +4,7 @@
     v-model="checked"
     type="checkbox"
     :value="value"
-    class="flex-shrink-0 mt-0.5 border-ash-200 border checked:border-none checked:bg-primary-600 dark:checked:bg-primary-600 shadow appearance-none rounded-[4px] w-4 h-4 focus:ring-1 after:content-[''] after:text-white checked:after:content-['✓'] after:flex after:items-center after:justify-center after:text-center after:text-xs after:font-bold after:relative"
+    class="flex-shrink-0 mt-0.5 border-ash-200 border bg-ash-50 checked:border-none checked:bg-primary-600 dark:checked:bg-primary-600 shadow-sm appearance-none rounded-[4px] w-4 h-4 focus:ring-1 after:content-[''] after:text-white checked:after:content-['✓'] after:flex after:items-center after:justify-center after:text-center after:text-xs after:font-bold after:relative"
   />
 </template>
 
