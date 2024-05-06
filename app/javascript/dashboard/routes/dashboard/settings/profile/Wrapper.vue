de35855e5c53d1f59db01b6cb5ae8909e699d52b
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2024-05-06 20:50:36 +0530

    Update app/javascript/dashboard/routes/dashboard/settings/profile/Wrapper.vue

--- app/javascript/dashboard/routes/dashboard/settings/profile/Wrapper.vue
+++ app/javascript/dashboard/routes/dashboard/settings/profile/Wrapper.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col justify-between flex-1 h-full m-0 overflow-auto bg-white dark:bg-ash-900"
+    class="flex flex-col justify-between flex-1 h-full m-0 overflow-auto bg-white dark:bg-slate-900"
   >
     <keep-alive v-if="keepAlive">
       <router-view />
