5d03829e36c7b97c40d1b96321b635fd5c985034
Jaideep Guntupalli <guntupallijaideep@gmail.com>
2024-05-10 10:45:43 +0530

    revert: update message delayjob

--- app/jobs/delay_dispatch_event_job.rb
+++ app/jobs/delay_dispatch_event_job.rb
@@ -1,21 +1,11 @@
 class DelayDispatchEventJob < ApplicationJob
   queue_as :high
 
-  def perform(event_name:, timestamp:, message_id:, performed_by_id: nil, previous_changes: nil)
+  def perform(event_name:, timestamp:, message_id:)
     message = Message.find_by(id: message_id)
-    return unless message
-
     data = {
       message: message
     }
-
-    if performed_by_id
-      performed_by = User.find_by(id: performed_by_id)
-      data[:performed_by] = performed_by if performed_by
-    end
-
-    data[:previous_changes] = previous_changes if previous_changes
-
     Rails.configuration.dispatcher.dispatch(event_name, timestamp, data)
   end
 end
