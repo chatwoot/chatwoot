7c3151e29be83fbbc25739b11a4528e127bb57d4
Sojan <sojan@pepalo.com>
2024-05-09 18:44:07 -0700

    chore: add comments

--- app/listeners/participation_listener.rb
+++ app/listeners/participation_listener.rb
@@ -6,6 +6,8 @@ class ParticipationListener < BaseListener
     return if conversation.assignee_id.blank?
 
     conversation.conversation_participants.find_or_create_by!(user_id: conversation.assignee_id)
+  # We have observed race conditions triggering these errors
+  # example: Assignment happening via automation, while auto assignment is also configured.
   rescue ActiveRecord::RecordNotUnique, ActiveRecord::RecordInvalid
     Rails.logger.warn "Failed to create conversation participant for account #{conversation.account.id} " \
                       ": user #{conversation.assignee_id} : conversation #{conversation.id}"
