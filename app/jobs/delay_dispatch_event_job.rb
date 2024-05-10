825f76054da8549fbff96add5eefbf42a86796c1
Jaideep Guntupalli <guntupallijaideep@gmail.com>
2024-05-10 10:54:07 +0530

    fix: error handling

--- app/jobs/delay_dispatch_event_job.rb
+++ app/jobs/delay_dispatch_event_job.rb
@@ -3,6 +3,8 @@ class DelayDispatchEventJob < ApplicationJob
 
   def perform(event_name:, timestamp:, message_id:)
     message = Message.find_by(id: message_id)
+    return unless message
+
     data = {
       message: message
     }
