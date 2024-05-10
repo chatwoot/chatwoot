7c1b00c9054a7dd6d1de2f0d8ae5d7ba1cbe55f9
Sojan Jose <sojan@pepalo.com>
2024-05-10 12:21:23 -0700

    chore: Handle conversation participation creation race condition error (#9449)
    We observed some race condition errors in the conversation participation listener while trying to create a conversation participation assignment. This PR handles this error and also adds additional debug information for future.
    
    fixes: https://linear.app/chatwoot/issue/CW-3296/activerecordrecordnotunique-pguniqueviolation-error-duplicate-key
    
    ## Changelog
    
    - handles `ActiveRecord::RecordNotUnique, ActiveRecord::RecordInvald` errors so that they won't pollute sentry
    - Adds a debug statement to log the cases
    - Add previous_changes into the dispatcher so that we know the exact attribute changes which trigger `assignee_changed, team_changed` events ( would be handy in future )

--- app/listeners/participation_listener.rb
+++ app/listeners/participation_listener.rb
@@ -3,6 +3,13 @@ class ParticipationListener < BaseListener
 
   def assignee_changed(event)
     conversation, _account = extract_conversation_and_account(event)
-    conversation.conversation_participants.find_or_create_by!(user_id: conversation.assignee_id) if conversation.assignee_id.present?
+    return if conversation.assignee_id.blank?
+
+    conversation.conversation_participants.find_or_create_by!(user_id: conversation.assignee_id)
+  # We have observed race conditions triggering these errors
+  # example: Assignment happening via automation, while auto assignment is also configured.
+  rescue ActiveRecord::RecordNotUnique, ActiveRecord::RecordInvalid
+    Rails.logger.warn "Failed to create conversation participant for account #{conversation.account.id} " \
+                      ": user #{conversation.assignee_id} : conversation #{conversation.id}"
   end
 end
