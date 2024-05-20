5687d84ad20f5bb65072e28bc3590973c3c85aba
Shivam Mishra <scm.mymail@gmail.com>
2024-05-20 19:36:13 +0530

    feat: add fallback to env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  client_id = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_ID', nil)
-  client_secret = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_SECRET', nil)
+  begin
+    client_id = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_ID', nil)
+    client_secret = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_SECRET', nil)
+  rescue ActiveRecord::NoDatabaseError
+    client_id = ENV.fetch('GOOGLE_OAUTH_CLIENT_ID', nil)
+    client_secret = ENV.fetch('GOOGLE_OAUTH_CLIENT_SECRET', nil)
+  end
 
   provider :google_oauth2, client_id, client_secret, { provider_ignores_state: true }
 end
