ac8989149db99d8647b9c8fa9e8f65530f536766
Shivam Mishra <scm.mymail@gmail.com>
2024-05-20 11:52:42 +0530

    feat: move Azure config to installation_config (#9481)
    This PR has the following changes
    
    1. Add `AZURE_APP_ID` and `AZURE_APP_SECRET` to installation config
    2. Add Microsoft config to `super_admin/features.yml`
    3. Replace usage of `ENV.fetch` with `GlobalConfigService.load` for
    fetch App ID and Secret

--- app/controllers/super_admin/app_configs_controller.rb
+++ app/controllers/super_admin/app_configs_controller.rb
@@ -35,6 +35,8 @@ class SuperAdmin::AppConfigsController < SuperAdmin::ApplicationController
     @allowed_configs = case @config
                        when 'facebook'
                          %w[FB_APP_ID FB_VERIFY_TOKEN FB_APP_SECRET IG_VERIFY_TOKEN FACEBOOK_API_VERSION ENABLE_MESSENGER_CHANNEL_HUMAN_AGENT]
+                       when 'microsoft'
+                         %w[AZURE_APP_ID AZURE_APP_SECRET]
                        when 'email'
                          ['MAILER_INBOUND_EMAIL_DOMAIN']
                        else
