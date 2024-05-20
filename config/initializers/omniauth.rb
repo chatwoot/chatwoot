aed429c03a3247b7fbd25c4df7200ce728e60c6a
Shivam Mishra <scm.mymail@gmail.com>
2024-05-20 19:20:29 +0530

    chore: revert omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV.fetch('GOOGLE_OAUTH_CLIENT_ID', nil), ENV.fetch('GOOGLE_OAUTH_CLIENT_SECRET', nil), {
-    provider_ignores_state: true
-  }
+  client_id = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_ID', nil)
+  client_secret = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_SECRET', nil)
+
+  provider :google_oauth2, client_id, client_secret, { provider_ignores_state: true }
 end
