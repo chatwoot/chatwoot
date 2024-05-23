fe7ffbeddf75fe8e515d9712de6c4edfc6c061ca
Shivam Mishra <scm.mymail@gmail.com>
2024-05-23 13:57:27 +0530

    refactor: use lambda

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,8 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  begin
+Rails.application.config.middleware.use lambda { |env|
+  OmniAuth::Builder.new(env) do
     client_id = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_ID', nil)
     client_secret = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_SECRET', nil)
-  rescue ActiveRecord::NoDatabaseError, ActiveRecord::StatementInvalid
-    client_id = ENV.fetch('GOOGLE_OAUTH_CLIENT_ID', nil)
-    client_secret = ENV.fetch('GOOGLE_OAUTH_CLIENT_SECRET', nil)
-  end
 
-  provider :google_oauth2, client_id, client_secret, { provider_ignores_state: true }
-end
+    provider :google_oauth2, client_id, client_secret, { provider_ignores_state: true }
+  end
+}
