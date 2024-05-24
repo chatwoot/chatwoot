7f3c70854db98d45f556bddd727c4fa4ba1df21b
Shivam Mishra <scm.mymail@gmail.com>
2024-05-24 11:21:05 +0530

    fix: CI issue with lazy loading omniauth middleware (#9504)
    Testing CI

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,8 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  client_id = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_ID', nil)
-  client_secret = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_SECRET', nil)
+ActiveSupport.on_load(:active_record) do
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    client_id = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_ID', nil)
+    client_secret = GlobalConfigService.load('GOOGLE_OAUTH_CLIENT_SECRET', nil)
 
-  provider :google_oauth2, client_id, client_secret, { provider_ignores_state: true }
+    provider :google_oauth2, client_id, client_secret, { provider_ignores_state: true }
+  end
 end
