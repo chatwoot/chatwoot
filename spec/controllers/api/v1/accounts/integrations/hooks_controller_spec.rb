a3897df9b59a8344d5ba53aa774e3b372b88cb11
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-05-30 10:24:21 +0530

    chore: add parsed error message

--- spec/controllers/api/v1/accounts/integrations/hooks_controller_spec.rb
+++ spec/controllers/api/v1/accounts/integrations/hooks_controller_spec.rb
@@ -98,6 +98,7 @@ RSpec.describe 'Integration Hooks API', type: :request do
              headers: agent.create_new_auth_token,
              as: :json
         expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.parsed_body['error']).to eq 'No processor found'
       end
     end
   end
