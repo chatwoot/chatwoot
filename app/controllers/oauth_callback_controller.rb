8bdb1a2ce097d250fc8beae81d279b5fef21728d
Shivam Mishra <scm.mymail@gmail.com>
2024-06-02 19:03:43 +0530

    refactor: move more functions to base controller

--- app/controllers/oauth_callback_controller.rb
+++ app/controllers/oauth_callback_controller.rb
@@ -6,6 +6,7 @@ class OauthCallbackController < ApplicationController
     )
 
     handle_response
+    ::Redis::Alfred.delete(users_data['email'].downcase)
   rescue StandardError => e
     ChatwootExceptionTracker.new(e).capture_exception
     redirect_to '/'
@@ -13,12 +14,61 @@ class OauthCallbackController < ApplicationController
 
   private
 
+  def find_or_create_inbox
+    channel_email = Channel::Email.find_by(email: users_data['email'], account: account)
+    # we need this value to know where to redirect on sucessful processing of the callback
+    channel_exists = channel_email.present?
+
+    channel_email ||= create_channel_with_inbox
+    update_channel(channel_email)
+
+    # reauthorize channel, this code path only triggers when microsoft auth is successful
+    # reauthorized will also update cache keys for the associated inbox
+    channel_email.reauthorized!
+
+    [channel_email.inbox, channel_exists]
+  end
+
   def provider_name
     raise NotImplementedError
   end
 
   def oauth_client
     raise NotImplementedError
+  end
+
+  def update_channel(channel_email)
+    raise NotImplementedError
+  end
+
+  def create_channel_with_inbox
+    ActiveRecord::Base.transaction do
+      channel_email = Channel::Email.create!(email: users_data['email'], account: account)
+      account.inboxes.create!(
+        account: account,
+        channel: channel_email,
+        name: users_data['name'] || fallback_name
+      )
+      channel_email
+    end
+  end
+
+  def users_data
+    decoded_token = JWT.decode parsed_body[:id_token], nil, false
+    decoded_token[0]
+  end
+
+  def account_id
+    ::Redis::Alfred.get(users_data['email'].downcase)
+  end
+
+  def account
+    @account ||= Account.find(account_id)
+  end
+
+  # Fallback name, for when name field is missing from users_data
+  def fallback_name
+    users_data['email'].split('@').first.parameterize.titleize
   end
 
   def oauth_code
