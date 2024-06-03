a7945384e04d9fc8750e7d52ae76dd8b55574482
Shivam Mishra <scm.mymail@gmail.com>
2024-06-03 12:50:58 +0530

    style: no need for assignment
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>

--- app/javascript/dashboard/composables/editor/useSpecialContent.js
+++ app/javascript/dashboard/composables/editor/useSpecialContent.js
@@ -39,7 +39,7 @@ export default function useSpecialContent() {
   const getCannedResponseNode = (editorView, content, from, to, variables) => {
     const updatedMessage = replaceVariablesInMessage({
       message: content,
-      variables: variables,
+      variables,
     });
 
     const node = new MessageMarkdownTransformer(messageSchema).parse(
