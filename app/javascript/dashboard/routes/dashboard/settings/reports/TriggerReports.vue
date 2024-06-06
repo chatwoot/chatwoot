975a88c0ef119e0852b900ee55743171e2b5b01d
Daniel Machado <danielmachadopintos@gmail.com>
2024-06-06 12:11:00 -0300

    feat: add reports translate

--- app/javascript/dashboard/routes/dashboard/settings/reports/TriggerReports.vue
+++ app/javascript/dashboard/routes/dashboard/settings/reports/TriggerReports.vue
@@ -2,7 +2,7 @@
   <div class="flex-1 overflow-auto p-4">
     <report-filters
       type="triggers"
-      :group-by-filter-items-list="groupByfilterItemsList"
+      :group-by-filter-items-list="groupByFilterItemsList"
       @date-range-change="onDateRangeChange"
       @business-hours-toggle="onBusinessHoursToggle"
     />
@@ -10,8 +10,8 @@
       <metric-card
         :is-live="false"
         :is-loading="triggersMetric.isFetchingMetrics"
-        header="Relatório de Disparos"
-        loading-message="carregando..."
+        :header="$t('TRIGGER_REPORTS.SUBTITLE')"
+        :loading-message="$t('REPORT.LOADING_CHART')"
       >
         <div
           v-for="(metric, name, index) in conversationMetrics"
@@ -24,7 +24,7 @@
       </metric-card>
     </div>
     <div class="row">
-      <metric-card header="Gráfico">
+      <metric-card :header="$t('TRIGGER_REPORTS.GRAPH_TITLE')">
         <div class="metric-content column">
           <woot-loading-state
             v-if="triggersMetric.isFetching"
@@ -71,7 +71,7 @@ export default {
       from: Math.floor(new Date().setMonth(new Date().getMonth() - 6) / 1000),
       to: Math.floor(Date.now() / 1000),
       groupBy: GROUP_BY_FILTER[1],
-      groupByfilterItemsList: this.$t('REPORT.GROUP_BY_DAY_OPTIONS'),
+      groupByFilterItemsList: this.$t('REPORT.GROUP_BY_DAY_OPTIONS'),
     };
   },
   computed: {
@@ -81,7 +81,9 @@ export default {
     conversationMetrics() {
       let metric = {};
       Object.keys(this.triggersMetric.metrics || {}).forEach(key => {
-        const metricName = TRIGGERS_METRICS[key]; // TODO: change to translate correctly
+        const metricName = this.$t(
+          'TRIGGER_REPORTS.METRICS.' + TRIGGERS_METRICS[key] + '.NAME'
+        );
         metric[metricName] = this.triggersMetric.metrics[key];
       });
       return metric;
@@ -137,8 +139,6 @@ export default {
       this.fetchAllData();
     },
     onGroupByFilterChange(payload) {
-      // eslint-disable-next-line no-console
-      console.log('onGroupByFilterChange', payload);
       this.groupBy = GROUP_BY_FILTER[payload.id];
       this.fetchAllData();
     },
