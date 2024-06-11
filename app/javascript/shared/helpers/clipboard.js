fe8e58a80b363f9607d5c6c5d1297bf5e1fb93b6
Fayaz Ahmed <fayazara@gmail.com>
2024-06-11 07:12:42 +0530

    fix: Show toast on clipboard errors

--- app/javascript/shared/helpers/clipboard.js
+++ app/javascript/shared/helpers/clipboard.js
@@ -5,10 +5,17 @@
  * @param {string} text text to be written to the clipboard
  * @throws {Error} unable to copy text to clipboard
  */
+import { emitter } from 'shared/helpers/mitt';
+
 export const copyTextToClipboard = async text => {
   try {
     await navigator.clipboard.writeText(text);
   } catch (error) {
+    // TODO: show localized error message
+    emitter.emit(
+      'newToastMessage',
+      'Network Error: Unable to copy text to clipboard.'
+    );
     throw new Error(`Unable to copy text to clipboard: ${error.message}`);
   }
 };
