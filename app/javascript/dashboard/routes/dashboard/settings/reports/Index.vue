9fb9bdccae8adcddbb9f0a50f5419f9363e02387
Jaideep Guntupalli <guntupallijaideep@gmail.com>
2024-06-12 01:12:34 +0530

    temporarily hide the button

--- app/javascript/dashboard/routes/dashboard/settings/reports/Index.vue
+++ app/javascript/dashboard/routes/dashboard/settings/reports/Index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex-1 overflow-auto p-4">
-    <woot-button
+    <!-- <woot-button
       color-scheme="success"
       class-names="button--fixed-top"
       icon="arrow-download"
       @click="downloadConversationReports"
     >
       {{ 'Download conversation reports' }}
-    </woot-button>
+    </woot-button> -->
     <report-filter-selector
       :show-agents-filter="false"
       :show-group-by-filter="true"
