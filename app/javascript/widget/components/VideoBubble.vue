9551d28faf00a3962057f4df926c30f381473e94
iamsivin <iamsivin@gmail.com>
2024-06-13 14:24:24 +0530

    feat: Adds video message viewing in widget bubble

---
+++ app/javascript/widget/components/VideoBubble.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  url: { type: String, default: '' },
+  readableTime: { type: String, default: '' },
+});
+
+const emits = defineEmits(['error']);
+
+const onVideoError = () => {
+  emits('error');
+};
+</script>
+<template>
+  <a
+    :href="url"
+    target="_blank"
+    rel="noreferrer noopener nofollow"
+    class="block"
+  >
+    <div class="flex relative max-w-full">
+      <video
+        class="w-full max-w-[250px] h-auto"
+        :src="url"
+        controls
+        @error="onVideoError"
+      />
+      <span
+        class="text-xs absolute text-white dark:text-white right-3 bottom-1 whitespace-nowrap"
+      >
+        {{ readableTime }}
+      </span>
+    </div>
+  </a>
+</template>
