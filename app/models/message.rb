5b1bb339a5a59b15b0f2519820e06f8999966543
CongWeploy <cong.nguyen@weployapp.com>
2024-06-17 09:20:32 +0700

    fix: unread count for new message

--- app/models/message.rb
+++ app/models/message.rb
@@ -260,6 +260,7 @@ class Message < ApplicationRecord
   def execute_after_create_commit_callbacks
     # rails issue with order of active record callbacks being executed https://github.com/rails/rails/issues/20911
     reopen_conversation
+    update_unread_count
     notify_via_mail
     set_conversation_activity
     update_message_sentiments
@@ -267,6 +268,13 @@ class Message < ApplicationRecord
     send_reply
     execute_message_template_hooks
     update_contact_activity
+  end
+
+  def update_unread_count
+    if incoming? || activity? || private
+      conversation.update(agent_unread_count: conversation.agent_unread_count + 1)
+      conversation.update(assignee_unread_count: conversation.assignee_unread_count + 1)
+    end
   end
 
   def update_contact_activity
