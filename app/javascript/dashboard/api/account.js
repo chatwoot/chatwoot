23da1f6d3bcb071c17d4a6440ffe893f13849582
Daniel Machado <danielmachadopintos@gmail.com>
2024-06-17 22:33:44 -0300

    feat: add account action

--- app/javascript/dashboard/api/account.js
+++ app/javascript/dashboard/api/account.js
@@ -9,6 +9,18 @@ class AccountAPI extends ApiClient {
   createAccount(data) {
     return axios.post(`${this.apiVersion}/accounts`, data);
   }
+
+  getPermissionsByUser(userId) {
+    this.setApiVersion('v2');
+    const url = `${this.apiVersion}/accounts/${this.accountIdFromRoute}/permissions/${userId}`;
+    return axios.get(url);
+  }
+
+  updatePermissionsByUser(userId, data) {
+    this.setApiVersion('v2');
+    const url = `${this.apiVersion}/accounts/${this.accountIdFromRoute}/permissions/${userId}`;
+    return axios.patch(url, data);
+  }
 }
 
 export default new AccountAPI();
