2307a311ba5db313691008bada41b462995db70e
Willian <willianpessoadeabreu@gmail.com>
2024-06-19 10:41:35 -0300

    fix: pequena quebra de layout quando altura tela ~590 px

--- app/javascript/dashboard/components/layout/sidebarComponents/Primary.vue
+++ app/javascript/dashboard/components/layout/sidebarComponents/Primary.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="h-full w-16 bg-white dark:bg-slate-900 border-r border-slate-50 dark:border-slate-800/50 rtl:border-l rtl:border-r-0 flex justify-between flex-col"
+    class="h-full w-16 bg-white dark:bg-slate-900 border-r border-slate-50 dark:border-slate-800/50 rtl:border-l rtl:border-r-0 flex justify-between flex-col pb-4"
   >
     <div class="flex flex-col items-center">
       <logo
@@ -18,7 +18,7 @@
         :is-child-menu-active="menuItem.key === activeMenuItem"
       />
     </div>
-    <div class="flex flex-col items-center justify-end pb-6">
+    <div class="flex flex-col items-center justify-end">
       <primary-nav-item
         v-if="!isACustomBrandedInstance"
         icon="book-open-globe"
