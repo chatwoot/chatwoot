b01ca810456d98a9913a575f442dcaac445bcb6d
Daniel Machado <danielmachadopintos@gmail.com>
2024-06-27 11:07:28 -0300

    feat: add account user

--- app/models/account_user.rb
+++ app/models/account_user.rb
@@ -90,8 +90,7 @@ class AccountUser < ApplicationRecord
     default_permissions = FEATURE_LIST.each_with_object({}) do |feature, hash|
       hash[feature['name']] = feature['enabled']
     end
-    self.permissions = default_permissions
-    save
+    permissions.merge!(default_permissions)
   end
 end
 
