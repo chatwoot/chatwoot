9cb8ac09a0f395f35f2b655d52110b8a9bfc3962
Daniel Machado <danielmachadopintos@gmail.com>
2024-06-27 14:34:32 -0300

    feat: add user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -119,7 +119,7 @@ class User < ApplicationRecord
   end
 
   def assigned_inboxes
-    administrator? || supervisor? ? Current.account.inboxes : inboxes.where(account_id: Current.account.id)
+    administrator? ? Current.account.inboxes : inboxes.where(account_id: Current.account.id)
   end
 
   def serializable_hash(options = nil)
