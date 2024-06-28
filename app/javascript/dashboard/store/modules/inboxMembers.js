eed0749cffe8707e3df34a31c0ad56be8ddfd854
Daniel Machado <danielmachadopintos@gmail.com>
2024-06-28 11:52:53 -0300

    feat: add mutation types

--- app/javascript/dashboard/store/modules/inboxMembers.js
+++ app/javascript/dashboard/store/modules/inboxMembers.js
@@ -1,4 +1,18 @@
+import * as types from '../mutation-types';
 import InboxMembersAPI from '../../api/inboxMembers';
+
+export const state = {
+  records: [],
+  uiFlags: {
+    isUpdating: false,
+  },
+};
+
+export const getters = {
+  getInboxMembers(_state) {
+    return _state.records;
+  },
+};
 
 export const actions = {
   get(_, { inboxId }) {
@@ -6,6 +20,26 @@ export const actions = {
   },
   create(_, { inboxId, agentList }) {
     return InboxMembersAPI.update({ inboxId, agentList });
+  },
+  addInboxesToAgent: async ({ commit }, { inboxIds, userId }) => {
+    commit(types.default.SET_INBOX_MEMBERS_UI_FLAG, { isUpdating: true });
+    try {
+      const response = await InboxMembersAPI.addInboxesToAgent({
+        inboxIds,
+        userId,
+      });
+      commit(types.default.SET_INBOX_MEMBERS, response.data);
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      commit(types.default.SET_INBOX_MEMBERS_UI_FLAG, { isUpdating: false });
+    }
+  },
+};
+
+export const mutations = {
+  [types.default.SET_INBOX_MEMBERS]: ($state, data) => {
+    $state.records = data;
   },
 };
 
