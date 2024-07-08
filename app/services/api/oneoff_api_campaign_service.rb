6a3e35b0476a54b764af83bf50f5ffe8f55cdd79
Henrriky <tectutors.123@gmail.com>
2024-07-08 12:58:08 -0300

    feat(api_campaign): use transaction to guarantee ACID operations when service fail

--- app/services/api/oneoff_api_campaign_service.rb
+++ app/services/api/oneoff_api_campaign_service.rb
@@ -2,23 +2,33 @@ class Api::OneoffApiCampaignService
   pattr_initialize [:campaign!]
 
   def perform
-    raise "Invalid campaign #{campaign.id}" if campaign.inbox.inbox_type != 'API' || !campaign.one_off?
-    raise 'Completed Campaign' if campaign.completed?
+    validate_campaign
+    ActiveRecord::Base.transaction do
+      # marks campaign completed so that other jobs won't pick it up
+      campaign.completed!
 
-    # marks campaign completed so that other jobs won't pick it up
-    campaign.completed!
-
-    # Get id of labels
-    audience_label_ids = campaign.audience.select { |audience| audience['type'] == 'Label' }.pluck('id')
-    # Get all labels that contains label ids
-    audience_labels = campaign.account.labels.where(id: audience_label_ids).pluck(:title)
-    process_audience(audience_labels)
+      # Get id of labels
+      audience_label_ids = campaign.audience.select { |audience| audience['type'] == 'Label' }.pluck('id')
+      # Get all labels that contains label ids
+      audience_labels = campaign.account.labels.where(id: audience_label_ids).pluck(:title)
+      process_audience(audience_labels)
+    end
+  rescue StandardError => e
+    # TODO: Mark campaign as active and exteds schedule time
+    ChatwootExceptionTracker.new(e).capture_exception
+    Rails.logger.error("Error in compaign processing: #{e.message}")
+    raise e
   end
 
   private
 
   delegate :inbox, to: :campaign
   delegate :channel, to: :inbox
+
+  def validate_campaign
+    raise "Invalid campaign #{campaign.id}" if campaign.inbox.inbox_type != 'API' || !campaign.one_off?
+    raise 'Completed Campaign' if campaign.completed?
+  end
 
   def process_audience(audience_labels)
     # get all contacts with labels
@@ -29,8 +39,23 @@ class Api::OneoffApiCampaignService
     end
   end
 
+  def build_contact_inbox(contact:)
+    contact_inbox = ContactInbox.find_or_initialize_by(contact_id: contact.id, inbox_id: campaign.inbox_id)
+
+    return contact_inbox if contact_inbox.persisted?
+
+    contact_created = ContactInboxBuilder.new(
+      contact: contact,
+      inbox: inbox,
+      hmac_verified: false
+    ).perform
+    contact_inbox.id = contact_created.id
+    contact_inbox.reload
+  end
+
   def build_conversation(contact:, contact_inbox:)
     conversation_params = {
+      campaign_id: campaign.id,
       account_id: inbox.account_id,
       inbox_id: campaign.inbox_id,
       contact_id: contact.id,
@@ -52,19 +77,8 @@ class Api::OneoffApiCampaignService
     ::Message.create!(message_params)
   end
 
-  def build_contact_inbox(contact:)
-    contact_inbox = ContactInbox.find_or_initialize_by(contact_id: contact.id, inbox_id: campaign.inbox_id)
-
-    return contact_inbox if contact_inbox.persisted?
-
-    ContactInboxBuilder.new(
-      contact: contact,
-      inbox: inbox,
-      hmac_verified: false
-    ).perform
-    contact_inbox.reload
-  end
-
+  # This will create a message and trigger dispatch_create_events callback
+  # which will call a Webhook Job that will create message in Inbox API
   def send_message(contact:, content:)
     contact_inbox = build_contact_inbox(contact: contact)
     conversation = build_conversation(contact: contact, contact_inbox: contact_inbox)
