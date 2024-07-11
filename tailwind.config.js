9e190e1f5d5bc05025bc44997a6d6e0fcbbbef91
iamsivin <iamsivin@gmail.com>
2024-07-11 13:22:22 +0530

    Fix: Show add/import/export buttons in mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,14 @@ module.exports = {
         inter: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     fontSize: {
       ...defaultTheme.fontSize,
       xxs: '0.625rem',
