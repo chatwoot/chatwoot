cc56416ee71fa48eae0b4009c304a154beef329a
Daniel Machado <danielmachadopintos@gmail.com>
2024-07-17 15:37:52 -0300

    feat: add reports js api

--- app/javascript/dashboard/api/reports.js
+++ app/javascript/dashboard/api/reports.js
@@ -16,6 +16,7 @@ class ReportsAPI extends ApiClient {
     id,
     groupBy,
     businessHours,
+    agents,
   }) {
     return axios.get(`${this.url}`, {
       params: {
@@ -27,6 +28,7 @@ class ReportsAPI extends ApiClient {
         group_by: groupBy,
         business_hours: businessHours,
         timezone_offset: getTimeOffset(),
+        agent_ids: agents,
       },
     });
   }
