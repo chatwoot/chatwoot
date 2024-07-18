ad6001689fc76e9baae548f3bc6c2f4076d1da31
Daniel Machado <danielmachadopintos@gmail.com>
2024-07-17 22:37:03 -0300

    feat: add app config controller

--- app/controllers/super_admin/app_configs_controller.rb
+++ app/controllers/super_admin/app_configs_controller.rb
@@ -1,6 +1,9 @@
+require 'mini_magick'
+
 class SuperAdmin::AppConfigsController < SuperAdmin::ApplicationController
   before_action :set_config
   before_action :allowed_configs
+
   def show
     # ref: https://github.com/rubocop/rubocop/issues/7767
     # rubocop:disable Style/HashTransformValues
@@ -15,13 +18,14 @@ class SuperAdmin::AppConfigsController < SuperAdmin::ApplicationController
     params['app_config'].each do |key, value|
       next unless @allowed_configs.include?(key)
 
+      handle_favicon(value) if key == 'FAVICON'
+      handle_favicon_badge(value) if key == 'FAVICON_BADGE'
+
       i = InstallationConfig.where(name: key).first_or_create(value: value, locked: false)
       i.value = value
       i.save!
     end
-    # rubocop:disable Rails/I18nLocaleTexts
-    redirect_to super_admin_settings_path, notice: 'App Configs updated successfully'
-    # rubocop:enable Rails/I18nLocaleTexts
+    redirect_to super_admin_settings_path, notice: I18n.t('global_config.update_success')
   end
 
   private
@@ -31,7 +35,33 @@ class SuperAdmin::AppConfigsController < SuperAdmin::ApplicationController
   end
 
   def allowed_configs
-    @allowed_configs = %w[FB_APP_ID FB_VERIFY_TOKEN FB_APP_SECRET]
+    @allowed_configs = %w[FB_APP_ID FB_VERIFY_TOKEN FB_APP_SECRET FAVICON FAVICON_BADGE]
+  end
+
+  def handle_favicon(favicon_url)
+    sizes = [16, 32, 96, 512]
+    convert_and_save_favicon(favicon_url, sizes)
+  end
+
+  def handle_favicon_badge(favicon_url)
+    badge_sizes = [16, 32, 96]
+    convert_and_save_favicon(favicon_url, badge_sizes, badge: true)
+  end
+
+  def convert_and_save_favicon(favicon_url, sizes, badge: false)
+    sizes.each do |size|
+      save_favicon(favicon_url, size, badge)
+    end
+  rescue StandardError => e
+    Rails.logger.error "Failed to convert and save favicon: #{e.message}"
+  end
+
+  def save_favicon(favicon_url, size, badge: false)
+    image = MiniMagick::Image.open(favicon_url)
+    image.resize "#{size}x#{size}"
+    suffix = badge ? '-badge' : ''
+    favicon_path = Rails.public_path.join("favicon#{suffix}-#{size}x#{size}.png")
+    image.write favicon_path
   end
 end
 
