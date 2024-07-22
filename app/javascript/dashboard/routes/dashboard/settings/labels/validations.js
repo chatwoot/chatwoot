20304008f1d9de584b472c918e4013d715ff40f7
iamsivin <iamsivin@gmail.com>
2024-07-22 21:10:03 +0530

    feat: Rewrite `labels/validationMixin mixin` to a helper

--- app/javascript/dashboard/routes/dashboard/settings/labels/validations.js
+++ app/javascript/dashboard/routes/dashboard/settings/labels/validations.js
@@ -2,6 +2,20 @@ import { required, minLength } from 'vuelidate/lib/validators';
 
 export const validLabelCharacters = (str = '') => !!str && !str.includes(' ');
 
+export const getLabelTitleErrorMessage = validation => {
+  let errorMessage = '';
+  if (!validation.title.$error) {
+    errorMessage = '';
+  } else if (!validation.title.required) {
+    errorMessage = 'LABEL_MGMT.FORM.NAME.REQUIRED_ERROR';
+  } else if (!validation.title.minLength) {
+    errorMessage = 'LABEL_MGMT.FORM.NAME.MINIMUM_LENGTH_ERROR';
+  } else if (!validation.title.validLabelCharacters) {
+    errorMessage = 'LABEL_MGMT.FORM.NAME.VALID_ERROR';
+  }
+  return errorMessage;
+};
+
 export default {
   title: {
     required,
