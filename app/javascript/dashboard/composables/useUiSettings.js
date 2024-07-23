c2676cce8ae2bf86ef6cf39ac3f56ae5d1111ed0
iamsivin <iamsivin@gmail.com>
2024-07-23 12:57:45 +0530

    chore: Fix code climate

--- app/javascript/dashboard/composables/useUiSettings.js
+++ app/javascript/dashboard/composables/useUiSettings.js
@@ -1,5 +1,5 @@
+import { useStore, useStoreGetters } from 'dashboard/composables/store';
 import { computed } from 'vue';
-import { useStoreGetters, useStore } from 'dashboard/composables/store';
 
 export const DEFAULT_CONVERSATION_SIDEBAR_ITEMS_ORDER = [
   { name: 'conversation_actions' },
@@ -15,37 +15,22 @@ export const DEFAULT_CONTACT_SIDEBAR_ITEMS_ORDER = [
   { name: 'previous_conversation' },
 ];
 
+/**
+ * Slugifies the channel name.
+ * Replaces spaces, hyphens, and double colons with underscores.
+ * @param {string} name - The channel name to slugify.
+ * @returns {string} The slugified channel name.
+ */
 const slugifyChannel = name =>
   name?.toLowerCase().replace(' ', '_').replace('-', '_').replace('::', '_');
 
 /**
- * Composable to manage UI settings related to user interface elements in the dashboard.
- * @returns {Object} An object containing reactive properties and methods for UI settings management.
+ * Computes the order of items in the conversation sidebar, using defaults if not present.
+ * @param {Object} uiSettings - Reactive UI settings object.
+ * @returns {Array} Ordered list of sidebar items.
  */
-export function useUiSettings() {
-  const getters = useStoreGetters();
-  const store = useStore();
-
-  const uiSettings = computed(() => getters.getUISettings.value);
-
-  /**
-   * Updates UI settings in the Vuex store.
-   * @param {Object} settings - New settings to merge with existing settings.
-   */
-  const updateUISettings = (settings = {}) => {
-    store.dispatch('updateUISettings', {
-      uiSettings: {
-        ...uiSettings.value,
-        ...settings,
-      },
-    });
-  };
-
-  /**
-   * Computes the order of items in the conversation sidebar, adding default items if not present.
-   * @returns {Array} Ordered list of sidebar items.
-   */
-  const conversationSidebarItemsOrder = computed(() => {
+const useConversationSidebarItemsOrder = uiSettings => {
+  return computed(() => {
     const { conversation_sidebar_items_order: itemsOrder } = uiSettings.value;
     // If the sidebar order is not set, use the default order.
     if (!itemsOrder) {
@@ -59,85 +44,103 @@ export function useUiSettings() {
     });
     return itemsOrder;
   });
+};
 
-  /**
-   * Computes the order of items in the contact sidebar, using defaults if not set.
-   * @returns {Array} Ordered list of sidebar items.
-   */
-  const contactSidebarItemsOrder = computed(() => {
+/**
+ * Computes the order of items in the contact sidebar,using defaults if not present.
+ * @param {Object} uiSettings - Reactive UI settings object.
+ * @returns {Array} Ordered list of sidebar items.
+ */
+const useContactSidebarItemsOrder = uiSettings => {
+  return computed(() => {
     const { contact_sidebar_items_order: itemsOrder } = uiSettings.value;
     return itemsOrder || DEFAULT_CONTACT_SIDEBAR_ITEMS_ORDER;
   });
+};
 
-  /**
-   * Determines if a specific sidebar item is open based on its key.
-   * @param {string} key - The key of the sidebar item.
-   * @returns {boolean} True if the item is open, otherwise false.
-   */
-  const isContactSidebarItemOpen = key => {
-    return !!uiSettings.value[key];
-  };
+/**
+ * Toggles the open state of a sidebar item.
+ * @param {string} key - The key of the sidebar item to toggle.
+ * @param {Object} uiSettings - Reactive UI settings object.
+ * @param {Function} updateUISettings - Function to update UI settings.
+ */
+const toggleSidebarUIState = (key, uiSettings, updateUISettings) => {
+  updateUISettings({ [key]: !uiSettings.value[key] });
+};
 
-  /**
-   * Toggles the open state of a sidebar item.
-   * @param {string} key - The key of the sidebar item to toggle.
-   */
-  const toggleSidebarUIState = key => {
-    updateUISettings({ [key]: !isContactSidebarItemOpen(key) });
-  };
+/**
+ * Sets the signature flag for a specific channel type in the inbox settings.
+ * @param {string} channelType - The type of the channel.
+ * @param {boolean} value - The value to set for the signature enabled flag.
+ * @param {Function} updateUISettings - Function to update UI settings.
+ */
+const setSignatureFlagForInbox = (channelType, value, updateUISettings) => {
+  if (!channelType) return;
 
-  /**
-   * Sets the signature flag for a specific channel type in the inbox settings.
-   * @param {string} channelType - The type of the channel.
-   * @param {boolean} value - The value to set for the signature enabled flag.
-   */
-  const setSignatureFlagForInbox = (channelType, value) => {
-    if (!channelType) return;
+  const slugifiedChannel = slugifyChannel(channelType);
+  updateUISettings({ [`${slugifiedChannel}_signature_enabled`]: value });
+};
 
-    const slugifiedChannel = slugifyChannel(channelType);
-    updateUISettings({ [`${slugifiedChannel}_signature_enabled`]: value });
-  };
+/**
+ * Fetches the signature flag for a specific channel type from UI settings.
+ * @param {string} channelType - The type of the channel.
+ * @param {Object} uiSettings - Reactive UI settings object.
+ * @returns {boolean} The value of the signature enabled flag.
+ */
+const fetchSignatureFlagFromUiSettings = (channelType, uiSettings) => {
+  if (!channelType) return false;
 
-  /**
-   * Fetches the signature flag for a specific channel type from UI settings.
-   * @param {string} channelType - The type of the channel.
-   * @returns {boolean} The value of the signature enabled flag.
-   */
-  const fetchSignatureFlagFromUiSettings = channelType => {
-    if (!channelType) return false;
+  const slugifiedChannel = slugifyChannel(channelType);
+  return uiSettings.value[`${slugifiedChannel}_signature_enabled`];
+};
 
-    const slugifiedChannel = slugifyChannel(channelType);
-    return uiSettings.value[`${slugifiedChannel}_signature_enabled`];
-  };
+/**
+ * Checks if a specific editor hotkey is enabled.
+ * @param {string} key - The key to check.
+ * @param {Object} uiSettings - Reactive UI settings object.
+ * @returns {boolean} True if the hotkey is enabled, otherwise false.
+ */
+const isEditorHotKeyEnabled = (key, uiSettings) => {
+  const {
+    editor_message_key: editorMessageKey,
+    enter_to_send_enabled: enterToSendEnabled,
+  } = uiSettings.value || {};
+  if (!editorMessageKey) {
+    return key === (enterToSendEnabled ? 'enter' : 'cmd_enter');
+  }
+  return editorMessageKey === key;
+};
 
-  /**
-   * Checks if a specific editor hotkey is enabled.
-   * @param {string} key - The key to check.
-   * @returns {boolean} True if the hotkey is enabled, otherwise false.
-   */
-  const isEditorHotKeyEnabled = key => {
-    const {
-      editor_message_key: editorMessageKey,
-      enter_to_send_enabled: enterToSendEnabled,
-    } = uiSettings.value || {};
-    if (!editorMessageKey) {
-      if (enterToSendEnabled) {
-        return key === 'enter';
-      }
-      return key === 'cmd_enter';
-    }
-    return editorMessageKey === key;
+/**
+ * Main composable function for managing UI settings.
+ * @returns {Object} An object containing reactive properties and methods for UI settings management.
+ */
+export function useUiSettings() {
+  const getters = useStoreGetters();
+  const store = useStore();
+  const uiSettings = computed(() => getters.getUISettings.value);
+
+  const updateUISettings = (settings = {}) => {
+    store.dispatch('updateUISettings', {
+      uiSettings: {
+        ...uiSettings.value,
+        ...settings,
+      },
+    });
   };
 
   return {
     uiSettings,
     updateUISettings,
-    conversationSidebarItemsOrder,
-    contactSidebarItemsOrder,
-    isContactSidebarItemOpen,
-    toggleSidebarUIState,
-    setSignatureFlagForInbox,
-    fetchSignatureFlagFromUiSettings,
-    isEditorHotKeyEnabled,
+    conversationSidebarItemsOrder: useConversationSidebarItemsOrder(uiSettings),
+    contactSidebarItemsOrder: useContactSidebarItemsOrder(uiSettings),
+    isContactSidebarItemOpen: key => !!uiSettings.value[key],
+    toggleSidebarUIState: key =>
+      toggleSidebarUIState(key, uiSettings, updateUISettings),
+    setSignatureFlagForInbox: (channelType, value) =>
+      setSignatureFlagForInbox(channelType, value, updateUISettings),
+    fetchSignatureFlagFromUiSettings: channelType =>
+      fetchSignatureFlagFromUiSettings(channelType, uiSettings),
+    isEditorHotKeyEnabled: key => isEditorHotKeyEnabled(key, uiSettings),
   };
 }
