54d2188b76a4154658b5678813886408fec914a9
iamsivin <iamsivin@gmail.com>
2024-07-24 10:44:06 +0530

    fix: Code climate

--- app/javascript/dashboard/composables/useKeyboardEvents.js
+++ app/javascript/dashboard/composables/useKeyboardEvents.js
@@ -5,78 +5,80 @@ import {
 } from 'shared/helpers/KeyboardHelpers';
 import { createKeybindingsHandler } from 'tinykeys';
 
+const addKeydownHandler = (keydownHandler, elRef, taggedHandlers) => {
+  const root = elRef?.value;
+  document.addEventListener('keydown', keydownHandler);
+  taggedHandlers.set(root, keydownHandler);
+};
+
+// Helper function to determine if the event should be ignored based on the type of element and handler settings
+const shouldIgnoreEvent = (e, handler) => {
+  const isTypeable = isActiveElementTypeable(e);
+  const allowOnFocusedInput =
+    typeof handler === 'function' ? false : handler.allowOnFocusedInput;
+
+  if (isTypeable) {
+    if (isEscape(e)) {
+      e.target.blur();
+    }
+    return !allowOnFocusedInput;
+  }
+  return false;
+};
+
+const keydownWrapper = handler => {
+  return e => {
+    if (shouldIgnoreEvent(e, handler)) return;
+    //  extract the action to perform from the handler
+
+    const actionToPerform =
+      typeof handler === 'function' ? handler : handler.action;
+    actionToPerform(e);
+  };
+};
+
+const wrapEventsInKeybindingsHandler = events => {
+  const wrappedEvents = {};
+  Object.keys(events).forEach(eventName => {
+    wrappedEvents[eventName] = keydownWrapper(events[eventName]);
+  });
+  return wrappedEvents;
+};
+
+const setupListeners = (elRef, getKeyboardEvents, addEventHandler) => {
+  const events = getKeyboardEvents();
+  const root = elRef?.value;
+  if (!(root instanceof Element)) {
+    return;
+  }
+  if (events) {
+    const wrappedEvents = wrapEventsInKeybindingsHandler(events);
+    const keydownHandler = createKeybindingsHandler(wrappedEvents);
+    addEventHandler(keydownHandler);
+  }
+};
+
+const removeListeners = (elRef, taggedHandlers) => {
+  const root = elRef?.value;
+  if (root instanceof Element && taggedHandlers.has(root)) {
+    const handlerToRemove = taggedHandlers.get(root);
+    document.removeEventListener('keydown', handlerToRemove);
+  }
+};
+
 export function useKeyboardEvents(keyboardEvents, elRef = null) {
   // this is a store that stores the handler globally, and only gets reset on reload
   const taggedHandlers = new WeakMap();
 
   const getKeyboardEvents = () => keyboardEvents || null;
-
-  const addEventHandler = keydownHandler => {
-    const root = elRef?.value;
-    document.addEventListener('keydown', keydownHandler);
-    taggedHandlers.set(root, keydownHandler);
-  };
-
-  // Helper function to determine if the event should be ignored based on the type of element and handler settings
-  const shouldIgnoreEvent = (e, handler) => {
-    const isTypeable = isActiveElementTypeable(e);
-    const allowOnFocusedInput =
-      typeof handler === 'function' ? false : handler.allowOnFocusedInput;
-
-    if (isTypeable) {
-      if (isEscape(e)) {
-        e.target.blur();
-      }
-      return !allowOnFocusedInput;
-    }
-    return false;
-  };
-
-  const getActionToPerform = handler =>
-    typeof handler === 'function' ? handler : handler.action;
-
-  const keydownWrapper = handler => e => {
-    if (shouldIgnoreEvent(e, handler)) return;
-
-    const actionToPerform = getActionToPerform(handler);
-    actionToPerform(e);
-  };
-
-  const wrapEventsInKeybindingsHandler = events => {
-    const wrappedEvents = {};
-    Object.keys(events).forEach(eventName => {
-      wrappedEvents[eventName] = keydownWrapper(events[eventName]);
-    });
-    return wrappedEvents;
-  };
-
-  const setupEventListeners = () => {
-    const events = getKeyboardEvents();
-    const root = elRef?.value;
-    if (!(root instanceof Element)) {
-      //   console.error('Invalid element reference');
-      return;
-    }
-
-    if (events) {
-      const wrappedEvents = wrapEventsInKeybindingsHandler(events);
-      const keydownHandler = createKeybindingsHandler(wrappedEvents);
-      addEventHandler(keydownHandler);
-    }
-  };
-
-  const removeEventListeners = () => {
-    const root = elRef?.value;
-    if (root instanceof Element && taggedHandlers.has(root)) {
-      const handlerToRemove = taggedHandlers.get(root);
-      document.removeEventListener('keydown', handlerToRemove);
-    }
-  };
+  const addEventHandler = keydownHandler =>
+    addKeydownHandler(keydownHandler, elRef, taggedHandlers);
+  const setupEventListeners = () =>
+    setupListeners(elRef, getKeyboardEvents, addEventHandler);
+  const removeEventListeners = () => removeListeners(elRef, taggedHandlers);
 
   onMounted(setupEventListeners);
   onBeforeUnmount(removeEventListeners);
 
-  return {
-    getKeyboardEvents,
-  };
+  return { getKeyboardEvents };
 }
