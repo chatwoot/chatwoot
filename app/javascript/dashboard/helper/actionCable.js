63b9b9fcc1a012adbf92b63fc1f9ee79c0fc335f
Shivam Mishra <scm.mymail@gmail.com>
2024-07-26 14:08:53 +0530

    feat: add console.warn for action cable ignore

--- app/javascript/dashboard/helper/actionCable.js
+++ app/javascript/dashboard/helper/actionCable.js
@@ -73,7 +73,11 @@ class ActionCableConnector extends BaseActionCableConnector {
     const { id } = payload;
     if (id) {
       this.app.$store.dispatch('updateConversation', payload);
+    } else {
+      // eslint-disable-next-line no-console
+      console.warn('Skipping assignee change event with payload: ', payload);
     }
+
     this.fetchConversationStats();
   };
 
