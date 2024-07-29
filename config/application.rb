dd47b00af037bc92262abeb515f97fa2aa52853e
sfhmi <42550917+sfhmi@users.noreply.github.com>
2024-07-28 21:41:23 -0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require 'dotenv-rails'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
