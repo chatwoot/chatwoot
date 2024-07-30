92c9fed3073b179666c8e334c8f28b6562d2edda
Daniel Machado <danielmachadopintos@gmail.com>
2024-07-29 21:56:43 -0300

    feat: add testing

---
+++ spec/factories/tickets.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :ticket do
+    title { "MyString" }
+    description { "MyText" }
+    status { 1 }
+    resolved_at { "2024-07-30 00:53:06" }
+    assigned_to { "" }
+    user { nil }
+    conversation { nil }
+  end
+end
