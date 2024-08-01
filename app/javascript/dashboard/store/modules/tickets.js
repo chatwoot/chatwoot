0dbc1459fb2ca2a7b135b82ae076f0bdb3ea6efb
Daniel Machado <danielmachadopintos@gmail.com>
2024-07-31 21:46:59 -0300

    feat: add stores tickets

---
+++ app/javascript/dashboard/store/modules/tickets.js
@@ -0,0 +1,104 @@
+import Vue from 'vue';
+import * as types from '../mutation-types';
+import TicketsAPI from '../../api/tickets';
+
+const state = {
+  record: [],
+  uiFlags: {
+    isCreating: false,
+    isFetching: false,
+    isUpdating: false,
+  },
+};
+
+export const getters = {
+  getUIFlags($state) {
+    return $state.uiFlags;
+  },
+  getTicketLabels($state) {
+    return $state.record;
+  },
+};
+
+export const actions = {
+  get: async ({ commit }) => {
+    commit(types.default.SET_TICKETS_UI_FLAG, { isFetching: true });
+    try {
+      const response = await TicketsAPI.get();
+      commit(types.default.SET_TICKETS, response.data);
+      commit(types.default.SET_TICKETS_UI_FLAG, {
+        isFetching: false,
+      });
+    } catch (error) {
+      commit(types.default.SET_TICKETS_UI_FLAG, {
+        isFetching: false,
+      });
+    }
+  },
+  create: async (
+    { commit },
+    { title, description, status, assigneeId, conversationId }
+  ) => {
+    commit(types.default.SET_TICKETS_UI_FLAG, { isCreating: true });
+    // eslint-disable-next-line no-console
+    console.log(conversationId);
+    try {
+      const response = await TicketsAPI.create({
+        title,
+        description,
+        status,
+        assignee_id: assigneeId,
+        conversation_id: conversationId,
+      });
+      commit(types.default.SET_TICKETS, response.data);
+      commit(types.default.SET_TICKETS_UI_FLAG, {
+        isCreating: false,
+      });
+    } catch (error) {
+      commit(types.default.SET_TICKETS_UI_FLAG, {
+        isCreating: false,
+      });
+      throw error;
+    }
+  },
+  update: async ({ commit }, { selectedEmailFlags, selectedPushFlags }) => {
+    commit(types.default.SET_TICKETS_UI_FLAG, { isUpdating: true });
+    try {
+      const response = await TicketsAPI.update({
+        notification_settings: {
+          selected_email_flags: selectedEmailFlags,
+          selected_push_flags: selectedPushFlags,
+        },
+      });
+      commit(types.default.SET_TICKETS, response.data);
+      commit(types.default.SET_TICKETS_UI_FLAG, {
+        isUpdating: false,
+      });
+    } catch (error) {
+      commit(types.default.SET_TICKETS_UI_FLAG, {
+        isUpdating: false,
+      });
+      throw error;
+    }
+  },
+};
+
+export const mutations = {
+  [types.default.SET_TICKETS_UI_FLAG]($state, data) {
+    $state.uiFlags = {
+      ...$state.uiFlags,
+      ...data,
+    };
+  },
+  [types.default.SET_TICKETS]: ($state, data) => {
+    Vue.set($state, 'record', data);
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
