61d08393a7b684d376138c09d88b6b01a47d4b77
Sojan <sojan@pepalo.com>
2024-08-06 15:02:08 -0700

    chore: Codeclimate fix 1

--- enterprise/app/jobs/enterprise/account/conversations_resolution_scheduler_job.rb
+++ enterprise/app/jobs/enterprise/account/conversations_resolution_scheduler_job.rb
@@ -3,14 +3,24 @@ module Enterprise::Account::ConversationsResolutionSchedulerJob
     super
 
     # TODO: remove this when response bot is remove in favor of captain
+    resolve_response_bot_conversations
+
+    # This is responsible for resolving captain conversations
+    resolve_captain_conversations
+  end
+
+  private
+
+  def resolve_response_bot_conversations
     # This is responsible for resolving response bot conversations
     Account.feature_response_bot.all.find_each(batch_size: 100) do |account|
       account.inboxes.each do |inbox|
         ResponseBot::InboxPendingConversationsResolutionJob.perform_later(inbox) if inbox.response_bot_enabled?
       end
     end
+  end
 
-    # This is responsible for resolving captain conversations
+  def resolve_captain_conversations
     Integrations::Hook.where(app_id: 'captain').all.find_each(batch_size: 100) do |hook|
       next unless hook.enabled?
 
