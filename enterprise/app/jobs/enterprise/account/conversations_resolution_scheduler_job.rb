b7ea28cede9e534f9a695717396e794c339dd8c8
Sojan <sojan@pepalo.com>
2024-08-06 15:05:08 -0700

    chore: codecliamte fix 2

--- enterprise/app/jobs/enterprise/account/conversations_resolution_scheduler_job.rb
+++ enterprise/app/jobs/enterprise/account/conversations_resolution_scheduler_job.rb
@@ -15,7 +15,7 @@ module Enterprise::Account::ConversationsResolutionSchedulerJob
     # This is responsible for resolving response bot conversations
     Account.feature_response_bot.all.find_each(batch_size: 100) do |account|
       account.inboxes.each do |inbox|
-        ResponseBot::InboxPendingConversationsResolutionJob.perform_later(inbox) if inbox.response_bot_enabled?
+        Captain::InboxPendingConversationsResolutionJob.perform_later(inbox) if inbox.response_bot_enabled?
       end
     end
   end
