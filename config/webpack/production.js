aeaba19a6b458d80efdc3d67b2f4857335e7d765
KongNC <nccong207@gmail.com>
2024-08-07 16:00:30 +0700

    feat: add sentry sourcemap

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,17 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
-const environment = require('./environment')
+const environment = require('./environment');
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 
-module.exports = environment.toWebpackConfig()
+module.exports = {
+  ...environment.toWebpackConfig(),
+  devtool: 'source-map',
+  plugins: [
+    ...environment.toWebpackConfig().plugins,
+    sentryWebpackPlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: 'hoatieu-crm',
+      project: 'hoatieu-crm',
+    }),
+  ],
+};
