a2eb6348a44b0e5c163c78d7a1ac5bfb9c552bad
Daniel Machado <danielmachadopintos@gmail.com>
2024-08-06 23:21:10 -0300

    feat: add wrapper

--- app/javascript/dashboard/routes/dashboard/tickets/TicketView.vue
+++ app/javascript/dashboard/routes/dashboard/tickets/TicketView.vue
@@ -2,23 +2,6 @@
   <section class="ticket-page bg-white dark:bg-slate-900">
     <div class="flex flex-row w-full">
       <div class="flex-col w-full" :class="{ 'w-[70%]': !!ticket }">
-        <div
-          class="flex items-center justify-between py-3 border-b border-slate-75 dark:border-slate-700"
-        >
-          <div class="flex max-w-[85%] justify-center items-center">
-            <h1
-              class="text-xl break-words overflow-hidden whitespace-nowrap text-ellipsis text-black-900 dark:text-slate-100 mb-0"
-              title="Tickets"
-            >
-              {{ $t('TICKETS.TITLE') }}
-            </h1>
-            <span
-              class="p-1 my-0.5 mx-1 rounded-md capitalize bg-slate-50 dark:bg-slate-800 text-xxs text-slate-600 dark:text-slate-300"
-            >
-              {{ $t(`TICKETS.STATUS.OPEN`) }}
-            </span>
-          </div>
-        </div>
         <div class="flex px-3">
           <ticket-type-tabs
             :tabs="assigneeTabItems"
--- app/javascript/dashboard/routes/dashboard/tickets/ticket.routes.js
+++ app/javascript/dashboard/routes/dashboard/tickets/ticket.routes.js
@@ -2,13 +2,25 @@
 import { AllRoles } from '../../../featureFlags';
 import { frontendURL } from '../../../helper/URLHelper';
 
+const SettingsContent = () =>
+  import('dashboard/routes/dashboard/settings/Wrapper.vue');
 const TicketView = () => import('./TicketView.vue');
 
 export const routes = [
   {
     path: frontendURL('accounts/:accountId/tickets'),
-    name: 'tickets_dashboard',
-    roles: AllRoles,
-    component: TicketView,
+    component: SettingsContent,
+    props: {
+      headerTitle: 'TICKETS.TITLE',
+      icon: 'arrow-swap',
+    },
+    children: [
+      {
+        path: '',
+        name: 'tickets_dashboard',
+        roles: AllRoles,
+        component: TicketView,
+      },
+    ],
   },
 ];
