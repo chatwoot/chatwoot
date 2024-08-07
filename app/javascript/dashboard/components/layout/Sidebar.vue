5218ed20a3454d200b4b60606d9a1dc383f47047
KongNC <nccong207@gmail.com>
2024-08-07 23:20:37 +0700

    fix: fetch conversation unread stats

--- app/javascript/dashboard/components/layout/Sidebar.vue
+++ app/javascript/dashboard/components/layout/Sidebar.vue
@@ -195,7 +195,9 @@ export default {
     this.$store.dispatch('teams/get');
     this.$store.dispatch('attributes/get');
     this.fetchCustomViews();
-    bus.$on('fetch_conversation_unread_stats', this.fetchUnreadStats);
+    if (this.activeSecondaryMenu.parentNav === 'conversations') {
+      bus.$on('fetch_conversation_unread_stats', this.fetchUnreadStats);
+    }
   },
 
   methods: {
