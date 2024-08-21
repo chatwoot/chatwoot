9aef7a75b4c66655517b8b5f74dfe57ee78fb982
Pranav <pranav@chatwoot.com>
2024-08-21 18:27:53 +0530

    feat: Update the design for macros design page (#9999)
    This is the continuation of the design update for settings page. This PR updates the design for the macros page.
    
    Co-authored-by: Sivin Varghese <64252451+iamsivin@users.noreply.github.com>

--- app/javascript/dashboard/routes/dashboard/settings/macros/macros.routes.js
+++ app/javascript/dashboard/routes/dashboard/settings/macros/macros.routes.js
@@ -1,6 +1,7 @@
 import { frontendURL } from 'dashboard/helper/URLHelper';
 
 const SettingsContent = () => import('../Wrapper.vue');
+const SettingsWrapper = () => import('../SettingsWrapper.vue');
 const Macros = () => import('./Index.vue');
 const MacroEditor = () => import('./MacroEditor.vue');
 
@@ -8,16 +9,7 @@ export default {
   routes: [
     {
       path: frontendURL('accounts/:accountId/settings/macros'),
-      component: SettingsContent,
-      props: params => {
-        const showBackButton = params.name !== 'macros_wrapper';
-        return {
-          headerTitle: 'MACROS.HEADER',
-          headerButtonText: 'MACROS.HEADER_BTN_TXT',
-          icon: 'flash-settings',
-          showBackButton,
-        };
-      },
+      component: SettingsWrapper,
       children: [
         {
           path: '',
@@ -27,17 +19,30 @@ export default {
             permissions: ['administrator', 'agent'],
           },
         },
+      ],
+    },
+    {
+      path: frontendURL('accounts/:accountId/settings/macros'),
+      component: SettingsContent,
+      props: () => {
+        return {
+          headerTitle: 'MACROS.HEADER',
+          icon: 'flash-settings',
+          showBackButton: true,
+        };
+      },
+      children: [
         {
-          path: 'new',
-          name: 'macros_new',
+          path: ':macroId/edit',
+          name: 'macros_edit',
           component: MacroEditor,
           meta: {
             permissions: ['administrator', 'agent'],
           },
         },
         {
-          path: ':macroId/edit',
-          name: 'macros_edit',
+          path: 'new',
+          name: 'macros_new',
           component: MacroEditor,
           meta: {
             permissions: ['administrator', 'agent'],
