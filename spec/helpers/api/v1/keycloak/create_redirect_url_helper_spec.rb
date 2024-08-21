e14f115abf5e242b0698abab0ce5bf24012d7938
Rishabh Pandram <rishabh@onehash.ai>
2024-08-21 18:08:39 +0530

    browser fix

---
+++ spec/helpers/api/v1/keycloak/create_redirect_url_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the Api::V1::Keycloak::CreateRedirectUrlHelper. For example:
+#
+# describe Api::V1::Keycloak::CreateRedirectUrlHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe Api::V1::Keycloak::CreateRedirectUrlHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
