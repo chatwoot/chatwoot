dbff7dbceab1af632cfb591f07be2f35d6dbc5d3
mandythcuyen <mandythcuyen@gmail.com>
2024-09-02 01:50:09 +0700

    Trigger some events matching with configurations on gg_ads and gg_tag_manager

--- app/javascript/widget/constants/sdkEvents.js
+++ app/javascript/widget/constants/sdkEvents.js
@@ -1,4 +1,7 @@
-export const CHATWOOT_ERROR = 'chatwoot:error';
-export const CHATWOOT_ON_MESSAGE = 'chatwoot:on-message';
-export const CHATWOOT_ON_START_CONVERSATION = 'chatwoot:on-start-conversation';
 export const CHATWOOT_READY = 'chatwoot:ready';
+export const CHATWOOT_ERROR = 'chatwoot:error';
+export const CHATWOOT_OPEN_DIALOG = 'chatwoot:open-dialog';
+export const CHATWOOT_ON_START_CONVERSATION = 'chatwoot:on-start-conversation';
+export const CHATWOOT_ON_SUBMIT_PRE_CHAT_FORM =
+  'chatwoot:on-submit-pre-chat-form';
+export const CHATWOOT_ON_MESSAGE = 'chatwoot:on-message';
