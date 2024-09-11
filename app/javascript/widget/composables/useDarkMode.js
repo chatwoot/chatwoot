2ed26940a975a7808a97629137bad1cf9f049337
Fayaz Ahmed <fayazara@gmail.com>
2024-09-11 15:53:29 +0530

    Reduce complexity

--- app/javascript/widget/composables/useDarkMode.js
+++ app/javascript/widget/composables/useDarkMode.js
@@ -1,27 +1,34 @@
 import { computed } from 'vue';
 import { useMapGetter } from 'dashboard/composables/store';
 
+// Helper functions to improve readability and reduce complexity
+const isDarkModeAuto = mode => mode === 'auto';
+const isDarkMode = mode => mode === 'dark';
+
 /**
  * Composable for handling dark mode.
  * @returns {Object} An object containing computed properties and methods for dark mode.
  */
-
 export function useDarkMode() {
   const darkMode = useMapGetter('appConfig/darkMode');
+
   const memoizedMatchMedia = computed(
     () => window.matchMedia?.('(prefers-color-scheme: dark)').matches ?? false
   );
-  const prefersDarkMode = computed(
-    () =>
-      (darkMode.value === 'auto' && memoizedMatchMedia.value) ||
-      darkMode.value === 'dark'
+
+  const prefersDarkMode = computed(() =>
+    isDarkModeAuto(darkMode.value)
+      ? memoizedMatchMedia.value
+      : isDarkMode(darkMode.value)
   );
-  const getThemeClass = (light, dark) =>
-    darkMode.value === 'auto'
-      ? `${light} ${dark}`
-      : darkMode.value === 'dark'
-      ? dark
-      : light;
+
+  const getThemeClass = (light, dark) => {
+    if (isDarkModeAuto(darkMode.value)) {
+      return `${light} ${dark}`;
+    }
+    return isDarkMode(darkMode.value) ? dark : light;
+  };
+
   return {
     darkMode,
     prefersDarkMode,
