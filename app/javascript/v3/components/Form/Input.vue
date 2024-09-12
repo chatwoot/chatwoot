cd1685ad6d13c56987ed9105f70d1052cdc5096a
Shivam Mishra <scm.mymail@gmail.com>
2024-09-12 12:43:53 +0530

    fix: outline

--- app/javascript/v3/components/Form/Input.vue
+++ app/javascript/v3/components/Form/Input.vue
@@ -51,10 +51,9 @@ const model = defineModel({
     <input
       v-bind="$attrs"
       v-model="model"
-      class="block w-full border-none rounded-md shadow-sm appearance-none outline outline-1 focus:outline-2 text-slate-900 dark:text-slate-100 placeholder:text-slate-400 sm:text-sm sm:leading-6 dark:bg-slate-800"
+      class="block w-full border-none rounded-md shadow-sm appearance-none outline outline-1 focus:outline focus:outline-1 text-slate-900 dark:text-slate-100 placeholder:text-slate-400 sm:text-sm sm:leading-6 dark:bg-slate-800"
       :class="{
-        'focus:outline-red-600 outline-red-600 dark:focus:outline-red-600 dark:outline-red-600':
-          hasError,
+        'focus:outline-red-600 outline-red-600': hasError,
         'outline-slate-200 dark:outline-slate-600 dark:focus:outline-woot-500 focus:outline-woot-500':
           !hasError,
         'px-3 py-3': spacing === 'base',
