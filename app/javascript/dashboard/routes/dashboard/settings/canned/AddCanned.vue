1a96fad99a03bbc51af09c39787d8ba296c1f91a
Shivam Mishra <scm.mymail@gmail.com>
2024-09-12 13:13:06 +0530

    fix: add canned options

--- app/javascript/dashboard/routes/dashboard/settings/canned/AddCanned.vue
+++ app/javascript/dashboard/routes/dashboard/settings/canned/AddCanned.vue
@@ -8,6 +8,7 @@ import Modal from '../../../../components/Modal.vue';
 import WootMessageEditor from 'dashboard/components/widgets/WootWriter/Editor.vue';
 
 export default {
+  name: 'AddCanned',
   components: {
     WootSubmitButton,
     Modal,
@@ -95,7 +96,7 @@ export default {
               v-model="shortCode"
               type="text"
               :placeholder="$t('CANNED_MGMT.ADD.FORM.SHORT_CODE.PLACEHOLDER')"
-              @input="v$.shortCode.$touch"
+              @blur="v$.shortCode.$touch"
             />
           </label>
         </div>
