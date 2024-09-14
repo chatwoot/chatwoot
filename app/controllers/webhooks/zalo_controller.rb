13258479f0817f3f77d90adaf8d05f1dcef67ab5
KongNC <nccong207@gmail.com>
2024-09-14 08:27:36 +0700

    zns receive webhook

--- app/controllers/webhooks/zalo_controller.rb
+++ app/controllers/webhooks/zalo_controller.rb
@@ -1,7 +1,12 @@
 class Webhooks::ZaloController < ActionController::API
   def process_payload
     Rails.logger.info('Zalo webhook received events')
-    Webhooks::ZaloEventsJob.perform_later(params.to_unsafe_hash, signature: request.headers['X-ZEvent-Signature'], post_body: request.body.read)
+    Webhooks::ZaloEventsJob.perform_later(
+      params.to_unsafe_hash,
+      signature: request.headers['X-ZEvent-Signature'],
+      post_body: request.body.read,
+      from_zns: request.headers['X-ZEvent-Signature'] == 'ZNS'
+    )
     head :ok
   end
 end
