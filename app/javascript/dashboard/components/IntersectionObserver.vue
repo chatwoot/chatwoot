fe4581051dded684549cc1cc994fffe6ca6a2eec
Shivam Mishra <scm.mymail@gmail.com>
2024-09-17 15:34:16 +0530

    feat: refactor intersection observer to use vueuse

--- app/javascript/dashboard/components/IntersectionObserver.vue
+++ app/javascript/dashboard/components/IntersectionObserver.vue
@@ -1,34 +1,29 @@
-<script>
-export default {
-  props: {
-    options: {
-      type: Object,
-      default: () => ({ root: document, rootMargin: '100px 0 100px 0)' }),
-    },
+<script setup>
+import { ref, defineEmits } from 'vue';
+import { useIntersectionObserver } from '@vueuse/core';
+
+const { options } = defineProps({
+  options: {
+    type: Object,
+    default: () => ({ root: document, rootMargin: '100px 0 100px 0)' }),
   },
-  mounted() {
-    this.intersectionObserver = null;
-    this.registerInfiniteLoader();
+});
+
+const emit = defineEmits(['observed']);
+const observedElement = ref('');
+
+useIntersectionObserver(
+  observedElement,
+  ([{ isIntersecting }]) => {
+    if (isIntersecting) {
+      console.log('Element is intersecting');
+      emit('observed');
+    }
   },
-  beforeDestroy() {
-    this.unobserveInfiniteLoadObserver();
-  },
-  methods: {
-    registerInfiniteLoader() {
-      this.intersectionObserver = new IntersectionObserver(entries => {
-        if (entries && entries[0].isIntersecting) {
-          this.$emit('observed');
-        }
-      }, this.options);
-      this.intersectionObserver.observe(this.$refs.observedElement);
-    },
-    unobserveInfiniteLoadObserver() {
-      this.intersectionObserver.unobserve(this.$refs.observedElement);
-    },
-  },
-};
+  options
+);
 </script>
 
 <template>
-  <div ref="observedElement" class="h-6 w-full" />
+  <div ref="observedElement" class="h-6 w-full bg-green-400" />
 </template>
