ee83675ed509773e06c9dd41f036ca9b3b8f11f4
Shivam Mishra <scm.mymail@gmail.com>
2024-09-17 20:10:41 +0530

    fix: portal pack

--- app/javascript/portal/portalHelpers.js
+++ app/javascript/portal/portalHelpers.js
@@ -1,10 +1,12 @@
-import slugifyWithCounter from '@sindresorhus/slugify';
-import Vue from 'vue';
+import { createApp } from 'vue';
+import VueDOMPurifyHTML from 'vue-dompurify-html';
+import { domPurifyConfig } from '../shared/helpers/HTMLSanitizer';
+import { directive as onClickaway } from 'vue3-click-away';
 
+import slugifyWithCounter from '@sindresorhus/slugify';
 import PublicArticleSearch from './components/PublicArticleSearch.vue';
 import TableOfContents from './components/TableOfContents.vue';
 import { initializeTheme } from './portalThemeHelper.js';
-import { directive as onClickaway } from 'vue3-click-away';
 
 export const getHeadingsfromTheArticle = () => {
   const rows = [];
@@ -79,24 +81,28 @@ export const InitializationHelpers = {
   initializeSearch: () => {
     const isSearchContainerAvailable = document.querySelector('#search-wrap');
     if (isSearchContainerAvailable) {
-      new Vue({
+      const app = createApp({
         components: { PublicArticleSearch },
         directives: {
           'on-clickaway': onClickaway,
         },
         template: '<PublicArticleSearch />',
-      }).$mount('#search-wrap');
+      }).mount('#search-wrap');
+
+      app.use(VueDOMPurifyHTML, domPurifyConfig);
     }
   },
 
   initializeTableOfContents: () => {
     const isOnArticlePage = document.querySelector('#cw-hc-toc');
     if (isOnArticlePage) {
-      new Vue({
+      const app = createApp({
         components: { TableOfContents },
         data: { rows: getHeadingsfromTheArticle() },
         template: '<table-of-contents :rows="rows" />',
-      }).$mount('#cw-hc-toc');
+      }).mount('#cw-hc-toc');
+
+      app.use(VueDOMPurifyHTML, domPurifyConfig);
     }
   },
 
