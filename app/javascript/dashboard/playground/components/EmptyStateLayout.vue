6fbc12d3d8d795042d405cc334994c50c0aad613
iamsivin <iamsivin@gmail.com>
2024-09-25 22:38:31 +0530

    feat: Empty state layout

---
+++ app/javascript/dashboard/playground/components/EmptyStateLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center w-full h-full min-h-screen p-4 overflow-hidden font-interDisplay"
+  >
+    <div
+      class="relative w-full max-w-[940px] mx-auto overflow-hidden h-full max-h-[448px]"
+    >
+      <div
+        class="w-full h-full space-y-4 overflow-y-hidden opacity-50 pointer-events-none"
+      >
+        <slot name="empty-state-item" />
+      </div>
+      <div
+        class="absolute inset-x-0 bottom-0 flex flex-col items-center justify-end w-full h-full pb-9 bg-gradient-to-t from-white dark:from-slate-900 to-transparent"
+      >
+        <slot name="empty-state" />
+      </div>
+    </div>
+  </div>
+</template>
