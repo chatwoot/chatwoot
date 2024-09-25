298f4b58b2a49a54eed7196360d0a10a3d2644c2
iamsivin <iamsivin@gmail.com>
2024-09-25 22:39:10 +0530

    feat: Card layout

---
+++ app/javascript/dashboard/playground/HelpCenter/components/CardLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="relative flex flex-col w-full gap-3 px-6 py-5 rounded-2xl bg-slate-50 dark:bg-slate-800/50"
+  >
+    <slot name="header" />
+    <slot name="footer" />
+  </div>
+</template>
