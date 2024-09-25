02acac65fc32a8c217c2e85a84ef0fa7614606d2
Jaideep Guntupalli <guntupallijaideep@gmail.com>
2024-09-25 15:29:07 +0530

    revert random comments

--- app/controllers/devise_overrides/token_validations_controller.rb
+++ app/controllers/devise_overrides/token_validations_controller.rb
@@ -6,27 +6,5 @@ class DeviseOverrides::TokenValidationsController < DeviseTokenAuth::TokenValida
     else
       render_validate_token_error
     end
-    # Rails.logger.info("Token validated for user #{@resource.inspect}")
-    # # get all the user's accounts
-    # accounts = @resource.accounts
-    # Rails.logger.info("User's accounts: #{accounts.inspect}")
-
-    # # get all user account ids
-    # account_ids = accounts.pluck(:id)
-
-    # # make a request to 'localhost:3001/csdb/auth/verify?accountId=123 and ifs successful, render the token or else invalidate the token
-    # account_ids.each do |account_id|
-    #   response = HTTParty.get("http://localhost:3003/prod/csdb/auth/verify?accountId=#{account_id}")
-    #   if response.success?
-    # render 'devise/token', formats: [:json]
-    #     else
-    #       # error out with 401 status
-    #       render json: { error: 'Unauthorized' }, status: :unauthorized
-    #       return
-    #     end
-    #   end
-    # else
-    #   render_validate_token_error
-    # end
   end
 end
