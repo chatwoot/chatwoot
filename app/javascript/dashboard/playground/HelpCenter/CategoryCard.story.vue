d164a1ef8eefceacce5e2f6aaf7102a58a917e04
iamsivin <iamsivin@gmail.com>
2024-09-26 23:39:10 +0530

    feat: Category card story

---
+++ app/javascript/dashboard/playground/HelpCenter/CategoryCard.story.vue
@@ -0,0 +1,27 @@
+<script setup>
+import CategoryCard from './CategoryCard.vue';
+
+const categories = [
+  {
+    title: 'Getting Started',
+    description: 'Essential guides for new users',
+    articlesCount: '5',
+  },
+];
+</script>
+
+<template>
+  <Story title="CategoryCard" :layout="{ type: 'grid', width: '400px' }">
+    <Variant
+      v-for="(category, index) in categories"
+      :key="index"
+      :title="category.title"
+    >
+      <CategoryCard
+        :title="category.title"
+        :description="category.description"
+        :articlesCount="category.articlesCount"
+      />
+    </Variant>
+  </Story>
+</template>
