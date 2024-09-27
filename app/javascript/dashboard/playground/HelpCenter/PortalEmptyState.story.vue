45c44929a210e0f2262b2000323850c2d8d0e212
iamsivin <iamsivin@gmail.com>
2024-09-27 14:06:19 +0530

    feat: Adds story for portal empty state

---
+++ app/javascript/dashboard/playground/HelpCenter/PortalEmptyState.story.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+import PortalEmptyState from './PortalEmptyState.vue';
+</script>
+
+<template>
+  <Story
+    title="HelpCenter/PortalEmptyState"
+    :layout="{ type: 'grid', width: '100%' }"
+  >
+    <Variant title="Portal Empty State with dialog">
+      <PortalEmptyState />
+    </Variant>
+  </Story>
+</template>
+
+<script>
+export default {
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+</script>
