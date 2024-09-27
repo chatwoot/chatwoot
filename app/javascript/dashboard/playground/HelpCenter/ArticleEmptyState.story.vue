95df5e4a7cdd7f55fe114f5bb57f057ef25570ea
iamsivin <iamsivin@gmail.com>
2024-09-28 00:17:52 +0530

    feat: Article empty state story

---
+++ app/javascript/dashboard/playground/HelpCenter/ArticleEmptyState.story.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ArticleEmptyState from './ArticleEmptyState.vue';
+</script>
+
+<template>
+  <Story
+    title="HelpCenter/ArticleEmptyState"
+    :layout="{ type: 'grid', width: '100%' }"
+  >
+    <Variant title="Article Empty State">
+      <ArticleEmptyState />
+    </Variant>
+  </Story>
+</template>
+
+<script>
+export default {
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+</script>
