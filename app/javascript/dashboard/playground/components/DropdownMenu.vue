44a3c1b32fd5912404c15d7c1132ee16f18249c8
iamsivin <iamsivin@gmail.com>
2024-09-30 11:37:22 +0530

    feat: Dropdown menu component

---
+++ app/javascript/dashboard/playground/components/DropdownMenu.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import ButtonV4 from './Button.vue';
+
+defineProps({
+  menuItems: {
+    type: Array,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['action']);
+
+const handleAction = action => {
+  emit('action', action);
+};
+</script>
+
+<template>
+  <div
+    class="bg-white dark:bg-gray-800 absolute rounded-xl py-3 px-1 min-w-[136px] shadow-lg"
+  >
+    <ButtonV4
+      v-for="item in menuItems"
+      :key="item.action"
+      :label="item.label"
+      :icon="item.icon"
+      variant="ghost"
+      size="sm"
+      :text-variant="item.action === 'delete' ? 'danger' : ''"
+      @click="handleAction(item.action)"
+    />
+  </div>
+</template>
