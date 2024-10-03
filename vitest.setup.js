9c70cebbda24d6915493a79875a4b4ac9131f020
Shivam Mishra <scm.mymail@gmail.com>
2024-10-03 23:25:11 +0530

    test: update accountselector spec

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,5 +1,10 @@
 import { config } from '@vue/test-utils';
 import { createI18n } from 'vue-i18n';
+import i18nMessages from 'dashboard/i18n';
 
-const i18n = createI18n();
+const i18n = createI18n({
+  locale: 'en',
+  messages: i18nMessages,
+});
+
 config.global.plugins = [i18n];
