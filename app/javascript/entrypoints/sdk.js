adb0e94cbd57a837f6928bb04ede247af0132119
Shivam Mishra <scm.mymail@gmail.com>
2024-10-08 11:40:53 +0530

    chore: remove logs

--- app/javascript/entrypoints/sdk.js
+++ app/javascript/entrypoints/sdk.js
@@ -19,45 +19,34 @@ import { setCookieWithDomain } from '../sdk/cookieHelpers';
 import { SDK_SET_BUBBLE_VISIBILITY } from 'shared/constants/sharedFrameEvents';
 
 const runSDK = ({ baseUrl, websiteToken }) => {
-  console.log('runSDK called with:', { baseUrl, websiteToken });
 
   if (window.$chatwoot) {
-    console.log('Chatwoot already initialized.');
     return;
   }
 
   if (window.Turbo) {
-    console.log('Turbo detected.');
     document.addEventListener('turbo:before-render', event => {
-      console.log('Turbo before render triggered.');
       restoreWidgetInDOM(event.detail.newBody);
     });
   }
 
   if (window.Turbolinks) {
-    console.log('Turbolinks detected.');
     document.addEventListener('turbolinks:before-render', event => {
-      console.log('Turbolinks before render triggered.');
       restoreWidgetInDOM(event.data.newBody);
     });
   }
 
-  console.log('Checking for Astro app...');
   document.addEventListener('astro:before-swap', event => {
-    console.log('Astro before swap triggered.');
     restoreWidgetInDOM(event.newDocument.body);
   });
 
   const chatwootSettings = window.chatwootSettings || {};
-  console.log('Chatwoot settings:', chatwootSettings);
 
   let locale = chatwootSettings.locale;
   let baseDomain = chatwootSettings.baseDomain;
-  console.log('Initial locale and baseDomain:', { locale, baseDomain });
 
   if (chatwootSettings.useBrowserLanguage) {
     locale = window.navigator.language.replace('-', '_');
-    console.log('Using browser language:', locale);
   }
 
   window.$chatwoot = {
@@ -79,12 +68,10 @@ const runSDK = ({ baseUrl, websiteToken }) => {
     darkMode: getDarkMode(chatwootSettings.darkMode),
 
     toggle(state) {
-      console.log('Toggling bubble state:', state);
       IFrameHelper.events.toggleBubble(state);
     },
 
     toggleBubbleVisibility(visibility) {
-      console.log('Toggling bubble visibility:', visibility);
       let widgetElm = document.querySelector('.woot--bubble-holder');
       let widgetHolder = document.querySelector('.woot-widget-holder');
       if (visibility === 'hide') {
@@ -102,7 +89,6 @@ const runSDK = ({ baseUrl, websiteToken }) => {
     },
 
     popoutChatWindow() {
-      console.log('Popout chat window triggered.');
       IFrameHelper.events.popoutChatWindow({
         baseUrl: window.$chatwoot.baseUrl,
         websiteToken: window.$chatwoot.websiteToken,
@@ -111,7 +97,6 @@ const runSDK = ({ baseUrl, websiteToken }) => {
     },
 
     setUser(identifier, user) {
-      console.log('Setting user:', { identifier, user });
       if (typeof identifier !== 'string' && typeof identifier !== 'number') {
         console.error('Invalid identifier type:', identifier);
         throw new Error('Identifier should be a string or a number');
@@ -127,13 +112,11 @@ const runSDK = ({ baseUrl, websiteToken }) => {
       const userCookieName = getUserCookieName();
       const existingCookieValue = Cookies.get(userCookieName);
       const hashToBeStored = computeHashForUserData({ identifier, user });
-      console.log('User cookie name and hash:', {
         userCookieName,
         hashToBeStored,
       });
 
       if (hashToBeStored === existingCookieValue) {
-        console.log('User hash matches existing cookie. No update needed.');
         return;
       }
 
@@ -147,7 +130,6 @@ const runSDK = ({ baseUrl, websiteToken }) => {
     },
 
     setCustomAttributes(customAttributes = {}) {
-      console.log('Setting custom attributes:', customAttributes);
       if (!customAttributes || !Object.keys(customAttributes).length) {
         console.error('Invalid custom attributes:', customAttributes);
         throw new Error('Custom attributes should have at least one key');
@@ -157,7 +139,6 @@ const runSDK = ({ baseUrl, websiteToken }) => {
     },
 
     deleteCustomAttribute(customAttribute = '') {
-      console.log('Deleting custom attribute:', customAttribute);
       if (!customAttribute) {
         console.error('Custom attribute is missing.');
         throw new Error('Custom attribute is required');
@@ -169,7 +150,6 @@ const runSDK = ({ baseUrl, websiteToken }) => {
     },
 
     setConversationCustomAttributes(customAttributes = {}) {
-      console.log('Setting conversation custom attributes:', customAttributes);
       if (!customAttributes || !Object.keys(customAttributes).length) {
         console.error(
           'Invalid conversation custom attributes:',
@@ -184,7 +164,6 @@ const runSDK = ({ baseUrl, websiteToken }) => {
     },
 
     deleteConversationCustomAttribute(customAttribute = '') {
-      console.log('Deleting conversation custom attribute:', customAttribute);
       if (!customAttribute) {
         console.error('Conversation custom attribute is missing.');
         throw new Error('Custom attribute is required');
@@ -196,29 +175,24 @@ const runSDK = ({ baseUrl, websiteToken }) => {
     },
 
     setLabel(label = '') {
-      console.log('Setting label:', label);
       IFrameHelper.sendMessage('set-label', { label });
     },
 
     removeLabel(label = '') {
-      console.log('Removing label:', label);
       IFrameHelper.sendMessage('remove-label', { label });
     },
 
     setLocale(localeToBeUsed = 'en') {
-      console.log('Setting locale:', localeToBeUsed);
       IFrameHelper.sendMessage('set-locale', { locale: localeToBeUsed });
     },
 
     setColorScheme(darkMode = 'light') {
-      console.log('Setting color scheme to:', darkMode);
       IFrameHelper.sendMessage('set-color-scheme', {
         darkMode: getDarkMode(darkMode),
       });
     },
 
     reset() {
-      console.log('Resetting Chatwoot.');
       if (window.$chatwoot.isOpen) {
         IFrameHelper.events.toggleBubble();
       }
@@ -227,7 +201,6 @@ const runSDK = ({ baseUrl, websiteToken }) => {
       Cookies.remove(getUserCookieName());
 
       const iframe = IFrameHelper.getAppFrame();
-      console.log('Reloading iframe with new URL.');
       iframe.src = IFrameHelper.getUrl({
         baseUrl: window.$chatwoot.baseUrl,
         websiteToken: window.$chatwoot.websiteToken,
@@ -237,7 +210,6 @@ const runSDK = ({ baseUrl, websiteToken }) => {
     },
   };
 
-  console.log('Creating iframe for Chatwoot...');
   IFrameHelper.createFrame({
     baseUrl,
     websiteToken,
