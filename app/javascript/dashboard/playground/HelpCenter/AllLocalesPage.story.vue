86d5454cdddcf24609157388ed6bb0022e9f1a4f
iamsivin <iamsivin@gmail.com>
2024-10-08 19:51:00 +0530

    feat: Adds locale page story

---
+++ app/javascript/dashboard/playground/HelpCenter/AllLocalesPage.story.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AllLocalesPage from './AllLocalesPage.vue';
+</script>
+
+<!-- eslint-disable vue/no-bare-strings-in-template -->
+<!-- eslint-disable vue/no-undef-components -->
+<template>
+  <Story
+    title="HelpCenter/AllLocalesPage"
+    :layout="{ type: 'grid', width: '100%' }"
+  >
+    <Variant title="All Locales">
+      <AllLocalesPage />
+    </Variant>
+  </Story>
+</template>
