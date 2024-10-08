fa29cd8fc26ef3369854ee4266d1582474fcbe2c
iamsivin <iamsivin@gmail.com>
2024-10-08 14:40:24 +0530

    feat: All articles page story

---
+++ app/javascript/dashboard/playground/HelpCenter/AllArticlesPage.story.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AllArticlesPage from './AllArticlesPage.vue';
+</script>
+
+<!-- eslint-disable vue/no-bare-strings-in-template -->
+<!-- eslint-disable vue/no-undef-components -->
+<template>
+  <Story
+    title="HelpCenter/AllArticlesPage"
+    :layout="{ type: 'grid', width: '100%' }"
+  >
+    <Variant title="All Articles">
+      <AllArticlesPage />
+    </Variant>
+  </Story>
+</template>
