2bbcbd20e6af2ed929fbcfc2886decd55cacdbcb
Shivam Mishra <scm.mymail@gmail.com>
2024-10-09 21:58:41 +0530

    feat: add remix icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,7 +90,7 @@ module.exports = {
     // eslint-disable-next-line
     require('@tailwindcss/typography'),
     iconsPlugin({
-      collections: getIconCollections(['lucide', 'logos']),
+      collections: getIconCollections(['lucide', 'logos', 'ri']),
     }),
   ],
 };
