a637eec46fda048614e2e34d6f65ca015395dd9f
iamsivin <iamsivin@gmail.com>
2024-10-09 17:25:40 +0530

    chore: Update card layout to support click event

--- app/javascript/dashboard/playground/HelpCenter/components/CardLayout.vue
+++ app/javascript/dashboard/playground/HelpCenter/components/CardLayout.vue
@@ -1,6 +1,15 @@
+<script setup>
+const emit = defineEmits(['click']);
+
+const handleClick = () => {
+  emit('click');
+};
+</script>
+
 <template>
   <div
-    class="relative flex flex-col w-full gap-3 px-6 py-5 rounded-2xl bg-slate-25 dark:bg-slate-800/50"
+    class="relative flex flex-col w-full gap-3 px-6 py-5 group rounded-2xl bg-slate-25 dark:bg-slate-800/50"
+    @click="handleClick"
   >
     <slot name="header" />
     <slot name="footer" />
