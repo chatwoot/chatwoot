384382b854ca27fb3cec2d810faefd0a44dd7d1c
mandythcuyen <mandythcuyen@gmail.com>
2024-10-10 17:31:30 +0700

    Refactor

--- app/javascript/dashboard/routes/dashboard/settings/reports/components/Filters/v3/AddFilterChip.vue
+++ app/javascript/dashboard/routes/dashboard/settings/reports/components/Filters/v3/AddFilterChip.vue
@@ -7,7 +7,7 @@ import FilterDropdownEmptyState from './FilterDropdownEmptyState.vue';
 import { ref } from 'vue';
 import { createPopper } from '@popperjs/core';
 
-defineProps({
+const props = defineProps({
   name: {
     type: String,
     required: true,
@@ -35,32 +35,17 @@ defineProps({
 });
 
 const hoveredItemId = ref(null);
+const popperInstances = ref({});
+
+const getPopperInstance = id => {
+  return popperInstances.value[id];
+};
 
 const showSubMenu = id => {
   hoveredItemId.value = id;
 
-  const reference = document.querySelector(`#filter-item-${id}`);
-  const popper = document.querySelector(`#filter-submenu-${id}`);
-  const viewport = document.querySelector('#kanban-board');
-
-  createPopper(reference, popper, {
-    placement: 'right-start',
-    modifiers: [
-      {
-        name: 'offset',
-        options: {
-          offset: [0, 2],
-        },
-      },
-      {
-        name: 'preventOverflow',
-        padding: 20,
-        options: {
-          boundary: viewport,
-        },
-      },
-    ],
-  });
+  const popperInstance = getPopperInstance(id);
+  popperInstance.update();
 };
 
 const hideSubMenu = () => {
@@ -79,6 +64,35 @@ const closeDropdown = () => {
   hideSubMenu();
   emit('closeDropdown');
 };
+
+const createPopperInstances = () => {
+  popperInstances.value = props.menuOption.reduce((acc, { id }) => {
+    const reference = document.querySelector(`#filter-item-${id}`);
+    const popper = document.querySelector(`#filter-submenu-${id}`);
+    const viewport = document.querySelector('#kanban-board');
+    return {
+      ...acc,
+      [id]: createPopper(reference, popper, {
+        placement: 'right-start',
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: [0, 2],
+            },
+          },
+          {
+            name: 'preventOverflow',
+            padding: 20,
+            options: {
+              boundary: viewport,
+            },
+          },
+        ],
+      }),
+    };
+  }, {});
+};
 </script>
 
 <template>
@@ -88,6 +102,7 @@ const closeDropdown = () => {
       <filter-list-dropdown
         v-on-clickaway="closeDropdown"
         class="left-0 md:right-0 top-10"
+        @createPopperInstances="createPopperInstances"
       >
         <template #listItem>
           <filter-dropdown-empty-state
@@ -106,14 +121,12 @@ const closeDropdown = () => {
             <!-- Submenu with search and clear button  -->
             <template v-if="item.options" #dropdown>
               <filter-list-dropdown
+                v-show="isHovered(item.id)"
                 :id="`filter-submenu-${item.id}`"
                 :list-items="item.options"
                 :input-placeholder="$t('GENERAL.LIST_SEARCH_PLACEHOLDER')"
                 :enable-search="enableSearch"
-                :class="[
-                  'flex flex-col w-80 overflow-y-auto top-0 left-36 max-h-[calc(100vh-7rem)]',
-                  { hidden: !isHovered(item.id) },
-                ]"
+                class="flex flex-col w-80 overflow-y-auto top-0 left-36 max-h-[calc(100vh-7rem)]"
                 @click="addFilter"
               />
             </template>
