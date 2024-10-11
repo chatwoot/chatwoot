f836ad4406b26e3496c58fc2488105369edaaa40
iamsivin <iamsivin@gmail.com>
2024-10-11 22:50:02 +0530

    fix: file name

--- app/javascript/dashboard/components-next/HelpCenter/Pages/ArticlePage/ArticlesPage.story.vue
+++ app/javascript/dashboard/components-next/HelpCenter/Pages/ArticlePage/ArticlesPage.story.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AllArticlesPage from './AllArticles.vue';
+import ArticlesPage from './ArticlesPage.vue';
 
 const articles = [
   {
@@ -64,10 +64,10 @@ const articles = [
 <!-- eslint-disable vue/no-bare-strings-in-template -->
 <!-- eslint-disable vue/no-undef-components -->
 <template>
-  <Story title="HelpCenter/AllArticlesPage" :layout="{ type: 'single' }">
+  <Story title="HelpCenter/Pages/ArticlesPage" :layout="{ type: 'single' }">
     <Variant title="All Articles">
       <div class="w-full min-h-screen bg-white dark:bg-slate-900">
-        <AllArticlesPage :articles="articles" />
+        <ArticlesPage :articles="articles" />
       </div>
     </Variant>
   </Story>
