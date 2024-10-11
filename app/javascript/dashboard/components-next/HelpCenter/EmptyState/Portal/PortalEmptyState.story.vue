dfe44f5e4687f1d69a362d9e49a985ff071d76ca
iamsivin <iamsivin@gmail.com>
2024-10-11 23:13:14 +0530

    feat: Portal empty state

---
+++ app/javascript/dashboard/components-next/HelpCenter/EmptyState/Portal/PortalEmptyState.story.vue
@@ -0,0 +1,18 @@
+<script setup>
+import PortalEmptyState from './PortalEmptyState.vue';
+</script>
+
+<!-- eslint-disable vue/no-bare-strings-in-template -->
+<!-- eslint-disable vue/no-undef-components -->
+<template>
+  <Story
+    title="HelpCenter/EmptyState/PortalEmptyState"
+    :layout="{ type: 'single', width: '1100px' }"
+  >
+    <Variant title="Portal Empty State">
+      <div class="w-full h-full px-20 mx-auto bg-white dark:bg-slate-900">
+        <PortalEmptyState />
+      </div>
+    </Variant>
+  </Story>
+</template>
