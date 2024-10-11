8e05481ac7c23268ea7ba6e278088954a3c197e9
Pranav <pranavrajs@gmail.com>
2024-10-11 15:05:02 -0700

    Update eslintrc file

--- app/javascript/dashboard/components-next/button/Button.story.vue
+++ app/javascript/dashboard/components-next/button/Button.story.vue
@@ -2,8 +2,6 @@
 import Button from './Button.vue';
 </script>
 
-<!-- eslint-disable vue/no-bare-strings-in-template -->
-<!-- eslint-disable vue/no-undef-components -->
 <template>
   <Story title="Components/Button" :layout="{ type: 'grid', width: '400' }">
     <Variant title="Default">
