9708e3357912c878afa5e4aed3565babee376cdf
iamsivin <iamsivin@gmail.com>
2024-10-11 21:32:43 +0530

    feat: Category card component

---
+++ app/javascript/dashboard/components-next/CardLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+const emit = defineEmits(['click']);
+const handleClick = () => {
+  emit('click');
+};
+</script>
+
+<template>
+  <div
+    class="relative flex flex-col w-full gap-3 px-6 py-5 group/cardLayout rounded-2xl bg-slate-25 dark:bg-slate-800/50"
+    @click="handleClick"
+  >
+    <slot name="header" />
+    <slot name="footer" />
+  </div>
+</template>
