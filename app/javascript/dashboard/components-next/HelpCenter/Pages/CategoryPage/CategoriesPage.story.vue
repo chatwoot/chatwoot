9288984fd87cc15e28d682cd3f0245bd6a0f9aaf
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2024-10-12 22:51:55 +0530

    Update app/javascript/dashboard/components-next/HelpCenter/Pages/CategoryPage/CategoriesPage.story.vue

--- app/javascript/dashboard/components-next/HelpCenter/Pages/CategoryPage/CategoriesPage.story.vue
+++ app/javascript/dashboard/components-next/HelpCenter/Pages/CategoryPage/CategoriesPage.story.vue
@@ -201,7 +201,7 @@ const categories = [
 <!-- eslint-disable vue/no-bare-strings-in-template -->
 <!-- eslint-disable vue/no-undef-components -->
 <template>
-  <Story title="HelpCenter/Pages/CategoryPage" :layout="{ type: 'single' }">
+  <Story title="Pages/HelpCenter/CategoryPage" :layout="{ type: 'single' }">
     <Variant title="All Categories">
       <div class="w-full min-h-screen bg-white dark:bg-slate-900">
         <CategoriesPage :categories="categories" />
