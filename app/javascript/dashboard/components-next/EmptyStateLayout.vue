52e59545e50f5f5ed405c992e516fa0d8f54a0a8
iamsivin <iamsivin@gmail.com>
2024-10-12 18:40:21 +0530

    chore: Review fix

--- app/javascript/dashboard/components-next/EmptyStateLayout.vue
+++ app/javascript/dashboard/components-next/EmptyStateLayout.vue
@@ -1,3 +1,16 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
 <template>
   <section
     class="relative flex flex-col items-center justify-center w-full h-full min-h-screen p-4 overflow-hidden"
@@ -13,7 +26,21 @@
       <div
         class="absolute inset-x-0 bottom-0 flex flex-col items-center justify-end w-full h-full pb-9 bg-gradient-to-t from-white dark:from-slate-900 to-transparent font-interDisplay"
       >
-        <slot name="empty-state" />
+        <div class="flex flex-col items-center justify-center gap-6">
+          <div class="flex flex-col items-center justify-center gap-2">
+            <h2
+              class="text-3xl font-medium text-center text-slate-900 dark:text-white"
+            >
+              {{ title }}
+            </h2>
+            <p
+              class="max-w-lg text-base text-center text-slate-600 dark:text-slate-300"
+            >
+              {{ subtitle }}
+            </p>
+          </div>
+          <slot name="actions" />
+        </div>
       </div>
     </div>
   </section>
