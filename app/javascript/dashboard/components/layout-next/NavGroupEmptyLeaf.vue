abf9a1dcf7f64d2aba4606573fdfef44b118d013
Shivam Mishra <scm.mymail@gmail.com>
2024-10-15 17:13:51 +0530

    feat: add empty leaf component

---
+++ app/javascript/dashboard/components/layout-next/NavGroupEmptyLeaf.vue
@@ -0,0 +1,7 @@
+<template>
+  <li
+    class="py-1 pl-3 text-n-slate-10 border rounded-lg border-dashed text-center border-n-alpha-2 text-xs h-8 grid place-content-center"
+  >
+    {{ 'No items' }}
+  </li>
+</template>
