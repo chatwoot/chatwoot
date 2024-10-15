4c650f0dbbb6208088c0eb0b54a6400b9119a0c2
Shivam Mishra <scm.mymail@gmail.com>
2024-10-15 20:03:49 +0530

    fix: icon class

--- app/javascript/dashboard/components/layout-next/NavGroupLeaf.vue
+++ app/javascript/dashboard/components/layout-next/NavGroupLeaf.vue
@@ -26,19 +26,13 @@ defineProps({
     :is="to ? 'router-link' : 'div'"
     :to="to"
     :title="name"
-    class="flex h-8 items-center gap-2 px-2 py-1 rounded-lg max-w-[151px] hover:bg-gradient-to-r from-transparent via-n-slate-3/70 to-n-slate-3/70"
+    class="flex h-8 items-center gap-2 px-2 py-1 rounded-lg max-w-[151px] hover:bg-gradient-to-r from-transparent via-n-slate-3/70 to-n-slate-3/70 group"
     :class="{
-      'text-n-blue bg-n-alpha-2 font-medium': active,
+      'text-n-blue bg-n-alpha-2 font-medium active': active,
     }"
   >
-    <div>
-      <Icon
-        v-if="icon"
-        :icon="icon"
-        class="size-4 inline-block"
-        :class="active ? 'bg-n-blue/20' : 'bg-n-alpha-2'"
-      />
-    </div>
+    <Icon v-if="icon" :icon="icon" class="size-4 inline-block" />
+
     <div class="flex-1 truncate min-w-0">{{ name }}</div>
   </component>
 </template>
