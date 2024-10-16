f0c7a6d2cd7be3ef0f1a6ecae4ff2bf739777a23
Shivam Mishra <scm.mymail@gmail.com>
2024-10-16 14:31:18 +0530

    feat: ensure a sub group header is shown only if it has some items

--- app/javascript/dashboard/components/layout-next/SidebarSubGroup.vue
+++ app/javascript/dashboard/components/layout-next/SidebarSubGroup.vue
@@ -1,19 +1,40 @@
 <script setup>
+import { computed } from 'vue';
 import SidebarGroupLeaf from './SidebarGroupLeaf.vue';
 import SidebarGroupSeparator from './SidebarGroupSeparator.vue';
 import SidebarGroupEmptyLeaf from './SidebarGroupEmptyLeaf.vue';
+import { usePolicy } from 'dashboard/composables/usePolicy';
+import { useSidebarContext } from './provider';
 
-defineProps({
+const props = defineProps({
   isExpanded: { type: Boolean, default: false },
   name: { type: String, required: true },
   icon: { type: [Object, String], required: true },
   children: { type: Array, default: undefined },
   activeChild: { type: Object, default: undefined },
 });
+
+const { resolvePermissions, resolveFeatureFlag } = useSidebarContext();
+const { checkFeatureAllowed, checkPermissions } = usePolicy();
+
+const hasAccessibleItems = computed(() => {
+  return props.children.some(child => {
+    const permissions = resolvePermissions(child.to);
+    const featureFlag = resolveFeatureFlag(child.to);
+
+    return checkPermissions(permissions) && checkFeatureAllowed(featureFlag);
+  });
+});
 </script>
 
 <template>
-  <SidebarGroupSeparator v-show="isExpanded" :name :icon class="my-1" />
+  <SidebarGroupSeparator
+    v-if="hasAccessibleItems"
+    v-show="isExpanded"
+    :name
+    :icon
+    class="my-1"
+  />
   <ul class="m-0 list-none">
     <template v-if="children.length">
       <SidebarGroupLeaf
