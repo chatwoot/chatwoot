dff57013c3fbc4c847b972accdd88dcf98d66906
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2024-10-18 08:26:38 +0530

    feat: Add article empty state component (#10278)

---
+++ app/javascript/dashboard/components-next/HelpCenter/EmptyState/Article/ArticleEmptyState.story.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ArticleEmptyState from './ArticleEmptyState.vue';
+</script>
+
+<template>
+  <Story
+    title="Components/HelpCenter/EmptyState/ArticleEmptyState"
+    :layout="{ type: 'single', width: '1100px' }"
+  >
+    <Variant title="Article Empty State">
+      <div class="w-full h-full px-20 mx-auto bg-white dark:bg-slate-900">
+        <ArticleEmptyState />
+      </div>
+    </Variant>
+  </Story>
+</template>
