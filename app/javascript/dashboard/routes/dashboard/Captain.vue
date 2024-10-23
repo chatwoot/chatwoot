e552f49bce647ada2ee98683b3bfea6509dbfde2
Shivam Mishra <scm.mymail@gmail.com>
2024-10-23 18:04:30 +0530

    feat: update example

--- app/javascript/dashboard/routes/dashboard/Captain.vue
+++ app/javascript/dashboard/routes/dashboard/Captain.vue
@@ -4,10 +4,18 @@ import setupCaptain from '@chatwoot/captain-dashboard/dist/captain.es.js';
 
 onMounted(async () => {
   await nextTick();
-  setupCaptain('#captain', 'app/accounts/2/captain');
+  setupCaptain(
+    '#captain',
+    'app/accounts/2/captain',
+    'http://localhost:3000/api/v1/captin'
+  );
 });
 </script>
 
 <template>
   <div id="captain" class="w-full" />
 </template>
+
+<style>
+@import '@chatwoot/captain-dashboard/dist/style.css';
+</style>
