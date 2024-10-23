b7e017f568853d379d4d64e2e0f46fc7d5d69068
Shivam Mishra <scm.mymail@gmail.com>
2024-10-23 17:45:21 +0530

    feat: setup captain usage

--- app/javascript/dashboard/routes/dashboard/Captain.vue
+++ app/javascript/dashboard/routes/dashboard/Captain.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { nextTick, onMounted } from 'vue';
-import setupCaptain from '@chatwoot/captain-dashboard';
+import setupCaptain from '@chatwoot/captain-dashboard/dist/captain.es.js';
 
 onMounted(async () => {
   await nextTick();
-  setupCaptain('#captain');
+  setupCaptain('#captain', 'app/accounts/2/captain');
 });
 </script>
 
 <template>
-  <div id="captain" />
+  <div id="captain" class="w-full" />
 </template>
