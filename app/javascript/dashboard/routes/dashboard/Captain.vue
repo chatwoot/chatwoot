70eb81ed51bc1616151dbc83a479fc5189ecf9b9
Shivam Mishra <scm.mymail@gmail.com>
2024-10-24 21:38:48 +0530

    feat: override fetch fn

--- app/javascript/dashboard/routes/dashboard/Captain.vue
+++ app/javascript/dashboard/routes/dashboard/Captain.vue
@@ -1,14 +1,19 @@
 <script setup>
 import { nextTick, onMounted } from 'vue';
+import { useAccount } from 'dashboard/composables/useAccount';
 import setupCaptain from '@chatwoot/captain-dashboard/dist/captain.es.js';
+
+const { accountId } = useAccount();
 
 onMounted(async () => {
   await nextTick();
-  setupCaptain(
-    '#captain',
-    'app/accounts/2/captain',
-    'http://localhost:3000/api/v1/captin'
-  );
+  setupCaptain('#captain', {
+    routerBase: `app/accounts/${accountId}/captain`,
+    fetchFn: async (source, options) => {
+      console.log(source, options);
+      return fetch(source, options);
+    },
+  });
 });
 </script>
 
