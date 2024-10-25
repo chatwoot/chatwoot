7132308c5248596be1c3a8b1b77856f775712e9f
Shivam Mishra <scm.mymail@gmail.com>
2024-10-25 14:19:26 +0530

    feat: validate method

--- app/controllers/api/v1/accounts/integrations/captain_controller.rb
+++ app/controllers/api/v1/accounts/integrations/captain_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::Accounts::Integrations::CaptainController < Api::V1::Accounts::BaseController
   before_action :check_admin_authorization?
   before_action :hook
+  before_action :validate_method, only: :proxy
 
   def proxy
     base_url = InstallationConfig.find_by(name: 'CAPTAIN_API_URL').value
@@ -8,7 +9,6 @@ class Api::V1::Accounts::Integrations::CaptainController < Api::V1::Accounts::Ba
 
     # make the request to the Captain service
     # also add the access token and email to header use X-User-Email and X-User-Token
-    #
     response = HTTParty.send(params[:method].downcase, url, body: params[:body], headers: {
                                'X-User-Email' => hook.settings[:account_email],
                                'X-User-Token' => hook.settings[:access_token]
@@ -21,5 +21,11 @@ class Api::V1::Accounts::Integrations::CaptainController < Api::V1::Accounts::Ba
 
   def hook
     @hook ||= Current.account.hooks.find_by!(app_id: 'captain')
+  end
+
+  def validate_method
+    return if params[:method].present? && %w[get post put delete].include?(params[:method].downcase)
+
+    render json: { error: 'Invalid or missing HTTP method' }, status: :unprocessable_entity
   end
 end
