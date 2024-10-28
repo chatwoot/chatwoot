6a2acc41ded15da5fc44b9e8f6e56ad9250018e5
Vishnu Narayanan <vishnu@chatwoot.com>
2024-10-28 17:35:01 +0530

    chore: debug vite.sh entrypoint

--- docker/entrypoints/vite.sh
+++ docker/entrypoints/vite.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-set -e
+set -x
 
 rm -rf /app/tmp/pids/server.pid
 rm -rf /app/tmp/cache/*
 
-pnpm install --frozen-lockfile
+pnpm install --reporter=default
 
 echo "Waiting for pnpm and bundle integrity to match lockfiles...."
 PNPM="pnpm list --json | grep -q 'Missing dependencies'"
