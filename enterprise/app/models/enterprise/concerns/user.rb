11c3d2cc4c195ba8f6229f039345e6c2b46edbe5
bielvelozo <gabrielvelozodev@gmail.com>
2024-10-28 10:11:45 -0300

    chore: remove user create limit

--- enterprise/app/models/enterprise/concerns/user.rb
+++ enterprise/app/models/enterprise/concerns/user.rb
@@ -8,6 +8,6 @@ module Enterprise::Concerns::User
   def ensure_installation_pricing_plan_quantity
     return unless ChatwootHub.pricing_plan == 'premium'
 
-    errors.add(:base, 'User limit reached. Please purchase more licenses from super admin') if User.count >= ChatwootHub.pricing_plan_quantity
+    # errors.add(:base, 'User limit reached. Please purchase more licenses from super admin') if User.count >= ChatwootHub.pricing_plan_quantity
   end
 end
