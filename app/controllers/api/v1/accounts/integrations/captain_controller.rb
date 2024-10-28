da548e02dfa26c3d70fa64ba4d537a744096d98d
Sojan <sojan@pepalo.com>
2024-10-28 13:00:16 +0100

    chore: minor fixes

--- app/controllers/api/v1/accounts/integrations/captain_controller.rb
+++ app/controllers/api/v1/accounts/integrations/captain_controller.rb
@@ -5,13 +5,13 @@ class Api::V1::Accounts::Integrations::CaptainController < Api::V1::Accounts::Ba
 
   def proxy
     base_url = InstallationConfig.find_by(name: 'CAPTAIN_API_URL').value
-    url = "#{base_url}/api/v1/accounts/#{hook.settings[:account_id]}/#{params[:route]}"
+    url = "#{base_url}/accounts/#{hook.settings['account_id']}#{params[:route]}"
 
     # make the request to the Captain service
     # also add the access token and email to header use X-User-Email and X-User-Token
     response = HTTParty.send(params[:method].downcase, url, body: params[:body], headers: {
-                               'X-User-Email' => hook.settings[:account_email],
-                               'X-User-Token' => hook.settings[:access_token]
+                               'X-User-Email' => hook.settings['account_email'],
+                               'X-User-Token' => hook.settings['access_token']
                              })
 
     render json: response.body, status: response.code
