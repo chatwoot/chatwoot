d03fda8f04b59785628dd67fc2bf8619f2cd44f7
KongNC <nccong207@gmail.com>
2024-10-29 20:35:58 +0700

    Revert rack attack in website live-chat

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -121,20 +121,17 @@ class Rack::Attack
 
     ## Prevent Contact update Bombing in Widget API ###
     throttle('api/v1/widget/contacts', limit: 60, period: 1.hour) do |req|
-      website_token = ActionDispatch::Request.new(req.env).params['website_token']
-      website_token || req.ip if req.path_without_extentions == '/api/v1/widget/contacts' && (req.patch? || req.put?)
+      req.ip if req.path_without_extentions == '/api/v1/widget/contacts' && (req.patch? || req.put?)
     end
 
     ## Prevent Conversation Bombing through multiple sessions
     throttle('widget?website_token={website_token}&cw_conversation={x-auth-token}', limit: 5, period: 1.hour) do |req|
-      website_token = ActionDispatch::Request.new(req.env).params['website_token']
-      website_token || req.ip if req.path_without_extentions == '/widget' && ActionDispatch::Request.new(req.env).params['cw_conversation'].blank?
+      req.ip if req.path_without_extentions == '/widget' && ActionDispatch::Request.new(req.env).params['cw_conversation'].blank?
     end
 
     ## Prevent Conversation Bombing through multiple sessions
     throttle('widget?website_token={website_token}', limit: 5, period: 1.hour) do |req|
-      website_token = ActionDispatch::Request.new(req.env).params['website_token']
-      website_token || req.ip if req.path_without_extentions == '/widget'
+      req.ip if req.path_without_extentions == '/widget'
     end
   end
 
