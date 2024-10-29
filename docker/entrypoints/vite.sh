3cc73e38349899f3bd59c8e0398364e7a183ae1e
Vishnu Narayanan <iamwishnu@gmail.com>
2024-10-29 15:16:10 +0530

    fix: pnpm in vite docker (#10344)
    - Fix pnpm path in vite docker
    - Remove webpack files
    - fFx vite server port

--- docker/entrypoints/vite.sh
+++ docker/entrypoints/vite.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
-set -e
+set -x
 
 rm -rf /app/tmp/pids/server.pid
 rm -rf /app/tmp/cache/*
 
-pnpm install --frozen-lockfile
-
-echo "Waiting for pnpm and bundle integrity to match lockfiles...."
-PNPM="pnpm list --json | grep -q 'Missing dependencies'"
-BUNDLE="bundle check"
-
-until ! $PNPM && $BUNDLE
-do
-  sleep 2;
-done
+pnpm store prune
+pnpm install
 
 echo "Ready to run Vite development server."
 
