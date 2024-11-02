67c7d9d10c1092d5adcdf90b52fcefdd18777bbd
Leni Kadali <52788034+lenikadali@users.noreply.github.com>
2024-11-02 09:56:03 +0300

    Re-add content security policy config
    Re-added the content security policy config since
    it has been overwritten by the addition of Vite to Chatwoot

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,12 +4,12 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data
+  policy.img_src     :self, :https, :data
+  policy.object_src  :none
+  policy.script_src  :self, :https
 # Allow @vite/client to hot reload javascript changes in development
 #   policy.script_src *policy.script_src, :unsafe_eval, "http://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
 # You may need to enable this in production as well depending on your setup.
@@ -20,9 +20,9 @@
 # Allow @vite/client to hot reload changes in development
 #    policy.connect_src *policy.connect_src, "ws://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+  # Specify URI for violation reports
+  policy.report_uri "/csp-violation-report-endpoint"
+end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
