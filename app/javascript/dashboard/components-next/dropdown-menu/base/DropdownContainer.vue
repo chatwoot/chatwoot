4f2c4dabd0949b3ccb125655b605c9fe4504b445
Shivam Mishra <scm.mymail@gmail.com>
2024-11-06 16:19:00 +0530

    feat: don't use toggle
    Weird behaviour in Histoire

--- app/javascript/dashboard/components-next/dropdown-menu/base/DropdownContainer.vue
+++ app/javascript/dashboard/components-next/dropdown-menu/base/DropdownContainer.vue
@@ -1,14 +1,23 @@
 <script setup>
-import { useToggle } from '@vueuse/core';
+import { ref } from 'vue';
 import { provideDropdownContext } from './provider.js';
 
 const emit = defineEmits(['close']);
-const [isOpen, toggle] = useToggle(false);
+
+const isOpen = ref(false);
 
 const closeMenu = () => {
   if (isOpen.value) {
     emit('close');
-    toggle(false);
+    isOpen.value = false;
+  }
+};
+
+const toggle = newState => {
+  if (newState !== undefined) {
+    isOpen.value = newState;
+  } else {
+    isOpen.value = !isOpen.value;
   }
 };
 
@@ -20,9 +29,9 @@ provideDropdownContext({
 </script>
 
 <template>
-  <div class="relative z-20 space-y-2">
+  <div class="relative space-y-2">
     <slot name="trigger" :is-open :toggle="() => toggle()" />
-    <div v-if="isOpen" v-on-clickaway="closeMenu" class="absolute">
+    <div v-if="isOpen" v-on-clickaway="closeMenu" class="absolute z-20">
       <slot />
     </div>
   </div>
