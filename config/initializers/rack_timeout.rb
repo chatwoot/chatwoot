674eb9d27a29dcdc70662c3865ac6c68e0baf7e8
Vishnu Narayanan <vishnu@chatwoot.com>
2024-11-11 15:40:21 +0530

    chore: fix tests

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,5 +1,4 @@
-
-require "rack-timeout"
+require 'rack-timeout'
 
 # Reduce noise by filtering state=ready and state=completed which are logged at INFO level
 Rails.application.config.after_initialize do
