0db221a8518bfe1e7119e9e8b52581185095adbc
Shivam Mishra <scm.mymail@gmail.com>
2024-11-12 09:46:00 +0530

    feat: separate channel icon component

---
+++ app/javascript/dashboard/components-next/icon/ChannelIcon.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useChannelIcon } from './provider';
+import Icon from 'next/icon/Icon.vue';
+
+const props = defineProps({
+  inbox: {
+    type: Object,
+    required: true,
+  },
+});
+
+const channelIcon = useChannelIcon(props.inbox);
+</script>
+
+<template>
+  <Icon :icon="channelIcon" />
+</template>
