de73df20bca760f468e67eef8bde1df098d060c4
Shivam Mishra <scm.mymail@gmail.com>
2024-11-14 08:46:52 +0530

    feat: use popover instead of dialog

--- app/javascript/dashboard/components-next/filter/ConversationFilter.vue
+++ app/javascript/dashboard/components-next/filter/ConversationFilter.vue
@@ -2,7 +2,6 @@
 import {
   defineModel,
   useTemplateRef,
-  onMounted,
   onBeforeUnmount,
   computed,
   ref,
@@ -10,10 +9,10 @@ import {
 import { useI18n } from 'vue-i18n';
 import { useTrack } from 'dashboard/composables';
 import { useStore } from 'dashboard/composables/store';
+import { useKeyboardEvents } from 'dashboard/composables/useKeyboardEvents';
 import { CONVERSATION_EVENTS } from 'dashboard/helper/AnalyticsHelper/events';
 
 import Button from 'next/button/Button.vue';
-import Dialog from 'next/dialog/Dialog.vue';
 import ConditionRow from './ConditionRow.vue';
 
 const props = defineProps({
@@ -36,6 +35,7 @@ const folderNameLocal = ref(props.folderName);
 
 const { t } = useI18n();
 const store = useStore();
+
 const removeFilter = index => {
   filters.value.splice(index, 1);
 };
@@ -50,7 +50,6 @@ const addFilter = () => {
 };
 
 const conditionRefs = useTemplateRef('conditions');
-const dialogRef = useTemplateRef('dialog');
 
 function validateAndSubmit() {
   const isValid = conditionRefs.value
@@ -72,75 +71,76 @@ function validateAndSubmit() {
   });
 }
 
-onMounted(() => {
-  dialogRef.value.open();
-});
+onBeforeUnmount(() => emit('close'));
 
-onBeforeUnmount(() => {
-  dialogRef.value.close();
+const closeIfOpen = () => {
   emit('close');
-});
+};
 
 const filterModalHeaderTitle = computed(() => {
   return !props.isFolderView
     ? t('FILTER.TITLE')
     : t('FILTER.EDIT_CUSTOM_FILTER');
 });
+
+useKeyboardEvents({ Escape: { action: closeIfOpen } });
 </script>
 
 <template>
-  <Dialog ref="dialog" :title="filterModalHeaderTitle" @close="emit('close')">
-    <template #form>
-      <div v-if="props.isFolderView" class="">
-        <label class="border-b border-n-weak pb-6">
-          <div class="text-n-slate-11 text-sm mb-2">
-            {{ t('FILTER.FOLDER_LABEL') }}
-          </div>
-          <input
-            v-model="folderNameLocal"
-            class="py-1.5 px-3 text-n-slate-12 bg-n-alpha-1 text-sm rounded-lg reset-base w-full"
-            :placeholder="t('FILTER.INPUT_PLACEHOLDER')"
-          />
-        </label>
-      </div>
-      <ul class="grid gap-4 list-none">
-        <template v-for="(filter, index) in filters" :key="filter.id">
-          <ConditionRow
-            v-if="index === 0"
-            ref="conditions"
-            v-model:attribute-key="filter.attribute_key"
-            v-model:filter-operator="filter.filter_operator"
-            v-model:values="filter.values"
-            is-first
-            @remove="removeFilter(index)"
-          />
-          <ConditionRow
-            v-else
-            ref="conditions"
-            v-model:attribute-key="filter.attribute_key"
-            v-model:filter-operator="filter.filter_operator"
-            v-model:query-operator="filters[index - 1].query_operator"
-            v-model:values="filter.values"
-            @remove="removeFilter(index)"
-          />
-        </template>
-      </ul>
-    </template>
-
-    <template #footer>
-      <div class="flex gap-2 justify-between">
-        <Button sm ghost blue @click="addFilter">
-          {{ $t('FILTER.ADD_NEW_FILTER') }}
-        </Button>
-        <div class="flex gap-2">
-          <Button sm faded slate @click="filters = []">
-            {{ t('FILTER.CLEAR_BUTTON_LABEL') }}
-          </Button>
-          <Button sm solid blue @click="validateAndSubmit">
-            {{ t('FILTER.SUBMIT_BUTTON_LABEL') }}
-          </Button>
+  <div
+    v-on-clickaway="closeIfOpen"
+    popover
+    class="bg-n-alpha-3 grid gap-6 border border-n-weak p-6 rounded-xl overflow-visible w-[680px]"
+  >
+    <h3 class="text-base font-medium leading-6 text-n-slate-12">
+      {{ filterModalHeaderTitle }}
+    </h3>
+    <div v-if="props.isFolderView" class="">
+      <label class="border-b border-n-weak pb-6">
+        <div class="text-n-slate-11 text-sm mb-2">
+          {{ t('FILTER.FOLDER_LABEL') }}
         </div>
+        <input
+          v-model="folderNameLocal"
+          class="py-1.5 px-3 text-n-slate-12 bg-n-alpha-1 text-sm rounded-lg reset-base w-full"
+          :placeholder="t('FILTER.INPUT_PLACEHOLDER')"
+        />
+      </label>
+    </div>
+    <ul class="grid gap-4 list-none">
+      <template v-for="(filter, index) in filters" :key="filter.id">
+        <ConditionRow
+          v-if="index === 0"
+          ref="conditions"
+          v-model:attribute-key="filter.attribute_key"
+          v-model:filter-operator="filter.filter_operator"
+          v-model:values="filter.values"
+          is-first
+          @remove="removeFilter(index)"
+        />
+        <ConditionRow
+          v-else
+          ref="conditions"
+          v-model:attribute-key="filter.attribute_key"
+          v-model:filter-operator="filter.filter_operator"
+          v-model:query-operator="filters[index - 1].query_operator"
+          v-model:values="filter.values"
+          @remove="removeFilter(index)"
+        />
+      </template>
+    </ul>
+    <div class="flex gap-2 justify-between">
+      <Button sm ghost blue @click="addFilter">
+        {{ $t('FILTER.ADD_NEW_FILTER') }}
+      </Button>
+      <div class="flex gap-2">
+        <Button sm faded slate @click="filters = []">
+          {{ t('FILTER.CLEAR_BUTTON_LABEL') }}
+        </Button>
+        <Button sm solid blue @click="validateAndSubmit">
+          {{ t('FILTER.SUBMIT_BUTTON_LABEL') }}
+        </Button>
       </div>
-    </template>
-  </Dialog>
+    </div>
+  </div>
 </template>
