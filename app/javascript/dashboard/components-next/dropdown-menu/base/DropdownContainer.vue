db802c49b9b4bb1103c6e35d6b7940beb86ea6b0
Shivam Mishra <scm.mymail@gmail.com>
2024-11-19 09:21:36 +0530

    fix: use vueuse directive for outside click

--- app/javascript/dashboard/components-next/dropdown-menu/base/DropdownContainer.vue
+++ app/javascript/dashboard/components-next/dropdown-menu/base/DropdownContainer.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useToggle } from '@vueuse/core';
+import { vOnClickOutside } from '@vueuse/components';
 import { provideDropdownContext } from './provider.js';
 
 const emit = defineEmits(['close']);
@@ -22,7 +23,7 @@ provideDropdownContext({
 <template>
   <div class="relative z-20 space-y-2">
     <slot name="trigger" :is-open :toggle="() => toggle()" />
-    <div v-if="isOpen" v-on-clickaway="closeMenu" class="absolute">
+    <div v-if="isOpen" v-on-click-outside="closeMenu" class="absolute z-20">
       <slot />
     </div>
   </div>
