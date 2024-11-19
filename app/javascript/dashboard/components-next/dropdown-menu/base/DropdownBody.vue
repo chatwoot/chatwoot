fe98b84783e3e49644392503153baa25702ba607
Shivam Mishra <scm.mymail@gmail.com>
2024-11-20 03:59:45 +0530

    fix: Update the dropdown bg to match the design system (#10438)
    This PR updates the background used in dropdown to match our design system. Previous PR failed to add this correctly.
    
    ---------
    
    Co-authored-by: Pranav <pranav@chatwoot.com>

--- app/javascript/dashboard/components-next/dropdown-menu/base/DropdownBody.vue
+++ app/javascript/dashboard/components-next/dropdown-menu/base/DropdownBody.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="absolute">
     <ul
-      class="text-sm bg-n-solid-1 border border-n-weak rounded-xl shadow-sm py-2 n-dropdown-body gap-2 grid list-none px-2 reset-base"
+      class="text-sm bg-n-alpha-3 backdrop-blur-[100px] border border-n-weak rounded-xl shadow-sm py-2 n-dropdown-body gap-2 grid list-none px-2 reset-base"
     >
       <slot />
     </ul>
