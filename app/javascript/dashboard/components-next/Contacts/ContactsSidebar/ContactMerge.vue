2a724cad7c7b0ad7951859c8eee38be658227f13
iamsivin <iamsivin@gmail.com>
2024-11-21 14:52:19 +0530

    chore: Create contact merge component

--- app/javascript/dashboard/components-next/Contacts/ContactsSidebar/ContactMerge.vue
+++ app/javascript/dashboard/components-next/Contacts/ContactsSidebar/ContactMerge.vue
@@ -10,9 +10,8 @@ import ContactAPI from 'dashboard/api/contacts';
 import { debounce } from '@chatwoot/utils';
 import { CONTACTS_EVENTS } from 'dashboard/helper/AnalyticsHelper/events';
 
-import Avatar from 'dashboard/components-next/avatar/Avatar.vue';
 import Button from 'dashboard/components-next/button/Button.vue';
-import ComboBox from 'dashboard/components-next/combobox/ComboBox.vue';
+import ContactMergeForm from 'dashboard/components-next/Contacts/ContactsForm/ContactMergeForm.vue';
 
 const props = defineProps({
   selectedContact: {
@@ -110,82 +109,19 @@ const onMergeContacts = async () => {
         {{ t('CONTACTS_LAYOUT.SIDEBAR.MERGE.DESCRIPTION') }}
       </p>
     </div>
-    <div class="flex flex-col">
-      <div class="flex flex-col gap-2">
-        <div class="flex items-center justify-between h-5 gap-2">
-          <label class="text-sm text-n-slate-12">
-            {{ t('CONTACTS_LAYOUT.SIDEBAR.MERGE.PRIMARY') }}
-          </label>
-          <span
-            class="flex items-center justify-center w-24 h-5 text-xs rounded-md text-n-teal-11 bg-n-alpha-2"
-          >
-            {{ t('CONTACTS_LAYOUT.SIDEBAR.MERGE.PRIMARY_HELP_LABEL') }}
-          </span>
-        </div>
-        <ComboBox
-          id="inbox"
-          v-model="state.primaryContactId"
-          :options="primaryContactList"
-          :empty-state="
-            isSearching
-              ? t('CONTACTS_LAYOUT.SIDEBAR.MERGE.IS_SEARCHING')
-              : t('CONTACTS_LAYOUT.SIDEBAR.MERGE.EMPTY_STATE')
-          "
-          :search-placeholder="
-            t('CONTACTS_LAYOUT.SIDEBAR.MERGE.SEARCH_PLACEHOLDER')
-          "
-          :placeholder="t('CONTACTS_LAYOUT.SIDEBAR.MERGE.PLACEHOLDER')"
-          :has-error="!!v$.primaryContactId.$error"
-          :message="
-            v$.primaryContactId.$error
-              ? t('CONTACTS_LAYOUT.SIDEBAR.MERGE.PRIMARY_REQUIRED_ERROR')
-              : ''
-          "
-          class="[&>div>button]:bg-n-alpha-black2"
-          @search="onContactSearch"
-        />
-      </div>
-      <div class="relative flex justify-center gap-2 top-4">
-        <div v-for="i in 3" :key="i" class="relative w-4 h-8">
-          <div
-            class="absolute w-0 h-0 border-l-[4px] border-r-[4px] border-b-[6px] border-l-transparent border-r-transparent border-n-strong ltr:translate-x-[4px] rtl:-translate-x-[4px] -translate-y-[4px]"
-          />
-          <div
-            class="absolute w-[1px] h-full bg-n-strong left-1/2 transform -translate-x-1/2"
-          />
-        </div>
-      </div>
-      <div class="flex flex-col gap-2">
-        <div class="flex items-center justify-between h-5 gap-2">
-          <label class="text-sm text-n-slate-12">
-            {{ t('CONTACTS_LAYOUT.SIDEBAR.MERGE.PARENT') }}
-          </label>
-          <span
-            class="flex items-center justify-center w-24 h-5 text-xs rounded-md text-n-ruby-11 bg-n-alpha-2"
-          >
-            {{ t('CONTACTS_LAYOUT.SIDEBAR.MERGE.PARENT_HELP_LABEL') }}
-          </span>
-        </div>
-        <div
-          class="border border-n-strong h-[60px] gap-2 flex items-center rounded-xl p-3"
-        >
-          <Avatar
-            :name="selectedContact.name || ''"
-            :src="selectedContact.thumbnail || ''"
-            :size="32"
-            rounded-full
-          />
-          <div class="flex flex-col gap-1">
-            <span class="text-sm leading-4 truncate text-n-slate-11">
-              {{ selectedContact.name }}
-            </span>
-            <span class="text-sm leading-4 truncate text-n-slate-11">
-              {{ selectedContact.email }}
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ContactMergeForm
+      v-model:primary-contact-id="state.primaryContactId"
+      :selected-contact="selectedContact"
+      :primary-contact-list="primaryContactList"
+      :is-searching="isSearching"
+      :has-error="!!v$.primaryContactId.$error"
+      :error-message="
+        v$.primaryContactId.$error
+          ? t('CONTACTS_LAYOUT.SIDEBAR.MERGE.PRIMARY_REQUIRED_ERROR')
+          : ''
+      "
+      @search="onContactSearch"
+    />
     <div class="flex items-center justify-between gap-3">
       <Button
         variant="faded"
