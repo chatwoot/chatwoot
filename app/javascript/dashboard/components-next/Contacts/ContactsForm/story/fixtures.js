cb98905816114d2a3b1694e1cf73f4d6051463e9
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2024-11-21 10:48:25 +0530

    feat: Add Contact card and form component (#10466)
    Co-authored-by: Pranav <pranavrajs@gmail.com>

---
+++ app/javascript/dashboard/components-next/Contacts/ContactsForm/story/fixtures.js
@@ -0,0 +1,20 @@
+export default {
+  id: 370,
+  name: 'John Doe',
+  email: 'johndoe@chatwoot.com',
+  phoneNumber: '+918634322418',
+  additionalAttributes: {
+    city: 'Kerala',
+    country: 'India',
+    description: 'Curious about the web.',
+    companyName: 'Chatwoot',
+    countryCode: 'IN',
+    socialProfiles: {
+      github: 'johndoe',
+      twitter: 'johndoe',
+      facebook: 'johndoe',
+      linkedin: 'johndoe',
+      instagram: 'johndoe',
+    },
+  },
+};
