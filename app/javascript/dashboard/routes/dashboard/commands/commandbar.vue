0e1d0a6f345a213068abf3ab3408b47173c8297a
Parsaa404 <121176600+Parsaa404@users.noreply.github.com>
2024-11-22 06:28:07 +0800

    1.0.0

--- app/javascript/dashboard/routes/dashboard/commands/commandbar.vue
+++ app/javascript/dashboard/routes/dashboard/commands/commandbar.vue
@@ -96,7 +96,7 @@ onMounted(setCommandBarData);
 <style lang="scss">
 ninja-keys {
   --ninja-accent-color: var(--w-500);
-  --ninja-font-family: 'Inter';
+  --ninja-font-family: 'Vazirmatn';
   z-index: 9999;
 }
 
