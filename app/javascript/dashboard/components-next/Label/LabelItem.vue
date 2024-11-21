3e8ef635d0f6cd9e76b76f864138bdd9af43b94e
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2024-11-22 03:27:43 +0530

    feat: Add Label Input components (#10480)

---
+++ app/javascript/dashboard/components-next/Label/LabelItem.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  label: {
+    type: Object,
+    default: null,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="bg-n-alpha-2 rounded-md flex items-center h-7 w-fit py-1 ltr:pl-1 rtl:pr-1 ltr:pr-1.5 rtl:pl-1.5"
+  >
+    <div
+      class="w-2 h-2 m-1 rounded-sm"
+      :style="{ backgroundColor: label.color }"
+    />
+    <span class="text-sm text-n-slate-12">
+      {{ label.title }}
+    </span>
+  </div>
+</template>
