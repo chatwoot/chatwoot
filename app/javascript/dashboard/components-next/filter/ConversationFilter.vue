3cbd405a4ff0a0029616f702e4695ba33ee2d04b
Shivam Mishra <scm.mymail@gmail.com>
2024-11-27 09:54:33 +0530

    fix: z-index

--- app/javascript/dashboard/components-next/filter/ConversationFilter.vue
+++ app/javascript/dashboard/components-next/filter/ConversationFilter.vue
@@ -104,7 +104,7 @@ const outsideClickHandler = [
 <template>
   <div
     v-on-click-outside="outsideClickHandler"
-    class="z-[999] max-w-3xl lg:w-[750px] overflow-visible w-full border border-n-weak bg-n-alpha-3 backdrop-blur-[100px] shadow-lg rounded-xl p-6 grid gap-6"
+    class="z-40 max-w-3xl lg:w-[750px] overflow-visible w-full border border-n-weak bg-n-alpha-3 backdrop-blur-[100px] shadow-lg rounded-xl p-6 grid gap-6"
   >
     <h3 class="text-base font-medium leading-6 text-n-slate-12">
       {{ filterModalHeaderTitle }}
