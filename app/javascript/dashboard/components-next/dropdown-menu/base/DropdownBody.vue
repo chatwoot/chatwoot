fce41dff0ae0758ead41a39a43ec55e21c053562
Shivam Mishra <scm.mymail@gmail.com>
2024-11-27 09:26:04 +0530

    feat: allow strong border option

--- app/javascript/dashboard/components-next/dropdown-menu/base/DropdownBody.vue
+++ app/javascript/dashboard/components-next/dropdown-menu/base/DropdownBody.vue
@@ -1,7 +1,22 @@
+<script setup>
+import { computed } from 'vue';
+const { strong } = defineProps({
+  strong: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const borderClass = computed(() => {
+  return strong ? 'border-n-strong' : 'border-n-weak';
+});
+</script>
+
 <template>
   <div class="absolute">
     <ul
-      class="text-sm bg-n-alpha-3 backdrop-blur-[100px] border border-n-weak rounded-xl shadow-sm py-2 n-dropdown-body gap-2 grid list-none px-2 reset-base"
+      class="text-sm bg-n-slate-3 backdrop-blur-[100px] border rounded-xl shadow-sm py-2 n-dropdown-body gap-2 grid list-none px-2 reset-base"
+      :class="borderClass"
     >
       <slot />
     </ul>
