59481afc2c7d89b3b2886c3af01ad3a1154d8a4b
Shivam Mishra <scm.mymail@gmail.com>
2024-11-29 16:45:52 +0530

    feat: setup email content

--- app/javascript/dashboard/components-next/message/bubbles/Email.vue
+++ app/javascript/dashboard/components-next/message/bubbles/Email.vue
@@ -1,16 +1,25 @@
 <script setup>
+import { computed } from 'vue';
 import BaseBubble from 'next/message/bubbles/Base.vue';
 
-defineProps({
+const props = defineProps({
   content: {
     type: String,
     required: true,
   },
+  contentAttributes: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+
+const contentToShow = computed(() => {
+  return props.contentAttributes?.email?.htmlContent?.full ?? props.content;
 });
 </script>
 
 <template>
   <BaseBubble class="w-full">
-    <div class="p-4 prose prose-sm max-w-none" v-html="content" />
+    <div class="p-4 prose prose-sm max-w-none" v-html="contentToShow" />
   </BaseBubble>
 </template>
