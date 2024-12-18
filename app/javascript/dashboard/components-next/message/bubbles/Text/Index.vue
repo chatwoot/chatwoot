e310eef64f15fe95a5eb58831a6d1af5096b3090
Shivam Mishra <scm.mymail@gmail.com>
2024-12-18 14:20:20 +0530

    fix: warning

--- app/javascript/dashboard/components-next/message/bubbles/Text/Index.vue
+++ app/javascript/dashboard/components-next/message/bubbles/Text/Index.vue
@@ -20,7 +20,7 @@ import { MESSAGE_TYPES } from '../../constants';
  */
 const props = defineProps({
   content: {
-    type: String,
+    type: [String, null],
     required: true,
   },
   attachments: {
