154bff455534f687ffba7018bb0ad92fade1b525
Shivam Mishra <scm.mymail@gmail.com>
2024-12-18 14:27:53 +0530

    fix: white-alpha color

--- app/javascript/dashboard/components-next/message/bubbles/Audio.vue
+++ app/javascript/dashboard/components-next/message/bubbles/Audio.vue
@@ -36,9 +36,6 @@ const attachment = computed(() => {
 
 <template>
   <BaseBubble class="bg-transparent" data-bubble-name="audio">
-    <AudioChip
-      :attachment="attachment"
-      class="p-2 text-n-slate-12 bg-n-alpha-3"
-    />
+    <AudioChip :attachment="attachment" class="p-2 text-n-slate-12" />
   </BaseBubble>
 </template>
