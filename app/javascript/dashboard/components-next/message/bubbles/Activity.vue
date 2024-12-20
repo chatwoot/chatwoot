ddddcc4d9238517667a6d48985dec86712da5e7e
Shivam Mishra <scm.mymail@gmail.com>
2024-12-20 13:08:25 +0530

    feat: add title to activity content

--- app/javascript/dashboard/components-next/message/bubbles/Activity.vue
+++ app/javascript/dashboard/components-next/message/bubbles/Activity.vue
@@ -16,7 +16,7 @@ const readableTime = computed(() =>
     class="px-2 py-0.5 !rounded-full flex items-center gap-2"
     data-bubble-name="activity"
   >
-    <span v-dompurify-html="content" class="truncate" />
+    <span v-dompurify-html="content" :title="content" class="truncate" />
     <div v-if="readableTime" class="w-px h-3 rounded-full bg-n-slate-7" />
     <time class="text-n-slate-10 truncate flex-shrink" :title="readableTime">
       {{ readableTime }}
