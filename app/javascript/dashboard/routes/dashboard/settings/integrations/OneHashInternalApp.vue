732e45c80941dd71550bb3e8307e71b154a3b60a
Arjun3492 <arjun.kohli.330@gmail.com>
2024-12-24 13:59:41 +0530

    chore:cal-chat integration ui/ux improvements

--- app/javascript/dashboard/routes/dashboard/settings/integrations/OneHashInternalApp.vue
+++ app/javascript/dashboard/routes/dashboard/settings/integrations/OneHashInternalApp.vue
@@ -22,6 +22,7 @@ export default {
       showCalIntegrationPopup: false,
       loading: false,
       cal_user_slug: '',
+      requestRaised: false,
     };
   },
   computed: {
@@ -76,14 +77,12 @@ export default {
         }
 
         this.closeCalIntegrationPopup();
-        const res = await this.$store.dispatch(
-          'integrations/addOneHashIntegration',
-          {
-            integrationId: this.integrationId,
-            slug: this.cal_user_slug,
-          }
-        );
-        useAlert(res.data.message);
+        await this.$store.dispatch('integrations/addOneHashIntegration', {
+          integrationId: this.integrationId,
+          slug: this.cal_user_slug,
+        });
+        useAlert(this.$t('INTEGRATION_SETTINGS.ONEHASH_CAL.API.MESSAGE'));
+        this.requestRaised = true;
       } catch (error) {
         useAlert(this.$t('INTEGRATION_SETTINGS.ADD.API.ERROR_MESSAGE'));
       } finally {
@@ -103,101 +102,111 @@ export default {
 </script>
 
 <template>
-  <div
-    class="flex flex-col items-start justify-between md:flex-row md:items-center shadow-md rounded-md py-2"
-  >
-    <div class="flex items-center justify-start flex-1 m-0 mx-4">
-      <img
-        :src="`/dashboard/images/integrations/${integrationId}.png`"
-        class="w-16 h-16 p-2 mr-4"
-      />
-      <div>
-        <h3 class="mb-1 text-xl font-medium text-slate-800 dark:text-slate-100">
-          {{ integrationName }}
-        </h3>
-        <p class="text-slate-700 dark:text-slate-200">
-          {{
-            useInstallationName(
-              integrationDescription,
-              globalConfig.installationName
-            )
-          }}
-        </p>
+  <div>
+    <div
+      class="flex flex-col items-start justify-between md:flex-row md:items-center shadow-md rounded-md py-2"
+    >
+      <div class="flex items-center justify-start flex-1 m-0 mx-4">
+        <img
+          :src="`/dashboard/images/integrations/${integrationId}.png`"
+          class="w-16 h-16 p-2 mr-4"
+        />
+        <div>
+          <h3
+            class="mb-1 text-xl font-medium text-slate-800 dark:text-slate-100"
+          >
+            {{ integrationName }}
+          </h3>
+          <p class="text-slate-700 dark:text-slate-200">
+            {{
+              useInstallationName(
+                integrationDescription,
+                globalConfig.installationName
+              )
+            }}
+          </p>
+        </div>
       </div>
-    </div>
-    <div class="flex justify-center items-center mb-0 w-[15%]">
-      <div v-if="integrationEnabled">
-        <woot-button
-          variant="smooth"
-          color-scheme="danger"
-          icon="delete"
-          @click="openDeletePopup"
-        >
-          {{
-            deleteButtonText ||
-            $t('INTEGRATION_SETTINGS.WEBHOOK.DELETE.BUTTON_TEXT')
-          }}
-        </woot-button>
-        <!-- <button class="button alert" @click="openDeletePopup">
+      <div class="flex justify-center items-center mb-0 w-[15%]">
+        <div v-if="integrationEnabled">
+          <woot-button
+            variant="smooth"
+            color-scheme="danger"
+            icon="delete"
+            @click="openDeletePopup"
+          >
+            {{
+              deleteButtonText ||
+              $t('INTEGRATION_SETTINGS.WEBHOOK.DELETE.BUTTON_TEXT')
+            }}
+          </woot-button>
+          <!-- <button class="button alert" @click="openDeletePopup">
           {{
             deleteButtonText ||
             $t('INTEGRATION_SETTINGS.WEBHOOK.DELETE.BUTTON_TEXT')
           }}
         </button> -->
+        </div>
+        <div v-else>
+          <woot-button
+            variant="smooth"
+            color-scheme="secondary"
+            icon="plus-sign"
+            :is-loading="loading"
+            @click="handleConfigure"
+          >
+            {{ $t('INTEGRATION_SETTINGS.WEBHOOK.CONFIGURE') }}
+          </woot-button>
+        </div>
       </div>
-      <div v-else>
-        <woot-button
-          variant="smooth"
-          color-scheme="secondary"
-          icon="plus-sign"
-          :is-loading="loading"
-          @click="handleConfigure"
-        >
-          {{ $t('INTEGRATION_SETTINGS.WEBHOOK.CONFIGURE') }}
-        </woot-button>
-      </div>
+
+      <woot-delete-modal
+        :show.sync="showDeleteConfirmationPopup"
+        :on-close="closeDeletePopup"
+        :on-confirm="confirmDeletion"
+        :title="
+          deleteConfirmationText.title ||
+          $t('INTEGRATION_SETTINGS.WEBHOOK.DELETE.CONFIRM.TITLE')
+        "
+        :message="
+          deleteConfirmationText.message ||
+          $t('INTEGRATION_SETTINGS.WEBHOOK.DELETE.CONFIRM.MESSAGE')
+        "
+        :confirm-text="$t('INTEGRATION_SETTINGS.WEBHOOK.DELETE.CONFIRM.YES')"
+        :reject-text="$t('INTEGRATION_SETTINGS.WEBHOOK.DELETE.CONFIRM.NO')"
+      />
+
+      <woot-modal
+        :show.sync="showCalIntegrationPopup"
+        :on-close="closeCalIntegrationPopup"
+      >
+        <div class="bg-white p-6 rounded-lg text-center">
+          <!-- Modal Title -->
+          <h2 class="text-xl font-semibold mb-4">
+            {{ $t('INTEGRATION_SETTINGS.ONEHASH_CAL.ADD_TITLE') }}
+          </h2>
+
+          <p class="text-gray-700 mb-4">
+            {{ $t('INTEGRATION_SETTINGS.ONEHASH_CAL.ADD_MESSAGE') }}
+          </p>
+
+          <input
+            v-model="cal_user_slug"
+            class="w-full p-2 mb-4 border border-gray-300 rounded-md"
+            type="text"
+            @keydown.enter="addCalIntegration"
+          />
+
+          <button class="button nice" @click="addCalIntegration">
+            {{ $t('INTEGRATION_SETTINGS.ONEHASH_CAL.PROCEED') }}
+          </button>
+        </div>
+      </woot-modal>
     </div>
-    <woot-delete-modal
-      :show.sync="showDeleteConfirmationPopup"
-      :on-close="closeDeletePopup"
-      :on-confirm="confirmDeletion"
-      :title="
-        deleteConfirmationText.title ||
-        $t('INTEGRATION_SETTINGS.WEBHOOK.DELETE.CONFIRM.TITLE')
-      "
-      :message="
-        deleteConfirmationText.message ||
-        $t('INTEGRATION_SETTINGS.WEBHOOK.DELETE.CONFIRM.MESSAGE')
-      "
-      :confirm-text="$t('INTEGRATION_SETTINGS.WEBHOOK.DELETE.CONFIRM.YES')"
-      :reject-text="$t('INTEGRATION_SETTINGS.WEBHOOK.DELETE.CONFIRM.NO')"
-    />
-
-    <woot-modal
-      :show.sync="showCalIntegrationPopup"
-      :on-close="closeCalIntegrationPopup"
-    >
-      <div class="bg-white p-6 rounded-lg text-center">
-        <!-- Modal Title -->
-        <h2 class="text-xl font-semibold mb-4">
-          {{ $t('INTEGRATION_SETTINGS.ONEHASH_CAL.ADD_TITLE') }}
-        </h2>
-
-        <p class="text-gray-700 mb-4">
-          {{ $t('INTEGRATION_SETTINGS.ONEHASH_CAL.ADD_MESSAGE') }}
-        </p>
-
-        <input
-          v-model="cal_user_slug"
-          @keydown.enter="addCalIntegration"
-          type="text"
-          class="w-full p-2 mb-4 border border-gray-300 rounded-md"
-        />
-
-        <button class="button nice" @click="addCalIntegration">
-          {{ $t('INTEGRATION_SETTINGS.ONEHASH_CAL.PROCEED') }}
-        </button>
-      </div>
-    </woot-modal>
+    <p v-if="requestRaised" class="mx-3 mt-2 text-sm text-gray-500">
+      {{
+        'Note : After accepting request from Cal, please refresh this page to view changes'
+      }}
+    </p>
   </div>
 </template>
