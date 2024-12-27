eca029889dae857fa0e18ee150cff8417ffefad5
Vishnu Narayanan <vishnu@chatwoot.com>
2024-12-28 00:44:00 +0530

    fix: throttle reports api endpoint

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -154,6 +154,12 @@ class Rack::Attack
     match_data[:account_id] if match_data.present?
   end
 
+  ## Prevent abuse of reports api
+  throttle('/api/v2/accounts/:account_id/reports', limit: ENV.fetch('RATE_LIMIT_REPORTS_API', '100').to_i, period: 1.minute) do |req|
+    match_data = %r{/api/v2/accounts/(?<account_id>\d+)/reports}.match(req.path)
+    match_data[:account_id] if match_data.present?
+  end
+
   ## ----------------------------------------------- ##
 end
 
