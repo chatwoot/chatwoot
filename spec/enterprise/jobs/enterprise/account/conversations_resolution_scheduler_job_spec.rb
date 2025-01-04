e927599602ec8b4e3defacf9aea9e635ef192be7
Pranav <pranavrajs@gmail.com>
2025-01-04 14:58:15 -0800

    Remove response bot related changes

--- spec/enterprise/jobs/enterprise/account/conversations_resolution_scheduler_job_spec.rb
+++ spec/enterprise/jobs/enterprise/account/conversations_resolution_scheduler_job_spec.rb
@@ -7,26 +7,6 @@ RSpec.describe Account::ConversationsResolutionSchedulerJob, type: :job do
   let!(:inbox_without_bot) { create(:inbox, account: account_without_bot) }
   let(:response_source) { create(:response_source, account: account_with_bot) }
 
-  describe '#perform - response bot resolutions' do
-    before do
-      skip_unless_response_bot_enabled_test_environment
-      account_with_bot.enable_features!(:response_bot)
-      create(:inbox_response_source, inbox: inbox_with_bot, response_source: response_source)
-    end
-
-    it 'enqueues resolution jobs only for inboxes with response bot enabled' do
-      expect do
-        described_class.perform_now
-      end.to have_enqueued_job(Captain::InboxPendingConversationsResolutionJob).with(inbox_with_bot).and have_enqueued_job.exactly(:once)
-    end
-
-    it 'does not enqueue resolution jobs for inboxes without response bot enabled' do
-      expect do
-        described_class.perform_now
-      end.not_to have_enqueued_job(Captain::InboxPendingConversationsResolutionJob).with(inbox_without_bot)
-    end
-  end
-
   describe '#perform - captain resolutions' do
     before do
       create(:integrations_hook, app_id: 'captain', account: account_with_bot, settings: {
