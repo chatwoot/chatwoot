4dc1275c53cbfda1161ff64c1df8a866dcfbfd14
Vishnu Narayanan <vishnu@chatwoot.com>
2025-01-08 14:18:09 +0530

    chore: refactor

--- 2.sh
+++
@@ -1,9 +0,0 @@
-curl --location --request GET 'http://localhost:3000/api/v2/accounts/1/reports/agents?since=1603823400&until=1604341800' \
---header 'api_access_token: R5sCYvpAYqdpQc7iR2FnVF4q' \
---header 'Content-Type: text/plain' \
---header 'Cookie: __profilin=p%3Dt' \
---data-raw '{
-  "since" =>  "1603823400",
-  "until"=> "1604341800"
-
-}'
