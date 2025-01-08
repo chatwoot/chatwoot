f3bc019ef2fbfac1ce7a281c5b5888e21ebbeb1e
Muhsin Keloth <muhsinkeramam@gmail.com>
2025-01-08 21:25:11 +0530

    chore: Configure iOS universal linking (#10651)
    - Moved `apple-app-site-association` to
    `.well-known/apple-app-site-association`
    https://docs.expo.dev/linking/ios-universal-links/#create-aasa-file
    - Updated the paths pattern to accept conversation links only.

--- config/routes.rb
+++ config/routes.rb
@@ -461,8 +461,8 @@ Rails.application.routes.draw do
 
   # ----------------------------------------------------------------------
   # Routes for external service verifications
-  get 'apple-app-site-association' => 'apple_app#site_association'
   get '.well-known/assetlinks.json' => 'android_app#assetlinks'
+  get '.well-known/apple-app-site-association' => 'apple_app#site_association'
   get '.well-known/microsoft-identity-association.json' => 'microsoft#identity_association'
 
   # ----------------------------------------------------------------------
