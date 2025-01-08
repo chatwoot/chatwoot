3aad1d0f4e4cdac0c4a9647b55dadfbd6a1b055a
Shivam Mishra <scm.mymail@gmail.com>
2025-01-08 13:58:27 +0530

    fix: block quote

--- app/javascript/dashboard/components-next/message/bubbles/Text/FormattedContent.vue
+++ app/javascript/dashboard/components-next/message/bubbles/Text/FormattedContent.vue
@@ -26,6 +26,6 @@ const formattedContent = computed(() => {
 <template>
   <span
     v-dompurify-html="formattedContent"
-    class="[&_.link]:text-n-slate-11 [&_.link]:underline [&>p:last-child]:mb-0 [&>ul]:list-inside [&>ol]:list-inside [&>dl]:list-inside [&>ol]:list-decimal [&>ul]:list-disc"
+    class="[&_.link]:text-n-slate-11 [&_.link]:underline [&>p:last-child]:mb-0 [&>ul]:list-inside [&>ol]:list-inside [&>dl]:list-inside [&>ol]:list-decimal [&>ul]:list-disc [&>blockquote]:border-l-4 [&>blockquote]:border-n-alpha-black1 [&>blockquote]:pl-2"
   />
 </template>
