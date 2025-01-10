e70f7a2550b41a7e829401c29d777eb7fa8807e1
Shivam Mishra <scm.mymail@gmail.com>
2025-01-10 15:40:12 +0530

    fix: Bubble typography (#10653)
    ![CleanShot 2025-01-08 at 18 22
    36@2x](https://github.com/user-attachments/assets/77cc99d2-1bdf-49ff-9016-57bb9c50b149)

--- app/javascript/dashboard/components-next/message/bubbles/Text/FormattedContent.vue
+++ app/javascript/dashboard/components-next/message/bubbles/Text/FormattedContent.vue
@@ -24,8 +24,5 @@ const formattedContent = computed(() => {
 </script>
 
 <template>
-  <span
-    v-dompurify-html="formattedContent"
-    class="[&_.link]:text-n-slate-11 [&_.link]:underline [&>p:last-child]:mb-0 [&>ul]:list-inside [&>ol]:list-inside [&>dl]:list-inside [&>ol]:list-decimal [&>ul]:list-disc [&>blockquote]:border-l-4 [&>blockquote]:border-n-alpha-black1 [&>blockquote]:pl-2"
-  />
+  <span v-dompurify-html="formattedContent" class="prose prose-bubble" />
 </template>
