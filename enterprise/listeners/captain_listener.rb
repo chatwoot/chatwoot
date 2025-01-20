b1ddcb946e694443f45085f3324b09d771f4af0b
Shivam Mishra <scm.mymail@gmail.com>
2025-01-20 16:55:42 +0530

    feat: only check for captain_active

--- enterprise/listeners/captain_listener.rb
+++ enterprise/listeners/captain_listener.rb
@@ -3,25 +3,7 @@ class CaptainListener < BaseListener
     conversation = extract_conversation_and_account(event)[0]
     return unless conversation.inbox.captain_active?
 
-    generate_notes(assistant, conversation) if assistant.config['feature_memory'].present?
-    generate_faqs(assistant, conversation) if assistant.config['feature_faq'].present?
-  end
-
-  private
-
-  def generate_notes(assistant, conversation)
-    Captain::Llm::ContactNotesService.new(assistant, conversation).generate_and_update_notes
-
-    # account = conversation.account
-    # Rails.logger.info("[CAPTAIN][ContactNotesService] Incrementing response usage for #{account.id}")
-    # account.increment_response_usage
-  end
-
-  def generate_faqs(assistant, conversation)
-    Captain::Llm::ConversationFaqService.new(assistant, conversation).generate_and_deduplicate
-
-    # account = conversation.account
-    # Rails.logger.info("[CAPTAIN][ConversationFaqService] Incrementing response usage for #{account.id}")
-    # account.increment_response_usage
+    Captain::Llm::ContactNotesService.new(assistant, conversation).generate_and_update_notes if assistant.config['feature_memory'].present?
+    Captain::Llm::ConversationFaqService.new(assistant, conversation).generate_and_deduplicate if assistant.config['feature_faq'].present?
   end
 end
