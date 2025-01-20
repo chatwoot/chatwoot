cd72aa7bbff4c73ba37c1856c32db655c1885065
Shivam Mishra <scm.mymail@gmail.com>
2025-01-20 17:48:06 +0530

    fix: listener

--- enterprise/listeners/captain_listener.rb
+++ enterprise/listeners/captain_listener.rb
@@ -1,6 +1,7 @@
 class CaptainListener < BaseListener
   def conversation_resolved(event)
     conversation = extract_conversation_and_account(event)[0]
+    assistant = conversation.inbox.captain_assistant
     return unless conversation.inbox.captain_active?
 
     Captain::Llm::ContactNotesService.new(assistant, conversation).generate_and_update_notes if assistant.config['feature_memory'].present?
