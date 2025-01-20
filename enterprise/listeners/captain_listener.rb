eae815993670c83fb5b81b46218e19a98776c474
Shivam Mishra <scm.mymail@gmail.com>
2025-01-20 15:44:04 +0530

    feat: increment usage for notes and faqs

--- enterprise/listeners/captain_listener.rb
+++ enterprise/listeners/captain_listener.rb
@@ -1,9 +1,24 @@
 class CaptainListener < BaseListener
   def conversation_resolved(event)
     conversation = extract_conversation_and_account(event)[0]
+    account = conversation.account
+    inbox = conversation.inbox
+
     return unless conversation.inbox.captain_active?
 
-    Captain::Llm::ContactNotesService.new(assistant, conversation).generate_and_update_notes if assistant.config['feature_memory'].present?
-    Captain::Llm::ConversationFaqService.new(assistant, conversation).generate_and_deduplicate if assistant.config['feature_faq'].present?
+    generate_notes(assistant, conversation) if assistant.config['feature_memory'].present?
+    generate_faqs(assistant, conversation) if assistant.config['feature_faq'].present?
+  end
+
+  private
+
+  def generate_notes(assistant, conversation)
+    Captain::Llm::ContactNotesService.new(assistant, conversation).generate_and_update_notes
+    conversation.account.increment_response_usage
+  end
+
+  def generate_faqs(assistant, conversation)
+    Captain::Llm::ConversationFaqService.new(assistant, conversation).generate_and_deduplicate
+    conversation.account.increment_response_usage
   end
 end
