08e1ed2a0f1eeb01178ed8dfb6dcca8ec67b769a
Shivam Mishra <scm.mymail@gmail.com>
2025-01-22 20:15:03 +0530

    feat: add banner for document and responses limit

---
+++ app/javascript/dashboard/components-next/captain/pageComponents/response/LimitBanner.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useAccount } from 'dashboard/composables/useAccount';
+import { useRouter } from 'vue-router';
+
+import Banner from 'dashboard/components-next/banner/Banner.vue';
+
+const router = useRouter();
+const { accountId } = useAccount();
+
+const openBilling = () => {
+  router.push({
+    name: 'billing_settings_index',
+    params: { accountId: accountId.value },
+  });
+};
+</script>
+
+<template>
+  <Banner
+    color="amber"
+    :action-label="$t('CAPTAIN.PAYWALL.UPGRADE_NOW')"
+    @action="openBilling"
+  >
+    {{ $t('CAPTAIN.BANNER.RESPONSES') }}
+  </Banner>
+</template>
