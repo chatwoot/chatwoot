85939a7a14c7dfa48bce0ef1bcb4e163a5360e37
Shivam Mishra <scm.mymail@gmail.com>
2025-01-22 18:15:54 +0530

    feat: add captain paywall component

---
+++ app/javascript/dashboard/components-next/captain/pageComponents/Paywall.vue
@@ -0,0 +1,19 @@
+<script setup>
+import BasePaywallModal from 'dashboard/routes/dashboard/settings/components/BasePaywallModal.vue';
+
+const emit = defineEmits(['upgrade']);
+</script>
+
+<template>
+  <div
+    class="w-full max-w-[960px] mx-auto h-full max-h-[448px] grid place-content-center"
+  >
+    <BasePaywallModal
+      class="mx-auto"
+      feature-prefix="CAPTAIN"
+      i18n-key="PAYWALL"
+      is-on-chatwoot-cloud
+      @upgrade="emit('upgrade')"
+    />
+  </div>
+</template>
