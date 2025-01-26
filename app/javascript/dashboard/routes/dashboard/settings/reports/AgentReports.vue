feaf75f5beda82317ca8211b9c20b1008760152a
Pranav <pranavrajs@gmail.com>
2025-01-26 15:45:31 -0800

    Add a feature flag

---
+++ app/javascript/dashboard/routes/dashboard/settings/reports/AgentReports.vue
@@ -0,0 +1,14 @@
+<script setup>
+import WootReports from './components/WootReports.vue';
+</script>
+
+<template>
+  <WootReports
+    key="agent-reports"
+    type="agent"
+    getter-key="agents/getAgents"
+    action-key="agents/get"
+    :download-button-label="$t('REPORT.DOWNLOAD_AGENT_REPORTS')"
+    :report-title="$t('AGENT_REPORTS.HEADER')"
+  />
+</template>
