57f65c926d62f45284a2c96464b56c86f2c040aa
Shivam Mishra <scm.mymail@gmail.com>
2025-01-27 16:53:37 +0530

    feat: update policy component

--- app/javascript/dashboard/components/policy.vue
+++ app/javascript/dashboard/components/policy.vue
@@ -15,17 +15,28 @@ const props = defineProps({
     type: String,
     default: null,
   },
+  installationTypes: {
+    type: Array,
+    default: null,
+  },
 });
 
-const { checkFeatureAllowed, checkPermissions } = usePolicy();
+const { checkFeatureAllowed, checkPermissions, checkInstallationType } =
+  usePolicy();
 
 const isFeatureAllowed = computed(() => checkFeatureAllowed(props.featureFlag));
 const hasPermission = computed(() => checkPermissions(props.permissions));
+const matchesInstallationType = computed(() =>
+  checkInstallationType(props.installationTypes)
+);
 </script>
 
 <!-- eslint-disable vue/no-root-v-if -->
 <template>
-  <component :is="as" v-if="isFeatureAllowed && hasPermission">
+  <component
+    :is="as"
+    v-if="isFeatureAllowed && hasPermission && matchesInstallationType"
+  >
     <slot />
   </component>
 </template>
