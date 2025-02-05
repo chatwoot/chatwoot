fc0456e0b9f2f4cb8538385bc0ec6685cf56db17
Shivam Mishra <scm.mymail@gmail.com>
2025-02-05 17:41:26 +0530

    fix: audio chip default if time is not present

--- app/javascript/dashboard/components-next/message/chips/Audio.vue
+++ app/javascript/dashboard/components-next/message/chips/Audio.vue
@@ -38,9 +38,10 @@ onMounted(() => {
 });
 
 const formatTime = time => {
+  if (!time || Number.isNaN(time)) return '00:00';
   const minutes = Math.floor(time / 60);
   const seconds = Math.floor(time % 60);
-  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 };
 
 const toggleMute = () => {
