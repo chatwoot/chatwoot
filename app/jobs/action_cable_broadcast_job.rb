8c53fef9515a357ee0649052e0560686d4257735
Pranav <pranavrajs@gmail.com>
2025-02-10 22:47:35 -0800

    chore: Reload conversation data in ActionCableBroadcastJob before sending

--- app/jobs/action_cable_broadcast_job.rb
+++ app/jobs/action_cable_broadcast_job.rb
@@ -2,8 +2,31 @@ class ActionCableBroadcastJob < ApplicationJob
   queue_as :critical
 
   def perform(members, event_name, data)
+    return if members.blank?
+
+    broadcast_data = prepare_broadcast_data(event_name, data)
+    broadcast_to_members(members, event_name, broadcast_data)
+  end
+
+  private
+
+  def prepare_broadcast_data(event_name, data)
+    return data unless event_name == 'conversation.updated'
+
+    account = Account.find(data[:account_id])
+    conversation = account.conversations.find_by!(display_id: data[:id])
+    conversation.push_event_data.merge(account_id: data[:account_id])
+  end
+
+  def broadcast_to_members(members, event_name, broadcast_data)
     members.each do |member|
-      ActionCable.server.broadcast(member, { event: event_name, data: data })
+      ActionCable.server.broadcast(
+        member,
+        {
+          event: event_name,
+          data: broadcast_data
+        }
+      )
     end
   end
 end
