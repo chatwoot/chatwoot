3415fea5e7c647d0d9770e3a03fb53340cac2f2a
Khoa Nguyen <thanhkhoait@gmail.com>
2025-02-12 14:04:43 +0700

    feat: add Blazer and RailsExecution integrations

--- lib/rails_execution/file_reader.rb
+++ lib/rails_execution/file_reader.rb
@@ -1,10 +1,14 @@
-class RailsExecution::FileReader < RailsExecution::Files::Reader
-  # Output: {
-  #   'File1' => 'url',
-  #   'FileZ' => 'url',
-  # }
-  def call
-    # YourAttachment.where(task: task).pluck(:name, :url).to_h
-    {}
+module RailsExecution
+  class FileReader < RailsExecution::Files::Reader
+
+    # Output: {
+    #   'File1' => 'url',
+    #   'FileZ' => 'url',
+    # }
+    def call
+      # YourAttachment.where(task: task).pluck(:name, :url).to_h
+      return {}
+    end
+
   end
 end
--- lib/rails_execution/file_uploader.rb
+++ lib/rails_execution/file_uploader.rb
@@ -1,7 +1,11 @@
-class RailsExecution::FileUploader < RailsExecution::Files::Uploader
-  def call
-    files.each do |item|
-      # YourAttachment.create!(task: task, name: item.name, file: item.file)
+module RailsExecution
+  class FileUploader < RailsExecution::Files::Uploader
+
+    def call
+      files.each do |item|
+        # YourAttachment.create!(task: task, name: item.name, file: item.file)
+      end
     end
+
   end
 end
