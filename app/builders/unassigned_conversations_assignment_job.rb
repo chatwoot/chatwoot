f0f7be19be8835d11b033827f1249a75813be318
Jay Panchal <66618767+JayP09@users.noreply.github.com>
2025-02-13 09:40:39 +0530

    fix: Added logic to check if the team is assigned to conversation then add intersection of the team ids and conversation inbox ids (#179)

--- app/builders/unassigned_conversations_assignment_job.rb
+++ app/builders/unassigned_conversations_assignment_job.rb
@@ -15,7 +15,15 @@ class UnassignedConversationsAssignmentJob < ApplicationJob
     unassigned_conversations = inbox.conversations.unassigned
 
     unassigned_conversations.each do |conversation|
-      ::AutoAssignment::AgentAssignmentService.new(conversation: conversation, allowed_agent_ids: inbox.member_ids_with_assignment_capacity).perform
+      allowed_agent_ids = inbox.member_ids_with_assignment_capacity
+
+      if conversation.team_id?
+        team_id = conversation.team_id
+        team = Team.find_by(id: team_id)
+        allowed_agent_ids &= team.members.ids if team.present?
+      end
+
+      ::AutoAssignment::AgentAssignmentService.new(conversation: conversation, allowed_agent_ids: allowed_agent_ids).perform
     end
 
     Rails.cache.delete(cache_key)
