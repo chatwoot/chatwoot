6f94a4214270cef034c2b52f78b2e4c92d7ff8e4
Shivam Mishra <scm.mymail@gmail.com>
2025-02-20 14:23:46 +0530

    chore: revert annotation changes

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -9,23 +9,23 @@
 #  auto_assignment_config        :jsonb
 #  business_name                 :string
 #  channel_type                  :string
-#  csat_survey_enabled          :boolean          default(FALSE)
+#  csat_survey_enabled           :boolean          default(FALSE)
 #  email_address                 :string
-#  enable_auto_assignment       :boolean          default(TRUE)
-#  enable_email_collect         :boolean          default(TRUE)
-#  greeting_enabled             :boolean          default(FALSE)
-#  greeting_message             :string
-#  lock_to_single_conversation  :boolean          default(FALSE), not null
-#  name                         :string           not null
-#  out_of_office_message        :string
-#  sender_name_type             :integer          default("friendly"), not null
-#  timezone                     :string           default("UTC")
-#  working_hours_enabled        :boolean          default(FALSE)
-#  created_at                   :datetime         not null
-#  updated_at                   :datetime         not null
-#  account_id                   :integer          not null
-#  channel_id                   :integer          not null
-#  portal_id                    :bigint
+#  enable_auto_assignment        :boolean          default(TRUE)
+#  enable_email_collect          :boolean          default(TRUE)
+#  greeting_enabled              :boolean          default(FALSE)
+#  greeting_message              :string
+#  lock_to_single_conversation   :boolean          default(FALSE), not null
+#  name                          :string           not null
+#  out_of_office_message         :string
+#  sender_name_type              :integer          default("friendly"), not null
+#  timezone                      :string           default("UTC")
+#  working_hours_enabled         :boolean          default(FALSE)
+#  created_at                    :datetime         not null
+#  updated_at                    :datetime         not null
+#  account_id                    :integer          not null
+#  channel_id                    :integer          not null
+#  portal_id                     :bigint
 #
 # Indexes
 #
