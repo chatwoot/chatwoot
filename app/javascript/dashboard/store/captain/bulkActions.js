48811076f0089ae9aec1bc4269293d8a91e400a7
iamsivin <iamsivin@gmail.com>
2025-02-20 13:22:56 +0530

    feat: Adds store

---
+++ app/javascript/dashboard/store/captain/bulkActions.js
@@ -0,0 +1,21 @@
+import CaptainBulkActionsAPI from 'dashboard/api/captain/bulkActions';
+import { createStore } from './storeFactory';
+import { throwErrorMessage } from 'dashboard/store/utils/api';
+
+export default createStore({
+  name: 'CaptainBulkAction',
+  API: CaptainBulkActionsAPI,
+  actions: mutations => ({
+    process: async function processAction({ commit }, payload) {
+      commit(mutations.SET_UI_FLAG, { isUpdating: true });
+      try {
+        const response = await CaptainBulkActionsAPI.create(payload);
+        commit(mutations.SET_UI_FLAG, { isUpdating: false });
+        return response.data;
+      } catch (error) {
+        commit(mutations.SET_UI_FLAG, { isUpdating: false });
+        return throwErrorMessage(error);
+      }
+    },
+  }),
+});
