024772adf8a9e901142d355551408889d8108a64
iamsivin <iamsivin@gmail.com>
2025-03-04 01:12:02 +0530

    chore: Components

---
+++ app/javascript/dashboard/components-next/feature-spotlight/FeatureSpotlightPopover.vue
@@ -0,0 +1,143 @@
+<script setup>
+import { ref } from 'vue';
+import { useToggle } from '@vueuse/core';
+import { vOnClickOutside } from '@vueuse/components';
+import Button from 'dashboard/components-next/button/Button.vue';
+
+const props = defineProps({
+  buttonLabel: {
+    type: String,
+    default: '',
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+  note: {
+    type: String,
+    default: '',
+  },
+  videoUrl: {
+    type: String,
+    default: '',
+  },
+  thumbnail: {
+    type: String,
+    default: '',
+  },
+  fallbackThumbnail: {
+    type: String,
+    default: '',
+  },
+  fallbackThumbnailDark: {
+    type: String,
+    default: '',
+  },
+  learnMoreUrl: {
+    type: String,
+    default: '',
+  },
+});
+
+const imageError = ref(false);
+const [isPopupVisible, togglePopup] = useToggle();
+
+const handleImageError = () => {
+  imageError.value = true;
+};
+
+const watchVideo = () => {
+  if (props.videoUrl) {
+    window.open(props.videoUrl, '_blank');
+  }
+};
+</script>
+
+<template>
+  <div class="relative">
+    <Button
+      :label="buttonLabel"
+      slate
+      ghost
+      sm
+      :class="{ 'bg-n-alpha-2': isPopupVisible }"
+      @click="togglePopup"
+    />
+
+    <div
+      v-if="isPopupVisible"
+      v-on-click-outside="() => isPopupVisible && (isPopupVisible = false)"
+    >
+      <section
+        class="absolute top-full mt-6 ltr:left-0 rtl:right-0 outline outline-1 outline-n-weak bg-n-alpha-3 backdrop-blur-[100px] rounded-xl p-4 w-80"
+      >
+        <div
+          class="absolute -top-[0.77rem] ltr:left-12 rtl:right-12 w-6 h-6 ltr:rotate-45 rtl:-rotate-45 rtl:rounded-tr ltr:rounded-tl rtl:border-r ltr:border-l border-t border-n-weak bg-n-alpha-3 z-10"
+        />
+
+        <div class="relative flex flex-col items-start gap-4 z-20">
+          <div class="flex-shrink-0 bg-gray-800 h-[7.5rem] rounded-lg">
+            <img
+              v-if="!imageError && thumbnail"
+              :src="thumbnail"
+              :alt="title"
+              class="w-full h-full object-cover rounded-lg"
+              @error="handleImageError"
+            />
+
+            <img
+              v-if="fallbackThumbnailDark"
+              :src="fallbackThumbnailDark"
+              :alt="title"
+              class="w-full h-full object-cover hidden dark:block"
+            />
+
+            <img
+              v-if="fallbackThumbnail"
+              :src="fallbackThumbnail"
+              :alt="title"
+              class="w-full h-full object-cover block dark:hidden"
+            />
+          </div>
+
+          <p v-if="note" class="text-n-slate-12 text-start text-sm mb-0">
+            {{ note }}
+          </p>
+
+          <div class="flex gap-3 justify-between w-full">
+            <slot name="actions">
+              <Button
+                v-if="videoUrl"
+                :label="$t('FEATURE_SPOTLIGHT.WATCH_VIDEO')"
+                sm
+                faded
+                slate
+                icon="i-lucide-circle-play"
+                class="w-full"
+                @click="watchVideo"
+              />
+
+              <Button
+                v-if="learnMoreUrl"
+                sm
+                faded
+                slate
+                trailing-icon
+                class="w-full"
+                icon="i-lucide-arrow-up-right"
+              >
+                <a
+                  :href="learnMoreUrl"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {{ $t('FEATURE_SPOTLIGHT.LEARN_MORE') }}
+                </a>
+              </Button>
+            </slot>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
