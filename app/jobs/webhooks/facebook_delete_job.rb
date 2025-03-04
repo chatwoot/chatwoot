74c611325f51a00d92e3bde96a1a8799d11c99ed
Shivam Mishra <scm.mymail@gmail.com>
2025-03-04 14:32:57 +0530

    feat: don't remove inbox

--- app/jobs/webhooks/facebook_delete_job.rb
+++ app/jobs/webhooks/facebook_delete_job.rb
@@ -1,12 +1,8 @@
 class Webhooks::FacebookDeleteJob < ApplicationJob
   queue_as :low
 
-  attr_reader :id_to_process
-
   def perform(id_to_process)
     @id_to_process = id_to_process
-
-    delete_channel_if_present
     delete_contact_if_present
 
     unset_processing
@@ -15,20 +11,12 @@ class Webhooks::FacebookDeleteJob < ApplicationJob
   private
 
   def unset_processing
-    key = format(::Redis::Alfred::META_DELETE_PROCESSING, id: id_to_process)
+    key = format(::Redis::Alfred::META_DELETE_PROCESSING, id: @id_to_process)
     ::Redis::Alfred.del(key)
   end
 
-  def delete_channel_if_present
-    channel = Channel::FacebookPage.find_by(page_id: id_to_process)
-    return unless channel
-
-    inbox = channel.inbox
-    inbox.destroy
-  end
-
   def remove_contact_if_present
-    contact_inbox = ContactInbox.find_by(source_id: id_to_process)
+    contact_inbox = ContactInbox.find_by(source_id: @id_to_process)
     return unless contact_inbox
 
     contact = contact_inbox.contact
