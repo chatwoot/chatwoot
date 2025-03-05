8cdc8670e8e16e36cdf7fbb4ee28b12bcbcd2596
Pranav <pranavrajs@gmail.com>
2025-03-04 20:09:52 -0800

    fix: Extend the locale without variant check for article locales as well

--- app/controllers/public/api/v1/portals/base_controller.rb
+++ app/controllers/public/api/v1/portals/base_controller.rb
@@ -27,14 +27,7 @@ class Public::Api::V1::Portals::BaseController < PublicController
   end
 
   def switch_locale_with_portal(&)
-    locale_without_variant = params[:locale].split('_')[0]
-    is_locale_available = I18n.available_locales.map(&:to_s).include?(params[:locale])
-    is_locale_variant_available = I18n.available_locales.map(&:to_s).include?(locale_without_variant)
-    if is_locale_available
-      @locale = params[:locale]
-    elsif is_locale_variant_available
-      @locale = locale_without_variant
-    end
+    @locale = validate_and_get_locale(params[:locale])
 
     I18n.with_locale(@locale, &)
   end
@@ -44,13 +37,28 @@ class Public::Api::V1::Portals::BaseController < PublicController
     Rails.logger.info "Article: not found for slug: #{params[:article_slug]}"
     render_404 && return if article.blank?
 
-    @locale = if article.category.present?
-                article.category.locale
-              else
-                article.portal.default_locale
-              end
-
+    article_locale = if article.category.present?
+                       article.category.locale
+                     else
+                       article.portal.default_locale
+                     end
+    @locale = validate_and_get_locale(article_locale)
     I18n.with_locale(@locale, &)
+  end
+
+  def validate_and_get_locale(locale)
+    return I18n.default_locale.to_s if locale.blank?
+
+    available_locales = I18n.available_locales.map(&:to_s)
+    locale_without_variant = locale.split('_')[0]
+
+    if available_locales.include?(locale)
+      locale
+    elsif available_locales.include?(locale_without_variant)
+      locale_without_variant
+    else
+      I18n.default_locale.to_s
+    end
   end
 
   def allow_iframe_requests
