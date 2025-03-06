2c4b70fc9809587949fa971bbef218b61706912a
Shivam Mishra <scm.mymail@gmail.com>
2025-03-06 12:52:51 +0530

    refactor(spec): extract common variables

--- spec/services/whatsapp/send_on_whatsapp_service_spec.rb
+++ spec/services/whatsapp/send_on_whatsapp_service_spec.rb
@@ -20,6 +20,22 @@ describe Whatsapp::SendOnWhatsappService do
       let!(:whatsapp_channel) { create(:channel_whatsapp, sync_templates: false) }
       let!(:contact_inbox) { create(:contact_inbox, inbox: whatsapp_channel.inbox, source_id: '123456789') }
       let!(:conversation) { create(:conversation, contact_inbox: contact_inbox, inbox: whatsapp_channel.inbox) }
+      let(:api_key) { 'test_key' }
+      let(:headers) { { 'D360-API-KEY' => api_key, 'Content-Type' => 'application/json' } }
+      let(:template_body) do
+        {
+          to: '123456789',
+          template: {
+            name: 'sample_shipping_confirmation',
+            namespace: '23423423_2342423_324234234_2343224',
+            language: { 'policy': 'deterministic', 'code': 'en_US' },
+            components: [{ 'type': 'body', 'parameters': [{ 'type': 'text', 'text': '3' }] }]
+          },
+          type: 'template'
+        }
+      end
+
+      let(:success_response) { { 'messages' => [{ 'id' => '123456789' }] }.to_json }
 
       it 'calls channel.send_message when with in 24 hour limit' do
         # to handle the case of 24 hour window limit.
@@ -30,13 +46,10 @@ describe Whatsapp::SendOnWhatsappService do
 
         stub_request(:post, 'https://waba.360dialog.io/v1/messages')
           .with(
-            headers: {
-              'D360-API-KEY' => 'test_key',
-              'Content-Type' => 'application/json'
-            },
+            headers: headers,
             body: { 'to' => '123456789', 'text' => { 'body' => 'test' }, 'type' => 'text' }.to_json
           )
-          .to_return(status: 200, body: { 'messages' => [{ 'id' => '123456789' }] }.to_json, headers: { 'content-type' => 'application/json' })
+          .to_return(status: 200, body: success_response, headers: { 'content-type' => 'application/json' })
 
         described_class.new(message: message).perform
         expect(message.reload.source_id).to eq('123456789')
@@ -48,39 +61,23 @@ describe Whatsapp::SendOnWhatsappService do
 
         stub_request(:post, 'https://waba.360dialog.io/v1/messages')
           .with(
-            headers: { 'D360-API-KEY' => 'test_key', 'Content-Type' => 'application/json' },
-            body: {
-              to: '123456789',
-              template: {
-                name: 'sample_shipping_confirmation',
-                namespace: '23423423_2342423_324234234_2343224',
-                language: { 'policy': 'deterministic', 'code': 'en_US' },
-                components: [{ 'type': 'body', 'parameters': [{ 'type': 'text', 'text': '3' }] }]
-              },
-              type: 'template'
-            }.to_json
-          ).to_return(status: 200, body: { 'messages' => [{ 'id' => '123456789' }] }.to_json, headers: { 'content-type' => 'application/json' })
-        described_class.new(message: message).perform
+            headers: headers,
+            body: template_body.to_json
+          ).to_return(status: 200, body: success_response, headers: { 'content-type' => 'application/json' })
 
+        described_class.new(message: message).perform
         expect(message.reload.source_id).to eq('123456789')
       end
 
       it 'calls channel.send_template if template_params are present' do
         message = create(:message, additional_attributes: { template_params: template_params },
                                    content: 'Your package will be delivered in 3 business days.', conversation: conversation, message_type: :outgoing)
-        stub_request(:post, 'https://waba.360dialog.io/v1/messages').with(
-          headers: { 'D360-API-KEY' => 'test_key', 'Content-Type' => 'application/json' },
-          body: {
-            to: '123456789',
-            template: {
-              name: 'sample_shipping_confirmation',
-              namespace: '23423423_2342423_324234234_2343224',
-              language: { 'policy': 'deterministic', 'code': 'en_US' },
-              components: [{ 'type': 'body', 'parameters': [{ 'type': 'text', 'text': '3' }] }]
-            },
-            type: 'template'
-          }.to_json
-        ).to_return(status: 200, body: { 'messages' => [{ 'id' => '123456789' }] }.to_json, headers: { 'content-type' => 'application/json' })
+        stub_request(:post, 'https://waba.360dialog.io/v1/messages')
+          .with(
+            headers: headers,
+            body: template_body.to_json
+          ).to_return(status: 200, body: success_response, headers: { 'content-type' => 'application/json' })
+
         described_class.new(message: message).perform
         expect(message.reload.source_id).to eq('123456789')
       end
@@ -93,19 +90,20 @@ describe Whatsapp::SendOnWhatsappService do
           conversation: conversation
         )
 
-        stub_request(:post, 'https://waba.360dialog.io/v1/messages').with(
-          headers: { 'D360-API-KEY' => 'test_key', 'Content-Type' => 'application/json' },
-          body: {
-            to: '123456789',
-            template: {
-              name: 'customer_yes_no',
-              namespace: '2342384942_32423423_23423fdsdaf23',
-              language: { 'policy': 'deterministic', 'code': 'ar' },
-              components: [{ 'type': 'body', 'parameters': [] }]
-            },
-            type: 'template'
-          }.to_json
-        ).to_return(status: 200, body: { 'messages' => [{ 'id' => '123456789' }] }.to_json, headers: { 'content-type' => 'application/json' })
+        stub_request(:post, 'https://waba.360dialog.io/v1/messages')
+          .with(
+            headers: headers,
+            body: {
+              to: '123456789',
+              template: {
+                name: 'customer_yes_no',
+                namespace: '2342384942_32423423_23423fdsdaf23',
+                language: { 'policy': 'deterministic', 'code': 'ar' },
+                components: [{ 'type': 'body', 'parameters': [] }]
+              },
+              type: 'template'
+            }.to_json
+          ).to_return(status: 200, body: success_response, headers: { 'content-type' => 'application/json' })
 
         described_class.new(message: message).perform
         expect(message.reload.source_id).to eq('123456789')
