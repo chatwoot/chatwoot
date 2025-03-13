b4f883af19448153dfdb60d16b10583ace025ba7
iamsivin <iamsivin@gmail.com>
2025-03-13 09:15:11 +0530

    fix: Settings layout

--- app/javascript/dashboard/routes/dashboard/settings/macros/MacroProperties.vue
+++ app/javascript/dashboard/routes/dashboard/settings/macros/MacroProperties.vue
@@ -30,7 +30,7 @@ export default {
 
 <template>
   <div
-    class="p-3 bg-white dark:bg-slate-900 h-[calc(100vh-3.5rem)] flex flex-col border-l border-n-weak"
+    class="p-3 bg-n-background h-[calc(100vh-3.5rem)] flex flex-col border-l border-n-weak"
   >
     <div>
       <woot-input
