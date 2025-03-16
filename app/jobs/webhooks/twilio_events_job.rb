d108f999006c6ac36658bdba06748262a3faec54
Pranav <pranavrajs@gmail.com>
2025-03-15 17:02:50 -0700

    Remove comments

--- app/jobs/webhooks/twilio_events_job.rb
+++ app/jobs/webhooks/twilio_events_job.rb
@@ -2,10 +2,8 @@ class Webhooks::TwilioEventsJob < ApplicationJob
   queue_as :low
 
   def perform(params = {})
-    # Skip processing if Body parameter is not present
     return if params[:Body].blank? && params[:MediaUrl0].blank?
 
-    # Process the Twilio webhook event in the background
     ::Twilio::IncomingMessageService.new(params: params).perform
   end
 end
