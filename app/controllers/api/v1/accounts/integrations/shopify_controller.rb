2a74badb880fe1e30a811af1e6099196f2e237de
Pranav <pranavrajs@gmail.com>
2025-03-17 13:36:09 -0700

    Add shopify context to controller

--- app/controllers/api/v1/accounts/integrations/shopify_controller.rb
+++ app/controllers/api/v1/accounts/integrations/shopify_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Accounts::Integrations::ShopifyController < Api::V1::Accounts::BaseController
   include Shopify::IntegrationHelper
+  before_action :setup_shopify_context
   before_action :fetch_hook, except: [:auth]
 
   def auth
@@ -20,5 +21,20 @@ class Api::V1::Accounts::Integrations::ShopifyController < Api::V1::Accounts::Ba
 
   def fetch_hook
     @hook = Integrations::Hook.find_by!(account: Current.account, app_id: 'shopify')
+  end
+
+  def setup_shopify_context
+    shopify_client_id = GlobalConfigService.load('SHOPIFY_CLIENT_ID', nil)
+    shopify_client_secret = GlobalConfigService.load('SHOPIFY_CLIENT_SECRET', nil)
+    required_scopes = %w[read_customers read_orders read_fulfillments].freeze
+
+    return if shopify_client_id.blank? || shopify_client_secret.blank?
+
+    ShopifyAPI::Context.setup(
+      api_key: shopify_client_id,
+      api_secret_key: shopify_client_secret,
+      api_version: '2025-01'.freeze,
+      scope: required_scopes.join(',')
+    )
   end
 end
