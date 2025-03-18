632f856c247c72db8bbf7802252756548fd523b0
Sojan <sojan@pepalo.com>
2025-03-17 18:01:22 -0700

    chore: move to db.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,11 +88,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # frequency in seconds to periodically run the Reaper, which attempts
-  # to find and recover connections from dead threads
-  # https://api.rubyonrails.org/v5.1/classes/ActiveRecord/ConnectionAdapters/ConnectionPool.html
-  config.active_record.connection_pool_reaping_frequency = ENV.fetch('DB_POOL_REAPING_FREQUENCY', 30).to_i
-
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
