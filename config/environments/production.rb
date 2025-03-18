027d68cb48aa6b20c1f5e1ef64a705192e87e4c7
Sojan Jose <sojan@pepalo.com>
2025-03-17 17:35:40 -0700

    chore: change reaping frequency

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # frequency in seconds to periodically run the Reaper, which attempts
   # to find and recover connections from dead threads
   # https://api.rubyonrails.org/v5.1/classes/ActiveRecord/ConnectionAdapters/ConnectionPool.html
-  config.active_record.connection_pool_reaping_frequency = ENV.fetch('DB_POOL_REAPING_FREQUENCY', 60).to_i
+  config.active_record.connection_pool_reaping_frequency = ENV.fetch('DB_POOL_REAPING_FREQUENCY', 30).to_i
 
   config.action_mailer.perform_caching = false
 
