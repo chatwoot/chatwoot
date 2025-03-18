7c6582175c2b07afeeacebea38b347ba6b134ba4
Shivam Mishra <scm.mymail@gmail.com>
2025-03-18 16:07:02 +0530

    feat: use next button

--- app/javascript/dashboard/routes/dashboard/settings/inbox/channels/Website.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/channels/Website.vue
@@ -2,6 +2,7 @@
 import { mapGetters } from 'vuex';
 import { useAlert } from 'dashboard/composables';
 import router from '../../../../index';
+import NextButton from 'dashboard/components-next/button/Button.vue';
 import PageHeader from '../../SettingsSubPageHeader.vue';
 import GreetingsEditor from 'shared/components/GreetingsEditor.vue';
 
@@ -9,6 +10,7 @@ export default {
   components: {
     PageHeader,
     GreetingsEditor,
+    NextButton,
   },
   data() {
     return {
@@ -188,12 +190,15 @@ export default {
         "
         :richtext="!textAreaChannels"
       />
-      <div class="flex flex-row justify-end w-full gap-2 px-0 py-2">
+      <div class="flex flex-row justify-end w-full gap-2 px-0 py-2 mt-4">
         <div class="w-full">
-          <woot-submit-button
-            :loading="uiFlags.isCreating"
+          <NextButton
+            type="submit"
+            :is-loading="uiFlags.isCreating"
             :disabled="!channelWebsiteUrl || !inboxName"
-            :button-text="$t('INBOX_MGMT.ADD.WEBSITE_CHANNEL.SUBMIT_BUTTON')"
+            solid
+            blue
+            :label="$t('INBOX_MGMT.ADD.WEBSITE_CHANNEL.SUBMIT_BUTTON')"
           />
         </div>
       </div>
