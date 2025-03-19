b0e4c006e41a9b81cd8a6be00e3dfc6c3ea6de73
iamsivin <iamsivin@gmail.com>
2025-03-19 13:48:33 +0530

    feat: Update auth screens

--- app/javascript/v3/components/Divider/SimpleDivider.vue
+++ app/javascript/v3/components/Divider/SimpleDivider.vue
@@ -12,12 +12,10 @@ export default {
 <template>
   <div class="relative my-4 section-separator">
     <div class="absolute inset-0 flex items-center" aria-hidden="true">
-      <div class="w-full border-t border-slate-200 dark:border-slate-600" />
+      <div class="w-full border-t border-n-strong" />
     </div>
     <div v-if="label" class="relative flex justify-center text-sm">
-      <span
-        class="bg-white dark:bg-slate-800 px-2 text-slate-500 dark:text-white"
-      >
+      <span class="bg-white dark:bg-n-solid-2 px-2 text-n-slate-10">
         {{ label }}
       </span>
     </div>
