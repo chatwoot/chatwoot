fbaf3a60622ad2eb0b83dfaeb94ebd1e7ea0ae4c
iamsivin <iamsivin@gmail.com>
2025-03-21 17:29:39 +0530

    chore: Update buttons in dashboard

--- app/javascript/dashboard/components-next/button/Button.vue
+++ app/javascript/dashboard/components-next/button/Button.vue
@@ -184,7 +184,7 @@ const STYLE_CONFIG = {
     center: 'justify-center',
     end: 'justify-end',
   },
-  base: 'inline-flex items-center min-w-0 gap-2 transition-all duration-200 ease-in-out border-0 rounded-lg outline-1 outline disabled:opacity-50',
+  base: 'inline-flex items-center min-w-0 gap-2 transition-all duration-200 ease-in-out border-0 rounded-lg outline-1 outline disabled:opacity-50 outline-offset-[-1px]',
 };
 
 const variantClasses = computed(() => {
