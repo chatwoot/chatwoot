2d5165bc77921b786356716328ae239fcf1aec8f
Muhsin Keloth <muhsinkeramam@gmail.com>
2025-03-25 15:49:30 +0530

    chore: add channel_instagram to feature flags

--- app/javascript/dashboard/components/widgets/ChannelItem.vue
+++ app/javascript/dashboard/components/widgets/ChannelItem.vue
@@ -32,6 +32,10 @@ export default {
         return this.enabledFeatures.channel_email;
       }
 
+      if (key === 'instagram') {
+        return this.enabledFeatures.channel_instagram;
+      }
+
       return [
         'website',
         'twilio',
