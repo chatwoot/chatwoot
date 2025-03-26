6d69dbd589635a64ad5013f7c9f0bcee8428fc76
Vishnu Narayanan <vishnu@chatwoot.com>
2025-03-26 23:17:19 +0530

    chore: add specs

--- spec/jobs/internal/remove_stale_contacts_job_spec.rb
+++ spec/jobs/internal/remove_stale_contacts_job_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Internal::RemoveStaleContactsJob do
-  subject(:job) { described_class.perform_later }
+  subject(:job) { described_class.perform_later(account) }
+
+  let(:account) { create(:account) }
 
   it 'enqueues the job' do
     expect { job }.to have_enqueued_job(described_class)
-      .on_queue('scheduled_jobs')
+      .with(account)
+      .on_queue('low')
   end
 
   it 'calls the RemoveStaleContactsService' do
     service = instance_double(Internal::RemoveStaleContactsService)
-    expect(Internal::RemoveStaleContactsService).to receive(:new).and_return(service)
+    expect(Internal::RemoveStaleContactsService).to receive(:new).with(account: account).and_return(service)
     expect(service).to receive(:perform)
-    described_class.perform_now
+    described_class.perform_now(account)
   end
 end
