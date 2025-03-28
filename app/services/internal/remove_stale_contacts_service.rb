e4ad8d61e7912c862575f52cd451c3b2b21d575c
Shivam Mishra <scm.mymail@gmail.com>
2025-03-28 12:02:20 +0530

    refactor: use delete_all

--- app/services/internal/remove_stale_contacts_service.rb
+++ app/services/internal/remove_stale_contacts_service.rb
@@ -5,11 +5,15 @@ class Internal::RemoveStaleContactsService
     contacts_to_remove = @account.contacts.stale_without_conversations(30.days.ago)
     total_deleted = 0
 
+    Rails.logger.info "[Internal::RemoveStaleContactsService] Starting removal of stale contacts for account #{@account.id}"
+
     contacts_to_remove.find_in_batches(batch_size: batch_size) do |batch|
       contact_ids = batch.map(&:id)
-      Contact.where(id: contact_ids).destroy_all
+
+      ContactInbox.where(contact_id: contact_ids).delete_all
+      Contact.where(id: contact_ids).delete_all
       total_deleted += batch.size
-      Rails.logger.info "Deleted #{batch.size} contacts (#{total_deleted} total) for account #{@account.id}"
+      Rails.logger.info "[Internal::RemoveStaleContactsService] Deleted #{batch.size} contacts (#{total_deleted} total) for account #{@account.id}"
     end
   end
 end
