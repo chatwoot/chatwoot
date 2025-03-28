9dd3155cb364bf814a7bba398b384c3b9c0cd797
Vishnu Narayanan <vishnu@chatwoot.com>
2025-03-28 11:30:44 +0530

    chore: log total count

--- app/services/internal/remove_stale_contacts_service.rb
+++ app/services/internal/remove_stale_contacts_service.rb
@@ -3,11 +3,13 @@ class Internal::RemoveStaleContactsService
 
   def perform(batch_size = 1000)
     contacts_to_remove = @account.contacts.stale_without_conversations(30.days.ago)
+    total_deleted = 0
 
     contacts_to_remove.find_in_batches(batch_size: batch_size) do |batch|
-      Rails.logger.info "Removing #{batch.size} stale contacts for account #{@account.id}"
       contact_ids = batch.map(&:id)
       Contact.where(id: contact_ids).destroy_all
+      total_deleted += batch.size
+      Rails.logger.info "Deleted #{batch.size} contacts (#{total_deleted} total) for account #{@account.id}"
     end
   end
 end
