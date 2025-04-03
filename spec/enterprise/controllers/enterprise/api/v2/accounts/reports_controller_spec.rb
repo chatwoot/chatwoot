25820f76a2a9887a7c5bbe52d9aa14008452f93d
Sojan <sojan@pepalo.com>
2025-04-02 18:41:29 -0700

    chore: fixes

--- spec/enterprise/controllers/enterprise/api/v2/accounts/reports_controller_spec.rb
+++ spec/enterprise/controllers/enterprise/api/v2/accounts/reports_controller_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe 'Enterprise Reports API', type: :request do
   let(:agent) { create(:user, account: account, role: :agent) }
 
   # Create a custom role with report_manage permission
-  let(:custom_role) { create(:custom_role, account: account, permissions: ['report_manage']) }
-  let(:agent_with_role) { create(:user) }
+  let!(:custom_role) { create(:custom_role, account: account, permissions: ['report_manage']) }
+  let!(:agent_with_role) { create(:user) }
   let(:agent_with_role_account_user) do
     create(:account_user, user: agent_with_role, account: account, role: :agent, custom_role: custom_role)
   end
@@ -17,6 +17,10 @@ RSpec.describe 'Enterprise Reports API', type: :request do
   let(:start_of_today) { Time.current.in_time_zone(default_timezone).beginning_of_day.to_i }
   let(:end_of_today) { Time.current.in_time_zone(default_timezone).end_of_day.to_i }
   let(:params) { { timezone_offset: Time.zone.utc_offset } }
+
+  before do
+    agent_with_role_account_user
+  end
 
   describe 'GET /api/v2/accounts/:account_id/reports' do
     context 'when it is an authenticated user' do
@@ -27,15 +31,6 @@ RSpec.describe 'Enterprise Reports API', type: :request do
           since: start_of_today.to_s,
           until: end_of_today.to_s
         )
-      end
-
-      it 'returns unauthorized for regular agents' do
-        get "/api/v2/accounts/#{account.id}/reports",
-            params: params,
-            headers: agent.create_new_auth_token,
-            as: :json
-
-        expect(response).to have_http_status(:unauthorized)
       end
 
       it 'returns success for agents with report_manage permission' do
@@ -57,15 +52,6 @@ RSpec.describe 'Enterprise Reports API', type: :request do
           since: start_of_today.to_s,
           until: end_of_today.to_s
         )
-      end
-
-      it 'returns unauthorized for regular agents' do
-        get "/api/v2/accounts/#{account.id}/reports/summary",
-            params: params,
-            headers: agent.create_new_auth_token,
-            as: :json
-
-        expect(response).to have_http_status(:unauthorized)
       end
 
       it 'returns success for agents with report_manage permission' do
