2e16a72b3aca753dd49122749472e5e97488117b
rohit-yelmar <rohit.a.yelmar150904@gmail.com>
2025-04-04 14:36:42 +0530

    Onboarding

--- app/javascript/dashboard/routes/dashboard/dashboard.routes.js
+++ app/javascript/dashboard/routes/dashboard/dashboard.routes.js
@@ -10,6 +10,13 @@ import campaignsRoutes from './campaigns/campaigns.routes';
 import { routes as captainRoutes } from './captain/captain.routes';
 import AppContainer from './Dashboard.vue';
 import Suspended from './suspended/Index.vue';
+import RouteLayout from 'v3/views/onboarding/RouteLayout.vue';
+import SetupProfile from 'v3/views/onboarding/SetupProfile.vue';
+import AddAgent from 'v3/views/onboarding/AddAgent.vue';
+import InboxChannels from 'v3/views/onboarding/InboxChannels.vue';
+import InboxConfigure from 'v3/views/onboarding/InboxConfigure.vue';
+import InboxAddAgents from 'v3/views/onboarding/InboxAddAgents.vue';
+import InboxFinishSetup from 'v3/views/onboarding/InboxFinishSetup.vue';
 
 export default {
   routes: [
@@ -35,6 +42,78 @@ export default {
         permissions: ['administrator', 'agent', 'custom_role'],
       },
       component: Suspended,
+    },
+    {
+      path: frontendURL('accounts/:accountId/start'),
+      meta: {
+        permissions: ['administrator'],
+      },
+      component: RouteLayout,
+      children: [
+        {
+          path: frontendURL('accounts/:accountId/start/setup-profile'),
+          name: 'onboarding_setup_profile',
+          meta: {
+            permissions: ['administrator'],
+          },
+          component: SetupProfile,
+        },
+        {
+          path: frontendURL('accounts/:accountId/start/add-agent'),
+          name: 'onboarding_add_agent',
+          meta: {
+            permissions: ['administrator'],
+          },
+          component: AddAgent,
+        },
+        {
+          path: frontendURL('accounts/:accountId/start/setup-inbox'),
+          name: 'onboarding_setup_inbox',
+          meta: {
+            permissions: ['administrator'],
+          },
+          component: InboxChannels,
+        },
+        {
+          path: frontendURL(
+            'accounts/:accountId/start/setup-inbox/configure/:channel'
+          ),
+          name: 'onboarding_setup_inbox_configure',
+          meta: {
+            permissions: ['administrator'],
+          },
+          component: InboxConfigure,
+          props: route => {
+            return { channel: route.params.channel };
+          },
+        },
+        {
+          path: frontendURL(
+            'accounts/:accountId/start/setup-inbox/add-agents/:inbox_id'
+          ),
+          name: 'onboarding_setup_inbox_add_agents',
+          meta: {
+            permissions: ['administrator'],
+          },
+          component: InboxAddAgents,
+          props: route => {
+            return { inbox_id: route.params.inbox_id };
+          },
+        },
+        {
+          path: frontendURL(
+            'accounts/:accountId/start/setup-inbox/finish/:inbox_id'
+          ),
+          name: 'onboarding_setup_inbox_finish',
+          meta: {
+            permissions: ['administrator'],
+          },
+          component: InboxFinishSetup,
+          props: route => {
+            return { inbox_id: route.params.inbox_id };
+          },
+        },
+      ],
     },
   ],
 };
