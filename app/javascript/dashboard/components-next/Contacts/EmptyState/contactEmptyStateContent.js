dd83d95f95cae90a00cc09c2192b9a0b8bd6158d
Diego <diego.renau@gmail.com>
2025-04-04 16:45:39 -0300

    Email y textos

--- app/javascript/dashboard/components-next/Contacts/EmptyState/contactEmptyStateContent.js
+++ app/javascript/dashboard/components-next/Contacts/EmptyState/contactEmptyStateContent.js
@@ -18,8 +18,8 @@ export default [
     availabilityStatus: 'offline',
     email: 'cande@buzzcrm.ai',
     id: 22,
-    name: 'Candice Matherson',
-    phoneNumber: '+14155552671',
+    name: 'Cande',
+    phoneNumber: '+55552671',
     identifier: null,
     thumbnail: '',
     customAttributes: {
@@ -35,11 +35,11 @@ export default [
   },
   {
     additionalAttributes: {
-      city: 'San Francisco',
-      country: 'United States',
+      city: 'Montevideo',
+      country: 'Uruguay',
       description: 'Passionate about design and user experience.',
       companyName: 'Designify',
-      countryCode: 'US',
+      countryCode: 'UY',
       socialProfiles: {
         github: 'ophelia-folkard',
         twitter: 'oph_designs',
