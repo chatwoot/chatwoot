5dd572e5181bb2d1a7c0cacc79a77856865d30dc
Bayu <mbayu.nugraha@radyalabs.com>
2025-04-08 16:16:52 +0700

    change layout conversation/inbox

--- db/schema.rb
+++ db/schema.rb
@@ -1057,22 +1057,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_03_24_103350) do
     t.text "description"
   end
 
-  create_table "subscription_plans_copy1", id: :bigint, default: -> { "nextval('subscription_plans_id_seq'::regclass)" }, force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "max_mau", default: 0, null: false
-    t.integer "max_ai_agents", default: 0, null: false
-    t.integer "max_ai_responses", default: 0, null: false
-    t.integer "max_human_agents", default: 0, null: false
-    t.text "available_channels", default: [], array: true
-    t.string "support_level"
-    t.integer "duration_days"
-    t.decimal "monthly_price", precision: 16, scale: 2, null: false
-    t.decimal "annual_price", precision: 16, scale: 2, null: false
-    t.boolean "is_active", default: true
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "subscription_usage", force: :cascade do |t|
     t.bigint "subscription_id", null: false
     t.integer "mau_count", default: 0
