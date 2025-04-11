1643191a9b1a361cc0718e9c4712bb2ea2b5619b
Muhammad <muhammad.abrar@eritheialabs.com>
2025-04-11 19:07:58 +0500

    fix: CRUISE-130-93: replaced chatwoot assets & logo with cruise control (#7)
    * fix: replace brand assets with Cruise Control
    
    * fix: replace hard code brand name with cruise control
    
    * fix: installation config update for all keys
    
    * fix: curise control favicons added
    
    ---------
    
    Co-authored-by: Muhammad <muhammad@eritheialabs.io>

--- app/controllers/super_admin/installation_configs_controller.rb
+++ app/controllers/super_admin/installation_configs_controller.rb
@@ -7,10 +7,8 @@ class SuperAdmin::InstallationConfigsController < SuperAdmin::ApplicationControl
   def update
     @resource = InstallationConfig.find(params[:id])
     new_values = params.dig(:installation_config, :value) || []
-    formatted_new_values = format_new_values(new_values)
-    updated_values = update_existing_values(@resource.value || [], formatted_new_values)
 
-    if @resource.update(value: updated_values)
+    if @resource.update(value: new_values)
       redirect_to super_admin_installation_config_path(@resource), notice: 'Updated successfully'
     else
       render :edit
@@ -53,37 +51,4 @@ class SuperAdmin::InstallationConfigsController < SuperAdmin::ApplicationControl
 
   # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
   # for more information
-
-  private
-
-  # Format new values by extracting relevant data and applying boolean conversion where necessary
-  def format_new_values(new_values)
-    new_values.values.map do |value|
-      {
-        'name' => value['name'],
-        'display_name' => value['display_name'],
-        'enabled' => boolean_value(value['enabled']),
-        'help_url' => value['help_url'],
-        'premium' => boolean_value(value['premium']),
-        'deprecated' => boolean_value(value['deprecated']),
-        'chatwoot_internal' => boolean_value(value['chatwoot_internal'])
-      }.compact
-    end
-  end
-
-  # Convert string to boolean if it's a "true" or "false" string, else return the original value
-  def boolean_value(value)
-    return true if value == 'true'
-    return false if value == 'false'
-
-    value
-  end
-
-  # Update existing values by merging with formatted new values if names match
-  def update_existing_values(existing_values, formatted_new_values)
-    existing_values.map do |existing|
-      new_value = formatted_new_values.find { |v| v['name'] == existing['name'] }
-      new_value ? existing.merge(new_value) : existing
-    end
-  end
 end
