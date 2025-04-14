3bac74680132914f3a572ec286eb475a8957b67a
Viktor Khon <vkhon00+git@gmail.com>
2025-04-13 20:04:08 -0500

    Updating docker file to fix deployment error with Postgres

--- docker/entrypoints/rails.sh
+++ docker/entrypoints/rails.sh
@@ -8,27 +8,29 @@ rm -rf /app/tmp/cache/*
 
 echo "Waiting for postgres to become ready...."
 
-# Let DATABASE_URL env take presedence over individual connection params.
-# This is done to avoid printing the DATABASE_URL in the logs
+# Let DATABASE_URL env take precedence over individual connection params.
+# This is done to avoid printing the DATABASE_URL in the logs.
 $(docker/entrypoints/helpers/pg_database_url.rb)
 PG_READY="pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USERNAME"
 
 until $PG_READY
 do
-  sleep 2;
+  sleep 2
 done
 
 echo "Database ready to accept connections."
 
-#install missing gems for local dev as we are using base image compiled for production
+# Install missing gems (for development if needed)
 bundle install
 
 BUNDLE="bundle check"
-
 until $BUNDLE
 do
-  sleep 2;
+  sleep 2
 done
 
-# Execute the main process of the container
+echo "Running database migrations..."
+bundle exec rails db:chatwoot_prepare
+
+# Execute the main process of the container.
 exec "$@"
