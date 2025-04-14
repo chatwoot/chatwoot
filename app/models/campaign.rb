5d675d4b460dce04fe0e8ba3e7bc62c81930887f
avnotaklu <sukhmander111101@gmail.com>
2025-04-14 15:44:05 +0530

    BUG FIX: Campaign is now properly completed

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -143,7 +143,7 @@ class Campaign < ApplicationRecord
     case inbox.inbox_type
     when 'Email'
       self.campaign_type = 'email'
-      self.campaign_status = 'active' # REVIEW: is it okay to set status here as active
+      self.campaign_status = 'active' if new_record?
       self.scheduled_at ||= Time.now.utc
     when 'Twilio SMS', 'Sms'
       self.campaign_type = 'one_off'
