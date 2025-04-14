c1f5693ab78ccfab0db4b9a2a0955708905001e9
avnotaklu <sukhmander111101@gmail.com>
2025-04-14 15:47:33 +0530

    Reactivating campaign on time update

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -143,7 +143,7 @@ class Campaign < ApplicationRecord
     case inbox.inbox_type
     when 'Email'
       self.campaign_type = 'email'
-      self.campaign_status = 'active' if new_record?
+      self.campaign_status = 'active' if scheduled_at > Time.now.utc
       self.scheduled_at ||= Time.now.utc
     when 'Twilio SMS', 'Sms'
       self.campaign_type = 'one_off'
