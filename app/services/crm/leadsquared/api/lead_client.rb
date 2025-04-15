5cc7e7a445cf3f24c801869e14827670ab948ad2
Shivam Mishra <scm.mymail@gmail.com>
2025-04-15 20:35:47 +0530

    fix: lead client

--- app/services/crm/leadsquared/api/lead_client.rb
+++ app/services/crm/leadsquared/api/lead_client.rb
@@ -3,15 +3,13 @@ class Crm::Leadsquared::Api::LeadClient < Crm::Leadsquared::Api::BaseClient
   def search_lead(key)
     return { success: false, error: 'Search key is required' } if key.blank?
 
-    path = '/LeadManagement.svc/Leads.GetByQuickSearch'
+    path = 'LeadManagement.svc/Leads.GetByQuickSearch'
     params = { key: key }
 
-    response = get(path, params)
+    # Empty response: {:success=>true, :data=>[]}
+    # Error response: {:success=>false, :error=>'Error message', :code=>404}
 
-    # For Quick Search API, empty array means no matching leads found
-    return { success: true, data: nil } if response[:success] && response[:data].is_a?(Array) && response[:data].empty?
-
-    response
+    get(path, params)
   end
 
   # https://apidocs.leadsquared.com/create-or-update/#api
@@ -34,6 +32,8 @@ class Crm::Leadsquared::Api::LeadClient < Crm::Leadsquared::Api::BaseClient
     end
 
     response = post(path, {}, formatted_data)
+    # Success Response: {:success=>true, :data=>{"Status"=>"Success", "Message"=>{"Id"=>"8e0f69ae-e2ac-40fc-a0cf-827326181c8a"}}}
+    # Error response: {:success=>false, :error=>'Error message', :code=>404}
 
     handle_lead_create_or_update_response(response)
   end
@@ -53,6 +53,9 @@ class Crm::Leadsquared::Api::LeadClient < Crm::Leadsquared::Api::BaseClient
     end
 
     response = post(path, {}, formatted_data)
+    # success response: {:success=>true, :data=>{"Status"=>"Success", "Message"=>{"AffectedRows"=>1}}}
+    # Error response: {:success=>false, :error=>'Error message', :code=>404}
+
     if response[:success]
       response
     else
@@ -66,23 +69,17 @@ class Crm::Leadsquared::Api::LeadClient < Crm::Leadsquared::Api::BaseClient
   def handle_lead_create_or_update_response(response)
     if valid_lead_create_or_update_response?(response)
       { success: true, data: response[:data]['Message'] }
-    elsif response[:success]
-      # Response was technically successful but no lead ID returned
-      { success: false, error: 'No leads created or updated' }
     else
-      response
+      Rails.logger.error("Call to LeadSquared leads API failed with [#{response[:code]}] #{response[:error]}")
+      { success: false, error: 'No leads created or updated' }
     end
   end
 
   def valid_lead_create_or_update_response?(response)
     response[:success] &&
-      valid_response_data?(response[:data])
-  end
-
-  def valid_response_data?(data)
-    data.is_a?(Hash) &&
-      data['Status'] == 'Success' &&
-      data['Message'] &&
-      data['Message']['Id'].present?
+      response[:data].is_a?(Hash) &&
+      response[:data]['Status'] == 'Success' &&
+      response[:data]['Message'] &&
+      response[:data]['Message']['Id'].present?
   end
 end
--- spec/services/crm/leadsquared/api/lead_client_spec.rb
+++ spec/services/crm/leadsquared/api/lead_client_spec.rb
@@ -3,11 +3,19 @@ require 'rails_helper'
 RSpec.describe Crm::Leadsquared::Api::LeadClient do
   let(:access_key) { SecureRandom.hex }
   let(:secret_key) { SecureRandom.hex }
-  let(:endpoint_url) { 'https://api.leadsquared.com/' }
+  let(:headers) do
+    {
+      'Content-Type': 'application/json',
+      'x-LSQ-AccessKey': access_key,
+      'x-LSQ-SecretKey': secret_key
+    }
+  end
+
+  let(:endpoint_url) { 'https://api.leadsquared.com/v2' }
   let(:client) { described_class.new(access_key, secret_key, endpoint_url) }
 
   describe '#search_lead' do
-    let(:path) { '/v2/LeadManagement.svc/Leads.GetByQuickSearch' }
+    let(:path) { 'LeadManagement.svc/Leads.GetByQuickSearch' }
     let(:search_key) { 'test@example.com' }
     let(:full_url) { URI.join(endpoint_url, path).to_s }
 
@@ -22,7 +30,7 @@ RSpec.describe Crm::Leadsquared::Api::LeadClient do
     context 'when no leads are found' do
       before do
         stub_request(:get, full_url)
-          .with(query: { accessKey: access_key, secretKey: secret_key, key: search_key })
+          .with(query: { key: search_key }, headers: headers)
           .to_return(
             status: 200,
             body: [].to_json,
@@ -30,10 +38,10 @@ RSpec.describe Crm::Leadsquared::Api::LeadClient do
           )
       end
 
-      it 'returns success with nil data' do
+      it 'returns success with [] data' do
         response = client.search_lead(search_key)
         expect(response[:success]).to be true
-        expect(response[:data]).to be_nil
+        expect(response[:data]).to eq([])
       end
     end
 
@@ -49,7 +57,7 @@ RSpec.describe Crm::Leadsquared::Api::LeadClient do
 
       before do
         stub_request(:get, full_url)
-          .with(query: { accessKey: access_key, secretKey: secret_key, key: search_key })
+          .with(query: { key: search_key }, headers: headers, body: anything)
           .to_return(
             status: 200,
             body: lead_data.to_json,
@@ -66,7 +74,7 @@ RSpec.describe Crm::Leadsquared::Api::LeadClient do
   end
 
   describe '#create_or_update_lead' do
-    let(:path) { '/v2/LeadManagement.svc/Lead.CreateOrUpdate' }
+    let(:path) { 'LeadManagement.svc/Lead.CreateOrUpdate' }
     let(:full_url) { URI.join(endpoint_url, path).to_s }
     let(:lead_data) do
       {
@@ -96,10 +104,8 @@ RSpec.describe Crm::Leadsquared::Api::LeadClient do
       let(:success_response) do
         {
           'Status' => 'Success',
-          'Value' => {
-            'Message' => 'Lead created successfully',
-            'ProspectId' => SecureRandom.uuid,
-            'AffectedRows' => 1
+          'Message' => {
+            'Id' => '8e0f69ae-e2ac-40fc-a0cf-827326181c8a'
           }
         }
       end
@@ -107,9 +113,8 @@ RSpec.describe Crm::Leadsquared::Api::LeadClient do
       before do
         stub_request(:post, full_url)
           .with(
-            query: { accessKey: access_key, secretKey: secret_key },
             body: formatted_lead_data.to_json,
-            headers: { 'Content-Type' => 'application/json' }
+            headers: headers
           )
           .to_return(
             status: 200,
@@ -121,56 +126,20 @@ RSpec.describe Crm::Leadsquared::Api::LeadClient do
       it 'returns success with lead data' do
         response = client.create_or_update_lead(lead_data)
         expect(response[:success]).to be true
-        expect(response[:data]).to eq(success_response['Value'])
-      end
-    end
-
-    context 'when no leads are affected' do
-      let(:no_affect_response) do
-        {
-          'Status' => 'Success',
-          'Value' => {
-            'Message' => 'No changes required',
-            'AffectedRows' => 0
-          }
-        }
-      end
-
-      before do
-        stub_request(:post, full_url)
-          .with(
-            query: { accessKey: access_key, secretKey: secret_key },
-            body: formatted_lead_data.to_json,
-            headers: { 'Content-Type' => 'application/json' }
-          )
-          .to_return(
-            status: 200,
-            body: no_affect_response.to_json,
-            headers: { 'Content-Type' => 'application/json' }
-          )
-      end
-
-      it 'returns error indicating no leads affected' do
-        response = client.create_or_update_lead(lead_data)
-        expect(response[:success]).to be false
-        expect(response[:error]).to eq('No leads created or updated')
+        expect(response[:data]).to eq(success_response['Message'])
       end
     end
 
     context 'when request fails' do
       let(:error_response) do
-        {
-          'Status' => 'Error',
-          'ExceptionMessage' => 'Invalid lead data'
-        }
+        { :success => false, :error => 'Error message', :code => 404 }
       end
 
       before do
         stub_request(:post, full_url)
           .with(
-            query: { accessKey: access_key, secretKey: secret_key },
             body: formatted_lead_data.to_json,
-            headers: { 'Content-Type' => 'application/json' }
+            headers: headers
           )
           .to_return(
             status: 200,
@@ -182,7 +151,7 @@ RSpec.describe Crm::Leadsquared::Api::LeadClient do
       it 'returns error response' do
         response = client.create_or_update_lead(lead_data)
         expect(response[:success]).to be false
-        expect(response[:error]).to eq('Invalid lead data')
+        expect(response[:error]).to eq('No leads created or updated')
       end
     end
   end
