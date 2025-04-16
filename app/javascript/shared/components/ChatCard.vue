7b71ec910b72215f0cc5ee5d8c9dbc9b47a6665d
Viktor Khon <vkhon00@gmail.com>
2025-04-16 23:32:32 +0000

    Fixing template in chatcard.vue

--- app/javascript/shared/components/ChatCard.vue
+++ app/javascript/shared/components/ChatCard.vue
@@ -35,51 +35,19 @@ export default {
 </script>
 
 <template>
-  <div
-    class="card-message chat-bubble agent bg-n-background dark:bg-n-solid-3 max-w-56 rounded-lg overflow-hidden"
-  >
-    <div v-if="items && items.length > 0">
-      <div v-for="(item, index) in items" :key="index" class="mb-4">
-        <img
-          v-if="item.media_url"
-          class="w-full object-contain max-h-[150px] rounded-[5px]"
-          :src="item.media_url"
-        />
-        <div class="card-body">
-          <h4
-            class="!text-base !font-medium !mt-1 !mb-1 !leading-[1.5] text-n-slate-12"
-          >
-            {{ item.title }}
-          </h4>
-          <p class="!mb-1 text-n-slate-11">
-            {{ item.description }}
-          </p>
-          <p v-if="item.price" class="!mb-3 font-bold text-xl text-center text-n-slate-12">
-            {{ item.price }}
-          </p>
-          <CardButton v-for="action in item.actions" :key="action.id" :action="action" />
-        </div>
-      </div>
-    </div>
-    <div v-else>
-      <img
-        class="w-full object-contain max-h-[150px] rounded-[5px]"
-        :src="mediaUrl"
-      />
-      <div class="card-body">
-        <h4
-          class="!text-base !font-medium !mt-1 !mb-1 !leading-[1.5] text-n-slate-12"
-        >
-          {{ title }}
-        </h4>
-        <p class="!mb-1 text-n-slate-11">
-          {{ description }}
-        </p>
-        <p v-if="price" class="!mb-3 font-bold text-xl text-center text-n-slate-12">
-          {{ price }}
-        </p>
-        <CardButton v-for="action in actions" :key="action.id" :action="action" />
-      </div>
+  <div class="card-message chat-bubble agent bg-n-background dark:bg-n-solid-3 max-w-56 rounded-lg overflow-hidden">
+    <img class="w-full object-contain max-h-[150px] rounded-[5px]" :src="mediaUrl" />
+    <div class="card-body">
+      <h4 class="!text-base !font-medium !mt-1 !mb-1 !leading-[1.5] text-n-slate-12">
+        {{ title }}
+      </h4>
+      <p class="!mb-1 text-n-slate-11">
+        {{ description }}
+      </p>
+      <p v-if="price" class="!mb-1 font-bold text-n-slate-12">
+        {{ price }}
+      </p>
+      <CardButton v-for="action in actions" :key="action.id" :action="action" />
     </div>
   </div>
 </template>
