ef5a227f42f325c7195ffe57cf30434bfcd33597
Muhsin Keloth <muhsinkeramam@gmail.com>
2025-04-17 22:55:26 +0530

    chore: improve markup

--- app/javascript/dashboard/routes/dashboard/settings/inbox/channels/instagram/DuplicateInboxBanner.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/channels/instagram/DuplicateInboxBanner.vue
@@ -1,6 +1,5 @@
 <script setup>
 import Banner from 'dashboard/components-next/banner/Banner.vue';
-import FluentIcon from 'shared/components/FluentIcon/DashboardIcon.vue';
 
 defineProps({
   content: {
@@ -13,9 +12,7 @@ defineProps({
 <template>
   <Banner color="amber">
     <div class="flex items-center gap-2">
-      <div class="flex">
-        <FluentIcon icon="info" size="16" />
-      </div>
+      <div class="flex"><fluent-icon icon="info" size="16" /></div>
       {{ content }}
     </div>
   </Banner>
