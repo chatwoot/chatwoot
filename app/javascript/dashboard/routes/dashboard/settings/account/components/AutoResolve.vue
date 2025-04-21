879b79f2dcf6fce32b29ed0552c073b0d96a0e45
Shivam Mishra <scm.mymail@gmail.com>
2025-04-21 14:58:33 +0530

    feat: handle new settings

--- app/javascript/dashboard/routes/dashboard/settings/account/components/AutoResolve.vue
+++ app/javascript/dashboard/routes/dashboard/settings/account/components/AutoResolve.vue
@@ -1,17 +1,59 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useI18n } from 'vue-i18n';
+import { useAccount } from 'dashboard/composables/useAccount';
+import { useAlert } from 'dashboard/composables';
 import SectionLayout from './SectionLayout.vue';
 import WithLabel from 'v3/components/Form/WithLabel.vue';
 import DurationInput from 'next/input/DurationInput.vue';
-import TextAreaNext from 'next/textarea/TextArea.vue';
+import TextArea from 'next/textarea/TextArea.vue';
+import NextButton from 'dashboard/components-next/button/Button.vue';
 
 const { t } = useI18n();
-
 const duration = ref(0);
+const autoResolveMessage = ref('');
 
-const handleSubmit = () => {
-  // Add submit handling logic here
+const { currentAccount, updateAccount } = useAccount();
+
+const durationToDisplay = computed({
+  get() {
+    return currentAccount.value?.settings?.auto_resolve_after;
+  },
+  set(value) {
+    duration.value = value;
+  },
+});
+
+const messageToDisplay = computed({
+  get() {
+    return currentAccount.value?.settings?.auto_resolve_message;
+  },
+  set(value) {
+    autoResolveMessage.value = value;
+  },
+});
+
+const updateAccountSettings = async settings => {
+  try {
+    await updateAccount({ settings });
+    useAlert(t('GENERAL_SETTINGS.FORM.AUTO_RESOLVE_DURATION.API.SUCCESS'));
+  } catch (error) {
+    useAlert(t('GENERAL_SETTINGS.FORM.AUTO_RESOLVE_DURATION.API.ERROR'));
+  }
+};
+
+const handleSubmit = async () => {
+  return updateAccountSettings({
+    auto_resolve_after: duration.value,
+    auto_resolve_message: autoResolveMessage.value,
+  });
+};
+
+const handleDisable = async () => {
+  return updateAccountSettings({
+    auto_resolve_after: null,
+    auto_resolve_message: autoResolveMessage.value,
+  });
 };
 </script>
 
@@ -29,7 +71,7 @@ const handleSubmit = () => {
         <div class="gap-2 w-full grid grid-cols-[3fr_1fr]">
           <!-- allow 0 to 999 days -->
           <DurationInput
-            v-model="duration"
+            v-model="durationToDisplay"
             min="0"
             max="1439856"
             class="w-full"
@@ -42,13 +84,24 @@ const handleSubmit = () => {
           t('GENERAL_SETTINGS.FORM.AUTO_RESOLVE_DURATION.MESSAGE_HELP')
         "
       >
-        <TextAreaNext
+        <TextArea
+          v-model="messageToDisplay"
           class="w-full"
           :placeholder="
             t('GENERAL_SETTINGS.FORM.AUTO_RESOLVE_DURATION.MESSAGE_PLACEHOLDER')
           "
         />
       </WithLabel>
+      <div class="flex gap-2">
+        <NextButton blue type="submit" label="Update Auto Resolve" />
+        <NextButton
+          v-if="durationToDisplay"
+          type="button"
+          slate
+          label="Disable Auto Resolve"
+          @click.prevent="handleDisable"
+        />
+      </div>
     </form>
   </SectionLayout>
 </template>
