c0b8b4aa46136d7526b377376c3d82bf839f9989
iamsivin <iamsivin@gmail.com>
2025-04-24 21:13:47 +0530

    chore: Review fix

--- app/javascript/dashboard/components-next/message/bubbles/Activity.vue
+++ app/javascript/dashboard/components-next/message/bubbles/Activity.vue
@@ -14,7 +14,7 @@ const readableTime = computed(() =>
 <template>
   <BaseBubble
     v-tooltip.top="readableTime"
-    class="px-3 py-1 !rounded-3xl flex min-w-0 items-center gap-2"
+    class="px-3 py-1 !rounded-xl flex min-w-0 items-center gap-2"
     data-bubble-name="activity"
   >
     <span v-dompurify-html="content" :title="content" />
