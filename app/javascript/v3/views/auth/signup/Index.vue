3e27b82b89ec9127b1e838743490ff1ff828a363
Yunus <yunus@radyalabs.com>
2025-04-30 15:53:42 +0700

    Update design signup

--- app/javascript/v3/views/auth/signup/Index.vue
+++ app/javascript/v3/views/auth/signup/Index.vue
@@ -34,40 +34,46 @@ export default {
 
 <template>
   <div class="w-full h-full dark:bg-slate-900">
-    <div v-show="!isLoading" class="flex h-full min-h-screen items-center">
+    <div v-show="!isLoading" class="flex h-full min-h-screen">
       <div
-        class="flex-1 min-h-[640px] inline-flex items-center h-full justify-center overflow-auto py-6"
+        class="flex flex-col md:flex-row *:md:flex-1 w-full"
       >
-        <div class="px-8 max-w-[560px] w-full overflow-auto">
-          <div class="mb-4">
-            <img
-              :src="globalConfig.logo"
-              :alt="globalConfig.installationName"
-              class="block w-auto h-8 dark:hidden"
-            />
-            <img
-              v-if="globalConfig.logoDark"
-              :src="globalConfig.logoDark"
-              :alt="globalConfig.installationName"
-              class="hidden w-auto h-8 dark:block"
-            />
-            <h2
-              class="mt-6 text-3xl font-medium text-left mb-7 text-slate-900 dark:text-woot-50"
-            >
-              {{ $t('REGISTER.TRY_WOOT') }}
-            </h2>
-          </div>
-          <SignupForm />
-          <div class="px-1 text-sm text-slate-800 dark:text-woot-50">
-            <span>{{ $t('REGISTER.HAVE_AN_ACCOUNT') }}</span>
-            <router-link class="text-link" to="/app/login">
-              {{
-                useInstallationName(
-                  $t('LOGIN.TITLE'),
-                  globalConfig.installationName
-                )
-              }}
-            </router-link>
+      <div class="py-16 md:flex-1 flex" style="background: linear-gradient(180deg, #0F0F0F 0%, #1A7B7D 100%);">
+        <div class="flex flex-row items-center justify-center w-full h-full">
+          <img
+            :src="globalConfig.logo"
+            :alt="globalConfig.installationName"
+            class="block w-auto h-11 mx-auto dark:hidden"
+          />
+          <img
+            v-if="globalConfig.logoDark"
+            :src="globalConfig.logoDark"
+            :alt="globalConfig.installationName"
+            class="hidden w-auto h-11 mx-auto dark:block"
+          />
+        </div>
+      </div>
+        <div class="px-8 w-full overflow-auto flex items-center">
+          <div class="w-full">
+            <div class="mb-4">
+              <h2
+                class="mt-6 text-3xl font-medium text-left mb-7 text-slate-900 dark:text-woot-50"
+              >
+                {{ $t('REGISTER.TRY_WOOT') }}
+              </h2>
+            </div>
+            <SignupForm />
+            <div class="px-1 text-sm text-slate-800 dark:text-woot-50">
+              <span>{{ $t('REGISTER.HAVE_AN_ACCOUNT') }}</span>
+              <router-link class="text-link" to="/app/login">
+                {{
+                  useInstallationName(
+                    $t('LOGIN.TITLE'),
+                    globalConfig.installationName
+                  )
+                }}
+              </router-link>
+            </div>
           </div>
         </div>
       </div>
