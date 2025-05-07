6a532dc3e5139c8c19c23f0f0d322c78120e1f46
Vishnu Narayanan <vishnu@chatwoot.com>
2025-05-07 22:56:55 +0530

    fix: thottle stalecontacts job

--- app/jobs/internal/process_stale_contacts_job.rb
+++ app/jobs/internal/process_stale_contacts_job.rb
@@ -7,14 +7,18 @@
 class Internal::ProcessStaleContactsJob < ApplicationJob
   queue_as :scheduled_jobs
 
+  ACCOUNTS_PER_DAY = 5000
+
   def perform
     return unless ChatwootApp.chatwoot_cloud?
 
-    Account.find_in_batches(batch_size: 100) do |accounts|
-      accounts.each do |account|
-        Rails.logger.info "Enqueuing RemoveStaleContactsJob for account #{account.id}"
-        Internal::RemoveStaleContactsJob.perform_later(account)
-      end
+    # Use day of week (0-6) to determine which accounts to process today
+    day_offset = Time.current.wday * ACCOUNTS_PER_DAY
+
+    Account.order(:id).offset(day_offset).limit(ACCOUNTS_PER_DAY).find_each do |account|
+      Rails.logger.info "Enqueuing RemoveStaleContactsJob for account #{account.id}"
+      # Add a small delay between jobs to prevent queue overload
+      Internal::RemoveStaleContactsJob.set(wait: 5.seconds).perform_later(account)
     end
   end
 end
