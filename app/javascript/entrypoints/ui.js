fc119d14d9d035b0565a1bfa3db590c5c8e1611d
Shivam Mishra <scm.mymail@gmail.com>
2025-05-08 09:06:49 +0530

    feat: styles are working

--- app/javascript/entrypoints/ui.js
+++ app/javascript/entrypoints/ui.js
@@ -4,6 +4,8 @@ import { createI18n } from 'vue-i18n';
 import '../dashboard/assets/scss/app.scss';
 import 'vue-multiselect/dist/vue-multiselect.css';
 import 'floating-vue/dist/style.css';
+// Import Tailwind styles - we'll use these in the shadow DOM
+import tailwindStyles from '../dashboard/assets/scss/_woot.scss?inline';
 
 import store from 'dashboard/store';
 import constants from 'dashboard/constants/globals';
@@ -12,7 +14,7 @@ import createAxios from 'dashboard/helper/APIHelper';
 import commonHelpers from 'dashboard/helper/commons';
 import i18nMessages from 'dashboard/i18n';
 
-import ChatButton from '../ui/ChatButton.vue';
+import WootButton from '../dashboard/components-next/button/Button.vue';
 import WootInput from '../dashboard/components-next/input/Input.vue';
 import WootMessage from '../dashboard/components-next/message/Message.vue';
 
@@ -27,13 +29,15 @@ const ceOptions = {
     app.use(i18n);
     app.use(store);
   },
+  // Include tailwind styles in the shadow DOM of each custom element
+  styles: [tailwindStyles],
 };
 
 commonHelpers();
 window.WootConstants = constants;
 window.axios = createAxios(axios);
 
-export const buttonElement = defineCustomElement(ChatButton, ceOptions);
+export const buttonElement = defineCustomElement(WootButton, ceOptions);
 export const inputElement = defineCustomElement(WootInput, ceOptions);
 export const messageElement = defineCustomElement(WootMessage, ceOptions);
 
@@ -42,5 +46,3 @@ window.__CHATWOOT_STORE__ = store;
 customElements.define('woot-button', buttonElement);
 customElements.define('woot-input', inputElement);
 customElements.define('woot-message', messageElement);
-
-export { store, ChatButton };
