1e5ecfe0125de220b81f96d41b8f68aa34bddc33
Vishnu Narayanan <vishnu@chatwoot.com>
2025-05-13 17:39:14 +0530

    chore: refactor specs

--- spec/services/email_validation_service_spec.rb
+++ spec/services/email_validation_service_spec.rb
@@ -3,154 +3,87 @@
 require 'rails_helper'
 
 RSpec.describe EmailValidationService, type: :service do
+  let(:service) { described_class.new(email) }
+  let(:blocked_domains) { "gmail.com\noutlook.com" }
+
+  before do
+    allow(GlobalConfigService).to receive(:load).with('BLOCKED_EMAIL_DOMAINS', '').and_return(blocked_domains)
+  end
+
   describe '#validate' do
-    let(:service) { described_class.new(email) }
+    context 'with blocked domain' do
+      let(:email) { 'test@gmail.com' }
 
-    context 'when email domain is blocked' do
-      before do
-        allow(GlobalConfigService).to receive(:load).with('BLOCKED_EMAIL_DOMAINS', '').and_return("gmail.com\noutlook.com")
-      end
-
-      context 'with lowercase domain' do
-        let(:email) { 'test@gmail.com' }
-
-        it 'raises InvalidEmail exception' do
-          expect { service.validate }.to raise_error(CustomExceptions::Account::InvalidEmail)
-        end
-      end
-
-      context 'with uppercase domain' do
-        let(:email) { 'test@GMAIL.COM' }
-
-        it 'raises InvalidEmail exception' do
-          expect { service.validate }.to raise_error(CustomExceptions::Account::InvalidEmail)
-        end
-      end
-
-      context 'with mixed case domain' do
-        let(:email) { 'test@Gmail.com' }
-
-        it 'raises InvalidEmail exception' do
-          expect { service.validate }.to raise_error(CustomExceptions::Account::InvalidEmail)
-        end
+      it 'raises InvalidEmail exception' do
+        expect { service.validate }.to raise_error(CustomExceptions::Account::InvalidEmail)
       end
     end
 
-    context 'when email domain is not blocked' do
-      before do
-        allow(GlobalConfigService).to receive(:load).with('BLOCKED_EMAIL_DOMAINS', '').and_return('')
+    context 'with case insensitive matching' do
+      let(:email) { 'test@GMAIL.COM' }
+
+      it 'raises InvalidEmail exception' do
+        expect { service.validate }.to raise_error(CustomExceptions::Account::InvalidEmail)
       end
+    end
 
-      context 'with valid email' do
-        let(:email) { 'test@example.com' }
+    context 'with valid non-blocked email' do
+      let(:email) { 'test@example.com' }
 
-        it 'does not raise an exception' do
-          expect { service.validate }.not_to raise_error
-        end
+      it 'does not raise an exception' do
+        expect { service.validate }.not_to raise_error
       end
+    end
 
-      context 'with invalid email format' do
-        let(:email) { 'invalid-email' }
+    context 'with invalid email format' do
+      let(:email) { 'invalid-email' }
 
-        it 'raises InvalidEmail exception' do
-          expect { service.validate }.to raise_error(CustomExceptions::Account::InvalidEmail)
-        end
+      it 'raises InvalidEmail exception' do
+        expect { service.validate }.to raise_error(CustomExceptions::Account::InvalidEmail)
       end
+    end
 
-      context 'with disposable email' do
-        let(:email) { 'test@mailinator.com' }
+    context 'with disposable email' do
+      let(:email) { 'test@mailinator.com' }
 
-        it 'raises InvalidEmail exception' do
-          expect { service.validate }.to raise_error(CustomExceptions::Account::InvalidEmail)
-        end
+      it 'raises InvalidEmail exception' do
+        expect { service.validate }.to raise_error(CustomExceptions::Account::InvalidEmail)
       end
     end
   end
 
   describe '#business_email?' do
-    let(:service) { described_class.new(email) }
+    context 'with blocked domain' do
+      let(:email) { 'test@gmail.com' }
 
-    context 'when email domain is blocked' do
-      before do
-        allow(GlobalConfigService).to receive(:load).with('BLOCKED_EMAIL_DOMAINS', '').and_return("gmail.com\noutlook.com")
-      end
-
-      context 'with lowercase domain' do
-        let(:email) { 'test@gmail.com' }
-
-        it 'returns false' do
-          expect(service.business_email?).to be(false)
-        end
-      end
-
-      context 'with uppercase domain' do
-        let(:email) { 'test@GMAIL.COM' }
-
-        it 'returns false' do
-          expect(service.business_email?).to be(false)
-        end
+      it 'returns false' do
+        expect(service.business_email?).to be(false)
       end
     end
 
-    context 'when email domain is not blocked' do
-      before do
-        allow(GlobalConfigService).to receive(:load).with('BLOCKED_EMAIL_DOMAINS', '').and_return("gmail.com\noutlook.com")
-      end
+    context 'with non-blocked domain' do
+      let(:email) { 'test@example.com' }
 
-      context 'with non-blocked domain' do
-        let(:email) { 'test@example.com' }
-
-        it 'returns true' do
-          expect(service.business_email?).to be(true)
-        end
+      it 'returns true' do
+        expect(service.business_email?).to be(true)
       end
     end
   end
 
   describe '#domain_blocked?' do
-    let(:service) { described_class.new(email) }
+    context 'with blocked domain (case insensitive)' do
+      let(:email) { 'test@Gmail.com' }
 
-    context 'when email domain is blocked' do
-      before do
-        allow(GlobalConfigService).to receive(:load).with('BLOCKED_EMAIL_DOMAINS', '').and_return("gmail.com\noutlook.com")
-      end
-
-      context 'with lowercase domain' do
-        let(:email) { 'test@gmail.com' }
-
-        it 'returns true' do
-          expect(service.domain_blocked?).to be(true)
-        end
-      end
-
-      context 'with uppercase domain' do
-        let(:email) { 'test@GMAIL.COM' }
-
-        it 'returns true' do
-          expect(service.domain_blocked?).to be(true)
-        end
-      end
-
-      context 'with mixed case domain' do
-        let(:email) { 'test@Gmail.com' }
-
-        it 'returns true' do
-          expect(service.domain_blocked?).to be(true)
-        end
+      it 'returns true' do
+        expect(service.domain_blocked?).to be(true)
       end
     end
 
-    context 'when email domain is not blocked' do
-      before do
-        allow(GlobalConfigService).to receive(:load).with('BLOCKED_EMAIL_DOMAINS', '').and_return("gmail.com\noutlook.com")
-      end
+    context 'with non-blocked domain' do
+      let(:email) { 'test@example.com' }
 
-      context 'with non-blocked domain' do
-        let(:email) { 'test@example.com' }
-
-        it 'returns false' do
-          expect(service.domain_blocked?).to be(false)
-        end
+      it 'returns false' do
+        expect(service.domain_blocked?).to be(false)
       end
     end
   end
