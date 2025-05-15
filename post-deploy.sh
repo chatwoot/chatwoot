d964d324c2356ee4e053abd20012776bd6e6d923
Amit Pareek <amit@cakewalk.in>
2025-05-15 22:55:27 +0400

    Updated post-deploy.sh

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# Run migrations
-bundle exec rails db:migrate
-
 # Ensure installation type is set correctly
 bundle exec rails runner "
   begin
@@ -11,8 +7,8 @@ bundle exec rails runner "
     installation_config.value = { installation_type: 'self_hosted' }.to_json
     installation_config.save!
     
-    env_config = GlobalConfig.find_by(name: 'INSTALLATION_ENV')
-    env_config = GlobalConfig.new(name: 'INSTALLATION_ENV') if env_config.nil?
+    env_config = InstallationConfig.find_by(name: 'INSTALLATION_ENV')
+    env_config = InstallationConfig.new(name: 'INSTALLATION_ENV') if env_config.nil?
     env_config.value = 'self_hosted'
     env_config.save!
     
