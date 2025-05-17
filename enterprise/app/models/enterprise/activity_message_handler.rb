cbdac458240f0948857b47fa949c139295ec57c0
Pranav <pranav@chatwoot.com>
2025-05-16 19:27:57 -0700

    feat: Improve Captain interactions, activity messages (#11493)
    Show captain messages under the name of the assistant which generated
    the message.
    
    - Add support for `Captain::Assistant` sender type
    - Add push_event_data for captain_assistants
    - Add activity message handler for captain_assistants
    - Update UI to show captain messages under the name of the assistant
    - Fix the issue where openAI errors when image is sent
    - Add support for custom name of the assistant
    
    ---------
    
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>
    Co-authored-by: Sivin Varghese <64252451+iamsivin@users.noreply.github.com>

--- enterprise/app/models/enterprise/activity_message_handler.rb
+++ enterprise/app/models/enterprise/activity_message_handler.rb
@@ -1,7 +1,20 @@
 module Enterprise::ActivityMessageHandler
   def automation_status_change_activity_content
-    if Current.executed_by.instance_of?(Captain::Assistant) && resolved?
-      I18n.t('conversations.activity.captain.resolved', user_name: Current.executed_by.name)
+    if Current.executed_by.instance_of?(Captain::Assistant)
+      locale = Current.executed_by.account.locale
+      if resolved?
+        I18n.t(
+          'conversations.activity.captain.resolved',
+          user_name: Current.executed_by.name,
+          locale: locale
+        )
+      elsif open?
+        I18n.t(
+          'conversations.activity.captain.open',
+          user_name: Current.executed_by.name,
+          locale: locale
+        )
+      end
     else
       super
     end
