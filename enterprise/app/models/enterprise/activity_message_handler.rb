0bbde870b5ce6c203966fbd4a3cc266d7f1846dd
Pranav <pranavrajs@gmail.com>
2025-05-16 18:57:09 -0700

    Codeclimate

--- enterprise/app/models/enterprise/activity_message_handler.rb
+++ enterprise/app/models/enterprise/activity_message_handler.rb
@@ -2,15 +2,15 @@ module Enterprise::ActivityMessageHandler
   def automation_status_change_activity_content
     if Current.executed_by.instance_of?(Captain::Assistant)
       locale = Current.executed_by.account.locale
-      message = if resolved?
-                  'conversations.activity.captain.resolved'
-                elsif open?
-                  'conversations.activity.captain.open'
-                end
-
-      if message.present?
+      if resolved?
         I18n.t(
-          message,
+          'conversations.activity.captain.resolved',
+          user_name: Current.executed_by.name,
+          locale: locale
+        )
+      elsif open?
+        I18n.t(
+          'conversations.activity.captain.open',
           user_name: Current.executed_by.name,
           locale: locale
         )
