c8de3ebeb033f2acf0c0f0585795163110c3ea97
Pranav <pranavrajs@gmail.com>
2025-05-16 18:46:18 -0700

    Update activity message

--- enterprise/app/models/enterprise/activity_message_handler.rb
+++ enterprise/app/models/enterprise/activity_message_handler.rb
@@ -1,7 +1,20 @@
 module Enterprise::ActivityMessageHandler
   def automation_status_change_activity_content
-    if Current.executed_by.instance_of?(Captain::Assistant) && resolved?
-      I18n.t('conversations.activity.captain.resolved', user_name: Current.executed_by.name)
+    if Current.executed_by.instance_of?(Captain::Assistant)
+      locale = Current.executed_by.account.locale
+      message = if resolved?
+                  'conversations.activity.captain.resolved'
+                elsif open?
+                  'conversations.activity.captain.open'
+                end
+
+      if message.present?
+        I18n.t(
+          message,
+          user_name: Current.executed_by.name,
+          locale: locale
+        )
+      end
     else
       super
     end
