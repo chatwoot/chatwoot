58c33743a1651602799faa1f80d7b4d0930d6c59
Pranav <pranavrajs@gmail.com>
2025-05-19 17:00:56 -0700

    Remove unnecessary changes

--- app/models/account.rb
+++ app/models/account.rb
@@ -64,7 +64,6 @@ class Account < ApplicationRecord
   has_many :canned_responses, dependent: :destroy_async
   has_many :categories, dependent: :destroy_async, class_name: '::Category'
   has_many :contacts, dependent: :destroy_async
-  has_many :copilot_threads, dependent: :destroy
   has_many :conversations, dependent: :destroy_async
   has_many :csat_survey_responses, dependent: :destroy_async
   has_many :custom_attribute_definitions, dependent: :destroy_async
