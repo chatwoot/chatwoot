33c75c4067d6c6dd5c824db335e7764721aa76b1
Shivam Mishra <scm.mymail@gmail.com>
2025-05-21 15:50:24 +0530

    feat: allow saving unit and label

--- app/models/account.rb
+++ app/models/account.rb
@@ -35,7 +35,9 @@ class Account < ApplicationRecord
       {
         'auto_resolve_after': { 'type': %w[integer null], 'minimum': 10, 'maximum': 1_439_856 },
         'auto_resolve_message': { 'type': %w[string null] },
-        'auto_resolve_ignore_waiting': { 'type': %w[boolean null] }
+        'auto_resolve_unit': { 'enum': ['days', 'minutes', 'hours', nil] },
+        'auto_resolve_ignore_waiting': { 'type': %w[boolean null] },
+        'auto_resolve_label': { 'type': %w[string null] }
       },
     'required': [],
     'additionalProperties': true
