9c0ef2d8996ab15cab59b47949e42ac92d4d137f
Bayu <mbayu.nugraha@radyalabs.com>
2025-05-22 08:51:15 +0700

    debug payload

--- app/listeners/webhook_listener_wa_unofficial.rb
+++ app/listeners/webhook_listener_wa_unofficial.rb
@@ -1,5 +1,5 @@
 module WebhookListenerWaUnofficial
-  def perform(inbox, payload)
+  def self.perform(inbox, payload)
     webhook_url = inbox.channel.webhook_url
     uri = URI.parse(webhook_url)
     params = URI.decode_www_form(uri.query).to_h
