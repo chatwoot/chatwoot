e6b8aded398b0aa09b769bf8bd64a4119b4ef00c
Shivam Mishra <scm.mymail@gmail.com>
2025-05-22 11:46:24 +0530

    feat: add embed renderer

--- lib/custom_markdown_renderer.rb
+++ lib/custom_markdown_renderer.rb
@@ -78,81 +78,30 @@ class CustomMarkdownRenderer < CommonMarker::HtmlRenderer
 
   def make_youtube_embed(youtube_match)
     video_id = youtube_match[1]
-    %(
-      <div style="position: relative; padding-bottom: 62.5%; height: 0;">
-       <iframe
-        src="https://www.youtube-nocookie.com/embed/#{video_id}"
-        frameborder="0"
-        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen></iframe>
-      </div>
-    )
+    EmbedRenderer.youtube(video_id)
   end
 
   def make_loom_embed(loom_match)
     video_id = loom_match[1]
-    %(
-      <div style="position: relative; padding-bottom: 62.5%; height: 0;">
-        <iframe
-         src="https://www.loom.com/embed/#{video_id}"
-         frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen
-         style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
-      </div>
-    )
+    EmbedRenderer.loom(video_id)
   end
 
   def make_vimeo_embed(vimeo_match)
     video_id = vimeo_match[1]
-    %(
-      <div style="position: relative; padding-bottom: 62.5%; height: 0;">
-       <iframe
-        src="https://player.vimeo.com/video/#{video_id}?dnt=true"
-        frameborder="0"
-        allow="autoplay; fullscreen; picture-in-picture"
-        allowfullscreen
-        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
-       </div>
-    )
+    EmbedRenderer.vimeo(video_id)
   end
 
   def make_video_embed(link_url)
-    %(
-      <video width="640" height="360" controls>
-        <source src="#{link_url}" type="video/mp4">
-        Your browser does not support the video tag.
-      </video>
-    )
+    EmbedRenderer.video(link_url)
   end
 
-  # Generates an HTML embed for a Wistia video.
-  # @param wistia_match [MatchData] A match object from the WISTIA_REGEX regex, where wistia_match[2] contains the video ID.
   def make_wistia_embed(wistia_match)
     video_id = wistia_match[2]
-    %(
-      <div style="position: relative; padding-bottom: 56.25%; height: 0;">
-        <script src="https://fast.wistia.com/player.js" async></script>
-        <script src="https://fast.wistia.com/embed/#{video_id}.js" async type="module"></script>
-        <style>wistia-player[media-id='#{video_id}']:not(:defined) { background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/#{video_id}/swatch'); display: block; filter: blur(5px); padding-top:56.25%; }</style>
-        <wistia-player media-id="#{video_id}" aspect="1.7777777777777777" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></wistia-player>
-      </div>
-    )
+    EmbedRenderer.wistia(video_id)
   end
 
   def make_arcade_embed(arcade_match)
     video_id = arcade_match[1]
-    %(
-    <div style="position: relative; padding-bottom: 62.5%; height: 0;">
-      <iframe
-        src="https://app.arcade.software/embed/#{video_id}"
-        frameborder="0"
-        webkitallowfullscreen
-        mozallowfullscreen
-        allowfullscreen
-        allow="fullscreen"
-        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
-      </iframe>
-    </div>
-  )
+    EmbedRenderer.arcade(video_id)
   end
 end
