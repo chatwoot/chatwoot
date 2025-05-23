a48b1341e9073176d3d8a200b27f962b8c1cbf2c
Pranav <pranavrajs@gmail.com>
2025-05-22 22:00:46 -0700

    Move to action cable listener

--- enterprise/app/listeners/captain_listener.rb
+++ enterprise/app/listeners/captain_listener.rb
@@ -10,23 +10,4 @@ class CaptainListener < BaseListener
     Captain::Llm::ContactNotesService.new(assistant, conversation).generate_and_update_notes if assistant.config['feature_memory'].present?
     Captain::Llm::ConversationFaqService.new(assistant, conversation).generate_and_deduplicate if assistant.config['feature_faq'].present?
   end
-
-  def copilot_message_created(event)
-    copilot_message = event.data[:copilot_message]
-    copilot_thread = copilot_message.copilot_thread
-    account = copilot_thread.account
-    user = copilot_thread.user
-
-    broadcast(account, [user.pubsub_token], COPILOT_MESSAGE_CREATED, copilot_message.push_event_data)
-  end
-
-  private
-
-  def broadcast(account, tokens, event_name, data)
-    return if tokens.blank?
-
-    payload = data.merge(account_id: account.id)
-
-    ::ActionCableBroadcastJob.perform_later(tokens.uniq, event_name, payload)
-  end
 end
--- spec/enterprise/listeners/captain_listener_spec.rb
+++ spec/enterprise/listeners/captain_listener_spec.rb
@@ -54,21 +54,4 @@ describe CaptainListener do
       end
     end
   end
-
-  describe '#copilot_message_created' do
-    let(:event_name) { :copilot_message_created }
-    let(:copilot_thread) { create(:captain_copilot_thread, account: account, user: user, assistant: assistant) }
-    let(:copilot_message) { create(:captain_copilot_message, copilot_thread: copilot_thread) }
-    let(:event) { Events::Base.new(event_name, Time.zone.now, copilot_message: copilot_message) }
-
-    it 'broadcasts message to the user' do
-      expect(ActionCableBroadcastJob).to receive(:perform_later).with(
-        [user.pubsub_token],
-        'copilot.message.created',
-        copilot_message.push_event_data.merge(account_id: account.id)
-      )
-
-      listener.copilot_message_created(event)
-    end
-  end
 end
