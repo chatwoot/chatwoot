225a682e7c58603e2e71d4aa0626ea6d91ec1e66
shaunak45 <shaunakk@usc.edu>
2025-05-27 12:04:24 +0000

    Custom Brand UI

--- theme/colors.js
+++ theme/colors.js
@@ -11,23 +11,26 @@ const {
   redDark,
   violet,
   violetDark,
+  orange,
+  orangeDark
 } = require('@radix-ui/colors');
 
 export const colors = {
   woot: {
-    25: blue.blue2,
-    50: blue.blue3,
-    75: blue.blue4,
-    100: blue.blue5,
-    200: blue.blue7,
-    300: blue.blue8,
-    400: blueDark.blue11,
-    500: blueDark.blue10,
-    600: blueDark.blue9,
-    700: blueDark.blue8,
-    800: blueDark.blue6,
-    900: blueDark.blue2,
-  },
+    25: orange.orange2,
+    50: orange.orange3,
+    75: orange.orange4,
+    100: orange.orange5,
+    200: orange.orange7,
+    300: orange.orange8,
+    400: orangeDark.orange11,
+    500: orangeDark.orange10,
+    600: orangeDark.orange9,
+    700: orangeDark.orange8,
+    800: orangeDark.orange6,
+    900: orangeDark.orange2,
+}
+,
   green: {
     50: greenDark.green12,
     100: green.green6,
@@ -414,6 +417,6 @@ export const colors = {
     container: 'rgba(var(--border-container))',
     strong: 'rgb(var(--border-strong) / <alpha-value>)',
     'blue-border': 'rgba(var(--border-blue))',
-    'blue-text': 'rgba(var(--text-blue))',
+    'blue-text': 'rgba(var(--text-custom))',
   },
 };
