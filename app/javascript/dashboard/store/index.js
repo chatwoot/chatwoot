7d1f63cea6a2b37dead9f4c2b43ae878c7b232e0
Pranav <pranavrajs@gmail.com>
2025-05-27 17:47:14 -0600

    feat: Add stores for copilotMessages and copilotThreads

--- app/javascript/dashboard/store/index.js
+++ app/javascript/dashboard/store/index.js
@@ -51,6 +51,9 @@ import captainDocuments from './captain/document';
 import captainResponses from './captain/response';
 import captainInboxes from './captain/inboxes';
 import captainBulkActions from './captain/bulkActions';
+import copilotThreads from './captain/copilotThreads';
+import copilotMessages from './captain/copilotMessages';
+
 const plugins = [];
 
 export default createStore({
@@ -106,6 +109,8 @@ export default createStore({
     captainResponses,
     captainInboxes,
     captainBulkActions,
+    copilotThreads,
+    copilotMessages,
   },
   plugins,
 });
