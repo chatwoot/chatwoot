57fd50bf5c56efd209ebf68774e3f9a3f31d96e0
Muhsin Keloth <muhsinkeramam@gmail.com>
2025-05-30 11:45:50 +0530

    chore: code cleanups

--- app/services/csat_survey_service.rb
+++ app/services/csat_survey_service.rb
@@ -16,14 +16,15 @@ class CsatSurveyService
   delegate :inbox, :contact, to: :conversation
 
   def should_send_csat_survey?
-    return false unless conversation.resolved?
-    # should not send since the link will be public
-    return false if conversation.tweet?
-    return false unless inbox.csat_survey_enabled?
-    # only send CSAT once per conversation
-    return false if csat_already_sent?
+    conversation_allows_csat? && csat_enabled? && !csat_already_sent?
+  end
 
-    true
+  def conversation_allows_csat?
+    conversation.resolved? && !conversation.tweet?
+  end
+
+  def csat_enabled?
+    inbox.csat_survey_enabled?
   end
 
   def csat_already_sent?
