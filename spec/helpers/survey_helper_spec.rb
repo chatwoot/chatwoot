b7c68619818aea5936d4a578ae084a6c768b9ff6
Muhsin Keloth <muhsinkeramam@gmail.com>
2025-06-03 11:02:46 +0530

    Update survey_helper_spec.rb

--- spec/helpers/survey_helper_spec.rb
+++ spec/helpers/survey_helper_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe SurveyHelper do
-  describe '.survey_url' do
-    let(:conversation_uuid) { 'test-uuid-123' }
+  let(:conversation_uuid) { 'test-uuid-123' }
+  let(:helper_instance) { Object.new.extend(described_class) }
 
+  describe '#survey_url' do
     context 'when FRONTEND_URL is set' do
       before do
         allow(ENV).to receive(:fetch).with('FRONTEND_URL', nil).and_return('https://app.chatwoot.com')
@@ -11,7 +12,7 @@ describe SurveyHelper do
 
       it 'returns the correct survey URL' do
         expected_url = 'https://app.chatwoot.com/survey/responses/test-uuid-123'
-        expect(described_class.survey_url(conversation_uuid)).to eq expected_url
+        expect(helper_instance.survey_url(conversation_uuid)).to eq expected_url
       end
     end
   end
