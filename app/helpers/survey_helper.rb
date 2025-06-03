93ae146d37a36d4b1a6745a7b6227275f7ebcbaa
Muhsin Keloth <muhsinkeramam@gmail.com>
2025-06-03 10:59:53 +0530

    chore: code cleanups

--- app/helpers/survey_helper.rb
+++ app/helpers/survey_helper.rb
@@ -2,8 +2,4 @@ module SurveyHelper
   def survey_url(conversation_uuid)
     "#{ENV.fetch('FRONTEND_URL', nil)}/survey/responses/#{conversation_uuid}"
   end
-
-  def self.survey_url(conversation_uuid)
-    "#{ENV.fetch('FRONTEND_URL', nil)}/survey/responses/#{conversation_uuid}"
-  end
 end
