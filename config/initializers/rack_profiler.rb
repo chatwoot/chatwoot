a2849b919b52ceded286f37a4ced729ea4e0d82d
Sojan <sojan@pepalo.com>
2025-06-03 23:01:37 -0500

    chore: Improvements for codespaces

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if Rails.env.development?
+if Rails.env.development? && !ENV['DISABLE_MINI_PROFILER'].present?
   require 'rack-mini-profiler'
 
   # initialization is skipped so trigger it
