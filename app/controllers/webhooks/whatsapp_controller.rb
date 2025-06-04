c67c14ada10de34405e2bc9ea3c3586d72ddee59
iwanlaudin <iwan@radyalabs.com>
2025-06-04 13:48:28 +0700

    Add logging of webhook params and phone number in WhatsApp controller
    Log incoming webhook parameters and phone number in process_payload to improve debugging and traceability of WhatsApp webhook requests.

--- app/controllers/webhooks/whatsapp_controller.rb
+++ app/controllers/webhooks/whatsapp_controller.rb
@@ -2,6 +2,8 @@ class Webhooks::WhatsappController < ActionController::API
   include MetaTokenVerifyConcern
 
   def process_payload
+    Rails.logger.info "[WHATSAPP WEBHOOK] Params: #{params.to_unsafe_hash.inspect}"
+    Rails.logger.info "[WHATSAPP WEBHOOK] Phone number: #{params[:phone_number]}"
     Webhooks::WhatsappEventsJob.perform_later(params.to_unsafe_hash)
     head :ok
   end
