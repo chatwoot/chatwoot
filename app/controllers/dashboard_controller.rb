2832409562d53478829753db126583f1bd5d202c
Pranav <pranavrajs@gmail.com>
2025-06-10 14:18:36 -0400

    fix: Avoid throwing 406 for non-json requests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -15,7 +15,7 @@ class DashboardController < ActionController::Base
   private
 
   def ensure_html_format
-    head :not_acceptable unless request.format.html?
+    head :not_acceptable if request.format.json?
   end
 
   def set_global_config
