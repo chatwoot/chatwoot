0be44cf471c37e8cb7bbd5f85e0db7e1d2b0865b
Yunus <yunus@radyalabs.com>
2025-06-10 17:59:25 +0700

    Update landing page

---
+++ public/landing/_next/static/chunks/pages/_error-cc3f077a18ea1793.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{2164:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(9341)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(2164)),_N_E=_.O()}]);
