ba1f363a28b2eab5205e0ee5141acc3b813118ed
Shivam Mishra <scm.mymail@gmail.com>
2025-06-11 17:34:08 +0530

    fix: gap and text color

--- app/javascript/dashboard/components/widgets/WootWriter/EditorModeToggle.vue
+++ app/javascript/dashboard/components/widgets/WootWriter/EditorModeToggle.vue
@@ -55,10 +55,16 @@ const translateValue = computed(() => {
     class="flex items-center w-auto h-8 p-1 transition-all border rounded-full bg-n-alpha-2 group relative duration-300 ease-in-out z-0"
     @click="$emit('toggleMode')"
   >
-    <div ref="wootEditorReplyMode" class="flex items-center gap-1 px-2 z-20">
+    <div
+      ref="wootEditorReplyMode"
+      class="flex items-center gap-1 px-2 z-20 text-n-slate-11"
+    >
       {{ $t('CONVERSATION.REPLYBOX.REPLY') }}
     </div>
-    <div ref="wootEditorPrivateMode" class="flex items-center gap-1 px-2 z-20">
+    <div
+      ref="wootEditorPrivateMode"
+      class="flex items-center gap-1 px-2 z-20 text-n-slate-11"
+    >
       {{ $t('CONVERSATION.REPLYBOX.PRIVATE_NOTE') }}
     </div>
     <div
