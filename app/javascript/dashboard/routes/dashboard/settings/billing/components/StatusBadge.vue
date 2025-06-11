83c331be98208a0a3eb0e1fcbcb26b919356e859
Yunus <yunus@radyalabs.com>
2025-06-11 10:33:38 +0700

    Update wording

--- app/javascript/dashboard/routes/dashboard/settings/billing/components/StatusBadge.vue
+++ app/javascript/dashboard/routes/dashboard/settings/billing/components/StatusBadge.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex items-start">
         <div class="rounded-full px-3 py-2 flex items-center justify-center" :class="{
-            'bg-[#EBF9F1] text-[#1F9254]': data.toLowerCase() === 'paid',
+            'bg-[#EBF9F1] text-[#1F9254]': data.toLowerCase() === 'paid' || data.toLowerCase() === 'success',
             'bg-[#FEF2E5] text-[#CD6200]': data.toLowerCase() === 'pending',
-            'bg-[#FBE7E8] text-[#A30D11]': data.includes('cancel'),
+            'bg-[#FBE7E8] text-[#A30D11]': data.includes('cancel') || data.toLowerCase() === 'expired',
         }">
-        <span class="text-xs font-medium">
-            {{ $t(`BILLING.STATUS_PAYMENT.${data}`) }}
-        </span>
+            <span class="text-xs font-medium">
+                {{ $t(`BILLING.STATUS_PAYMENT.${data}`) }}
+            </span>
         </div>
     </div>
 </template>
