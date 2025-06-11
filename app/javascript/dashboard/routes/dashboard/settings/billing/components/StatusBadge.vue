7a718b3f46137f49c15b3067bebb1720efaafea5
Yunus <yunus@radyalabs.com>
2025-06-11 15:01:13 +0700

    Update badge status payment failed

--- app/javascript/dashboard/routes/dashboard/settings/billing/components/StatusBadge.vue
+++ app/javascript/dashboard/routes/dashboard/settings/billing/components/StatusBadge.vue
@@ -3,7 +3,7 @@
         <div class="rounded-full px-3 py-2 flex items-center justify-center" :class="{
             'bg-[#EBF9F1] text-[#1F9254]': data.toLowerCase() === 'paid' || data.toLowerCase() === 'success',
             'bg-[#FEF2E5] text-[#CD6200]': data.toLowerCase() === 'pending',
-            'bg-[#FBE7E8] text-[#A30D11]': data.includes('cancel') || data.toLowerCase() === 'expired',
+            'bg-[#FBE7E8] text-[#A30D11]': data.toLowerCase() === 'failed' || data.toLowerCase() === 'expired',
         }">
             <span class="text-xs font-medium">
                 {{ $t(`BILLING.STATUS_PAYMENT.${data}`) }}
