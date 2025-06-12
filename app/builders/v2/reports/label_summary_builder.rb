cdf24db35fb76dad39b25d58dafc74f4d6a35f0b
Shivam Mishra <scm.mymail@gmail.com>
2025-06-12 17:19:57 +0530

    style: remove usage of magic number

--- app/builders/v2/reports/label_summary_builder.rb
+++ app/builders/v2/reports/label_summary_builder.rb
@@ -65,7 +65,7 @@ class V2::Reports::LabelSummaryBuilder < V2::Reports::BaseSummaryBuilder
   def fetch_resolved_counts(conversation_filter)
     # since the base query is ActsAsTaggableOn,
     # the status :resolved won't automatically be converted to integer status
-    fetch_counts(conversation_filter.merge(status: 1))
+    fetch_counts(conversation_filter.merge(status: Conversation.statuses[:resolved]))
   end
 
   def fetch_counts(conversation_filter)
