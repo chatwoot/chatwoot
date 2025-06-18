06c01586d5477151acb1452c25c86175d0ca4170
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2025-06-18 22:11:24 +0530

    Update app/javascript/dashboard/components-next/message/Message.vue

--- app/javascript/dashboard/components-next/message/Message.vue
+++ app/javascript/dashboard/components-next/message/Message.vue
@@ -379,8 +379,7 @@ const shouldRenderMessage = computed(() => {
 function openContextMenu(e) {
   const shouldSkipContextMenu =
     e.target?.classList.contains('skip-context-menu') ||
-    e.target?.tagName.toLowerCase() === 'a' ||
-    e.target?.tagName.toLowerCase() === 'img';
+    ['a', 'img'].includes(e.target?.tagName.toLowerCase());
   if (shouldSkipContextMenu || getSelection().toString()) {
     return;
   }
