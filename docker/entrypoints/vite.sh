d2fb2f41869189c65cac9b4db413c77ffd0d285f
Solera <and_val9@hotmail.com>
2025-06-19 13:29:42 -0600

    Fix for vite service not initializing

--- docker/entrypoints/vite.sh
+++ docker/entrypoints/vite.sh
@@ -3,12 +3,13 @@ set -x
 
 rm -rf /app/tmp/pids/server.pid
 rm -rf /app/tmp/cache/bootsnap /app/tmp/cache/vite
+rm -rf /app/tmp/cache/bootsnap /app/tmp/cache/vite
 
 pnpm store prune
 pnpm install --force
 
 # Install both bundler versions to handle any conflicts
-gem install bundler -v "2.5.11"
+gem install bundler -v '2.5.11'
 gem install bundler -v '2.5.16'
 
 # Run bundle install to ensure all gems are properly set up
