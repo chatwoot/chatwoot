3854895a137cd5e7ffcff9a1b3b511ee966aa7f2
Tanmay Deep Sharma <tanmaydeepsharma21@gmail.com>
2025-06-24 09:04:04 +0530

    remove wrong migration file

--- enterprise/db/migrate/20250623_add_document_type_and_file_to_captain_documents.rb
+++
@@ -1,6 +0,0 @@
-class AddDocumentTypeToCaptainDocuments < ActiveRecord::Migration[7.0]
-  def change
-    add_column :captain_documents, :document_type, :integer, default: 0, null: false
-    add_index :captain_documents, :document_type
-  end
-end
