e11741dc3318175d48cbbaa66d42cceaffce4cbd
avnotaklu <sukhmander111101@gmail.com>
2025-06-27 19:18:04 +0530

    Sentry breadcrumbs implemented

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,7 @@ if ENV['SENTRY_DSN'].present?
   Sentry.init do |config|
     config.dsn = ENV['SENTRY_DSN']
     config.enabled_environments = %w[staging production]
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
     # To activate performance monitoring, set one of these options.
     # We recommend adjusting the value in production:
