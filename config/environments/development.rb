470cca448c9a063c64070318aac71283516f7321
Solera <and_val9@hotmail.com>
2025-07-04 10:24:49 -0600

    CU-86a9zxdc2 Enable hot-reload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
   config.after_initialize do
     Bullet.enable = true
     Bullet.bullet_logger = true
+    Bullet.console = true
     Bullet.rails_logger = true
   end
 end
