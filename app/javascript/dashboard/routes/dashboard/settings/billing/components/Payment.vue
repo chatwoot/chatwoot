ca41703407b17304da616008451458d1ee90138b
Yunus <yunus@radyalabs.com>
2025-07-09 14:21:24 +0700

    Update pay button in payment page

--- app/javascript/dashboard/routes/dashboard/settings/billing/components/Payment.vue
+++ app/javascript/dashboard/routes/dashboard/settings/billing/components/Payment.vue
@@ -295,37 +295,10 @@ const submit = async () => {
 
       <!-- Tombol -->
       <div class="w-full mt-6">
-        <button
-          type="button"
-          :disabled="v$.$invalid || isSubmitting"
-          class="bg-primary-600 text-white w-full py-2 rounded font-semibold flex items-center justify-center"
-          @click="submit"
-        >
-          <span v-if="isSubmitting" class="mr-2">
-            <!-- Simple loading spinner -->
-            <svg
-              class="animate-spin h-5 w-5 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                class="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                stroke-width="4"
-              />
-              <path
-                class="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              />
-            </svg>
-          </span>
-          {{ isSubmitting ? 'Processing...' : 'Continue' }}
-        </button>
+        <woot-button :is-loading="isSubmitting"
+          :size="'expanded'" class="w-full" :disabled="v$.$invalid || isSubmitting">
+            {{ isSubmitting ? $t('PAYMENT.PROCESSING') : $t('PAYMENT.CONTINUE') }}
+          </woot-button>
       </div>
     </form>
   </div>
