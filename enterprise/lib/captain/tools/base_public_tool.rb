2f6d58138463ae0a1b0775f0fd16430efd4e51a3
Shivam Mishra <scm.mymail@gmail.com>
2025-07-15 21:53:12 +0530

    feat: get contact_id from the state

--- enterprise/lib/captain/tools/base_public_tool.rb
+++ enterprise/lib/captain/tools/base_public_tool.rb
@@ -30,6 +30,13 @@ class Captain::Tools::BasePublicTool < Agents::Tool
     account_scoped(::Conversation).find_by(id: conversation_id)
   end
 
+  def find_contact(state)
+    contact_id = state[:contact][:id]
+    return nil unless contact_id
+
+    account_scoped(::Contact).find_by(id: contact_id)
+  end
+
   def log_tool_usage(action, details = {})
     Rails.logger.info do
       "#{self.class.name}: #{action} for assistant #{@assistant&.id} - #{details.inspect}"
