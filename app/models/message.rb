6def35f471e7dae96b2731e4b56d22e40c33410e
cdeveloper123 <cdeveloper2016@gmail.com>
2025-07-22 13:04:50 +0530

    fix: changing follow-up delay time

--- app/models/message.rb
+++ app/models/message.rb
@@ -413,7 +413,9 @@ class Message < ApplicationRecord
 
     return if conversation.stop_follow_up
 
-    jid = Conversations::FollowUpJob.set(wait: 24.hours).perform_later(conversation.id, 1)
+    config_value = InstallationConfig.find_by(name: 'FOLLOW_UP_FIRST_DELAY_HOURS')&.value
+    follow_up_1_delay = (config_value || 1).to_i.hours
+    jid = Conversations::FollowUpJob.set(wait: follow_up_1_delay).perform_later(conversation.id, 1)
     conversation.update!(follow_up_jid: jid.provider_job_id)
   end
 
