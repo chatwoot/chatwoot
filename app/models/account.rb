0ac0740d86578566cb35e7ee5b41a2bea2190a23
Sojan Jose <sojan@pepalo.com>
2025-07-24 15:02:13 +0400

    chore: minor cleanup

--- app/models/account.rb
+++ app/models/account.rb
@@ -91,7 +91,6 @@ class Account < ApplicationRecord
   has_many :telegram_bots, dependent: :destroy_async
   has_many :telegram_channels, dependent: :destroy_async, class_name: '::Channel::Telegram'
   has_many :twilio_sms, dependent: :destroy_async, class_name: '::Channel::TwilioSms'
-  has_many :voice_channels, dependent: :destroy_async, class_name: '::Channel::Voice'
   has_many :twitter_profiles, dependent: :destroy_async, class_name: '::Channel::TwitterProfile'
   has_many :users, through: :account_users
   has_many :web_widgets, dependent: :destroy_async, class_name: '::Channel::WebWidget'
