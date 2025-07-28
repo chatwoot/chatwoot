f74adc7f1cb79a59958e9e6deacfdfc49eb24db9
iwanlaudin <iwan@radyalabs.com>
2025-07-28 23:21:36 +0700

    feat: add debug logging for outgoing echo status in message building process

--- app/builders/messages/instagram/message_builder.rb
+++ app/builders/messages/instagram/message_builder.rb
@@ -108,6 +108,7 @@ class Messages::Instagram::MessageBuilder < Messages::Messenger::MessageBuilder
 
   def build_message
     Rails.logger.info("[DEBUG] Building new message #{message_params.inspect} for conversation: #{conversation.id}")
+    Rails.logger.info("[DEBUG] Outgoing echo: #{@outgoing_echo} - Already sent from chatwoot: #{already_sent_from_chatwoot?}")
     return if @outgoing_echo && already_sent_from_chatwoot?
     return if message_content.blank? && all_unsupported_files?
 
