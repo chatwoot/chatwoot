823c058beec7933e9e447aa060c5eca27907814d
alejandropereira <apereiracvo@gmail.com>
2025-07-30 14:35:42 -0600

    Hide team reports, more locales, concepts

--- app/javascript/dashboard/components-next/sidebar/Sidebar.vue
+++ app/javascript/dashboard/components-next/sidebar/Sidebar.vue
@@ -99,12 +99,12 @@ const newReportRoutes = () => [
     to: accountScopedRoute('inbox_reports_index'),
     activeOn: ['inbox_reports_show'],
   },
-  {
-    name: 'Reports Team',
-    label: t('SIDEBAR.REPORTS_TEAM'),
-    to: accountScopedRoute('team_reports_index'),
-    activeOn: ['team_reports_show'],
-  },
+  // {
+  //   name: 'Reports Team',
+  //   label: t('SIDEBAR.REPORTS_TEAM'),
+  //   to: accountScopedRoute('team_reports_index'),
+  //   activeOn: ['team_reports_show'],
+  // },
 ];
 
 const reportRoutes = computed(() => newReportRoutes());
@@ -401,12 +401,12 @@ const menuItems = computed(() => {
           icon: 'i-lucide-square-user',
           to: accountScopedRoute('agent_list'),
         },
-        {
-          name: 'Settings Teams',
-          label: t('SIDEBAR.TEAMS'),
-          icon: 'i-lucide-users',
-          to: accountScopedRoute('settings_teams_list'),
-        },
+        // {
+        //   name: 'Settings Teams',
+        //   label: t('SIDEBAR.TEAMS'),
+        //   icon: 'i-lucide-users',
+        //   to: accountScopedRoute('settings_teams_list'),
+        // },
         {
           name: 'Settings Inboxes',
           label: t('SIDEBAR.INBOXES'),
