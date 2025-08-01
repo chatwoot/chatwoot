95ad9e7ffa6896e473f8e4475dab5e81fa6368b5
Vishnu Narayanan <vishnu@chatwoot.com>
2025-08-01 19:32:04 +0530

    fix: switch to using ec2 instance role

--- config/initializers/sidekiq_cloudwatch.rb
+++ config/initializers/sidekiq_cloudwatch.rb
@@ -1,16 +1,8 @@
 if defined?(Sidekiq::CloudWatchMetrics) && defined?(ChatwootApp) && ChatwootApp.chatwoot_cloud? && ENV['ENABLE_SIDEKIQ_CLOUDWATCH'].present?
   require 'sidekiq/cloudwatchmetrics'
 
-  # Configure AWS region if specified
-  if ENV['AWS_REGION'].present?
-    require 'aws-sdk-cloudwatch'
-    client = Aws::CloudWatch::Client.new(region: ENV['AWS_REGION'])
-  else
-    client = nil # Use default client (EC2 instance role or env credentials)
-  end
-
+  # Use EC2 instance role for AWS credentials (no custom client needed)
   Sidekiq::CloudWatchMetrics.enable!(
-    client: client,
     namespace: ENV.fetch('SIDEKIQ_CLOUDWATCH_NAMESPACE', 'Chatwoot/Sidekiq'),
     interval: ENV.fetch('SIDEKIQ_CLOUDWATCH_INTERVAL', '60').to_i
   )
