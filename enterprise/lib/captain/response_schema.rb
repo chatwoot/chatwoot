54514d9d215e88a7886ec4fbe5a1f0bb31952bd1
Shivam Mishra <scm.mymail@gmail.com>
2025-08-04 16:58:00 +0530

    feat: add response schema for captain

---
+++ enterprise/lib/captain/response_schema.rb
@@ -0,0 +1,4 @@
+class Captain::ResponseSchema < RubyLLM::Schema
+  string :response, description: 'The message to send to the user'
+  string :reasoning, description: "Agent's thought process", required: false
+end
