048682130c452c0d3797f5895b59299ecdf8b711
Tanmay Sharma <tanmaydeepsharma21@gmail.com>
2025-08-11 22:26:36 +0530

    add unique check to inbox in inbox assignment policy

--- db/migrate/20250806140001_create_inbox_assignment_policies.rb
+++ db/migrate/20250806140001_create_inbox_assignment_policies.rb
@@ -3,7 +3,7 @@
 class CreateInboxAssignmentPolicies < ActiveRecord::Migration[7.1]
   def change
     create_table :inbox_assignment_policies do |t|
-      t.references :inbox, null: false, index: true
+      t.references :inbox, null: false, index: { unique: true }
       t.references :assignment_policy, null: false, index: true
 
       t.timestamps
