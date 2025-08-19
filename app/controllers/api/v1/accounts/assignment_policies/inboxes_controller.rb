afe76ed3622b6d045b3a32bf4c04e4f4a2018e44
Pranav <pranavrajs@gmail.com>
2025-08-18 18:20:01 -0700

    Remove POST/DELETE assignment_policies/inboxes

--- app/controllers/api/v1/accounts/assignment_policies/inboxes_controller.rb
+++ app/controllers/api/v1/accounts/assignment_policies/inboxes_controller.rb
@@ -1,29 +1,20 @@
 class Api::V1::Accounts::AssignmentPolicies::InboxesController < Api::V1::Accounts::BaseController
   before_action :fetch_assignment_policy
-  before_action :fetch_inbox, only: [:create, :destroy]
   before_action -> { check_authorization(AssignmentPolicy) }
 
   def index
     @inboxes = @assignment_policy.inboxes
   end
 
-  def create
-    @inbox.inbox_assignment_policy&.destroy
-    @inbox_assignment_policy = @inbox.create_inbox_assignment_policy!(assignment_policy: @assignment_policy)
-  end
-
-  def destroy
-    @inbox.inbox_assignment_policy&.destroy!
-    head :ok
-  end
-
   private
 
   def fetch_assignment_policy
-    @assignment_policy = Current.account.assignment_policies.find(params[:assignment_policy_id])
+    @assignment_policy = Current.account.assignment_policies.find(
+      params[:assignment_policy_id]
+    )
   end
 
-  def fetch_inbox
-    @inbox = Current.account.inboxes.find(params[:id] || params[:inbox_id])
+  def permitted_params
+    params.permit(:assignment_policy_id)
   end
 end
