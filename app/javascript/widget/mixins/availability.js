5a90fb6fcf62650d3ec9d8d16b1e270e92118ec8
iwanlaudin <iwan@radyalabs.com>
2025-08-27 12:07:06 +0700

    refactor: update replyTimeStatus and workingHoursEnabled for clarity and consistency

--- app/javascript/widget/mixins/availability.js
+++ app/javascript/widget/mixins/availability.js
@@ -10,16 +10,17 @@ export default {
       return window.chatwootWebChannel.replyTime;
     },
     replyTimeStatus() {
-      switch (this.replyTime) {
-        case 'in_a_few_minutes':
-          return this.$t('REPLY_TIME.IN_A_FEW_MINUTES');
-        case 'in_a_few_hours':
-          return this.$t('REPLY_TIME.IN_A_FEW_HOURS');
-        case 'in_a_day':
-          return this.$t('REPLY_TIME.IN_A_DAY');
-        default:
-          return this.$t('REPLY_TIME.IN_A_FEW_HOURS');
-      }
+      // switch (this.replyTime) {
+      //   case 'in_a_few_minutes':
+      //     return this.$t('REPLY_TIME.IN_A_FEW_MINUTES');
+      //   case 'in_a_few_hours':
+      //     return this.$t('REPLY_TIME.IN_A_FEW_HOURS');
+      //   case 'in_a_day':
+      //     return this.$t('REPLY_TIME.IN_A_DAY');
+      //   default:
+      //     return this.$t('REPLY_TIME.IN_A_FEW_HOURS');
+      // }
+      return this.$t('REPLY_TIME.IN_A_FEW_MINUTES');
     },
     replyWaitMessage() {
       const { workingHoursEnabled } = this.channelConfig;
