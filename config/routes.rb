9598021f87608e1af3fdeba06944f0f4bb9f6318
Shivam Mishra <scm.mymail@gmail.com>
2025-08-28 15:50:35 +0530

    chore: remove redundant route

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'devise_overrides/omniauth_callbacks'
   }, via: [:get, :post]
 
-  # SAML routes - bypass the redirect_callbacks to handle POST directly
-  match '/omniauth/saml/callback', to: 'devise_overrides/omniauth_callbacks#saml', via: [:get, :post], constraints: { provider: 'saml' }
-
   ## renders the frontend paths only if its not an api only server
   if ActiveModel::Type::Boolean.new.cast(ENV.fetch('CW_API_ONLY_SERVER', false))
     root to: 'api#index'
