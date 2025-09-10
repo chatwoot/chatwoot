67df815ede3ac08a91be75a01ff1ce25784bc19a
Sojan Jose <sojan@pepalo.com>
2025-09-10 20:08:55 +0530

    fix(DeleteObjectJob): batch pre-purge heavy associations (Account/Inbox) before destroy; simplify mapping and add minimal specs

--- app/jobs/delete_object_job.rb
+++ app/jobs/delete_object_job.rb
@@ -1,12 +1,38 @@
 class DeleteObjectJob < ApplicationJob
   queue_as :low
 
+  BATCH_SIZE = 5_000
+  HEAVY_ASSOCIATIONS = {
+    Account => %i[conversations contacts],
+    Inbox => %i[conversations contact_inboxes]
+  }.freeze
+
   def perform(object, user = nil, ip = nil)
+    # Pre-purge heavy associations for large objects to avoid timeouts
+    purge_heavy_associations(object)
     object.destroy!
     process_post_deletion_tasks(object, user, ip)
   end
 
   def process_post_deletion_tasks(object, user, ip); end
+
+  private
+
+  def purge_heavy_associations(object)
+    klass = HEAVY_ASSOCIATIONS.keys.find { |k| object.is_a?(k) }
+    return unless klass
+
+    HEAVY_ASSOCIATIONS[klass].each do |assoc|
+      next unless object.respond_to?(assoc)
+      batch_destroy(object.public_send(assoc))
+    end
+  end
+
+  def batch_destroy(relation)
+    relation.find_in_batches(batch_size: BATCH_SIZE) do |batch|
+      batch.each(&:destroy!)
+    end
+  end
 end
 
 DeleteObjectJob.prepend_mod_with('DeleteObjectJob')
--- spec/jobs/delete_object_job_spec.rb
+++ spec/jobs/delete_object_job_spec.rb
@@ -1,20 +1,65 @@
 require 'rails_helper'
 
-RSpec.describe DeleteObjectJob do
-  subject(:job) { described_class.perform_later(account) }
+RSpec.describe DeleteObjectJob, type: :job do
+  describe '#perform' do
+    context 'heavy object (Inbox)' do
+      let!(:account) { create(:account) }
+      let!(:inbox) { create(:inbox, account: account) }
 
-  let(:account) { create(:account) }
+      before do
+        create_list(:conversation, 3, account: account, inbox: inbox)
+      end
 
-  it 'enqueues the job' do
-    expect { job }.to have_enqueued_job(described_class)
-      .with(account)
-      .on_queue('low')
-  end
+      it 'enqueues on the low queue' do
+        expect { described_class.perform_later(inbox) }
+          .to have_enqueued_job(described_class).with(inbox).on_queue('low')
+      end
 
-  context 'when an object is passed to the job' do
-    it 'is deleted' do
-      described_class.perform_now(account)
-      expect { account.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      it 'pre-deletes heavy associations and then destroys the object' do
+        conv_ids = inbox.conversations.pluck(:id)
+        ci_ids = inbox.contact_inboxes.pluck(:id)
+        contact_ids = inbox.contacts.pluck(:id)
+
+        described_class.perform_now(inbox)
+
+        expect(Conversation.where(id: conv_ids)).to be_empty
+        expect(ContactInbox.where(id: ci_ids)).to be_empty
+        # Contacts should not be deleted for inbox destroy
+        expect(Contact.where(id: contact_ids)).not_to be_empty
+        expect { inbox.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+      context 'pre-purge timing' do
+        it 'removes associations during pre-purge, independent of final destroy' do
+          conv_ids = inbox.conversations.pluck(:id)
+          ci_ids = inbox.contact_inboxes.pluck(:id)
+          contact_ids = inbox.contacts.pluck(:id)
+
+          # Prevent the final destroy! to isolate pre-purge effects
+          allow(inbox).to receive(:destroy!).and_raise(StandardError, 'halt-after-purge')
+
+          expect { described_class.perform_now(inbox) }.to raise_error(StandardError, 'halt-after-purge')
+
+          # Inbox still exists since destroy! was halted
+          expect(inbox.reload).to be_present
+          # But heavy associations were purged by the job before destroy!
+          expect(Conversation.where(id: conv_ids)).to be_empty
+          expect(ContactInbox.where(id: ci_ids)).to be_empty
+          # Contacts should not be deleted for inbox destroy path
+          expect(Contact.where(id: contact_ids)).not_to be_empty
+        end
+      end
+    end
+
+    context 'regular object (Label)' do
+      it 'just destroys the object without batched purges' do
+        label = create(:label)
+        expect_any_instance_of(described_class).not_to receive(:batch_destroy)
+
+        described_class.perform_now(label)
+
+        expect { label.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
