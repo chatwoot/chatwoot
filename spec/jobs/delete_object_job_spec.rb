61773c500f0b7229efa0b41a0926739d5a72bf66
Sojan Jose <sojan@pepalo.com>
2025-09-10 21:52:46 +0530

    spec: simplify DeleteObjectJob happy-path; keep heavy cases readable

--- spec/jobs/delete_object_job_spec.rb
+++ spec/jobs/delete_object_job_spec.rb
@@ -31,41 +31,6 @@ RSpec.describe DeleteObjectJob, type: :job do
         expect(Contact.where(id: contact_ids)).not_to be_empty
         expect { inbox.reload }.to raise_error(ActiveRecord::RecordNotFound)
       end
-
-      context 'when pre-purge happens before final destroy' do
-        it 'removes associations during pre-purge, independent of final destroy' do
-          conv_ids = inbox.conversations.pluck(:id)
-          ci_ids = inbox.contact_inboxes.pluck(:id)
-          contact_ids = inbox.contacts.pluck(:id)
-          re_ids = inbox.reporting_events.pluck(:id)
-
-          # Prevent the final destroy! to isolate pre-purge effects
-          allow(inbox).to receive(:destroy!).and_raise(StandardError, 'halt-after-purge')
-
-          expect { described_class.perform_now(inbox) }.to raise_error(StandardError, 'halt-after-purge')
-
-          # Inbox still exists since destroy! was halted
-          expect(inbox.reload).to be_present
-          # But heavy associations were purged by the job before destroy!
-          expect(Conversation.where(id: conv_ids)).to be_empty
-          expect(ContactInbox.where(id: ci_ids)).to be_empty
-          expect(ReportingEvent.where(id: re_ids)).to be_empty
-          # Contacts should not be deleted for inbox destroy path
-          expect(Contact.where(id: contact_ids)).not_to be_empty
-        end
-      end
-    end
-
-    context 'when object is regular (Label)' do
-      it 'just destroys the object without batched purges' do
-        label = create(:label)
-        job = described_class.new
-        expect(job).not_to receive(:batch_destroy)
-
-        job.perform(label)
-
-        expect { label.reload }.to raise_error(ActiveRecord::RecordNotFound)
-      end
     end
 
     context 'when object is heavy (Account)' do
@@ -93,6 +58,16 @@ RSpec.describe DeleteObjectJob, type: :job do
         expect(Inbox.where(id: inbox_ids)).to be_empty
         expect(ReportingEvent.where(id: re_ids)).to be_empty
         expect { account.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'when object is regular (Label)' do
+      it 'just destroys the object' do
+        label = create(:label)
+
+        described_class.perform_now(label)
+
+        expect { label.reload }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
