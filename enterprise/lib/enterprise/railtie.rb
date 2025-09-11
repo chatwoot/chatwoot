a60680e524f124c4751f0b91175288497bf1cd4d
Akshay Borade <akshay.cs.borade@gmail.com>
2025-09-11 15:33:31 +0530

    Fix:12414 Rubocop lint fix

--- enterprise/lib/enterprise/railtie.rb
+++ enterprise/lib/enterprise/railtie.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-class Enterprise::Railtie < Rails::Railtie
-  rake_tasks do
-    Dir[File.expand_path('../tasks/**/*.rake', __dir__)].each { |f| load f }
+module Enterprise
+  class Railtie < Rails::Railtie
+    rake_tasks do
+      Dir[File.expand_path('../tasks/**/*.rake', __dir__)].each { |f| load f }
+    end
   end
 end
