1ff276ed10010e9cf1ab98b5225bfa5906889bb5
Shivam Mishra <scm.mymail@gmail.com>
2025-09-12 12:11:53 +0530

    chore: remove rebase changes

--- enterprise/app/controllers/enterprise/devise_overrides/sessions_controller.rb
+++ enterprise/app/controllers/enterprise/devise_overrides/sessions_controller.rb
@@ -36,25 +36,4 @@ module Enterprise::DeviseOverrides::SessionsController
       )
     end
   end
-
-  private
-
-  def check_saml_user
-    # Skip if using SSO token (SAML users can use SSO tokens)
-    return if params[:sso_auth_token].present?
-    return if params[:email].blank?
-
-    user = User.from_email(params[:email])
-    return unless user&.provider == 'saml'
-
-    check_saml_enabled_accounts(user)
-  end
-
-  def check_saml_enabled_accounts(user)
-    # Allow regular login if SAML is not configured/enabled for the account
-    user.accounts.each do |account|
-      saml_settings = account.account_saml_settings
-      raise CustomExceptions::Base.new(I18n.t('messages.login_saml_user'), :unauthorized) if saml_settings&.saml_enabled?
-    end
-  end
 end
