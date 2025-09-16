a7f4e7a243866d5d1db640813bb78a1532988888
Pranav <pranavrajs@gmail.com>
2025-09-15 19:44:15 -0700

    Revert changes on widget helper

--- app/helpers/widget_helper.rb
+++ app/helpers/widget_helper.rb
@@ -1,12 +1,7 @@
 module WidgetHelper
   def build_contact_inbox_with_token(web_widget, additional_attributes = {})
     contact_inbox = web_widget.create_contact_inbox(additional_attributes)
-    payload = {
-      source_id: contact_inbox.source_id,
-      inbox_id: web_widget.inbox.id,
-      iat: Time.current.to_i,
-      exp: 1.day.from_now.to_i
-    }
+    payload = { source_id: contact_inbox.source_id, inbox_id: web_widget.inbox.id }
 
     token = ::Widget::TokenService.new(payload: payload).generate_token
 
