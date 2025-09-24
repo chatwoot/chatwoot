139e54681f882ff1fb0a007d525a29798c9fe883
Pranav <pranavrajs@gmail.com>
2025-09-24 07:57:02 -0700

    Add verified_for_sending to EDITABLE_ATTRS

--- app/models/channel/email.rb
+++ app/models/channel/email.rb
@@ -43,7 +43,7 @@ class Channel::Email < ApplicationRecord
   self.table_name = 'channel_email'
   EDITABLE_ATTRS = [:email, :imap_enabled, :imap_login, :imap_password, :imap_address, :imap_port, :imap_enable_ssl,
                     :smtp_enabled, :smtp_login, :smtp_password, :smtp_address, :smtp_port, :smtp_domain, :smtp_enable_starttls_auto,
-                    :smtp_enable_ssl_tls, :smtp_openssl_verify_mode, :smtp_authentication, :provider].freeze
+                    :smtp_enable_ssl_tls, :smtp_openssl_verify_mode, :smtp_authentication, :provider, :verified_for_sending].freeze
 
   validates :email, uniqueness: true
   validates :forward_to_email, uniqueness: true
