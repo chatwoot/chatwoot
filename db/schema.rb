eca92e3aff2275839fc37d601f851049395d2b4b
Pranav <pranavrajs@gmail.com>
2025-09-24 09:37:05 -0700

    remove unnecessary spacing

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2025_09_17_012759) do
-
   # These extensions should be enabled to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
