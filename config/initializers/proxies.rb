d7fdb0a6bac6cea3a674103a724745249e2dfb07
Matteo C. <hello@micorksen.eu>
2025-09-27 15:46:37 +0200

    fix: Align functions

--- config/initializers/proxies.rb
+++ config/initializers/proxies.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_dispatch.trusted_proxies = ENV.fetch('TRUSTED_PROXIES', '')
-    .split(',')
-    .map(&:strip)
-    .reject(&:empty?)
-    .map { |proxy| IPAddr.new(proxy) }
+                                              .split(',')
+                                              .map(&:strip)
+                                              .reject(&:empty?)
+                                              .map { |proxy| IPAddr.new(proxy) }
 end
