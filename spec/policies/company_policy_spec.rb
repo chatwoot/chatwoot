6efae4e00b3f1a41b3ecbd4ef80d673692e032c4
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2025-09-30 22:57:13 +0530

    fix: change parameter to symbol in spec test

--- spec/policies/company_policy_spec.rb
+++ spec/policies/company_policy_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe CompanyPolicy, type: :policy do
   subject(:company_policy) { described_class }
 
   let(:account) { create(:account) }
-  let(:administrator) { create(:user, administrator, account: account) }
+  let(:administrator) { create(:user, :administrator, account: account) }
   let(:agent) { create(:user, account: account) }
   let(:company) { create(:company, account: account) }
 
