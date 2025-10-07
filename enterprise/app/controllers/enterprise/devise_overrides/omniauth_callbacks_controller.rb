38a6859fe63dbf22f190f0dab6160f79a4fb9dbf
Shivam Mishra <scm.mymail@gmail.com>
2025-10-07 16:45:05 +0530

    refactor: defensively encode error

--- enterprise/app/controllers/enterprise/devise_overrides/omniauth_callbacks_controller.rb
+++ enterprise/app/controllers/enterprise/devise_overrides/omniauth_callbacks_controller.rb
@@ -84,7 +84,7 @@ module Enterprise::DeviseOverrides::OmniauthCallbacksController
 
   def redirect_to_mobile_error(error)
     mobile_deep_link_base = GlobalConfigService.load('MOBILE_DEEP_LINK_BASE', 'chatwootapp://')
-    redirect_to "#{mobile_deep_link_base}://auth/saml?error=#{error}", allow_other_host: true
+    redirect_to "#{mobile_deep_link_base}://auth/saml?error=#{ERB::Util.url_encode(error)}", allow_other_host: true
   end
 
   def saml_enabled_for_account?(account_id)
