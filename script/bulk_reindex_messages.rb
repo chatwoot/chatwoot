06b05e554cf459a9f42ae8ab4342cba331355ce9
Vishnu Narayanan <vishnu@chatwoot.com>
2025-10-09 15:56:36 +0530

    feat: add a script to throttle bulkreindex job creation

---
+++ script/bulk_reindex_messages.rb
@@ -0,0 +1,52 @@
+# Bulk reindex all messages with throttling to prevent DB overload
+# This creates jobs slowly to avoid overwhelming the database connection pool
+
+JOBS_PER_MINUTE = 50  # Adjust based on your DB capacity
+BATCH_SIZE = 1000     # Messages per job
+
+batch_count = 0
+total_batches = (Message.count / BATCH_SIZE.to_f).ceil
+start_time = Time.now
+
+puts '=' * 80
+puts "Bulk Reindex Started at #{start_time}"
+puts '=' * 80
+puts "Total messages: #{Message.count}"
+puts "Batch size: #{BATCH_SIZE}"
+puts "Total batches: #{total_batches}"
+puts "Rate: #{JOBS_PER_MINUTE} jobs/minute (#{JOBS_PER_MINUTE * BATCH_SIZE} messages/minute)"
+puts "Estimated time: #{(total_batches / JOBS_PER_MINUTE.to_f / 60).round(2)} hours"
+puts '=' * 80
+puts ''
+
+Message.find_in_batches(batch_size: BATCH_SIZE) do |batch|
+  batch_count += 1
+
+  # Enqueue to low priority queue
+  Searchkick::BulkReindexJob.set(queue: :bulk_reindex_low).perform_later(
+    class_name: 'Message',
+    record_ids: batch.map(&:id).map(&:to_s),
+    index_name: Message.searchkick_index.name,
+    method_name: nil
+  )
+
+  # Throttle: wait after every N jobs
+  if batch_count % JOBS_PER_MINUTE == 0
+    elapsed = Time.now - start_time
+    progress = (batch_count.to_f / total_batches * 100).round(2)
+    queue_size = Sidekiq::Queue.new('bulk_reindex_low').size
+
+    puts "[#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}] Progress: #{batch_count}/#{total_batches} (#{progress}%)"
+    puts "  Queue size: #{queue_size}"
+    puts "  Elapsed: #{(elapsed / 3600).round(2)} hours"
+    puts "  ETA: #{((elapsed / batch_count * (total_batches - batch_count)) / 3600).round(2)} hours remaining"
+    puts ''
+
+    sleep(60)
+  end
+end
+
+puts '=' * 80
+puts "Done! Created #{batch_count} jobs"
+puts "Total time: #{((Time.now - start_time) / 3600).round(2)} hours"
+puts '=' * 80
