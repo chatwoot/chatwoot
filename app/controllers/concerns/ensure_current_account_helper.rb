0110cab3fe051ced8d0a0d789397a3cbeb260f66
Sojan Jose <sojan@pepalo.com>
2025-10-17 15:38:53 -0700

    fix: ensure Current context for API token requests

--- app/controllers/concerns/ensure_current_account_helper.rb
+++ app/controllers/concerns/ensure_current_account_helper.rb
@@ -10,24 +10,21 @@ module EnsureCurrentAccountHelper
     account = Account.find(params[:account_id])
     render_unauthorized('Account is suspended') and return unless account.active?
 
-    acting_user = current_user || Current.user
-
-    case acting_user
-    when User
-      account_accessible_for_user?(account, acting_user)
-    when AgentBot
-      account_accessible_for_bot?(account, acting_user)
+    if current_user
+      account_accessible_for_user?(account)
+    elsif @resource.is_a?(AgentBot)
+      account_accessible_for_bot?(account)
     end
     account
   end
 
-  def account_accessible_for_user?(account, user)
-    @current_account_user = account.account_users.find_by(user_id: user.id)
+  def account_accessible_for_user?(account)
+    @current_account_user = account.account_users.find_by(user_id: current_user.id)
     Current.account_user = @current_account_user
     render_unauthorized('You are not authorized to access this account') unless @current_account_user
   end
 
-  def account_accessible_for_bot?(account, bot)
-    render_unauthorized('Bot is not authorized to access this account') unless bot.agent_bot_inboxes.find_by(account_id: account.id)
+  def account_accessible_for_bot?(account)
+    render_unauthorized('Bot is not authorized to access this account') unless @resource.agent_bot_inboxes.find_by(account_id: account.id)
   end
 end
