bcd681cbc9c54c66d157797f5d74413f89606418
Muhsin <muhsinkeramam@gmail.com>
2025-10-27 11:42:22 +0530

    chore: move constant file

--- app/javascript/dashboard/api/changelog.js
+++ app/javascript/dashboard/api/changelog.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import ApiClient from './ApiClient';
+import { CHANGELOG_API_URL } from 'shared/constants/links';
 
 class ChangelogApi extends ApiClient {
   constructor() {
@@ -8,7 +9,7 @@ class ChangelogApi extends ApiClient {
 
   // eslint-disable-next-line class-methods-use-this
   fetchFromHub() {
-    return axios.get('https://hub.2.chatwoot.com/changelogs');
+    return axios.get(CHANGELOG_API_URL);
   }
 }
 
