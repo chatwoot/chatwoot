ec7ccc26cc5caac9951d25be62c137b0800ad742
Jordan <jordanviking@gmail.com>
2025-10-27 22:53:02 -0300

    Create dockerfile script

---
+++ create_docker.sh
@@ -0,0 +1,9 @@
+#/bin/bash!
+
+VERSION=$(cat package.json | jq -r .version)
+
+echo "FROM chatwoot/chatwoot:v${VERSION}-ce" > Dockerfile
+
+for i in `git diff master HEAD --name-only`; do 
+    echo "COPY $i /app/$i" >> Dockerfile;
+done
