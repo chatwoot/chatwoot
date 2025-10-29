e3a76f7fb4b96f727cccdf42689f9e483b47c094
Matteo C. <hello@micorksen.eu>
2025-10-29 12:37:49 +0100

    chore: Align filter function

--- config/initializers/proxies.rb
+++ config/initializers/proxies.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.action_dispatch.trusted_proxies = ENV.fetch('TRUSTED_PROXIES', '')
                                               .split(',')
                                               .map(&:strip)
-                                              .reject(&:empty?)
+                                              .reject(&:empty?)                                             
 .filter_map do |proxy|
     IPAddr.new(proxy)
   rescue IPAddr::InvalidAddressError => e
