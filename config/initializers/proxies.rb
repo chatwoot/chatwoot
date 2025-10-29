f6af7d32348468505ddfa1ea2186034fff891a2e
Matteo C. <hello@micorksen.eu>
2025-10-29 12:41:35 +0100

    chore: Align filter_map (thanks @stonecharioteer)

--- config/initializers/proxies.rb
+++ config/initializers/proxies.rb
@@ -2,11 +2,11 @@ Rails.application.configure do
   config.action_dispatch.trusted_proxies = ENV.fetch('TRUSTED_PROXIES', '')
                                               .split(',')
                                               .map(&:strip)
-                                              .reject(&:empty?)                                             
-.filter_map do |proxy|
-    IPAddr.new(proxy)
-  rescue IPAddr::InvalidAddressError => e
-    Rails.logger.warn "Invalid IP: #{proxy}"
-    nil # return nil for invalid entries
-  end
+                                              .reject(&:empty?)
+                                              .filter_map do |proxy|
+                                                IPAddr.new(proxy)
+                                              rescue IPAddr::InvalidAddressError => e
+                                                Rails.logger.warn "Invalid IP: #{proxy}"
+                                                nil # return nil for invalid entries
+                                              end
 end
