34870d5938797c6980c17dc870969f49d0afda68
Sojan Jose <sojan@pepalo.com>
2025-11-03 15:38:42 -0800

    test: add Contacts::BulkActionService spec for labels delegation to BulkAssignLabelsService

--- spec/services/contacts/bulk_action_service_spec.rb
+++ spec/services/contacts/bulk_action_service_spec.rb
@@ -20,5 +20,19 @@ RSpec.describe Contacts::BulkActionService do
         service.perform
       end
     end
+
+    context 'when labels are provided' do
+      let(:params) { { ids: [10, 20], labels: { add: %w[vip support] }, extra: 'ignored' } }
+
+      it 'delegates to the bulk assign labels service with permitted params' do
+        bulk_assign_service = instance_double(Contacts::BulkAssignLabelsService, perform: true)
+
+        expect(Contacts::BulkAssignLabelsService).to receive(:new)
+          .with(account: account, contact_ids: [10, 20], labels: %w[vip support])
+          .and_return(bulk_assign_service)
+
+        service.perform
+      end
+    end
   end
 end
