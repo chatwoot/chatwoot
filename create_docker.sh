91deaf52357e9693f54712a4726977025ba2dcdf
Jordan <jordanviking@gmail.com>
2025-11-04 23:11:53 -0300

    Fix create Dockerfile script

--- create_docker.sh
+++ create_docker.sh
@@ -5,5 +5,5 @@ VERSION=$(cat package.json | jq -r .version)
 echo "FROM chatwoot/chatwoot:v${VERSION}-ce" > Dockerfile
 
 for i in `git diff master HEAD --name-only`; do 
-    echo "COPY $i /app/$i" >> Dockerfile;
+    echo "COPY /$i /app/$i" >> Dockerfile;
 done
