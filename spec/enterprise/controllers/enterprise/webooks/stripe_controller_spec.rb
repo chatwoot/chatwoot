cec1a64d6f2c20a37d953fa94a62148f8bdb7ede
Tanmay Deep Sharma <tanmaydeepsharma21@gmail.com>
2025-11-07 00:02:11 +0530

    fix customer creation flow

--- spec/enterprise/controllers/enterprise/webooks/stripe_controller_spec.rb
+++ spec/enterprise/controllers/enterprise/webooks/stripe_controller_spec.rb
@@ -1,31 +1,32 @@
 require 'rails_helper'
-# rubocop:disable RSpec/VerifiedDoubles
 
 RSpec.describe 'Enterprise::Webhooks::StripeController', type: :request do
   describe 'POST /enterprise/webhooks/stripe' do
     let(:params) { { content: 'hello' } }
+    let(:v1_event_params) { { type: 'invoice.created', data: { object: {} } }.to_json }
 
     it 'delegates to the v1 handler for legacy events' do
-      handle_stripe = double
-      event = double('Stripe::Event', type: 'invoice.created')
+      handle_stripe = instance_double(Enterprise::Billing::HandleStripeEventService)
+      event = instance_double(Stripe::Event, type: 'invoice.created')
 
       allow(Stripe::Webhook).to receive(:construct_event).and_return(event)
       allow(Enterprise::Billing::HandleStripeEventService).to receive(:new).and_return(handle_stripe)
       allow(handle_stripe).to receive(:perform)
 
-      post '/enterprise/webhooks/stripe', headers: { 'Stripe-Signature': 'test' }, params: params
+      post '/enterprise/webhooks/stripe', headers: { 'Stripe-Signature': 'test' }, params: v1_event_params, as: :json
 
       expect(handle_stripe).to have_received(:perform).with(event: event)
     end
 
     it 'delegates v2 billing events to the v2 webhook handler' do
-      event = double('Stripe::Event', type: 'v2.billing.pricing_plan_subscription.servicing_activated')
+      v2_event_params = { type: 'v2.billing.pricing_plan_subscription.servicing_activated', data: { object: {} } }.to_json
+      event = instance_double(Stripe::Event, type: 'v2.billing.pricing_plan_subscription.servicing_activated')
       handler_double = instance_double(Enterprise::Billing::V2::WebhookHandlerService, perform: { success: true })
 
       allow(Stripe::Webhook).to receive(:construct_event).and_return(event)
       allow(Enterprise::Billing::V2::WebhookHandlerService).to receive(:new).and_return(handler_double)
 
-      post '/enterprise/webhooks/stripe', headers: { 'Stripe-Signature': 'test' }, params: params
+      post '/enterprise/webhooks/stripe', headers: { 'Stripe-Signature': 'test' }, params: v2_event_params, as: :json
 
       expect(Enterprise::Billing::V2::WebhookHandlerService).to have_received(:new)
       expect(handler_double).to have_received(:perform).with(event: event)
@@ -43,4 +44,3 @@ RSpec.describe 'Enterprise::Webhooks::StripeController', type: :request do
     end
   end
 end
-# rubocop:enable RSpec/VerifiedDoubles
