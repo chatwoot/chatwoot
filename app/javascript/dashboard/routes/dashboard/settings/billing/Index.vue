1891bf99612fb09ceaa92a066e9985199a948313
zafrandivac <zafrandivac38@gmail.com>
2025-11-06 13:59:13 +0700

    fix: custom plan display

--- app/javascript/dashboard/routes/dashboard/settings/billing/Index.vue
+++ app/javascript/dashboard/routes/dashboard/settings/billing/Index.vue
@@ -702,7 +702,7 @@ function scrollToPackage() {
           >
             <p>{{ $t('BILLING.NO_CUSTOM_PLANS') }}</p>
           </div>
-          <div v-else class="pricing-plans mt-4">
+          <div v-else class="custom-plans mt-4">
             <div v-for="plan in custom_plans" :key="plan.id" class="pricing-card bg-[#fff] dark:bg-[#23252d]">
               <div class="plan-header flex flex-row">
                 <div class="flex-1">
@@ -1187,8 +1187,9 @@ export default {
 
 .custom-plans {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 280px));
+  grid-template-columns: repeat(auto-fit, minmax(250px, 350px));
   gap: 1.5rem;
+  justify-content: center;
 }
 
 .pricing-card {
