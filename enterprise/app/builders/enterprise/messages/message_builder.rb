0314ae00d24515888621f928556608d6f4568eb1
Sojan Jose <sojan@pepalo.com>
2025-11-05 19:35:14 -0800

    chore: lint enterprise voice helpers

--- enterprise/app/builders/enterprise/messages/message_builder.rb
+++ enterprise/app/builders/enterprise/messages/message_builder.rb
@@ -3,10 +3,11 @@ module Enterprise::Messages::MessageBuilder
 
   def message_type
     # Allow incoming messages in API and Voice inboxes
-    if !['Channel::Api', 'Channel::Voice'].include?(@conversation.inbox.channel_type) && @message_type == 'incoming'
+    if @message_type == 'incoming' &&
+       ['Channel::Api', 'Channel::Voice'].exclude?(@conversation.inbox.channel_type)
       raise StandardError, 'Incoming messages are only allowed in Api and Voice inboxes'
     end
+
     @message_type
   end
 end
-
--- enterprise/app/services/enterprise/contacts/contactable_inboxes_service.rb
+++ enterprise/app/services/enterprise/contacts/contactable_inboxes_service.rb
@@ -4,11 +4,13 @@ module Enterprise::Contacts::ContactableInboxesService
   # Extend base selection to include Voice inboxes
   def get_contactable_inbox(inbox)
     return voice_contactable_inbox(inbox) if inbox.channel_type == 'Channel::Voice'
+
     super
   end
 
   def voice_contactable_inbox(inbox)
     return if @contact.phone_number.blank?
+
     { source_id: @contact.phone_number, inbox: inbox }
   end
 end
