9398e1263499c7ab9836630307bb561145c29b55
zafrandivac <75294191+zafrandivac@users.noreply.github.com>
2025-11-06 16:39:55 +0700

    remove unused code

--- app/javascript/dashboard/routes/dashboard/aiagents/AiAgentGeneralSettingsView.vue
+++ app/javascript/dashboard/routes/dashboard/aiagents/AiAgentGeneralSettingsView.vue
@@ -8,7 +8,6 @@ import {
   watch,
   watchEffect,
 } from 'vue';
-import { useStore } from 'vuex';
 import Input from 'dashboard/components-next/input/Input.vue';
 import TextArea from 'dashboard/components-next/textarea/TextArea.vue';
 import { required } from '@vuelidate/validators';
@@ -424,4 +423,5 @@ function resetChat() {
     </div>
     <!-- Chat Preview Section -->
   </div>
+
 </template>
