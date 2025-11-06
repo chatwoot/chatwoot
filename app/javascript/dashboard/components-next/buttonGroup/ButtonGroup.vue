854e5dc27d9bc836d0fae8f2989317f87a4f168e
iamsivin <iamsivin@gmail.com>
2025-11-06 15:12:04 +0530

    chore: Clean up

--- app/javascript/dashboard/components-next/buttonGroup/ButtonGroup.vue
+++ app/javascript/dashboard/components-next/buttonGroup/ButtonGroup.vue
@@ -1,6 +1,19 @@
+<script setup>
+defineProps({
+  noAnimation: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
 <template>
   <div
-    class="has-[button:not(:disabled):active]:scale-[0.98] transition-transform duration-150 ease-out"
+    :class="
+      noAnimation
+        ? ''
+        : 'has-[button:not(:disabled):active]:scale-[0.98] transition-transform duration-150 ease-out'
+    "
   >
     <slot />
   </div>
