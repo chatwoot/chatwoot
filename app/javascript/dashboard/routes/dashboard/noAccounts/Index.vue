d4d263b6bf5cfaed4ceb5c6be655c59b88ecb02f
Pranav <pranav@chatwoot.com>
2025-11-06 17:28:26 -0800

    fix: Handle login when there are no accounts

---
+++ app/javascript/dashboard/routes/dashboard/noAccounts/Index.vue
@@ -0,0 +1,26 @@
+<script setup>
+import EmptyState from 'dashboard/components/widgets/EmptyState.vue';
+import NextButton from 'dashboard/components-next/button/Button.vue';
+import Auth from 'dashboard/api/auth';
+
+const handleLogout = () => {
+  Auth.logout();
+};
+</script>
+
+<template>
+  <div
+    class="flex flex-col flex-1 items-center justify-center w-full h-full gap-6 bg-n-slate-2"
+  >
+    <EmptyState
+      :title="$t('APP_GLOBAL.NO_ACCOUNTS.TITLE')"
+      :message="$t('APP_GLOBAL.NO_ACCOUNTS.MESSAGE')"
+    />
+    <NextButton
+      variant="smooth"
+      color-scheme="secondary"
+      :label="$t('APP_GLOBAL.NO_ACCOUNTS.LOGOUT')"
+      @click="handleLogout"
+    />
+  </div>
+</template>
