b21ee28758eb3e107ab32619bc35edf0beb4188b
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2025-11-14 19:52:01 +0530

    fix: remove rogue commit unrelated to this change

--- db/schema.rb
+++ db/schema.rb
@@ -577,7 +577,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_14_140554) do
     t.bigint "account_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "contacts_count", default: 0, null: false
     t.index ["account_id", "domain"], name: "index_companies_on_account_and_domain", unique: true, where: "(domain IS NOT NULL)"
     t.index ["account_id"], name: "index_companies_on_account_id"
     t.index ["name", "account_id"], name: "index_companies_on_name_and_account_id"
