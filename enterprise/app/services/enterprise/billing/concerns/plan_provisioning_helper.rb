69b818896a30863f9af6a50646591a74ac1f4dc8
Tanmay Deep Sharma <tanmaydeepsharma21@gmail.com>
2025-11-19 20:01:49 +0530

    update plan catalog display_name

--- enterprise/app/services/enterprise/billing/concerns/plan_provisioning_helper.rb
+++ enterprise/app/services/enterprise/billing/concerns/plan_provisioning_helper.rb
@@ -7,26 +7,7 @@ module Enterprise::Billing::Concerns::PlanProvisioningHelper
     plan_definition = Enterprise::Billing::V2::PlanCatalog.definition_for(new_pricing_plan_id)
     return unless plan_definition
 
-    plan_name = extract_plan_name(plan_definition)
+    plan_name = plan_definition[:display_name]
     enable_plan_specific_features(plan_name) if plan_name.present?
-  end
-
-  def extract_plan_name(plan_definition)
-    plan_definition[:display_name].split.find { |word| %w[Startups Business Enterprise].include?(word) }
-  end
-
-  def update_account_plan(new_pricing_plan_id, quantity, next_billing_date)
-    attributes = {
-      'stripe_pricing_plan_id' => new_pricing_plan_id,
-      'pending_stripe_pricing_plan_id' => nil,
-      'pending_subscription_quantity' => nil,
-      'subscribed_quantity' => quantity,
-      'next_billing_date' => next_billing_date
-    }
-
-    plan_definition = Enterprise::Billing::V2::PlanCatalog.definition_for(new_pricing_plan_id)
-    attributes['plan_name'] = plan_definition[:display_name] if plan_definition
-
-    update_custom_attributes(attributes)
   end
 end
