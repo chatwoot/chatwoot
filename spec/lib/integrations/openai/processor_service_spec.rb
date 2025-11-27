13f287c81512cfda2a67dac504c4972cfe8e9821
aakashb95 <aakash@chatwoot.com>
2025-11-27 12:45:24 +0530

    fix tests

--- spec/lib/integrations/openai/processor_service_spec.rb
+++ spec/lib/integrations/openai/processor_service_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Integrations::Openai::ProcessorService do
 
   # Mock RubyLLM objects
   let(:mock_chat) { instance_double(RubyLLM::Chat) }
+  let(:mock_context) { instance_double(RubyLLM::Context) }
   let(:mock_response) do
     instance_double(
       RubyLLM::Message,
@@ -26,7 +27,9 @@ RSpec.describe Integrations::Openai::ProcessorService do
   end
 
   before do
-    allow(RubyLLM).to receive(:chat).and_return(mock_chat)
+    allow(RubyLLM).to receive(:context).and_yield(double('config').as_null_object).and_return(mock_context)
+    allow(mock_context).to receive(:chat).and_return(mock_chat)
+
     allow(mock_chat).to receive(:with_instructions).and_return(mock_chat)
     allow(mock_chat).to receive(:add_message).and_return(mock_chat)
     allow(mock_chat).to receive(:ask).and_return(mock_response)
