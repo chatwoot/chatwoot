a5a460c855cfa2e2a0de94acb954a8260016575a
Inga Rondel <90716651+rondelinga@users.noreply.github.com>
2025-11-28 16:33:29 +0400

    fix: add priority flag to inbox

--- config/routes.rb
+++ config/routes.rb
@@ -324,7 +324,7 @@ Rails.application.routes.draw do
             end
           end
           resources :working_hours, only: [:update]
-
+          resources :priority_groups, only: [:index]
           resources :portals do
             member do
               patch :archive
