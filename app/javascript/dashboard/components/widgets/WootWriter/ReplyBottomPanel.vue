53ac5306e7122053c16e854323cc9411d8a1c2e8
iamsivin <iamsivin@gmail.com>
2025-12-01 14:53:07 +0530

    chore: Convert reply bottom panel to composition API

--- app/javascript/dashboard/components/widgets/WootWriter/ReplyBottomPanel.vue
+++ app/javascript/dashboard/components/widgets/WootWriter/ReplyBottomPanel.vue
@@ -1,279 +1,280 @@
-<script>
-import { ref } from 'vue';
+<script setup>
+import { ref, computed, onMounted } from 'vue';
 import { useUISettings } from 'dashboard/composables/useUISettings';
 import { useKeyboardEvents } from 'dashboard/composables/useKeyboardEvents';
+import { useMapGetter } from 'dashboard/composables/store';
+import { useInbox } from 'dashboard/composables/useInbox';
+import { useAccount } from 'dashboard/composables/useAccount';
+import { useI18n } from 'vue-i18n';
 import FileUpload from 'vue-upload-component';
 import * as ActiveStorage from 'activestorage';
-import inboxMixin from 'shared/mixins/inboxMixin';
 import { FEATURE_FLAGS } from 'dashboard/featureFlags';
 import { getAllowedFileTypesByChannel } from '@chatwoot/utils';
 import { ALLOWED_FILE_TYPES } from 'shared/constants/messages';
 import VideoCallButton from '../VideoCallButton.vue';
 import AIAssistanceButton from '../AIAssistanceButton.vue';
 import { INBOX_TYPES } from 'dashboard/helper/inbox';
-import { mapGetters } from 'vuex';
 import NextButton from 'dashboard/components-next/button/Button.vue';
 
-export default {
-  name: 'ReplyBottomPanel',
-  components: { NextButton, FileUpload, VideoCallButton, AIAssistanceButton },
-  mixins: [inboxMixin],
-  props: {
-    isNote: {
-      type: Boolean,
-      default: false,
-    },
-    onSend: {
-      type: Function,
-      default: () => {},
-    },
-    sendButtonText: {
-      type: String,
-      default: '',
-    },
-    recordingAudioDurationText: {
-      type: String,
-      default: '00:00',
-    },
-    // inbox prop is used in /mixins/inboxMixin,
-    // remove this props when refactoring to composable if not needed
-    // eslint-disable-next-line vue/no-unused-properties
-    inbox: {
-      type: Object,
-      default: () => ({}),
-    },
-    showFileUpload: {
-      type: Boolean,
-      default: false,
-    },
-    showAudioRecorder: {
-      type: Boolean,
-      default: false,
-    },
-    onFileUpload: {
-      type: Function,
-      default: () => {},
-    },
-    toggleEmojiPicker: {
-      type: Function,
-      default: () => {},
-    },
-    toggleAudioRecorder: {
-      type: Function,
-      default: () => {},
-    },
-    toggleAudioRecorderPlayPause: {
-      type: Function,
-      default: () => {},
-    },
-    isRecordingAudio: {
-      type: Boolean,
-      default: false,
-    },
-    recordingAudioState: {
-      type: String,
-      default: '',
-    },
-    isSendDisabled: {
-      type: Boolean,
-      default: false,
-    },
-    showEditorToggle: {
-      type: Boolean,
-      default: false,
-    },
-    isOnPrivateNote: {
-      type: Boolean,
-      default: false,
-    },
-    enableMultipleFileUpload: {
-      type: Boolean,
-      default: true,
-    },
-    enableWhatsAppTemplates: {
-      type: Boolean,
-      default: false,
-    },
-    enableContentTemplates: {
-      type: Boolean,
-      default: false,
-    },
-    conversationId: {
-      type: Number,
-      required: true,
-    },
-    message: {
-      type: String,
-      default: '',
-    },
-    newConversationModalActive: {
-      type: Boolean,
-      default: false,
-    },
-    portalSlug: {
-      type: String,
-      required: true,
-    },
-    conversationType: {
-      type: String,
-      default: '',
-    },
-    showQuotedReplyToggle: {
-      type: Boolean,
-      default: false,
-    },
-    quotedReplyEnabled: {
-      type: Boolean,
-      default: false,
-    },
+const props = defineProps({
+  isNote: {
+    type: Boolean,
+    default: false,
   },
-  emits: [
-    'replaceText',
-    'toggleInsertArticle',
-    'toggleEditor',
-    'selectWhatsappTemplate',
-    'selectContentTemplate',
-    'toggleQuotedReply',
-  ],
-  setup() {
-    const { setSignatureFlagForInbox, fetchSignatureFlagFromUISettings } =
-      useUISettings();
-
-    const uploadRef = ref(false);
-
-    const keyboardEvents = {
-      '$mod+Alt+KeyA': {
-        action: () => {
-          // TODO: This is really hacky, we need to replace the file picker component with
-          // a custom one, where the logic and the component markup is isolated.
-          // Once we have the custom component, we can remove the hacky logic below.
-
-          const uploadTriggerButton = document.querySelector(
-            '#conversationAttachment'
-          );
-          uploadTriggerButton.click();
-        },
-        allowOnFocusedInput: true,
-      },
-    };
-
-    useKeyboardEvents(keyboardEvents);
-
-    return {
-      setSignatureFlagForInbox,
-      fetchSignatureFlagFromUISettings,
-      uploadRef,
-    };
+  onSend: {
+    type: Function,
+    default: () => {},
   },
-  data() {
-    return {
-      ALLOWED_FILE_TYPES,
-    };
+  sendButtonText: {
+    type: String,
+    default: '',
   },
-  computed: {
-    ...mapGetters({
-      accountId: 'getCurrentAccountId',
-      isFeatureEnabledonAccount: 'accounts/isFeatureEnabledonAccount',
-      uiFlags: 'integrations/getUIFlags',
-    }),
-    showAttachButton() {
-      return this.showFileUpload || this.isNote;
-    },
-    showAudioRecorderButton() {
-      if (this.isALineChannel) {
-        return false;
-      }
-      // Disable audio recorder for safari browser as recording is not supported
-      // const isSafari = /^((?!chrome|android|crios|fxios).)*safari/i.test(
-      //   navigator.userAgent
-      // );
+  recordingAudioDurationText: {
+    type: String,
+    default: '00:00',
+  },
+  inbox: {
+    type: Object,
+    default: () => ({}),
+  },
+  showFileUpload: {
+    type: Boolean,
+    default: false,
+  },
+  showAudioRecorder: {
+    type: Boolean,
+    default: false,
+  },
+  onFileUpload: {
+    type: Function,
+    default: () => {},
+  },
+  toggleEmojiPicker: {
+    type: Function,
+    default: () => {},
+  },
+  toggleAudioRecorder: {
+    type: Function,
+    default: () => {},
+  },
+  toggleAudioRecorderPlayPause: {
+    type: Function,
+    default: () => {},
+  },
+  isRecordingAudio: {
+    type: Boolean,
+    default: false,
+  },
+  recordingAudioState: {
+    type: String,
+    default: '',
+  },
+  isSendDisabled: {
+    type: Boolean,
+    default: false,
+  },
+  showEditorToggle: {
+    type: Boolean,
+    default: false,
+  },
+  isOnPrivateNote: {
+    type: Boolean,
+    default: false,
+  },
+  enableMultipleFileUpload: {
+    type: Boolean,
+    default: true,
+  },
+  enableWhatsAppTemplates: {
+    type: Boolean,
+    default: false,
+  },
+  enableContentTemplates: {
+    type: Boolean,
+    default: false,
+  },
+  conversationId: {
+    type: Number,
+    required: true,
+  },
+  message: {
+    type: String,
+    default: '',
+  },
+  newConversationModalActive: {
+    type: Boolean,
+    default: false,
+  },
+  portalSlug: {
+    type: String,
+    required: true,
+  },
+  conversationType: {
+    type: String,
+    default: '',
+  },
+  showQuotedReplyToggle: {
+    type: Boolean,
+    default: false,
+  },
+  quotedReplyEnabled: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-      return (
-        this.isFeatureEnabledonAccount(
-          this.accountId,
-          FEATURE_FLAGS.VOICE_RECORDER
-        ) && this.showAudioRecorder
-        // !isSafari
+const emit = defineEmits([
+  'replaceText',
+  'toggleInsertArticle',
+  'toggleEditor',
+  'selectWhatsappTemplate',
+  'selectContentTemplate',
+  'toggleQuotedReply',
+]);
+
+const { t } = useI18n();
+
+const uploadRef = ref(false);
+
+const { setSignatureFlagForInbox, fetchSignatureFlagFromUISettings } =
+  useUISettings();
+
+const keyboardEvents = {
+  '$mod+Alt+KeyA': {
+    action: () => {
+      // TODO: This is really hacky, we need to replace the file picker component with
+      // a custom one, where the logic and the component markup is isolated.
+      // Once we have the custom component, we can remove the hacky logic below.
+
+      const uploadTriggerButton = document.querySelector(
+        '#conversationAttachment'
       );
+      uploadTriggerButton.click();
     },
-    showAudioPlayStopButton() {
-      return this.showAudioRecorder && this.isRecordingAudio;
-    },
-    isInstagramDM() {
-      return this.conversationType === 'instagram_direct_message';
-    },
-    allowedFileTypes() {
-      // Use default file types for private notes
-      if (this.isOnPrivateNote) {
-        return this.ALLOWED_FILE_TYPES;
-      }
-
-      let channelType = this.channelType || this.inbox?.channel_type;
-
-      if (this.isAnInstagramChannel || this.isInstagramDM) {
-        channelType = INBOX_TYPES.INSTAGRAM;
-      }
-
-      return getAllowedFileTypesByChannel({
-        channelType,
-        medium: this.inbox?.medium,
-      });
-    },
-    enableDragAndDrop() {
-      return !this.newConversationModalActive;
-    },
-    audioRecorderPlayStopIcon() {
-      switch (this.recordingAudioState) {
-        // playing paused recording stopped inactive destroyed
-        case 'playing':
-          return 'i-ph-pause';
-        case 'paused':
-          return 'i-ph-play';
-        case 'stopped':
-          return 'i-ph-play';
-        default:
-          return 'i-ph-stop';
-      }
-    },
-    showMessageSignatureButton() {
-      return !this.isOnPrivateNote;
-    },
-    sendWithSignature() {
-      // channelType is sourced from inboxMixin
-      return this.fetchSignatureFlagFromUISettings(this.channelType);
-    },
-    signatureToggleTooltip() {
-      return this.sendWithSignature
-        ? this.$t('CONVERSATION.FOOTER.DISABLE_SIGN_TOOLTIP')
-        : this.$t('CONVERSATION.FOOTER.ENABLE_SIGN_TOOLTIP');
-    },
-    enableInsertArticleInReply() {
-      return this.portalSlug;
-    },
-    isFetchingAppIntegrations() {
-      return this.uiFlags.isFetching;
-    },
-    quotedReplyToggleTooltip() {
-      return this.quotedReplyEnabled
-        ? this.$t('CONVERSATION.REPLYBOX.QUOTED_REPLY.DISABLE_TOOLTIP')
-        : this.$t('CONVERSATION.REPLYBOX.QUOTED_REPLY.ENABLE_TOOLTIP');
-    },
-  },
-  mounted() {
-    ActiveStorage.start();
-  },
-  methods: {
-    toggleMessageSignature() {
-      this.setSignatureFlagForInbox(this.channelType, !this.sendWithSignature);
-    },
-    replaceText(text) {
-      this.$emit('replaceText', text);
-    },
-    toggleInsertArticle() {
-      this.$emit('toggleInsertArticle');
-    },
+    allowOnFocusedInput: true,
   },
 };
+
+useKeyboardEvents(keyboardEvents);
+
+const uiFlags = useMapGetter('integrations/getUIFlags');
+const { isCloudFeatureEnabled } = useAccount();
+
+const {
+  channelType,
+  isALineChannel,
+  isAnInstagramChannel,
+  isAWebWidgetInbox,
+  isAPIInbox,
+} = useInbox();
+
+const showAttachButton = computed(() => {
+  return props.showFileUpload || props.isNote;
+});
+
+const showAudioRecorderButton = computed(() => {
+  if (isALineChannel.value) {
+    return false;
+  }
+  // Disable audio recorder for safari browser as recording is not supported
+  // const isSafari = /^((?!chrome|android|crios|fxios).)*safari/i.test(
+  //   navigator.userAgent
+  // );
+
+  return (
+    isCloudFeatureEnabled(FEATURE_FLAGS.VOICE_RECORDER) &&
+    props.showAudioRecorder
+    // !isSafari
+  );
+});
+
+const showAudioPlayStopButton = computed(() => {
+  return props.showAudioRecorder && props.isRecordingAudio;
+});
+
+const isInstagramDM = computed(() => {
+  return props.conversationType === 'instagram_direct_message';
+});
+
+const allowedFileTypes = computed(() => {
+  // Use default file types for private notes
+  if (props.isOnPrivateNote) {
+    return ALLOWED_FILE_TYPES;
+  }
+
+  let channelTypeValue = channelType.value || props.inbox?.channel_type;
+
+  if (isAnInstagramChannel.value || isInstagramDM.value) {
+    channelTypeValue = INBOX_TYPES.INSTAGRAM;
+  }
+
+  return getAllowedFileTypesByChannel({
+    channelType: channelTypeValue,
+    medium: props.inbox?.medium,
+  });
+});
+
+const enableDragAndDrop = computed(() => {
+  return !props.newConversationModalActive;
+});
+
+const audioRecorderPlayStopIcon = computed(() => {
+  switch (props.recordingAudioState) {
+    // playing paused recording stopped inactive destroyed
+    case 'playing':
+      return 'i-ph-pause';
+    case 'paused':
+      return 'i-ph-play';
+    case 'stopped':
+      return 'i-ph-play';
+    default:
+      return 'i-ph-stop';
+  }
+});
+
+const showMessageSignatureButton = computed(() => {
+  return !props.isOnPrivateNote;
+});
+
+const sendWithSignature = computed(() => {
+  return fetchSignatureFlagFromUISettings(channelType.value);
+});
+
+const signatureToggleTooltip = computed(() => {
+  return sendWithSignature.value
+    ? t('CONVERSATION.FOOTER.DISABLE_SIGN_TOOLTIP')
+    : t('CONVERSATION.FOOTER.ENABLE_SIGN_TOOLTIP');
+});
+
+const enableInsertArticleInReply = computed(() => {
+  return props.portalSlug;
+});
+
+const isFetchingAppIntegrations = computed(() => {
+  return uiFlags.value.isFetching;
+});
+
+const quotedReplyToggleTooltip = computed(() => {
+  return props.quotedReplyEnabled
+    ? t('CONVERSATION.REPLYBOX.QUOTED_REPLY.DISABLE_TOOLTIP')
+    : t('CONVERSATION.REPLYBOX.QUOTED_REPLY.ENABLE_TOOLTIP');
+});
+
+const toggleMessageSignature = () => {
+  setSignatureFlagForInbox(channelType.value, !sendWithSignature.value);
+};
+
+const replaceText = text => {
+  emit('replaceText', text);
+};
+
+const toggleInsertArticle = () => {
+  emit('toggleInsertArticle');
+};
+
+onMounted(() => {
+  ActiveStorage.start();
+});
 </script>
 
 <template>
@@ -283,7 +284,7 @@ export default {
   >
     <div class="items-center flex gap-1.5">
       <NextButton
-        v-tooltip.top-end="$t('CONVERSATION.REPLYBOX.TIP_EMOJI_ICON')"
+        v-tooltip.top-end="t('CONVERSATION.REPLYBOX.TIP_EMOJI_ICON')"
         icon="i-ph-smiley-sticker"
         slate
         ghost
@@ -296,7 +297,7 @@ export default {
       />
       <FileUpload
         ref="uploadRef"
-        v-tooltip.top-end="$t('CONVERSATION.REPLYBOX.TIP_ATTACH_ICON')"
+        v-tooltip.top-end="t('CONVERSATION.REPLYBOX.TIP_ATTACH_ICON')"
         input-id="conversationAttachment"
         :size="4096 * 4096"
         :accept="allowedFileTypes"
@@ -311,7 +312,7 @@ export default {
       >
         <NextButton
           v-if="showAttachButton"
-          v-tooltip.top-end="$t('CONVERSATION.REPLYBOX.TIP_ATTACH_ICON')"
+          v-tooltip.top-end="t('CONVERSATION.REPLYBOX.TIP_ATTACH_ICON')"
           icon="i-ph-paperclip"
           slate
           ghost
@@ -324,7 +325,7 @@ export default {
       />
       <NextButton
         v-if="showAudioRecorderButton"
-        v-tooltip.top-end="$t('CONVERSATION.REPLYBOX.TIP_AUDIORECORDER_ICON')"
+        v-tooltip.top-end="t('CONVERSATION.REPLYBOX.TIP_AUDIORECORDER_ICON')"
         :icon="!isRecordingAudio ? 'i-ph-microphone' : 'i-ph-microphone-slash'"
         slate
         ghost
@@ -337,7 +338,7 @@ export default {
       />
       <NextButton
         v-if="showEditorToggle"
-        v-tooltip.top-end="$t('CONVERSATION.REPLYBOX.TIP_FORMAT_ICON')"
+        v-tooltip.top-end="t('CONVERSATION.REPLYBOX.TIP_FORMAT_ICON')"
         icon="i-ph-quotes"
         slate
         ghost
@@ -350,7 +351,7 @@ export default {
       />
       <NextButton
         v-if="showAudioPlayStopButton"
-        v-tooltip.top-end="$t('CONVERSATION.REPLYBOX.TIP_FORMAT_ICON')"
+        v-tooltip.top-end="t('CONVERSATION.REPLYBOX.TIP_FORMAT_ICON')"
         :icon="audioRecorderPlayStopIcon"
         slate
         ghost
@@ -391,7 +392,7 @@ export default {
       />
       <NextButton
         v-if="enableWhatsAppTemplates"
-        v-tooltip.top-end="$t('CONVERSATION.FOOTER.WHATSAPP_TEMPLATES')"
+        v-tooltip.top-end="t('CONVERSATION.FOOTER.WHATSAPP_TEMPLATES')"
         icon="i-ph-whatsapp-logo"
         slate
         ghost
@@ -437,7 +438,7 @@ export default {
         >
           <fluent-icon icon="cloud-backup" size="40" />
           <h4 class="text-2xl break-words text-n-slate-12">
-            {{ $t('CONVERSATION.REPLYBOX.DRAG_DROP') }}
+            {{ t('CONVERSATION.REPLYBOX.DRAG_DROP') }}
           </h4>
         </div>
       </transition>
@@ -447,7 +448,7 @@ export default {
       />
       <NextButton
         v-if="enableInsertArticleInReply"
-        v-tooltip.top-end="$t('HELP_CENTER.ARTICLE_SEARCH.OPEN_ARTICLE_SEARCH')"
+        v-tooltip.top-end="t('HELP_CENTER.ARTICLE_SEARCH.OPEN_ARTICLE_SEARCH')"
         icon="i-ph-article-ny-times"
         slate
         ghost
@@ -460,9 +461,13 @@ export default {
         :label="sendButtonText"
         type="submit"
         sm
-        :color="isNote ? 'amber' : 'blue'"
+        color="blue"
         :disabled="isSendDisabled"
-        class="flex-shrink-0"
+        class="flex-shrink-0 !text-xs"
+        :class="{
+          '!bg-n-solid-amber-button !text-[#251801] hover:enabled:!brightness-95 focus-visible:!brightness-95':
+            isNote,
+        }"
         @click="onSend"
       />
     </div>
