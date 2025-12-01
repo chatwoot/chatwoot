6cad23bcd1eb5d0c5ee9158189ab7ce1621d3d66
aakashb95 <aakash@chatwoot.com>
2025-12-01 17:25:21 +0530

    remove openai stuff from assistant

--- enterprise/app/services/captain/llm/assistant_chat_service.rb
+++ enterprise/app/services/captain/llm/assistant_chat_service.rb
@@ -1,6 +1,4 @@
-require 'openai'
-
-class Captain::Llm::AssistantChatService < Llm::BaseOpenAiService
+class Captain::Llm::AssistantChatService
   include Captain::ChatHelper
 
   def initialize(assistant: nil, conversation_id: nil)
@@ -8,9 +6,11 @@ class Captain::Llm::AssistantChatService < Llm::BaseOpenAiService
 
     @assistant = assistant
     @conversation_id = conversation_id
+    @model = InstallationConfig.find_by(name: 'CAPTAIN_OPEN_AI_MODEL')&.value.presence || Llm::Config::DEFAULT_MODEL
+
     @messages = [system_message]
     @response = ''
-    register_tools
+    @tools = build_tools
   end
 
   # additional_message: A single message (String) from the user that should be appended to the chat.
@@ -28,9 +28,8 @@ class Captain::Llm::AssistantChatService < Llm::BaseOpenAiService
 
   private
 
-  def register_tools
-    @tool_registry = Captain::ToolRegistryService.new(@assistant, user: nil)
-    @tool_registry.register_tool(Captain::Tools::SearchDocumentationService)
+  def build_tools
+    [Captain::Tools::SearchDocumentationService.new(@assistant, user: nil)]
   end
 
   def system_message
