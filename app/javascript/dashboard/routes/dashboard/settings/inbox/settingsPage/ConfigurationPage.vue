3af0e55376ac884db37b72499e78d68cc97ec75c
Sojan Jose <sojan@pepalo.com>
2025-12-04 17:50:23 -0800

    Voice: remove conversation voice i18n and config whitespace

--- app/javascript/dashboard/routes/dashboard/settings/inbox/settingsPage/ConfigurationPage.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/settingsPage/ConfigurationPage.vue
@@ -188,6 +188,7 @@ export default {
       <woot-code :script="inbox.voice_status_webhook_url" lang="html" />
     </SettingsSection>
   </div>
+
   <div v-else-if="isALineChannel" class="mx-8">
     <SettingsSection
       :title="$t('INBOX_MGMT.ADD.LINE_CHANNEL.API_CALLBACK.TITLE')"
