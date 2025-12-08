e12bc1afe89eefdabc60693f7e9154c479ba0686
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2025-12-08 17:23:42 +0530

    fix: Skip advanced search specs when searchkick not loaded
    Check if Message.search exists instead of Enterprise module.
    In CI, searchkick is not loaded when OPENSEARCH_URL is not set.

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -265,7 +265,7 @@ describe SearchService do
     end
   end
 
-  describe '#advanced_search with filters', if: defined?(Enterprise::SearchService) do
+  describe '#advanced_search with filters', if: Message.respond_to?(:search) do
     let(:params) { { q: 'test' } }
     let(:search_type) { 'Message' }
 
