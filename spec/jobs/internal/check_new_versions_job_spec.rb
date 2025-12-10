f2ea305886857a99c73a64416126ededf13e7a7e
Vishnu Narayanan <vishnu@chatwoot.com>
2025-12-10 20:17:29 +0530

    feat: distributed scheduling for version check job
    - change from all instances checking at UTC 1200 to distributed hourly
    checks
    - each instance checks once daily with a random jitter
    - update version check job schedule from daily to hourly

--- spec/jobs/internal/check_new_versions_job_spec.rb
+++ spec/jobs/internal/check_new_versions_job_spec.rb
@@ -7,8 +7,21 @@ RSpec.describe Internal::CheckNewVersionsJob do
     data = { 'version' => '1.2.3' }
     allow(Rails.env).to receive(:production?).and_return(true)
     allow(ChatwootHub).to receive(:sync_with_hub).and_return(data)
+
+    allow_any_instance_of(described_class).to receive(:should_run_check?).and_return(true)
+    allow_any_instance_of(described_class).to receive(:sleep)
+
     job
     expect(ChatwootHub).to have_received(:sync_with_hub)
     expect(Redis::Alfred.get(Redis::Alfred::LATEST_CHATWOOT_VERSION)).to eq data['version']
+  end
+
+  it 'does not run when not the designated hour' do
+    allow(Rails.env).to receive(:production?).and_return(true)
+    allow_any_instance_of(described_class).to receive(:should_run_check?).and_return(false)
+    allow(ChatwootHub).to receive(:sync_with_hub)
+
+    job
+    expect(ChatwootHub).not_to have_received(:sync_with_hub)
   end
 end
