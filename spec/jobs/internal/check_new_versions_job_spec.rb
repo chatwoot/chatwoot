a30f3801af37c828e65ef970aad04ffa6abdd007
Vishnu Narayanan <vishnu@chatwoot.com>
2025-12-11 18:16:07 +0530

    chore: fix rubocop

--- spec/jobs/internal/check_new_versions_job_spec.rb
+++ spec/jobs/internal/check_new_versions_job_spec.rb
@@ -3,25 +3,26 @@ require 'rails_helper'
 RSpec.describe Internal::CheckNewVersionsJob do
   subject(:job) { described_class.perform_now }
 
+  before do
+    # Mock sleep to avoid test delays
+    allow(Kernel).to receive(:sleep)
+  end
+
   it 'updates the latest chatwoot version in redis' do
     data = { 'version' => '1.2.3' }
     allow(Rails.env).to receive(:production?).and_return(true)
     allow(ChatwootHub).to receive(:sync_with_hub).and_return(data)
 
-    allow_any_instance_of(described_class).to receive(:should_run_check?).and_return(true)
-    allow_any_instance_of(described_class).to receive(:sleep)
+    # Set time to match the designated hour
+    installation_id = 'test-uuid'
+    allow(ChatwootHub).to receive(:installation_identifier).and_return(installation_id)
+    designated_hour = Digest::MD5.hexdigest(installation_id).hex % 24
+    travel_to Time.zone.parse("2024-01-15 #{designated_hour}:00:00 UTC")
 
     job
     expect(ChatwootHub).to have_received(:sync_with_hub)
     expect(Redis::Alfred.get(Redis::Alfred::LATEST_CHATWOOT_VERSION)).to eq data['version']
-  end
 
-  it 'does not run when not the designated hour' do
-    allow(Rails.env).to receive(:production?).and_return(true)
-    allow_any_instance_of(described_class).to receive(:should_run_check?).and_return(false)
-    allow(ChatwootHub).to receive(:sync_with_hub)
-
-    job
-    expect(ChatwootHub).not_to have_received(:sync_with_hub)
+    travel_back
   end
 end
