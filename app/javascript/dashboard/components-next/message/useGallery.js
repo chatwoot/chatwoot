44d363f43757272f36c9578768f7ae2e464eed2f
Shivam Mishra <scm.mymail@gmail.com>
2025-12-15 14:39:45 +0530

    feat: always allow gallery

--- app/javascript/dashboard/components-next/message/useGallery.js
+++ app/javascript/dashboard/components-next/message/useGallery.js
@@ -5,13 +5,10 @@ export function useGallery() {
   const [showGallery] = useToggle(false);
 
   const isGalleryAllowed = computed(() => {
-    // eslint-disable-next-line no-underscore-dangle
-    return !window.__WOOT_ISOLATED_SHELL__;
+    return true;
   });
 
   const toggleGallery = value => {
-    if (!isGalleryAllowed.value) return;
-
     showGallery.value = value;
   };
 
