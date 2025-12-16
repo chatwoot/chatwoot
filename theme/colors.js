32f0225add8819477691ddf3eaab93190f00a498
Oshan <lucas.oshan@gmail.com>
2025-12-16 19:27:53 -0300

    feat: update brand color from #2781F6 to #E94771 across multiple components and styles

--- theme/colors.js
+++ theme/colors.js
@@ -211,7 +211,7 @@ export const colors = {
     },
 
     black: '#000000',
-    brand: '#2781F6',
+    brand: '#E94771',
     background: 'rgb(var(--background-color) / <alpha-value>)',
     solid: {
       1: 'rgb(var(--solid-1) / <alpha-value>)',
