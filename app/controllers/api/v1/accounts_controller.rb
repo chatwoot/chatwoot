350fdd0fa0e0fe7f8e2e0cca36d553469b99f168
aakashb95 <aakashbakhle@gmail.com>
2025-12-19 13:58:45 +0530

    fix: upsert instead of replace captain_models obj inside settings

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -94,8 +94,15 @@ class Api::V1::AccountsController < Api::BaseController
   def settings_params
     permitted = params.permit(:auto_resolve_after, :auto_resolve_message, :auto_resolve_ignore_waiting, :audio_transcriptions, :auto_resolve_label,
                               conversation_required_attributes: [])
-    permitted[:captain_models] = params[:captain_models].to_unsafe_h if params[:captain_models].present?
-    permitted[:captain_features] = params[:captain_features].to_unsafe_h if params[:captain_features].present?
+
+    if params[:captain_models].present?
+      existing_models = @account.captain_models || {}
+      permitted[:captain_models] = existing_models.merge(params[:captain_models].to_unsafe_h)
+    end
+    if params[:captain_features].present?
+      existing_features = @account.captain_features || {}
+      permitted[:captain_features] = existing_features.merge(params[:captain_features].to_unsafe_h)
+    end
     permitted
   end
 
