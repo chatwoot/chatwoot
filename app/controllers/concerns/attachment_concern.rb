5ef5a6db18f9913fae5be11c4d9978f46c064e22
Pranav <pranav@chatwoot.com>
2025-12-19 18:31:54 -0800

    cyclomatic complexity

--- app/controllers/concerns/attachment_concern.rb
+++ app/controllers/concerns/attachment_concern.rb
@@ -8,18 +8,28 @@ module AttachmentConcern
     sanitized = actions.map do |action|
       next action unless action[:action_name] == 'send_attachment'
 
-      blob_id = action[:action_params].first
-      blob = ActiveStorage::Blob.find_signed(blob_id.to_s)
-      if blob.present?
-        blobs << blob
-        action.merge(action_params: [blob.id])
-      elsif record && record.files.any? { |f| f.blob_id == blob_id.to_i }
-        action
-      else
-        return [nil, nil, I18n.t('errors.attachments.invalid')]
-      end
+      result = process_attachment_action(action, record, blobs)
+      return [nil, nil, I18n.t('errors.attachments.invalid')] unless result
+
+      result
     end
 
     [blobs, sanitized, nil]
+  end
+
+  private
+
+  def process_attachment_action(action, record, blobs)
+    blob_id = action[:action_params].first
+    blob = ActiveStorage::Blob.find_signed(blob_id.to_s)
+
+    return action.merge(action_params: [blob.id]).tap { blobs << blob } if blob.present?
+    return action if blob_already_attached?(record, blob_id)
+
+    nil
+  end
+
+  def blob_already_attached?(record, blob_id)
+    record&.files&.any? { |f| f.blob_id == blob_id.to_i }
   end
 end
