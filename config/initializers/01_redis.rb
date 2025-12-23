2984571d9c81bde2fdb00e1dc7c9618106876e3e
tanmay <tanmay-chatwoot@tanmays-MacBook-Pro.local>
2025-12-23 13:12:58 +0700

    increase the alfred connection pool size to 10

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -5,7 +5,7 @@
 # Alfred
 # Add here as you use it for more features
 # Used for Round Robin, Conversation Emails & Online Presence
-$alfred = ConnectionPool.new(size: 5, timeout: 1) do
+$alfred = ConnectionPool.new(size: 10, timeout: 1) do
   redis = Rails.env.test? ? MockRedis.new : Redis.new(Redis::Config.app)
   Redis::Namespace.new('alfred', redis: redis, warning: true)
 end
