d611808945c733e13cf28a90e023376d4bc5b858
aakashb95 <aakashbakhle@gmail.com>
2026-01-06 17:55:05 +0530

    fix: attempt CI fail fix

--- lib/custom_exceptions/pdf_processing_error.rb
+++ lib/custom_exceptions/pdf_processing_error.rb
@@ -1,9 +1,7 @@
 require_relative 'base'
 
-module CustomExceptions
-  class PdfProcessingError < Base
-    def initialize(message = 'PDF processing failed')
-      super(message)
-    end
+class CustomExceptions::PdfProcessingError < Base
+  def initialize(message = 'PDF processing failed')
+    super(message)
   end
 end
