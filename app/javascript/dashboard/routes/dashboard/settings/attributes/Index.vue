aa9e1603e6bd093c2e156fed42629c6bfb6b4446
cdeveloper123 <cdeveloper2016@gmail.com>
2026-01-08 17:30:20 +0530

    fix:tabbar issue fixes

--- app/javascript/dashboard/routes/dashboard/settings/attributes/Index.vue
+++ app/javascript/dashboard/routes/dashboard/settings/attributes/Index.vue
@@ -7,6 +7,7 @@ import AddAttribute from './AddAttribute.vue';
 import EditAttribute from './EditAttribute.vue';
 import SettingsLayout from '../SettingsLayout.vue';
 import Button from 'dashboard/components-next/button/Button.vue';
+import TabBar from 'dashboard/components-next/tabbar/TabBar.vue';
 import AttributeListItem from 'dashboard/components-next/ConversationWorkflow/AttributeListItem.vue';
 import { useI18n } from 'vue-i18n';
 import {
@@ -58,6 +59,10 @@ const tabs = computed(() => {
   ];
 });
 
+const tabsForTabBar = computed(() =>
+  tabs.value.map(tab => ({ label: tab.name, key: tab.key }))
+);
+
 onMounted(() => {
   store.dispatch('attributes/get');
 });
@@ -71,11 +76,7 @@ const attributes = computed(() =>
 );
 
 const onClickTabChange = tab => {
-  if (typeof tab === 'number') {
-    selectedTabIndex.value = tab; // for woot-tabs: 0 or 1
-  } else if (tab && typeof tab.key !== 'undefined') {
-    selectedTabIndex.value = tab.key; // for TabBar: { key, label }
-  }
+  selectedTabIndex.value = tab.key;
 };
 
 const handleEditAttribute = attribute => {
@@ -168,6 +169,12 @@ const derivedAttributes = computed(() =>
     </template>
     <template #body>
       <div class="flex flex-col gap-6">
+        <TabBar
+          :tabs="tabsForTabBar"
+          :initial-active-tab="selectedTabIndex"
+          class="max-w-xl"
+          @tab-changed="onClickTabChange"
+        />
         <div v-if="derivedAttributes.length" class="grid gap-3">
           <AttributeListItem
             v-for="attribute in derivedAttributes"
