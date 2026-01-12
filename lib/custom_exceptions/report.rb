31f4ac58f640bbccda4e0a46dab33d06fe3a5a40
Pranav <pranav@chatwoot.com>
2026-01-12 13:47:07 -0800

    Fix report.rb

--- lib/custom_exceptions/report.rb
+++ lib/custom_exceptions/report.rb
@@ -1,9 +1,11 @@
-class CustomExceptions::Report::InvalidDateRange < CustomExceptions::Base
-  def message
-    I18n.t('errors.reports.date_range_too_long')
-  end
+module CustomExceptions::Report
+  class InvalidDateRange < CustomExceptions::Base
+    def message
+      I18n.t('errors.reports.date_range_too_long')
+    end
 
-  def http_status
-    :unprocessable_entity
+    def http_status
+      :unprocessable_entity
+    end
   end
 end
