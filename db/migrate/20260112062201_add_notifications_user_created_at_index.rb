3b838b89c2124c26e3941d3e8ac9603e7ed9f6b1
Tanmay Deep Sharma <tanmaydeepsharma21@gmail.com>
2026-01-12 12:16:18 +0530

    remove the optimisation db query

--- db/migrate/20260112062201_add_notifications_user_created_at_index.rb
+++
@@ -1,9 +0,0 @@
-class AddNotificationsUserCreatedAtIndex < ActiveRecord::Migration[7.1]
-  disable_ddl_transaction!
-
-  def change
-    add_index :notifications, [:user_id, :created_at],
-              name: 'idx_notifications_user_created_at',
-              algorithm: :concurrently
-  end
-end
--- spec/jobs/notification/trim_user_notifications_job_spec.rb
+++ spec/jobs/notification/trim_user_notifications_job_spec.rb
@@ -5,67 +5,35 @@ RSpec.describe Notification::TrimUserNotificationsJob do
   let(:user) { create(:user, account: account) }
   let(:conversation) { create(:conversation, account: account) }
 
-  it 'enqueues the job' do
+  it 'enqueues the job on low queue' do
     expect { described_class.perform_later(user.id) }
       .to have_enqueued_job(described_class).with(user.id).on_queue('low')
   end
 
-  context 'when user has fewer than 300 notifications' do
-    before do
-      create_list(:notification, 10, user: user, account: account, primary_actor: conversation)
-    end
-
-    it 'does not delete any notifications' do
-      expect { described_class.perform_now(user.id) }.not_to change { Notification.count }
-    end
+  it 'does not delete notifications when under limit' do
+    create_list(:notification, 5, user: user, account: account, primary_actor: conversation)
+    expect { described_class.perform_now(user.id) }.not_to(change(Notification, :count))
   end
 
-  context 'when user has exactly 300 notifications' do
-    before do
-      create_list(:notification, 300, user: user, account: account, primary_actor: conversation)
-    end
+  it 'trims to 300 notifications keeping the most recent' do
+    old_notifications = create_list(:notification, 50, user: user, account: account, primary_actor: conversation, created_at: 2.days.ago)
+    recent_notifications = create_list(:notification, 300, user: user, account: account, primary_actor: conversation, created_at: 1.hour.ago)
 
-    it 'does not delete any notifications' do
-      expect { described_class.perform_now(user.id) }.not_to change { Notification.count }
-    end
+    described_class.perform_now(user.id)
+
+    expect(Notification.where(user_id: user.id).count).to eq(300)
+    expect(Notification.where(id: old_notifications.map(&:id))).to be_empty
+    expect(Notification.where(id: recent_notifications.map(&:id)).count).to eq(300)
   end
 
-  context 'when user has more than 300 notifications' do
-    let!(:old_notifications) do
-      create_list(:notification, 50, user: user, account: account, primary_actor: conversation, created_at: 2.days.ago)
-    end
-    let!(:recent_notifications) do
-      create_list(:notification, 300, user: user, account: account, primary_actor: conversation, created_at: 1.hour.ago)
-    end
+  it 'only affects the specified user' do
+    other_user = create(:user, account: account)
+    create_list(:notification, 310, user: user, account: account, primary_actor: conversation)
+    create_list(:notification, 5, user: other_user, account: account, primary_actor: conversation)
 
-    it 'keeps only the 300 most recent notifications' do
-      described_class.perform_now(user.id)
-      expect(Notification.where(user_id: user.id).count).to eq(300)
-    end
+    described_class.perform_now(user.id)
 
-    it 'deletes the oldest notifications' do
-      described_class.perform_now(user.id)
-      expect(Notification.where(id: old_notifications.map(&:id))).to be_empty
-    end
-
-    it 'keeps the recent notifications' do
-      described_class.perform_now(user.id)
-      expect(Notification.where(id: recent_notifications.map(&:id)).count).to eq(300)
-    end
-  end
-
-  context 'with multiple users' do
-    let(:user2) { create(:user, account: account) }
-
-    before do
-      create_list(:notification, 350, user: user, account: account, primary_actor: conversation)
-      create_list(:notification, 100, user: user2, account: account, primary_actor: conversation)
-    end
-
-    it 'only affects the specified user notifications' do
-      described_class.perform_now(user.id)
-      expect(Notification.where(user_id: user.id).count).to eq(300)
-      expect(Notification.where(user_id: user2.id).count).to eq(100)
-    end
+    expect(Notification.where(user_id: user.id).count).to eq(300)
+    expect(Notification.where(user_id: other_user.id).count).to eq(5)
   end
 end
