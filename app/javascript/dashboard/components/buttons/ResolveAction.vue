c28a3fc83bd2660564954f4d0835f6eedc4b472f
Jose Ulisses <joseulisses03@gmail.com>
2026-01-13 11:53:53 -0300

    Mudando os botoes de abrir e deixar pendente.

--- app/javascript/dashboard/components/buttons/ResolveAction.vue
+++ app/javascript/dashboard/components/buttons/ResolveAction.vue
@@ -17,6 +17,7 @@ import {
 
 import ButtonGroup from 'dashboard/components-next/buttonGroup/ButtonGroup.vue';
 import Button from 'dashboard/components-next/button/Button.vue';
+import { STATUS } from '../../store/constants';
 
 const store = useStore();
 const getters = useStoreGetters();
@@ -138,7 +139,8 @@ useEmitter(CMD_RESOLVE_CONVERSATION, onCmdResolveConversation);
       class="rounded-lg shadow outline-1 outline flex-shrink-0"
       :class="!showOpenButton ? 'outline-n-container' : 'outline-transparent'"
     >
-      <Button
+      <!-- TODO: Voltar ao normal, para aparecerem novamente as outras opçoes -->
+      <!-- <Button
         v-if="isOpen"
         :label="t('CONVERSATION.HEADER.RESOLVE_ACTION')"
         size="sm"
@@ -147,6 +149,16 @@ useEmitter(CMD_RESOLVE_CONVERSATION, onCmdResolveConversation);
         class="ltr:rounded-r-none rtl:rounded-l-none !outline-0"
         :is-loading="isLoading"
         @click="onCmdResolveConversation"
+      /> -->
+      <Button
+        v-if="isOpen"
+        :label="t('CONVERSATION.RESOLVE_DROPDOWN.MARK_PENDING')"
+        size="sm"
+        color="slate"
+        no-animation
+        class="ltr:rounded-r-none rtl:rounded-l-none !outline-0"
+        :is-loading="isLoading"
+        @click="toggleStatus(wootConstants.STATUS_TYPE.PENDING)"
       />
       <Button
         v-else-if="isResolved"
@@ -167,7 +179,8 @@ useEmitter(CMD_RESOLVE_CONVERSATION, onCmdResolveConversation);
         :is-loading="isLoading"
         @click="onCmdOpenConversation"
       />
-      <Button
+      <!-- TODO: Voltar ao normal, para aparecerem novamente as outras opçoes -->
+      <!-- <Button
         v-if="showAdditionalActions"
         ref="arrowDownButtonRef"
         icon="i-lucide-chevron-down"
@@ -178,9 +191,10 @@ useEmitter(CMD_RESOLVE_CONVERSATION, onCmdResolveConversation);
         color="slate"
         trailing-icon
         @click="openDropdown"
-      />
+      /> -->
     </ButtonGroup>
-    <div
+    <!-- TODO: Voltar ao normal, para aparecerem novamente as outras opçoes -->
+    <!-- <div
       v-if="showActionsDropdown"
       v-on-clickaway="closeDropdown"
       class="border rounded-lg shadow-lg border-n-strong dark:border-n-strong box-content p-2 w-fit z-10 bg-n-alpha-3 backdrop-blur-[100px] absolute block left-auto top-full mt-0.5 start-0 xl:start-auto xl:end-0 max-w-[12.5rem] min-w-[9.75rem] [&_ul>li]:mb-0"
@@ -211,6 +225,6 @@ useEmitter(CMD_RESOLVE_CONVERSATION, onCmdResolveConversation);
           />
         </WootDropdownItem>
       </WootDropdownMenu>
-    </div>
+    </div> -->
   </div>
 </template>
