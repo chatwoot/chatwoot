9aabb71c6e8547ed70aa6a9f3e75ada7ccd39b76
aakashb95 <aakashbakhle@gmail.com>
2026-01-14 16:57:21 +0530

    fix: pass formatted conversation history

--- lib/captain/reply_suggestion_service.rb
+++ lib/captain/reply_suggestion_service.rb
@@ -5,12 +5,17 @@ class Captain::ReplySuggestionService < Captain::BaseTaskService
     make_api_call(
       model: GPT_MODEL,
       messages: [
-        { role: 'system', content: prompt_from_file('reply') }
-      ].concat(conversation_messages)
+        { role: 'system', content: prompt_from_file('reply') },
+        { role: 'user', content: formatted_conversation }
+      ]
     )
   end
 
   private
+
+  def formatted_conversation
+    LlmFormatter::ConversationLlmFormatter.new(conversation).format
+  end
 
   def event_name
     'reply_suggestion'
