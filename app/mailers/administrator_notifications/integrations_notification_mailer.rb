b6aea9e0da1db2d2aff24da51ab5cf815f7e917c
aakashb95 <aakashbakhle@gmail.com>
2026-01-19 13:17:43 +0530

    add: mailers to admins to update openai key

--- app/mailers/administrator_notifications/integrations_notification_mailer.rb
+++ app/mailers/administrator_notifications/integrations_notification_mailer.rb
@@ -9,4 +9,10 @@ class AdministratorNotifications::IntegrationsNotificationMailer < Administrator
     subject = 'Your Dialogflow integration was disconnected'
     send_notification(subject)
   end
+
+  def openai_disconnect
+    subject = 'Your OpenAI integration needs attention'
+    action_url = settings_url('integrations/openai')
+    send_notification(subject, action_url: action_url)
+  end
 end
