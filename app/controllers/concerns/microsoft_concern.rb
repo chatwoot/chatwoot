9165897276edff4a2064747ea5bcb28910d8f3db
Justus K <justus.k@protonmail.com>
2026-01-20 14:28:07 +0000

    feat: Support azure single tenant app

--- app/controllers/concerns/microsoft_concern.rb
+++ app/controllers/concerns/microsoft_concern.rb
@@ -4,12 +4,22 @@ module MicrosoftConcern
   def microsoft_client
     app_id = GlobalConfigService.load('AZURE_APP_ID', nil)
     app_secret = GlobalConfigService.load('AZURE_APP_SECRET', nil)
+    tenant_id = GlobalConfigService.load('AZURE_TENANT_ID', nil)
+
+    # Determine the endpoints based on whether a tenant ID is provided
+    if tenant_id.present?
+      authorize_url = "https://login.microsoftonline.com/#{tenant_id}/oauth2/v2.0/authorize"
+      token_url = "https://login.microsoftonline.com/#{tenant_id}/oauth2/v2.0/token"
+    else
+      authorize_url = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
+      token_url = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
+    end
 
     ::OAuth2::Client.new(app_id, app_secret,
                          {
                            site: 'https://login.microsoftonline.com',
-                           authorize_url: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-                           token_url: 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
+                           authorize_url: authorize_url,
+                           token_url: token_url
                          })
   end
 
