a856b591f4d49b0d7df73b1a7b6d162adecbfd10
Nitish Gupta <imnitish.ng@gmail.com>
2026-01-21 15:24:30 +0000

    CI CD possibly?

--- docker/entrypoints/rails.sh
+++ docker/entrypoints/rails.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 set -x
-
+source /app/.env
+ls /app
 # Remove a potentially pre-existing server.pid for Rails.
 rm -rf /app/tmp/pids/server.pid
 rm -rf /app/tmp/cache/*
 
 echo "Waiting for postgres to become ready...."
-
+echo "DATABASE_URL: $DATABASE_URL"
+echo "POSTGRES_HOST: $POSTGRES_HOST"
+echo "POSTGRES_PORT: $POSTGRES_PORT"
+echo "POSTGRES_USERNAME: $POSTGRES_USERNAME"
+echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
+echo "POSTGRES_DB: $POSTGRES_DB"
 # Let DATABASE_URL env take presedence over individual connection params.
 # This is done to avoid printing the DATABASE_URL in the logs
 $(docker/entrypoints/helpers/pg_database_url.rb)
@@ -21,7 +27,7 @@ done
 echo "Database ready to accept connections."
 
 #install missing gems for local dev as we are using base image compiled for production
-bundle install
+# bundle install
 
 BUNDLE="bundle check"
 
