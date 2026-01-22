e6910178254be50e5460bf0de4551990c87b92ca
aakashb95 <aakashbakhle@gmail.com>
2026-01-22 15:27:58 +0530

    fix: use name for observation cooonsistency

--- enterprise/app/services/captain/tools/instrumentation.rb
+++ enterprise/app/services/captain/tools/instrumentation.rb
@@ -3,14 +3,8 @@ module Captain::Tools::Instrumentation
   include Integrations::LlmInstrumentation
 
   def execute(**args)
-    instrument_tool_call(tool_name, args) do
+    instrument_tool_call(name, args) do
       super
     end
-  end
-
-  private
-
-  def tool_name
-    self.class.name.demodulize.underscore.delete_suffix('_service')
   end
 end
