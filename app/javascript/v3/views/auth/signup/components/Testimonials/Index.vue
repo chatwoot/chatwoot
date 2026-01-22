68d4158a415eda9c86675cdab65b95d5a146b92f
Pranav <pranav@chatwoot.com>
2026-01-22 14:20:12 -0800

    Update testimonial fetch

--- app/javascript/v3/views/auth/signup/components/Testimonials/Index.vue
+++ app/javascript/v3/views/auth/signup/components/Testimonials/Index.vue
@@ -1,14 +1,27 @@
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onBeforeMount } from 'vue';
 import TestimonialCard from './TestimonialCard.vue';
+import { getTestimonialContent } from '../../../../../api/testimonials';
 
 const emit = defineEmits(['resizeContainers']);
 
-const reviewContent =
-  "Chatwoot has transformed the way we engage with our customers. It's omni-channel feature allows us to manage conversations simultaneously across multiple channels.\n\nI've used Chatwoot at my previous companies and continue to rely on it today at Converso. After years as a happy customer, I can confidently say it's an indispensable tool for any customer-focused business.";
+const testimonial = ref(null);
 
-onMounted(() => {
-  emit('resizeContainers', true);
+const fetchTestimonials = async () => {
+  try {
+    const { data } = await getTestimonialContent();
+    if (data.length) {
+      testimonial.value = data[Math.floor(Math.random() * data.length)];
+    }
+  } catch {
+    // Ignoring the error as the UI wouldn't break
+  } finally {
+    emit('resizeContainers', !!testimonial.value);
+  }
+};
+
+onBeforeMount(() => {
+  fetchTestimonials();
 });
 </script>
 
@@ -17,9 +30,11 @@ onMounted(() => {
     class="relative flex-1 flex flex-col items-start justify-center bg-n-alpha-black2 dark:bg-n-solid-3 px-12 py-14 rounded-e-lg"
   >
     <TestimonialCard
-      :review-content="reviewContent"
-      author-name="Ariel Lambrecht"
-      author-designation="Founder of 99 - Brazil's first unicorn and Converso"
+      v-if="testimonial"
+      :review-content="testimonial.authorReview"
+      :author-image="testimonial.authorImage"
+      :author-name="testimonial.authorName"
+      :author-designation="testimonial.authorCompany"
     />
     <div class="absolute bottom-8 right-8 grid grid-cols-3 gap-1.5">
       <span class="w-2 h-2 rounded-full bg-n-slate-7" />
