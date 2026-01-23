f9efbad5168e37f09acf5b9ce51567eadf9f8279
Pranav <pranav@chatwoot.com>
2026-01-22 18:47:42 -0800

    chore: Improve signup flow, reduce the number of inputs (#13350)
    - Improved design for the Chatwoot sign up page

--- app/javascript/dashboard/constants/globals.js
+++ app/javascript/dashboard/constants/globals.js
@@ -34,7 +34,8 @@ export default {
   DOCS_URL: 'https://www.chatwoot.com/docs/product/',
   HELP_CENTER_DOCS_URL:
     'https://www.chatwoot.com/docs/product/others/help-center',
-  TESTIMONIAL_URL: 'https://testimonials.cdn.chatwoot.com/content.json',
+  TESTIMONIAL_URL:
+    'https://testimonials.cdn.chatwoot.com/testimonial-content.json',
   WHATSAPP_EMBEDDED_SIGNUP_DOCS_URL:
     'https://developers.facebook.com/docs/whatsapp/embedded-signup/custom-flows/onboarding-business-app-users#limitations',
   SMALL_SCREEN_BREAKPOINT: 768,
