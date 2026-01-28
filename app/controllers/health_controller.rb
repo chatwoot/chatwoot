bb4bb6221881ddbb87713bc625f47fa11f7bbbf5
Vishnu Narayanan <iamwishnu@gmail.com>
2026-01-28 23:47:01 +0530

    chore: fix specs and review comments

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,4 +1,6 @@
-class HealthController < ActionController::Base
+# Inherits from ActionController::Base to skip all middleware,
+# authentication, and callbacks. Used for health checks
+class HealthController < ActionController::Base # rubocop:disable Rails/ApplicationController
   def show
     render json: { status: 'woot' }
   end
