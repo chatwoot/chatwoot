9813c3aac30b20a997c83e95801ef67f0096ecb4
adham90 <hi@adham.dev>
2026-02-10 17:12:48 +0200

    fix(storefront): include products with null stock in listing

--- app/controllers/storefront/products_controller.rb
+++ app/controllers/storefront/products_controller.rb
@@ -2,7 +2,7 @@ class Storefront::ProductsController < Storefront::BaseController
   before_action :ensure_catalog_enabled!
 
   def index
-    @products = @account.products.where.not(stock: 0).order(created_at: :desc)
+    @products = @account.products.where("stock IS NULL OR stock != 0").order(created_at: :desc)
     @currency = catalog_settings.currency
   end
 
