bab68ab6fe48b5af78ba0e7a0dacd42272de2bd7
Shivam Mishra <scm.mymail@gmail.com>
2026-02-18 13:53:02 +0530

    chore: disable rubocop

--- db/migrate/20260218075101_add_secret_to_webhooks.rb
+++ db/migrate/20260218075101_add_secret_to_webhooks.rb
@@ -2,7 +2,9 @@ class AddSecretToWebhooks < ActiveRecord::Migration[7.1]
   def up
     add_column :webhooks, :secret, :string
     Webhook.find_each do |webhook|
+      # rubocop:disable Rails/SkipsModelValidations
       webhook.update_column(:secret, SecureRandom.urlsafe_base64(24))
+      # rubocop:enable Rails/SkipsModelValidations
     end
   end
 
