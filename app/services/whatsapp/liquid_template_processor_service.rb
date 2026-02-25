53939604e7668d46c363b39ffefa5e7db47f87c3
Muhsin <muhsinkeramam@gmail.com>
2026-02-25 17:29:59 +0400

    chore: fix codex comments

--- app/services/whatsapp/liquid_template_processor_service.rb
+++ app/services/whatsapp/liquid_template_processor_service.rb
@@ -4,10 +4,10 @@ class Whatsapp::LiquidTemplateProcessorService
   def process_template_params(template_params)
     return template_params if template_params.blank?
 
-    template_params_copy = deep_copy(template_params)
+    template_params_copy = template_params.deep_dup
     processed_params = template_params_copy['processed_params']
 
-    return template_params_copy if processed_params.blank?
+    return template_params_copy if processed_params.blank? || !processed_params.is_a?(Hash)
 
     process_body_params(processed_params)
     process_header_params(processed_params)
@@ -61,16 +61,5 @@ class Whatsapp::LiquidTemplateProcessorService
 
   def liquid_service
     @liquid_service ||= Liquid::CampaignTemplateService.new(campaign: campaign, contact: contact)
-  end
-
-  def deep_copy(obj)
-    case obj
-    when Hash
-      obj.transform_values { |v| deep_copy(v) }
-    when Array
-      obj.map { |v| deep_copy(v) }
-    else
-      obj.duplicable? ? obj.dup : obj
-    end
   end
 end
