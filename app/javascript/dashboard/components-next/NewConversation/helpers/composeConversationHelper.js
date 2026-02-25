2086ee569d884c57f30a3b11498c4c79031ca726
Pranav <pranav@chatwoot.com>
2026-02-25 08:22:50 -0800

    fix: Use search API instead of filter in the filter in the endpoints

--- app/javascript/dashboard/components-next/NewConversation/helpers/composeConversationHelper.js
+++ app/javascript/dashboard/components-next/NewConversation/helpers/composeConversationHelper.js
@@ -176,32 +176,11 @@ export const prepareWhatsAppMessagePayload = ({
   };
 };
 
-export const generateContactQuery = ({ keys = ['email'], query }) => {
-  return {
-    payload: keys.map(key => {
-      const filterPayload = {
-        attribute_key: key,
-        filter_operator: 'contains',
-        values: [query],
-        attribute_model: 'standard',
-      };
-      if (keys.findIndex(k => k === key) !== keys.length - 1) {
-        filterPayload.query_operator = 'or';
-      }
-      return filterPayload;
-    }),
-  };
-};
-
 // API Calls
-export const searchContacts = async ({ keys, query }) => {
+export const searchContacts = async query => {
   const {
     data: { payload },
-  } = await ContactAPI.filter(
-    undefined,
-    'name',
-    generateContactQuery({ keys, query })
-  );
+  } = await ContactAPI.search(query);
   const camelCasedPayload = camelcaseKeys(payload, { deep: true });
   // Filter contacts that have either phone_number or email
   const filteredPayload = camelCasedPayload?.filter(
