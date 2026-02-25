b0fd4317fc6f139a891872b078bd1c79c922b8e4
Sojan Jose <sojan@pepalo.com>
2026-02-24 19:21:52 -0800

    test(hub): use explicit static url in enterprise spec

--- spec/enterprise/lib/chatwoot_hub_spec.rb
+++ spec/enterprise/lib/chatwoot_hub_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ChatwootHub do
   describe '.base_url' do
-    it 'ignores CHATWOOT_HUB_URL outside development for enterprise edition' do
+    it 'uses the static hub url outside development for enterprise edition' do
       with_modified_env CHATWOOT_HUB_URL: 'https://custom.example.com' do
         allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('production'))
 
-        expect(described_class.base_url).to eq(Enterprise::ChatwootHub::ENTERPRISE_BASE_URL)
+        expect(described_class.base_url).to eq('https://hub.2.chatwoot.com')
       end
     end
 
