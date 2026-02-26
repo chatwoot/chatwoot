1b57809ae91e4c74f5eb29a66e667e584e648412
Shivam Mishra <scm.mymail@gmail.com>
2026-02-26 14:16:57 +0530

    refactor: separate schema and data changes in webhook secret migration
    Move the data backfill out of the schema migration so AddSecretToWebhooks
    only adds the column. This keeps DDL and DML concerns separate.

--- db/migrate/20260218075101_add_secret_to_webhooks.rb
+++ db/migrate/20260218075101_add_secret_to_webhooks.rb
@@ -1,14 +1,5 @@
 class AddSecretToWebhooks < ActiveRecord::Migration[7.1]
-  def up
+  def change
     add_column :webhooks, :secret, :string
-    Webhook.find_each do |webhook|
-      # rubocop:disable Rails/SkipsModelValidations
-      webhook.update_column(:secret, SecureRandom.urlsafe_base64(24))
-      # rubocop:enable Rails/SkipsModelValidations
-    end
-  end
-
-  def down
-    remove_column :webhooks, :secret
   end
 end
