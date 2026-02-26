5ca6212ff7aeba3605e13a9a2fb42ae8cd92e2f1
Sojan Jose <sojan@pepalo.com>
2026-02-26 14:59:41 -0800

    Revert "fix: call authorization_error! on IMAP auth failures (#13560)"
    This reverts commit 7acd239c7050887fc9b0c5dd562e545a96080abc.

--- app/jobs/inboxes/fetch_imap_email_inboxes_job.rb
+++ app/jobs/inboxes/fetch_imap_email_inboxes_job.rb
@@ -15,7 +15,6 @@ class Inboxes::FetchImapEmailInboxesJob < ApplicationJob
     return false if inbox.account.suspended?
     return false unless inbox.channel.imap_enabled
     return false if inbox.channel.reauthorization_required?
-    return false if inbox.channel.in_backoff?
 
     return true unless ChatwootApp.chatwoot_cloud?
     return false if default_plan?(inbox.account)
