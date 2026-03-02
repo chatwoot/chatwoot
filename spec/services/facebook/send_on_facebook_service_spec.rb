0c34e4a825296c627454fed2cdf84d4f8ef6262c
Muhsin <muhsinkeramam@gmail.com>
2026-03-02 14:14:32 +0400

    chore: review fixes
    Call GlobalConfig.clear_cache in the spec setup to ensure cached configuration does not leak between examples. This prevents flakiness caused by stale global config values in spec/services/facebook/send_on_facebook_service_spec.rb.

--- spec/services/facebook/send_on_facebook_service_spec.rb
+++ spec/services/facebook/send_on_facebook_service_spec.rb
@@ -7,6 +7,7 @@ describe Facebook::SendOnFacebookService do
     allow(Facebook::Messenger::Subscriptions).to receive(:subscribe).and_return(true)
     allow(bot).to receive(:deliver).and_return({ recipient_id: '1008372609250235', message_id: 'mid.1456970487936:c34767dfe57ee6e339' }.to_json)
     create(:message, message_type: :incoming, inbox: facebook_inbox, account: account, conversation: conversation)
+    GlobalConfig.clear_cache
   end
 
   let!(:account) { create(:account) }
