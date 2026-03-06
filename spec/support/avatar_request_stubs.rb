f6c7eff899a417bc581a682816dde778980ae895
Sojan Jose <sojan@pepalo.com>
2026-03-05 16:22:24 -0800

    test(reports): narrow favicon stub scope

--- spec/support/avatar_request_stubs.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.configure do |config|
-  config.before do
-    stub_request(:get, %r{\Ahttps://www\.google\.com/s2/favicons\?.*})
-      .to_return(status: 404, body: '', headers: {})
-  end
-end
