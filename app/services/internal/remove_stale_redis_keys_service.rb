1d18a2aba1db543a3c1f09af28e15770816deb9e
Tanmay Deep Sharma <32020192+tds-1@users.noreply.github.com>
2026-03-09 18:23:44 +0530

    perf: reduce presence update frequency and fix background tab throttling (#13726)
    ## Description
    Reduces the frequency of update_presence WebSocket calls from the live
    chat widget and fixes agents appearing offline when the dashboard is in
    a background tab.
    
    ## Fixes # (issue)
    https://github.com/chatwoot/chatwoot/issues/13720
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    
    
    ## Checklist:
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented on my code, particularly in hard-to-understand
    areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged and published in downstream
    modules

--- app/services/internal/remove_stale_redis_keys_service.rb
+++ app/services/internal/remove_stale_redis_keys_service.rb
@@ -3,7 +3,7 @@ class Internal::RemoveStaleRedisKeysService
 
   def perform
     Rails.logger.info "Removing redis stale keys for account #{@account_id}"
-    range_start = (Time.zone.now - OnlineStatusTracker::PRESENCE_DURATION).to_i
+    range_start = (Time.zone.now - OnlineStatusTracker::CONTACT_PRESENCE_DURATION).to_i
     # exclusive minimum score is specified by prefixing (
     # we are clearing old records because this could clogg up the sorted set
     ::Redis::Alfred.zremrangebyscore(
