3d71ea7b92cd39553bc42746b6097112adf65b6b
Tanmay Deep Sharma <tanmaydeepsharma21@gmail.com>
2026-03-11 18:50:50 +0530

    feat:  trigger v2 auto assignment on conversation resolve

--- app/models/concerns/auto_assignment_handler.rb
+++ app/models/concerns/auto_assignment_handler.rb
@@ -11,7 +11,7 @@ module AutoAssignmentHandler
   def run_auto_assignment
     # Round robin kicks in on conversation create & update
     # run it only when conversation status changes to open
-    return unless conversation_status_changed_to_open?
+    return unless conversation_status_changed_to_open? || conversation_status_changed_to_resolved?
     return unless should_run_auto_assignment?
 
     if inbox.auto_assignment_v2_enabled?
@@ -23,6 +23,10 @@ module AutoAssignmentHandler
       allowed_agent_ids = team_id.present? ? team_member_ids_with_capacity : inbox.member_ids_with_assignment_capacity
       AutoAssignment::AgentAssignmentService.new(conversation: self, allowed_agent_ids: allowed_agent_ids).perform
     end
+  end
+
+  def conversation_status_changed_to_resolved?
+    inbox.auto_assignment_v2_enabled? && saved_change_to_status? && resolved?
   end
 
   def team_member_ids_with_capacity
