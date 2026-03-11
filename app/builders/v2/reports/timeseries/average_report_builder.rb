629133b56a743c2f2298f46a7fb2c42fd11d9bb9
Shivam Mishra <scm.mymail@gmail.com>
2026-03-11 19:21:26 +0530

    refactor: share report timeseries builder

--- app/builders/v2/reports/timeseries/average_report_builder.rb
+++ app/builders/v2/reports/timeseries/average_report_builder.rb
@@ -1,9 +1,2 @@
-class V2::Reports::Timeseries::AverageReportBuilder < V2::Reports::Timeseries::BaseTimeseriesBuilder
-  def timeseries
-    data_source.timeseries
-  end
-
-  def aggregate_value
-    data_source.aggregate
-  end
+class V2::Reports::Timeseries::AverageReportBuilder < V2::Reports::Timeseries::ReportBuilder
 end
