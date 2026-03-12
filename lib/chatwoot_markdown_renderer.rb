6778ad3aa02c597c1a6d81162ea690edbace2ba7
hiro <shioih@gravo.jp>
2026-03-12 22:50:45 +0900

    Fix: Preserve multiple blank lines in email rendering (fixes #13338)

--- lib/chatwoot_markdown_renderer.rb
+++ lib/chatwoot_markdown_renderer.rb
@@ -5,8 +5,10 @@ class ChatwootMarkdownRenderer
 
   def render_message
     markdown_renderer = BaseMarkdownRenderer.new
-    doc = CommonMarker.render_doc(@content, :DEFAULT, [:strikethrough])
+    content = preserve_multiple_newlines(@content)
+    doc = CommonMarker.render_doc(content, :DEFAULT, [:strikethrough])
     html = markdown_renderer.render(doc)
+    html = restore_multiple_newlines_as_html(html)
     render_as_html_safe(html)
   end
 
@@ -23,6 +25,17 @@ class ChatwootMarkdownRenderer
   end
 
   private
+
+  def preserve_multiple_newlines(content)
+    content.gsub(/\n{3,}/) do |match|
+      extra_blank_lines = match.length - 2
+      "\n\n" + ("{{BLANK_LINE}}\n\n" * extra_blank_lines)
+    end
+  end
+
+  def restore_multiple_newlines_as_html(html)
+    html.gsub('<p>{{BLANK_LINE}}</p>', '<p><br></p>')
+  end
 
   def render_as_html_safe(html)
     # rubocop:disable Rails/OutputSafety
