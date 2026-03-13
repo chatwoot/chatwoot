9f28358c37cbae106184282ce159754f5a1a55a0
Shivam Mishra <scm.mymail@gmail.com>
2026-03-13 10:04:11 +0530

    feat: don't allow saml binding for first call

--- enterprise/app/builders/saml_user_builder.rb
+++ enterprise/app/builders/saml_user_builder.rb
@@ -16,13 +16,26 @@ class SamlUserBuilder
   def find_or_create_user
     user = User.from_email(auth_attribute('email'))
 
-    if user
-      confirm_user_if_required(user)
-      convert_existing_user_to_saml(user)
-      return user
-    end
+    return create_user unless user
+    return existing_user_for_account(user) if user_belongs_to_account?(user)
 
-    create_user
+    unauthorized_user
+  end
+
+  def existing_user_for_account(user)
+    confirm_user_if_required(user)
+    convert_existing_user_to_saml(user)
+    user
+  end
+
+  def user_belongs_to_account?(user)
+    user.account_users.exists?(account_id: @account_id)
+  end
+
+  def unauthorized_user
+    User.new(email: auth_attribute('email')).tap do |user|
+      user.errors.add(:base, I18n.t('auth.saml.authentication_failed'))
+    end
   end
 
   def confirm_user_if_required(user)
