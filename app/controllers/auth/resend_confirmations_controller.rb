1ea5cc03b730ee88702517160f1ada65a739c7ea
Shivam Mishra <scm.mymail@gmail.com>
2026-03-27 13:33:51 +0530

    feat: Validate email param type before calling strip

--- app/controllers/auth/resend_confirmations_controller.rb
+++ app/controllers/auth/resend_confirmations_controller.rb
@@ -8,7 +8,10 @@ class Auth::ResendConfirmationsController < ActionController::API
   def create
     return head(:ok) unless ChatwootCaptcha.new(params[:h_captcha_client_response]).valid?
 
-    user = User.from_email(params[:email]&.strip&.downcase)
+    email = params[:email]
+    return head(:ok) unless email.is_a?(String)
+
+    user = User.from_email(email.strip.downcase)
     user&.send_confirmation_instructions unless user&.confirmed?
     head :ok
   end
