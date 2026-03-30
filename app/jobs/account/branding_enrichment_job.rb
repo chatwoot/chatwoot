af7391426984fbe3aa1ccb0ee75869291d1cd093
Shivam Mishra <scm.mymail@gmail.com>
2026-03-30 14:33:07 +0530

    feat: add enrichment job

---
+++ app/jobs/account/branding_enrichment_job.rb
@@ -0,0 +1,54 @@
+class Account::BrandingEnrichmentJob < ApplicationJob
+  queue_as :low
+
+  def perform(account_id, email)
+    Account::SignUpEmailValidationService.new(email).perform
+
+    domain = email.split('@').last&.downcase
+    result = WebsiteBrandingService.new(domain).perform
+    return if result.blank?
+
+    account = Account.find(account_id)
+    update_account(account, result)
+  rescue CustomExceptions::Account::InvalidEmail
+    # Skip enrichment for invalid/disposable/blocked emails
+    nil
+  end
+
+  private
+
+  def update_account(account, data)
+    set_account_name(account, data[:business_name])
+    set_account_locale(account, data[:language])
+    set_custom_attr(account, 'website', data[:website])
+    set_custom_attr(account, 'industry', data[:industry_category])
+    set_custom_attr(account, 'social_handles', data[:social_handles]) if data[:social_handles]&.values&.any?(&:present?)
+    set_branding(account, data)
+
+    account.save! if account.changed?
+  end
+
+  def set_account_name(account, name)
+    account.name = name if name.present?
+  end
+
+  def set_account_locale(account, language)
+    return if language.blank? || account.locale != 'en'
+
+    account.locale = language if LANGUAGES_CONFIG.any? { |_k, v| v[:iso_639_1_code] == language }
+  end
+
+  def set_branding(account, data)
+    branding = {}
+    branding[:website] = data[:website] if data[:website].present?
+    branding.merge!(data[:branding]) if data[:branding]&.values&.any?(&:present?)
+    set_custom_attr(account, 'branding', branding) if branding.present?
+  end
+
+  def set_custom_attr(account, key, value)
+    return if value.blank?
+    return if account.custom_attributes[key].present?
+
+    account.custom_attributes[key] = value
+  end
+end
