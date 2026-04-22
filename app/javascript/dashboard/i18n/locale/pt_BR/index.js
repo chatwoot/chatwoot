95a4fd9a639050cd5af9791f2da98dfcc6523d5b
Lorrayne <lorrayneparaisoc@gmail.com>
2026-04-22 10:02:33 -0300

    chore(i18n): synapseos metrics keys (en + pt_BR)

--- app/javascript/dashboard/i18n/locale/pt_BR/index.js
+++ app/javascript/dashboard/i18n/locale/pt_BR/index.js
@@ -41,6 +41,7 @@ import teamsSettings from './teamsSettings.json';
 import webhooks from './webhooks.json';
 import whatsappTemplates from './whatsappTemplates.json';
 import yearInReview from './yearInReview.json';
+import synapseos from './synapseos.json';
 
 export default {
   ...advancedFilters,
@@ -86,4 +87,5 @@ export default {
   ...webhooks,
   ...whatsappTemplates,
   ...yearInReview,
+  ...synapseos,
 };
