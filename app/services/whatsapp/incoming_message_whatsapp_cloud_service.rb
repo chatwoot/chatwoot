f7331676eb016c75a22a60fb2ecf4053b35cfd30
Rorrick Smith <rorrick@bulksms.com>
2026-04-24 16:57:35 +0200

    fix: preserve original filenames for WhatsApp cloud attachments

--- app/services/whatsapp/incoming_message_whatsapp_cloud_service.rb
+++ app/services/whatsapp/incoming_message_whatsapp_cloud_service.rb
@@ -13,8 +13,25 @@ class Whatsapp::IncomingMessageWhatsappCloudService < Whatsapp::IncomingMessageB
       inbox.channel.media_url(attachment_payload[:id]),
       headers: inbox.channel.api_headers
     )
+
     # This url response will be failure if the access token has expired.
     inbox.channel.authorization_error! if url_response.unauthorized?
-    Down.download(url_response.parsed_response['url'], headers: inbox.channel.api_headers) if url_response.success?
+
+    return unless url_response.success?
+
+    downloaded_file = Down.download(url_response.parsed_response['url'], headers: inbox.channel.api_headers)
+    return downloaded_file if attachment_payload[:filename].blank?
+
+    tempfile = Tempfile.new([
+      File.basename(attachment_payload[:filename], '.*'),
+      File.extname(attachment_payload[:filename])
+    ])
+
+    FileUtils.cp(downloaded_file.path, tempfile.path)
+
+    tempfile.define_singleton_method(:content_type) { downloaded_file.content_type }
+    tempfile.define_singleton_method(:original_filename) { attachment_payload[:filename] }
+
+    tempfile
   end
 end
