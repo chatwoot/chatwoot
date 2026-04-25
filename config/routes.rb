b8ef540ab92e5cfae47b3dd5226211d4569b5ff7
Jophat Tamayo <tamay06131993@gmail.com>
2026-04-25 13:17:03 +0800

    feat(help-center): add root sitemap.xml with sitemapindex for all portals
      Adds a /sitemap.xml endpoint that returns a sitemapindex listing all
      active Help Center portal sitemaps. Resolves FATAL routing errors logged
      when crawlers probe the standard sitemap URL.

--- config/routes.rb
+++ config/routes.rb
@@ -552,6 +552,7 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'sitemap.xml', to: 'public/api/v1/sitemaps#index'
   get 'hc/:slug', to: 'public/api/v1/portals#show'
   get 'hc/:slug/sitemap.xml', to: 'public/api/v1/portals#sitemap'
   get 'hc/:slug/:locale', to: 'public/api/v1/portals#show'
