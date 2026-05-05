7860cb0e7883126fe70f46657f9fc57de8b86b67
iamsivin <iamsivin@gmail.com>
2026-05-05 13:40:37 +0530

    chore: Review fix

--- app/javascript/dashboard/helper/markdownEmbeds.js
+++ app/javascript/dashboard/helper/markdownEmbeds.js
@@ -1,7 +1,4 @@
-import yaml from 'js-yaml';
-import yamlSource from '../../../../config/markdown_embeds.yml?raw';
-
-const config = yaml.load(yamlSource);
+import config from '../../../../config/markdown_embeds.yml';
 
 // Gists rely on document.write() and can't render inline in the editor.
 const NON_PREVIEWABLE_EMBEDS = new Set(['github_gist']);
