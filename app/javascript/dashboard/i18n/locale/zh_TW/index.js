ea3f5f4183b7db2cdf798a06293232fd591e184d
Sojan Jose <sojan@pepalo.com>
2026-05-18 17:16:59 +0530

    fix(i18n): load onboarding locale bundles

--- app/javascript/dashboard/i18n/locale/zh_TW/index.js
+++ app/javascript/dashboard/i18n/locale/zh_TW/index.js
@@ -27,6 +27,7 @@ import integrations from './integrations.json';
 import labelsMgmt from './labelsMgmt.json';
 import login from './login.json';
 import macros from './macros.json';
+import onboarding from './onboarding.json';
 import report from './report.json';
 import resetPassword from './resetPassword.json';
 import search from './search.json';
@@ -68,6 +69,7 @@ export default {
   ...labelsMgmt,
   ...login,
   ...macros,
+  ...onboarding,
   ...report,
   ...resetPassword,
   ...search,
