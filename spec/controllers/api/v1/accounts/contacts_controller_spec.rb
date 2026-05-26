160a0709c1c8a4334fca5bd2cee405ff2cf2472e
Sojan Jose <sojan@pepalo.com>
2026-05-27 01:23:20 +0530

    test(companies): align contact company specs

--- spec/controllers/api/v1/accounts/contacts_controller_spec.rb
+++ spec/controllers/api/v1/accounts/contacts_controller_spec.rb
@@ -118,8 +118,9 @@ RSpec.describe 'Contacts API', type: :request do
 
         expect(response).to have_http_status(:success)
         response_body = response.parsed_body
-        expect(response_body['payload'].first['email']).to eq(contact_1.email)
-        expect(response_body['payload'].first['id']).to eq(contact_1.id)
+        expect(response_body['payload'].first['email']).to eq(contact.email)
+        expect(response_body['payload'].first['id']).to eq(contact.id)
+        expect(response_body['payload'].second['id']).to eq(contact_1.id)
         expect(response_body['payload'].last['email']).to eq(contact_4.email)
       end
 
