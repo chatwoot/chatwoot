36a054262420f1380c07cf9842f5dc32cc7a0abd
Sojan Jose <sojan@pepalo.com>
2026-05-31 01:45:15 +0530

    refactor(portal): clean up search results view

--- enterprise/app/controllers/enterprise/public/api/v1/portals/search_controller.rb
+++ enterprise/app/controllers/enterprise/public/api/v1/portals/search_controller.rb
@@ -2,12 +2,8 @@ module Enterprise::Public::Api::V1::Portals::SearchController
   private
 
   def search_articles
-    return super if @query.blank?
+    return super if @query.blank? || !@portal.account.feature_enabled?('help_center_embedding_search')
 
-    if @portal.account.feature_enabled?('help_center_embedding_search')
-      @articles = @articles.vector_search(search_params.merge(account_id: @portal.account_id))
-    else
-      super
-    end
+    @articles = @articles.vector_search(search_params.merge(account_id: @portal.account_id))
   end
 end
