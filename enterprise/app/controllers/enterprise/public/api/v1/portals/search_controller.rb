5cda1acd8a4544cd1749d02cd3df3b825644f8ea
Sojan Jose <sojan@pepalo.com>
2026-05-31 02:41:14 +0530

    fix(portal): address search review comments

--- enterprise/app/controllers/enterprise/public/api/v1/portals/search_controller.rb
+++ enterprise/app/controllers/enterprise/public/api/v1/portals/search_controller.rb
@@ -4,6 +4,6 @@ module Enterprise::Public::Api::V1::Portals::SearchController
   def search_articles
     return super if @query.blank? || !@portal.account.feature_enabled?('help_center_embedding_search')
 
-    @articles = @articles.vector_search(search_params.merge(account_id: @portal.account_id))
+    @articles = @articles.vector_search(search_params.merge(account_id: @portal.account_id, limit: nil))
   end
 end
