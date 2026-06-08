c2a5e7bc451244b8bf8657b765f3a0ab71e1654a
Muhsin <12408980+muhsin-k@users.noreply.github.com>
2026-06-08 14:55:06 +0400

    Update send_on_facebook_service_spec.rb

--- spec/services/facebook/send_on_facebook_service_spec.rb
+++ spec/services/facebook/send_on_facebook_service_spec.rb
@@ -93,9 +93,10 @@ describe Facebook::SendOnFacebookService do
         message = create(:message, message_type: 'outgoing', inbox: facebook_inbox, account: account, conversation: conversation)
         described_class.new(message: message).perform
         expect(bot).to have_received(:deliver).with(
-          hash_including(messaging_type: 'RESPONSE').and(hash_excluding(:tag)),
+          hash_including(messaging_type: 'RESPONSE'),
           { page_id: facebook_channel.page_id }
         )
+        expect(bot).not_to have_received(:deliver).with(hash_including(:tag), anything)
       end
 
       it 'sends with HUMAN_AGENT tag when ENABLE_MESSENGER_CHANNEL_HUMAN_AGENT is enabled' do
