86b1493412989d6b314a10b634896286d9dbd457
mix5003 <mix5003@gmail.com>
2026-06-10 13:15:59 +0700

    fix: prevent line create double conversation by lock channel

--- app/jobs/webhooks/line_events_job.rb
+++ app/jobs/webhooks/line_events_job.rb
@@ -1,12 +1,19 @@
-class Webhooks::LineEventsJob < ApplicationJob
+class Webhooks::LineEventsJob < MutexApplicationJob
   queue_as :default
+  retry_on LockAcquisitionError, wait: 1.second, attempts: 8
 
   def perform(params: {}, signature: '', post_body: '')
     @params = params
     return unless valid_event_payload?
     return unless valid_post_body?(post_body, signature)
 
-    Line::IncomingMessageService.new(inbox: @channel.inbox, params: @params['line'].with_indifferent_access).perform
+    key = format(::Redis::Alfred::LINE_MESSAGE_MUTEX, line_channel_id: @params[:line_channel_id])
+
+    with_lock(key, 5.minutes) do
+      Line::IncomingMessageService.new(inbox: @channel.inbox, params: @params['line'].with_indifferent_access).perform
+    end
+  rescue LockAcquisitionError
+    Rails.logger.error "Lock failed for line channel - #{@params[:line_channel_id]}"
   end
 
   private
