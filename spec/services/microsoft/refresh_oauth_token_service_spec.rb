027753215d4b47d12884bafac7a2c3938cd90919
Augusto Xavier <augusto.cesar@mpgo.mp.br>
2026-06-10 02:10:37 -0300

    Revert "test: expect OAuth2::Error from oauth token refresh services"
    This reverts commit 03643e2485fce98b3893bbf16fe7b1aceb71b734.

--- spec/services/microsoft/refresh_oauth_token_service_spec.rb
+++ spec/services/microsoft/refresh_oauth_token_service_spec.rb
@@ -82,7 +82,7 @@ RSpec.describe Microsoft::RefreshOauthTokenService do
 
         expect do
           described_class.new(channel: microsoft_channel).access_token
-        end.to raise_error(OAuth2::Error, /A refresh_token is not available/)
+        end.to raise_error(RuntimeError, 'A refresh_token is not available')
       end
     end
   end
