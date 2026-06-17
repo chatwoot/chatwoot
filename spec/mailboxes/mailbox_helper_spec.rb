6fd98655a6b1eb3afa0e6a1728d6177de2293ade
mvanhorn <mvanhorn@gmail.com>
2026-06-17 09:05:01 -0700

    test(mailbox): satisfy rubocop in inline-image spec
    Use instance_double(Mail::Part) for attachment stubs and build the fixture
    mail via assignment instead of a Mail.new block chain, clearing
    RSpec/VerifiedDoubles, RSpec/VariableName, and Style/MultilineBlockChain.

--- spec/mailboxes/mailbox_helper_spec.rb
+++ spec/mailboxes/mailbox_helper_spec.rb
@@ -121,28 +121,28 @@ RSpec.describe MailboxHelper do
     end
 
     it 'detects inline images when the HTML body references the CID' do
-      original_attachment = double(content_type: 'image/png', inline?: true, cid: 'image001.jpg@test')
+      original_attachment = instance_double(Mail::Part, content_type: 'image/png', inline?: true, cid: 'image001.jpg@test')
       helper_instance.instance_variable_set(:@html_content, '<img src="cid:image001.jpg@test">')
 
       expect(helper_instance.send(:inline_attachment?, { original: original_attachment })).to be true
     end
 
     it 'does not detect inline-marked images when the HTML body does not reference the CID' do
-      original_attachment = double(content_type: 'image/png', inline?: true, cid: 'image001.jpg@test')
+      original_attachment = instance_double(Mail::Part, content_type: 'image/png', inline?: true, cid: 'image001.jpg@test')
       helper_instance.instance_variable_set(:@html_content, '<attachment id="image001.jpg@test"></attachment>')
 
       expect(helper_instance.send(:inline_attachment?, { original: original_attachment })).to be false
     end
 
     it 'does not detect non-image inline parts as inline attachments' do
-      original_attachment = double(content_type: 'application/pdf', inline?: true, cid: 'document001@test')
+      original_attachment = instance_double(Mail::Part, content_type: 'application/pdf', inline?: true, cid: 'document001@test')
       helper_instance.instance_variable_set(:@html_content, '<img src="cid:document001@test">')
 
       expect(helper_instance.send(:inline_attachment?, { original: original_attachment })).to be false
     end
 
     it 'does not raise when HTML content is missing' do
-      original_attachment = double(content_type: 'image/png', inline?: true, cid: 'image001.jpg@test')
+      original_attachment = instance_double(Mail::Part, content_type: 'image/png', inline?: true, cid: 'image001.jpg@test')
       helper_instance.instance_variable_set(:@html_content, nil)
 
       expect { helper_instance.send(:inline_attachment?, { original: original_attachment }) }.not_to raise_error
@@ -197,28 +197,29 @@ RSpec.describe MailboxHelper do
 
     context 'when an inline-marked image CID is not referenced in the HTML body' do
       let(:mail) do
-        Mail.new do
-          from 'Sender <sender@example.com>'
-          to 'Inbox <inbox@example.com>'
-          subject 'Inline image not referenced'
-          message_id '<unreferenced-inline-image@example.com>'
-          text_part do
-            body 'This is a plain text version of the message.'
-          end
-          html_part do
-            content_type 'text/html; charset=UTF-8'
-            body '<html><body><p>Apple inline attachment</p><attachment id="image001.jpg@test"></attachment></body></html>'
-          end
-        end.tap do |message|
-          message.add_part(
-            Mail::Part.new do
-              content_type 'image/png'
-              content_disposition 'inline; filename="image001.png"'
-              content_id '<image001.jpg@test>'
-              body File.binread(Rails.root.join('spec/assets/avatar.png'))
-            end
-          )
+        text_part = Mail::Part.new do
+          body 'This is a plain text version of the message.'
         end
+        html_part = Mail::Part.new do
+          content_type 'text/html; charset=UTF-8'
+          body '<html><body><p>Apple inline attachment</p><attachment id="image001.jpg@test"></attachment></body></html>'
+        end
+        inline_image_part = Mail::Part.new do
+          content_type 'image/png'
+          content_disposition 'inline; filename="image001.png"'
+          content_id '<image001.jpg@test>'
+          body File.binread(Rails.root.join('spec/assets/avatar.png'))
+        end
+
+        message = Mail.new
+        message.from = 'Sender <sender@example.com>'
+        message.to = 'Inbox <inbox@example.com>'
+        message.subject = 'Inline image not referenced'
+        message.message_id = '<unreferenced-inline-image@example.com>'
+        message.text_part = text_part
+        message.html_part = html_part
+        message.add_part(inline_image_part)
+        message
       end
 
       it 'processes the image as a regular attachment' do
