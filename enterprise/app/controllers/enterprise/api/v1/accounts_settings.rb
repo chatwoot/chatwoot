323484d41df120a582e3ecb5a6d9bbdc7a0dfde3
Sojan Jose <sojan@pepalo.com>
2026-06-17 12:25:56 -0700

    fix: limit attribution capture to cloud signup path

--- enterprise/app/controllers/enterprise/api/v1/accounts_settings.rb
+++ enterprise/app/controllers/enterprise/api/v1/accounts_settings.rb
@@ -1,18 +1,5 @@
 module Enterprise::Api::V1::AccountsSettings
-  def create
-    super
-    record_marketing_attribution
-  end
-
   private
-
-  def record_marketing_attribution
-    return if current_user.present?
-
-    Internal::Accounts::MarketingAttributionService.new(account: @account, cookies: cookies).perform
-  rescue StandardError => e
-    ChatwootExceptionTracker.new(e).capture_exception
-  end
 
   def permitted_settings_attributes
     super + [{ conversation_required_attributes: [] }]
--- enterprise/app/controllers/enterprise/devise_overrides/omniauth_callbacks_controller.rb
+++ enterprise/app/controllers/enterprise/devise_overrides/omniauth_callbacks_controller.rb
@@ -27,20 +27,7 @@ module Enterprise::DeviseOverrides::OmniauthCallbacksController
     end
   end
 
-  def create_account_for_user
-    super
-    record_marketing_attribution
-  end
-
   private
-
-  def record_marketing_attribution
-    return if @account.blank?
-
-    Internal::Accounts::MarketingAttributionService.new(account: @account, cookies: cookies).perform
-  rescue StandardError => e
-    ChatwootExceptionTracker.new(e).capture_exception
-  end
 
   def handle_saml_auth
     account_id = extract_saml_account_id
