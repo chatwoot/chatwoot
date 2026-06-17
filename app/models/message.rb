af8d1eb39cdcea4a34877cefce24ac720bbbbe02
Kevin Joshua <kingkevinjosh7@gmail.com>
2026-06-18 00:25:21 +0530

    style: fix rubocop violations

--- app/models/message.rb
+++ app/models/message.rb
@@ -107,7 +107,7 @@ class Message < ApplicationRecord
   # [:deleted] : Used to denote whether the message was deleted by the agent
   # Pass-through coder to allow `store` to provide HashWithIndifferentAccess and dirty tracking
   # without performing JSON stringification, since the underlying column is already `json`/`jsonb`.
-  class PassThroughCoder
+  PASS_THROUGH_CODER = Module.new do
     def self.dump(obj)
       obj
     end
@@ -116,15 +116,16 @@ class Message < ApplicationRecord
       obj || {}
     end
   end
+  private_constant :PASS_THROUGH_CODER
 
   # [:external_created_at] : Can specify if the message was created at a different timestamp externally
   # [:external_error : Can specify if the message creation failed due to an error at external API
   # [:data] : Used for structured content types such as voice_call
   store :content_attributes, accessors: [:submitted_email, :items, :submitted_values, :email, :in_reply_to, :deleted,
                                          :external_created_at, :story_sender, :story_id, :external_error,
-                                         :translations, :in_reply_to_external_id, :is_unsupported, :data], coder: PassThroughCoder
+                                         :translations, :in_reply_to_external_id, :is_unsupported, :data], coder: PASS_THROUGH_CODER
 
-  store :external_source_ids, accessors: [:slack], prefix: :external_source_id, coder: PassThroughCoder
+  store :external_source_ids, accessors: [:slack], prefix: :external_source_id, coder: PASS_THROUGH_CODER
 
   scope :created_since, ->(datetime) { where('created_at > ?', datetime) }
   scope :chat, -> { where.not(message_type: :activity).where(private: false) }
