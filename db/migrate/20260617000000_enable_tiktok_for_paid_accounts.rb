75ffb849fb7c82fb748d639f7ce68c3aada21044
Muhsin <12408980+muhsin-k@users.noreply.github.com>
2026-06-19 09:04:58 +0400

    chore: remove tiktok paid-account migration in favor of console script

--- db/migrate/20260617000000_enable_tiktok_for_paid_accounts.rb
+++
@@ -1,9 +0,0 @@
-class EnableTiktokForPaidAccounts < ActiveRecord::Migration[7.0]
-  PAID_PLAN_NAMES = %w[Startups Business Enterprise].freeze
-
-  def up
-    Account.where("custom_attributes ->> 'plan_name' IN (?)", PAID_PLAN_NAMES).find_in_batches(batch_size: 100) do |accounts|
-      accounts.each { |account| account.enable_features!('channel_tiktok') }
-    end
-  end
-end
