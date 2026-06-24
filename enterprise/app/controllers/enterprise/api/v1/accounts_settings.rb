266755d521d600aac02611db6a0541277c83fd2d
Sojan Jose <sojan@pepalo.com>
2026-06-23 19:46:35 -0700

    Track cloud conversion events

--- enterprise/app/controllers/enterprise/api/v1/accounts_settings.rb
+++ enterprise/app/controllers/enterprise/api/v1/accounts_settings.rb
@@ -2,6 +2,7 @@ module Enterprise::Api::V1::AccountsSettings
   def create
     super
     record_marketing_attribution
+    enqueue_marketing_signup_conversion
   end
 
   private
@@ -13,6 +14,15 @@ module Enterprise::Api::V1::AccountsSettings
     Internal::Accounts::MarketingAttributionService.new(account: @account, cookies: cookies).perform
   rescue StandardError => e
     ChatwootExceptionTracker.new(e).capture_exception
+  end
+
+  def enqueue_marketing_signup_conversion
+    return if current_user.present?
+    return if @account.blank?
+
+    Internal::Accounts::MarketingConversionTrackingJob.perform_later(@account.id, 'cloud_signup', @account.created_at.iso8601)
+  rescue StandardError => e
+    ChatwootExceptionTracker.new(e, account: @account).capture_exception
   end
 
   def permitted_settings_attributes
