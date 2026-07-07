1705161e9945435bb1d2f8280eafff5964183973
Vishnu Narayanan <iamwishnu@gmail.com>
2026-07-07 13:27:03 +0530

    fix(hmac): verify contact inbox hmac server-side

--- spec/controllers/api/v1/accounts/contacts/contact_inboxes_controller_spec.rb
+++ spec/controllers/api/v1/accounts/contacts/contact_inboxes_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe '/api/v1/accounts/{account.id}/contacts/:id/contact_inboxes', type: :request do
   let(:account) { create(:account) }
-  let(:contact) { create(:contact, account: account, email: 'f.o.o.b.a.r@gmail.com') }
+  let(:contact) { create(:contact, account: account, email: 'f.o.o.b.a.r@gmail.com', identifier: 'contact-identifier') }
   let(:channel_twilio_sms) { create(:channel_twilio_sms, account: account) }
   let(:channel_email) { create(:channel_email, account: account) }
   let(:channel_api) { create(:channel_api, account: account) }
@@ -45,11 +45,28 @@ RSpec.describe '/api/v1/accounts/{account.id}/contacts/:id/contact_inboxes', typ
         expect(contact.reload.contact_inboxes.map(&:inbox_id)).to include(channel_email.inbox.id)
       end
 
-      it 'creates an hmac verified contact inbox' do
+      it 'does not trust client supplied hmac_verified param' do
         create(:inbox_member, inbox: channel_api.inbox, user: agent)
         expect do
           post "/api/v1/accounts/#{account.id}/contacts/#{contact.id}/contact_inboxes",
                params: { inbox_id: channel_api.inbox.id, hmac_verified: true },
+               headers: agent.create_new_auth_token,
+               as: :json
+        end.to change(ContactInbox, :count).by(1)
+
+        expect(response).to have_http_status(:success)
+        contact_inbox = contact.reload.contact_inboxes.find_by(inbox_id: channel_api.inbox.id)
+        expect(contact_inbox).to be_present
+        expect(contact_inbox.hmac_verified).to be(false)
+      end
+
+      it 'creates an hmac verified contact inbox with a valid identifier hash' do
+        create(:inbox_member, inbox: channel_api.inbox, user: agent)
+        identifier_hash = OpenSSL::HMAC.hexdigest('sha256', channel_api.hmac_token, contact.identifier)
+
+        expect do
+          post "/api/v1/accounts/#{account.id}/contacts/#{contact.id}/contact_inboxes",
+               params: { inbox_id: channel_api.inbox.id, identifier: contact.identifier, identifier_hash: identifier_hash },
                headers: agent.create_new_auth_token,
                as: :json
         end.to change(ContactInbox, :count).by(1)
