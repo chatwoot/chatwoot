8846ef50a736a426a6aee7501e95228e095fc3f7
Vishnu Narayanan <iamwishnu@gmail.com>
2026-07-07 13:27:09 +0530

    fix(hmac): compare identifier hashes securely

--- app/controllers/api/v1/widget/contacts_controller.rb
+++ app/controllers/api/v1/widget/contacts_controller.rb
@@ -62,11 +62,15 @@ class Api::V1::Widget::ContactsController < Api::V1::Widget::BaseController
   end
 
   def valid_hmac?
-    params[:identifier_hash] == OpenSSL::HMAC.hexdigest(
+    expected_hash = OpenSSL::HMAC.hexdigest(
       'sha256',
       @web_widget.hmac_token,
       params[:identifier].to_s
     )
+    identifier_hash = params[:identifier_hash].to_s
+    return false unless identifier_hash.bytesize == expected_hash.bytesize
+
+    ActiveSupport::SecurityUtils.secure_compare(identifier_hash, expected_hash)
   end
 
   def permitted_params
