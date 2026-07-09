5f5634ced35acb229fad8e0d2b7f48d9260aa9bc
Muhsin <12408980+muhsin-k@users.noreply.github.com>
2026-07-09 13:05:12 +0400

    feat: add support action to suspended account page

--- app/javascript/dashboard/routes/dashboard/suspended/Index.vue
+++ app/javascript/dashboard/routes/dashboard/suspended/Index.vue
@@ -1,10 +1,17 @@
 <script setup>
 import EmptyState from 'dashboard/components/widgets/EmptyState.vue';
+import NextButton from 'dashboard/components-next/button/Button.vue';
 import { onMounted } from 'vue';
 
 const toggleSupportWidgetVisibility = () => {
   if (window.$chatwoot) {
     window.$chatwoot.toggleBubbleVisibility('show');
+  }
+};
+
+const toggleSupportWidget = () => {
+  if (window.$chatwoot) {
+    window.$chatwoot.toggle();
   }
 };
 
@@ -25,6 +32,14 @@ onMounted(() => {
     <EmptyState
       :title="$t('APP_GLOBAL.ACCOUNT_SUSPENDED.TITLE')"
       :message="$t('APP_GLOBAL.ACCOUNT_SUSPENDED.MESSAGE')"
-    />
+    >
+      <div class="flex justify-center">
+        <NextButton
+          icon="i-lucide-life-buoy"
+          :label="$t('SIDEBAR_ITEMS.CONTACT_SUPPORT')"
+          @click="toggleSupportWidget"
+        />
+      </div>
+    </EmptyState>
   </div>
 </template>
