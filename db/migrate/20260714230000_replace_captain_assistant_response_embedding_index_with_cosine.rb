f22e66b869d316c75c866281648b051c9e89f257
aakashb95 <aakashbakhle@gmail.com>
2026-07-14 23:17:34 +0530

    fix(captain): require iterative vector scans

--- db/migrate/20260714230000_replace_captain_assistant_response_embedding_index_with_cosine.rb
+++ db/migrate/20260714230000_replace_captain_assistant_response_embedding_index_with_cosine.rb
@@ -3,8 +3,11 @@ class ReplaceCaptainAssistantResponseEmbeddingIndexWithCosine < ActiveRecord::Mi
 
   OLD_INDEX_NAME = 'vector_idx_knowledge_entries_embedding'.freeze
   NEW_INDEX_NAME = 'vector_idx_captain_assistant_responses_embedding_cosine'.freeze
+  MINIMUM_PGVECTOR_VERSION = Gem::Version.new('0.8.0')
 
   def up
+    ensure_iterative_scans_supported!
+
     add_index :captain_assistant_responses,
               :embedding,
               using: :ivfflat,
@@ -22,5 +25,14 @@ class ReplaceCaptainAssistantResponseEmbeddingIndexWithCosine < ActiveRecord::Mi
               name: OLD_INDEX_NAME,
               algorithm: :concurrently
     remove_index :captain_assistant_responses, name: NEW_INDEX_NAME, algorithm: :concurrently
+  end
+
+  private
+
+  def ensure_iterative_scans_supported!
+    installed_version = Gem::Version.new(select_value("SELECT extversion FROM pg_extension WHERE extname = 'vector'"))
+    return if installed_version >= MINIMUM_PGVECTOR_VERSION
+
+    raise StandardError, "pgvector #{MINIMUM_PGVECTOR_VERSION} or newer is required for filtered FAQ search"
   end
 end
