289a3c19d3b5856b323569e7f824adffae73ced9
Vishnu Narayanan <iamwishnu@gmail.com>
2026-07-28 16:04:22 +0530

    fix: add if_not_exists to conversations.created_at index migration

--- db/migrate/20260724000100_add_index_to_conversations_created_at.rb
+++ db/migrate/20260724000100_add_index_to_conversations_created_at.rb
@@ -2,6 +2,6 @@ class AddIndexToConversationsCreatedAt < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
   def change
-    add_index :conversations, :created_at, algorithm: :concurrently
+    add_index :conversations, :created_at, algorithm: :concurrently, if_not_exists: true
   end
 end
