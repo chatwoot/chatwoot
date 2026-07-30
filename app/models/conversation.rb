0c606babea02688917eb952979cd42b13f5b4b81
Tanmay Deep Sharma <32020192+tds-1@users.noreply.github.com>
2026-07-30 16:04:17 +0530

    feat: time based automation (#15022)
    ## Description
    
    Add automations that trigger based on how long a conversation has been
    in a given state.
    
    ## Type of change
    
    - [ ] New feature (non-breaking change which adds functionality)
    
    ## How Has This Been Tested?
    
    - UI flows 
    - Specs (https://github.com/chatwoot/chatwoot/pull/15021) 
    
    ## Checklist:
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented on my code, particularly in hard-to-understand
    areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged and published in downstream
    modules
    
    ---------
    
    Co-authored-by: Sony Mathew <sony@chatwoot.com>
    Co-authored-by: Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
    Co-authored-by: iamsivin <iamsivin@gmail.com>

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -15,6 +15,7 @@
 #  priority               :integer
 #  snoozed_until          :datetime
 #  status                 :integer          default("open"), not null
+#  status_changed_at      :datetime
 #  uuid                   :uuid             not null
 #  waiting_since          :datetime
 #  created_at             :datetime         not null
@@ -125,8 +126,10 @@ class Conversation < ApplicationRecord
   has_many :notifications, as: :primary_actor, dependent: :destroy_async
   has_many :attachments, through: :messages
   has_many :reporting_events, dependent: :destroy_async
+  has_many :automation_rule_pending_executions, dependent: :delete_all
 
   before_save :ensure_snooze_until_reset
+  before_save :set_status_changed_at
   before_create :determine_conversation_status
   before_create :ensure_waiting_since
 
@@ -276,6 +279,10 @@ class Conversation < ApplicationRecord
 
   def ensure_snooze_until_reset
     self.snoozed_until = nil unless snoozed?
+  end
+
+  def set_status_changed_at
+    self.status_changed_at = Time.current if new_record? || status_changed?
   end
 
   def ensure_waiting_since
