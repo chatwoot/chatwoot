1534a3ef35ee8222a5fbd67b8154a15eab162912
aakashb95 <aakashbakhle@gmail.com>
2026-08-03 16:33:14 +0530

    feat(captain): add inactivity timer backend

--- enterprise/app/controllers/api/v1/accounts/captain/assistants_controller.rb
+++ enterprise/app/controllers/api/v1/accounts/captain/assistants_controller.rb
@@ -129,6 +129,9 @@ class Api::V1::Accounts::Captain::AssistantsController < Api::V1::Accounts::Base
       :feature_contact_attributes, :welcome_message, :handoff_message,
       :resolution_message, :instructions, :temperature, :auto_resolve_mode
     ]
+    if Current.account.feature_enabled?('captain_integration_v2')
+      assistant_config_attributes += [:auto_resolve_after, :send_inactivity_resolution_message]
+    end
 
     permitted = params.require(:assistant).permit(:name, :description,
                                                   config: assistant_config_attributes)
