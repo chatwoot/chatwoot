91b1925823725cd519fddcddad49802227cf532c
Sojan Jose <sojan@pepalo.com>
2026-08-05 00:38:03 -0700

    refactor: extract inbox bot status

--- enterprise/app/models/enterprise/inbox.rb
+++ enterprise/app/models/enterprise/inbox.rb
@@ -8,12 +8,8 @@ module Enterprise::Inbox
     super - overloaded_agent_ids
   end
 
-  def active_bot?(include_captain: true)
-    super() || (include_captain && captain_active?)
-  end
-
-  def external_bot_active?
-    active_bot?(include_captain: false)
+  def active_bot?
+    super || captain_active?
   end
 
   def captain_active?
