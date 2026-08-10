82fb735d6d5ae30cce4fa7e0e1cad3df15fa60f0
Muhsin <12408980+muhsin-k@users.noreply.github.com>
2026-08-10 12:44:40 +0530

    fix(telegram): batch business config backfill

--- app/jobs/migration/sync_telegram_business_config_job.rb
+++ app/jobs/migration/sync_telegram_business_config_job.rb
@@ -1,7 +1,13 @@
 class Migration::SyncTelegramBusinessConfigJob < ApplicationJob
+  BATCH_SIZE = 100
+
   queue_as :async_database_migration
 
-  def perform
-    Channel::Telegram.find_each(&:refresh_business_config!)
+  def perform(after_id: 0)
+    channels = Channel::Telegram.where('id > ?', after_id).order(:id).limit(BATCH_SIZE + 1).to_a
+    batch = channels.first(BATCH_SIZE)
+
+    batch.each(&:refresh_business_config!)
+    self.class.perform_later(after_id: batch.last.id) if channels.size > BATCH_SIZE
   end
 end
--- spec/jobs/migration/sync_telegram_business_config_job_spec.rb
+++ spec/jobs/migration/sync_telegram_business_config_job_spec.rb
@@ -1,12 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Migration::SyncTelegramBusinessConfigJob do
-  it 'refreshes Telegram Business capability for every existing channel' do
-    channel = instance_double(Channel::Telegram)
+  before do
+    stub_const("#{described_class}::BATCH_SIZE", 2)
+  end
 
-    allow(Channel::Telegram).to receive(:find_each).and_yield(channel)
-    expect(channel).to receive(:refresh_business_config!)
+  it 'refreshes one batch and enqueues the next batch from the last processed channel' do
+    first_channel = instance_double(Channel::Telegram, id: 10)
+    second_channel = instance_double(Channel::Telegram, id: 20)
+    remaining_channel = instance_double(Channel::Telegram, id: 30)
+    scope = double
 
-    described_class.perform_now
+    allow(Channel::Telegram).to receive(:where).with('id > ?', 0).and_return(scope)
+    allow(scope).to receive(:order).with(:id).and_return(scope)
+    allow(scope).to receive(:limit).with(3).and_return(scope)
+    allow(scope).to receive(:to_a).and_return([first_channel, second_channel, remaining_channel])
+    allow(first_channel).to receive(:refresh_business_config!)
+    allow(second_channel).to receive(:refresh_business_config!)
+    allow(remaining_channel).to receive(:refresh_business_config!)
+
+    expect { described_class.perform_now }
+      .to have_enqueued_job(described_class).with(after_id: 20).on_queue('async_database_migration')
+
+    expect(first_channel).to have_received(:refresh_business_config!)
+    expect(second_channel).to have_received(:refresh_business_config!)
+    expect(remaining_channel).not_to have_received(:refresh_business_config!)
+  end
+
+  it 'does not enqueue another job after the final batch' do
+    channel = instance_double(Channel::Telegram, id: 30, refresh_business_config!: true)
+    scope = double
+
+    allow(Channel::Telegram).to receive(:where).with('id > ?', 20).and_return(scope)
+    allow(scope).to receive(:order).with(:id).and_return(scope)
+    allow(scope).to receive(:limit).with(3).and_return(scope)
+    allow(scope).to receive(:to_a).and_return([channel])
+
+    expect { described_class.perform_now(after_id: 20) }.not_to have_enqueued_job(described_class)
+    expect(channel).to have_received(:refresh_business_config!)
   end
 end
