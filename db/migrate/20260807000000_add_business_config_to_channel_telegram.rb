4bbe6c5f9bfb111748223b77b912106f02c989ad
Muhsin <12408980+muhsin-k@users.noreply.github.com>
2026-08-10 12:52:21 +0530

    refactor(telegram): use reversible business config migration

--- db/migrate/20260807000000_add_business_config_to_channel_telegram.rb
+++ db/migrate/20260807000000_add_business_config_to_channel_telegram.rb
@@ -1,15 +1,11 @@
 class AddBusinessConfigToChannelTelegram < ActiveRecord::Migration[7.1]
-  def up
+  def change
     add_column :channel_telegram, :business_config, :jsonb, default: {}, null: false
     add_column :channel_telegram, :business_config_checked_at, :datetime
     add_column :channel_telegram, :business_config_error, :string, limit: 500
 
-    Migration::SyncTelegramBusinessConfigJob.perform_later
-  end
-
-  def down
-    remove_column :channel_telegram, :business_config_error
-    remove_column :channel_telegram, :business_config_checked_at
-    remove_column :channel_telegram, :business_config
+    reversible do |direction|
+      direction.up { Migration::SyncTelegramBusinessConfigJob.perform_later }
+    end
   end
 end
