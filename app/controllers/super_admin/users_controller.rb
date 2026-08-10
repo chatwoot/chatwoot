9ae60943e2f08df178fc8004e8b0a4f5a9401faa
Vishnu Narayanan <iamwishnu@gmail.com>
2026-08-10 16:51:50 +0530

    feat: add resend confirmation email action in super admin

--- app/controllers/super_admin/users_controller.rb
+++ app/controllers/super_admin/users_controller.rb
@@ -52,6 +52,16 @@ class SuperAdmin::UsersController < SuperAdmin::ApplicationController
     redirect_back(fallback_location: super_admin_users_path)
   end
 
+  def resend_confirmation
+    user = requested_resource
+    if user.confirmed?
+      redirect_back(fallback_location: super_admin_user_path(user), alert: 'User is already confirmed.')
+    else
+      user.send_confirmation_instructions
+      redirect_back(fallback_location: super_admin_user_path(user), notice: 'Confirmation email sent.')
+    end
+  end
+
   def scoped_resource
     resource_class.with_attached_avatar
   end
