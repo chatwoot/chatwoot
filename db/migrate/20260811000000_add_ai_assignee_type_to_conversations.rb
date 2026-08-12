c3c2025812f5b4f8aa4318e6a16911d2dba26969
Sojan Jose <sojan@pepalo.com>
2026-08-11 20:12:36 -0700

    refactor: combine AI owner migration

--- db/migrate/20260811000000_add_ai_assignee_type_to_conversations.rb
+++ db/migrate/20260811000000_add_ai_assignee_type_to_conversations.rb
@@ -1,5 +1,15 @@
 class AddAiAssigneeTypeToConversations < ActiveRecord::Migration[7.1]
-  def change
+  disable_ddl_transaction!
+
+  def up
     add_column :conversations, :ai_assignee_type, :string
+
+    conversation_model = Class.new(ActiveRecord::Base) { self.table_name = 'conversations' }
+
+    # rubocop:disable Rails/SkipsModelValidations
+    conversation_model.where.not(assignee_agent_bot_id: nil).in_batches(of: 1000) do |conversations|
+      conversations.update_all(ai_assignee_type: 'AgentBot')
+    end
+    # rubocop:enable Rails/SkipsModelValidations
   end
 end
