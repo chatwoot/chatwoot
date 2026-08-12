d604263f159aeffe4635f1bef08c08b47cad9999
Sojan Jose <sojan@pepalo.com>
2026-08-11 20:19:19 -0700

    refactor: use conversation model in migration

--- db/migrate/20260811000000_add_ai_assignee_type_to_conversations.rb
+++ db/migrate/20260811000000_add_ai_assignee_type_to_conversations.rb
@@ -4,10 +4,8 @@ class AddAiAssigneeTypeToConversations < ActiveRecord::Migration[7.1]
   def up
     add_column :conversations, :ai_assignee_type, :string
 
-    conversation_model = Class.new(ActiveRecord::Base) { self.table_name = 'conversations' }
-
     # rubocop:disable Rails/SkipsModelValidations
-    conversation_model.where.not(assignee_agent_bot_id: nil).in_batches(of: 1000) do |conversations|
+    Conversation.where.not(assignee_agent_bot_id: nil).in_batches(of: 1000) do |conversations|
       conversations.update_all(ai_assignee_type: 'AgentBot')
     end
     # rubocop:enable Rails/SkipsModelValidations
