0d846707f1ca511cb43c421921f864df4c8b3d08
Sojan Jose <sojan@pepalo.com>
2026-08-11 19:39:56 -0700

    refactor: batch AI assignee type backfill

--- db/migrate/20260811000000_add_ai_assignee_type_to_conversations.rb
+++ db/migrate/20260811000000_add_ai_assignee_type_to_conversations.rb
@@ -1,15 +1,5 @@
 class AddAiAssigneeTypeToConversations < ActiveRecord::Migration[7.1]
   def change
     add_column :conversations, :ai_assignee_type, :string
-
-    reversible do |direction|
-      direction.up do
-        execute <<~SQL.squish
-          UPDATE conversations
-          SET ai_assignee_type = 'AgentBot'
-          WHERE assignee_agent_bot_id IS NOT NULL
-        SQL
-      end
-    end
   end
 end
