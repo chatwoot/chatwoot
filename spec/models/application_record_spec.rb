a04763e6fd0ce249f9288bcd6792342260551b36
Leandro G. Gehlen <leandrogehlen@gmail.com>
2026-08-18 09:57:03 -0300

    test: add account and contact associations to application record specs

--- spec/models/application_record_spec.rb
+++ spec/models/application_record_spec.rb
@@ -3,6 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationRecord do
+  let(:account) { create(:account) }
+  let(:contact) { create(:contact, account: account) }
+
   describe 'MAX_TEXT_COLUMN_LENGTH' do
     it 'defaults to 20000' do
       expect(described_class::MAX_TEXT_COLUMN_LENGTH).to eq(20_000)
@@ -12,7 +15,7 @@ RSpec.describe ApplicationRecord do
   describe '#validates_column_content_length' do
     context 'when content is within the configured limit' do
       it 'is valid' do
-        note = build(:note, content: 'a' * described_class::MAX_TEXT_COLUMN_LENGTH)
+        note = build(:note, account: account, contact: contact, content: 'a' * described_class::MAX_TEXT_COLUMN_LENGTH)
 
         expect(note).to be_valid
       end
@@ -20,7 +23,7 @@ RSpec.describe ApplicationRecord do
 
     context 'when content exceeds the configured limit' do
       it 'adds an error' do
-        note = build(:note, content: 'a' * (described_class::MAX_TEXT_COLUMN_LENGTH + 1))
+        note = build(:note, account: account, contact: contact, content: 'a' * (described_class::MAX_TEXT_COLUMN_LENGTH + 1))
 
         expect(note).not_to be_valid
         expect(note.errors[:content]).to include("is too long (maximum is #{described_class::MAX_TEXT_COLUMN_LENGTH} characters)")
@@ -31,7 +34,7 @@ RSpec.describe ApplicationRecord do
       it 'enforces the configured limit' do
         stub_const('ApplicationRecord::MAX_TEXT_COLUMN_LENGTH', 3)
 
-        note = build(:note, content: 'a' * 4)
+        note = build(:note, account: account, contact: contact, content: 'a' * 4)
 
         expect(note).not_to be_valid
         expect(note.errors[:content]).to include('is too long (maximum is 3 characters)')
