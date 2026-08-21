a18361b74fb86c0269b6d0cbfe63a9667e73b7a1
Sojan Jose <sojan@pepalo.com>
2026-08-20 17:38:02 -0700

    fix: bound AI assignee backfill scans

--- db/migrate/20260811000000_add_ai_assignee_type_to_conversations.rb
+++ db/migrate/20260811000000_add_ai_assignee_type_to_conversations.rb
@@ -5,8 +5,10 @@ class AddAiAssigneeTypeToConversations < ActiveRecord::Migration[7.1]
     add_column :conversations, :ai_assignee_type, :string unless column_exists?(:conversations, :ai_assignee_type)
 
     # rubocop:disable Rails/SkipsModelValidations
-    Conversation.where(ai_assignee_type: nil).where.not(assignee_agent_bot_id: nil).in_batches(of: 1000) do |conversations|
-      conversations.update_all(ai_assignee_type: 'AgentBot')
+    Conversation.in_batches(of: 100_000, use_ranges: true) do |conversation_range|
+      conversation_range.where(ai_assignee_type: nil).where.not(assignee_agent_bot_id: nil).in_batches(of: 1000) do |conversations|
+        conversations.update_all(ai_assignee_type: 'AgentBot')
+      end
     end
     # rubocop:enable Rails/SkipsModelValidations
   end
