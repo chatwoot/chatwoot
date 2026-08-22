e17702d2eb4b11238a1ba3c222300e1e2658328b
Samuel Koesnadi <samuel@kira.id>
2026-08-22 23:43:50 +0700

    fixed docker-compose

--- app/controllers/api/v1/accounts/assignable_agents_controller.rb
+++ app/controllers/api/v1/accounts/assignable_agents_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Accounts::AssignableAgentsController < Api::V1::Accounts::BaseController
   def index
-    @assignable_agents = Current.account.users.where(id: Current.account.inbox_members.where(inbox_id: permitted_params[:inbox_ids]).select(:user_id))
+    @assignable_agents = Current.account.users.where(id: InboxMember.where(inbox_id: permitted_params[:inbox_ids]).select(:user_id))
   end
 
   private
