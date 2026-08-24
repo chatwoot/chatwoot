c2b1be8b33b69f746fc01638b451789ff03f283c
vaibhavmashal <vaibhavmashal098@gmail.com>
2026-08-24 23:06:58 +0530

    refactor: extract perform logic to helper methods to fix rubocop AbcSize

--- app/services/labels/update_service.rb
+++ app/services/labels/update_service.rb
@@ -2,30 +2,37 @@ class Labels::UpdateService
   pattr_initialize [:new_label_title!, :old_label_title!, :account_id!]
 
   def perform
-    tagged_conversations.find_in_batches do |conversation_batch|
-      conversation_batch.each do |conversation|
-        conversation.label_list.remove(old_label_title)
-        conversation.label_list.add(new_label_title)
-        conversation.save!
+    update_records(tagged_conversations, 'conversation.updated')
+    update_records(tagged_contacts, 'contact.updated')
+  end
 
-        event = Events::Base.new('conversation.updated', Time.zone.now, { conversation: conversation })
-        ActionCableListener.instance.conversation_updated(event)
-      end
-    end
+  private
 
-    tagged_contacts.find_in_batches do |contact_batch|
-      contact_batch.each do |contact|
-        contact.label_list.remove(old_label_title)
-        contact.label_list.add(new_label_title)
-        contact.save!
-
-        event = Events::Base.new('contact.updated', Time.zone.now, { contact: contact })
-        ActionCableListener.instance.contact_updated(event)
+  def update_records(records, event_name)
+    records.find_in_batches do |batch|
+      batch.each do |record|
+        update_record_labels(record)
+        broadcast_update(record, event_name)
       end
     end
   end
 
-  private
+  def update_record_labels(record)
+    record.label_list.remove(old_label_title)
+    record.label_list.add(new_label_title)
+    record.save!
+  end
+
+  def broadcast_update(record, event_name)
+    event_data = { record.class.name.downcase.to_sym => record }
+    event = Events::Base.new(event_name, Time.zone.now, event_data)
+
+    if record.is_a?(Conversation)
+      ActionCableListener.instance.conversation_updated(event)
+    else
+      ActionCableListener.instance.contact_updated(event)
+    end
+  end
 
   def tagged_conversations
     account.conversations.tagged_with(old_label_title)
