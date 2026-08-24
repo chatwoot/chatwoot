8151dfc632f3b5845642a8cdcb3da593c0584a3c
Shivam Mishra <scm.mymail@gmail.com>
2026-08-24 13:53:08 +0530

    refactor(web-crawling): add firecrawl spider

--- enterprise/app/services/firecrawl/configuration.rb
+++ enterprise/app/services/firecrawl/configuration.rb
@@ -1,31 +1,23 @@
 module Firecrawl::Configuration
-  INSTALLATION_CONFIG_KEY = 'CAPTAIN_FIRECRAWL_API_KEY'.freeze
-  EXCLUDE_TAGS = %w[iframe .sidebar .cookie-banner [role=navigation] [role=banner] [role=contentinfo]].freeze
-  DEFAULT_SCRAPE_MAX_AGE_MS = 7 * 24 * 60 * 60 * 1000
+  INSTALLATION_CONFIG_KEY = WebCrawling::Firecrawl::Configuration::INSTALLATION_CONFIG_KEY
+  EXCLUDE_TAGS = WebCrawling::Firecrawl::Configuration::EXCLUDE_TAGS
+  DEFAULT_SCRAPE_MAX_AGE_MS = WebCrawling::Firecrawl::Configuration::DEFAULT_SCRAPE_MAX_AGE_MS
 
   module_function
 
   def configured?
-    api_key.present?
+    WebCrawling::Firecrawl::Configuration.configured?
   end
 
   def client
-    key = api_key
-    raise ::Firecrawl::FirecrawlError, "#{INSTALLATION_CONFIG_KEY} is not configured" if key.blank?
-
-    ::Firecrawl::Client.new(api_key: key)
+    WebCrawling::Firecrawl::Configuration.client
   end
 
   def api_key
-    InstallationConfig.find_by(name: INSTALLATION_CONFIG_KEY)&.value
+    WebCrawling::Firecrawl::Configuration.api_key
   end
 
   def default_scrape_options(max_age: DEFAULT_SCRAPE_MAX_AGE_MS)
-    ::Firecrawl::Models::ScrapeOptions.new(
-      formats: ['markdown'],
-      only_main_content: true,
-      exclude_tags: EXCLUDE_TAGS,
-      max_age: max_age
-    )
+    WebCrawling::Firecrawl::Configuration.default_scrape_options(max_age: max_age)
   end
 end
