402723f467c470d9964bfd0ee5db74df1c5b126f
Sojan Jose <sojan@pepalo.com>
2026-08-28 00:18:27 -0700

    refactor(companies): move functionality to core

--- app/controllers/api/v1/accounts/contacts_controller.rb
+++ app/controllers/api/v1/accounts/contacts_controller.rb
@@ -171,7 +171,12 @@ class Api::V1::Accounts::ContactsController < Api::V1::Accounts::BaseController
   end
 
   def permitted_params
-    params.permit(:name, :identifier, :email, :phone_number, :avatar, :blocked, :avatar_url, additional_attributes: {}, custom_attributes: {})
+    permitted_params = params.permit(:name, :identifier, :email, :phone_number, :avatar, :avatar_url, :blocked, :company_id,
+                                     additional_attributes: {}, custom_attributes: {})
+    company_id = permitted_params.delete(:company_id)
+    return permitted_params unless Current.account.feature_enabled?('companies') && params.key?(:company_id)
+
+    permitted_params.merge(company_id: company_id.present? ? Current.account.companies.find(company_id).id : nil)
   end
 
   def contact_custom_attributes
