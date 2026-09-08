f50b4095cd2a3f18cc245b4f1e6fde7ae525af0a
Shivam Mishra <scm.mymail@gmail.com>
2026-09-08 20:53:36 +0530

    feat(captain): show new agent overview by default

--- app/javascript/dashboard/constants/localStorage.js
+++ app/javascript/dashboard/constants/localStorage.js
@@ -8,5 +8,4 @@ export const LOCAL_STORAGE_KEYS = {
   MESSAGE_REPLY_TO: 'messageReplyTo',
   RECENT_SEARCHES: 'recentSearches',
   SIDEBAR_MINIMIZED_SECTIONS: 'sidebarMinimizedSections',
-  CAPTAIN_OVERVIEW_V2: 'captain_overview_v2',
 };
