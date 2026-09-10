1c1a66a25293cb97bea3bf6b49076911066389df
Sojan Jose <sojan@pepalo.com>
2026-09-09 23:36:16 -0700

    fix: enforce entitlement for automatic Captain ownership

--- enterprise/app/models/enterprise/inbox.rb
+++ enterprise/app/models/enterprise/inbox.rb
@@ -13,7 +13,7 @@ module Enterprise::Inbox
   end
 
   def captain_active?
-    captain_assistant.present? && more_responses?
+    account.feature_enabled?('captain_integration') && captain_assistant.present? && more_responses?
   end
 
   private
