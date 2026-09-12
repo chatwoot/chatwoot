64880f0e61281de1de027957e63938897bf8ba3d
CoDe-ReDz <rastogisurya1@gmail.com>
2026-09-11 22:22:15 -0700

    Fix #15675: Inject voice_region into Twilio conference TwiML

--- enterprise/app/controllers/twilio/voice_controller.rb
+++ enterprise/app/controllers/twilio/voice_controller.rb
@@ -143,17 +143,17 @@ class Twilio::VoiceController < ApplicationController
     Twilio::TwiML::VoiceResponse.new.tap do |response|
       response.dial do |dial|
         dial.conference(
-          conference_sid,
-          start_conference_on_enter: agent_leg?(twilio_from),
-          end_conference_on_exit: false,
-          record: call.recording_enabled? ? 'record-from-start' : 'do-not-record',
-          recording_status_callback: recording_status_callback_url,
-          recording_status_callback_event: 'completed',
-          recording_status_callback_method: 'POST',
-          status_callback: conference_status_callback_url,
-          status_callback_event: 'start end join leave',
-          status_callback_method: 'POST',
-          participant_label: participant_label_for(twilio_from)
+          region: @channel.voice_region.presence,
+            start_conference_on_enter: agent_leg?(twilio_from),
+            end_conference_on_exit: false,
+            record: call.recording_enabled? ? 'record-from-start' : 'do-not-record',
+            recording_status_callback: recording_status_callback_url,
+            recording_status_callback_event: 'completed',
+            recording_status_callback_method: 'POST',
+            status_callback: conference_status_callback_url,
+            status_callback_event: 'start end join leave',
+            status_callback_method: 'POST',
+            participant_label: participant_label_for(twilio_from)
         )
       end
     end.to_s
