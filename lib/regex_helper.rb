ec6cc7040a84c46553a68771c932c5e4439e69ff
usamabhatti1998 <usamabhatti4061@gmail.com>
2026-09-14 10:09:03 +0500

    fix(labels): allow single-character label titles

--- lib/regex_helper.rb
+++ lib/regex_helper.rb
@@ -6,7 +6,7 @@ module RegexHelper
   # shouldn't start with a underscore or hyphen
   # \z (not \Z) anchors strictly to the end of the string -- \Z would also accept a single
   # trailing "\n", which would let a title like "hello_world\n" pass this validation.
-  UNICODE_CHARACTER_NUMBER_HYPHEN_UNDERSCORE = Regexp.new('\A[\p{L}\p{N}]+[\p{L}\p{N}_-]+\z')
+  UNICODE_CHARACTER_NUMBER_HYPHEN_UNDERSCORE = Regexp.new('\A[\p{L}\p{N}][\p{L}\p{N}_-]*\z')
   # Regex to match mention markdown links and extract display names
   # Matches: [@display name](mention://user|team/id/url_encoded_name)
   # Captures: 1) @display name (including emojis), 2) url_encoded_name
