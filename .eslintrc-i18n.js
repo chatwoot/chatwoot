8bbacb76462b4caba595fc00cd91f0b3d5b5f80a
Sojan Jose <sojan@pepalo.com>
2026-09-16 14:50:23 -0700

    chore(i18n): validate translation syntax in CI

---
+++ .eslintrc-i18n.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['plugin:@intlify/vue-i18n/base'],
+  settings: {
+    'vue-i18n': {
+      localeDir: './app/javascript/*/i18n/**/*.json',
+      messageSyntaxVersion: '^9.0.0',
+    },
+  },
+  rules: {
+    // Report locale catalogs also contain numeric option IDs.
+    '@intlify/vue-i18n/valid-message-syntax': ['error', { allowNotString: true }],
+  },
+};
