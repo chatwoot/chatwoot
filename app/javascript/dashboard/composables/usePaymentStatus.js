72976dc6cf0d65327b22b6f047a6f5baaf7e4fd7
Sony Mathew <2040199+sony-mathew@users.noreply.github.com>
2026-09-16 23:41:10 -0700

    feat(billing): add self-hosted payment warnings

--- app/javascript/dashboard/composables/usePaymentStatus.js
+++ app/javascript/dashboard/composables/usePaymentStatus.js
@@ -1,18 +1,31 @@
 import { computed } from 'vue';
 import { useAccount } from './useAccount';
 import { useAdmin } from './useAdmin';
+import { useMapGetter } from './store';
 
 export function usePaymentStatus() {
   const { accountId, currentAccount, isOnChatwootCloud } = useAccount();
-  const { isAdmin: canManagePayment } = useAdmin();
+  const { isAdmin } = useAdmin();
+  const currentUser = useMapGetter('getCurrentUser');
 
-  const isPastDue = computed(
-    () =>
-      isOnChatwootCloud.value &&
+  const isPastDue = computed(() => {
+    if (!isOnChatwootCloud.value) {
+      return (
+        currentAccount.value?.installation_subscription_status === 'past_due'
+      );
+    }
+    return (
       currentAccount.value?.billing_provider === 'stripe' &&
       currentAccount.value?.custom_attributes?.subscription_status ===
         'past_due'
+    );
+  });
+
+  const canManagePayment = computed(() =>
+    isOnChatwootCloud.value
+      ? isAdmin.value
+      : currentUser.value.type === 'SuperAdmin'
   );
 
-  return { accountId, isPastDue, canManagePayment };
+  return { accountId, isPastDue, canManagePayment, isOnChatwootCloud };
 }
