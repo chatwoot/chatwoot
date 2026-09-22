ed9a9fbf52fb0de4ada817871e96ae307f39db9b
Sojan Jose <sojan@pepalo.com>
2026-09-22 15:42:22 -0700

    fix(companies): preserve contact company parameter validation

--- app/controllers/api/v1/accounts/contacts_controller.rb
+++ app/controllers/api/v1/accounts/contacts_controller.rb
@@ -171,11 +171,11 @@ class Api::V1::Accounts::ContactsController < Api::V1::Accounts::BaseController
   end
 
   def permitted_params
-    permitted_params = params.permit(:name, :identifier, :email, :phone_number, :avatar, :avatar_url, :blocked, :company_id,
+    permitted_params = params.permit(:name, :identifier, :email, :phone_number, :avatar, :avatar_url, :blocked,
                                      additional_attributes: {}, custom_attributes: {})
-    company_id = permitted_params.delete(:company_id)
     return permitted_params unless Current.account.feature_enabled?('companies') && params.key?(:company_id)
 
+    company_id = params[:company_id]
     permitted_params.merge(company_id: company_id.present? ? Current.account.companies.find(company_id).id : nil)
   end
 
