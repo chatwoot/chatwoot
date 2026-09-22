85b93d82e6e265da02ec048d6f98c8d1b16394b4
Vishnu Narayanan <iamwishnu@gmail.com>
2026-09-22 12:08:43 -0700

    chore: drop stray model annotation churn

--- app/models/channel/facebook_page.rb
+++ app/models/channel/facebook_page.rb
@@ -4,7 +4,6 @@
 #
 #  id                :integer          not null, primary key
 #  page_access_token :string           not null
-#  provider_name     :string
 #  user_access_token :string           not null
 #  created_at        :datetime         not null
 #  updated_at        :datetime         not null
