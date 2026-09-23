282ab2d84acddf5322dfa3727db9179fbe47c7b9
Sojan Jose <sojan@pepalo.com>
2026-09-23 14:39:16 -0700

    refactor(companies): rely on existing route access pattern

--- app/javascript/dashboard/routes/dashboard/companies/routes.js
+++ app/javascript/dashboard/routes/dashboard/companies/routes.js
@@ -2,25 +2,6 @@ import { frontendURL } from '../../../helper/URLHelper';
 import CompaniesIndex from './pages/CompaniesIndex.vue';
 import CompanyDetailView from './pages/CompanyDetailView.vue';
 import { FEATURE_FLAGS } from '../../../featureFlags';
-import store from '../../../store';
-
-const ensureCompaniesEnabled = async to => {
-  const accountId = Number(to.params.accountId);
-  if (!store.getters['accounts/getAccount'](accountId).features) {
-    await store.dispatch('accounts/get', { accountId });
-  }
-
-  if (
-    !store.getters['accounts/isFeatureEnabledonAccount'](
-      accountId,
-      FEATURE_FLAGS.COMPANIES
-    )
-  ) {
-    return frontendURL(`accounts/${accountId}/dashboard`);
-  }
-
-  return true;
-};
 
 const commonMeta = {
   featureFlag: FEATURE_FLAGS.COMPANIES,
@@ -32,7 +13,6 @@ export const routes = [
     path: frontendURL('accounts/:accountId/companies'),
     component: CompaniesIndex,
     meta: commonMeta,
-    beforeEnter: ensureCompaniesEnabled,
     children: [
       {
         path: '',
@@ -46,7 +26,6 @@ export const routes = [
     path: frontendURL('accounts/:accountId/companies/:companyId'),
     component: CompanyDetailView,
     meta: commonMeta,
-    beforeEnter: ensureCompaniesEnabled,
     children: [
       {
         path: '',
