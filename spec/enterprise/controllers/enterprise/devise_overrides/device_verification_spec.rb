28ec8dda173ef9ddc1d70f82e58e22bfeecd54d8
Vishnu Narayanan <iamwishnu@gmail.com>
2026-09-23 14:27:12 -0700

    test: cover device verification rejecting a locked account

--- spec/enterprise/controllers/enterprise/devise_overrides/device_verification_spec.rb
+++ spec/enterprise/controllers/enterprise/devise_overrides/device_verification_spec.rb
@@ -114,6 +114,19 @@ RSpec.describe 'Device verification on sign-in', type: :request do
       expect(user.locked_at).to be_nil
     end
 
+    it 'rejects redemption when the account became locked after the challenge was issued' do
+      sign_in!
+      token = issued_token
+      code = emailed_codes.last
+      user.lock_access!
+
+      redeem!(token, code)
+
+      expect(response).to have_http_status(:bad_request)
+      expect(response.headers['access-token']).to be_nil
+      expect(user.reload.access_locked?).to be true
+    end
+
     it 'does not set the trusted-device cookie when remember_device is false' do
       sign_in!
       redeem!(issued_token, emailed_codes.last, remember_device: false)
